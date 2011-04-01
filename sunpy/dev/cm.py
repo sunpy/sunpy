e908efaa3ba4adcfa0a783374e1559a49e0f71b7
Keith Hughitt <keith.hughitt@nasa.gov>
2011-04-01 16:05:38 -0400

    Added some more documentation to cm module

--- sunpy/dev/cm.py
+++ sunpy/dev/cm.py
@@ -7,6 +7,8 @@
 #
 # <License info will go here...>
 #
+"""A module for dealing with static and dynamically-generated colormaps"""
+
 import matplotlib.colors
 import operator
 import numpy
@@ -15,20 +17,31 @@ import numpy
 # Things to try:
 #   use sub-sampling to speed up histogram generation
 #
-def log_adaptive(data, bits=12):
-    """Creates a custom color map scaled to the specified image data."""
+def log_adaptive(data, bits=12, vmin=1, vmax=2000):
+    """Creates a custom log-scaled color map scaled to the specified image data.
+    
+       In order increase contrast in those pixel ranges which occur
+       frequently a histogram is created for the log of the data and
+       those values which occur most often are given unique colors.
+       
+       The method needs to be tested on other AIA, etc. data. For the sample
+       image the color maps generated tend favor detail in the corona.
+       
+       So far little tweaking has been done though and there are probably
+       things that could improved. Feel free to try!
+    """
     
     # Number of entries in the color map
     cmap_size = 2 ** bits
     
     # Ignore negative values and outliers
-    data = numpy.log(data.clip(1, 2000))
+    data = numpy.log(data.clip(vmin, vmax))
     
     # Sort bins by frequency
     sorted_counts = _get_pixel_counts(data, cmap_size)
     indices = sorted([x[0] for x in sorted_counts[0:cmap_size - 1]])
     
-    # Take exponent of each indice
+    # Map it back to the actual data range
     indices = [numpy.exp(x) for x in indices]
     
     # Scale from 0 to 1
@@ -71,7 +84,17 @@ def _generate_cdict_for_indices(indices, cmap_size):
     return cdict
 
 def _get_pixel_counts(data, cmap_size):
-    """Returns a list of the pixel values sorted by frequency"""
+    """Returns a list of the pixel values sorted by frequency
+    
+           ex = [(0.00092784453851832058, 54033),
+                 (7.5999746150035641, 3616),
+                 (4.6347998824217607, 1186),
+                 (5.6273771261136147, 1178),
+                 (6.7257286471965561, 1169),
+                 ...,
+                 (1.7257286471965561, 1)]
+
+    """
     # Bin the data
     hist, bins = numpy.histogram(data, bins=cmap_size)
     
