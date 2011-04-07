c70345cbf5befdd25369ad8d460a9a531640318d
Keith Hughitt <keith.hughitt@gmail.com>
2011-04-07 07:52:46 -0400

    Moved OptParser code to BenchMarkTimer class.

--- benchmarks/time_test3_cuda.py
+++ benchmarks/time_test3_cuda.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import sys
+import benchmark
 import pycuda.autoinit
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
 import pycuda.curandom as curandom
 import scikits.cuda.linalg
 
-def time_test3_cuda(scale_factor=1):
+def main(argv):
+    """Main application"""
+    timer = benchmark.BenchmarkTimer()
+    
+    options = timer.parse_arguments()
+    timer.print_header("TIME_TEST3_CUDA")
+    
+    run_tests(timer, options.scale_factor)
+    
+    timer.print_summary()
+
+def run_tests(timer, scale_factor):
     """PyCUDA port of time_test3.pro"""
+    nofileio = True
+
+    #initialize time
+    timer.reset()   
+
     #
     # khughitt (2011/04/04): Non-CUDA tests from above will go here...
     #
@@ -33,3 +51,6 @@ def time_test3_cuda(scale_factor=1):
         b = scikits.cuda.linalg.transpose(a, pycuda.autoinit.device)
 
     time_test_timer('Transpose %d^2 byte, TRANSPOSE function x 100')
+    
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
