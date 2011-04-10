8fc0942c0e8bee5080e4f1ee50976671a8d67a69
Keith Hughitt <keith.hughitt@gmail.com>
2011-04-10 17:06:47 -0400

    Added a simple docstring to sunpy package itself

--- sunpy/__init__.py
+++ sunpy/__init__.py
@@ -1,3 +1,22 @@
+"""
+SunPy
+=====
+
+An open-source Python library for Solar Physics data analysis.
+
+Classes
+-------
+Sun
+    Solar-related constants
+Map
+    A spatially-aware 2d data array
+
+Available subpackages
+---------------------
+dev
+    Experimental functions not meant for use in production
+
+"""
 import dev
 import Sun
 from Map import Map
