9015ca44b7898e8eda86b2e0c3a8bb583b7a752a
Keith Hughitt <keith.hughitt@gmail.com>
2011-04-13 14:34:33 -0400

    Created a simple MapCube class that subclasses numpy.ndarray and behaves similar to the Map object. Moved header parsing methods to a separate 'data' package. May move things around again later..

--- sunpy/__init__.py
+++ sunpy/__init__.py
@@ -10,14 +10,21 @@ Sun
     Solar-related constants
 Map
     A spatially-aware 2d data array
+MapCube
+    A spatially-aware 3d data array
+
 
 Available subpackages
 ---------------------
 dev
     Experimental functions not meant for use in production
+data
+    Methods relating to the parsing of common solar data formats
 
 """
 import dev
+import data
 import Sun
 from Map import Map
+from MapCube import MapCube
 
