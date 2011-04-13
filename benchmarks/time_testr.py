2527dcbbc825f836272420d7144aa7e026009156
Keith Hughitt <keith.hughitt@gmail.com>
2011-04-13 13:29:51 -0400

    Few small changes to time_testr

--- benchmarks/time_testr.py
+++ benchmarks/time_testr.py
@@ -43,53 +43,58 @@ from optparse import IndentedHelpFormatter
 
 def main(argv):
     """Main application"""
-    options = parse_arguments()
-    run_tests(options.scale_factor)
+    timer = benchmark.BenchmarkTimer()
+   
+    options = timer.parse_arguments()
+    timer.print_header("TIME_TESTR(ichard)")
+    
+    run_tests(timer, options.scale_factor)
+    
+    timer.print_summary()
 
-def run_tests(scale_factor):
+def run_tests(timer, scale_factor):
     '''Go through each test and print out the results'''
     nofileio = True
-    
-    timer = benchmark.BenchmarkTimer()
-    timer.print_header("TIME_TESTR(Ichard)")
 
-    #initialize time
-    timer.reset()   
-    
     siz = 500
-    a = np.arange(siz**2, dtype=np.uint8).reshape(siz, siz)
-    b = a
+    a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+    b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+    
+    #initialize time
+    timer.reset()
 
     #Test 1 - Matrix Multiplication Large Arrays (500,500) 10*scale_factor times
     nrep = 10 * scale_factor
     for i in xrange(nrep):
-        c = np.dot(a,b)
+        c = np.dot(a, b)
     timer.log("Matrix Multiplication Large Arrays (500,500) %d times" % nrep)
 
     #Test 2 - Matrix Multiplication Small Array (50,50) 10000*scale_factor times
     siz = 50
-    a = np.arange(siz**2, dtype=np.uint8).reshape(siz, siz)
-    b = a
-
+    a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+    b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
     nrep = 10000 * scale_factor
+    
+    timer.reset()
     for i in xrange(nrep):
         c = np.dot(a,b) 
-        
     timer.log("Matrix Multiplication Small Array (50,50) %d times" % nrep)
     
     #Test 3 - Sorting 1 million elements 10*scale_factor times
     nrep = 10 * scale_factor
-    a = np.random.uniform(0, 1, 1e6).astype(np.float32)
-    # the first sort changes the array so all other sorts are worthless
-    # need to sort new mixed up arrays each time
+    a = [np.random.uniform(0, 1, 1e6).astype(np.float32) for i in range(nrep)]
+    timer.reset()
+
     for i in xrange(nrep):
-        a.sort()
+        a[i].sort()
     timer.log("Sorting 1 million elements %d times" % nrep)
     
     #Test 4 - Moving 1 million elements 1000*scale_factor times
     a = np.random.uniform(0, 1, 1e6).astype(np.float32)
     c = np.zeros(1e6)
     nrep = 1000 * scale_factor
+    timer.reset()
+    
     for i in xrange(nrep):
         c = a    
     timer.log("Moving 1 million elements %d times" % nrep)
@@ -97,24 +102,31 @@ def run_tests(scale_factor):
     # Test 5 - indirect addressing 1 million elements 100*scale_factor times
     # this test does not seem right, should really create a random index
     # which accesses the elements
-    a = 1e6*np.random.uniform(0, 1, 1e6).astype(np.float32)
+    a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
     b = np.zeros(1e6).astype(np.uint32)
     nrep = 100 * scale_factor
+    timer.reset()
+    
     for i in xrange(nrep):
         c = a[b]
     timer.log('Indirect addressing 1 million elements %d times' % nrep)
     
     #Test 6 - Shift 512 by 512 byte and store
-    a = 1e6*np.random.uniform(0, 1, 1e6).astype(np.float32)
-    c = 1e6*np.random.uniform(0, 1, 1e6).astype(np.float32)
+    a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
+    c = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
     nrep = 100 * scale_factor
+    timer.reset()
+    
     for i in xrange(nrep):
-        c = np.roll(b, 12, axis=0)
+        #c = np.roll(b, 12, axis=0)
+        c = np.roll(a, 12, axis=0)
     timer.log('Shifting 1 million elements %d times' % nrep)
  
     #Test 7 - Cosine 1 million elements 100*scale_factor times
     nrep = 10 * scale_factor
-    a = 1e6*np.random.uniform(0, 1, 1e6).astype(np.float32)
+    a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
+    timer.reset()
+    
     for i in xrange(nrep):
         c = np.cos(a)
     timer.log('Cosine 1 million elements %d times' % nrep)
@@ -128,7 +140,9 @@ def run_tests(scale_factor):
     #Test 9 - Alog 1 million elements 100*scale_factor
     a = np.arange(1e6).astype(np.uint8)
     b = np.zeros(1e6).astype(np.uint8)
-    nrep = 100*scale_factor
+    nrep = 100 * scale_factor
+    timer.reset()
+
     for i in xrange(nrep):
         np.save('test.npy', a)
         b = np.load('test.npy')
