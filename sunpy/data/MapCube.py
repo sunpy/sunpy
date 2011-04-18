3d22a5e6491cc169bb86c94e79620990e8f91fc0
Keith Hughitt <hughitt1@Europa>
2011-04-18 06:10:00 -0400

    Updated MapCube to make store MapSlices (data-less Maps) instead of simply storing the original headers for each image.

--- sunpy/data/MapCube.py
+++ sunpy/data/MapCube.py
@@ -7,6 +7,9 @@ __email__ = "keith.hughitt@nasa.gov"
 
 import numpy as np
 import pyfits
+from BaseMap import BaseMap
+from BaseMap import UnrecognizedDataSouce
+from sources import *
 
 #
 # 2011/04/13: Should Map be broken up into Map and MapHeader classes? This way
@@ -41,18 +44,23 @@ class MapCube(np.ndarray):
     >>> mapcube = sunpy.MapCube('images/')
 
     """
-    def __new__(cls, input_):
+    def __new__(cls, input_, coalign=False, derotate=False):
         """Creates a new Map instance"""
         if isinstance(input_, str):
             headers, data = self._parseFiles(input_)
 
             obj = np.asarray(data).view(cls)
-            obj.headers = headers
+            obj.slices = slices
             
         elif isinstance(input_, list):
             obj = np.asarray(input_).view(cls)
         elif isinstance(input_, np.ndarray):
             obj = input_
+            
+        if coalign:
+            self._coalign()
+        if derotate:
+            self.derotate()
             
         return obj
         
@@ -62,20 +70,41 @@ class MapCube(np.ndarray):
         Reads in the files at the specified location, stores their headers,
         and creates a 3d array from their contents
         """
-        data    = []
-        headers = []
+        data = []
+        slices = []
         
         for input_ in os.listdir(directory):
             try:
                 fits = pyfits.open(input_)
                 data.append(fits[0].data)
-                headers.append(fits[0].header)
+                slices.append(self._parse_header(fits[0].header))                
             except IOError:
                 sys.exit("Unable to read the file %s" % input_)
                 
-        return headers, data
+        return slices, data
+    
+    def _parse_header(self):
+        """Returns a MapSlice instance corresponding to an image header.
+        
+        Attempts to construct a MapSlice instance using the header information
+        provided. MapSlice instances (e.g. AIAMapSlice) are basically just
+        empty (data-less) Map objects. This provides a way to keep track of
+        the meta-information for each of the images that were used to build
+        the MapCube separately from the data. 
+        """
+        for cls in BaseMap.__subclasses__():
+            if cls.is_datasource_for(fits[0].header):
+                return cls.as_slice(header)
+        raise UnrecognizedDataSouce
+    
+    def _coalign(self):
+        """Coaligns the layers in the MapCube"""
+        pass
+    
+    def derotate(self):
+        """Derotates the layers in the MapCube"""
+        pass
             
     def __array_finalize__(self, obj):
         """Finishes instantiation of the new map object"""
         if obj is None: return
-
