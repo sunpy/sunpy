daba03df3df8cc0fa922476d13a96ad37daceb57
Florian Mayer <florian.mayer@bitsrc.org>
2011-08-06 19:55:20 +0200

    Add VSO download service.

---
+++ sunpy/net/download.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+# Author: Florian Mayer <florian.mayer@bitsrc.org>
+
+import os
+import urllib2
+import select
+import socket
+import threading
+import itertools
+
+from collections import defaultdict, deque
+
+class IDPool(object):
+    """
+    Pool that returns unique identifiers in a thread-safe way.
+    
+    Identifierers obtained using the get method are guaranteed to not be
+    returned by it again until they are released using the release method.
+    
+        >>> pool = IDPool()
+        >>> pool.get()
+        0
+        >>> pool.get()
+        1
+        >>> pool.get()
+        2
+        >>> pool.release(1)
+        >>> pool.get()
+        1
+        >>> 
+    """
+    def __init__(self):
+        self.max_id = -1
+        self.free_ids = []
+        
+        self._lock = threading.Lock()
+    
+    def get(self):
+        """ Return a new integer that is unique in this pool until
+        it is released. """
+        self._lock.acquire()
+        try:
+            if self.free_ids:
+                return self.free_ids.pop()
+            else:
+                self.max_id += 1
+                return self.max_id
+        finally:
+            self._lock.release()
+    
+    def release(self, id_):
+        """ Release the id. It can now be returned by get again.
+        
+        Will reset the IDPool if the last id in use is released. """
+        self._lock.acquire()
+        try:
+            self.free_ids.append(id_)
+            if len(self.free_ids) == self.max_id + 1:
+                self.reset()
+        finally:
+            self._lock.release()
+    
+    def reset(self):
+        """ Reset the state of the IDPool. This should only be called when
+        no identifier is in use. """
+        self.max_id = -1
+        self.free_ids = []
+
+
+def socketpair():
+    """ Return pair of connected sockets. Unlike socket.socketpair this
+    is platform independant. However, if socket.socketpair is available,
+    it is used here as well. """
+    if hasattr(socket, 'socketpair'):
+        # Unix.
+        return socket.socketpair()
+    
+    try:
+        acceptor = socket.socket()
+        # Random port. Only accept local connections.
+        acceptor.bind(('127.0.0.1', 0))
+        # We know we'll only get one connection.
+        acceptor.listen(1)
+
+        one = socket.socket()
+        one.connect(acceptor.getsockname())
+        
+        other = acceptor.accept()[0]
+    finally:
+        acceptor.close()
+    return one, other
+
+
+class Reactor(object):
+    def __init__(self):
+        self.syncr, self.synce = socketpair()
+        self.ids = IDPool()
+        self.call = []
+        self.tcalls = {}
+        self.callb = {}
+        self.running = False
+    
+    def _unset_running(self):
+        self.running = False
+    
+    def stop(self):
+        self.call_sync(self._unset_running)
+    
+    def run(self):
+        self.running = True
+        while self.running:
+            ret = self.poll()
+            self._call_calls()
+            self._call_tcalls()
+            for fd in ret:
+                try:
+                    fun, args, kwargs = self.callb[fd]
+                except KeyError:
+                    continue
+                fun(*args, **kwargs)
+    
+    def poll(self):
+        raise NotImplementedError
+    
+    def call_sync(self, fun, args=None, kwargs=None):
+        self.call.append(
+            (fun,
+             [] if args is None else args,
+             {} if kwargs is None else kwargs
+             )
+        )
+        self.synce.send('m')
+    
+    def _call_tcalls(self):
+        for call in list(self.tcalls.itervalues()):
+            fun, args, kwargs = call
+            fun(*args, **kwargs)
+    
+    def add_tcall(self, fun, args=None, kwargs=None):
+        id_ = self.ids.get()
+        self.tcalls[id_] = (
+            fun, [] if args is None else args, {} if kwargs is None else kwargs
+        )
+        return id_
+    
+    def rem_tcall(self, id_):
+        del self.tcalls[id_]
+        self.ids.release(id_)
+    
+    def _call_calls(self):
+        for call in self.call:
+            fun, args, kwargs = call
+            fun(*args, **kwargs)
+            self.syncr.recv(1)
+        self.calls = []
+    
+    def add_fd(self, fd, callback, args=None, kwargs=None):
+        self.callb[fd] = (
+            callback,
+            [] if args is None else args,
+            {} if kwargs is None else kwargs
+        )
+    
+    def rem_fd(self, fd):
+        del self.callb[fd]
+
+
+class SelectReactor(Reactor):
+    avail = hasattr(select, 'select')
+    def __init__(self, fds=None):
+        Reactor.__init__(self)
+        self.fds = [] if fds is None else fds
+        self.fds.append(self.syncr)
+    
+    def poll(self):
+        return select.select(self.fds, [], [])[0]
+    
+    def add_fd(self, fd, callback, args=None, kwargs=None):
+        super(SelectReactor, self).add_fd(fd, callback, args, kwargs)
+        self.fds.append(fd)
+        
+    def rem_fd(self, fd):
+        super(SelectReactor, self).rem_fd(fd)
+        self.fds.remove(fd)
+    
+    def close(self):
+        pass
+
+
+# FIXME: Add [E]PollReactor and KQueueReactor.
+DReactor = None
+for reactor in [SelectReactor]:
+    if reactor.avail:
+        DReactor = reactor
+
+if DReactor is None:
+    raise EnvironmentError
+
+
+class Downloader(object):
+    def __init__(self, max_conn=5, max_total=20):
+        self.max_conn = max_conn
+        self.max_total = max_total
+        self.conns = 0
+        
+        self.connections = defaultdict(int) # int() -> 0
+        self.q = defaultdict(deque)
+        
+        self.reactor = DReactor()
+        self.buf = 9096
+    
+    def download(self, sock, fd, callback, id_=None):
+        rec = sock.read(self.buf)
+        if not rec:
+            fun, args, kwargs = callback
+            fun(*args, **kwargs)
+            
+            if id_ is not None:
+                self.reactor.rem_tcall(id_)
+            else:
+                self.reactor.rem_fd(sock)
+            fd.close()
+        else:
+            fd.write(rec)
+    
+    def _start_download(self, url, path, callback=None):
+        server = url.split('/')[0]
+        
+        self.connections[server] += 1
+        self.conns += 1
+        
+        sock = urllib2.urlopen(url)
+        sock.setblocking = lambda x: None
+        name = sock.headers.get('Content-Disposition', url.rsplit('/', 1)[-1])
+        fullname = os.path.join(path, name)
+        
+        try:
+            sock.fileno()
+        except AttributeError:
+            nofileno = True
+        else:
+            nofileno = False
+        
+        if nofileno:
+            args = [
+                    sock, open(fullname, 'w'),
+                    (self.close, [callback, {'path': fullname}, server], {}),
+                ]
+            id_ = self.reactor.add_tcall(self.download, args)
+            args.append(id_)
+        else:
+            self.reactor.add_fd(
+                sock,
+                self.download, 
+                [
+                    sock, open(fullname, 'w'),
+                    (self.close, [callback, {'path': fullname}, server], {})
+                ]
+            )
+    
+    def _attempt_download(self, url, path, callback=None):
+        server = url.split('/')[0]
+        
+        if self.connections[server] < self.max_conn and self.conns < self.max_total:
+            self._start_download(url, path, callback)
+            return True
+        return False
+
+    def download_topath(self, url, path, callback=None):
+        server = url.split('/')[0]
+        
+        if not self._attempt_download(url, path, callback):
+            self.q[server].append((url, path, callback))
+    
+    def close(self, callback, args, server):
+        callback(*args)
+        
+        if self.q[server]:
+            self._start_download(*self.q[server].pop())
+        else:
+            self.connections[server] -= 1
+            self.conns -= 1
+            
+            
+            for k, v in self.q.iteritems():
+                while v:
+                    if self._attempt_download(*v[0]):
+                        v.pop()
+                        if self.conns == self.max_total:
+                            return
+                    else:
+                        break
+
+
+if __name__ == '__main__':
+    def foo(handler):
+        print 'Hello'
+        foo.n += 1
+        if foo.n == 3:
+            dw.reactor.stop()
+    
+    foo.n = 0
+    
+    import tempfile
+    
+    dir_ = tempfile.mkdtemp()
+    print dir_
+    
+    dw = Downloader(1, 2)
+    dw.download_topath('ftp://speedtest.inode.at/speedtest-5mb', dir_, foo)
+    dw.download_topath('ftp://speedtest.inode.at/speedtest-20mb', dir_, foo)
+    dw.download_topath('https://bitsrc.org', dir_, foo)
+    dw.download_topath('ftp://speedtest.inode.at/speedtest-100mb', dir_, foo)
+    
+    print dw.conns
+    
+    dw.reactor.run()
---
+++ sunpy/net/vso.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+# Author: Florian Mayer <florian.mayer@bitsrc.org>
+
+import sys
+import os
+from collections import defaultdict
+
+from suds import client
+
+DEFAULT_URL = 'file://' + os.path.abspath('./VSOi_rpc_literal.wsdl')
+TIMEFORMAT = '%Y%m%d%H%M%S'
+
+
+from suds.transport import Reply
+from suds.transport.https import HttpAuthenticated
+
+class LoopbackTransport(HttpAuthenticated):
+    def send(self, request):
+        return Reply(200, {}, request.message)
+
+
+loopback = client.Client(DEFAULT_URL, transport=LoopbackTransport(), retxml=True)
+
+
+class _Attr(object):
+    def __and__(self, other):
+        return _AttrAnd([self, other])
+
+
+class _AttrAnd(_Attr):
+    def __init__(self, attrs):
+        self.attrs = attrs
+    
+    def __and__(self, other):
+        return _AttrAnd(self.attrs + [other])
+    
+    def apply(self, queryblock):
+        for attr in self.attrs:
+            attr.apply(queryblock)
+
+
+class _ComplexAttr(_Attr):
+    def __init__(self, attr, attrs):
+        self.attr = attr
+        self.attrs = attrs
+    
+    def apply(self, queryblock):
+        for name in self.attr:
+            queryblock = getattr(queryblock, name)
+        
+        for k, v in self.attrs.iteritems():
+            queryblock[k] = v
+
+
+class _SimpleAttr(_Attr):
+    def __init__(self, field, value):
+        self.field = field
+        self.value = value
+    
+    def apply(self, queryblock):
+        queryblock[self.field] = self.value
+
+# ----------------------------------------
+
+class Wave(_ComplexAttr):
+    def __init__(self, wavemin, wavemax, waveunit):
+        _ComplexAttr.__init__(self, ['wave'], {
+            'wavemin': wavemin,
+            'wavemax': wavemax,
+            'waveunit': waveunit,
+        })
+
+
+class Time(_ComplexAttr):
+    def __init__(self, start, end, near=None):
+        _ComplexAttr.__init__(self, ['time'], {
+            'start': start.strftime(TIMEFORMAT),
+            'end': end.strftime(TIMEFORMAT),
+            'near': near.strftime(TIMEFORMAT) if near is not None else '',
+        })
+
+
+class Extent(_ComplexAttr):
+    def __init__(self, x, y, width, length, type_):
+        _ComplexAttr.__init__(self, ['extent'], {
+            'x': x,
+            'y': y,
+            'width': width,
+            'length': length,
+            'type': type_,
+        })
+
+
+class Field(_ComplexAttr):
+    def __init__(self, fielditem):
+        _ComplexAttr.__init__(self, ['field'], {
+            'fielditem': fielditem
+        })
+
+
+def  _mk_simpleattr(field):
+    """ Create a _SimpleField class for field. """
+    class _foo(_SimpleAttr):
+        def __init__(self, arg):
+            _SimpleAttr.__init__(self, field, arg)
+    _foo.__name__ = field.capitalize()
+    return _foo
+
+
+for elem in ['provider', 'source', 'instrument', 'physobs', 'pixels',
+             'level', 'resolution', 'detector', 'filter', 'sample',
+             'quicklook', 'pscale']:
+    setattr(sys.modules[__name__], elem.capitalize(), _mk_simpleattr(elem))
+
+# ----------------------------------------
+
+
+class API(object):
+    method_order = [
+        'URL-TAR_GZ', 'URL-ZIP', 'URL-TAR', 'URL-FILE', 'URL-packaged'
+    ]
+    def __init__(self, url=DEFAULT_URL, *args):
+        self.api = client.Client(url, *args)
+        self.api.set_options(port='nsoVSOi')
+    
+    def query(self, query):
+        queryreq = self.api.factory.create('QueryRequest')
+        query.apply(queryreq.block)
+        return self.api.service.Query(queryreq)
+    
+    def make_getdatarequest(self, response, methods=None):
+        if methods is None:
+            methods = self.method_order + ['URL']
+        
+        return self.create_getdatarequest(
+            dict((k, [x.fileid for x in v])
+                 for k, v in self.by_provider(response).iteritems()),
+            methods
+        )
+    
+    def create_getdatarequest(self, map_, methods, info=None):
+        if info is None:
+            info = {}
+        
+        request = self.api.factory.create('VSOGetDataRequest')
+        r = request.request
+        r.method.methodtype.extend(methods)
+        
+        for k, v in info.iteritems():
+            r.info[k] = v
+        
+        for k, v in map_.iteritems():
+            datarequest = self.api.factory.create('DataRequestItem')
+            datarequest.provider = k
+            datarequest.fileiditem.fileid = v
+            r.datacontainer.datarequestitem.append(datarequest)
+        
+        return request
+    
+    def download_all(self, response, methods, info=None):
+        map_ = {}
+        for dresponse in response.getdataresponseitem:
+            code = (
+                dresponse.status[5:8] if hasattr(dresponse, 'status') else '200'
+            )
+            if code == '200':
+                for dataitem in dresponse.getdataitem.dataitem:
+                    path = self.download(
+                        dresponse.method.methodtype[0],
+                        dataitem.url
+                    )
+                    for fileid in dataitem.fileiditem.fileid:
+                        map_[fileid] = path
+            elif code == '300' or code == '412':
+                files = []
+                for dataitem in dresponse.getdataitem.dataitem:
+                    for fileid in dataitem.fileiditem.fileid:
+                        files.append(fileid)
+                if code == '300':
+                    try:
+                        methods = self.multiple_choices(
+                            dresponse.method.methodtype, dresponse
+                        )
+                    except ValueError:
+                        # TODO: Log.
+                        continue
+                elif code == '412':
+                    info = self.missing_information(
+                        info, dresponse.info
+                    )
+                request = self.create_getdatarequest(
+                    {dresponse.provider: files}, methods, info
+                )
+                
+                map_.update(
+                    self.download_all(
+                        self.api.service.GetData(request), methods, info
+                    )
+                )
+        return map_
+    
+    def download(self, method, url):
+        print method, url
+    
+    @staticmethod
+    def by_provider(response):
+        map_ = defaultdict(list)
+        for prov_item in a.provideritem:
+            for record_item in prov_item.record.recorditem:
+                map_[record_item.provider].append(record_item)
+        return map_
+    
+    def multiple_choices(self, choices, response):
+        for elem in self.method_order:
+            if elem in choices:
+                return [elem]
+        raise ValueError
+    
+    def missing_information(self, info, field):
+        print info, field
+        return {'EMAIL': 'segfaulthunter@gmail.com'}
+
+
+# TODO: class InteractiveAPI(API)
+
+if __name__ == '__main__':
+    from datetime import datetime
+    api = API()
+    
+    a = api.query(
+        Time(datetime(2010, 1, 1), datetime(2010, 1, 1, 1)) & Instrument('eit')
+    )
+    
+    print api.download_all(
+        api.api.service.GetData(api.make_getdatarequest(a, ['URL'])),
+        ['STAGING-ZIP']
+    )
