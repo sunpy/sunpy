d2fc9aa840f18779e2350848a5ebb1061492364b
Florian Mayer <florian.mayer@bitsrc.org>
2011-09-05 23:00:22 +0200

    Make imports in __init__ absolute.

--- sunpy/__init__.py
+++ sunpy/__init__.py
@@ -22,14 +22,16 @@ map
 """
 #pylint: disable=W0401,W0622
 
+from __future__ import absolute_import
+
 __version__ = 0.01
 
-import map
-import data.sample
-from map import Map
-from map.mapcube import MapCube
-from map.compositemap import CompositeMap
-from cm import *
+import sunpy.map
+import sunpy.data.sample
+from sunpy.map import Map
+from sunpy.map.mapcube import MapCube
+from sunpy.map.compositemap import CompositeMap
+from sunpy.cm import *
 
 # Sample data
-from data.sample import AIA_171_IMAGE, RHESSI_IMAGE
+from sunpy.data.sample import AIA_171_IMAGE, RHESSI_IMAGE
