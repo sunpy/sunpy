7713a3161b54a4c95c054f5967c659c0a22bfd92
Florian Mayer <florian.mayer@bitsrc.org>
2011-09-05 02:09:30 +0200

    Rename BaseMap.plot to .show and add .plot.

--- sunpy/map/basemap.py
+++ sunpy/map/basemap.py
@@ -71,10 +71,10 @@ class BaseMap(np.ndarray):
     [ 0.    ,  0.    , -1.1875, ...,  0.125 ,  0.    ,  0.6875]])
     >>> aia.header.get('cunit1')
     'arcsec'
-    >>> aia.plot()
+    >>> aia.show()
     >>> import matplotlib.cm as cm
     >>> import matplotlib.colors as colors
-    >>> aia.plot(cmap=cm.hot, norm=colors.Normalize(1, 2048))
+    >>> aia.show(cmap=cm.hot, norm=colors.Normalize(1, 2048))
     
     See Also:
     ---------
@@ -300,7 +300,11 @@ class BaseMap(np.ndarray):
             
         plt.imshow(self, origin='lower', extent=extent, **params)
         plt.colorbar()
-        plt.show()
+        return plt
+    
+    def show(self, draw_limb=False, **matplot_args):
+        self.plot(draw_limb, **matplot_args).show()
+
 
 class UnrecognizedDataSouceError(ValueError):
     """Exception to raise when an unknown datasource is encountered"""
