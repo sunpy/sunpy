17096c3e4bf4e9639a58852dd48bcb772831ecc6
steven christe <steven.d.christe@nasa.gov>
2011-09-06 09:58:42 -0400

    updates to solwcs and added sample eit file

--- sunpy/solwcs/solwcs.py
+++ sunpy/solwcs/solwcs.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 
 """
 Solar WCS provides functions to parse a World Coordinate System (WCS) fits header.
+
+Note that SOLAR_B0, HGLT_OBS, and CRLT_OBS are all synonyms.
 """
 
 __authors__ = ["Steven Christe"]
@@ -17,6 +19,8 @@ def solar_limb(header):
     # check to see if the answer is provided in the fits header itself
     rsun_obs = header.get('RSUN_OBS')
     
+    # As a last resort just set it to a default value
+    # TODO: don't hardcode this number
     if rsun_obs is None: rsun_obs = 960.0
     
     return rsun_obs
@@ -30,7 +34,7 @@ def observer_position(header):
     return dsun_obs
 
 def get_center(header, axis = None):
-    
+    """Return the center of the map."""
     x = header.get('cdelt1')*header.get('naxis1')/2 + header.get('crval1') - header.get('crpix1')*header.get('cdelt1')
     y = header.get('cdelt2')*header.get('naxis2')/2 + header.get('crval2') - header.get('crpix2')*header.get('cdelt2')
     
@@ -39,7 +43,7 @@ def get_center(header, axis = None):
     if axis is None: return [x,y]
     
 def get_units(header, axis = None):
-    
+    """Return the units used for crpix, crdelt in the header."""
     xunits = header.get('cunit1')
     yunits = header.get('cunit2')
     
@@ -49,3 +53,38 @@ def get_units(header, axis = None):
     if axis is 'x': return xunits
     if axis is 'y': return yunits
     if axis is None: return [xunits,yunits]
+    
+def get_scale(header, axis = None):
+    """Return the units used for crpix, crdelt in the header."""
+    xscale = header.get('cdelt1')
+    yscale = header.get('cdelt2')
+    
+    if axis is 'x': return xunits
+    if axis is 'y': return yunits
+    if axis is None: return [xunits,yunits]
+    
+def get_solar_b0(header):
+    """Return the solar B0 angle which is simply the heliographic latitude of the observer."""
+    
+    # check to see if the answer is provided by the header
+    solar_b0 = header.get('HGLT_OBS')
+    
+    # check in another place
+    if solar_b0 is None: solar_b0 = header.get('CRLT_OBS')
+    
+    # As a last resort just set it to a default value
+    if solar_b0 is None: solar_b0 = 0
+    
+    return solar_b0
+
+def get_carrington_heliographic_longitude(header):
+    "Return the Carrington heliographic longitude of the observer."
+    
+    # check to see if the answer is provided by the header
+    crln_obs = header.get('CRLN_OBS')
+
+    # TODO: as a last resort, assume the observer is at Earth
+    crln_obs = 0
+    
+    return crln_obs
+    
