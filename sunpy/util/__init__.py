f16c4a97c4da2faa034e15d78b406a0823550ec4
Keith Hughitt <keith.hughitt@gmail.com>
2011-09-07 12:38:26 -0400

    Added SUDS to sunpy.util.system_info()

--- sunpy/util/__init__.py
+++ sunpy/util/__init__.py
@@ -69,6 +69,11 @@ def system_info():
         pyfits_version = "NOT INSTALLED"
         
     try:
+        from suds import __version__ as suds_version
+    except ImportError:
+        suds_version = "NOT INSTALLED"
+        
+    try:
         from PyQt4.QtCore import PYQT_VERSION_STR as pyqt_version
     except ImportError:
         pyqt_version = "NOT INSTALLED"
@@ -81,5 +86,6 @@ def system_info():
     print("NumPy: %s" % numpy_version)
     print("SciPy: %s" % scipy_version)
     print("Matplotlib: %s" % matplotlib_version)
-    print("PyFITS: %s\n" % pyfits_version)
+    print("PyFITS: %s" % pyfits_version)
+    print("SUDS: %s" % suds_version)
     print("PyQt: %s\n" % pyqt_version)
