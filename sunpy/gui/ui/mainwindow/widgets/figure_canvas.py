72a4572e78368f5b93789d7e6b9a044212f13f93
Matt Earnshaw <matt@earnshaw.org.uk>
2011-09-16 15:32:38 +0100

    Fixed memory issue.

--- sunpy/gui/ui/mainwindow/widgets/figure_canvas.py
+++ sunpy/gui/ui/mainwindow/widgets/figure_canvas.py
@@ -10,6 +10,7 @@ Author: Matt Earnshaw <matt@earnshaw.org.uk>
 
 from PyQt4.QtGui import QSizePolicy
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
+from matplotlib import pyplot
 
 
 class FigureCanvas(FigureCanvasQTAgg):
@@ -19,12 +20,16 @@ class FigureCanvas(FigureCanvasQTAgg):
         self.figure = figure
         FigureCanvasQTAgg.__init__(self, self.figure)
         FigureCanvasQTAgg.setSizePolicy(self,
-                                   QSizePolicy.MinimumExpanding,
-                                   QSizePolicy.MinimumExpanding)
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
         FigureCanvasQTAgg.updateGeometry(self)
 
     def update_figure(self, figure):
+        pyplot.close() # Destroy any previous figures... :(
         self.figure = figure
+
+        # This is required to get plot to size correctly...
         self.updateGeometry()
         self.resize(1, 1)
+
         self.draw()
