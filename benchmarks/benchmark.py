d062d596556d6d982f176fc998ec386138e64506
Florian Mayer <florian.mayer@bitsrc.org>
2011-09-22 02:03:20 +0200

    Rewrite benchmark suite.

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -6,70 +6,135 @@ import time
 import datetime
 import platform
 import math
+import contextlib
+import inspect
 from optparse import OptionParser
 from optparse import IndentedHelpFormatter
+from collections import defaultdict
 
-class BenchmarkTimer:
-    """A simple benchmark timer class"""
+def stdist(values):
+    ln = float(len(values))
+    avg = sum(values) / ln
+    stdev = math.sqrt(1 / (ln - 1) * sum((v - avg) ** 2 for v in values))
+    return avg, stdev
+
+
+class StopWatch(object):
     def __init__(self):
-        """Instantiates a new BenchmarkTimer"""
-        self.start_time = 0.0
-        self.total_time = 0.0
-        self.geom_time = 0.0
-        self.test_num = 0
-        
-    def log(self, msg):
-        """Print out a string with time taken for test"""
-        end_time = time.time()
-        
-        self.test_num += 1
-        
-        elapsed_time = end_time - self.start_time
-        
-        self.total_time += elapsed_time
-        self.geom_time += math.log(elapsed_time)
-        
-        output = '\t%d\t%f\t%s' % (self.test_num, elapsed_time, msg)
-        print(output)
+        self._start = None
+        self.elapsed = 0
+    
+    def start(self):
+        self._start = time.time()
+        return self
+    
+    def stop(self, *args):
+        self.elapsed += time.time() - self._start
+    
+    __enter__ = start
+    __exit__ = stop
+    
+    def _pausefun(self):
+        self.stop()
+        yield
+        self.start()
+    
+    def paused(self):
+        return contextlib.contextmanager(self._pausefun)()
 
-        self.start_time = time.time()
 
-    def reset(self):
-        """Reset the timer"""
-        self.start_time = time.time()
+class BenchmarkResult(object):
+    def __init__(self, bench, results=None, scaled=None):
+        if results is None:
+            results = []
+        self.results = results
+        self.scaled = scaled
+        self.bench = bench
+    
+    def append(self, other):
+        self.results.append(other)
+
+    @property
+    def stdist(self):
+        return stdist(self.results)
+    
+    @property
+    def avg(self):
+        return stdist(self.results)[0]
+    
+    @property
+    def stdev(self):
+        return stdist(self.results)[1]
+    
+    @property
+    def total(self):
+        return sum(self.results)
+    
+    
+class BenchmarkSuite(object):
+    def __init__(self, title, benchs=None):
+        if benchs is None:
+            benchs = []
+        self.title = title
+        self.benchs = benchs
+        # print [v for k, v in inspect.getmembers(self) if k.startswith('bench')]
+        self.benchs.extend(
+            v for k, v in inspect.getmembers(self)
+            if k.startswith('bench') and inspect.ismethod(v)
+        )
         
-    def parse_arguments(self):
-        ''' Gets command-line arguments and handles validation '''
+        self.benchs.sort()
+    
+    @staticmethod
+    def _run_bench(bench, times=1):
+        bres = BenchmarkResult(bench)
+        for _ in xrange(times):
+            with StopWatch() as watch:
+                bres.scaled = bench(watch)
+            bres.append(watch.elapsed)
+        return bres
+    
+    def run(self, times=1):
+        results = [self._run_bench(bench, times) for bench in self.benchs]
+        
+        elapsed = sum(res.total for res in results)
+        gelapsed = sum(math.log(res.total) for res in results)
+        return results, elapsed, gelapsed
+    
+    def print_result(self, results, elapsed, gelapsed):
+        print (
+            "|%s performance for Python %s (%s)\n"
+            "|\tOS_FAMILY=%s, OS=%s, ARCH=%s %s\n"
+            "|\t%s" % (
+                self.title, platform.python_version(), platform.python_build()[0], 
+                platform.system(), " ".join(platform.dist()), 
+                platform.processor(), platform.machine(), 
+                datetime.datetime.today().ctime()
+            )
+        )
+        
+        for res in results:
+            print '\t%d\t%f (%f)\t%s' % (
+                self.benchs.index(res.bench) + 1, res.avg, res.stdev, res.scaled)
+            
+        gelapsed = math.exp(gelapsed / len(results))
+        print (
+            "\t%f=Total Time,"
+            "\t%f=Geometric mean,"
+            "\t%d tests." % (elapsed, gelapsed, len(results))
+        )
+    
+    def main(self, argv):
         parser = OptionParser("%prog [options]", 
                               formatter=IndentedHelpFormatter(4, 80))
         parser.add_option("-s", "--scale-factor", dest="scale_factor", 
                           type="int", help="factor to scale tests by", 
                           metavar="NUM", default=1)
+        parser.add_option("-t", "--times", dest="times", 
+                          type="int", help="run each test NUM  times", 
+                          metavar="NUM", default=1)
 
-        options, args = parser.parse_args() # pylint: disable=W0612
-
-        return options
-            
-    def print_header(self, title):
-        """Prints the output header containing system and time information"""
-        header = ("|%s performance for Python %s (%s)\n"
-                  "|\tOS_FAMILY=%s, OS=%s, ARCH=%s %s\n"
-                  "|\t%s") % (
-            title, platform.python_version(), platform.python_build()[0], 
-            platform.system(), " ".join(platform.dist()), 
-            platform.processor(), platform.machine(), 
-            datetime.datetime.today().ctime()
-        )
-
-        #Display header information
-        print(header)
-            
-    def print_summary(self):
-        """Prints a summary of the test results"""
-        geom_mean = math.exp(self.geom_time / self.test_num)
-        summary = ("\t%f=Total Time,"
-                   "\t%f=Geometric mean,"
-                   "\t%d tests.") % (self.total_time, geom_mean, self.test_num)
-
-        print(summary)
-
+        options, args = parser.parse_args(argv) # pylint: disable=W0612
+        
+        self.scale = options.scale_factor        
+        self.print_result(*self.run(options.times))
--- benchmarks/time_testr.py
+++ benchmarks/time_testr.py
@@ -34,116 +34,103 @@ import benchmark
 import os
 import sys
 
-def main():
-    """Main application"""
-    timer = benchmark.BenchmarkTimer()
-   
-    options = timer.parse_arguments()
-    timer.print_header("TIME_TESTR(ichard)")
-    
-    run_tests(timer, options.scale_factor)
-    
-    timer.print_summary()
-
-def run_tests(timer, scale_factor):
+class BenchmarkSuite(benchmark.BenchmarkSuite):
     '''Go through each test and print out the results'''
     #nofileio = True
+    def bench_1(self, watch):
+        with watch.paused():
+            siz = 500
+            a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+            b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
 
-    siz = 500
-    a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
-    b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+        #Test 1 - Matrix Multiplication Large Arrays (500,500) 10*self.scale times
+        nrep = 10 * self.scale
+        for i in range(nrep):
+            c = np.dot(a, b) #pylint: disable=W0612
+        return "Matrix Multiplication Large Arrays (500,500) %d times" % nrep
+    def bench_2(self, watch):
+        #Test 2 - Matrix Multiplication Small Array (50,50) 10000*self.scale times
+        with watch.paused():
+            siz = 50
+            a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+            b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
+            nrep = 10000 * self.scale
+        
+        for i in range(nrep):
+            c = np.dot(a,b) 
+        return "Matrix Multiplication Small Array (50,50) %d times" % nrep
+    def bench_3(self, watch):
+        #Test 3 - Sorting 1 million elements 10*self.scale times
+        with watch.paused():
+            nrep = 10 * self.scale
+            a = [np.random.uniform(0, 1, 1e6).astype(np.float32) for i in range(nrep)]
     
-    #initialize time
-    timer.reset()
+        for i in range(nrep):
+            a[i].sort()
+        return "Sorting 1 million elements %d times" % nrep
+    def bench_4(self, watch):
+        #Test 4 - Moving 1 million elements 1000*self.scale times
+        with watch.paused():
+            a = np.random.uniform(0, 1, 1e6).astype(np.float32)
+            c = np.zeros(1e6)
+            nrep = 1000 * self.scale
+        
+        for i in range(nrep):
+            c = a    
+        return "Moving 1 million elements %d times" % nrep
+    def bench_5(self, watch):
+        # Test 5 - indirect addressing 1 million elements 100*self.scale times
+        # this test does not seem right, should really create a random index
+        # which accesses the elements
+        with watch.paused():
+            a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
+            b = np.zeros(1e6).astype(np.uint32)
+            nrep = 100 * self.scale
+        
+        for i in range(nrep):
+            c = a[b]
+        return 'Indirect addressing 1 million elements %d times' % nrep
+    def bench_6(self, watch):
+        #Test 6 - Shift 512 by 512 byte and store
+        with watch.paused():
+            a = np.random.randint(2**32, size=1e6)
+            c = np.zeros(1e6, dtype=np.uint32)
+            nrep = 1000 * self.scale
+        
+        for i in range(nrep):
+            c = np.roll(a, 12, axis=0)
+        return 'Shifting 1 million elements %d times' % nrep
+    def bench_7(self, watch):
+        #Test 7 - Cosine 1 million elements 100*self.scale times
+        with watch.paused():
+            nrep = 100 * self.scale
+            a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
+            c = np.zeros(1e6, dtype=np.float32)
+        
+        for i in range(nrep * 10):
+            c = np.cos(a)
+        return 'Cosine 1 million elements %d times' % nrep
+    def bench_8(self, watch):
+        #Test 8 - Alog 1 million elements 100*self.scale
+        nrep = 100 * self.scale
+        for i in range(nrep):
+            np.log(a)
+        return ' Alog 1 million elements %d times' % nrep
+    def bench_9(self, watch):
+        #Test 9 - Alog 1 million elements 100*self.scale
+        with watch.paused():
+            a = np.arange(1e6).astype(np.uint8)
+            b = np.zeros(1e6).astype(np.uint8)
+            nrep = 1000 * self.scale
+    
+        for i in range(nrep):
+            np.save('test.npy', a)
+            b = np.load('test.npy')
+        
+        os.remove('test.npy')
+        return ' Writing and reading bytarr(1e6) %d times' % nrep
 
-    #Test 1 - Matrix Multiplication Large Arrays (500,500) 10*scale_factor times
-    nrep = 10 * scale_factor
-    for i in range(nrep):
-        c = np.dot(a, b) #pylint: disable=W0612
-    timer.log("Matrix Multiplication Large Arrays (500,500) %d times" % nrep)
-
-    #Test 2 - Matrix Multiplication Small Array (50,50) 10000*scale_factor times
-    siz = 50
-    a = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
-    b = np.arange(siz**2, dtype=np.float32).reshape(siz, siz)
-    nrep = 10000 * scale_factor
-    
-    timer.reset()
-    for i in range(nrep):
-        c = np.dot(a,b) 
-    timer.log("Matrix Multiplication Small Array (50,50) %d times" % nrep)
-    
-    #Test 3 - Sorting 1 million elements 10*scale_factor times
-    nrep = 10 * scale_factor
-    a = [np.random.uniform(0, 1, 1e6).astype(np.float32) for i in range(nrep)]
-    timer.reset()
-
-    for i in range(nrep):
-        a[i].sort()
-    timer.log("Sorting 1 million elements %d times" % nrep)
-    
-    #Test 4 - Moving 1 million elements 1000*scale_factor times
-    a = np.random.uniform(0, 1, 1e6).astype(np.float32)
-    c = np.zeros(1e6)
-    nrep = 1000 * scale_factor
-    timer.reset()
-    
-    for i in range(nrep):
-        c = a    
-    timer.log("Moving 1 million elements %d times" % nrep)
-
-    # Test 5 - indirect addressing 1 million elements 100*scale_factor times
-    # this test does not seem right, should really create a random index
-    # which accesses the elements
-    a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
-    b = np.zeros(1e6).astype(np.uint32)
-    nrep = 100 * scale_factor
-    timer.reset()
-    
-    for i in range(nrep):
-        c = a[b]
-    timer.log('Indirect addressing 1 million elements %d times' % nrep)
-    
-    #Test 6 - Shift 512 by 512 byte and store
-    a = np.random.randint(2**32, size=1e6)
-    c = np.zeros(1e6, dtype=np.uint32)
-    nrep = 1000 * scale_factor
-    timer.reset()
-    
-    for i in range(nrep):
-        c = np.roll(a, 12, axis=0)
-    timer.log('Shifting 1 million elements %d times' % nrep)
- 
-    #Test 7 - Cosine 1 million elements 100*scale_factor times
-    nrep = 100 * scale_factor
-    a = 1e6 * np.random.uniform(0, 1, 1e6).astype(np.float32)
-    c = np.zeros(1e6, dtype=np.float32)
-    timer.reset()
-    
-    for i in range(nrep * 10):
-        c = np.cos(a)
-    timer.log('Cosine 1 million elements %d times' % nrep)
-
-    #Test 8 - Alog 1 million elements 100*scale_factor
-    nrep = 100 * scale_factor
-    for i in range(nrep):
-        np.log(a)
-    timer.log(' Alog 1 million elements %d times' % nrep)
-
-    #Test 9 - Alog 1 million elements 100*scale_factor
-    a = np.arange(1e6).astype(np.uint8)
-    b = np.zeros(1e6).astype(np.uint8)
-    nrep = 1000 * scale_factor
-    timer.reset()
-
-    for i in range(nrep):
-        np.save('test.npy', a)
-        b = np.load('test.npy')
-    
-    timer.log(' Writing and reading bytarr(1e6) %d times' % nrep)
-    os.remove('test.npy')
-
-    timer.print_summary()
     
 if __name__ == '__main__':
-    sys.exit(main())
+    bench = BenchmarkSuite('TIME_TESTR')
+    sys.exit(bench.main(sys.argv))
