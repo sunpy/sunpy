73cfc1d80d396dcef1fd7adda2b3adf907dcc599
Keith Hughitt <keith.hughitt@gmail.com>
2011-09-28 07:36:10 -0400

    Added paver task to build a windows binary

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ solar physics using the Python programming language.
 DOCLINES = __doc__.split("\n")
 
 CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 3 - Alpha',
     'Intended Audience :: Science/Research',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
