c773a4c8e8b103b89ff0ef2cff199c5a90ddfa0c
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-04 11:22:00 -0400

    Exception raised for unrecognized file types.

--- sunpy/map/io/__init__.py
+++ sunpy/map/io/__init__.py
@@ -14,3 +14,10 @@ def read_file(filepath):
     for extension, reader in _known_formats.items():
         if filepath.endswith(extension):
             return reader.read(filepath)
+
+    # Raise an error if an unsupported filetype is encountered
+    raise UnrecognizedFileTypeError
+
+class UnrecognizedFileTypeError(IOError):
+    """Exception to raise when an unknown file type is encountered"""
+    pass
