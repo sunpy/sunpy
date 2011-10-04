73e806405c9f8cefae5984d9bc396fcb4fe0d81e
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-04 11:08:07 -0400

    Moved FITS-specific parsing logic to sunpy.map.io.fits

--- sunpy/map/io/__init__.py
+++ sunpy/map/io/__init__.py
@@ -2,3 +2,15 @@ from __future__ import absolute_import
 """
 File input and output functions
 """
+from sunpy.map.io import fits
+
+# File formats supported by SunPy
+_known_formats = {
+    ('fts', 'fits'): fits
+}
+
+def read_file(filepath):
+    """Determines the filetype and reads in the file"""
+    for extension, reader in _known_formats.items():
+        if filepath.endswith(extension):
+            return reader.read(filepath)
