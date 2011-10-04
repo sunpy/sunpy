07f0cce99acfdcc7243a65f6cc320b34357f3c45
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-04 11:29:48 -0400

    Added jp2 module for future JPEG 2000 support

---
+++ sunpy/map/io/jp2.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+"""
+JPEG 2000 File Reader
+"""
+__author__ = "Keith Hughitt"
+__email__ = "keith.hughitt@nasa.gov"
+
+def read(filepath):
+    """Reads in the file at the specified location"""
+    # TODO: Implement initially as a cmd-line wrapper for j2k_to_image
+    pass
