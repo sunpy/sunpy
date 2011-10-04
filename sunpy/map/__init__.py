316b8b83ae5803f8930772c95cbf9d8c748f5a56
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-04 11:08:07 -0400

    Moved FITS-specific parsing logic to sunpy.map.io.fits

--- sunpy/map/__init__.py
+++ sunpy/map/__init__.py
@@ -18,7 +18,7 @@ __author__ = "Keith Hughitt"
 __email__ = "keith.hughitt@nasa.gov"
 
 import sys
-import pyfits
+from sunpy.map.io import read_file
 from sunpy.map.basemap import BaseMap
 from sunpy.map.basemap import UnrecognizedDataSouceError
 from sunpy.map.header import MapHeader
@@ -42,39 +42,19 @@ def Map(input_):
     -------
     out : BaseMap
         Returns a BaseMap or BaseMap subclass instance
-        
-    Notes
-    -----
-    PyFITS
-        [1] Due to the way PyFITS works with images the header dictionary may
-        differ depending on whether is accessed before or after the fits[0].data
-        is requested. If the header is read before the data then the original
-        header will be returned. If the header is read after the data has been
-        accessed then the data will have been scaled and a modified header
-        reflecting these changes will be returned: BITPIX may differ and
-        BSCALE and B_ZERO may be dropped in the modified version.
-        
-        [2] The verify('fix') call attempts to handle violations of the FITS
-        standard. For example, nan values will be converted to "nan" strings.
-        Attempting to cast a pyfits header to a dictionary while it contains
-        invalid header tags will result in an error so verifying it early on
-        makes the header easier to work with later.
-    References
-    ----------
-    | http://stackoverflow.com/questions/456672/class-factory-in-python
-    | http://stsdas.stsci.edu/download/wikidocs/The_PyFITS_Handbook.pdf
     """
     if isinstance(input_, basestring):
-        # TODO: Add logic to determine filetype instead of assuming FITS
-        fits = pyfits.open(input_)
-        fits.verify('silentfix')        
-        data = fits[0].data
-        header = MapHeader(fits[0].header)
+        data, dict_header = read_file(input_)
+        
+        header = MapHeader(dict_header)
 
         for cls in BaseMap.__subclasses__():
             if cls.is_datasource_for(header):
                 return cls(data, header)
         raise UnrecognizedDataSouceError
-
     else:
         return BaseMap(input_)
+    
+if __name__ == "__main__":
+    import sunpy
+    t = sunpy.Map(sunpy.AIA_171_IMAGE)
---
+++ sunpy/map/io/fits.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+"""
+FITS reader implementation
+
+Notes
+-----
+PyFITS
+    [1] Due to the way PyFITS works with images the header dictionary may
+    differ depending on whether is accessed before or after the fits[0].data
+    is requested. If the header is read before the data then the original
+    header will be returned. If the header is read after the data has been
+    accessed then the data will have been scaled and a modified header
+    reflecting these changes will be returned: BITPIX may differ and
+    BSCALE and B_ZERO may be dropped in the modified version.
+    
+    [2] The verify('fix') call attempts to handle violations of the FITS
+    standard. For example, nan values will be converted to "nan" strings.
+    Attempting to cast a pyfits header to a dictionary while it contains
+    invalid header tags will result in an error so verifying it early on
+    makes the header easier to work with later.
+References
+----------
+| http://stackoverflow.com/questions/456672/class-factory-in-python
+| http://stsdas.stsci.edu/download/wikidocs/The_PyFITS_Handbook.pdf
+"""
+__author__ = "Keith Hughitt"
+__email__ = "keith.hughitt@nasa.gov"
+
+import pyfits
+
+def read(filepath):
+    """Reads in the file at the specified location"""
+    hdulist = pyfits.open(filepath)
+    hdulist.verify('silentfix')
+            
+    return hdulist[0].data, hdulist[0].header 
