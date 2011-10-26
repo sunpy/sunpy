c294e3e1def2d1f37ab3fac4054a839c6b9877e5
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-26 16:42:08 -0400

    Fixed an issue with OpenJPEG support on Windows

--- sunpy/map/io/jp2.py
+++ sunpy/map/io/jp2.py
@@ -38,8 +38,10 @@ def get_data(filepath, j2k_to_image="j2k_to_image"):
     Uses the OpenJPEG j2k_to_image command, if available, to extract the data
     portion of a JPEG 2000 image. The image is first converted to a temporary
     intermediate file (PGM) and then read back in and stored an as ndarray.
+    
+    NOTE: PIL is also required for Matplotlib to read in PGM images.
     """
-    if os.name is "nt":
+    if j2k_to_image == "j2k_to_image" and os.name is "nt":
         j2k_to_image = "j2k_to_image.exe"
 
     if which(j2k_to_image) is None:
