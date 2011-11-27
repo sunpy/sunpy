dd1c1a701314bff6c88446eb2cd5949d616b9174
Nicky Chorley <ndchorley@gmail.com>
2011-11-27 14:03:00 +0000

    More values in _cgs.py.

--- sunpy/sun/_cgs.py
+++ sunpy/sun/_cgs.py
@@ -13,8 +13,8 @@ si_consts = _si.physical_constants
 
 physical_constants = {}
 
-physical_constants['mass'] = (si_consts['mass'][0] * 1000.0, 'g', -1)
-physical_constants['radius'] = (si_consts['radius'][0] * 100.0, 'cm', -1)
+physical_constants['mass'] = (si_consts['mass'][0] * 1.0e3, 'g', -1)
+physical_constants['radius'] = (si_consts['radius'][0] * 1.0e2, 'cm', -1)
 physical_constants['diameter'] = (physical_constants['radius'][0] * 2.0, 
                                   'cm', -1)
 physical_constants['volume'] = (4 / 3. * np.pi *
@@ -38,8 +38,19 @@ physical_constants['absolute magnitude'] = si_consts['absolute magnitude']
 physical_constants['visual magnitude'] = si_consts['visual magnitude']
 physical_constants['mass conversion rate'] = (si_consts['mass conversion rate']
                                               [0] * 1.0e3, 'g s^-1', -1)
+physical_constants['mean energy production'] = (
+    si_consts['mean energy production'][0] * 1.0e4, 'erg g^-1', -1)
+physical_constants['ellipticity'] = si_consts['ellipticity']
+physical_constants['GM'] = (si_consts['GM'][0] * 1.0e6, 'cm^3 s^-2', -1)
+physical_constants['surface gravity'] = (si_consts['surface gravity'][0] *
+                                         1.0e-3, 'g cm^-3', -1)
+physical_constants['escape velocity'] = (si_consts['escape velocity'][0] *
+                                         1.0e5, 'cm s^-1', -1)
+physical_constants['sunspot cycle'] = si_consts['sunspot cycle']
+physical_constants['metallicity'] = si_consts['metallicity']
 
+physical_constants['solar flux unit'] = (si_consts['solar flux unit'][0] *
+                                         1.0e3, 'erg s^-1 cm^-2 Hz^-1', -1)
+physical_constants['average_angular_size'] = si_consts['average_angular_size']
 
-
-
-
+standard_model = _si.standard_model
