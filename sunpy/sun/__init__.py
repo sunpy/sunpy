7a4417164cedbdbb7e9a0bb4aa683b9a5407ceb9
steven christe <steven.d.christe@nasa.gov>
2011-12-19 10:31:33 -0500

    Added doc string to sun module __init__.py

--- sunpy/sun/__init__.py
+++ sunpy/sun/__init__.py
@@ -1,3 +1,22 @@
+"""
+Contains astronomical and physical constants for use in SunPy or other
+places.
+
+The package contains a `~sunpy.sun._cgs` and `~sunpy.sun._si`
+module that define constants in CGS and SI units, respectively.  A typical use
+case might be::
+
+    from sunpy.sun._cgs import physical_constants as con
+
+"""
+
 # Not sure why this is necessary right now, but SunPy doesn't find
 # the CGS module otherwise.
-import _cgs
+# TODO: Note that with the new _cgs and _si packages, the use of constants.py
+# which was copied over from SciPy does not support using the cgs package as
+# it is hard coded to import the si units. Should we move away from this 
+# paradigm?
+
+from . import _cgs
+from . import _si
+from . import constants
