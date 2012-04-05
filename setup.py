6b814b31f0303117cf8f42936415a87df7c55ed0
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-05 14:21:08 -0400

    Fixed setup.py dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def install(setup): #pylint: disable=W0621
             'scipy',
             'suds',
             'pandas',
-            'pyqt',
+            'pyqt4',
             'matplotlib>=1.0'
         ],
         license="BSD",
