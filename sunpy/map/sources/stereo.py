284e0c4e7f2b45a4b7ebb0c58b66ae1df3bfa636
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-05 17:00:39 -0400

    Updated STEREO maps

--- sunpy/map/sources/stereo.py
+++ sunpy/map/sources/stereo.py
@@ -1,69 +1,50 @@
 """STEREO Map subclass definitions"""
-#pylint: disable=W0221,W0222
+#pylint: disable=W0221,W0222,E1121,W0613
 
 __author__ = "Keith Hughitt"
 __email__ = "keith.hughitt@nasa.gov"
 
 from sunpy.map.basemap import BaseMap
-from datetime import datetime
+from sunpy.time import parse_time
 from sunpy.cm import cm
 
 class EUVIMap(BaseMap):
     """EUVI Image Map definition"""
     def __new__(cls, data, header):
         return BaseMap.__new__(cls, data)
-        
-    @classmethod
-    def get_properties(cls, header):
-        """Returns the default and normalized values to use for the Map"""
-        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+    
+    def __init__(self, data, header):
+        BaseMap.__init__(self, header)
+        self.date = parse_time(header.get('date_obs'))
+        self.det = "EUVI"
+        self.inst = "SECCHI"
+        self.obs = header.get('obsrvtry')
+        self.name = "EUVI %s" % self.meas
+        self.cmap = cm.get_cmap('sohoeit%d' % header.get('wavelnth'))
 
-        properties = BaseMap.get_properties(header)
-        properties.update({
-            "date": datetime.strptime(header['date_obs'], date_format),
-            "det": "EUVI",
-            "inst": "SECCHI",
-            "meas": header['wavelnth'],
-            "obs": header['obsrvtry'],
-            "dsun": header.get('dsun_obs'),
-            "cmap": cm.get_cmap(name='sohoeit' + str(header.get('wavelnth'))),
-            "name": "EUVI %s" % header['wavelnth'],
-            "exptime": header.get('exptime')
-        })
-        return properties
-        
     @classmethod
     def is_datasource_for(cls, header):
         """Determines if header corresponds to an EUVI image"""
-        return header.get('detector') == 'EUVI'
+        return header.get('detector') is 'EUVI'
         
 class CORMap(BaseMap):
     """COR Image Map definition"""
     def __new__(cls, data, header):
         return BaseMap.__new__(cls, data)
-        
-    @classmethod
-    def get_properties(cls, header):
-        """Returns the default and normalized values to use for the Map"""
-        properties = BaseMap.get_properties(header)
-        date_format = "%Y-%m-%dT%H:%M:%S.%f"
-
-        # @TODO: Deal with invalid values for exptime. E.g. STEREO-B COR2
-        # on 2012/03/20 has -1 for some images.
-        properties.update({
-            "date": datetime.strptime(header['date_obs'], date_format),
-            "det": header['detector'],
-            "inst": "SECCHI",
-            "meas": "white-light",
-            "obs": header['obsrvtry'],
-            'dsun': header.get('dsun_obs'),
-            "name": "SECCHI %s" % header['detector'],
-            "exptime": header.get('exptime')
-        })
-        return properties
+    
+    # @TODO: Deal with invalid values for exptime. E.g. STEREO-B COR2
+    # on 2012/03/20 has -1 for some images.
+    def __init__(self, data, header):
+        BaseMap.__init__(self, header)
+        self.date = parse_time(header.get('date_obs'))
+        self.det = header.get('detector')
+        self.inst = "SECCHI"
+        self.obs = header.get('obsrvtry')
+        self.meas = "white-light"
+        self.name = "SECCHI %s" % header.get('detector')
         
     @classmethod
     def is_datasource_for(cls, header):
         """Determines if header corresponds to an COR image"""
-        return header.get('detector') and header.get('detector')[0:3] == "COR"
+        return header.get('detector', '').startswith('COR')
 
