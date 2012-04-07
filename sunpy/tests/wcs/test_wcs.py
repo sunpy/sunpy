beedfd0e093a1db5aa24ef406e97f00212a1101b
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-07 07:50:06 -0400

    Renamed rsun to rsun_arcseconds and added rsun_meters. Passing in rsun to wcs functions.

--- sunpy/tests/wcs/test_wcs.py
+++ sunpy/tests/wcs/test_wcs.py
@@ -7,20 +7,21 @@ from numpy.testing import assert_array_almost_equal
 
 fits = pyfits.open(sunpy.AIA_171_IMAGE)
 header = fits[0].header
+img = sunpy.make_map(sunpy.AIA_171_IMAGE)
 
 def test_conv_hpc_hcc():
     coord = [40.0, 32.0]
-    result = wcs.convert_hpc_hcc(header, coord[0], coord[1])
+    result = wcs.convert_hpc_hcc(header, img.rsun_arcseconds, coord[0], coord[1])
     assert_array_almost_equal(result, [28748691, 22998953], decimal=3)
  
 def test_conv_hcc_hpc():
     coord = [34.0, 132.0]
-    result = wcs.convert_hcc_hpc(header, coord[0], coord[1])
+    result = wcs.convert_hcc_hpc(header, img.rsun_arcseconds, coord[0], coord[1])
     assert_array_almost_equal(result, [1.3140782e-08, 5.1017152e-08], decimal=2)
 
 def test_conv_hcc_hg():
     coord = [13.0, 58.0]
-    result = wcs.convert_hcc_hg(header, coord[0], coord[1])
+    result = wcs.convert_hcc_hg(header, img.rsun_arcseconds, coord[0], coord[1])
     assert_array_almost_equal(result, [1.0791282e-06, -7.0640732], decimal=2)
 
 def test_conv_hg_hcc():
@@ -30,6 +31,6 @@ def test_conv_hg_hcc():
     
 def test_conv_hg_hpc():
     coord = [34.0, 96.0]
-    result = wcs.convert_hg_hpc(header, coord[0], coord[1])
+    result = wcs.convert_hg_hpc(header, img.rsun_arcseconds, coord[0], coord[1])
     assert_array_almost_equal(result, [0.096365756, 0.22138465], decimal=2)
     
