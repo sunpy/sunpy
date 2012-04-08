76569240a87a8e2762bd241ded01a14d3553b5db
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-08 16:36:17 -0400

    Removed unecessary header references

--- sunpy/tests/wcs/test_wcs.py
+++ sunpy/tests/wcs/test_wcs.py
@@ -11,33 +11,33 @@ img = sunpy.make_map(sunpy.AIA_171_IMAGE)
 
 def test_conv_hpc_hcc():
     coord = [40.0, 32.0]
-    result = wcs.convert_hpc_hcc(header, img.rsun_arcseconds, 
+    result = wcs.convert_hpc_hcc(img.rsun_arcseconds, 
                                  img.dsun, img.units_x, img.units_y, 
                                  coord[0], coord[1])
     assert_array_almost_equal(result, [28748691, 22998953], decimal=3)
  
 def test_conv_hcc_hpc():
     coord = [34.0, 132.0]
-    result = wcs.convert_hcc_hpc(header, img.rsun_arcseconds, img.dsun, 
+    result = wcs.convert_hcc_hpc(img.rsun_arcseconds, img.dsun, 
                                  coord[0], coord[1])
     assert_array_almost_equal(result, [1.3140782e-08, 5.1017152e-08], decimal=2)
 
 def test_conv_hcc_hg():
     coord = [13.0, 58.0]
     result = wcs.convert_hcc_hg(img.rsun_arcseconds, 
-                                self.get_solar_b0(), self.get_solar_l0(),
+                                img.get_solar_b0(), img.get_solar_l0(),
                                 coord[0], coord[1])
     assert_array_almost_equal(result, [1.0791282e-06, -7.0640732], decimal=2)
 
 def test_conv_hg_hcc():
     coord = [34.0, 96.0]
-    result = wcs.convert_hg_hcc(self.rsun, self.get_solar_b0(), 
-                                self.get_solar_l0(), coord[0], coord[1])
+    result = wcs.convert_hg_hcc(img.rsun_arcseconds, img.get_solar_b0(), 
+                                img.get_solar_l0(), coord[0], coord[1])
     assert_array_almost_equal(result, [-40653538.0, 6.7903529e08], decimal=2)
     
 def test_conv_hg_hpc():
     coord = [34.0, 96.0]
-    result = wcs.convert_hg_hpc(header, img.rsun_arcseconds, img.dsun,
+    result = wcs.convert_hg_hpc(img.rsun_arcseconds, img.dsun, 
                                 coord[0], coord[1])
     assert_array_almost_equal(result, [0.096365756, 0.22138465], decimal=2)
     
