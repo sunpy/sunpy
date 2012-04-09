b5b8092b97b9aaee66f7a9fe760299765402b515
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-09 15:06:39 -0400

    Renamed map.header => map.fits_header

--- sunpy/tests/map/test_basemap.py
+++ sunpy/tests/map/test_basemap.py
@@ -24,24 +24,24 @@ class TestBaseMap:
         """Make sure conversion from data units to pixels is accurate"""
         # Check conversion of reference pixel
         # Note: FITS pixels starts from 1,1
-        assert self.map.data_to_pixel(self.map.header['crval1'], 'x') == self.map.header['crpix1'] - 1
-        assert self.map.data_to_pixel(self.map.header['crval2'], 'y') == self.map.header['crpix2'] - 1
+        assert self.map.data_to_pixel(self.map.fits_header['crval1'], 'x') == self.map.fits_header['crpix1'] - 1
+        assert self.map.data_to_pixel(self.map.fits_header['crval2'], 'y') == self.map.fits_header['crpix2'] - 1
         
         # Check conversion of map center
-        assert self.map.data_to_pixel(self.map.center['x'], 'x') == (self.map.header['naxis1'] - 1) / 2.
-        assert self.map.data_to_pixel(self.map.center['y'], 'y') == (self.map.header['naxis2'] - 1) / 2.
+        assert self.map.data_to_pixel(self.map.center['x'], 'x') == (self.map.fits_header['naxis1'] - 1) / 2.
+        assert self.map.data_to_pixel(self.map.center['y'], 'y') == (self.map.fits_header['naxis2'] - 1) / 2.
         
         # Check conversion of map edges
         # Note: data coords are at pixel centers, so edges are 0.5 pixels wider
         assert self.map.data_to_pixel(self.map.xrange[0], 'x') == 0. - 0.5
         assert self.map.data_to_pixel(self.map.yrange[0], 'y') == 0. - 0.5
-        assert self.map.data_to_pixel(self.map.xrange[1], 'x') == (self.map.header['naxis1'] - 1) + 0.5
-        assert self.map.data_to_pixel(self.map.yrange[1], 'y') == (self.map.header['naxis2'] - 1) + 0.5
+        assert self.map.data_to_pixel(self.map.xrange[1], 'x') == (self.map.fits_header['naxis1'] - 1) + 0.5
+        assert self.map.data_to_pixel(self.map.yrange[1], 'y') == (self.map.fits_header['naxis2'] - 1) + 0.5
     
     def test_data_range(self):
         """Make sure xrange and yrange work"""
-        assert self.map.xrange[1] - self.map.xrange[0] == self.map.header['cdelt1'] * self.map.header['naxis1']
-        assert self.map.yrange[1] - self.map.yrange[0] == self.map.header['cdelt2'] * self.map.header['naxis2']
+        assert self.map.xrange[1] - self.map.xrange[0] == self.map.fits_header['cdelt1'] * self.map.fits_header['naxis1']
+        assert self.map.yrange[1] - self.map.yrange[0] == self.map.fits_header['cdelt2'] * self.map.fits_header['naxis2']
         
         assert np.average(self.map.xrange) == self.map.center['x']
         assert np.average(self.map.yrange) == self.map.center['y']
@@ -56,15 +56,15 @@ class TestBaseMap:
         
         # Expected offset for center
         offset = {
-            "x": self.map.header.get('crpix1') - width / 2,
-            "y": self.map.header.get('crpix2') - height / 2,
+            "x": self.map.fits_header.get('crpix1') - width / 2,
+            "y": self.map.fits_header.get('crpix2') - height / 2,
         }
         
         # Check to see if submap header was updated properly
-        assert submap.header.get('crpix1') == offset['x'] 
-        assert submap.header.get('crpix1') == offset['y']
-        assert submap.header.get('naxis1') == width / 2
-        assert submap.header.get('naxis2') == height / 2
+        assert submap.fits_header.get('crpix1') == offset['x'] 
+        assert submap.fits_header.get('crpix1') == offset['y']
+        assert submap.fits_header.get('naxis1') == width / 2
+        assert submap.fits_header.get('naxis2') == height / 2
         
         # Check data
         assert (np.asarray(self.map)[height/2:height, 
@@ -81,4 +81,4 @@ class TestBaseMap:
         # header information in fits[0].header
         self.fits[0].data #pylint: disable=W0104
         
-        assert dict(self.map.header) == dict(self.fits[0].header)
+        assert dict(self.map.fits_header) == dict(self.fits[0].header)
