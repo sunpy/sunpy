d11b5509b44b1ff0495a609ec646573f018e462a
Keith Hughitt <keith.hughitt@gmail.com>
2012-04-12 17:30:12 -0400

    Adding skipped file

---
+++ sunpy/util/xml.py
@@ -0,0 +1,69 @@
+"""XML helper functions"""
+
+from __future__ import absolute_import
+from xml.dom.minidom import parseString #pylint: disable=E0611,F0401
+
+#
+# Converting XML to a Dictionary
+# Author: Christoph Dietze
+# URL   : http://code.activestate.com/recipes/116539/
+#
+class NotTextNodeError(Exception):
+    pass
+
+def xml_to_dict(xmlstring):
+    """Converts an XML string to a Python dictionary"""
+    return node_to_dict(parseString(xmlstring))
+
+def node_to_dict(node):
+    """
+    node_to_dict() scans through the children of node and makes a dictionary 
+    from the content.
+    
+    Three cases are differentiated:
+    1. If the node contains no other nodes, it is a text-node and 
+       {nodeName: text} is merged into the dictionary.
+    2. If the node has the attribute "method" set to "true", then it's children 
+       will be appended to a list and this list is merged to the dictionary in 
+       the form: {nodeName:list}.
+    3. Else, node_to_dict() will call itself recursively on the nodes children 
+       (merging {nodeName: node_to_dict()} to the dictionary).
+    """
+    dic = {} 
+    for n in node.childNodes:
+        if n.nodeType != n.ELEMENT_NODE:
+            continue
+        if n.getAttribute("multiple") == "true":
+            # node with multiple children: put them in a list
+            l = []
+            for c in n.childNodes:
+                if c.nodeType != n.ELEMENT_NODE:
+                    continue
+                l.append(node_to_dict(c))
+                dic.update({n.nodeName: l})
+            continue
+            
+        try:
+            text = get_node_text(n)
+        except NotTextNodeError:
+            # 'normal' node
+            dic.update({n.nodeName: node_to_dict(n)})
+            continue
+    
+        # text node
+        dic.update({n.nodeName: text})
+        continue
+    return dic
+
+def get_node_text(node):
+    """
+    scans through all children of node and gathers the text. if node has 
+    non-text child-nodes, then NotTextNodeError is raised.
+    """
+    t = ""
+    for n in node.childNodes:
+        if n.nodeType == n.TEXT_NODE:
+            t += n.nodeValue
+        else:
+            raise NotTextNodeError
+    return t
