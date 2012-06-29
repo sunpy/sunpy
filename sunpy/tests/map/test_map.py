f2c41d5232d7221ef32289cda07a88130b8fcd41
Keith Hughitt <keith.hughitt@gmail.com>
2012-06-29 14:34:08 -0400

    Fixed submap unit test

--- sunpy/tests/map/test_map.py
+++ sunpy/tests/map/test_map.py
@@ -63,8 +63,13 @@ class TestMap:
         # Check to see if submap properties were updated properly
         assert submap.reference_pixel['x'] == offset['x'] 
         assert submap.reference_pixel['y'] == offset['y']
-        assert submap._original_header.shape[0] == width / 2
-        assert submap._original_header.shape[1] == height / 2
+        assert submap.shape[0] == width / 2
+        assert submap.shape[1] == height / 2
+        
+        # Check to see if header was updated
+        submap_header = submap.get_header()
+        assert submap_header.get('naxis1') == width / 2
+        assert submap_header.get('naxis2') == height / 2
         
         # Check data
         assert (np.asarray(self.map)[height/2:height, 
