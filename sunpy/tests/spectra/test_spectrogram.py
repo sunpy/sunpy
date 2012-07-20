bf08374ecfc10cb942a456ffc2223a2c41e16115
Florian Mayer <florian.mayer@bitsrc.org>
2012-07-20 16:11:37 +0200

    Fix time_to_x.

--- sunpy/tests/spectra/test_spectrogram.py
+++ sunpy/tests/spectra/test_spectrogram.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import numpy as np
 
-from sunpy.spectra.spectrogram import Spectrogram
+from sunpy.spectra.spectrogram import Spectrogram, LinearTimeSpectrogram
 
 
 def mk_spec(image):
@@ -34,10 +34,22 @@ def test_slice_time_axis():
 	new = spectrogram[:, 59:]
 	assert new.t_init == 59
 	assert np.array_equal(new.time_axis, np.linspace(0, 3600 - 60, 3600 - 59))
-	assert new.start == datetime(2010, 10, 10, 0, 0,	 59)
+	assert new.start == datetime(2010, 10, 10, 0, 0, 59)
 
 
 def test_slice_freq_axis():
 	spectrogram = mk_spec(np.zeros((200, 3600)))
 	new = spectrogram[100:, :]
 	assert np.array_equal(new.freq_axis, np.linspace(100, 199, 100))
+
+
+def test_time_to_x():
+	image = np.zeros((200, 3600))
+	spectrogram = LinearTimeSpectrogram(
+		image, np.linspace(0, image.shape[1] - 1, image.shape[1]),
+		np.linspace(0, image.shape[0] - 1, image.shape[0]),
+		datetime(2010, 10, 10), datetime(2010, 10, 10, 1), 0, 1
+	)
+	ret = spectrogram.time_to_x(datetime(2010, 10, 10, 0, 0, 59))
+	assert isinstance(ret, int)
+	assert ret == 59
