6f1eb659a2f18a742b5f5de48c4498eccb64118f
Florian Mayer <florian.mayer@bitsrc.org>
2012-07-20 16:33:38 +0200

    Add test for joining in time.

--- sunpy/tests/spectra/test_spectrogram.py
+++ sunpy/tests/spectra/test_spectrogram.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 import numpy as np
 
+from scipy import ndimage
+
 from sunpy.spectra.spectrogram import Spectrogram, LinearTimeSpectrogram
 
 
@@ -53,3 +55,24 @@ def test_time_to_x():
 	ret = spectrogram.time_to_x(datetime(2010, 10, 10, 0, 0, 59))
 	assert isinstance(ret, int)
 	assert ret == 59
+
+def test_join():
+	image = np.random.rand(200, 3600)
+	one = LinearTimeSpectrogram(
+		image, np.linspace(0, image.shape[1] - 1, image.shape[1]),
+		np.linspace(0, image.shape[0] - 1, image.shape[0]),
+		datetime(2010, 10, 10), datetime(2010, 10, 10, 0, 30), 0, 0.5,
+	)
+
+	image = np.random.rand(200, 3600)
+	other = LinearTimeSpectrogram(
+		image, np.linspace(0, image.shape[1] - 1, image.shape[1]),
+		np.linspace(0, image.shape[0] - 1, image.shape[0]),
+		datetime(2010, 10, 10, 0, 29), datetime(2010, 10, 10, 1, 29), 1799, 1,
+	)
+
+	z = LinearTimeSpectrogram.join_many([one, other])
+	assert z.shape == (200, 3 * 3600 - 2)
+
+	assert np.array_equal(z[:, :3598], one[:, :-2])
+	assert np.array_equal(z[:, 3598:], ndimage.zoom(other, (1, 2)))
