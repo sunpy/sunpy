d332c3d6e59bf414898b09b84db270918e5275a7
Florian Mayer <florian.mayer@bitsrc.org>
2012-07-24 11:32:18 +0200

    Test download.

--- sunpy/tests/spectra/test_callisto.py
+++ sunpy/tests/spectra/test_callisto.py
@@ -3,12 +3,18 @@
 
 from __future__ import absolute_import
 
+import os
+import shutil
+
+from tempfile import mkdtemp
 from datetime import datetime
 
 import numpy as np
 
 from sunpy.data.sample import CALLISTO_IMAGE
-from sunpy.spectra.sources.callisto import CallistoSpectrogram, query
+from sunpy.spectra.sources.callisto import (
+	CallistoSpectrogram, query, download
+)
 
 
 def test_read():
@@ -65,3 +71,21 @@ def test_query_number():
 	RESULTS.sort()
 
 	assert result == [URL + res for res in RESULTS]
+
+
+def test_download():
+	directory = mkdtemp()
+	try:
+		result = query(
+			datetime(2011, 9, 22, 5), datetime(2011, 9, 22, 6), set(["BIR"]), 1
+		)
+		RESULTS = [
+			"BIR_20110922_050000_01.fit.gz",
+			"BIR_20110922_051500_01.fit.gz",
+			"BIR_20110922_053000_01.fit.gz",
+			"BIR_20110922_060000_01.fit.gz",
+		]
+		download(result, directory)
+		assert sorted(os.listdir(directory)) == RESULTS
+	finally:
+		shutil.rmtree(directory)
