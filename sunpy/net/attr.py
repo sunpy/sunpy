50390b2d3a75733aeea437003c2222f24d05c0a6
Florian Mayer <florian.mayer@bitsrc.org>
2012-07-31 14:29:23 +0200

    Documentation.

--- sunpy/net/attr.py
+++ sunpy/net/attr.py
@@ -13,6 +13,12 @@ and OR (|) always are in disjunctive normal form, that is, there are only two
 levels ­- the first being disjunction and the second being conjunction. In other
 words, every combinations of attributes looks like this:
 (a AND b AND c) OR (d AND e).
+
+Walkers are used to traverse the tree that results from combining attributes.
+They are implemented using sunpy.util.multimethod. Multimethods are functions
+that are not assigned to classes but still dispatch by type of one or more
+of their arguments. For more information about multimethods, refer to
+sunpy.util.multimethod.
 """
 
 from __future__ import absolute_import
