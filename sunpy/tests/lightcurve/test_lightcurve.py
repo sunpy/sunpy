82f91e91b3701761d0884eb137577575b9816a6a
Keith Hughitt <keith.hughitt@gmail.com>
2012-08-07 11:24:24 -0400

    file input unit test

--- sunpy/tests/lightcurve/test_lightcurve.py
+++ sunpy/tests/lightcurve/test_lightcurve.py
@@ -9,11 +9,13 @@ from __future__ import absolute_import
 #   time deltas instead of datetimes?
 
 #pylint: disable=C0103,R0904,W0201,W0232,E1101,E1103
-import sunpy
 import pytest
 import datetime
+import sunpy
+from sunpy.data.test import (EVE_AVERAGES_CSV)
 
 def test_input_empty():
+    """Tests empty input"""
     with pytest.raises(NotImplementedError):
         sunpy.lightcurve.LightCurve()
 
@@ -23,5 +25,7 @@ def test_input_dict_datetimes():
     dates = [base - datetime.timedelta(minutes=x) for x in range(0, 24 * 60)]
     sunpy.lightcurve.LightCurve({"param": range(24 * 60)}, index=dates)
     
-        
-        
+def test_input_file():
+    """Tests filepath input"""
+    with pytest.raises(NotImplementedError):
+        sunpy.lightcurve.LightCurve(EVE_AVERAGES_CSV)
