45ce3f313d2d76c73c074b7b2e762cd0b3ae023e
Keith Hughitt <keith.hughitt@gmail.com>
2012-08-07 11:26:28 -0400

    ndarray + dates input unit test

--- sunpy/tests/lightcurve/test_lightcurve.py
+++ sunpy/tests/lightcurve/test_lightcurve.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import
 #   time deltas instead of datetimes?
 
 #pylint: disable=C0103,R0904,W0201,W0232,E1101,E1103
+import numpy as np
 import pytest
 import datetime
 import sunpy
@@ -24,6 +25,12 @@ def test_input_dict_datetimes():
     base = datetime.datetime.today()
     dates = [base - datetime.timedelta(minutes=x) for x in range(0, 24 * 60)]
     sunpy.lightcurve.LightCurve({"param": range(24 * 60)}, index=dates)
+    
+def test_input_ndarray_datetimes():
+    """Tests LightCurve creation from a dictionary and list of datetimes"""
+    base = datetime.datetime.today()
+    dates = [base - datetime.timedelta(minutes=x) for x in range(0, 24 * 60)]
+    sunpy.lightcurve.LightCurve(np.arange(24 * 60), index=dates)
     
 def test_input_file():
     """Tests filepath input"""
