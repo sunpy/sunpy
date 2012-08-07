c8914c09208a1817fa45f0e63591fb7fbdc52487
Keith Hughitt <keith.hughitt@gmail.com>
2012-08-07 11:12:45 -0400

    empty input unit test

--- sunpy/tests/lightcurve/test_lightcurve.py
+++ sunpy/tests/lightcurve/test_lightcurve.py
@@ -8,15 +8,20 @@ from __future__ import absolute_import
 #   ndarray + indices?
 #   time deltas instead of datetimes?
 
-#pylint: disable=C0103,R0904,W0201,W0232,E1103
+#pylint: disable=C0103,R0904,W0201,W0232,E1101,E1103
 import sunpy
+import pytest
 import datetime
 
-def test_dict_and_datetimes():
+def test_input_empty():
+    with pytest.raises(NotImplementedError):
+        sunpy.lightcurve.LightCurve()
+
+def test_input_dict_datetimes():
     """Tests LightCurve creation from a dictionary and list of datetimes"""
     base = datetime.datetime.today()
     dates = [base - datetime.timedelta(minutes=x) for x in range(0, 24 * 60)]
-    lc = sunpy.lightcurve.LightCurve({"param": range(24 * 60)}, index=dates)
+    sunpy.lightcurve.LightCurve({"param": range(24 * 60)}, index=dates)
     
         
         
