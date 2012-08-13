9917e9203a62606e62369fdcdf8b6aee0f7f1117
Florian Mayer <florian.mayer@bitsrc.org>
2012-08-14 00:26:29 +0100

    Mock sunpy.cm.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,7 +71,7 @@ __lt__
 MOCK_MODULES = [
     'scipy', 'matplotlib', 'matplotlib.pyplot', 'pyfits',
     'scipy.constants.constants', 'matplotlib.cm',
-    'matplotlib.image', 'matplotlib.colors']
+    'matplotlib.image', 'matplotlib.colors', 'sunpy.cm']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock(pi=math.pi, G=6.67364e-11)
 
