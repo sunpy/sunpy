ea12dd0826f700ff557e6cdb9bd7ddce70f236b3
Matt Earnshaw <matt@earnshaw.org.uk>
2012-08-22 14:12:36 +0100

    Change doc strings.

--- sunpy/util/map_manager.py
+++ sunpy/util/map_manager.py
@@ -1,13 +1,14 @@
 import weakref
 import sunpy
 
-class MapManager(weakref.WeakSet):
-    """Weak referenced set of maps created using functions decorated by manage_maps."""
+class DataManager(weakref.WeakSet):
+    """Weak referenced set of SunPy data types created using functions decorated by manage_data.
+    The list is at sunpy._data_manager"""
     pass
 
 def manage_maps(fn):
-    """Maps returned by functions decorated with manage_maps (eg. sunpy.make_map)
-    will be registered in the sunpy.map_manager list."""
+    """SunPy data types returned by functions decorated with manage_data (eg. sunpy.make_map)
+    will be registered in the sunpy._data_manager list."""
 
     def fn_manage_maps(*args, **kwargs):
         ret = fn(*args, **kwargs)
