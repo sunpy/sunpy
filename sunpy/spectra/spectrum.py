119f49667ee0322bd4c37194dbcdbcaa9761f27b
Steven Christe <steven.d.christe@nasa.gov>
2012-09-03 11:40:31 -0400

    updates to spectrum object

--- sunpy/spectra/spectrum.py
+++ sunpy/spectra/spectrum.py
@@ -8,23 +8,57 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Spectrum(np.ndarray):
-    """ Class representing a spectrum.
+    """ Class representing a general spectrum.
     
     Parameters
     ----------
-    data : np.ndarray
-        one-dimensional array which the intensity at a particular frequency
-        at every data-point.
-    freq_axis : np.ndarray
-        one-dimensional array with the frequency values at every data point
+    flux : np.ndarray
+        one-dimensional array with the flux intensity in a particular channel.
+    channel : 2d np.ndarray
+        two-dimensional array with the channel boundaries [lo, hi]
+    width : np.ndarray
+        bin width of the channels (allowed to be zero).
+    error : np.ndarray
+        error in flux in each channel
+    units : 2d str
+        units for the channel (e.g. energy, wavelength) and flux (e.g. photons/cm^2/s^1)
+    name : str
+        name of the spectrum
     """
+
     def __new__(cls, data, *args, **kwargs):
         return np.asarray(data).view(cls)
 
-    def __init__(self, data, freq_axis):
-        self.freq_axis = freq_axis
+    def __init__(self, channel, flux, width=None, error=None, name='spectrum', units=None, date=None):      
+        self.flux = flux
+        self.name = name
+        self.units = units
+        self.width = width
+        self.date = date
+        if units == None:
+            self.units = ['frequency [MHz]', 'solar flux units [sfu]']
+        
+        if np.array(channel.shape).shape[0] == 1:
+            
+            if channel.shape != flux.shape: 
+                raise ValueError("channel and flux don't match shapes!")
 
-    def plot(self, overlays=[], **matplotlib_args):
+            if width != None:
+                self.width = width
+                channel1 = channel
+                channel2 = channel + width
+                self.channel = np.array([channel, channel + width])
+
+            if width == None:
+                self.channel = np.array([channel, channel])
+        
+        if np.array(channel.shape).shape[0] == 2:
+            if channel.shape[1] != flux.shape[0]:
+                raise ValueError("channel and flux don't match shapes!")
+            self.channel = channel
+            self.width = channel[1,:] - channel[0,:]
+
+    def plot(self, figure=None, overlays=[], **matplotlib_args):
         """
         Plot spectrum onto figure.
         
@@ -38,17 +72,33 @@ class Spectrum(np.ndarray):
         """
         # [] as default argument is okay here because it is only read.
         # pylint: disable=W0102,R0914
-
         if figure is None:
             figure = plt.figure(frameon=True)
         axes = figure.add_subplot(111)
 
         params = {}
-
         params.update(matplotlib_args)
 
-        axes.plot(self.freq_axis, self, **params)
+        if self.width is None:
+            axes.plot(self.channel[0,:], self.flux, **params)
+        if self.width != None:
+            axes.step(self.channel[0,:], self.flux, where='post', **params)
 
-        for overlay in overlays:
-            figure, axes = overlay(figure, axes)
+        if self.name != None and self.date != None:
+            axes.set_title("%s %s" % (self.name, self.date))
+        if self.name != None:
+            axes.set_title("%s" % self.name)
+        if self.date != None:
+            axes.set_title("%s" % self.date)
+
+            
+        axes.set_xlabel(self.units[0])
+        axes.set_ylabel(self.units[1])
+            
+        #for overlay in overlays:
+        #    figure, axes = overlay(figure, axes)
         return figure
+        
+    def show(self, figure=None, overlays=None, **matplotlib_args):
+        """Displays map on screen. Arguments are same as plot()."""
+        self.plot(figure, overlays).show()
