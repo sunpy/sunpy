0e07a6846f2be37f6bc8f8aabc9d02e501b9665a
Jack Ireland <jack.ireland@nasa.gov>
2012-11-10 11:09:25 -0500

    added in a normalize color method for handling XRT data. small edit to a comment about Yohkoh/SXT data.

--- sunpy/map/sources/hinode.py
+++ sunpy/map/sources/hinode.py
@@ -50,6 +50,21 @@ class XRTMap(Map):
         })
         return properties
 
+    def norm(self):
+        """Returns a Normalize object to be used with XRT data"""
+        # byte-scaled images have most likely already been scaled
+        if self.dtype == np.uint8:
+            return None
+
+        mean = self.mean()
+        std = self.std()
+        
+        vmin = max(0, mean - 3 * std)
+        vmax = min(self.max(), mean + 3 * std)
+        
+        return colors.Normalize(vmin, vmax)
+
+
     @classmethod
     def is_datasource_for(cls, header):
         """Determines if header corresponds to an XRT image"""
