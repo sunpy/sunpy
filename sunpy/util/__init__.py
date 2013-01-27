86ae1e68a3a5f1bd02dbb62d844b53f8ecafb6fb
Russell Hewett <rhewett@mit.edu>
2013-01-27 12:52:30 -0500

    Fixed for correct relative imports

--- sunpy/util/__init__.py
+++ sunpy/util/__init__.py
@@ -1,6 +1,6 @@
 """SunPy utility functions"""
 from __future__ import absolute_import
 
-from util import *
-from unit_conversion import *
-from sysinfo import *
+from .util import *
+from .unit_conversion import *
+from .sysinfo import *
