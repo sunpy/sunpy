b415b7a9b0e7b26ad2febb00bd53b5401d6af470
Russell Hewett <rhewett@mit.edu>
2013-01-27 12:02:13 -0500

    Moved unit conversion functions ('astronomy-type utilities, per schriste) to their own module.  Corrected __init__ so that the external interface doesn't change.

--- sunpy/util/__init__.py
+++ sunpy/util/__init__.py
@@ -1,8 +1,6 @@
 """SunPy utility functions"""
 from __future__ import absolute_import
+
 from . util import *
-
+from unit_conversion import *
 from sysinfo import *
-
-__all__ = []
-__all__ += util.__all__
