223a28c9d19a522af436f87064ced981526fe128
Stuart Mumford <stuart@mumford.me.uk>
2013-02-04 16:00:27 +0000

    Modified comp_map.draw_limb to find first map with rsun attr.
    Also forcibly set zorder in map.draw_limb() to make it on top of comp map

--- sunpy/map/map.py
+++ sunpy/map/map.py
@@ -400,11 +400,11 @@ Dimension:\t [%d, %d]
         if hasattr(self, 'center'):
             circ = patches.Circle([self.center['x'], self.center['y']],
                                   radius=self.rsun_arcseconds, fill=False,
-                                  color='white')
+                                  color='white',zorder=100)
         else:
             print("Assuming center of Sun is center of image")
             circ = patches.Circle([0,0], radius=self.rsun_arcseconds,
-                                  fill=False, color='white')
+                                  fill=False, color='white',zorder=100)
         axes.add_artist(circ)
         
         return axes
