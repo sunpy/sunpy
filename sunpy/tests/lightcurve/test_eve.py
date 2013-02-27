e8564ce99a57bce04c60cabf8f584e4c6359f579
Nabobalis <nabil.freij@gmail.com>
2013-02-27 17:06:30 +0000

    Attempt to redo the EVE unit tests

--- sunpy/tests/lightcurve/test_eve.py
+++ sunpy/tests/lightcurve/test_eve.py
@@ -5,16 +5,17 @@ from __future__ import absolute_import
 
 #pylint: disable=C0103,R0904,W0201,W0232,E1103
 import sunpy
+import unittest
 from sunpy.data.test import (EVE_AVERAGES_CSV)
 
-class TestEve:
+class TestEve(unittest.TestCase):
     """Tests the EVE class"""
-    def setup_class(self):
-        pass
+    
+    def test_eve(self):
+        eve = sunpy.lightcurve.EVELightCurve.create('2012/06/20')
+        self.assertIsInstance(eve, sunpy.lightcurve.EVELightCurve)
 
-    def teardown_class(self):
-        pass
-        
     def test_csv_parsing(self):
         """Check support for parsing EVE CSV files"""
-        sunpy.lightcurve.EVELightCurve.create(EVE_AVERAGES_CSV)
+        csv = sunpy.lightcurve.EVELightCurve.create(EVE_AVERAGES_CSV)
+        self.assertIsInstance(csv, sunpy.lightcurve.sources.eve.EVELightCurve)
