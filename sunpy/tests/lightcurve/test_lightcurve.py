45d830cb6722ff7ee6fdc1288952d403410266b3
Nabil Freij <nabil.freij@gmail.com>
2013-02-28 20:11:32 +0000

    Last small changes before this gets closed before I end up killing Sunpy by accident.

--- sunpy/tests/lightcurve/test_lightcurve.py
+++ sunpy/tests/lightcurve/test_lightcurve.py
@@ -13,6 +13,7 @@ import pytest
 import datetime
 import sunpy
 import unittest
+import matplotlib as mpl
 from sunpy.data.test import (EVE_AVERAGES_CSV)
 
 # Generate input test data
@@ -23,24 +24,35 @@ class TestLight(unittest.TestCase):
     """
     Tests the general LightCurv
     """
-    def
+    def test_class(self):
+        lyra = sunpy.lightcurve.LYRALightCurve.create(
+        "http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev2_std.fits")
+        self.assertIsInstance(lyra.peek(),mpl.figure.Figure)
+        pass
     
+    def test_peek(self):
+        lyra = sunpy.lightcurve.LYRALightCurve.create(
+        "http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev2_std.fits")
+        self.assertIsInstance(lyra.peek(),mpl.figure.Figure)
+        pass
+    
+    def test_plot(self):
+        pass
 
-#@pytest.mark.parametrize(("data", "index"), [
-#    (range(24 * 60), dates),
-#    (np.arange(24 * 60), dates),
-#    ({"param": range(24 * 60)}, dates)
-#])
-#def test_input(data, index):
-#    """Tests different types of expected input"""
-#    sunpy.lightcurve.LightCurve.create(data, index=index)
-#
-#@pytest.mark.parametrize(("bad_input"), [
-#    (None),
-#    (EVE_AVERAGES_CSV)
-#])
-#def test_unimplemented(bad_input):
-#    """Tests input that has not been implemented for the generic LC class"""
-#    with pytest.raises((TypeError, NotImplementedError)):
-#        sunpy.lightcurve.LightCurve.create(bad_input)
-#
+@pytest.mark.parametrize(("data", "index"), [
+    (range(24 * 60), dates),
+    (np.arange(24 * 60), dates),
+    ({"param": range(24 * 60)}, dates)
+])
+def test_input(data, index):
+    """Tests different types of expected input"""
+    sunpy.lightcurve.LightCurve.create(data, index=index)
+
+@pytest.mark.parametrize(("bad_input"), [
+    (None),
+    (EVE_AVERAGES_CSV)
+])
+def test_unimplemented(bad_input):
+    """Tests input that has not been implemented for the generic LC class"""
+    with pytest.raises((TypeError, NotImplementedError)):
+        sunpy.lightcurve.LightCurve.create(bad_input)
