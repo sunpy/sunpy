a528163cea90e2a88f2c8ad5854c527426f26ad9
Keith Hughitt <keith.hughitt@gmail.com>
2013-04-04 09:19:48 -0400

    Fixes issue #416: Helioviwer tests should not break on server error.

--- sunpy/tests/net/test_helioviewer.py
+++ sunpy/tests/net/test_helioviewer.py
@@ -5,22 +5,32 @@ from __future__ import absolute_import
 
 #pylint: disable=C0103,R0904,W0201,W0212,W0232,E1103
 import sunpy
+import pytest
 from sunpy.net.helioviewer import HelioviewerClient
+
+# If server is not accessible, skip Helioviewer tests
+client = HelioviewerClient()
+if not client.is_online():
+    __SKIP_TESTS__ = True
+else:
+    __SKIP_TESTS__ = False
 
 class TestHelioviewerClient:
     """Tests the Helioviewer.org API Client class"""
     def setup_class(self):
-        self.client = HelioviewerClient()
+        self.client = client
         self.sources = self.client.get_data_sources()
 
     def teardown_class(self):
         self.client = None
-        
+    
+    @pytest.mark.skipif("__SKIP_TESTS__ is True")
     def test_get_datasources(self):
         """Makes sure datasource query returns a valid result and source id
         is casted to an integer"""
         assert type(self.sources['SDO']['AIA']['AIA']['171']['sourceId']) is int
         
+    @pytest.mark.skipif("__SKIP_TESTS__ is True")
     def test_get_closest_image(self):
         """Tests getClosestImage API method"""
         # check basic query
@@ -38,6 +48,7 @@ class TestHelioviewerClient:
         
         assert im1 == im2
         
+    @pytest.mark.skipif("__SKIP_TESTS__ is True")
     def test_download_jp2(self):
         """Tests getJP2Image API method"""
         filepath = self.client.download_jp2('2020/01/01', observatory='SOHO', 
@@ -50,4 +61,3 @@ class TestHelioviewerClient:
             pass
         else:
             assert isinstance(map_, sunpy.Map)
-    
