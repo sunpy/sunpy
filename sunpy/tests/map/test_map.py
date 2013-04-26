b377a8446bea32f34faaf5b3bfb5abfb3ff0d87d
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-04-26 18:27:36 +0530

    Commenting the tests in sunpy.map.map

--- sunpy/tests/map/test_map.py
+++ sunpy/tests/map/test_map.py
@@ -191,9 +191,14 @@ class TestMap:
         rotated_map_4 = self.map.rotate(np.pi/2, 1.5)
         rotated_map_5 = self.map.rotate(np.pi, 1.5)
         assert rotated_map_2.shape == rotated_map_1.shape == self.map.shape
+        # Rotation of a map by non-integral multiple of pi/2 cuts off the corners
+        # and assigns the value of 0 to corner pixels. This results in reduction
+        # of the mean and an increase in standard deviation.
         assert rotated_map_2.mean() < rotated_map_1.mean() < self.map.mean()
         assert rotated_map_2.std() > rotated_map_1.std() > self.map.std()
         assert rotated_map_2.get_header() == rotated_map_1.get_header() == self.map.get_header()
         assert rotated_map_3.mean() > self.map.mean()
+        # Mean and std should be equal when angle of rotation is integral multiple
+        # of pi/2
         assert int(rotated_map_3.mean()) == int(rotated_map_4.mean()) == int(rotated_map_5.mean())
         assert int(rotated_map_3.std()) == int(rotated_map_4.std()) == int(rotated_map_5.std())
