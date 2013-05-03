dc32d2f4a587be3691d4323d6d2bf82db01edd32
Michael Malocha <mjm159@humboldt.edu>
2013-05-02 17:32:08 -0700

    Test 3

--- sunpy/map/map.py
+++ sunpy/map/map.py
@@ -962,7 +962,7 @@ installed, falling back to the interpolation='spline' of order=3""" ,Warning)
         return new_map
 
     @toggle_pylab
-    def plot(self, gamma=None, annotate=True, axes=None, coords='HG', **imshow_args):
+    def plot(self, gamma=None, annotate=True, axes=None, coords=None, **imshow_args):
         """ Plots the map object using matplotlib, in a method equivalent
         to plt.imshow() using nearest neighbour interpolation.
         
@@ -1037,7 +1037,7 @@ installed, falling back to the interpolation='spline' of order=3""" ,Warning)
             else:
                 ylabel = 'Y-position [%s]' % self.units['y']
                 
-            axes.set_xlabel(xlabel)
+            axes.set_xlabel("hello")
             axes.set_ylabel(ylabel)
 
         # Determine extent
