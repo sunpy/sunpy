009920c7fb74ddf8c15ea9265ad11555f6b70b96
Russell Hewett <rhewett@mit.edu>
2013-05-12 22:02:59 -0400

    Added basic factory for map and set up the list of potential loading patterns for the argument string.

--- sunpy/map/map_factory.py
+++ sunpy/map/map_factory.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 import os
 import glob
-                
+import urllib2
+
 import numpy as np
 
 from sunpy.map.map import Map
@@ -11,7 +12,124 @@ from sunpy.map.mapcube import MapCube
 from sunpy.map.compositemap import CompositeMap
 from sunpy.map.sources import *
 
+
 __all__ = ['make_map', 'read_header']
+
+
+MapFactoryArgParser(object):
+
+    # This can be hacked later to use ConditionalDispatch, but I'm not going to
+    # do that.  (This explains why it is a class and not a function at the moment.  Really, this should be a class method of Map (the factory).
+
+	@classmethod
+	def read_file(cls, fname):
+		pass
+
+    @classmethod
+    def __call__(cls, *args, **kwargs):
+        
+        data_header_pairs = list()
+        already_maps = list()
+        
+        # For each of the arguments, handle each of the cases
+        i = 0
+        while i < len(args):
+            
+            arg = args[i]
+            
+            # Data-header pair in a tuple
+            if (type(arg) in [tuple, list]) and 
+                          isinstance(arg[0],np.ndarray) and # or NDData or something else?
+                          isinstance(arg[1],SunpyMetaBase): # FITSHeader, JP2kHeader, OrderedDict, dict?
+                data_header_pairs.append(arg)
+            
+            # Data-header pair not in a tuple
+            elif isinstance(arg, np.ndarray) and # or NDData or something else?
+                          isinstance(args[i+1],SunpyMetaBase): # FITSHeader, JP2kHeader, OrderedDict, dict? 
+                pair = (args[i], args[i+1])
+                data_header_pairs.append(pair)
+                i += 1 # an extra increment to account for the data-header pairing
+            
+            # File name
+            elif type(arg) is basestring and 
+                          os.path.isfile(os.path.expanduser(arg)):
+                path = os.path.expanduser(arg)
+                pair = cls.read_file(path)
+                data_header_pairs.append(pair)
+            
+            # Directory
+            elif type(arg) is basestring and 
+                          os.path.isdir(os.path.expanduser(arg)):
+                path = os.path.expanduser(arg)
+                files = [os.path.join(directory, elem) for elem in os.listdir(path)]
+                data_header_pairs += map(cls.read_file, files)
+            
+            # Glob
+            elif type(arg) is basestring and 
+                          '*' in arg:
+                files = glob.glob( os.path.expanduser(arg) )
+                data_header_pairs += map(cls.read_file, files)
+            
+            # Already a Map
+            elif isinstance(arg, MapBase):
+                already_maps.append(arg)
+                
+            # A URL
+            elif type(arg) is basestring and 
+                          urllib2.urlopen(arg):
+                default_dir = sunpy.config.get("downloads", "download_dir")
+                path = download_file(url, default_dir)
+                pair = cls.read_file(path)
+                data_header_pairs.append(pair)
+        
+            i += 1
+            
+        return data_header_pairs, already_maps
+
+class Map(RegisteredFactoryBase):
+    
+    GenericWidgetType = GenericMap
+    
+    def __new__(cls, composite=False, cube=False, *args, **kwargs):
+
+        if cls is Map:
+            
+            # Get list of data-header pairs, e.g., [(d1, h1), (d2, h2), ...]
+            data_header_pairs = MapFactoryArgParser(*args, **kwargs)
+            
+            # If the list is meant to be a cube, instantiate a map cube
+            if cube:
+                return MapCube(*data_header_pairs, **kwargs)
+            # If the list is meant to be a composite mape, instantiate one
+            if composite:
+                return CompositeMap(*data_header_pairs, **kwargs)
+                
+            # Otherwise, each pair in the list gets built on its own
+            new_maps = list()
+            
+            for data, header in zip(*data_header_pairs):
+                
+                # Test to see which type of Map this pair is.  If none of the
+                # registered Map types match, use a generic map.
+                WidgetType = None
+                for key in self.registry:
+                    
+                    if cls.registry[key](data, header, **kwargs):
+                        WidgetType = key
+                        break
+                else:
+                    WidgetType = cls.GenericWidgetType
+                    
+                # Instantiate the new map.
+                new_maps.append(WidgetType(data, header, **kwargs))
+                
+            # If there was only one map instantiated, return that, otherwise
+            # return the list of them.
+            return new_maps[1] if len(new_maps) == 1 else new_maps
+        else:
+            return super(Map, cls).__new__(cls, *args, **kwargs)
+ 
+
 
 def make_map(*args, **kwargs):
     """Processes one or more inputs and returns a Map, MapCube, or CompositeMap
