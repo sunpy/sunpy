1232c44e6885257bc47995c2481023eb00183370
derdon <liedtke.simon@googlemail.com>
2013-06-09 18:53:50 +0200

    added class CommandManager which acts as an undo-redo-manager and the command classes AddEntry, EditEntry, RemoveEntry

---
+++ sunpy/tests/database/test_commands.py
@@ -0,0 +1,93 @@
+from __future__ import absolute_import
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+from sunpy.database.commands import AddEntry, RemoveEntry, EditEntry,\
+    NoSuchEntryError
+from sunpy.database.tables import DatabaseEntry
+
+
+@pytest.fixture
+def session():
+    # always create an in-memory database with its own new table in each test
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker()
+    DatabaseEntry.metadata.create_all(bind=engine)
+    return Session(bind=engine)
+
+
+def test_add_entry(session):
+    assert not session.new
+    entry = DatabaseEntry()
+    AddEntry(session, entry)()
+    assert len(session.new) == 1
+    assert entry.id is None
+    session.commit()
+    assert not session.new
+    assert entry.id == 1
+
+
+def test_add_entry_undo(session):
+    entry = DatabaseEntry()
+    cmd = AddEntry(session, entry)
+    cmd()
+    assert session.query(DatabaseEntry).count() == 1
+    assert entry.id == 1
+    cmd.undo()
+    assert entry in session.deleted
+    assert session.query(DatabaseEntry).count() == 0
+
+
+def test_edit_entry_invalid(session):
+    with pytest.raises(ValueError):
+        EditEntry(DatabaseEntry())
+
+
+def test_edit_entry(session):
+    entry = DatabaseEntry()
+    session.add(entry)
+    session.commit()
+    assert entry.id == 1
+    EditEntry(entry, id=42)()
+    assert entry.id == 42
+
+
+def test_edit_entry_undo(session):
+    entry = DatabaseEntry()
+    session.add(entry)
+    session.commit()
+    cmd = EditEntry(entry, id=42)
+    cmd()
+    session.commit()
+    assert entry.id == 42
+    cmd.undo()
+    session.commit()
+    assert entry.id == 1
+
+
+def test_remove_existing_entry(session):
+    entry = DatabaseEntry()
+    session.add(entry)
+    assert session.query(DatabaseEntry).count() == 1
+    assert entry.id == 1
+    RemoveEntry(session, entry)()
+    assert entry in session.deleted
+    assert session.query(DatabaseEntry).count() == 0
+
+
+def test_remove_nonexisting_entry(session):
+    with pytest.raises(NoSuchEntryError):
+        RemoveEntry(session, DatabaseEntry())()
+
+
+def test_remove_entry_undo(session):
+    entry = DatabaseEntry()
+    session.add(entry)
+    cmd = RemoveEntry(session, entry)
+    session.commit()
+    cmd()
+    assert session.query(DatabaseEntry).count() == 0
+    cmd.undo()
+    assert session.query(DatabaseEntry).count() == 1
