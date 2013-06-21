2391722377860b8b447fdf391a49e02d5382ec41
rhewett <russell.j.hewett@gmail.com>
2013-06-21 12:04:43 -0400

    Update compositemap.py

--- sunpy/map/compositemap.py
+++ sunpy/map/compositemap.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from sunpy.map import GenericMap
 import sunpy.map.map_factory
 
+from sunpy.util import expand_list
+
 __all__ = ['CompositeMap']
 
 __author__ = "Keith Hughitt"
@@ -67,7 +69,7 @@ class CompositeMap(object):
     >>> comp_map.peek()
 
     """    
-    def __init__(self, maps, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Create a CompositeMap
         
@@ -76,7 +78,12 @@ class CompositeMap(object):
         Maps: SunPy Maps
             A sequence of maps
         """
-        self._maps = maps
+        
+        self._maps = expand_list(args)
+        
+        for m in self._maps:
+            if not isinstance(m, GenericMap):
+                raise ValueError('CompositeMap expects pre-constructed map objects.')
         
         # Default alpha and zorder values
         alphas = [1] * len(args)
