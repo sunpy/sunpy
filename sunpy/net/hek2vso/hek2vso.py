0bdd6f9a9da7ec36d66d0120db91329233254483
Michael Malocha <mjm159@humboldt.edu>
2013-06-24 16:59:39 -0700

    Updated HEK2VSOTool .query() to more efficiently handle a Hek query, updated to .testing() to include a simple VSO test, and added the method .return_hek_results to provide a method to return the JSON results from the HEK query in either unchanged or formatted results

--- sunpy/net/hek2vso/hek2vso.py
+++ sunpy/net/hek2vso/hek2vso.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import
 
 import json
 from sunpy.net import hek
+from sunpy.net import vso
 
 __author__ = "Michael Malocha"
 
@@ -18,23 +19,56 @@ class HEK2VSOTool(object):
     """This tool is to take in a HEK query and return a VSO result"""
     # Still needs the VSO handler
 
+    # Here is some test data
+    t_start = '2011/08/09 07:23:56'
+    t_end = '2011/08/09 12:40:29'
+    t_event = 'FL'
+
     def __init__(self):
         self.hek_client = hek.HEKClient()
+        self.hek_results = ''
 
-    def query(self, *query):
+    def query(self, *client_query):
         """Takes a HEK style query and passes it to a HEKClient instance"""
-        results = self.hek_client.query(query)
+        self.hek_results = self.hek_client.query(*client_query)
 
+        # Saving the results to a local log file. Future changes:
+        # - user set path
+        # - incorporate timestamp in name
         with open('h2v-test.log', 'w') as temp_file:
-            temp_file.write(json.dumps(results))
+            temp_file.write(json.dumps(self.hek_results))
 
-        with open('h2v-test.log', 'r') as temp_file:
-            results = temp_file.read()
+        # with open('h2v-test.log', 'r') as temp_file:
+        #     self.hek_results = temp_file.read()
 
-        print json.dumps(json.loads(results), sort_keys=True,
+
+    def return_hek_results(self, formatted=False):
+        """A simple method to return the HEK results in either formatted
+        or un-formatted JSON form"""
+        if formatted:
+            return json.dumps(self.hek_results, sort_keys=True,
                          indent=4, separators=(',', ': '))
+        else:
+            return self.hek_results
 
     def testing(self):
-        """A docstring"""
-        pass
+        """Testing to see if this class plays nicely with the HEK and
+        VSO webservice modules"""
 
+        # Testing the HEK interface
+        client = hek.HEKClient()
+        results = client.query(hek.attrs.Time(self.t_start, self.t_end),
+                               hek.attrs.EventType(self.t_event))
+        if len(results) > 0:
+            print "HEK Play-Nice Test: Pass"
+        else:
+            print "HEK Play-Nice Test: Fail"
+
+        # Testing the VSO interface
+        client = vso.VSOClient()
+        results = client.query(vso.attrs.Time('2001/1/1', '2001/1/2'),
+                               vso.attrs.Instrument('eit'))
+        if results.num_records() > 0:
+            print "VSO Play-Nice Test: Pass"
+        else:
+            print "VSO Play-Nice Test: Fail"
