5637f71bb0dae59eb2cb85589e35328c14c9b101
derdon <liedtke.simon@googlemail.com>
2013-07-31 23:55:00 +0200

    added FitsHeaderEntry attribute for querying the database

--- sunpy/tests/database/test_attrs.py
+++ sunpy/tests/database/test_attrs.py
@@ -4,7 +4,8 @@ import pytest
 
 from sunpy.database.database import Database
 from sunpy.database import tables
-from sunpy.database.attrs import walker, Starred, Tag, Path, DownloadTime
+from sunpy.database.attrs import walker, Starred, Tag, Path, DownloadTime,\
+    FitsHeaderEntry
 from sunpy.net.attr import DummyAttr, AttrAnd, AttrOr
 
 
@@ -31,6 +32,8 @@ def session():
         # every fifth entry gets the tag 'foo'
         if i % 5 == 0:
             database.tag(entry, 'foo')
+    # the last entry gets the FITS header entry INSTRUME=EIT
+    entry.fits_header_entries.append(tables.FitsHeaderEntry('INSTRUME', 'EIT'))
     database.commit()
     return database.session
 
@@ -103,6 +106,12 @@ def test_inverted_downloadtime_repr():
     assert repr(download_time) == expected_repr
 
 
+def test_fitsheaderentry_repr():
+    header_entry = FitsHeaderEntry('key', 'value')
+    assert repr(header_entry) == "<FitsHeaderEntry('key', 'value')>"
+    assert repr(~header_entry) == "<~FitsHeaderEntry('key', 'value')>"
+
+
 def test_walker_create_dummy(session):
     with pytest.raises(TypeError):
         walker.create(DummyAttr(), session)
@@ -112,6 +121,8 @@ def test_walker_create_starred_true(session):
     entries = walker.create(Starred(), session)
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     assert len(entries) == 5
     assert entries == [
         tables.DatabaseEntry(
@@ -125,7 +136,8 @@ def test_walker_create_starred_true(session):
             id=8, starred=True, download_time=datetime(2005, 6, 15, 8)),
         tables.DatabaseEntry(
             id=10, starred=True, tags=[tag],
-            download_time=datetime(2005, 6, 15, 10))]
+            download_time=datetime(2005, 6, 15, 10),
+            fits_header_entries=[fits_header_entry])]
 
 
 def test_walker_create_starred_false(session):
@@ -149,13 +161,16 @@ def test_walker_create_tag_positive(session):
     entries = walker.create(Tag('foo'), session)
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     assert len(entries) == 2
     assert entries == [
         tables.DatabaseEntry(
             id=5, tags=[tag], download_time=datetime(2005, 6, 15, 5)),
         tables.DatabaseEntry(
             id=10, starred=True, tags=[tag],
-            download_time=datetime(2005, 6, 15, 10))]
+            download_time=datetime(2005, 6, 15, 10),
+            fits_header_entries=[fits_header_entry])]
 
 
 def test_walker_create_tag_negative(session):
@@ -185,9 +200,12 @@ def test_walker_create_anded_query(session):
     assert len(entries) == 1
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     assert tables.DatabaseEntry(
         id=10, starred=True, tags=[tag],
-        download_time=datetime(2005, 6, 15, 10)) in entries
+        download_time=datetime(2005, 6, 15, 10),
+        fits_header_entries=[fits_header_entry]) in entries
 
 
 def test_walker_create_ored_query(session):
@@ -195,6 +213,8 @@ def test_walker_create_ored_query(session):
     assert len(entries) == 6
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     assert tables.DatabaseEntry(
         id=2, starred=True, download_time=datetime(2005, 6, 15, 2)) in entries
     assert tables.DatabaseEntry(
@@ -208,7 +228,8 @@ def test_walker_create_ored_query(session):
         id=8, starred=True, download_time=datetime(2005, 6, 15, 8)) in entries
     assert tables.DatabaseEntry(
         id=10, starred=True, tags=[tag],
-        download_time=datetime(2005, 6, 15, 10)) in entries
+        download_time=datetime(2005, 6, 15, 10),
+        fits_header_entries=[fits_header_entry]) in entries
 
 
 def test_walker_create_complex_query(session):
@@ -217,6 +238,8 @@ def test_walker_create_complex_query(session):
     assert len(entries) == 5
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     assert tables.DatabaseEntry(
         id=1, download_time=datetime(2005, 6, 15, 1)) in entries
     assert tables.DatabaseEntry(
@@ -227,7 +250,8 @@ def test_walker_create_complex_query(session):
         id=9, path='/tmp', download_time=datetime(2005, 6, 15, 9)) in entries
     assert tables.DatabaseEntry(
         id=10, starred=True, tags=[tag],
-        download_time=datetime(2005, 6, 15, 10)) in entries
+        download_time=datetime(2005, 6, 15, 10),
+        fits_header_entries=[fits_header_entry]) in entries
 
 
 def test_walker_create_path_attr_notfound(session):
@@ -249,6 +273,8 @@ def test_walker_create_path_attr_exists(session):
 def test_walker_create_path_inverted(session):
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     entries = walker.create(~Path('/tmp'), session)
     assert len(entries) == 7
     assert entries == [
@@ -265,7 +291,8 @@ def test_walker_create_path_inverted(session):
             id=8, starred=True, download_time=datetime(2005, 6, 15, 8)),
         tables.DatabaseEntry(
             id=10, starred=True, tags=[tag],
-            download_time=datetime(2005, 6, 15, 10))]
+            download_time=datetime(2005, 6, 15, 10),
+            fits_header_entries=[fits_header_entry])]
 
 
 def test_walker_create_downloadtime_notfound(session):
@@ -290,6 +317,8 @@ def test_walker_create_downloadtime_exists(session):
 def test_walker_create_downloadtime_inverted(session):
     tag = tables.Tag('foo')
     tag.id = 1
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
     download_time = ~DownloadTime(
         datetime(2005, 6, 15, 7), datetime(2005, 6, 15, 9))
     entries = walker.create(download_time, session)
@@ -310,4 +339,44 @@ def test_walker_create_downloadtime_inverted(session):
             download_time=datetime(2005, 6, 15, 6)),
         tables.DatabaseEntry(
             id=10, starred=True, tags=[tag],
-            download_time=datetime(2005, 6, 15, 10))]
+            download_time=datetime(2005, 6, 15, 10),
+            fits_header_entries=[fits_header_entry])]
+
+
+def test_walker_create_fitsheader(session):
+    tag = tables.Tag('foo')
+    tag.id = 1
+    entries = walker.create(FitsHeaderEntry('INSTRUME', 'EIT'), session)
+    fits_header_entry = tables.FitsHeaderEntry('INSTRUME', 'EIT')
+    fits_header_entry.id = 1
+    assert len(entries) == 1
+    assert entries == [tables.DatabaseEntry(
+        id=10, starred=True, tags=[tag],
+        download_time=datetime(2005, 6, 15, 10),
+        fits_header_entries=[fits_header_entry])]
+
+
+def test_walker_create_fitsheader_inverted(session):
+    tag = tables.Tag('foo')
+    tag.id = 1
+    entries = walker.create(~FitsHeaderEntry('INSTRUME', 'EIT'), session)
+    assert len(entries) == 9
+    assert entries == [
+        tables.DatabaseEntry(
+            id=1, download_time=datetime(2005, 6, 15, 1)),
+        tables.DatabaseEntry(
+            id=2, starred=True, download_time=datetime(2005, 6, 15, 2)),
+        tables.DatabaseEntry(
+            id=3, path='/tmp', download_time=datetime(2005, 6, 15, 3)),
+        tables.DatabaseEntry(
+            id=4, starred=True, download_time=datetime(2005, 6, 15, 4)),
+        tables.DatabaseEntry(
+            id=5, tags=[tag], download_time=datetime(2005, 6, 15, 5)),
+        tables.DatabaseEntry(
+            id=6, starred=True, path='/tmp',
+            download_time=datetime(2005, 6, 15, 6)),
+        tables.DatabaseEntry(id=7, download_time=datetime(2005, 6, 15, 7)),
+        tables.DatabaseEntry(
+            id=8, starred=True, download_time=datetime(2005, 6, 15, 8)),
+        tables.DatabaseEntry(
+            id=9, path='/tmp', download_time=datetime(2005, 6, 15, 9))]
