97d3253ebcbaa004fe3cfb8f8d3dfbf4f36c012b
derdon <liedtke.simon@googlemail.com>
2013-08-10 12:54:20 +0200

    remove sunpy.database.tables imports in sunpy.database package file avoids ambiguity because of naming clashes with classes from the attrs module

--- sunpy/database/__init__.py
+++ sunpy/database/__init__.py
@@ -5,9 +5,6 @@ The database package exports the following classes, functions and exceptions:
 
     :classes:
         - Database
-        - DatabaseEntry
-        - FitsHeaderEntry
-        - Tag
     :functions:
         - display_entries
     :exceptions:
@@ -23,12 +20,9 @@ from __future__ import absolute_import
 from sunpy.database.database import Database, EntryAlreadyAddedError,\
     EntryAlreadyStarredError, EntryAlreadyUnstarredError, NoSuchTagError,\
     EntryNotFoundError, TagAlreadyAssignedError
-from sunpy.database.tables import DatabaseEntry, FitsHeaderEntry, Tag,\
-    display_entries
 from sunpy.database.commands import NoSuchEntryError
 
 __all__ = [
     'Database', 'EntryAlreadyAddedError', 'NoSuchEntryError', 'NoSuchTagError',
     'EntryAlreadyStarredError', 'EntryAlreadyUnstarredError',
-    'EntryNotFoundError', 'TagAlreadyAssignedError', 'DatabaseEntry',
-    'FitsHeaderEntry', 'Tag', 'display_entries']
+    'EntryNotFoundError', 'TagAlreadyAssignedError', 'display_entries']
