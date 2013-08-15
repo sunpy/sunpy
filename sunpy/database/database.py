ab2a4b65e305027bcced647572e37330edc218fa
derdon <liedtke.simon@googlemail.com>
2013-08-16 01:49:49 +0200

    refer to Database.add in docs of Database.add_from_path

--- sunpy/database/database.py
+++ sunpy/database/database.py
@@ -510,10 +510,7 @@ class Database(object):
             see its documentation for more information on the supported syntax.
 
         ignore_already_added : bool, optional
-            If True, attempts to add an already exisiting database entry will
-            result in a :exc:`sunpy.database.EntryAlreadyAddedError`.
-            Otherwise, a new entry will be added and there will be duplicates
-            in the database.
+            See :meth:`sunpy.database.Database.add`.
 
         """
         cmds = []
