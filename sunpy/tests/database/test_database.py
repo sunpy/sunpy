1546f84d5628583e71cb9c37552c618f0866a590
derdon <liedtke.simon@googlemail.com>
2013-08-27 19:54:15 +0200

    simplify tests

--- sunpy/tests/database/test_database.py
+++ sunpy/tests/database/test_database.py
@@ -555,22 +555,16 @@ def test_lru_cache(database_using_lrucache):
     database_using_lrucache.add(entry3)
     assert len(database_using_lrucache) == 3
     entries = list(database_using_lrucache)
-    assert entries[0] == entry1
-    assert entries[1] == entry2
-    assert entries[2] == entry3
-    #assert database_using_lrucache._cache.items() == [
-    #    (1, entry1), (2, entry2), (3, entry3)]
+    assert database_using_lrucache._cache.items() == [
+        (1, entry1), (2, entry2), (3, entry3)]
     database_using_lrucache.get_entry_by_id(1)
     database_using_lrucache.get_entry_by_id(3)
     entry4 = DatabaseEntry()
     database_using_lrucache.add(entry4)
     assert len(database_using_lrucache) == 3
     entries = list(database_using_lrucache)
-    assert entries[0] == entry1
-    assert entries[1] == entry3
-    assert entries[2] == entry4
-    #assert database_using_lrucache._cache.items() == [
-    #    (1, entry1), (3, entry3), (4, entry4)]
+    assert database_using_lrucache._cache.items() == [
+        (1, entry1), (3, entry3), (4, entry4)]
 
 
 def test_lfu_cache(database_using_lfucache):
@@ -581,11 +575,8 @@ def test_lfu_cache(database_using_lfucache):
     database_using_lfucache.add(entry3)
     assert len(database_using_lfucache) == 3
     entries = list(database_using_lfucache)
-    assert entries[0] == entry1
-    assert entries[1] == entry2
-    assert entries[2] == entry3
-    #assert database_using_lrucache._cache.items() == [
-    #    (1, entry1), (2, entry2), (3, entry3)]
+    assert database_using_lfucache._cache.items() == [
+        (1, entry1), (2, entry2), (3, entry3)]
     # access the entries #1 and #2 to increment their counters
     database_using_lfucache.get_entry_by_id(1)
     database_using_lfucache.get_entry_by_id(2)
@@ -593,11 +584,8 @@ def test_lfu_cache(database_using_lfucache):
     database_using_lfucache.add(entry4)
     assert len(database_using_lfucache) == 3
     entries = list(database_using_lfucache)
-    assert entries[0] == entry1
-    assert entries[1] == entry2
-    assert entries[2] == entry4
-    #assert database_using_lrucache._cache.items() == [
-    #    (1, entry1), (2, entry2), (4, entry4)]
+    assert database_using_lfucache._cache.items() == [
+        (1, entry1), (2, entry2), (4, entry4)]
 
 
 def test_query_missing_arg(database):
