c65eafb5ecbbb5d81a4586e3e894c41033c0d0b0
Stuart Mumford <stuart@mumford.me.uk>
2013-08-30 15:02:26 +0100

    fix test on 2.6

--- sunpy/tests/lightcurve/test_goes.py
+++ sunpy/tests/lightcurve/test_goes.py
@@ -13,13 +13,22 @@ class TestGOESLightCurve(unittest.TestCase):
        
        assert isinstance(lc1, sunpy.lightcurve.GOESLightCurve)
     
+    def compare(self,lc1,lc2):
+        try:
+            (lc1.data == lc2.data)
+        except:
+            raise Exception
     def test_filename(self):
         lc1 = sunpy.lightcurve.GOESLightCurve.create('2012/06/01','2012/06/02')
         lc2 = sunpy.lightcurve.GOESLightCurve.create('2012/06/03','2012/06/04')
         #If the dataframes are non-idential it raises an error, if they are
         #identical it returns True
-        with self.assertRaises(Exception) as context:
-            (lc1.data == lc2.data).all()
+        self.assertRaises(Exception, self.compare, lc1, lc2)
         
-        self.assertEqual(context.exception.message,
-                         'Can only compare identically-labeled DataFrame objects')
+        #This snippet is better but python 2.7 only, maybe sometime it will be 
+        #useful
+#        with self.assertRaises(Exception) as context:
+#            (lc1.data == lc2.data).all()
+#        
+#        self.assertEqual(context.exception.message,
+#                         'Can only compare identically-labeled DataFrame objects')
