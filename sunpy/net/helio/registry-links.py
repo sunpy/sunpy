ec613ab18d07e62a1cf77a401837fbef47c32a18
Michael Malocha <mjm159@humboldt.edu>
2013-09-09 08:19:44 -0700

    adding WSDL parser functions

---
+++ sunpy/net/helio/registry-links.py
@@ -0,0 +1,4 @@
+# contains links to HELIO registries
+
+HEC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhec'
+HFC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhfc'
