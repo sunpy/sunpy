f753165db09b3e85030ceab6499703642e95acca
Michael Malocha <mjm159@humboldt.edu>
2013-09-09 08:31:06 -0700

    __init__ update

--- sunpy/net/__init__.py
+++ sunpy/net/__init__.py
@@ -1,3 +1,7 @@
 from __future__ import absolute_import
 
-from sunpy.net.helioviewer import *
+#from sunpy.net.helioviewer import *
+#from sunpy.net.hek import *
+#from sunpy.net.hek2vso import *
+#from sunpy.net.helio import *
+#from sunpy.net.vso import *
--- sunpy/net/helio/registry-links.py
+++ sunpy/net/helio/registry-links.py
@@ -1,4 +1,5 @@
 # contains links to HELIO registries
+from __future__ import absolute_import
 
 HEC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhec'
 HFC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhfc'
