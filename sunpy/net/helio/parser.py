edbd2c04758b5ead18218d3c936eb44a503bebd1
Michael Malocha <mjm159@humboldt.edu>
2013-09-09 08:19:44 -0700

    adding WSDL parser functions

---
+++ sunpy/net/helio/parser.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Author:   Michael Malocha <mjm159@humboldt.edu>
+# Last Edit:  September 7th, 2013
+#
+# This module was developed with funding from the GSOC 2013 summer of code
+#
