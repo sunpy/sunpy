d7245e946fb6884c20ac2f313272cf8600c8a385
Michael Malocha <mjm159@humboldt.edu>
2013-09-14 10:35:30 -0700

    fixed minor data-type error and fixed errors in the __init__ files I wrote.

--- sunpy/net/helio/__init__.py
+++ sunpy/net/helio/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 
 #from sunpy.net.helio.hec import *
 #from sunpy.net.helio.hfc import *
-from sunpy.net.helio.registry import *
-from sunpy.net.helio.parser import *
+#from sunpy.net.helio.registry import *
+#from sunpy.net.helio.parser import *
--- sunpy/net/helio/registry-links.py
+++
@@ -1,5 +0,0 @@
-# contains links to HELIO registries
-from __future__ import absolute_import
-
-HEC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhec'
-HFC = 'http://msslkz.mssl.ucl.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo%3A%2F%2Fhelio-vo.eu%2Fhfc'
