35096d7613d7f1b97fec53bef51a84f0522cb1d9
Michael Malocha <mjm159@humboldt.edu>
2013-09-14 10:35:30 -0700

    fixed minor data-type error and fixed errors in the __init__ files I wrote.

--- sunpy/net/__init__.py
+++ sunpy/net/__init__.py
@@ -1,7 +1,2 @@
 from __future__ import absolute_import
 
-#from sunpy.net.helioviewer import *
-#from sunpy.net.hek import *
-#from sunpy.net.hek2vso import *
-#from sunpy.net.helio import *
-#from sunpy.net.vso import *
