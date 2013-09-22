0b7f287d0bb59cf1906509622a94b009e8ba35f6
Michael Malocha <mjm159@humboldt.edu>
2013-09-21 19:03:39 -0700

    polished the parser module and began fleshing out the hec module. Currently returns a beautiful VOtable obejct containing the hec table names for a query.

--- sunpy/net/helio/__init__.py
+++ sunpy/net/helio/__init__.py
@@ -1,6 +1,2 @@
 from __future__ import absolute_import
 
-#from sunpy.net.helio.hec import *
-#from sunpy.net.helio.hfc import *
-#from sunpy.net.helio.registry import *
-#from sunpy.net.helio.parser import *
--- sunpy/net/helio/parser.py
+++ sunpy/net/helio/parser.py
@@ -22,21 +22,17 @@ __version__ = 'September 13th, 2013'
 def webservice_parser(service='HEC'):
     """
     Quickly parses important contents from HELIO registry.
-
-
     """
     link = RL.LINK
     # This if block will allow future service additions.
-    if service == 'HEC':
-        link += 'hec'
-    elif service == 'HFC':
-        link += 'hfc'
-    else:
-        print 'Unknown service'
+    # turn into a dictionary
+    link += service.lower()
+    if link_test(link) is None:
         return None
     xml = requests.get(link).text
     root = EL.fromstring(xml)
     links = []
+    # Add loop to parse xml text for table names under the 'relatedResource' tag.
     for interface in root.iter('interface'):
         service_type = interface.attrib
         key = service_type.keys()
@@ -63,6 +59,22 @@ def endpoint_parser(link):
     return endpoints
 
 
+def taverna_parser(link):
+    """
+    Takes a link to a list of endpoints and parses the taverna WSDL link
+    """
+    endpoints = endpoint_parser(link)
+    taverna_links = []
+    if endpoints is None:
+        return None
+    for web_link in endpoints:
+        if 'Taverna' in web_link:
+            taverna_links.append(web_link)
+    if len(taverna_links) == 0:
+        return None
+    return taverna_links
+
+
 def link_test(link):
     """
     Just a quick function to test a link.
@@ -76,18 +88,19 @@ def link_test(link):
 
 def wsdl_retriever(service='HEC'):
     """
-    uses webservices_parser and endpoint_parser to retrieve a WSDL file
+    uses webservices_parser & endpoint_parser to retrieve a taverna WSDL file
     """
     service_links = webservice_parser(service=service)
+    wsdl = None
+    wsdl_links = None
     if service_links is None:
         return None
     for link in service_links:
-        wsdl = endpoint_parser(link)
-        if wsdl is not None:
+        wsdl_links = taverna_parser(link)
+    if wsdl_links is None:
+        return None
+    for end_point in wsdl_links:
+        if end_point is not None and link_test(end_point) is not None:
+            wsdl = end_point
             break
-    wsdl_link = None
-    for link in wsdl:
-        if link_test(link) is not None:
-            wsdl_link = link
-            break
-    return wsdl_link
+    return wsdl
