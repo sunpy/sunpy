1f2cb8140bdd67f90f2d345ddd3519d24948897e
Tomas Meszaros <exo@tty.sk>
2013-11-15 15:27:26 +0100

    hypermap & coordinate_system prototype

---
+++ sunpy/hypermap/__init__.py
@@ -0,0 +1,8 @@
+"""SunPy Hypermap"""
+from __future__ import absolute_import
+
+__author__ = "Tomas Meszaros"
+__email__ = "exo@tty.sk"
+
+from sunpy.hypermap.hypermap import Hypermap
+from sunpy.hypermap.coordinate_system import CoordinateSystem
