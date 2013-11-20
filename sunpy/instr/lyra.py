79fc6808bf6059e8de8c0b5252c5508a1e8c3c14
aringlis <a.r.inglis@gmail.com>
2013-11-20 14:13:43 -0500

    added a split_series function to the instr/lyra module, which will split up a timeseries based on the results of a LYTAF query.

--- sunpy/instr/lyra.py
+++ sunpy/instr/lyra.py
@@ -56,6 +56,93 @@ def get_lytaf_events(timerange,lytaf_dir=''):
     
     return output
 
+def split_series_using_lytaf(timearray,data,lar):
+    """
+    Proba-2 analysis code for splitting up LYRA timeseries around locations where LARs
+    (and other data events) are observed.
+
+    Inputs
+    ------
+    timearray - an array of times that can be understood by the SunPy parse_time function
+    data - data array corresponding to the given time array
+    lar - list of events obtained from querying the LYTAF database using lyra.get_lytaf_events()
+
+    Output
+    ------
+    A list of dictionaries. Each dictionary contains a sub-series corresponding to an interval of 'good data'
+    """    
+    #lar is a dictionary with tags:
+    #'start_time'
+    #'end_time'
+    #'ref_time'
+    #'roi_description'
+    #'event_type_description'
+    #'event_type_id'
+
+    
+    n=len(timearray)
+    mask=np.ones(n)
+    el=len(lar)
+
+    #make the input time array a list of datetime objects
+    datetime_array=[]
+    for tim in timearray:
+        datetime_array.append(parse_time(tim))
+        
+
+        #scan through each entry retrieved from the LYTAF database
+    for j in range(0,el):
+        #want to mark all times with events as bad in the mask, i.e. = 0
+        start_dt=lar[j]['start_time']
+        end_dt=lar[j]['end_time']
+
+        #find the start and end indices for each event
+        start_ind=np.searchsorted(datetime_array,start_dt)
+        end_ind=np.searchsorted(datetime_array,end_dt)
+
+        #append the mask to mark event as 'bad'
+        mask[start_ind:end_ind] = 0
+
+
+    diffmask=np.diff(mask)
+    tmp_discontinuity=np.where(diffmask != 0.)
+    #disc contains the indices of mask where there are discontinuities
+    disc = tmp_discontinuity[0]
+
+    if len(disc) == 0:
+        print 'No events found within time series interval. Returning original series.'
+        return {'subtimes':datetime_array,'subdata':data}
+    
+    #-1 in diffmask means went from good data to bad
+    #+1 means went from bad data to good
+
+    #want to get the data between a +1 and the next -1
+
+    #if the first discontinuity is a -1 then the start of the series was good. 
+    if diffmask[disc[0]] == -1.0:
+        #make sure we can always start from disc[0] below
+        disc=np.insert(disc,0,0)
+    
+    split_series=[]
+
+    limit=len(disc)
+    #now extract the good data regions and ignore the bad ones
+    for h in range(0,limit,2):
+
+        if h == limit-1:
+            #can't index h+1 here. Go to end of series
+            subtimes=datetime_array[disc[h]:-1]
+            subdata=data[disc[h]:-1]
+            subseries={'subtimes':subtimes,'subdata':subdata}
+            split_series.append(subseries)
+        else:
+            subtimes=datetime_array[disc[h]:disc[h+1]]
+            subdata=data[disc[h]:disc[h+1]]
+            subseries={'subtimes':subtimes,'subdata':subdata}
+            split_series.append(subseries)
+
+    return split_series
+
 def _lytaf_event2string(integers):
     if type(integers) == int:
         integers=[integers]
