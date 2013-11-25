ad16b136e57cd015e071aee175f7181af9c3a10a
aringlis <a.r.inglis@gmail.com>
2013-11-25 10:17:09 -0500

    edits to lyra utility tests.

--- sunpy/tests/instr/test_lyra.py
+++ sunpy/tests/instr/test_lyra.py
@@ -3,15 +3,25 @@ from __future__ import absolute_import
 from urllib2 import urlopen
 from sunpy.time import TimeRange
 from sunpy.instr import lyra
+import tempfile
+import os
+import pytest
 
-def test_lytaf_url_exists():
-    url='http://proba2.oma.be/lyra/data/lytaf/annotation_ppt.db'
-    assert urlopen(url).code == 200
+@pytest.mark.online
+def test_lytaf_utils():
+    '''test the downloading of the LYTAF file'''
+    tmp_dir=tempfile.mkdtemp() + '/'
+    lyra.download_lytaf_database(lytaf_dir=tmp_dir)
+    assert os.path.exists(tmp_dir + 'annotation_ppt.db') == True
 
-def test_lytaf_query():
-    """Have a small sample of the LYTAF database available in data/tests for testing"""
-    dir='../../data/test/'
-    lar=lyra.get_lytaf_events(TimeRange('2010-06-13 02:00','2010-06-13 06:00'),lytaf_dir=dir)
+    #try doing a query on the temporary database
+    lar=lyra.get_lytaf_events(TimeRange('2010-06-13 02:00','2010-06-13 06:00'),lytaf_dir=tmp_dir)
     assert type(lar) == list
     assert type(lar[0]) == dict
+    assert type(lar[0]['start_time']) == datetime.datetime
+    assert type(lar[0]['end_time']) == datetime.datetime
+    assert type(lar[0]['roi_description'] == str
+    assert type(lar[0]['event_type_description']) == str
+    
+    
     
