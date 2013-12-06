0198bd42ec87eb59d881b348bb7663bb8cb754a5
Tomas Meszaros <exo@tty.sk>
2013-12-06 15:12:06 +0100

    Added support for Spectral + some error checking.

--- sunpy/hypermap/sources/iris.py
+++ sunpy/hypermap/sources/iris.py
@@ -7,7 +7,8 @@ from __future__ import absolute_import
 __author__ = "Tomas Meszaros"
 __email__ = "exo@tty.sk"
 
-import sunpy.io
+import collections
+
 from sunpy.hypermap.coordinate_system import CoordinateFrame
 from sunpy.hypermap.coordinate_system import CoordinateSystem
 from sunpy.hypermap.coordinate_system import SpatialFrame
@@ -18,24 +19,29 @@ class Parser(object):
 
     Parameters
     ----------
-    iris_raw_data : string
-        fits data file
+    header : collections.Mapping instance
+        e.g.: sunpy.io.header.FileHeader
     """
 
-    def __init__(self, iris_raw_data):
-        hdus = sunpy.io.read_file(iris_raw_data)
-        self._header = hdus[0][1]
+    def __init__(self, header):
+        if not isinstance(header, collections.Mapping):
+            raise HeaderTypeError("Header have to be an instance of the \
+                                   collections.Mapping!")
+        self._header = header
 
     _types_dictionary = {'HPLN-TAN': 'Spatial',
                          'HPLT-TAN': 'Spatial',
-                         'Time': 'Time'}
+                         'Time': 'Time',
+                         'WAVE': 'Spectral'}
 
     def _get_header_item_group(self, group):
         """
-        Filter and return item group (e.g. 'CTYPE') from IRIS header items.
+        Filter header and return list of items of a specific header
+        group (e.g. 'CTYPE').
+        Return empty list when unable to find @group in @_header.items().
         """
-        return filter(lambda x: not x[0].find(group) and
-                                not x[0] == group, self._header.items())
+        return filter(lambda x: not x[0].find(group) and not x[0] == group,
+                                self._header.items())
 
     def _make_frame_list(self):
         """
@@ -43,22 +49,47 @@ class Parser(object):
         with it later.
         """
         frame_list = []
-        num_axes = len(self._get_header_item_group('NAXIS'))
+
+        naxiss = self._get_header_item_group('NAXIS')
         ctypes = self._get_header_item_group('CTYPE')
         cunits = self._get_header_item_group('CUNIT')
         crpixs = self._get_header_item_group('CRPIX')
         # TODO: Where can we get axes names?
 
+        # We want to stop when we are unable to find required keywords,
+        # e.g. 'CTYPE', 'CUNIT', etc., in the header (header is probably
+        # missing them).
+        if [] in [naxiss, ctypes, cunits, crpixs]:
+            raise MissingWCSKeywords("Missing WCS keyword in the header!")
+
+        num_axes = len(self._get_header_item_group('NAXIS'))
+
         for i in range(num_axes):
             frame_type = self._types_dictionary.get(ctypes[i][1])
 
             if frame_type == 'Spatial':
-                frame_list.append(SpatialFrame(crpixs[i][1], None,
-                                               [cunits[i][1], cunits[i][1]]))
+                frame_list.append(SpatialFrame(reference_position=crpixs[i][1],
+                                               #axes_names=None,
+                                               units=[cunits[i][1],
+                                                      cunits[i][1]]))
             elif frame_type == 'Time':
-                frame_list.append(CoordinateFrame('Time', 1, None,
-                                                  [cunits[i][1]]))
+                frame_list.append(CoordinateFrame(system='Time',
+                                                  num_axes=1,
+                                                  axes_names=None,
+                                                  units=[cunits[i][1]]))
+            elif frame_type == 'Spectral':
+                frame_list.append(CoordinateFrame(system='Spectral',
+                                                  num_axes=1,
+                                                  axes_names=None,
+                                                  units=[cunits[i][1]]))
         return frame_list
 
     def get_coordinate_system(self, name="CompositeSystem"):
         return CoordinateSystem(self._make_frame_list(), name)
+
+
+class HeaderTypeError(Exception):
+    pass
+
+class MissingWCSKeywords(Exception):
+    pass
