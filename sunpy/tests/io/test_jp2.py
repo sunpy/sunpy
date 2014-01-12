a755adb4c852c2019f8a879df4ed3691f64603fd
wafels <jackireland@gmail.com>
2014-01-12 09:36:06 -0500

    first attempt at a JPEG2000 reading test suite

--- sunpy/tests/io/test_jp2.py
+++ sunpy/tests/io/test_jp2.py
@@ -12,7 +12,8 @@ from sunpy.io.header import FileHeader
 from sunpy.map import GenericMap
 from sunpy.map import Map
 
-# Tests to see if the main glymur library imports correctly
+# SunPy's JPEG2000 capabilities rely on the glymur library.  First we check to
+# make sure that glymur imports correctly before proceeding.
 try:
     __import__("glymur")
 except ImportError:
@@ -23,7 +24,7 @@ else:
 @pytest.mark.skipif("glymur_imports is False")
 def test_read_data():
     """Tests the reading of the JP2 data"""
-    data = glymur.Jp2k(filepath).read()
+    data = glymur.Jp2k(AIA_193_JP2).read()
     assert isinstance(data, np.ndarray)
 
 @pytest.mark.skipif("glymur_imports is False")
@@ -36,35 +37,5 @@ def test_read_header():
 def test_read_file():
     """Tests the reading of the complete JP2 file and its conversion into a
     SunPy map"""
-    map_ = Map(filepath)
+    map_ = Map(AIA_193_JP2)
     assert isinstance(map_, GenericMap)
-
-
-    @pytest.mark.online
-    @pytest.mark.skipif("__SKIP_TESTS__ is True")
-    def test_get_closest_image(self):
-        """Tests getClosestImage API method"""
-        # check basic query
-        im1 = self.client.get_closest_image('1994/01/01', 
-                                              observatory='SOHO', 
-                                              instrument='EIT', 
-                                              detector='EIT', 
-                                              measurement='195')
-        assert im1['width'] == im1['height'] == 1024
-        
-        # result should be same when using source id to query
-        source_id = self.sources['SOHO']['EIT']['EIT']['195']['sourceId']
-        
-        im2 = self.client.get_closest_image('1994/01/01', sourceId=source_id)
-        
-        assert im1 == im2
-    
-    @pytest.mark.online
-    @pytest.mark.skipif("__SKIP_TESTS__ is True")
-    def test_download_jp2(self):
-        """Tests getJP2Image API method"""
-        filepath = self.client.download_jp2('2020/01/01', observatory='SOHO', 
-                                            instrument='MDI', detector='MDI',
-                                            measurement='continuum')
-        map_ = sunpy.map.Map(filepath)
-        assert isinstance(map_, sunpy.map.GenericMap)
