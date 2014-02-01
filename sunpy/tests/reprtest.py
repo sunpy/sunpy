5344d27645a8a46ec93f02fc1ecb5d3a8e1072e8
VaticanCameos <chakrabortypritish@gmail.com>
2014-02-01 19:58:07 +0530

    Shifted Wave's repr test to test_vso.py, used assert statements

--- sunpy/tests/reprtest.py
+++
@@ -1,9 +0,0 @@
-from sunpy.net.vso.attrs import Wave
-import pytest
-
-def test_repr():
-    """Calls the __repr__ method of class vso.attrs.Wave"""
-    wav = Wave(12, 16)
-    print(wav)
-
-test_repr()
