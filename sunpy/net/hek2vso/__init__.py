3d6ef5294bdfef0c34da79f669a0b4c531223ca5
wafels <jackireland@gmail.com>
2014-02-13 23:01:08 -0500

    slight edit

--- sunpy/net/hek2vso/__init__.py
+++ sunpy/net/hek2vso/__init__.py
@@ -1,7 +1,7 @@
 """
 This module provides a translation layer between the HEK and the VSO.
-It allows you to acquire data records from the VSO corresponding to
-events found in the HEK.
+It allows you to acquire VSO data records that correspond to events
+found in the HEK.
 
 .. warning::
     This module is in beta and maybe unstable.
