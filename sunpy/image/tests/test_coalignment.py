6ffef0ec63fae0d3d93a08c37bbb165e7ac38a8e
wafels <jackireland@gmail.com>
2014-04-05 11:53:08 -0400

    tabs replaced with spaces in coalignment testing

--- sunpy/image/tests/test_coalignment.py
+++ sunpy/image/tests/test_coalignment.py
@@ -21,24 +21,24 @@ test_layer = testmap.data
 ny = test_layer.shape[0]
 nx = test_layer.shape[1]
 test_template = test_layer[1 + ny / 4 : 1 + 3 * ny / 4,
-						   2 + nx / 4 : 2 + 3 * nx / 4]
+                            2 + nx / 4 : 2 + 3 * nx / 4]
 
 # Used in testing the clipping
 clip_test_array = np.asarray([0.2, -0.3, -1.0001])
 
 
 def test_parabolic_turning_point():
-	assert(parabolic_turning_point(np.asarray([6.0, 2.0, 0.0])) == 1.5)
+    assert(parabolic_turning_point(np.asarray([6.0, 2.0, 0.0])) == 1.5)
 
 
 def test_repair_nonfinite():
-	a = np.ones((9))
-	for i in range(0, 9):
-		for non_number in [np.nan, np.inf]:
-			a[i] = non_number
-			b = a.reshape(3, 3)
-			c = repair_nonfinite(b)
-			assert(np.isfinite(c).all())
+    a = np.ones((9))
+    for i in range(0, 9):
+        for non_number in [np.nan, np.inf]:
+            a[i] = non_number
+            b = a.reshape(3, 3)
+            c = repair_nonfinite(b)
+            assert(np.isfinite(c).all())
 
 
 def test_match_template_to_layer():
@@ -65,50 +65,50 @@ def test_find_best_match_location():
     assert_allclose(x_test, 258.0, rtol=1e-3, atol=0)
 
 def test_lower_clip():
-	assert(_lower_clip(clip_test_array) == 2.0)
+    assert(_lower_clip(clip_test_array) == 2.0)
 
 
 def test_upper_clip():
-	assert(_upper_clip(clip_test_array) == 1.0)
+    assert(_upper_clip(clip_test_array) == 1.0)
 
 
 def test_calculate_clipping():
-	answer = calculate_clipping(clip_test_array, clip_test_array)
-	assert(answer == ([2.0, 1.0], [2.0, 1.0]))
+    answer = calculate_clipping(clip_test_array, clip_test_array)
+    assert(answer == ([2.0, 1.0], [2.0, 1.0]))
 
 
 def test_clip_edges():
-	a = np.zeros(shape=(341, 156))
-	yclip = [4, 0]
-	xclip = [1, 2]
-	new_a = clip_edges(a, yclip, xclip)
-	assert(a.shape[0] - (yclip[0] + yclip[1]) == 337)
-	assert(a.shape[1] - (xclip[0] + xclip[1]) == 153)
+    a = np.zeros(shape=(341, 156))
+    yclip = [4, 0]
+    xclip = [1, 2]
+    new_a = clip_edges(a, yclip, xclip)
+    assert(a.shape[0] - (yclip[0] + yclip[1]) == 337)
+    assert(a.shape[1] - (xclip[0] + xclip[1]) == 153)
 
 
 def test_calculate_shift():
-	result = calculate_shift(test_layer, test_template)
-	assert_allclose(result[0], 257.0,  rtol=1e-3, atol=0)
-	assert_allclose(result[1], 258.0,  rtol=1e-3, atol=0)
+    result = calculate_shift(test_layer, test_template)
+    assert_allclose(result[0], 257.0,  rtol=1e-3, atol=0)
+    assert_allclose(result[1], 258.0,  rtol=1e-3, atol=0)
 
 
 def test_default_fmap_function():
-	assert(default_fmap_function([1,2,3]).dtype == np.float64(1).dtype)
+    assert(default_fmap_function([1,2,3]).dtype == np.float64(1).dtype)
 
 
 def test_coalign():
-	# take the AIA image and shift it
-	# Pixel displacements have the y-displacement as the first entry
-	pixel_displacements = np.asarray([1.6, 10.1])
-	known_displacements = {'x':np.asarray([0.0, pixel_displacements[1] * testmap.scale['x']]), 'y':np.asarray([0.0, pixel_displacements[0] * testmap.scale['y']])}
-	# Apply the shift
-	d1 = shift(testmap.data, pixel_displacements)
-	m1 = map.Map((d1, testmap.meta))
-	# Create the mapcube
-	mc = map.Map([testmap, m1], cube=True)
-	# Do the coalignment
-	displacements = mc.coalign(displacements_only=True)
-	# Assert
-	assert_allclose(displacements['x'], known_displacements['x'], rtol=5e-2, atol=0)
-	assert_allclose(displacements['y'], known_displacements['y'], rtol=5e-2, atol=0 )
+    # take the AIA image and shift it
+    # Pixel displacements have the y-displacement as the first entry
+    pixel_displacements = np.asarray([1.6, 10.1])
+    known_displacements = {'x':np.asarray([0.0, pixel_displacements[1] * testmap.scale['x']]), 'y':np.asarray([0.0, pixel_displacements[0] * testmap.scale['y']])}
+    # Apply the shift
+    d1 = shift(testmap.data, pixel_displacements)
+    m1 = map.Map((d1, testmap.meta))
+    # Create the mapcube
+    mc = map.Map([testmap, m1], cube=True)
+    # Do the coalignment
+    displacements = mc.coalign(displacements_only=True)
+    # Assert
+    assert_allclose(displacements['x'], known_displacements['x'], rtol=5e-2, atol=0)
+    assert_allclose(displacements['y'], known_displacements['y'], rtol=5e-2, atol=0 )
 
