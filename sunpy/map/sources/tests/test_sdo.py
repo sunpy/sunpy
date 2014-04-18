ad9c47f191a539d4d5888645e4938259a9414f08
VaticanCameos <chakrabortypritish@gmail.com>
2014-04-19 00:06:31 +0530

    Tests for HMIMap. 1. test_observatory and test_measurement up and running. 2. Got the data argument for is_datasources_for from hmi.data. 3. Still working on header argument.

--- sunpy/map/sources/tests/test_sdo.py
+++ sunpy/map/sources/tests/test_sdo.py
@@ -3,16 +3,26 @@
 from sunpy.map.sources.sdo import HMIMap
 from sunpy.map.sources.sdo import AIAMap
 from sunpy.map import Map
+from sunpy.io import jp2 as jp
 from sunpy.net.helioviewer import HelioviewerClient
-import numpy as np
 import pytest
 
 @pytest.fixture
-def createHMIMap():
+def createHMI():
     hv = HelioviewerClient()
     filepath = hv.download_jp2('2012/07/05 00:30:00', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
-    hmi = Map(filepath)
-    return hmi
+    return filepath
 
-def test_is_datasource_for(createHMIMap):
-    assert (createHMIMap.is_datasource_for() == True)
+def test_is_datasource_for(createHMI):
+    hmi = Map(createHMI)
+    header = jp.get_header(createHMI)
+    assert (hmi.is_datasource_for(hmi.data, header) == True)
+
+def test_observatory(createHMI):
+    hmi = Map(createHMI)
+    assert(hmi.observatory == "SDO")
+
+def test_measurement(createHMI):
+    hmi = Map(createHMI)
+    assert(hmi.measurement == "continuum")
+    
