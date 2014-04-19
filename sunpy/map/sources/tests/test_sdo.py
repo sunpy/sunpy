7cf9274eb67758e90aceecc9737d2de4944ddb1b
VaticanCameos <chakrabortypritish@gmail.com>
2014-04-19 10:52:20 +0530

    Added some AIA tests. 1. Marked createHMI as online because of hv.download_jp2(). 2. Created AIA tests in a similar vein to HMI. 3. AIA object is available in sunpy module as AIA_171_IMAGE, so used it.

--- sunpy/map/sources/tests/test_sdo.py
+++ sunpy/map/sources/tests/test_sdo.py
@@ -4,14 +4,24 @@ from sunpy.map.sources.sdo import HMIMap
 from sunpy.map.sources.sdo import AIAMap
 from sunpy.map import Map
 from sunpy.net import HelioviewerClient
+from sunpy import AIA_171_IMAGE as aiaimg
 import pytest
 
+@pytest.mark.online
 @pytest.fixture
 def createHMI():
+    """Downloads a HMIMap jp2 object through the use of HelioviewerClient."""
     hv = HelioviewerClient()
     filepath = hv.download_jp2('2012/07/05 00:30:00', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
     return filepath
 
+@pytest.fixture
+def createAIAMap():
+    """Creates an AIAMap as given in documentation examples, through AIA_171_IMAGE."""
+    aia = Map(aiaimg)
+    return aia
+
+# HMI Tests
 def test_is_datasource_for(createHMI):
     hmi = Map(createHMI)
     header = dict(hmi.meta)
@@ -23,5 +33,15 @@ def test_observatory(createHMI):
 
 def test_measurement(createHMI):
     hmi = Map(createHMI)
-    assert(hmi.measurement == "continuum")
-    
+    assert (hmi.measurement == "continuum")
+
+# AIA Tests
+def test_is_datasource_for(createAIAMap):
+    header = dict(createAIAMap.meta)
+    assert (createAIAMap.is_datasource_for(createAIAMap.data, header) == True)
+
+def test_observatory(createAIAMap):
+    assert (createAIAMap.observatory == "SDO")
+
+def test_measurement(createAIAMap):
+    assert (createAIAMap.measurement == 171)
