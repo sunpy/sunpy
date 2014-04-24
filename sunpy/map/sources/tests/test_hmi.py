bccf8616d444617cfd42676c3c8bab80404fdcb2
VaticanCameos <chakrabortypritish@gmail.com>
2014-04-24 12:49:00 +0530

    Changed AIA and HMI tests. 1. Removed the use of createHMIMap fixture due to download process being expensive. 2. Tested the creation of a HMIMap using resampled_hmi.fits. 3. Tested the creation of a AIAMap using sunpy.AIA_171_IMAGE. 4. The other test methods remain the same mostly.

--- sunpy/map/sources/tests/test_hmi.py
+++ sunpy/map/sources/tests/test_hmi.py
@@ -1,12 +1,12 @@
-"""Test cases for SDO Map subclasses"""
-"""This particular test file pertains to HMIMap"""
+"""Test cases for SDO Map subclasses."""
+"""This particular test file pertains to HMIMap."""
 
 import pytest
 from sunpy.map.sources.sdo import HMIMap
-from sunpy.map.sources.sdo import AIAMap
 from sunpy.map import Map
-from sunpy.net import HelioviewerClient
+#from sunpy.net import HelioviewerClient
 
+# This fixture is no longer used on Stuart's instructions. Downloading is expensive.
 @pytest.mark.online
 @pytest.fixture
 def createHMI():
@@ -15,28 +15,26 @@ def createHMI():
     filepath = hv.download_jp2('2012/07/05 00:30:00', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
     return filepath
 
-@pytest.fixture
-def createAIAMap():
-    """Creates an AIAMap as given in documentation examples, through AIA_171_IMAGE."""
-    aia = Map(aiaimg)
-    return aia
-
 # HMI Tests
-def test_is_datasource_for(createHMI):
+def test_fitstoHMI():
+    """Tests the creation of HMIMap using FITS."""
+    assert (isinstance(Map('resampled_hmi.fits'), HMIMap) == True)
+    
+def test_is_datasource_for():
     """Test the is_datasource_for method of HMIMap.
     Note that header data to be provided as an argument
     can be a MapMeta object, which in this case is
     hmi.meta."""
-    hmi = Map(createHMI)
+    hmi = Map('resampled_hmi.fits')
     assert (hmi.is_datasource_for(hmi.data, hmi.meta) == True)
 
-def test_observatory(createHMI):
+def test_observatory():
     """Tests the observatory property of the HMIMap object."""
-    hmi = Map(createHMI)
+    hmi = Map('resampled_hmi.fits')
     assert(hmi.observatory == "SDO")
 
-def test_measurement(createHMI):
+def test_measurement():
     """Tests the measurement property of the HMIMap object."""
-    hmi = Map(createHMI)
+    hmi = Map('resampled_hmi.fits')
     assert (hmi.measurement == "continuum")
 
