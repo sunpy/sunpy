09b8c0b13b9dfb3ddff5a796c1a1b8c84f37689d
Dan Ryan <ryand5@tcd.ie>
2014-05-07 11:19:57 +0200

    Altered make_goes_chianti_temp_csv.py to add comments to the top of the csv file explaining what is contained in the csv files it creates as well as how the data were obtained. make_goes_chianti_temp.py was run again to create goes_chianti_temp_cor.csv and goes_chianti_temp_pho.csv with relevant comments on top.

--- sunpy/instr/make_goes_chianti_temp_csv.py
+++ sunpy/instr/make_goes_chianti_temp_csv.py
@@ -526,17 +526,24 @@ r_pho[14,:] = [2.65e-06,4.14e-06,6.32e-06,9.48e-06,1.40e-05,2.06e-05,2.98e-05,
 # Firstly csv file for coronal abundances using r_cor array.
 with open('goes_chianti_temp_cor.csv', 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=';')
-    # First create header containing column names and read into csv file
+    # Write comments at top of csv file
+    comment1 = ["# The data in this file represent the relationship " + \
+      "between the GOES flux ratio and the isothermal temperature of the " + \
+      "solar soft X-ray-emitting plasma assuming coronal abundances " + \
+      "as modelled by CHIANTI."]
+    comment2 = ["# The data in this file were obtained by copying and " + \
+      "pasting from the SSW routine goes_get_chianti_temp.pro on " + \
+      DATETAKEN + "."]
+    comment3 = ["#"]
+    csvwriter.writerow(comment1)
+    csvwriter.writerow(comment2)
+    csvwriter.writerow(comment3)
+    # Create header containing column names and read into csv file
     header = ['ratioGOES'+str(i) for i in range(1,NUMSATS+1)]
     header.insert(0, "log10temp")
-    header.append("date_taken_ssw")
     csvwriter.writerow(header)
     # Write in data row by row.
-    row = ["{:1.2e}".format(number) for number in r_cor[:,0]]
-    row.insert(0, log10temp[0])
-    row.append(DATETAKEN)
-    csvwriter.writerow(row)
-    for i in range(1, len(log10temp)):
+    for i in range(0, len(log10temp)):
         row = ["{:1.2e}".format(number) for number in r_cor[:,i]]
         row.insert(0, log10temp[i])
         csvwriter.writerow(row)
@@ -544,17 +551,24 @@ with open('goes_chianti_temp_cor.csv', 'w') as csvfile:
 # Next write csv file for photospheric abundances using r_pho array.
 with open('goes_chianti_temp_pho.csv', 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=';')
-    # First create header containing column names and read into csv file
+    # Write comments at top of csv file
+    comment1 = ["# The data in this file represent the relationship " + \
+      "between the GOES flux ratio and the isothermal temperature of the " + \
+      "solar soft X-ray-emitting plasma assuming photospheric abundances " + \
+      "as modelled by CHIANTI."]
+    comment2 = ["# The data in this file were obtained by copying and " + \
+      "pasting from the SSW routine goes_get_chianti_temp.pro on " + \
+      DATETAKEN + "."]
+    comment3 = ["#"]
+    csvwriter.writerow(comment1)
+    csvwriter.writerow(comment2)
+    csvwriter.writerow(comment3)
+    # Create header containing column names and read into csv file
     header = ['ratioGOES'+str(i) for i in range(1,NUMSATS+1)]
     header.insert(0, "log10temp_MK")
-    header.append("date_taken_ssw")
     csvwriter.writerow(header)
     # Write in data row by row.
-    row = ["{:1.2e}".format(number) for number in r_cor[:,0]]
-    row.insert(0, log10temp[0])
-    row.append(DATETAKEN)
-    csvwriter.writerow(row)
-    for i in range(1, len(log10temp)):
+    for i in range(0, len(log10temp)):
         row = ["{:1.2e}".format(number) for number in r_pho[:,i]]
         row.insert(0, log10temp[i])
         csvwriter.writerow(row)
