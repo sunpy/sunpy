4469744d7718c97857b09d74b4ddb5a33055f7d5
dpshelio <dps.helio@gmail.com>
2014-05-16 16:33:23 +0200

    Working scrapper version 1! It works, though it needs some possible refactoring

--- sunpy/util/scrapper.py
+++ sunpy/util/scrapper.py
@@ -1,35 +1,102 @@
 import datetime
+import urllib2
+from bs4 import BeautifulSoup
+import re
 
 class Scrapper:
     def __init__(self, pattern, **kwargs):
         self.pattern = pattern.format(**kwargs)
-        pass
+        self.now = datetime.datetime.now().strftime(self.pattern)
+
     def matches(self, filepath, date):
         return date.strftime(self.pattern) == filepath
+
     def range(self, starttime, endtime):
+        '''
+        Gets the directories for a certain range of time.
+        '''
         #find directory structure - without file names
         directorypattern = self.pattern[:-self.pattern[::-1].find('/')]
         #TODO what if there's not slashes?
         rangedelta = (endtime - starttime)
         timestep = self._smallerPattern(directorypattern)
-        # Number of elements in the time range (including end)
-        TotalTimeElements = int(rangedelta.total_seconds()/timestep.total_seconds()) + 1
-        directories = [(starttime + n * timestep).strftime(directorypattern)
-                       for n in range(TotalTimeElements)]
-        return directories
+        if timestep is None:
+            return [directorypattern]
+        else:
+            # Number of elements in the time range (including end)
+            TotalTimeElements = int(round(rangedelta.total_seconds()/timestep.total_seconds())) + 1
+            directories = [(starttime + n * timestep).strftime(directorypattern)
+                        for n in range(TotalTimeElements)] #todo if date <= endate
+            return directories
+
+    def URL_followsPattern(self, url):
+        time_conversions = {'%Y': '\d{4}', '%y': '\d{2}',
+                            '%b': '[A-Z]..', '%B': '\W', '%m': '\d{2}',
+                            '%d': '\d{2}', '%j': '\d{3}',
+                            '%H': '\d{2}', '%I': '\d{2}',
+                            '%M': '\d{2}',
+                            '%S': '\d{2}'}
+        pattern = self.pattern
+        for k,v in time_conversions.iteritems():
+            pattern = pattern.replace(k, v)
+        matches = re.match(pattern, url)
+        if matches:
+            return matches.end() == matches.endpos == len(self.now)
+        return False
+
+    def extractDateURL(self, url):
+        pattern_list = self.pattern.replace('.', '/').replace('_', '/').split('/')
+        url_list = url.replace('.', '/').replace('_', '/').split('/')
+
+        time_order = ['%Y', '%y', '%b', '%B', '%m', '%d', '%j', '%H', '%I', '%M', '%S']
+        final_date = []
+        final_pattern = []
+        #Find in directory and filename
+        for pattern_elem, url_elem in zip(pattern_list, url_list):
+            time_formats = [x for x in time_order if x in pattern_elem]
+            if len(time_formats) > 0:
+                final_date.append(url_elem)
+                final_pattern.append(pattern_elem)
+                for time_bit in time_formats:
+                    time_order.remove(time_bit)
+        return datetime.datetime.strptime(' '.join(final_date), ' '.join(final_pattern))
+
+    def filelist(self, starttime, endtime):
+        directories = self.range(starttime, endtime)
+        filesurls = []
+        for directory in directories:
+            opn = urllib2.urlopen(directory)
+            try:
+                soup = BeautifulSoup(opn)
+                for link in soup.find_all("a"):
+                    href = link.get("href")
+                    if href.endswith(self.pattern.split('.')[-1]):
+                        fullpath = directory + href
+                        if self.URL_followsPattern(fullpath):
+                            datehref = self.extractDateURL(fullpath)
+                            if (datehref >= starttime and
+                                datehref <= endtime):
+                                filesurls.append(fullpath)
+            finally:
+                opn.close()
+        return filesurls
+
     def _smallerPattern(self, directoryPattern):
         try: 
             if "%S" in directoryPattern:
                 return datetime.timedelta(seconds=1)
-            if "%M" in directoryPattern:
+            elif "%M" in directoryPattern:
                 return datetime.timedelta(minutes=1)
-            if any(hour in directoryPattern for hour in ["%H", "%I"]):
+            elif any(hour in directoryPattern for hour in ["%H", "%I"]):
                 return datetime.timedelta(hours=1)
-            if any(day in directoryPattern for day in ["%d", "%j"]):
+            elif any(day in directoryPattern for day in ["%d", "%j"]):
                 return datetime.timedelta(days=1)
-            if any(month in directoryPattern for month in ["%b","%B","%m"]):
+            elif any(month in directoryPattern for month in ["%b","%B","%m"]):
                 return datetime.timedelta(days=31)
-            if any(year in directoryPattern for year in ["%Y", "%y"]):
+            elif any(year in directoryPattern for year in ["%Y", "%y"]):
                 return datetime.timedelta(days=365)
+            else:
+                return None
         except:
-            raise 
+            raise
+
--- sunpy/util/test_scrapper.py
+++ sunpy/util/test_scrapper.py
@@ -1,52 +1,124 @@
 import pytest
 import datetime
-from .scrapper import Scrapper
+import os
 
-pattern_examples = [
+import sunpy.data.test
+from sunpy.util.scrapper import Scrapper
+
+PATTERN_EXAMPLES = [
     ('%b%y', datetime.timedelta(days=31)),
     ('%m%y', datetime.timedelta(days=31)),
+    ('%H%d', datetime.timedelta(hours=1)),
     ]
 
-class ScrapperTests:
-
+class TestScrapper:
     def testDirectoryDatePattern(self):
         s = Scrapper('%Y/%m/%d/%Y%m%d_%H%M%S_59.fit.gz')
         testpath = '2014/03/05/20140305_013000_59.fit.gz'
         d = datetime.datetime(2014,3,5,1,30)
-        self.failUnless(s.matches(testpath, d))
+        assert s.matches(testpath, d)
+        
     def testDirectoryDatePatternFalse(self):
         s = Scrapper('%Y/%m/%d/%Y%m%d_%H%M%S_59.fit.gz')
         testpath = '2013/03/05/20140305_013000_59.fit.gz'
         d = datetime.datetime(2014,3,5,1,30)
-        self.failIf(s.matches(testpath, d))
+        assert not s.matches(testpath, d)
+        
     def testDirectoryObsPattern(self):
         s = Scrapper('%y%m%d/{observatory}_%Y%m%d.fits', observatory = 'SDO')
         testpath = '140305/SDO_20140305.fits'
         d = datetime.datetime(2014,3,5)
-        self.failUnless(s.matches(testpath, d))
+        assert s.matches(testpath, d)
+        
     def testDirectoryRange(self):
         s = Scrapper('%Y/%m/%d/%Y%m%d_%H.fit.gz')
         directory_list = ['2009/12/30/', '2009/12/31/', '2010/01/01/', 
                           '2010/01/02/', '2010/01/03/']
         startdate = datetime.datetime(2009,12,30)
         enddate = datetime.datetime(2010,1,3)
-        self.failUnless(s.range(startdate, enddate) == directory_list)
+        assert s.range(startdate, enddate) == directory_list
+        
     def testDirectoryRangeFalse(self):
         s = Scrapper('%Y%m%d/%Y%m%d_%H.fit.gz')
         directory_list = ['20091230/', '20091231/', '20100101/', 
                           '20090102/', '20090103/']
         startdate = datetime.datetime(2009,12,30)
         enddate = datetime.datetime(2010,1,3)
-        self.failIf(s.range(startdate, enddate) == directory_list)
-    @pytest.mark.parametrize('pattern, mintime', pattern_examples)
-    def test_smallerPattern(self):
+        assert s.range(startdate, enddate) != directory_list
+
+    @pytest.mark.parametrize('pattern, mintime', PATTERN_EXAMPLES)
+    def test_smallerPattern(self, pattern, mintime):
         assert  mintime == Scrapper('')._smallerPattern(pattern)
-     
-# check when there's not directory structure (no /s)         
+
+    def testDirectoryRangeHours(self):
+        s = Scrapper('%Y%m%d_%H/%H%M.csv')
+        startdate = datetime.datetime(2009,12,31,23,40)
+        enddate = datetime.datetime(2010,01,01,01,15)
+        assert len(s.range(startdate, enddate)) == 3 #3 directories (1 per hour)
+
+    def testDirectoryRange_single(self):
+        s = Scrapper('%Y%m%d/%H_%M.csv')
+        startdate = datetime.datetime(2010,10,10,5,00)
+        enddate = datetime.datetime(2010,10,10,7,00)
+        assert len(s.range(startdate, enddate)) == 1
+        
+    def testDirectoryRange_Month(self):
+        s = Scrapper('%Y%m/%d/%j_%H.txt')
+        startdate = datetime.datetime(2008, 2,20,10)
+        enddate = datetime.datetime(2008, 3, 2, 5)
+        assert len(s.range(startdate, enddate)) == 12
+        startdate = datetime.datetime(2009, 2,20,10)
+        enddate = datetime.datetime(2009, 3, 2, 5)
+        assert len(s.range(startdate, enddate)) == 11
+
+    def testNoDirectory(self):
+        s = Scrapper('files/%Y%m%d_%H%M.dat')
+        startdate = datetime.datetime(2010, 1, 10, 20, 30)
+        enddate = datetime.datetime(2010, 1, 20, 20, 30)
+        assert len(s.range(startdate, enddate)) == 1
+
+    def testExtractDates_usingPattern(self):
+        s = Scrapper('data/%Y/%m/%d/fits/swap/swap_00174_fd_%Y%m%d_%H%M%S.fts.gz')
+        testURL = 'data/2014/05/14/fits/swap/swap_00174_fd_20140514_200135.fts.gz'
+        timeURL = datetime.datetime(2014, 5, 14, 20, 1, 35)
+        assert s.extractDateURL(testURL) == timeURL
+
+    def testURL_pattern(self):
+        s = Scrapper('fd_%Y%m%d_%H%M%S.fts')
+        assert s.URL_followsPattern('fd_20130410_231211.fts') == True
+        assert s.URL_followsPattern('fd_20130410_231211.fts.gz') == False
+        assert s.URL_followsPattern('fd_20130410_ar_231211.fts.gz') == False
+        
+    # def testFilesRange_sameDirectory_local(self):
+    #     s = Scrapper(os.path.join('file://',sunpy.data.test.rootdir,
+    #                               'EIT','efz%Y%m%d.%H%M%S_s.fits'))
+    #     startdate = datetime.datetime(2004, 3, 1, 4, 0)
+    #     enddate = datetime.datetime(2004, 3, 1, 6, 30)
+    #     assert len(s.filelist(startdate, enddate)) == 3
+    #     startdate = datetime.datetime(2010, 1, 10, 20, 30)
+    #     enddate = datetime.datetime(2010, 1, 20, 20, 30)
+    #     assert len(s.filelist(startdate, enddate)) == 0
+
+    def testFilesRange_sameDirectory_remote(self):
+        s = Scrapper('http://solarmonitor.org/data/%Y/%m/%d/fits/{instrument}/{instrument}_00174_fd_%Y%m%d_%H%M%S.fts.gz', instrument = 'swap')
+        startdate = datetime.datetime(2014, 5, 14, 0, 0)
+        enddate = datetime.datetime(2014, 5, 14, 6, 30)
+        assert len(s.filelist(startdate, enddate)) == 2
+        startdate = datetime.datetime(2014, 5, 14, 21, 0)
+        enddate = datetime.datetime(2014, 5, 14, 23, 30)
+        assert len(s.filelist(startdate, enddate)) == 0
+
+
+    
+
+               
+
+
+        #TODO: data gaps
                         
 
-def main():
-    unittest.main()
+#def main():
+#    pytest.main()
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
