b7f5f4df292def17302dabb925437eb05d50a0be
Drew Leonard <andy.j.leonard@gmail.com>
2014-05-20 17:00:33 +0100

    More test functions written. These will all need some significant tweaking, I expect

--- sunpy/instr/tests/test_aiaprep.py
+++ sunpy/instr/tests/test_aiaprep.py
@@ -8,11 +8,11 @@ import matplotlib.cm as cm
 
 # Define test image first so it's accessable to all functions.
 #original = sunpy.map.Map(sunpy.AIA_171_IMAGE)
-data = np.zeros((100, 100))
+data = np.zeros((101, 101))
 data[50:60, 50:80] = 1.0
 # An entirely made up test header.
-header = {'cdelt1': 0.599,
-          'cdelt2': 0.599,
+header = {'cdelt1': 0.6,
+          'cdelt2': 0.6,
           #crota1?
           #crota2: 
           'crpix1': data.shape[1]/2.0 + 0.5,
@@ -29,7 +29,7 @@ header = {'cdelt1': 0.599,
           'date-obs': '2014-05-20 14:35'}
 
 original = sunpy.map.Map(data, header)
-
+"""
 fig = plt.figure()
 original.plot(cmap=cm.binary)
 plt.show()
@@ -38,47 +38,166 @@ header = original.meta.keys()
 header.sort()
 for item in header:
     print item, original.meta[item]
+"""
+
+# Tolerance for tests - high for now just so they run. I'll make it much smaller
+# when actually testing
+atol = 1.0
+
+def plot_results(expect, result, diff):
+    fig = plt.figure()
+    
+    fig.add_subplot(1, 3, 1)
+    expect.plot(cmap=cm.binary)
+    plt.colorbar(orientation='horizontal')
+    
+    fig.add_subplot(1, 3, 2)
+    result.plot(cmap=cm.binary)
+    plt.colorbar(orientation='horizontal')
+    
+    fig.add_subplot(1, 3, 3)
+    diff.plot(cmap=cm.coolwarm)
+    plt.colorbar(orientation='horizontal')
 
 
 def test_aiaprep_rotation():
     # Check 360 degree rotation against original image
     original.meta['crota2'] = 360.0
     rot_map = aiaprep(original)
-    fig = plt.figure()
-    fig.add_subplot(1, 2, 1)
-    original.plot(cmap=cm.binary)
-    fig.add_subplot(1, 2, 2)
-    rot_map.plot(cmap=cm.binary)
-    assert np.allclose(original.data, rot_map.data)
+    diff_map = sunpy.map.GenericMap(original.data-rot_map.data, rot_map.meta)
+    plot_results(original, rot_map, diff_map)
+    assert np.allclose(original.data, rot_map.data, atol=atol)
+    plt.close()
+    
     # Check incremental 360 degree rotation against original image
+
     # Test 90 degree rotation against expected outcome
+    original.meta['crota2'] = 90.0
+    expected = sunpy.map.GenericMap(np.rot90(original.data.copy()), original.meta.copy())
+    rot_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-rot_map.data, rot_map.meta)
+    plot_results(expected, rot_map, diff_map)
+    assert np.allclose(expected.data, rot_map.data, atol=atol)
+    plt.close()
+
     # Test 90 degree rotation against -270 degree rotation
+    original.meta['crota2'] = -270.0
+    expected = sunpy.map.GenericMap(np.rot90(original.data.copy()), original.meta.copy())
+    rot_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-rot_map.data, rot_map.meta)
+    plot_results(expected, rot_map, diff_map)
+    assert np.allclose(expected.data, rot_map.data, atol=atol)
+    plt.close()
+
     # Test -90 degree rotation against 270 degree rotation
+    original.meta['crota2'] = 90.0
+    expected = sunpy.map.GenericMap(np.rot90(original.data.copy(), -3), original.meta.copy())
+    rot_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-rot_map.data, rot_map.meta)
+    plot_results(expected, rot_map, diff_map)
+    assert np.allclose(expected.data, rot_map.data, atol=atol)
+    plt.close()
+
     # Check rotated and derotated image against original
+    original.meta['crota2'] = 10.0
+    rot_map = aiaprep(original)
+    rot_map.meta['crota2'] = -10.0
+    rot_map = aiaprep(rot_map)
+    diff_map = sunpy.map.GenericMap(original.data-rot_map.data, rot_map.meta)
+    plot_results(original, rot_map, diff_map)
+    assert np.allclose(original.data, rot_map.data, atol=atol)
+    plt.close()
 
 
 def test_aiaprep_shift():
     # Check a shifted shape against expected outcome
+    original.meta['crota2'] = 0.0
+    expect_data = np.zeros((101, 101))
+    expect_data[45:55, 35:65] = 1.0 # Not entirely convinced by these numbers
+    expected = sunpy.map.GenericMap(expect_data, original.meta.copy())
+    original.meta['crpix1'], original.meta['crpix2'] = 65, 55
+    shift_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-shift_map.data, shift_map.meta)
+    plot_results(expected, shift_map, diff_map)
+    assert np.allclose(expected.data, shift_map.data, atol=atol)
+    plt.close()
+
     # Check shifted and unshifted shape against original image
-    pass
+    shift_map = aiaprep(original)
+    shift_map.meta['crpix1'], shift_map.meta['crpix2'] = data.shape[1]/2.0 + 0.5, data.shape[0]/2.0 + 0.5
+    shift_map = aiaprep(shift_map)
+    diff_map = sunpy.map.GenericMap(original.data-shift_map.data, shift_map.meta)
+    plot_results(original, shift_map, diff_map)
+    assert np.allclose(original.data, shift_map.data, atol=atol)
+    plt.close()
 
 
-def test_aiaprep_rot_and_shift():
-    # Check a shifted and rotated shape against expected outcome
-    # Check a shifted, rotated, then unshifted and derotated shape against original image
-    pass
+def test_aiaprep_scale():
+    # Check a scaled image against the expected outcome
+    original.meta['crota2'] = 0.0
+    expect_data = np.zeros((101, 101))
+    #expect_data[??, ??] = 1.0 # Not entirely sure what the expected outcome should be
+    expected = sunpy.map.GenericMap(expect_data, original.meta.copy())
+    original.meta['cdelt1'], original.meta['cdelt2'] = 0.3, 0.3
+    scale_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-scale_map.data, scale_map.meta)
+    plot_results(expected, scale_map, diff_map)
+    assert np.allclose(expected.data, scale_map.data, atol=atol)
+    plt.close()
+    
+    # Check a scaled and descaled image against the original
+    scale_map = aiaprep(original)
+    scale_map.meta['cdelt1'], scale_map.meta['cdelt2'] = 1.2, 1.2 # I think this should be the same as changing the target scale in aiaprep()
+    scale_map = aiaprep(scale_map)
+    diff_map = sunpy.map.GenericMap(original.data-scale_map.data, scale_map.meta)
+    plot_results(original, scale_map, diff_map)
+    assert np.allclose(original.data, scale_map.data, atol=atol)
+    plt.close()
 
 
-def test_aiaprep_headerinfo():
+def test_aiaprep_all():
+    # Check a shifted, rotated and scaled shape against expected outcome
+    original.meta['crota2'] = 90.0
+    expect_data = np.zeros((101, 101))
+    #expect_data[??, ??] = 1.0 # Not entirely sure what the expected outcome should be
+    expected = sunpy.map.GenericMap(expect_data, original.meta.copy())
+    original.meta['crpix1'], original.meta['crpix2'] = 65, 55 # Not entirely sure about this either
+    original.meta['cdelt1'], original.meta['cdelt2'] = 0.3, 0.3
+    prep_map = aiaprep(original)
+    diff_map = sunpy.map.GenericMap(expected.data-prep_map.data, prep_map.meta)
+    plot_results(expected, prep_map, diff_map)
+    assert np.allclose(expected.data, prep_map.data, atol=atol)
+    plt.close()
+
+    # Check a prepped and de-prepped shape against original image
+    original.meta['crota2'] = 10.0
+    prep_map = aiaprep(original)
+    prep_map.meta['crota2'] = -10.0
+    prep_map.meta['crpix1'], original.meta['crpix2'] = data.shape[1]/2.0 + 0.5, data.shape[0]/2.0 + 0.5 # Not entirely sure about this either
+    prep_map.meta['cdelt1'], original.meta['cdelt2'] = 1.2, 1.2
+    prep_map = aiaprep(prep_map)
+    diff_map = sunpy.map.GenericMap(original.data-prep_map.data, prep_map.meta)
+    plot_results(original, prep_map, diff_map)
+    assert np.allclose(expected.data, prep_map.data, atol=atol)
+    plt.close()
+
     # Test that header info for the map has been correctly updated
-    pass
+    # Check all of these for Map attributes and .meta values?
+    # Also may be worth checking they stay the same when saved, I'm sure I've had issues with that before.
+    # Check crpix1 value
+    # Check crpix2 value
+    # Check cdelt1 value
+    # Check cdelt2 value
+    # Check crota1 value
+    # Check crota2 value
+    # Check level number
 
-"""In addition to all of the above, I need to test the scaling as well.
-Remembering that it hasn't neccessarily worked if the header says it has.
-"""
 
 try:
     test_aiaprep_rotation()
+    test_aiaprep_shift()
+    test_aiaprep_scale()
+    test_aiaprep_all()
 except AssertionError:
     plt.show()
     raise
