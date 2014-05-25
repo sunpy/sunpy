fd944fcf68e79fb3030938aa998ccd598b7b0eec
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-05-26 00:52:02 +0300

    add wait method to download, results object integrated

--- sunpy/net/lcsources/eve.py
+++ sunpy/net/lcsources/eve.py
@@ -3,32 +3,28 @@ from sunpy.net.vso.attrs import Time
 from sunpy.net.download  import Downloader
 from sunpy.time import TimeRange
 import datetime
-
-
+from sunpy.net.vso.vso import Results
+from sunpy.util import print_table
 
 class queryrequestblock(object):
 
-    def __init(self,url):
+    def __init__(self,url):
 	self.source = 'SDO'
 	self.provider = 'LASP'
 	self.phyobs = 'Irradiance'
 	self.instrument = 'EVE'
 	self.url = url
 	self.time = {}
-	self.time['start'] = datetime(url.split('/')[-1].split('_')[0]).strpftime('%Y%m%d')
-	self.time['end '] = self.time['start'] + datetime.timedelta(days=1)
+	self.time['start'] = datetime.datetime.strptime(url.split('/')[-1].split('_')[0],'%Y%m%d')
+	self.time['end'] = self.time['start'] + datetime.timedelta(days=1)
 
 
 def iter_urls(url_list):
 
     for aurl in url_list:
-          tmp = queryrequestblock(url)
+          tmp = queryrequestblock(aurl)
+	  print tmp
 	  yield tmp
-
-
-
-
-
 
 
 class queryresponse(list):
@@ -40,11 +36,12 @@ class queryresponse(list):
     @classmethod
     def create(cls,lst):
     	
+        i= iter_urls(lst)
 	return cls(iter_urls(lst))
     
     def num_records(self):
 	
-	return self
+	return len(self)
 
     def time_range(self):
     	
@@ -53,22 +50,19 @@ class queryresponse(list):
 		datetime.strftime(
 		max(qrblock.time['end'] for qrblock in self),'%Y/%m/%d'))
     
-    def show():
+    def show(self):
     
         table = [
-	          qrblock.time['start'].strftime('%Y/%m/%d'),
+	         [ 
+		  qrblock.time['start'].strftime('%Y/%m/%d'),
 		  qrblock.time['end'].strftime('%Y/%m/%d'),
 		  qrblock.source,
-		  qrblock.instrument,
+		  qrblock.instrument
+		 ] 
+		  for qrblock in self
 		]
-	table.insert(0,[])
 	table.insert(0,['Start time','End time','Source','Instrument'])
-	print print_table(table, colsep='  ', linesep-'\n')
-
-
-
-
-#TODO: add wait() method which blocks the download
+	print print_table(table, colsep='  ', linesep='\n')
 
 
 class EVEDownloader(object):
@@ -82,10 +76,8 @@ class EVEDownloader(object):
     def _get_url_for_timerange(cls,timerange,**kwargs):
          days = timerange.get_days()
 	 urls = []
-	 print days
 	 for day in days:
 	     urls.append(cls._get_url_for_date(day,**kwargs))
-	 print urls
 	 return urls
 
     @classmethod
@@ -96,8 +88,7 @@ class EVEDownloader(object):
         base_url = 'http://lasp.colorado.edu/eve/data_access/evewebdata/quicklook/L0CS/SpWx/'
         return urlparse.urljoin(base_url, date.strftime('%Y/%Y%m%d') + '_EVE_L0CS_DIODES_1m.txt')
     
-    @staticmethod
-    def makeargs(*args,**kwargs):
+    def makeargs(self,*args,**kwargs):
        '''map_:Dict'''
        for elem in args:
            if issubclass(elem.__class__,Time):
@@ -110,30 +101,38 @@ class EVEDownloader(object):
      
     def query(self,*args,**kwargs):
          
-	 EVEDownloader.makeargs(*args,**kwargs)
+	 self.makeargs(*args,**kwargs)
 	 urls = EVEDownloader._get_url_for_timerange(self.map_.get('TimeRange'),**kwargs)
          return queryresponse.create(urls)
 
-
+    
     def get(self,qres):
          
 	 urls=[]
 	 for qrblock in qres:
 	     urls.append(qrblock.url)
 
+         res = Results(lambda x: None,0,lambda map_:self.link(map_))
+
 	 dobj = Downloader(max_conn=len(urls),max_total=len(urls))
-	 for url in urls:
-	     dobj.download(url,self.map_.get('Path',None),self.map_.get('CallBack',None),self.map_.get('ErrorBack',None))
+	 for aurl,ncall in list(zip(urls,map(lambda x:res.require([x]),urls))):
+	     dobj.download(aurl,self.map_.get('Path',None),ncall,self.map_.get('ErrorBack',None))
          
+	 return res
          #add to database
 	 #eve_add_to_database(conn_url)
 
-     
+    def link(self,map_):
+
+    	paths = []
+	for k,v in map_.iteritems():
+	    paths.append(map_[k]['path'])
+	
+	return paths
     
    
     def download_legacy(self,timerange,path=None,callback=None,errback=None):
 	
-	print "hello"
         urls = EVEDownloader._get_url_for_timerange(timerange)
 	print urls
 	print len(urls)
