6e0bbaa0e6dd2ca2538c7d1de0a13fa1747fbd04
kaichogami <asishrocks95@gmail.com>
2014-05-28 18:40:20 +0530

    units are getting the expected output

--- sunpy/util/unit_conversion.py
+++ sunpy/util/unit_conversion.py
@@ -8,7 +8,7 @@ __all__ = ['degrees_to_hours', 'degrees_to_arc', 'kelvin_to_keV',
            'keV_to_kelvin', 'to_angstrom']
 
 boltz_unit = units.J / units.K
-           
+	          
 def degrees_to_hours(angle):
     """Converts an angle from the degree notation to the hour, arcmin, arcsec 
     notation (returned as a tuple)."""
@@ -20,7 +20,7 @@ def degrees_to_hours(angle):
     remainder =  remainder * 60 - arcminute
     arcsecond = remainder * 60.0
     return [hour * units.hourangle, arcminute * units.arcmin, arcsecond * units.arcsec]
-
+	
 def degrees_to_arc(angle):
     """Converts decimal degrees to degree, arcminute, 
     arcsecond (returned as a tuple)."""
@@ -32,22 +32,22 @@ def degrees_to_arc(angle):
     remainder =  remainder * 60 - arcminute
     arcsecond = remainder * 60.0
     return [degree * units.degree, arcminute * units.arcmin, arcsecond * units.arcsec]
-
+	
 def to_angstrom(value, unit):
     """Given a value with a unit (given in a string), convert to angstroms"""
     value_quantity = value * units.Unit(unit)
     return value_quantity.to(units.angstrom, equivalencies=units.spectral()).value
-
-def kelvin_to_keV(temperature):  #units are not converted properly
+	
+def kelvin_to_keV(temperature):
     """Convert from temperature expressed in Kelvin to a 
-    temperature expressed in keV"""
-	if not isinstance(temperature, units.Quantity):
-	    raise ValueError("temperature should be kelvin Quantity")
-    return temperature / ((constants.e * units.J) / (constants.k * boltz_unit) * 1000.0) 
-
-def keV_to_kelvin(temperature):  #units are not converted properly
+    temperature expressed in keV."""
+    if not isinstance(temperature, units.Quantity):
+		raise ValueError("temperature should be kelvin Quantity")
+    return temperature / ((constants.e * units.J / units.eV) / (constants.k * boltz_unit) * 1000.0) 
+	
+def keV_to_kelvin(temperature):
     """Convert from temperature expressed in keV to a temperature 
-    expressed in Kelvin"""
-	if not isinstance(temperature, units.Quantity):
-	    raise ValueError("temperature should be eV Quantity")
-    return temperature * ((constants.e * units.J) / (constants.k * units.J / units.K) * 1000.0) 
+    expressed in Kelvin."""
+    if not isinstance(temperature, units.Quantity):
+		raise ValueError("temperature should be keV Quantity")
+    return temperature * ((constants.e * units.J / units.eV) / (constants.k * boltz_unit) * 1000.0) 
