157443507b2f7e305d7b29ec55801d0d885b9d0a
Drew Leonard <andy.j.leonard@gmail.com>
2014-05-29 16:12:20 +0100

    Print statements removed and test added for checking the header values after saving and reloading the Map.

--- sunpy/instr/tests/test_aia.py
+++ sunpy/instr/tests/test_aia.py
@@ -2,38 +2,20 @@ from __future__ import absolute_import
 
 import sunpy
 from sunpy.instr.aia import aiaprep
-from sunpy.image.tests.test_rotate import alltests as test_rotation
+import sunpy.image.tests.test_rotate as testrot
 import matplotlib.pyplot as plt
 
-"""# Define test image first so it's accessable to all functions.
-data = images.camera()
-# An entirely made up test header.
-header = {'cdelt1': 0.6,
-          'cdelt2': 0.6,
-          'crpix1': data.shape[1]/2.0 - 0.5,
-          'crpix2': data.shape[0]/2.0 - 0.5,
-          'cunit1': 'arcsec',
-          'cunit2': 'arcsec',
-          'lvl_num': 1.0,
-          'naxis': 2,
-          'naxis1': data.shape[1],
-          'naxis2': data.shape[0],
-          'date-obs': sunpy.time.parse_time(dt.datetime.now())}"""
-#original = sunpy.map.Map(data, header)
+# Define the original and prepped images first so they're available to all functions
 original = sunpy.map.Map('aiaprep_test_image.fits')
+prep_map = aiaprep(original)
 
 
 def test_aiaprep():
     # Test that the affine transformation is working first
-    test_rotation()
+    testrot.alltests()
     
-    print 'Prepping AIA test image'
-    prep_map = aiaprep(original)
-
     # Test that header info for the map has been correctly updated
     # Check all of these for Map attributes and .meta values?
-    # Also may be worth checking they stay the same when saved, I'm sure I've had issues with that before.
-    print 'Testing header values ...',
     # Check crpix values
     assert prep_map.meta['crpix1'] == prep_map.shape[1]/2.0 - 0.5
     assert prep_map.meta['crpix2'] == prep_map.shape[1]/2.0 - 0.5
@@ -45,14 +27,29 @@ def test_aiaprep():
     assert prep_map.meta['crota2'] == 0.0
     # Check level number
     assert prep_map.meta['lvl_num'] == 1.5
-    print 'Passed'
 
+
+def test_filesave():
+    # Test that adjusted header values are still correct after saving the map
+    # and reloading it.
+    prep_map.save('prepped_map_save_test.fits',clobber=True)
+    load_map = sunpy.map.Map('prepped_map_save_test.fits')
+    # Check crpix values
+    assert load_map.meta['crpix1'] == prep_map.shape[1]/2.0 - 0.5
+    assert load_map.meta['crpix2'] == prep_map.shape[1]/2.0 - 0.5
+    # Check cdelt values
+    assert load_map.meta['cdelt1'] == 0.6
+    assert load_map.meta['cdelt2'] == 0.6
+    # Check crota values
+    assert load_map.meta['crota1'] == 0.0
+    assert load_map.meta['crota2'] == 0.0
+    # Check level number
+    assert load_map.meta['lvl_num'] == 1.5
 
 if __name__ == "__main__":
     try:
         test_aiaprep()
+        test_filesave()
     except AssertionError:
-        print 'Failed'
+        plt.show()
         raise
-    else:
-        print 'All tests completed successfully'
