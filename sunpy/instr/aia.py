b637feda9ceddf85d3c8371d87afb7dc745926c4
Drew Leonard <andy.j.leonard@gmail.com>
2014-06-03 14:17:12 +0100

    Adjustments to Map.rotate() based on @Cadair's suggestions

--- sunpy/instr/aia.py
+++ sunpy/instr/aia.py
@@ -2,8 +2,6 @@
 """
 Provides processing routines for data captured with the AIA instrument on SDO.
 """
-import numpy as np
-from sunpy.image.rotate import affine_transform
 from sunpy.map.sources.sdo import AIAMap
 from copy import deepcopy
 
@@ -24,28 +22,14 @@ def aiaprep(aiamap):
 
     scale_ref = 0.6
     scale_factor = aiamap.scale['x'] / scale_ref
-    angle = np.radians(-aiamap.rotation_angle['y'])
-
-    c = np.cos(angle)
-    s = np.sin(angle)
-    rmatrix = np.array([[c, -s], [s, c]])
-
-    map_center = (aiamap.shape[1] - aiamap.reference_pixel['x'],
-                  aiamap.shape[0] - aiamap.reference_pixel['y'])
 
     newmap = deepcopy(aiamap)
-    data = newmap.data
-    newmap.data = affine_transform(data, rmatrix=rmatrix, recenter=True,
-                                   scale=scale_factor, rotation_center=map_center,
-                                   missing=aiamap.min())
+    newmap = aiamap.rotate(recenter=True, scale=scale_factor,
+                           missing=aiamap.min())
 
     # Update header values as needed
-    newmap.meta['crpix1'] = newmap.shape[1]/2.0 - 0.5
-    newmap.meta['crpix2'] = newmap.shape[0]/2.0 - 0.5
     newmap.meta['cdelt1'] = scale_ref
     newmap.meta['cdelt2'] = scale_ref
-    newmap.meta['crota1'] = 0.0
-    newmap.meta['crota2'] = 0.0
     newmap.meta['lvl_num'] = 1.5
 
     return newmap
