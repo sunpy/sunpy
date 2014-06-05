2000956b83061d66ea241b37c4f6556c63200d1b
Drew Leonard <andy.j.leonard@gmail.com>
2014-06-05 15:36:15 +0100

    aiaprep() test file imported properly from test dir

--- sunpy/data/test/__init__.py
+++ sunpy/data/test/__init__.py
@@ -19,3 +19,8 @@ EVE_AVERAGES_CSV = os.path.join(rootdir, "EVE_He_II_304_averages.csv")
 #
 AIA_193_JP2 = os.path.join(rootdir,
                            "2013_06_24__17_31_30_84__SDO_AIA_AIA_193.jp2")
+
+#
+# aiaprep() test Map
+#
+aiaprep_testmap = os.path.join(rootdir, "aiaprep_test_image.fits")
