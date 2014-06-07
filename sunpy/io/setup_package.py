31bb2c7ec7551b2c3e5b62f5b240ad2ac457e4c9
Stuart Mumford <stuart@mumford.me.uk>
2014-06-07 19:06:45 +0100

    Changes to .travis.yml to try and make travis work

--- sunpy/io/setup_package.py
+++ sunpy/io/setup_package.py
@@ -11,9 +11,6 @@ def get_extensions():
     cfg = setup_helpers.DistutilsExtensionArgs()
     cfg['include_dirs'].append('numpy')
     cfg['sources'].extend(glob(os.path.join(os.path.dirname(__file__), 'src', 'ana', '*.c')))
-#    cfg['sources'].extend([os.path.join(os.path.dirname(__file__), 'src', 'rot_extn.c'),
-#                           os.path.join(os.path.dirname(__file__), 'src', 'transform', 'aff_tr.c')])
-    print cfg['sources']
     cfg['extra_compile_args'].extend(['-std=c99', '-O3'])
 
     e = Extension('sunpy.io._pyana', **cfg)
