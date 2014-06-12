90bf97d3355a9b3489015e29a67b5e43774c3b8a
VaticanCameos <chakrabortypritish@gmail.com>
2014-06-12 16:09:03 +0530

    Added some tests for the Frame API. Used HelioGraphic frame as a guinea pig, not yet specified how Stonyhurst and Carrington come into the picture. Will incorporate HelioCentric later as a second frame, similar to how Astropy's tests use ICRS and FK5.

--- sunpy/coordinates/tests/test_api.py
+++ sunpy/coordinates/tests/test_api.py
@@ -5,3 +5,53 @@ added.
 @Author: Pritish C.(VaticanCameos)
 """
 
+import numpy as np
+from numpy import testing as npt
+#import six
+
+from astropy.coordinates import units as u
+
+def test_frame_api():
+    from astropy.coordinates import SphericalRepresentation, \
+         CylindricalRepresentation, CartesianRepresentation
+    from ..sunpy_frames import HelioGraphic, HelioCentric, \
+         HelioProjective
+    # This method will test the use of the Coordinates API
+    # in a way similar to that of Astropy's testing methods.
+
+    hgic = HelioGraphic(SphericalRepresentation(lon=3*u.hour, lat=10*u.deg))
+
+    # Check the data attribute.
+    assert hgic.data.lon == 3*u.hourangle
+    assert hgic.data.lat == 10*u.deg
+
+    # Representation objects give us the actual positional information.
+    npt.assert_allclose(hgic.represent_as(SphericalRepresentation).lat.to(u.deg), 10*u.deg)
+    npt.assert_allclose(hgic.spherical.lat.to(u.deg), 10*u.deg)
+    assert hgic.represent_as(CartesianRepresentation).z.value > 0
+
+    # Now assert the same with hgic's attributes and not the representation's.
+    npt.assert_allclose(hgic.lat.to(u.deg), 10*u.deg)
+    npt.assert_allclose(hgic.lon.to(u.deg), 3*u.deg)
+
+    assert hgic.representation is SphericalRepresentation
+
+    # Now we try a low-level initialization of hgic.
+    hgic_2 = HelioGraphic(lon=10*u.hour, lat=8*u.deg, distance=1*u.kpc)
+    npt.assert_allclose(hgic.lon.to(u.deg), hgic_2.lon.to(u.deg))
+
+    # Try calculating the separation and testing against it.
+    # This is the on-sky separation.
+    coord1 = HelioGraphic(lon=0*u.hour, lat=0*u.deg)
+    coord2 = HelioGraphic(lon=0*u.hour, lat=1*u.deg)
+    assert coord1.separation(coord2).degree == 1.0
+
+    # Now try the same with the 3D separation.
+    coord3 = HelioGraphic(lon=0*u.hour, lat=0*u.deg, distance=1*u.kpc)
+    coord4 = HelioGraphic(lon=0*u.hour, lat=0*u.deg, distance=2*u.kpc)
+    assert coord3.separation_3d(coord4).kpc == 1.0
+
+    # Fail the following example because coord1 and 2 don't have
+    # a distance attribute.
+    with pytest.raises(ValueError):
+        assert coord1.separation_3d(coord2).kpc == 1.0
