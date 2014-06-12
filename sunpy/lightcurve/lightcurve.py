b4d58aa025c0e5ef1b3d91eb046755b468314b5a
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-06-12 18:30:57 +0100

    Fixed (?) Weird docstring error

--- sunpy/lightcurve/lightcurve.py
+++ sunpy/lightcurve/lightcurve.py
@@ -93,7 +93,7 @@ for compatability with map, please use meta instead""", Warning)
     @classmethod
     def from_time(cls, time, **kwargs):
         '''Called by Conditional Dispatch object when valid time is passed as input to create method.'''
-	date = parse_time(time)
+    date = parse_time(time)
         url = cls._get_url_for_date(date, **kwargs)
         filepath = cls._download(
             url, kwargs, err="Unable to download data for specified date"
@@ -128,10 +128,10 @@ for compatability with map, please use meta instead""", Warning)
     def from_file(cls, filename):
         '''Used to return Light Curve object by reading the given filename
 
-	Parameters:
-	    filename: Path of the file to be read.
+    Parameters:
+        filename: Path of the file to be read.
 
-	'''
+    '''
 
         filename = os.path.expanduser(filename)
         meta, data = cls._parse_filepath(filename)
@@ -143,26 +143,14 @@ for compatability with map, please use meta instead""", Warning)
     @classmethod
     def from_url(cls, url, **kwargs):
         '''
-	Downloads a file from the given url, reads and returns a Light Curve object.
+    Downloads a file from the given url, reads and returns a Light Curve object.
 
-	Parameters:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	    url : string
-=======
-	    url : string 
->>>>>>> upstream/master
-=======
-	    url : string
->>>>>>> 817e49b00eb6dc3e54df4ea1f7bb058e6c9b378b
-=======
-	    url : string 
->>>>>>> master
-	        Uniform Resource Locator pointing to the file.
+    Parameters:
+         url : String
+            Uniform Resource Locator pointing to the file.
 
-	    kwargs :Dict
-	        Dict object containing other related parameters to assist in download.
+       kwargs: Dict
+            Dict object containing other related parameters to assist in download.
 
         '''
         try:
@@ -175,11 +163,11 @@ for compatability with map, please use meta instead""", Warning)
     @classmethod
     def from_data(cls, data, index=None, meta=None):
         '''
-	Called by Conditional Dispatch object to create Light Curve object when corresponding data is passed
-	to create method.
-	'''
+    Called by Conditional Dispatch object to create Light Curve object when corresponding data is passed
+    to create method.
+    '''
 
-	return cls(
+    return cls(
             pandas.DataFrame(data, index=index),
             meta
         )
@@ -191,11 +179,11 @@ for compatability with map, please use meta instead""", Warning)
     @classmethod
     def from_dataframe(cls, dataframe, meta=None):
         '''
-	Called by Conditional Dispatch object to create Light Curve object when Pandas DataFrame is passed
-	to create method.
-	'''
+    Called by Conditional Dispatch object to create Light Curve object when Pandas DataFrame is passed
+    to create method.
+    '''
 
-	return cls(dataframe, meta)
+    return cls(dataframe, meta)
 
     def plot(self, axes=None, **plot_args):
         """Plot a plot of the light curve
