544583a7ba2b8e46ee7b8a60c1b3c8708010ea69
Dan Ryan <ryand5@tcd.ie>
2014-06-13 12:23:18 +0200

    Written first draft od find_lyra_event() to locate flares in LYRA data. Does not work yet though.

--- sunpy/instr/lyra_flare_detection/lyra_event_list.py
+++ sunpy/instr/lyra_flare_detection/lyra_event_list.py
@@ -27,7 +27,6 @@
 #-----Look for flares during decay phase------
 # 16. If there are possible start times between peak and end time,
 #     repeat steps 13-16.
-
 from __future__ import absolute_import
 from __future__ import division
 
@@ -37,6 +36,9 @@ from sunpy.time import parse_time
 
 import sqlite3
 from itertools import chain
+
+RISE_FACTOR = 1.4
+FALL_FACTOR = 0.5
 
 def extract_combined_lytaf(tstart, tend,
                            lytaf_path=os.path.join(os.path.curdir, "data"),
@@ -235,6 +237,9 @@ def find_lyra_events(flux, time):
     --------                
     
     """
+    # Define variables to be used later
+    flare_indices = []
+    flare_times = []
     # Get LYTAF file for given time range
     lytaf = extract_combined_lytaf(time[0], time[-1])
     # Find events in lytaf which are to be removed from time series.
@@ -242,7 +247,56 @@ def find_lyra_events(flux, time):
                               lytaf["event_type"] == u'Offpoint',
                               lytaf["event_type"] == u'LAR',
                               lytaf["event_type"] == u'Calibration')
-    # Set periods in flux during artifacts to -1
+    # Remove periods corresponding to artifacts from flux and time arrays
     for artifact in np.arange(len(artifacts))[artifacts]:
-        flux[np.logical_and(flux > lytaf["begin_time"][index],
-                            flux < lytaf["end_time"][index])] = -1
+        bad_period = np.logical_and(flux > lytaf["begin_time"][index],
+                                    flux < lytaf["end_time"][index])
+        flux = np.delete(flux, bad_period)
+        time = np.delete(time, bad_period)
+    # get derivative of flux wrt time
+    dt = time[0:-2] - time[1:-1]
+    dfdt = np.gradient(flux[0:-2], dt)
+    dfdt = np.append(dfdt, 0)
+    # Get locations where derivative is positive
+    pos_deriv = np.where(dfdt > 0)[0]
+    neg_deriv = np.where(dfdt < 0)[0]
+    # Find difference between each time point and the one 4
+    # observations ahead.
+    dt4 = time[0:-5] - time[4:-1]
+    # Find all possible flare start times.
+    for i in np.arange(len(pos_deriv)):
+        # Start time criteria
+        if pos_deriv[i:i+4]-pos_deriv[i] == np.arange(5) and dt4[i] == 4 and
+            flux[pos_deriv[i+4]] / flux[pos_deriv[i]] >= RISE_FACTOR:
+            # Next, find index of flare end time.
+            jj = np.where(neg_deriv > pos_deriv[i])[0]
+            j = neg_deriv[jj[0]]
+            end_index = np.where(flux[j:] <=
+                                 max(flux[i:j]) - (max(flux[i:j])-flux[i]) *
+                                 FALL_FACTOR)[0][0] + j
+            # find index of peak time
+            peak_index = np.where(flux == max(flux[pos_deriv[i]:j]))[0][0]
+            # Record flare start, peak and end times
+            flare_indices.append(pos_deriv[i]], peak_index, end_index)
+            flare_times.append(time[pos_deriv[i]],
+                               time[peak_index], time[end_index])
+            # If the most recently found flare is during the decay phase
+            # of another reset end time of previous flare to start time
+            # of this flare.
+            if flare_indices[-2][2] > flare_indices[-1][0]:
+                new_peak_index = np.where(flux == max(
+                    flux[flare_indices[-2][0]:flare_indices[-1][0]]))[0][0]
+                flare_indices[-2] = (flare_indices[-2][0],
+                                     new_peak_index, flare_indices[-1][0])
+                flare_times[-2] = (time[flare_indices[-2][0]],
+                                   time[new_peak_index],
+                                   time[flare_indices[-1][0]])
+            # Finally, set principle iterator, i, to the peak of the
+            # flare just found so that algorithm will start looking for
+            # flares during the decay phase of this flare and beyond,
+            # thereby skipping the rise phase of this flare.  This
+            # ensures that flares during the decay phase are also
+            # located.
+            i = peak_index
+
+    return flare_times
