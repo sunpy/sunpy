804d7d7ffef34c681704399d04f7c4061d948efc
VaticanCameos <chakrabortypritish@gmail.com>
2014-06-19 12:09:20 +0530

    Added the Heliocentric system. Heliocentric coordinates can be in Cartesian and radial representation. Specified the same in _frame_specific_representation_info.

--- sunpy/coordinates/sunpy_frames.py
+++ sunpy/coordinates/sunpy_frames.py
@@ -33,13 +33,13 @@ class HelioGraphicStonyhurst(BaseCoordinateFrame):
     ----------
     representation: `BaseRepresentation` or None
         A representation object or None to have no data.
-    lon: `Angle` object, optional, must be keyword.
+    lon: `Angle` object.
         The longitude for this object (``lat`` must also be given and ``representation``
         must be None).
-    lat: `Angle` object, optional, must be keyword.
+    lat: `Angle` object.
         The latitude for this object (``lon`` must also be given and ``representation``
         must be None).
-    rad: `astropy.units.Quantity` object, optional, must be keyword.
+    rad: `astropy.units.Quantity` object.
         This quantity holds the radial distance. If not specified, it is, by default,
         the solar radius.
     """
@@ -75,6 +75,9 @@ class HelioGraphicCarrington(HelioGraphicStonyhurst):
     This frame differs from the Stonyhurst version in the
     definition of the longitude, which is defined using
     an offset which is a time-dependent scalar value.
+    representation: `BaseRepresentation` or None.
+        A representation object. If specified, other parameters must
+        be in keyword form.
     lon: `Angle` object.
         The longitude for this object (``lat`` must also be given and ``representation``
         must be None).
@@ -97,3 +100,35 @@ class HelioGraphicCarrington(HelioGraphicStonyhurst):
 
         # TODO: Understand how Carrington offset works.
         super(HelioGraphicCarrington, self).__init__(args, kwargs)
+
+class HelioCentric(BaseCoordinateFrame):
+    """
+    A coordinate or frame in the Heliocentric system.
+    This frame may either be specified in Cartesian
+    or cylindrical representation.
+    Cylindrical representation replaces (x, y) with
+    (rho, psi) where rho is the impact parameter and
+    psi is the position angle in degrees.
+
+    Parameters
+    ----------
+    representation: `BaseRepresentation` or None.
+        A representation object. If specified, other parameters must
+        be in keyword form.
+    x: `Quantity` object.
+        X-axis coordinate, specified in kilometres.
+    y: `Quantity` object.
+        Y-axis coordinate, specified in kilometres.
+    z: `Quantity` object. Shared by both representations.
+        Z-axis coordinate, specified in kilometres.
+    """
+
+    default_representation = CartesianRepresentation
+
+    frame_attr_names = {}
+
+    _frame_specific_representation_info = {
+        'cartesian': {'names': ('x', 'y', 'z'), 'units': (u.km, u.km, u.km)},
+        'cylindrical': {'names': ('rho', 'psi', 'z'), 'units': (None, u.deg, u.km)}
+        }
+    
