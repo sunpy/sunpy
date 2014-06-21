7e811b06bec0fe958ad6c00fc5ffa069019eb889
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-06-21 13:48:09 -0500

    Attempt to fix errors via a rebase

--- sunpy/map/header.py
+++ sunpy/map/header.py
@@ -8,24 +8,26 @@ from sunpy.util.odict import OrderedDict
 
 __all__ = ['MapMeta']
 
+
 class MapMeta(OrderedDict):
     """
     A class to hold meta data associated with a Map derivative.
-    
-    This class handles everything a lower case. This allows case insensitive 
+
+    This class handles everything a lower case. This allows case insensitive
     indexing.
     """
     def __init__(self, adict, *args):
         """Creates a new MapHeader instance"""
         # Store all keys as upper-case to allow for case-insensitive indexing
-        #OrderedDict can be instanciated from a list of lists or a tuple of tuples
+        # OrderedDict can be instantiated from a
+        #   list of lists or a tuple of tuples
         if isinstance(adict, list) or isinstance(adict, tuple):
             tags = dict((k.upper(), v) for k, v in adict)
         elif isinstance(adict, dict):
             tags = dict((k.upper(), v) for k, v in adict.items())
         else:
             raise TypeError("Can not create a MapMeta from this type input")
-            
+
         super(MapMeta, self).__init__(tags, *args)
 
     def __contains__(self, key):
@@ -39,7 +41,7 @@ class MapMeta(OrderedDict):
     def __setitem__(self, key, value):
         """Overide [] indexing"""
         return OrderedDict.__setitem__(self, key.lower(), value)
-        
+
     def get(self, key, default=None):
         """Overide .get() indexing"""
         return OrderedDict.get(self, key.lower(), default)
@@ -54,7 +56,8 @@ class MapMeta(OrderedDict):
 
     def update(self, d2):
         """Overide .update() to perform case-insensitively"""
-        return OrderedDict.update(self, dict((k.lower(), v) for k, v in d2.items()))
+        dic = dict((k.lower(), v) for k, v in d2.items())
+        return OrderedDict.update(self, dic)
 
     def setdefault(self, key, default=None):
         """Overide .setdefault() to perform case-insensitively"""
