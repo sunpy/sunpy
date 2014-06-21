659f4550b822d10925ba9c0836250906067cbd2e
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-06-21 17:45:44 +0300

    PEP8 changes

--- sunpy/lightcurve/tests/test_lyra.py
+++ sunpy/lightcurve/tests/test_lyra.py
@@ -13,38 +13,39 @@ from sunpy.time import parse_time
 
 @pytest.mark.online
 def test_lyra_level():
-    lyra1 = sunpy.lightcurve.LYRALightCurve.create('2012/06/03',level=3)
-    lyra2 = sunpy.lightcurve.LYRALightCurve.create('2012/06/03',level=2)
-    assert isinstance(lyra1,sunpy.lightcurve.LYRALightCurve)
-    assert isinstance(lyra2,sunpy.lightcurve.LYRALightCurve)
+    lyra1 = sunpy.lightcurve.LYRALightCurve.create('2012/06/03', level=3)
+    lyra2 = sunpy.lightcurve.LYRALightCurve.create('2012/06/03', level=2)
+    assert isinstance(lyra1, sunpy.lightcurve.LYRALightCurve)
+    assert isinstance(lyra2, sunpy.lightcurve.LYRALightCurve)
 
     assert lyra1.time_range().start() == parse_time('2012-06-03 00:00:00.047000')
     assert lyra1.time_range().end() == parse_time('2012-06-03 00:23:59.047000')
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(('date','start','end'),
-[('2011/02/06','2011-02-06 00:00:00.008500','2011-02-06 23:59:59.008500'),
- ('2012/08/04','2012-08-04 00:00:00.043000','2012-08-04 23:59:59.043000'),
+@pytest.mark.parametrize(('date', 'start', 'end'),
+[('2011/02/06', '2011-02-06 00:00:00.008500', '2011-02-06 23:59:59.008500'),
+ ('2012/08/04', '2012-08-04 00:00:00.043000', '2012-08-04 23:59:59.043000'),
  ])
-def test_lyra_date(date,start,end):
+
+def test_lyra_date(date, start, end):
    
-   lyra=sunpy.lightcurve.LYRALightCurve.create(date)
-   assert isinstance(lyra,sunpy.lightcurve.LYRALightCurve)
-   assert lyra.time_range().start()==parse_time(start)
-   assert lyra.time_range().end() == parse_time(end)
+    lyra = sunpy.lightcurve.LYRALightCurve.create(date)
+    assert isinstance(lyra, sunpy.lightcurve.LYRALightCurve)
+    assert lyra.time_range().start() == parse_time(start)
+    assert lyra.time_range().end() == parse_time(end)
 
 @pytest.mark.online
-@pytest.mark.parametrize(('url','start','end'),
-[('http://proba2.oma.be/lyra/data/bsd/2012/02/04/lyra_20120204-000000_lev3_std.fits','2012-02-04 00:00:00.004000','2012-02-04 00:23:59.004000'),
-('http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev3_std.fits','2011-08-10 00:00:00.020000','2011-08-10 00:23:59.020000')])
+@pytest.mark.parametrize(('url', 'start', 'end'),
+[('http://proba2.oma.be/lyra/data/bsd/2012/02/04/lyra_20120204-000000_lev3_std.fits', '2012-02-04 00:00:00.004000', '2012-02-04 00:23:59.004000'),
+('http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev3_std.fits', '2011-08-10 00:00:00.020000', '2011-08-10 00:23:59.020000')])
 
-def test_online(url,start,end):
+def test_online(url, start, end):
    
-   lyra=sunpy.lightcurve.LYRALightCurve.create(url)
-   assert isinstance(lyra,sunpy.lightcurve.LYRALightCurve)
-   assert lyra.time_range().start()==parse_time(start)
-   assert lyra.time_range().end() == parse_time(end)
+    lyra = sunpy.lightcurve.LYRALightCurve.create(url)
+    assert isinstance(lyra, sunpy.lightcurve.LYRALightCurve)
+    assert lyra.time_range().start() == parse_time(start)
+    assert lyra.time_range().end() == parse_time(end)
 
  
 
