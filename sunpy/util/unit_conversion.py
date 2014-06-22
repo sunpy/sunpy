d1e08cc4396fe3a2e56eb0a6c694497f186e0648
kaichogami <asishrocks95@gmail.com>
2014-06-22 15:15:02 +0530

    removed uncessary functions

--- sunpy/util/unit_conversion.py
+++ sunpy/util/unit_conversion.py
@@ -8,31 +8,7 @@ __all__ = ['degrees_to_hours', 'degrees_to_arc', 'kelvin_to_keV',
            'keV_to_kelvin', 'to_angstrom']
 
 boltz_unit = units.J / units.K
-	          
-def degrees_to_hours(angle):
-    """Converts an angle from the degree notation to the hour, arcmin, arcsec
-    notation (returned as a tuple)."""
-    if not isinstance(angle, units.Quantity):
-        raise ValueError("angle should be a Quantity")
-    hour = (np.floor(angle / 15)) / units.deg
-    remainder = ((angle / 15.0) / units.deg) - hour
-    arcminute = (np.floor(remainder * 60)) 
-    remainder =  remainder * 60 - arcminute
-    arcsecond = remainder * 60.0
-    return [hour * units.hourangle, arcminute * units.arcmin, arcsecond * units.arcsec]
-	
-def degrees_to_arc(angle):
-    """Converts decimal degrees to degree, arcminute,
-    arcsecond (returned as a tuple)."""
-    if not isinstance(angle, units.Quantity):
-        raise ValueError("angle should be a Quantity")	
-    degree = (np.floor(angle)) / units.deg
-    remainder = (angle / units.deg) - degree
-    arcminute = (np.floor(remainder * 60))
-    remainder =  remainder * 60 - arcminute
-    arcsecond = remainder * 60.0
-    return [degree * units.degree, arcminute * units.arcmin, arcsecond * units.arcsec]
-	
+
 def to_angstrom(value, unit):
     """Given a value with a unit (given in a string), convert to angstroms"""
     value_quantity = value * units.Unit(unit)
