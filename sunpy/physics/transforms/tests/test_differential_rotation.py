c2eb63ed5dbca5cfa440b718559c66352c15ccf1
kaichogami <asishrocks95@gmail.com>
2014-06-22 15:10:14 +0530

    fixed tests for diff_rot

--- sunpy/physics/transforms/tests/test_differential_rotation.py
+++ sunpy/physics/transforms/tests/test_differential_rotation.py
@@ -1,17 +1,43 @@
-from numpy.testing import assert_array_almost_equal as a
-from sunpy.physics.transforms.differential_rotation import diff_rot
+from __future__ import absolute_import
+import unittest
+import numpy as np
 from astropy import units as u
-from astropy.coordinates import Latitude
+from sunpy.physics.transforms.differential_rotation import diff_rot
+#pylint: disable=C0103,R0904,W0201,W0212,W0232,E1103
 
-def test_diff_rot():
+class DiffRotTest(unittest.TestCase):
     """ Please note the numbers in these tests are not checked for physical
     accuracy, only that they are the values the function was outputting upon
     implementation. """
-
-    a(diff_rot(10, Latitude(30, u.deg)), 136.8216 * u.deg, decimal = 4)
-    a(diff_rot(10, Latitude(30, u.deg), rot_type='howard', frame_time='synodic'), 126.9656 * u.deg,decimal = 4)
-    a(diff_rot(10, Latitude(30, u.deg), rot_type='howard', frame_time='sidereal'), 136.8216 * u.deg, decimal=4)				
-    a(diff_rot(10, Latitude(30, u.deg), rot_type='howard'), 136.8216 * u.deg, decimal =4)
-    a(diff_rot(10, Latitude(30, u.deg), rot_type='allen'), 136.9 * u.deg, decimal = 1)
-    a(diff_rot(10, Latitude(30, u.deg), rot_type='snodgrass'), 135.4232 * u.deg, decimal = 4)
+    
+    def test_single(self):
+        rot = diff_rot(10, 30 * u.deg)
+        self.failUnless(rot == 136.8216 * u.deg)
+        
+    def test_array(self):
+        rot = diff_rot(10, np.linspace(-70, 70, 2) * u.deg)
+        self.failUnless(np.array_equal(rot, np.array([110.2725,  110.2725]) * u.deg))
+        
+    def test_synodic(self):
+        rot = diff_rot(10, 30 * u.deg, rot_type='howard', frame_time='synodic')
+        self.failUnless(rot == 126.9656 * u.deg)
+        
+    def test_sidereal(self):
+        rot = diff_rot(10, 30 * u.deg, rot_type='howard', frame_time='sidereal')
+        self.failUnless(rot == 136.8216 * u.deg)
+        
+    def test_howard(self):
+        rot = diff_rot(10, 30 * u.deg, rot_type='howard')
+        self.failUnless(rot == 136.8216 * u.deg)
+ 
+    def test_allen(self):
+        rot = diff_rot(10, 30 * u.deg, rot_type='allen')
+        self.failUnless(rot == 136.9 * u.deg)
+    
+    def test_snodgrass(self):
+        rot = diff_rot(10, 30 * u.deg, rot_type='snodgrass')
+        self.failUnless(rot == 135.4232 * u.deg)
+    
+    def test_fail(self):
+        self.assertRaises(ValueError, diff_rot, 10, 30 * u.deg, rot_type='garbage')
 
