968c9d1f7c7f1e88b1fd98aaa84e10a1036bad4a
Jack Ireland <jackireland@gmail.com>
2014-06-23 09:19:54 -0400

    adding placeholder for mapcube test functionality

---
+++ sunpy/map/tests/test_mapcube.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Test MapCube functionality. 
+"""
+from __future__ import absolute_import
+
+
+def test_all_maps_same_shape(aia_map_large):
+    """Test that this function can detect when all the maps in an mapcube
+    have the same shape, and when this is not true"""
+
+
+def test_as_array(aia_map_large):
+    """Test that this function returns a three dimensional ndarray."""
+
+
+def test_all_meta(aia_map_large):
+    """Test that this function returns a list of map meta objects."""
