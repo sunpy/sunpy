dc6f7d7670e0d501f4e20c94180e4df211c81e27
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-06-25 02:45:02 +0300

    Registered Lyra, norh, made changes to query response by unified downloader

--- sunpy/net/lcsources/__init__.py
+++ sunpy/net/lcsources/__init__.py
@@ -1,4 +1,8 @@
 from .factbase import UnifiedDownloader
 from .eve import EVEClient
+from .lyra import LYRAClient
+from .norh import NoRHClient
 UnifiedDownloader.register(EVEClient,EVEClient._can_handle_query)
+UnifiedDownloader.register(LYRAClient,LYRAClient._can_handle_query)
+UnifiedDownloader.register(NoRHClient,NoRHClient._can_handle_query)
 
