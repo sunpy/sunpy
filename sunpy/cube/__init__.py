fc30274ce261194873831daaea3fea26a19bf675
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-06-25 17:57:38 -0500

    Created new branch to avoid weird commits

---
+++ sunpy/cube/__init__.py
@@ -0,0 +1,6 @@
+'''SunPy Spectral Cubes'''
+
+__author__ = 'Mateo Inchaurrandieta'
+__email__ = 'mateo.inchaurrandieta@gmail.com'
+
+from . spectral_cube import SpectralCube
---
+++ sunpy/cube/sources/eis.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Author: Mateo Inchaurrandieta <mateo.inchaurrandieta@gmail.com>
+
+from __future__ import absolute_import
+
+import spectral_cube as sc
+import numpy as np
+from astropy.io import fits
+from astropy import wcs
+from sunpy.cube import SpectralCube
+
+__all__ = ['EISSpectralCube']
+
+
+def _clean(header):
+    """ Fixes non-standard or deprecated CTYPEn FITS keywords.
+
+    Parameters
+    ----------
+    header : astropy.io.fits.Header
+        The header to be cleaned.
+    """
+    header['ctype1'] = 'HPLN-TAN'  # Helioprojective longitude, TAN projection
+    header['ctype2'] = 'HPLT-TAN'  # Helioprojective latitude, TAN projection
+    header['ctype3'] = 'WAVE-   '  # Wavelength axis, default (TAB) projection
+    header['naxis'] = 3
+    return header
+
+
+class EISSpectralCube(SpectralCube):
+    # TODO: write docstring
+    def __init__(self, cube, dataHeader=None, primaryHeader=None):
+        SpectralCube.__init__(self, cube,
+                                           data_header=dataHeader,
+                                           primary_header=primaryHeader)
+
+    @classmethod
+    def read(cls, filename, **kwargs):
+        """ Reads in a given FITS file and returns a dictionary of new
+        EISSpectralCubes. Additional parameters are given to fits.open.
+
+        Parameters
+        ----------
+        filename : string
+            Complete location of the FITS file
+        """
+        hdulist = fits.open(name=filename, **kwargs)
+        header = _clean(hdulist[0].header)
+        w = wcs.WCS(header=header, naxis=3)
+        wavelengths = [c.name for c in hdulist[1].columns if c.dim is not None]
+        data = [hdulist[1].data[wav] for wav in wavelengths]
+        cubes = [sc.SpectralCube(data=d, wcs=w) for d in data]
+        scubes = [EISSpectralCube(c, dataHeader=hdulist[1], primaryHeader=hdulist[0]) for c in cubes]
+        return dict(zip(wavelengths, scubes))
+
+    @classmethod
+    def is_datasource_for(cls, data, header, **kwargs):
+        return True
