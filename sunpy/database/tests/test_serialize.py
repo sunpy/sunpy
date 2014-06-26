6167ce1ab5b21f94d2d68b6a9e715316ace3a8d4
kaichogami <asishrocks95@gmail.com>
2014-06-26 22:58:04 +0530

    removed variables from json value

--- sunpy/database/tests/test_serialize.py
+++ sunpy/database/tests/test_serialize.py
@@ -96,7 +96,7 @@ def test_attr_and():
 
 
 def test_decode_wave():
-    dump = '{"Wave": [10.0 * u.AA, 20.0 * u.AA]}'
+    dump = '{"Wave": [10.0, 20.0 ]}'
     assert json.loads(dump, object_hook=query_decode) == vso.attrs.Wave(10*u.AA, 20*u.AA)
 
 
