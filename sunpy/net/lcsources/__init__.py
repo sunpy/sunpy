34ae4685061fe2821d85ceae6945366077c62174
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-06-28 13:59:45 +0300

    Get method added

--- sunpy/net/lcsources/__init__.py
+++ sunpy/net/lcsources/__init__.py
@@ -2,7 +2,9 @@ from .factbase import UnifiedDownloader
 from .eve import EVEClient
 from .lyra import LYRAClient
 from .norh import NoRHClient
+from ..vso import VSOClient
 UnifiedDownloader.register(EVEClient,EVEClient._can_handle_query)
 UnifiedDownloader.register(LYRAClient,LYRAClient._can_handle_query)
 UnifiedDownloader.register(NoRHClient,NoRHClient._can_handle_query)
+UnifiedDownloader.register(VSOClient,VSOClient._can_handle_query)
 
