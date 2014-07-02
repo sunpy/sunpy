ca3990e9d1d662359590a751374ca6c9652e254c
DavidPS <dps.helio@gmail.com>
2014-07-02 17:47:26 +0200

    Changed test to units supported for an older version of astropy (0.32)

--- sunpy/net/tests/test_vso.py
+++ sunpy/net/tests/test_vso.py
@@ -156,8 +156,8 @@ def test_wave_toangstrom():
     assert int(w.min.to(u.AA, u.equivalencies.spectral()).value) == 199
 
     with pytest.raises(ValueError) as excinfo:
-        va.Wave(10 * u.deg_C, 23 * u.deg_C)
-    assert excinfo.value.message == "'deg_C' is not a spectral supported unit"
+        va.Wave(10 * u.g, 23 * u.g)
+    assert excinfo.value.message == "'g' is not a spectral supported unit"
 
 
 def test_time_xor():
