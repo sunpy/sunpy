Fixed choose slice functions to accept units, added _choose_wavelength_slice tests

sunpy/cube/spectral_cube.py
@@ -7,6 +7,7 @@ import astropy.nddata
 import sunpy.util.util as util
 from sunpy.map import GenericMap
 from sunpy.visualization.imageanimator import ImageAnimator
+import astropy.units as u
 
 __all__ = ['SpectralCube']
 
@@ -120,7 +121,7 @@ class SpectralCube(astropy.nddata.NDData):
 
         Parameters
         ----------
-        offset: int or float
+        offset: int or astropy quantity
             Offset from the cube's primary wavelength. If the value is an int,
             then it returns that slice. Otherwise, it will return the nearest
             wavelength to the one specified.
@@ -130,15 +131,14 @@ class SpectralCube(astropy.nddata.NDData):
            offset < len(self.data)):
             a = self.data[offset, :, :]
 
-        # TODO: this currently fails because delta is a numpy vector
-        if isinstance(offset, float):
-            delta = self.wcs.wcs.cdelt[2]
-            wloffset = offset / delta
+        if isinstance(offset, u.Quantity):
+            delta = self.wcs.wcs.cdelt[2] * u.m
+            wloffset = offset.to(u.m) / delta
             wloffset = int(wloffset)
             if wloffset >= 0 and wloffset < len(self.data):
                 a = self.data[wloffset, :, :]
 
-        return np.array(a)
+        return a
 
     def _choose_x_slice(self, offset):
         '''
@@ -147,7 +147,7 @@ class SpectralCube(astropy.nddata.NDData):
 
         Parameters
         ----------
-        offset: int or float
+        offset: int or astropy quantity
             Offset from the cube's initial x. If the value is an int,
             then it returns that slice. Otherwise, it will return the nearest
             wavelength to the one specified.
@@ -156,15 +156,16 @@ class SpectralCube(astropy.nddata.NDData):
         if (isinstance(offset, int) and offset >= 0 and
            offset < self.data.shape[2]):
             a = self.data[:, :, offset]
+            a = a.T
 
-        # TODO: This fails because delta is not a scalar (and it actually gets
-        # a wavelength slice, but nevermind...)
-        if isinstance(offset, float):
-            delta = self.wcs.wcs.cdelt[0]
-            wloffset = offset / delta
+        if isinstance(offset, u.Quantity):
+            unit = self.wcs.wcs.cunit[0]
+            delta = self.wcs.wcs.cdelt[0] * unit
+            wloffset = offset.to(unit) / delta
             wloffset = int(wloffset)
-            if wloffset >= 0 and wloffset < len(self.data):
+            if wloffset >= 0 and wloffset < self.data.shape[2]:
                 a = self.data[:, :, wloffset]
+                a = a.T
 
         return np.array(a).T
 

sunpy/cube/tests/test_spectralcube.py
@@ -7,6 +7,7 @@ import sunpy.cube.spectral_cube as sc
 import numpy as np
 from astropy.wcs import WCS
 import pytest
+import astropy.units as u
 
 
 # sample data for tests
@@ -35,4 +36,25 @@ def test_slice_to_map():
     m1 = cube.slice_to_map((0, 3))
     assert np.all(m0.data == cube.data[0])
     assert np.all(m1.data == cube.data.sum(0))
-    
+
+
+def test_choose_wavelength_slice():
+    ius = cube._choose_wavelength_slice(-1)  # integer, under range slice
+    iis = cube._choose_wavelength_slice(1)  # integer, in range slice
+    ios = cube._choose_wavelength_slice(11)  # integer, over range slice
+
+    qus = cube._choose_wavelength_slice(-1 * u.Angstrom)  # quantity, under
+    qis = cube._choose_wavelength_slice(0.5 * u.Angstrom)  # quantity, in
+    qos = cube._choose_wavelength_slice(8 * u.Angstrom)  # quantity, over
+
+    f = cube._choose_wavelength_slice(0.4)  # no units given
+
+    assert ius is None
+    assert np.all(iis == [[2, 4, 5, 3], [10, 5, 2, 2]])
+    assert ios is None
+
+    assert qus is None
+    assert np.all(qis == [[0, -1, 2, 3], [10, 3, 3, 0]])
+    assert qos is None
+
+    assert f is None