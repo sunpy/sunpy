7afe93ffa92301fb2567b3c444aafb834426d7cf
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-07-03 14:24:49 +0300

    Rhessi client added, query response show method handled

--- sunpy/net/unifieddownloader/__init__.py
+++ sunpy/net/unifieddownloader/__init__.py
@@ -3,8 +3,11 @@ from .eve import EVEClient
 from .lyra import LYRAClient
 from .norh import NoRHClient
 from ..vso import VSOClient
+from .RHESSI import RHESSIClient
 UnifiedDownloader.register(EVEClient,EVEClient._can_handle_query)
 UnifiedDownloader.register(LYRAClient,LYRAClient._can_handle_query)
 UnifiedDownloader.register(NoRHClient,NoRHClient._can_handle_query)
 UnifiedDownloader.register(VSOClient,VSOClient._can_handle_query)
-
+UnifiedDownloader.register(NOAAIndicesClient,NOAAIndicesClient._can_handle_query)
+UnifiedDownloader.register(NOAAIndicesClient,NOAAPredictClient._can_handle_query)
+UnifiedDownloader.register(RHESSIClient,RHESSIClient._can_handle_query)
---
+++ sunpy/net/unifieddownloader/rhessi.py
@@ -0,0 +1,40 @@
+from sunpy.net.vso.attrs import Time,Instrument
+from sunpy.net.unifieddownloader.client import GenericClient
+from sunpy.instr import rhessi
+
+__all__ = ['Time','Instrument']
+
+class RHEESIClient(GenericClient):
+        @classmethod
+        def _get_url_for_timerange(cls,timerange,**kwargs):
+            """Returns a URL to the RHESSI data for the specified date range.
+
+            Parameters
+            ----------
+            args : TimeRange, datetimes, date strings
+            Date range should be specified using a TimeRange, or start
+            and end dates at datetime instances or date strings.
+            """
+            url = rhessi.get_obssum_filename(timerange)
+            return [url]
+
+
+        def _makeimap(self):
+	    '''Helper Function:used to hold information about source. '''
+	    self.map_['source'] = ''
+	    self.map_['instrument'] = 'rhessi'
+	    self.map_['phyobs'] = 'irradiance'
+	    self.map_['provider'] = 'nasa'
+        
+        @classmethod
+        def _can_handle_query(cls,*query):
+            """Boolean Function:Answers whether client can service the query.
+            """
+	    chkattr =  ['Time','Instrument']
+            chklist =  [x.__class__.__name__ in chkattr for x in query]
+            for x in query:
+	        if x.__class__.__name__ == 'Instrument' and x.value == 'rhessi':
+                    return all(chklist)
+	    return False
+ 
+
