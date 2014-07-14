a7adc16bb44225f579356125d128fefc69c2b4f2
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-07-14 14:47:15 +0300

    corrected the error message.

--- sunpy/net/jsoc/jsoc.py
+++ sunpy/net/jsoc/jsoc.py
@@ -347,7 +347,7 @@ class JSOCClient(object):
 
         if kwargs.has_key('wavelength'):
 	    if series[0:3] != 'aia':
-	        raise TypeError("This series does not support wavelength attribute.")
+	        raise TypeError("This series does not support the wavelength attribute.")
 	    else:
 	       if isinstance(kwargs['wavelength'],list):
 	           tmp = str(kwargs['wavelength'])
