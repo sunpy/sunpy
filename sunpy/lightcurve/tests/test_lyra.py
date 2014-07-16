cb8775ae0f3f6e283b24f64aed6b23f67ed26a1f
Stuart Mumford <stuart@mumford.me.uk>
2014-07-16 13:10:35 +0100

    Fix up all glymur and ANA tests so they are skipped.

--- sunpy/lightcurve/tests/test_lyra.py
+++ sunpy/lightcurve/tests/test_lyra.py
@@ -8,7 +8,6 @@ import pytest
 #pylint: disable=C0103,R0904,W0201,W0232,E1103
 import sunpy
 import matplotlib as mpl
-from matplotlib.testing.decorators import cleanup
 
 @pytest.mark.online
 def test_lyra():
@@ -16,8 +15,3 @@ def test_lyra():
     "http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev2_std.fits")
     assert isinstance(lyra, sunpy.lightcurve.LYRALightCurve)
 
-#@cleanup
-#def test_peek():
-#    lyra = sunpy.lightcurve.LYRALightCurve.create(
-#    "http://proba2.oma.be/lyra/data/bsd/2011/08/10/lyra_20110810-000000_lev2_std.fits")
-#    assert isinstance(lyra.peek(),mpl.figure.Figure)
