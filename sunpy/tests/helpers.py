99ee80548205086e970abb7937c52e75860ba104
Stuart Mumford <stuart@mumford.me.uk>
2014-07-16 13:10:35 +0100

    Fix up all glymur and ANA tests so they are skipped.

--- sunpy/tests/helpers.py
+++ sunpy/tests/helpers.py
@@ -3,6 +3,35 @@ import warnings
 
 import pytest
 
+__all__ = ['skip_glymur', 'skip_ana', 'warnings_as_errors']
+
+# SunPy's JPEG2000 capabilities rely on the glymur library.  First we check to
+# make sure that glymur imports correctly before proceeding.
+try:
+    import glymur
+except ImportError:
+    SKIP_GLYMUR = True
+else:
+    SKIP_GLYMUR = False
+
+# Skip ana tests if we are on Windows or we can't import the c extension.
+import platform
+if platform.system() == 'Windows':
+    SKIP_ANA = True
+else:
+    SKIP_ANA = False
+
+try:
+    import sunpy.io._pyana
+except ImportError:
+    SKIP_ANA = True
+else:
+    SKIP_ANA = SKIP_ANA or False
+
+skip_glymur = pytest.mark.skipif(SKIP_GLYMUR, reason="Glymur can not be imported")
+
+skip_ana = pytest.mark.skipif(SKIP_ANA, reason="ANA is not availible")
+
 @pytest.fixture
 def warnings_as_errors(request):
     warnings.simplefilter('error')
