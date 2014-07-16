6dd30e53114bb5ec2e8a48ac5db634644a06264c
Stuart Mumford <stuart@mumford.me.uk>
2014-07-16 12:08:29 +0100

    Clean up ANA tests and make them skip if ANA not present or on windows
    Also make all jp2 tests skip if glymur is not present.

--- sunpy/io/tests/test_filetools.py
+++ sunpy/io/tests/test_filetools.py
@@ -2,9 +2,35 @@
 import numpy as np
 import os
 
+import pytest
+
 import sunpy
 import sunpy.io
 import sunpy.data.test
+
+# SunPy's JPEG2000 capabilities rely on the glymur library.  First we check to
+# make sure that glymur imports correctly before proceeding.
+try:
+    import glymur
+except ImportError:
+    glymur_imports = False
+else:
+    glymur_imports = True
+
+# Skip ana tests if we are on Windows or we can't import the c extension.
+import platform
+if platform.system() == 'Windows':
+    skip_ana = True
+else:
+    skip_ana = False
+
+try:
+    import sunpy.io._pyana
+except ImportError:
+    skip_ana = True
+else:
+    skip_ana = skip_ana or False
+    
 #==============================================================================
 # Test, read, get_header and write through the file independant layer
 #==============================================================================
@@ -28,6 +54,7 @@ class TestFiletools():
         assert all([isinstance(p[1],
                                sunpy.io.header.FileHeader) for p in pairs])
 
+    @pytest.mark.skipif("glymur_imports is False")
     def test_read_file_jp2(self):
         #Test read jp2
         pair = sunpy.io.read_file(os.path.join(sunpy.data.test.rootdir,
@@ -46,6 +73,7 @@ class TestFiletools():
         assert len(hlist) == 1
         assert isinstance(hlist[0], sunpy.io.header.FileHeader)
 
+    @pytest.mark.skipif("glymur_imports is False")
     def test_read_file_header_jp2(self):
         #Test jp2
         hlist = sunpy.io.read_file_header(os.path.join(sunpy.data.test.rootdir,
@@ -66,6 +94,7 @@ class TestFiletools():
         assert outpair[1] == aiapair[1]
         os.remove("aia_171_image.fits")
 
+    @pytest.mark.skipif("skip_ana is True")
     def test_read_file_ana(self):
         ana_data = sunpy.io.read_file(os.path.join(sunpy.data.test.rootdir,"test_ana.fz"))
         assert isinstance(ana_data, list)
@@ -74,12 +103,14 @@ class TestFiletools():
         assert isinstance(ana_data[0][0], np.ndarray)
         assert isinstance(ana_data[0][1], sunpy.io.header.FileHeader)
 
+    @pytest.mark.skipif("skip_ana is True")
     def test_read_file__header_ana(self):
         ana_data = sunpy.io.read_file_header(os.path.join(sunpy.data.test.rootdir,"test_ana.fz"))
         assert isinstance(ana_data, list)
         assert len(ana_data) == 1
         assert isinstance(ana_data[0], sunpy.io.header.FileHeader)
 
+    @pytest.mark.skipif("skip_ana is True")
     def test_write_file_ana(self):
         ana = sunpy.io.read_file(os.path.join(sunpy.data.test.rootdir,"test_ana.fz"))[0]
         sunpy.io.write_file("ana_test_write.fz", ana[0], str(ana[1]))
