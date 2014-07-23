1e4ade31146fc12d65138a62d03c789769ca7683
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-07-23 14:05:37 +0300

    indentation fix

--- sunpy/net/unifieddownloader/sources/lyra.py
+++ sunpy/net/unifieddownloader/sources/lyra.py
@@ -10,14 +10,14 @@ class LYRAClient(GenericClient):
         Helper function:
         Input:
         timerange: Time-range over which data is to be downloaded
-	Output: List of urls
-        """               
+        Output: List of urls
+        """
         if not timerange:
-	    return []
+            return []
 
-	days = timerange.get_dates()
+        days = timerange.get_dates()
         urls = []
-	for day in days:
+        for day in days:
             urls.append(self._get_url_for_date(day, **kwargs))
         return urls
 
@@ -27,25 +27,23 @@ class LYRAClient(GenericClient):
             raise ValueError("This method requires a date")
         filename = "lyra_%s000000_lev%d_%s.fits" % (date.strftime('%Y%m%d-'), 2, 'std')
         base_url = "http://proba2.oma.be/lyra/data/bsd/"
-	url_path = urlparse.urljoin(date.strftime('%Y/%m/%d/'), filename)
+        url_path = urlparse.urljoin(date.strftime('%Y/%m/%d/'), filename)
         return urlparse.urljoin(base_url, url_path)
 
     def _makeimap(self):
-    '''Helper Function:used to hold information about source. '''
+        '''Helper Function:used to hold information about source. '''
         self.map_['source'] = 'Proba2'
         self.map_['instrument'] = 'lyra'
         self.map_['phyobs'] = 'irradiance'
         self.map_['provider'] = 'esa'
-        
+
     @classmethod
     def _can_handle_query(cls, *query):
-    """Boolean Function:Answers whether client can service the query.
-    """
+        """Boolean Function:Answers whether client can service the query.
+        """
         chkattr =  ['Time', 'Instrument', 'Level']
         chklist =  [x.__class__.__name__ in chkattr for x in query]
         for x in query:
             if x.__class__.__name__ == 'Instrument' and x.value == 'lyra':
                 return all(chklist)
-	return False
- 
-
+        return False
