e6207d73e042598e79d8d063ab7782d46ac2a883
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-07-23 14:05:37 +0300

    indentation fix

--- sunpy/net/unifieddownloader/sources/rhessi.py
+++ sunpy/net/unifieddownloader/sources/rhessi.py
@@ -5,7 +5,7 @@ from sunpy.instr import rhessi
 __all__ = ['Time', 'Instrument']
 
 class RHESSIClient(GenericClient):
-        
+
     def _get_url_for_timerange(self, timerange, **kwargs):
         """
         Returns a URL to the RHESSI data for the specified date range.
@@ -15,8 +15,8 @@ class RHESSIClient(GenericClient):
         Date range should be specified using a TimeRange, or start
         and end dates at datetime instances or date strings.
         """
-	if not timerange:
-	    return []
+        if not timerange:
+            return []
 
         url = rhessi.get_obssum_filename(timerange)
         return [url]
@@ -28,7 +28,7 @@ class RHESSIClient(GenericClient):
         self.map_['instrument'] = 'rhessi'
         self.map_['phyobs'] = 'irradiance'
         self.map_['provider'] = 'nasa'
-        
+
     @classmethod
     def _can_handle_query(cls, *query):
         """Boolean Function:Answers whether client can service the query.
@@ -36,8 +36,6 @@ class RHESSIClient(GenericClient):
         chkattr =  ['Time', 'Instrument']
         chklist =  [x.__class__.__name__ in chkattr for x in query]
         for x in query:
-	    if x.__class__.__name__ == 'Instrument' and x.value == 'rhessi':
+            if x.__class__.__name__ == 'Instrument' and x.value == 'rhessi':
                 return all(chklist)
-	return False
- 
-
+        return False
