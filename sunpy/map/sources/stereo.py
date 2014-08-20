dde63cfc263bb6e8a0ed37f41170b07703317345
Stuart Mumford <stuart@mumford.me.uk>
2014-08-20 12:32:37 +0200

    Add STEREO HI colour maps and a HIMap class.

--- sunpy/map/sources/stereo.py
+++ sunpy/map/sources/stereo.py
@@ -68,3 +68,30 @@ class CORMap(GenericMap):
     def is_datasource_for(cls, data, header, **kwargs):
         """Determines if header corresponds to an COR image"""
         return header.get('detector', '').startswith('COR')
+
+class HIMap(GenericMap):
+    """HI Image Map definition"""
+
+    def __init__(self, data, header, **kwargs):
+
+        GenericMap.__init__(self, data, header, **kwargs)
+
+        self._name = self.observatory + " " + self.detector + " " + str(self.measurement)
+        self._nickname = "{0}-{1}".format(self.detector, self.observatory[-1])
+
+        self.cmap = cm.get_cmap('stereohi%s' % self.detector[-1])
+
+        # Try to identify when the FITS meta data ddes not have the correct
+        # date FITS keyword
+        if ('date_obs' in self.meta) and not('date-obs' in self.meta):
+            self.meta['date-obs'] = self.meta['date_obs']
+
+    @property
+    def measurement(self):
+        # TODO: This needs to do more than white-light.  Should give B, pB, etc.
+        return "white-light"
+
+    @classmethod
+    def is_datasource_for(cls, data, header, **kwargs):
+        """Determines if header corresponds to an COR image"""
+        return header.get('detector', '').startswith('HI')
