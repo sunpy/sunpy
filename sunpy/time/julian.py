ae6791b59402f2190552f5d1bd18d03a7e5963d9
dpshelio <dps.helio@gmail.com>
2014-08-29 15:10:54 +0200

    converted julian.py to use Time from astropy

--- sunpy/time/julian.py
+++ sunpy/time/julian.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
 
 from sunpy.time import parse_time
+from astropy.time import Time
 
 __all__ = ['julian_day', 'julian_centuries']
 
 # The number of days between Jan 1 1900 and the Julian reference date of
 # 12:00 noon Jan 1, 4713 BC
 JULIAN_DAY_ON_NOON01JAN1900 = 2415021.0
+# Epoch J2000.0 = 2000 January 1.5 TD = JDE 2451 545.0
+JULIAN_DAY_EPHEM_ON_NOON01JAN2000 = 2451545.0
 
 def julian_day(t='now'):
     """Returns the (fractional) Julian day defined as the number of days
@@ -15,27 +18,12 @@ def julian_day(t='now'):
 
     # Good online reference for fractional julian day
     # http://www.stevegs.com/jd_calc/jd_calc.htm
-    JULIAN_REF_DAY = parse_time('1900/1/1 12:00:00')
-    time = parse_time(t)
 
-    tdiff = time - JULIAN_REF_DAY
-
-    julian = tdiff.days + JULIAN_DAY_ON_NOON01JAN1900
-
-    result = julian + 1 / 24. * (time.hour + time.minute / 60.0 +
-                                 time.second / (60. * 60.))
-
-    # This is because the days in datetime objects start at 00:00,
-    # not 12:00 as for Julian days.
-    if time.hour >= 12:
-        result = result - 0.5
-    else:
-        result = result + 0.5
-
-    return result
+    return Time(parse_time(t), scale = 'utc').jd
 
 def julian_centuries(t='now'):
-    """Returns the number of Julian centuries since 1900 January 0.5."""
+    """Returns the number of Julian centuries since 2000 January 1.5 TD."""
     DAYS_IN_YEAR = 36525.0
 
-    return (julian_day(t) - JULIAN_DAY_ON_NOON01JAN1900) / DAYS_IN_YEAR
+    jde = Time(parse_time(t), scale='utc').tt.jd
+    return (jde - JULIAN_DAY_EPHEM_ON_NOON01JAN2000)/ DAYS_IN_YEAR
