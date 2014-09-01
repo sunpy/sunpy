dbc16282e714fca4e5cc7417e36b7c3f99c4889e
wafels <jackireland@gmail.com>
2014-09-01 11:25:53 -0400

    adding in test code for solar derotate

--- sunpy/image/tests/test_solar_differential_rotation.py
+++ sunpy/image/tests/test_solar_differential_rotation.py
@@ -4,7 +4,7 @@
 #
 import numpy as np
 from astropy import units as u
-from numpy.testing import assert_allclose, assert_array_almost_equal
+from numpy.testing import assert_allclose
 from scipy.ndimage.interpolation import shift
 from sunpy import AIA_171_IMAGE
 from sunpy import map
@@ -18,11 +18,13 @@ def test_mapcube_solar_derotate():
     assert(isinstance(test_output, map.MapCube))
 
     # Test the return of the only the displacements.
-    test_displacements = mapcube_solar_derotate(mc, return_displacements_only=True)
+    test_output = mapcube_solar_derotate(mc, return_displacements_only=True)
     # Assert
-    assert(isinstance(test_displacements, dict))
-    assert_allclose(test_displacements['x'], known_displacements['x'], rtol=5e-2, atol=0)
-    assert_allclose(test_displacements['y'], known_displacements['y'], rtol=5e-2, atol=0 )
+    assert(isinstance(test_output, dict))
+    assert_allclose(test_output['x'], known_displacements['x'], rtol=5e-2, atol=0)
+    assert_allclose(test_output['y'], known_displacements['y'], rtol=5e-2, atol=0 )
+    assert(isinstance(test_output['x'], u.Quantity) and test_output['x'].unit == 'arcsec')
+    assert(isinstance(test_output['y'], u.Quantity) and test_output['y'].unit == 'arcsec')
 
     # Test returning using the "with_displacements" option
     test_output = mapcube_solar_derotate(mc, with_displacements=True)
@@ -31,4 +33,6 @@ def test_mapcube_solar_derotate():
     assert(isinstance(test_output[1], dict))
     assert_allclose(test_output[1]['x'], known_displacements['x'], rtol=5e-2, atol=0)
     assert_allclose(test_output[1]['y'], known_displacements['y'], rtol=5e-2, atol=0 )
+    assert(isinstance(test_output[1]['x'], u.Quantity) and test_output[1]['x'].unit == 'arcsec')
+    assert(isinstance(test_output[1]['y'], u.Quantity) and test_output[1]['y'].unit == 'arcsec')
 
