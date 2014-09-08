4c950894ee5673e6f2434a7266a806d6145bd7c4
Steven Christe <steven.d.christe@nasa.gov>
2014-09-08 11:27:18 -0400

    update to use astropy downloader

--- sunpy/data/sample/__init__.py
+++ sunpy/data/sample/__init__.py
@@ -2,75 +2,94 @@
 from __future__ import absolute_import
 import sunpy
 import os
+from os.path import join, splitext
+from os import rename, remove
+from astropy.utils.data import download_file
 from zipfile import ZipFile
-from os import remove
-from os.path import exists, expanduser, isdir, join, splitext
+import urllib2
 
-__author__ = "Keith Hughitt"
-__email__ = "keith.hughitt@nasa.gov"
+__author__ = "Steven Christe"
+__email__ = "steven.christe@nasa.gov"
+
 
 rootdir = os.path.join(os.path.dirname(sunpy.__file__), "data", "sample")
 
-def download(progress=True):
-    """
-    Download the sample data.
-    """
-    
-    base_url = 'https://s3.amazonaws.com/bokeh_data/'
-    
-    files = [
+_base_urls = ('http://data.sunpy.org/sample-data/', 'http://hesperia.gsfc.nasa.gov/~schriste/sunpy-sample-data/')
+
+_files = [
         "AIA20110319_105400_0171.fits",
         "hsi_image_20101016_191218.fits",
         "eit_l1_20020625_100011.fits",
         "BIR_20110922_103000_01.fit",
         "hsi_calib_ev_20020220_1106_20020220_1106_25_40.fits",
-        "swap_lv1_20120101_001607.fits"
+        "swap_lv1_20120101_001607.fits",
+        "aia.lev1.193A_2013-09-21T16_00_06.84Z.image_lev1.fits.zip"
         ]
 
-    for file_name in files:
-        _getfile(base_url, file_name, data_dir, progress=progress)
+def download(progress=True):
+    """
+    Download the sample data.
+    
+    Parameters
+    ----------
+    progress: bool
+        Show a progress bar during download
+    
+    Returns
+    -------
+    None
+    """
+    
+    for base_url in _base_urls:
+        for file_name in _files:
+            if url_exists(join(base_url, file_name)):
+                f = download_file(join(base_url, file_name))
+                
+                real_name, ext = splitext(file_name)
+                
+                if ext == '.zip':
+                    print("Unpacking: %s" % real_name)
+                    with ZipFile(f, 'r') as zip_file:
+                        zip_file.extract(real_name, rootdir)
+                    remove(f)
+                else:
+                    # move files to the data directory
+                    rename(f, join(rootdir, file_name))
+                    
+def url_exists(url, timeout=2):
+    """
+    Checks whether a url is online.
 
-def _getfile(base_url, file_name, data_dir, progress=True):
-    file_url = join(base_url, file_name)
-    file_path = join(data_dir, file_name)
+    Parameters
+    ----------
+    url: str
+        A string containing a URL
 
-    url = urlopen(file_url)
+    Returns
+    -------
+    value: bool
 
-    with open(file_path, 'wb') as file:
-        file_size = int(url.headers["Content-Length"])
-        print("Downloading: %s (%d bytes)" % (file_name, file_size))
+    Examples
+    --------
+    >>> from sunpy.net.helio import parser
+    >>> url_exists('http://www.google.com')
+    True
+    >>> url_exists('http://aslkfjasdlfkjwerf.com')
+    False
+    """
+    try:
+        urllib2.urlopen(url, timeout=timeout)
+    except urllib2.HTTPError, e:
+        #print(url)
+        #print(e.reason)
+        return False
+    except urllib2.URLError, e:
+        #print(url)
+        #print(e.reason)
+        return False
+    else:
+        return True
 
-        fetch_size = 0
-        block_size = 16384
-
-        while True:
-            data = url.read(block_size)
-            if not data:
-                break
-
-            fetch_size += len(data)
-            file.write(data)
-
-            if progress:
-                status = "\r%10d [%6.2f%%]" % (fetch_size, fetch_size*100.0/file_size)
-                stdout.write(status)
-                stdout.flush()
-
-    if progress:
-        print()
-
-    real_name, ext = splitext(file_name)
-
-    if ext == '.zip':
-        if not splitext(real_name)[1]:
-            real_name += ".csv"
-
-        print("Unpacking: %s" % real_name)
-
-        with ZipFile(file_path, 'r') as zip_file:
-            zip_file.extract(real_name, data_dir)
-
-        remove(file_path)
 #
 # AIA20110319_105400_0171.fits
 #
