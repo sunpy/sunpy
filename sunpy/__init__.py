a43072740b7742f12fb64bea7c2d5f69499e41ab
Steven Christe <steven.d.christe@nasa.gov>
2014-09-09 10:14:40 -0400

    file shortcuts now created automatically

--- sunpy/__init__.py
+++ sunpy/__init__.py
@@ -7,7 +7,7 @@ An open-source Python library for Solar Physics data analysis.
 Web Links
 ---------
 Homepage: http://www.sunpy.org
-Documentation: http://sunpy.readthedocs.org/en/latest/index.html
+Documentation: http://docs.sunpy.org
 """
 from __future__ import absolute_import
 
@@ -25,9 +25,10 @@ from sunpy.util.config import load_config, print_config
 from sunpy.util import system_info
 from sunpy.tests import main as self_test
 
-# Sample data
-from sunpy.data.sample import (AIA_171_IMAGE, RHESSI_IMAGE, EIT_195_IMAGE,
-                               RHESSI_EVENT_LIST, CALLISTO_IMAGE)
-
 # Load user configuration
 config = load_config()
+
+# Create shortcuts to sample data
+import sunpy.data.sample
+for key in sunpy.data.sample.sample_files:
+    setattr(sunpy.data.sample, key, sunpy.data.sample.sample_files[key])
