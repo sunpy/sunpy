Fix IRIS rotation test

sunpy/instr/tests/test_iris.py
@@ -27,7 +27,7 @@ def test_iris_rot():
     assert isinstance(irismap_rot, sunpy.map.sources.IRISMap)
     assert not np.allclose(irismap_rot.data, irismap.data)
 
-    assert irismap_rot.meta['pc1_1'] == 1
-    assert irismap_rot.meta['pc1_2'] == 0
-    assert irismap_rot.meta['pc2_1'] == 0
-    assert irismap_rot.meta['pc2_2'] == 1
+    np.testing.assert_allclose(irismap_rot.meta['pc1_1'], 1)
+    np.testing.assert_allclose(irismap_rot.meta['pc1_2'], 0, atol=1e-7)
+    np.testing.assert_allclose(irismap_rot.meta['pc2_1'], 0, atol=1e-7)
+    np.testing.assert_allclose(irismap_rot.meta['pc2_2'], 1)