006a36320f92129ad75b6d45d4a088221ce7128b
Stuart Mumford <stuart@mumford.me.uk>
2014-11-06 10:32:14 +0000

    Allow equivalencies and 100% test coverage

--- sunpy/util/quantity_decorator.py
+++ sunpy/util/quantity_decorator.py
@@ -18,14 +18,15 @@ def quantity_input(*f_args, **f_kwargs):
         solarx.value # this is now in arcsec
         solary.value # now in arcsec
     """
-    
+    equivalencies = f_kwargs.pop('equivalencies', [])
     def check_quantities(f):
         # Number of args in decorator must equal number of args in function
         if f.func_defaults:
             num_kwargs = len(f.func_defaults)
         else:
             num_kwargs = 0
-        if len(f_args) != f.func_code.co_argcount - num_kwargs:
+        num_args = f.func_code.co_argcount - num_kwargs
+        if len(f_args) != num_args:
             raise ValueError("Number of decorator arguments does not equal number of function arguments")
 
         def new_f(*args, **kwds):
@@ -34,7 +35,7 @@ def quantity_input(*f_args, **f_kwargs):
             for i, (arg, f_arg) in enumerate(zip(args, f_args)):
                 if f_arg is not None:
                     try:
-                        arg.to(f_arg)
+                        arg.to(f_arg, equivalencies=equivalencies)
                     except u.UnitsError:
                         raise TypeError("Argument '{}' to function '{}' must be in units convertable to '{}'.".format(
                                         f.func_code.co_varnames[i], f.func_code.co_name, f_arg.to_string()))
@@ -43,16 +44,16 @@ def quantity_input(*f_args, **f_kwargs):
                                         f.func_code.co_varnames[i], f.func_code.co_name))
 
             # Check kwargs, only kwargs specified in the decorator are modified
-            for kwarg, value in f_kwargs.items():
+            for j, (kwarg, value) in enumerate(f_kwargs.items()):
                 if kwarg in kwds:
                     try:
-                        kwds[kwarg].to(value)
+                        kwds[kwarg].to(value, equivalencies=equivalencies)
                     except u.UnitsError:
-                        raise TypeError("Keyword argument '{}' to function '{}' must be an astropy Quantity object in units convertable to '{}'.".format(
+                        raise TypeError("Keyword argument '{}' to function '{}' must be in units convertable to '{}'.".format(
                                         kwarg, f.func_code.co_name, value.to_string()))
                     except AttributeError:
                         raise TypeError("Argument '{}' to function '{}' must be an astropy Quantity object".format(
-                                        f.func_code.co_varnames[i], f.func_code.co_name))
+                                        f.func_code.co_varnames[j+num_args], f.func_code.co_name))
 
             return f(*args, **kwds)
 
--- sunpy/util/tests/test_quantity_decorator.py
+++ sunpy/util/tests/test_quantity_decorator.py
@@ -5,6 +5,12 @@ import pytest
 import astropy.units as u
 from sunpy.util.quantity_decorator import quantity_input
 
+def test_wrong_num_args():
+    with pytest.raises(ValueError) as e:
+        @quantity_input(u.arcsec)
+        def myfunc_args(solarx, solary):
+            return solarx, solary
+    assert e.value.message == "Number of decorator arguments does not equal number of function arguments"
 
 def test_args():
     @quantity_input(u.arcsec, u.arcsec)
@@ -44,6 +50,19 @@ def test_args_nonquantity():
     assert isinstance(solary, int)
     
     assert solarx.unit == u.arcsec
+
+def test_arg_equivalencies():
+    @quantity_input(u.arcsec, u.eV, equivalencies=u.mass_energy())
+    def myfunc_args(solarx, solary):
+        return solarx, solary
+    
+    solarx, solary = myfunc_args(1*u.arcsec, 100*u.gram)
+    
+    assert isinstance(solarx, u.Quantity)
+    assert isinstance(solary, u.Quantity)
+    
+    assert solarx.unit == u.arcsec
+    assert solary.unit == u.gram
 
 def test_wrong_unit():
     @quantity_input(u.arcsec, u.deg)
@@ -90,3 +109,35 @@ def test_unused_kwargs():
 
     assert myk.unit == u.deg
     assert myk2 == 10
+
+
+def test_kwarg_equivalencies():
+    @quantity_input(u.arcsec, energy=u.eV, equivalencies=u.mass_energy())
+    def myfunc_args(solarx, energy=10*u.eV):
+        return solarx, energy
+    
+    solarx, energy = myfunc_args(1*u.arcsec, 100*u.gram)
+    
+    assert isinstance(solarx, u.Quantity)
+    assert isinstance(energy, u.Quantity)
+    
+    assert solarx.unit == u.arcsec
+    assert energy.unit == u.gram
+
+def test_kwarg_wrong_unit():
+    @quantity_input(u.arcsec,solary=u.deg)
+    def myfunc_args(solarx, solary=10*u.deg):
+        return solarx, solary
+   
+    with pytest.raises(TypeError) as e:
+        solarx, solary = myfunc_args(1*u.arcsec, solary=100*u.km)
+    assert e.value.message == "Keyword argument 'solary' to function 'myfunc_args' must be in units convertable to 'deg'."
+
+def test_kwarg_not_quantity():
+    @quantity_input(u.arcsec, solary=u.deg)
+    def myfunc_args(solarx, solary=10*u.deg):
+        return solarx, solary
+   
+    with pytest.raises(TypeError) as e:
+        solarx, solary = myfunc_args(1*u.arcsec, solary=100)
+    assert e.value.message == "Argument 'solary' to function 'myfunc_args' must be an astropy Quantity object"
