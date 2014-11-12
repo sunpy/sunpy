162c4251da5fed3eec6f785dcd386c5b95b807c7
Stuart Mumford <stuart@mumford.me.uk>
2014-11-12 10:48:12 +0000

    Quantity decorator as a class

--- sunpy/util/quantity_decorator.py
+++ sunpy/util/quantity_decorator.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import inspect
 
 import astropy.units as u
 
-def quantity_input(*f_args, **f_kwargs):
+
+class QuantityInput(object):
     """
     A decorator for a function that accepts some inputs as Quantity objects.
     
@@ -13,55 +15,57 @@ def quantity_input(*f_args, **f_kwargs):
     Examples
     --------
     
-    @quantity_input(u.arcsec, u.arcsec, None)
+    @QuantityInput(u.arcsec, u.arcsec, None)
     def myfunc(solarx, solary, someflag):
         pass
     """
-    equivalencies = f_kwargs.pop('equivalencies', [])
-    def check_quantities(f):
-        # Number of args in decorator must equal number of args in function
-        if f.func_defaults:
-            num_kwargs = len(f.func_defaults)
-        else:
-            num_kwargs = 0
-        num_args = f.func_code.co_argcount - num_kwargs
-        if len(f_args) != num_args:
-            raise ValueError("Number of decorator arguments does not equal number of function arguments")
+    # __init__ is called when the function is parsed, it creates the decorator
+    def __init__(self, **kwargs):
+        self.equivalencies = kwargs.pop('equivalencies', [])
+        self.f_kwargs = kwargs
 
-        def new_f(*args, **kwds):
-            # Check args, number of args in decorator must equal number of args in function
-            args = list(args)
-            for i, (arg, f_arg) in enumerate(zip(args, f_args)):
-                if f_arg is not None:
-                    try:
-                        arg.to(f_arg, equivalencies=equivalencies)
-                    except u.UnitsError:
-                        raise TypeError(
+    # __call__ is called when the wrapped function is called.
+    def __call__(self, wrapped_function):
+        
+        #Define a new function to return in place of the wrapped one
+        def wrapper(*func_args, **func_kwargs):
+            
+            args, varargs, keywords, defaults = inspect.getargspec(wrapped_function)
+
+            for name, target_unit in self.f_kwargs.items():
+                # We now parse the output of inspect to get the value of the
+                # arg or the kwarg.
+                if name in args:
+                    loc = args.index(name)
+                    # args includes names of kwargs which we want to skip here.
+                    if loc < len(func_args):
+                        arg = func_args[loc]
+
+                elif name in func_kwargs or loc >= len(func_args):
+                    arg = func_kwargs[name]
+                
+                else:
+                    raise ValueError(
+                        "Argument {0} is not a argument to wrapped function.".format(name))
+                
+                # Now we have the arg or the kwarg we check to see if it is 
+                # convertable to the unit specified in the decorator.
+                try:
+                    arg.to(target_unit, equivalencies=self.equivalencies)
+
+                # A UnitsError is raised if the conversion is not possible
+                except u.UnitsError:
+                    raise TypeError(
 "Argument '{0}' to function '{1}' must be in units convertable to '{2}'.".format(
-            f.func_code.co_varnames[i], f.func_code.co_name, f_arg.to_string()))
-                    except AttributeError:
-                        raise TypeError(
+            name, wrapped_function.func_code.co_name, target_unit.to_string()))
+
+                # AttributeError is raised if there is no `to` method.
+                # i.e. not something that quacks like a Quantity.
+                except AttributeError:
+                    raise TypeError(
 "Argument '{0}' to function '{1}' must be an astropy Quantity object".format(
-                                        f.func_code.co_varnames[i], f.func_code.co_name))
+                                     name, wrapped_function.func_code.co_name))
 
-            # Check kwargs, only kwargs specified in the decorator are modified
-            for j, (kwarg, value) in enumerate(f_kwargs.items()):
-                if kwarg in kwds:
-                    try:
-                        kwds[kwarg].to(value, equivalencies=equivalencies)
-                    except u.UnitsError:
-                        raise TypeError(
-"Keyword argument '{0}' to function '{1}' must be in units convertable to '{2}'.".format(
-                                kwarg, f.func_code.co_name, value.to_string()))
-                    except AttributeError:
-                        raise TypeError(
-"Argument '{0}' to function '{1}' must be an astropy Quantity object".format(
-                    f.func_code.co_varnames[j+num_args], f.func_code.co_name))
-
-            return f(*args, **kwds)
-
-
-        new_f.func_name = f.func_name
-        return new_f
-
-    return check_quantities
+            return wrapped_function(*args, **func_kwargs)
+        
+        return wrapper
