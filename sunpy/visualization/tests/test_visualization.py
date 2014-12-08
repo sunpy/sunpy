b24b975fd882f5c1ca164f90aba12a7a7818cdc9
Rajul <rajul09@gmail.com>
2014-12-08 18:27:13 +0530

    Added tests for the sunpy.visualization.visualization

--- sunpy/visualization/tests/test_visualization.py
+++ sunpy/visualization/tests/test_visualization.py
@@ -5,7 +5,31 @@
 
 from __future__ import absolute_import
 
+from matplotlib import pyplot
+
+from sunpy.visualization import visualization
 
 
 __authors__ = ["Rajul"]
 __email__ = "rajul09@gmail.com"
+
+
+def test_toggle_pylab():
+	def dummy_function(dummy_arg='foo'):
+		return dummy_arg
+
+	assert pyplot.isinteractive() == False
+
+	fn = visualization.toggle_pylab(dummy_function)
+	assert fn == dummy_function
+	assert fn() == 'foo'
+	assert fn('bar') == 'bar'
+
+	pyplot.ion()
+	assert pyplot.isinteractive() == True
+	
+	fn_toggle = visualization.toggle_pylab(dummy_function)
+	assert fn == dummy_function
+	assert fn() == 'foo'
+	assert fn('bar') == 'bar'
+	assert pyplot.isinteractive() == True
