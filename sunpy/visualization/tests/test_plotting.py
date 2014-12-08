2276514dd8dc5e11c69be2c24c788c3b5791eb5e
Rajul <rajul09@gmail.com>
2014-12-08 17:59:10 +0530

    Created test files and added headers

---
+++ sunpy/visualization/tests/test_plotting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Author: Rajul <rajul@gmail.com>
+
+#pylint: disable=W0613
+
+from __future__ import absolute_import
+
+
+
+__authors__ = ["Rajul"]
+__email__ = "rajul09@gmail.com"
