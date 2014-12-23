1963c3de963d5cf87bd71887418043192bcbc12e
Stuart Mumford <stuart@mumford.me.uk>
2014-12-23 09:56:56 +0000

    Import quantity_input from astropy if it exists.

--- sunpy/util/__init__.py
+++ sunpy/util/__init__.py
@@ -4,4 +4,9 @@ from __future__ import absolute_import
 from sunpy.util.util import *
 from sunpy.util.sysinfo import *
 
-from . unit_decorators import quantity_input
+import astropy.units
+
+try:
+    from astropy.units import quantity_input
+except ImportError:
+    from . unit_decorators import quantity_input
