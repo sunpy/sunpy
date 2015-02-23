10b784ec6138bd0df6c74172bc232ea6b4be5935
Stuart Mumford <stuart@mumford.me.uk>
2015-02-23 16:38:06 +0000

    backport int and nan casting

--- sunpy/image/transform.py
+++ sunpy/image/transform.py
@@ -16,7 +16,6 @@ except ImportError:  # pragma: no cover
 
 __all__ = ['affine_transform']
 
-
 def affine_transform(image, rmatrix, order=3, scale=1.0, image_center=None,
                      recenter=False, missing=0.0, use_scipy=False):
     """
@@ -32,13 +31,12 @@ def affine_transform(image, rmatrix, order=3, scale=1.0, image_center=None,
         Linear transformation rotation matrix.
     order : int 0-5
         Interpolation order to be used. When using scikit-image this parameter
-        is passed into :func:`skimage.transform.warp`.
-        The default value of order 3 is a bicubic interpolation when using
-        :func:`skimage.transform.warp` and a cubic spline interpolation
-        when using :func:`scipy.ndimage.interpolation.affine_transform`.
+        is passed into :func:`skimage.transform.warp` (e.g., 3 corresponds to
+        bi-cubic interpolation).
         When using scipy it is passed into
         :func:`scipy.ndimage.interpolation.affine_transform` where it controls
         the order of the spline.
+        Default: 3
     scale : float
         A scale factor for the image. Default is no scaling.
     image_center : tuple
@@ -60,6 +58,10 @@ def affine_transform(image, rmatrix, order=3, scale=1.0, image_center=None,
 
     Notes
     -----
+    This function will cast any integer input data to float64, it is possible to
+    manually undo this by casting back with `numpy.ndarray.astype`, however to avoid
+    possible loss of precision this is not done automatically.
+
     This function is used throughout the SunPy code base as an equivalent to
     the IDL's rot() function. However, this function does not use the same
     algorithm as the IDL rot() function.
@@ -132,13 +134,13 @@ def affine_transform(image, rmatrix, order=3, scale=1.0, image_center=None,
         adjusted_image -= im_min
         im_max = np.nanmax(adjusted_image)
         adjusted_image /= im_max
+        adjusted_missing = (missing - im_min) / im_max
         rotated_image = skimage.transform.warp(adjusted_image, tform, order=order,
-                                               mode='constant', cval=missing)
+                                               mode='constant', cval=adjusted_missing)
 
         rotated_image *= im_max
         rotated_image += im_min
 
         if rotated_image.dtype != image.dtype:
-            rotated_image = rotated_image.astype(image.dtype)
-
+            warnings.warn("Rotating this image has change it's dtype", RuntimeWarning)
     return rotated_image
