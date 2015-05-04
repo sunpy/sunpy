e24817cc4a35211891c535092af7196d3dab0517
Steven Christe <steven.d.christe@nasa.gov>
2015-05-03 23:25:54 -0400

    fixed rhessi colormap

--- sunpy/map/sources/rhessi.py
+++ sunpy/map/sources/rhessi.py
@@ -4,6 +4,8 @@
 __author__ = "Steven Christe"
 __email__ = "steven.d.christe@nasa.gov"
 
+import matplotlib.pyplot as plt
+
 from sunpy.map import GenericMap
 
 import astropy.units as u
@@ -39,6 +41,7 @@ class RHESSIMap(GenericMap):
 
         self.meta['waveunit'] = 'keV'
         self.meta['wavelnth'] = [self.meta['energy_l'], self.meta['energy_h']]
+        self.cmap = plt.get_cmap('jet')
 
     @property
     def measurement(self):
