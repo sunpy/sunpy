b99e65e48b5ba13cbc33f03e80c27915f02bdb57
Ankit Kumar <ankitkmr@users.noreply.github.com>
2015-06-10 17:58:23 +0530

    Refining HET _parse_txt routine code

--- sunpy/lightcurve/sources/het.py
+++ sunpy/lightcurve/sources/het.py
@@ -1,6 +1,8 @@
 from datetime import timedelta,datetime
+
 from astropy.io import ascii
-from astropy.table import Table, Column
+from astropy.table import Column
+
 
 def _parse_txt(filepath):
     """
@@ -9,7 +11,7 @@ def _parse_txt(filepath):
 
     """
     #Reading in Data only
-    data = ascii.read(filepath, delimiter = "\s", data_start = 20) 
+    data = ascii.read(filepath, delimiter = "\s", data_start = 20)
 
     #Determining the type of data from the number of columns in data
     number_of_columns = len(data.colnames)
@@ -17,7 +19,7 @@ def _parse_txt(filepath):
         type_of_data = '1min'
     else:
         type_of_data = 'other'
-
+ 
     #Header
     header = [  'Column 2: Electron flux, 0.7-1.4 MeV, particles/(cm2-sr-sec-MeV)', 
                 'Column 3: Uncertainty (sigma) for 0.7-1.4 MeV electron flux',
@@ -52,44 +54,53 @@ def _parse_txt(filepath):
     data_modify = []
     month_dict = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
 
+    #Storing data columns in recognizable variables (Common to all types of data)
+    start_year_col  = data['col2']
+    start_month_col = data['col3']
+    start_date_col  = data['col4']
+    start_time_col  = data['col5']
 
     #Adding Time Column based on type of data
     if type_of_data == '1min':
-        header = ['Verse Number', 'DateTime'] + header
-        
-        #Converting start datetime separate columns into a single datetime column
-        for i in range(len(data)): 
-            date = datetime(data['col2'][i], month_dict[ data['col3'][i] ], data['col4'][i], int(("%04d" % (data['col5'][i],))[:2]), int(("%04d" % (data['col5'][i],))[2:]) )
-            data_modify = data_modify + [date]
+        header = ['Verse Number', 'Column 1 : DateTime'] + header 
 
-        data.remove_columns(['col2','col3','col4','col5'])
+        for i in range(len(data)): 
+            date = datetime(start_year_col[i], month_dict[ start_month_col[i] ], start_date_col[i], int(("%04d" % (start_time_col[i],))[:2]), int(("%04d" % (start_time_col[i],))[2:]) )
+            data_modify.append(date)
+
+        data.remove_columns(['col{}'.format(i) for i in range(2,6)])
     else:
-        header = ['Verse Number','TimeRange'] + header
+        header = ['Verse Number','Column 1 : TimeRange'] + header
+        
+        #Storing data columns in recognizable variables
+        end_year_col    = data['col6']
+        end_month_col   = data['col7']
+        end_date_col    = data['col8']
+        end_time_col    = data['col9']
 
-        #Converting start datetime and end datetime separate columns into a single TimeRange column
         for i in range(len(data)): 
-            date1 = datetime(data['col2'][i], month_dict[ data['col3'][i] ], data['col4'][i], int(("%04d" % (data['col5'][i],))[:2]), int(("%04d" % (data['col5'][i],))[2:]) )
-            date2 = datetime(data['col6'][i], month_dict[ data['col7'][i] ], data['col8'][i], int(("%04d" % (data['col9'][i],))[:2]), int(("%04d" % (data['col9'][i],))[2:]) )
+            date1 = datetime(start_year_col[i], month_dict[ start_month_col[i] ], start_date_col[i], int(("%04d" % (start_time_col[i],))[:2]), int(("%04d" % (start_time_col[i],))[2:]) )
+            date2 = datetime(end_year_col[i], month_dict[ end_month_col[i] ], end_date_col[i], int(("%04d" % (end_time_col[i],))[:2]), int(("%04d" % (end_time_col[i],))[2:]) )
             
             #I don't know why but including it outside interferes with the upper case and results in segmetation fault 11 :/
             from sunpy.time import TimeRange
-            data_modify = data_modify + [TimeRange(date1,date2)]
+            data_modify.append(TimeRange(date1,date2))
 
-        data.remove_columns(['col2','col3','col4','col5','col6','col7','col8','col9'])
+        data.remove_columns(['col{}'.format(i) for i in range(2,10)])
 
     data.add_column(Column(data = data_modify, name='col'),1)
 
     #To add the column names in the astropy table object
-    for key2 in range(len(data.colnames)):
-        data.rename_column(data.colnames[key2], header[key2])        
+    for elem, head_key in enumerate(header):
+        data.rename_column(data.colnames[elem], head_key)        
 
     # Converting from astropy.table.Table to pandas.Dataframe
     # to_pandas() bound method is only available in the latest development build and none of the stable
     data = data.to_pandas()
-    
-    return header, data
 
-"""
+    return data
+
+""""
 _parse_txt('AeH07Apr.1m.txt')
 _parse_txt('AeH06Dec.15m.txt') 
 
