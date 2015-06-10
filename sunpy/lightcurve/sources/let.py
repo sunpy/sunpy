8afa66548ec6cb2546ae45fed7ccefb9398b517a
Ankit Kumar <ankitkmr@users.noreply.github.com>
2015-06-10 14:40:31 +0530

    Refining LET _parse_txt routine code
    This function is intended to read in data from LET data file stored on hard disk and return the header as a list and data as pandas dataframe. I'll implement the class structure around it soon.

--- sunpy/lightcurve/sources/let.py
+++ sunpy/lightcurve/sources/let.py
@@ -1,4 +1,5 @@
 from datetime import timedelta,datetime
+
 from astropy.io import ascii
 from astropy.table import Table, Column
 
@@ -7,25 +8,22 @@ def _parse_txt(filepath):
     Parses a STEREO LET file from
     http://www.srl.caltech.edu/STEREO/Public/LET_public.html
 
-    """
-    #Reading in Header  
-
+    """ 
     type_of_data = ''
     header = []
-    flag1 = -1
-    flag2 = -1
+    data_read = {'data_type_read': False, 'header_line_reached': False }
 
     #To Read in header and get index of line where data starts
     fp = open(filepath)
     for i,line in enumerate(fp):
-        #To Find the typr of data, 27 day, 1 day, 1 hour, 10 min or 1 min
-        if flag1 == -1:
+        #To Find the type of data, 27 day, 1 day, 1 hour, 10 min or 1 min
+        if data_read['data_type_read'] == False:
             if i == 2 and line[:3] == 'Bin':
                 type_of_data = '27day'
-                flag1 = 1
+                data_read['data_type_read'] = True
             elif i in [5,6] and line[:4] == 'Time': 
                 type_of_data = line[17:].strip('\n')
-                flag1 = 2
+                data_read['data_type_read'] = True
             else:
                 continue
 
@@ -48,11 +46,11 @@ def _parse_txt(filepath):
         # To figure out and customise header in case of any other type of data since their files are formatted 
         # similarly but different from 27 day type of data
         else:
-            if flag2 == -1 and line[:6] != 'Column':
+            if data_read['header_line_reached'] == False and line[:6] != 'Column':
                 continue
             elif line[:6] == 'Column':
                 header = header + [line]
-                flag2 = 0
+                data_read['header_line_reached'] = True
                 continue
             elif line == 'BEGIN DATA\n':
                 data_start = i+1
@@ -62,28 +60,39 @@ def _parse_txt(filepath):
     #Reading in Data only, using default i.e. 0 value for data_start keyword since all lines before data are commented
     data = ascii.read(filepath, delimiter = "\s", data_start = data_start) 
    
-    data_modify = []
-   
 
+    #Storing data columns in recognizable variables (Common to all types of data)
+    year_col = data['col1']
+    day_of_year_col = data['col2']
+   
+    data_modify = []
     #Converting separate datetime element into a single datetime.datetime column
     if type_of_data == '27day':
         for i in range(len(data)):
-            data_modify = data_modify + [datetime(data['col1'][i],1,1) + timedelta(days = data['col2'][i]) ]
+            data_modify.append(datetime(year_col[i],1,1) + timedelta(days = day_of_year_col[i]) )
         
-        data.remove_columns(['col1','col2'])
+        data.remove_columns(['col{}'.format(i) for i in range(1,3)])
         data.add_column(Column(data = data_modify, name='Datetime'),0)
-        
-    else: 
+    
+    #For all other types of data since they share the same format    
+    else:
+        #Storing data columns in recognizable variables
+        hour_col = data['col3']
+        minutes_col = data['col4']
+        seconds_col = data['col5']
+
         for i in range(len(data)):
-            data_modify = data_modify + [datetime(data['col1'][i],1,1,data['col3'][i],data['col4'][i],data['col5'][i]) + timedelta(days = int(data['col2'][i]-1)) ]
+            data_modify.append(datetime(year_col[i],1,1,hour_col[i],minutes_col[i],seconds_col[i]) + timedelta(days = int(day_of_year_col[i]-1)))
+
         header = ['Datetime'] + header[5:]
-        data.remove_columns(['col1','col2','col3','col4','col5'])
+
+        data.remove_columns(['col{}'.format(i) for i in range(1,6)])
         data.add_column(Column(data = data_modify, name='Datetime'),0)
     
     # Converting from astropy.table.Table to pandas.Dataframe
     # to_pandas() bound method is only available in the latest development build and none of the stable
     data = data.to_pandas()
-
+    
     return header, data
 
 """
