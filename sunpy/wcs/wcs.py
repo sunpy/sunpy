a54b036067a1155bba0c446c27ef52c204a0fd61
Brigitta Sipocz <bsipocz@gmail.com>
2015-06-17 02:43:55 +0100

    Fixing doctests in sunpy/wcs/wcs.py

--- sunpy/wcs/wcs.py
+++ sunpy/wcs/wcs.py
@@ -165,6 +165,7 @@ def convert_hpc_hcc(x, y, dsun_meters=None, angle_units='arcsec', z=False):
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hpc_hcc(40.0, 32.0, z=True)
     (28876152.176423457, 23100922.071266972, 694524220.8157959)
 
@@ -221,6 +222,7 @@ def convert_hcc_hpc(x, y, dsun_meters=None, angle_units='arcsec'):
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hcc_hpc(28748691, 22998953)
     (39.823439773829705, 31.858751644835717)
 
@@ -284,6 +286,7 @@ def convert_hcc_hg(x, y, z=None, b0_deg=0, l0_deg=0, radius=False):
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hcc_hg(230000.0,45000000.0,
     z=695508000.0 + 8000000.0, radius=True)
     (0.01873188196651189, 3.6599471896203317, 704945784.41465974)
@@ -338,6 +341,7 @@ def convert_hg_hcc(hglon_deg, hglat_deg, b0_deg=0, l0_deg=0, occultation=False,
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hg_hcc(0.01873188196651189, 3.6599471896203317,
     r=704945784.41465974, z=True)
     (230000.0, 45000000.0, 703508000.0)
@@ -402,6 +406,7 @@ def convert_hg_hpc(hglon_deg, hglat_deg, b0_deg=0, l0_deg=0, dsun_meters=None, a
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hg_hpc(34.0, 45.0, b0_deg=-7.064078, l0_deg=0.0)
     (380.05656560308898, 743.78281283290016)
     """
@@ -440,6 +445,7 @@ def convert_hpc_hg(x, y, b0_deg=0, l0_deg=0, dsun_meters=None, angle_units='arcs
 
     Examples
     --------
+    >>> import sunpy.wcs
     >>> sunpy.wcs.convert_hpc_hg(382, 748, b0_deg=-7.064078, l0_deg=0.0)
     (34.504653439914669, 45.443143275518182)
     """
