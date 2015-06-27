d42785e62923e0f90b28315b5c024b86a2dd3a95
Steven Christe <steven.d.christe@nasa.gov>
2015-06-26 23:29:08 -0400

    fixed bad merge

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -102,13 +102,7 @@ intersphinx_mapping['astropy'] = ('http://docs.astropy.org/en/stable/', None)
 intersphinx_mapping['sqlalchemy'] = ('http://docs.sqlalchemy.org/en/rel_0_8/', None)
 intersphinx_mapping['pandas'] = ('http://pandas.pydata.org/pandas-docs/stable/', None)
 intersphinx_mapping['skimage'] = ('http://scikit-image.org/docs/stable/', None)
-<<<<<<< HEAD
-intersphinx_mapping['numpy'] = ('http://docs.scipy.org/doc/numpy/', None)
-intersphinx_mapping['scipy'] = ('http://docs.scipy.org/doc/scipy/reference/', None)
-intersphinx_mapping['matplotlib'] = ('http://matplotlib.sourceforge.net/', None)
-=======
 intersphinx_mapping['wcsaxes'] = ('http://wcsaxes.readthedocs.org/en/stable/', None)
->>>>>>> upstream/master
 
 # -- Options for HTML output ---------------------------------------------------
 
