859a30cedbf28ee35e65c5128a72417da71d233f
Ankit Kumar <ankitkmr@users.noreply.github.com>
2015-06-30 14:26:02 +0530

    updated for stereo and soho
    updated for stereo and soho instruments

--- sunpy/net/dataretriever/clients.py
+++ sunpy/net/dataretriever/clients.py
@@ -9,6 +9,10 @@ from .sources.goes import GOESClient
 from .sources.norh import NoRHClient
 from .sources.rhessi import RHESSIClient
 from .sources.noaa import NOAAIndicesClient, NOAAPredictClient
+from .sources.url_stereo import SEPTClient, HETClient, SITClient, PLASTICClient, MAGClient, LETClient
+from .sources.url_soho import ERNEClient
+
+
 
 _Fido.register(EVEClient, EVEClient._can_handle_query)
 _Fido.register(LYRAClient, LYRAClient._can_handle_query)
@@ -17,6 +21,14 @@ _Fido.register(NoRHClient, NoRHClient._can_handle_query)
 _Fido.register(NOAAIndicesClient, NOAAIndicesClient._can_handle_query)
 _Fido.register(NOAAPredictClient, NOAAPredictClient._can_handle_query)
 _Fido.register(RHESSIClient, RHESSIClient._can_handle_query)
+_Fido.register(ERNEClient, ERNEClient._can_handle_query)
+_Fido.register(SEPTClient, SEPTClient._can_handle_query)
+_Fido.register(HETClient, HETClient._can_handle_query)
+_Fido.register(LETClient, LETClient._can_handle_query)
+_Fido.register(PLASTICClient, PLASTICClient._can_handle_query)
+_Fido.register(SITClient, SITClient._can_handle_query)
+# _Fido.register(MAGClient, MAGClient._can_handle_query)   **Just noticed I missed making its parse routine **
+
 
 
 # Import and register other sources
