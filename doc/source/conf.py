7092831c7aa1d448909f868acb5f28c56df704d0
Steven Christe <steven.christe@nasa.gov>
2015-07-07 10:00:57 -0400

    fixed a bad revision

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -63,6 +63,13 @@ except ImportError:
         if os.path.isdir(a_h_path):
             sys.path.insert(1, a_h_path)
 
+# -- Read the Docs Setup  -----------------------------------------------------
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:
+    os.environ['SUNPY_CONFIGDIR'] = '/home/docs/checkouts/readthedocs.org/user_builds/sunpy/'
+    os.environ['HOME'] = '/home/docs/checkouts/readthedocs.org/user_builds/sunpy/'
 
 # -- Download Sample Data -----------------------------------------------------
 
