c3a570ccb69e1b66c32e4f3db249a02a7ba142fa
ankitkmr <technocrat.ankit@gmail.com>
2015-07-11 11:41:04 +0530

    dataretriever/test_let.py
    ..
    
    ..

--- sunpy/net/dataretriever/tests/test_let.py
+++ sunpy/net/dataretriever/tests/test_let.py
@@ -43,7 +43,8 @@ def test_can_handle_query():
     assert ans3 == False
 
 def test_query():
-    qr1 = LCClient.query(Time(TimeRange('2012/8/9', '2012/8/10')), Instrument('stereo/let'))
+    qr1 = LCClient.query(Time(TimeRange('2012/8/9', '2012/8/10')), Instrument('stereo/let'), specie = 'Al', 
+                                                    duration_of_average = 10*u.min, stereo_spacecraft ='ahead', type_of_data ='summed')
     assert isinstance(qr1,QueryResponse)
     assert len(qr1) == 1
     assert qr1.time_range()[0] == '2012/08/09'
@@ -51,11 +52,9 @@ def test_query():
 
 
 @pytest.mark.online
-@pytest.mark.parametrize("time, instrument",
-[(Time(TimeRange('2012/11/27', '2012/11/27')), Instrument('stereo/let'), specie = 'Al', 
-                            duration_of_average = 10*u.min, stereo_spacecraft ='ahead', type_of_data ='summed'),
- (Time(TimeRange('2012/10/4', '2012/10/6')), Instrument('stereo/let'),specie = 'CNO_hi', 
-                            duration_of_average = 10*u.min, stereo_spacecraft ='behind', type_of_data ='sectored'),
+@pytest.mark.parametrize("time, instrument, specie, duration_of_average, stereo_spacecraft, type_of_data",
+[(Time(TimeRange('2012/11/27', '2012/11/27')), Instrument('stereo/let'), 'Al', 10*u.min, 'ahead', 'summed'),
+ (Time(TimeRange('2012/10/4', '2012/10/6')), Instrument('stereo/let'),'CNO_hi', 10*u.min, 'behind', 'sectored'),
 ])
 def test_get(time,instrument,specie,duration_of_average,stereo_spacecraft,type_of_data):
     qr1 = LCClient.query(time,instrument,specie =specie, duration_of_average = duration_of_average, 
