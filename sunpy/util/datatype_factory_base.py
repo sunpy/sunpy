48ced67b77afcbbd0365cbcb1e63a233f7d3f45d
Stuart Mumford <stuart@cadair.com>
2015-10-06 16:03:41 +0100

    no unicode_literals

--- sunpy/util/datatype_factory_base.py
+++ sunpy/util/datatype_factory_base.py
@@ -1,6 +1,5 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-                        
+from __future__ import absolute_import, division, print_function
+
 class BasicRegistrationFactory(object):
     """ Generalized registerable factory type.
 
