10b083d1ed0a583a41db9113a218495d86873d14
Stuart Mumford <stuart@cadair.com>
2015-10-07 15:34:31 +0100

    add util tests to the python3.sh script

--- continuous-integration/python3.sh
+++ continuous-integration/python3.sh
@@ -3,6 +3,10 @@
 # This script runs a passing subset of tests under Python 3.
 
 python setup.py install
+
+python setup.py test -P time
+python setup.py test -P util
+
 python -c "import sunpy.data"
 python -c "import sunpy.data; sunpy.data.download_sample_data()"
 python -c "import sunpy.data.sample"
