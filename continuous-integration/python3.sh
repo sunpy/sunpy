197b41b6ef9590dc4e153abb978ca5903f4c6f1f
Stuart Mumford <stuart@cadair.com>
2015-10-16 13:08:20 +0100

    tidy up test script

--- continuous-integration/python3.sh
+++ continuous-integration/python3.sh
@@ -4,12 +4,10 @@
 
 python setup.py install
 
-python setup.py test -P time
-python setup.py test -P util
-
 python -c "import sunpy.data"
 python -c "import sunpy.data; sunpy.data.download_sample_data()"
 python -c "import sunpy.data.sample"
 
+python setup.py test -P util
 python setup.py test -P time
 
