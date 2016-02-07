a7b41a262f97a573c5ac0bc9da92cdec96115f16
Stuart Mumford <stuart@cadair.com>
2016-02-07 20:55:17 +0100

    yoink the transform graph picture from astropy

--- sunpy/coordinates/transformations.py
+++ sunpy/coordinates/transformations.py
@@ -163,3 +163,40 @@ def hgs_to_hcc(heliogcoord, heliocframe):
 
     representation = CartesianRepresentation(x.to(u.km), y.to(u.km), zz.to(u.km))
     return heliocframe.realize_frame(representation)
+
+
+# Make a transformation graph for the documentation, borrowed lovingly from
+# Astropy.
+
+def _make_transform_graph_docs():
+    """
+    Generates a string for use with the coordinate package's docstring
+    to show the available transforms and coordinate systems
+    """
+    import inspect
+    from textwrap import dedent
+    from sunpy.extern import six
+    from astropy.coordinates.baseframe import BaseCoordinateFrame, frame_transform_graph
+
+    # TODO: Make this just show the SunPy Frames
+    isclass = inspect.isclass
+    coosys = [item for item in list(six.itervalues(globals()))
+              if isclass(item) and issubclass(item, BaseCoordinateFrame)]
+    graphstr = frame_transform_graph.to_dot_graph(addnodes=coosys,
+                                                  priorities=False)
+
+    docstr = """
+    The diagram below shows all of the coordinate systems built into the
+    `~astropy.coordinates` package, their aliases (useful for converting
+    other coordinates to them using attribute-style access) and the
+    pre-defined transformations between them.  The user is free to
+    override any of these transformations by defining new transformations
+    between these systems, but the pre-defined transformations should be
+    sufficient for typical usage.
+
+    .. graphviz::
+
+    """
+
+    return dedent(docstr) + '    ' + graphstr.replace('\n', '\n    ')
+__doc__ += _make_transform_graph_docs()
