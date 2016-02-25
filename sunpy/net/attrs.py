0bedf221545c90041c9f0cfdde5f3929bd3efb72
Nabil Freij <nabil.freij@gmail.com>
2016-02-25 17:27:22 +0000

    SOHO ERNE fixes before it is pulled

--- sunpy/net/attrs.py
+++ sunpy/net/attrs.py
@@ -4,5 +4,6 @@ from .vso import attrs as vso
 from .jsoc import attrs as jsoc
 
 from .vso.attrs import Time, Instrument, Wavelength, Level
+from .dataretriever.attrs import Species
 
-__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'vso', 'jsoc']
+__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'Species', 'vso', 'jsoc']
