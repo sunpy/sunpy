ee7b53ed0ee88e28cb63cb2b724857b7c739b5d4
Nabil Freij <nabil.freij@gmail.com>
2016-02-25 18:02:31 +0000

    The Lord said unto me, Push to git blindly my son.

--- sunpy/net/attrs.py
+++ sunpy/net/attrs.py
@@ -4,6 +4,5 @@ from .vso import attrs as vso
 from .jsoc import attrs as jsoc
 
 from .vso.attrs import Time, Instrument, Wavelength, Level
-from .dataretriever.attrs import Species
 
-__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'Species', 'vso', 'jsoc']
+__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'vso', 'jsoc']
