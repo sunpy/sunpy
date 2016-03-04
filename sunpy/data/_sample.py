221c9a093afff38498836ac49a5a0155c562ad7f
ankit <emonstar333@gmail.com>
2016-03-04 17:08:35 +0530

    Update _sample.py

--- sunpy/data/_sample.py
+++ sunpy/data/_sample.py
@@ -93,7 +93,7 @@ def download_sample_data(progress=True, overwrite=True):
                 if file_name[1] == '.zip':
                     print("Unpacking: {}".format(real_name))
                     with ZipFile(f, 'r') as zip_file:
-                       zip_file.extract(real_name, sampledata_dir)
+                        zip_file.extract(real_name, sampledata_dir)
                     os.remove(f)
                 else:
                     # move files to the data directory
@@ -101,7 +101,6 @@ def download_sample_data(progress=True, overwrite=True):
             # increment the number of files obtained to check later
                 number_of_files_fetched += 1
                 break
-                
 
     if number_of_files_fetched < len(list(_files.keys())):
         raise URLError("Could not download all samples files. Problem with accessing sample data servers.")
