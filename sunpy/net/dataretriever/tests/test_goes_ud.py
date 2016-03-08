9c8e08d24030fe8a3e1541f6033fe723f3957127
sudarshan <sudk1896@gmail.com>
2016-03-08 16:54:39 +0530

    Added a fido test to all clients.

--- sunpy/net/dataretriever/tests/test_goes_ud.py
+++ sunpy/net/dataretriever/tests/test_goes_ud.py
@@ -4,17 +4,19 @@ from sunpy.time.timerange import TimeRange
 from sunpy.net.vso.attrs import Time, Instrument
 from sunpy.net.dataretriever.client import QueryResponse
 import sunpy.net.dataretriever.sources.goes as goes
+from sunpy.net.dataretriever.downloader_factory import UnifiedResponse
+from sunpy.net import Fido
+from sunpy.net import attrs as a
 
 LCClient = goes.GOESClient()
 
-@pytest.mark.online
 @pytest.mark.parametrize("timerange,url_start,url_end",
 [(TimeRange('1995/06/03', '1995/06/04'),
 'http://umbra.nascom.nasa.gov/goes/fits/1995/go07950603.fits',
-'http://umbra.nascom.nasa.gov/goes/fits/1995/go07950603.fits'),
+'http://umbra.nascom.nasa.gov/goes/fits/1995/go07950604.fits'),
 (TimeRange('2008/06/02', '2008/06/03'),
 'http://umbra.nascom.nasa.gov/goes/fits/2008/go1020080602.fits',
-'http://umbra.nascom.nasa.gov/goes/fits/2008/go1020080602.fits')
+'http://umbra.nascom.nasa.gov/goes/fits/2008/go1020080603.fits')
 ])
 def test_get_url_for_time_range(timerange, url_start, url_end):
     urls = LCClient._get_url_for_timerange(timerange)
@@ -34,7 +36,7 @@ def test_can_handle_query():
 def test_query():
     qr1 = LCClient.query(Time('2012/8/9','2012/8/10'), Instrument('goes'))
     assert isinstance(qr1,QueryResponse)
-    assert len(qr1) == 1
+    assert len(qr1) == 2
     assert qr1.time_range()[0] == '2012/08/09'
     assert qr1.time_range()[1] == '2012/08/10'
 
@@ -50,3 +52,19 @@ def test_get(time, instrument):
     download_list = res.wait()
     assert len(download_list) == len(qr1)
 
+@pytest.mark.online
+def test_new_logic():
+    qr = LCClient.query(Time('2012/10/4','2012/10/6'), Instrument('goes'))
+    res = LCClient.get(qr)
+    download_list = res.wait()
+    assert len(download_list) == len(qr)
+
+@pytest.mark.online
+@pytest.mark.parametrize("time, instrument",
+                         [(a.Time("2012/10/4","2012/10/6"), a.Instrument("goes")),
+                          (a.Time('2013/10/5', '2013/10/7'), a.Instrument("goes"))])
+def test_fido(time, instrument):
+    qr = Fido.search(a.Time('2012/10/4','2012/10/6'), Instrument('goes'))
+    assert isinstance(qr, UnifiedResponse)
+    response = Fido.fetch(qr)
+    assert len(response) == qr._numfile
