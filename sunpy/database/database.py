685412f381423c36470067986bcdfa316491ed84
punyaslokpattnaik@yahoo.co.in <punyaslokpattnaik@yahoo.co.in>
2016-03-08 18:18:34 +0530

    Fixes #1140. Simplified it further

--- sunpy/database/database.py
+++ sunpy/database/database.py
@@ -238,9 +238,8 @@ class Database(object):
         self._session_cls = sessionmaker(bind=self._engine)
         self.session = self._session_cls()
         self._command_manager = commands.CommandManager()
-        if default_waveunit is None:
-            self.default_waveunit = None
-        else:
+        self.default_waveunit = default_waveunit
+        if self.default_waveunit is not None:
             try:
                 self.default_waveunit = units.Unit(default_waveunit)
             except ValueError:
