8077df8717b82573dd161d0aa50d63f11a031d45
Steven Christe <steven.d.christe@nasa.gov>
2016-03-25 13:43:16 -0700

    added more tests

--- sunpy/lightcurve/tests/test_lightcurve.py
+++ sunpy/lightcurve/tests/test_lightcurve.py
@@ -52,6 +52,8 @@ def test_unimplemented(bad_input):
 
 @pytest.mark.parametrize(("overlap_factor"), [0, 1, 5, 10, 100, 500, len(dates)-1])
 def test_concatenate_with_overlap(overlap_factor):
+    # test that lightcurves are being concatenated correctly both with and
+    # without any overlap
     lc1 = sunpy.lightcurve.LightCurve.create(base_input, index=dates)
     dt = dates[1] - dates[0]
     # create a new lc that is shifted in time so there is some overlap
@@ -68,7 +70,26 @@ def test_concatenate_with_overlap(overlap_factor):
     #assert np.all(concat_lc.data[shifted_dates[0]:shifted_dates[-1]] == lc2.data)
 
 
+def test_concatenate_meta():
+    # check that meta data is also being added.
+    eve = sunpy.lightcurve.EVELightCurve.create(EVE_AVERAGES_CSV)
+
+    lc1 = sunpy.lightcurve.LightCurve.create(base_input, index=dates)
+    dt = dates[1] - dates[0]
+    # create a new lc that is shifted in time so there is some overlap
+    lc2 = sunpy.lightcurve.LightCurve.create(base_input,
+                                             index=[t + (dates[-1] - dates[0]) - 1 * dt for t in
+                                                    dates])
+    concat_lc = lc1.concatenate(lc2)
+    assert len(concat_lc.meta) == len(lc1.meta) + len(lc2.meta)
 
 
+def test_concatenate_fail():
+    # check that concatenate throws an error when trying to concatenate
+    # two different lightcurve classes
+    lc1 = sunpy.lightcurve.GOESLightCurve.create(base_input, index=dates)
+    eve = sunpy.lightcurve.EVELightCurve.create(EVE_AVERAGES_CSV)
+    with pytest.raises(TypeError):
+        lc1.concatenate(eve)
 
 
