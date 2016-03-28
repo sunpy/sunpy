2727e1a7ab72c085277c4d1faa73e129ffbd2b58
Brigitta Sipocz <bsipocz@gmail.com>
2016-03-28 03:02:38 -0700

    Fixing the Spectrum.data issue, removing it actually as it's nowhere used

--- sunpy/spectra/spectrum.py
+++ sunpy/spectra/spectrum.py
@@ -18,7 +18,7 @@ class Spectrum(np.ndarray):
     freq_axis : `~numpy.ndarray`
         one-dimensional array with the frequency values.
 
-    data : `numpy.ndarray`
+    data\ : `numpy.ndarray`
         One-dimensional array which the intensity at a particular frequency at
         every data-point.
 
