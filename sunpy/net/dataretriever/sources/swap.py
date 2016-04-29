755099f5005117a42c5e249b84158050e702b4b7
sudarshan <sudk1896@gmail.com>
2016-04-29 19:21:19 +0530

    [unidown]Need input on implementing SWAPClient.

--- sunpy/net/dataretriever/sources/swap.py
+++ sunpy/net/dataretriever/sources/swap.py
@@ -7,8 +7,6 @@ import datetime
 import urllib2
 
 
-from astropy import units as u
-from bs4 import BeautifulSoup
 from sunpy.net.dataretriever.client import GenericClient
 from sunpy.util.scraper import Scraper
 
@@ -17,9 +15,9 @@ from sunpy.time import TimeRange
 __all__ = ['SWAPClient']
 
 class SWAPClient(GenericClient):
-    
-    """ Returns a list of URLS to Proba2 SWAP files corresponding to value of input timerange.
-    URL source: http://proba2.oma.be/swap/data/bsd/
+    """
+    Returns a list of URLS to Proba2 SWAP files corresponding to value of input timerange.
+    URL source: `http://proba2.oma.be/swap/data/bsd/`.
 
     The earliest date available is from 24-Nov-2009
 
@@ -34,34 +32,44 @@ class SWAPClient(GenericClient):
     Returns
     -------
     urls: list
-        list of urls corresponding to requested time range.
+    list of urls corresponding to requested time range.
 
     Examples
     --------
-    >>> from sunpy.time.timerange import TimeRange
-    >>> from sunpy.net.vso.attrs import Time,Instrument,Source,Level
-    >>> from sunpy.net.dataretriever.client import QueryResponse
-    >>> from sunpy.net.dataretriever.sources import swap
+    >>> from sunpy.net import Fido
+    >>> from sunpy.net import attrs as a
 
-    >>> LCClient = swap.SWAPClient()
-
-    >>> qr = LCClient.query(Time('2015-12-30 00:00:00','2015-12-31 00:05:00'),Instrument('swap'))
-    >>> res = LCClient.get(qr)
-    >>> dl = res.wait()
+    >>> results = Fido.search(a.Time('2015/12/28 00:00:00', '2015/12/28 00:03:00'), a.Instrument('swap'))
+    >>> print(results)
+    >>> [<Table length=2>
+         Start Time           End Time      Source Instrument
+           str19               str19         str6     str4   
+    ------------------- ------------------- ------ ----------
+    2015-12-28 00:00:00 2015-12-29 00:00:00 Proba2       swap
+    2015-12-29 00:00:00 2015-12-30 00:00:00 Proba2       swap]
+    
+    >>> response = Fido.fetch(results)
     """
     
-    def _get_url_for_timerange(cls, timerange, **kwargs):
+    def _get_url_for_timerange(self, timerange, Level, **kwargs):
         """ returns list of urls corresponding
         to given TimeRange. """
-
-        if timerange.start < datetime.datetime(2009,11,24):
+##        print(Level)
+        SWAP_STARTDATE = datetime.datetime(2009, 11, 24)
+        if timerange.start < SWAP_STARTDATE:
             raise ValueError('Earliest date for which SWAP data is available is 2009-11-24')
-        
-        url_pattern = url_pattern = ('http://proba2.oma.be/swap/data/bsd/'
+        if (Level == 1):
+            url_pattern = ('http://proba2.oma.be/swap/data/bsd/'
                '%Y/%m/%d/'
                '{instrument}_lv1_%Y%m%d_%H%M%S.fits')
-        
-        crawler = Scraper(url_pattern, instrument = 'swap')
+        else:
+            url_pattern = ('http://proba2.oma.be/swap/data/eng/'
+               '%Y/%m/%d/'
+               '{instrument}_lv0_%Y%m%d_%H%M%S.fits')
+##        url_pattern = ('http://proba2.oma.be/swap/data/bsd/'
+##               '%Y/%m/%d/'
+##               '{instrument}_lv1_%Y%m%d_%H%M%S.fits')
+        crawler = Scraper(url_pattern, instrument= 'swap')
         if not timerange:
             return []
         result = crawler.filelist(timerange)
@@ -78,7 +86,7 @@ class SWAPClient(GenericClient):
 
     @classmethod
     def _can_handle_query(cls, *query):
-         """
+        """
         Answers whether client can service the query.
         
         Parameters
@@ -87,21 +95,17 @@ class SWAPClient(GenericClient):
         
         Returns
         -------
-        boolean
-            answer as to whether client can service the query
+        boolean: answer as to whether client can service the query
         
         """
-         
-         chkattr = ['Time','Instrument']
-         chklist = [x.__class__.__name__ in chkattr for x in query]
-         for x in query:
-             if x.__class__.__name__ == 'Instrument' and x.value=='swap':
-                 return all(chklist)
-         return False
-
-    
-        
-        
-        
-        
-    
+        chkattr = ['Time', 'Instrument', 'Level']
+        chklist = [x.__class__.__name__ in chkattr for x in query]
+        chk_var = 0
+        for x in query:
+            if x.__class__.__name__ == 'Instrument' and x.value.lower() == 'swap':
+                chk_var += 1
+            if x.__class__.__name__ == 'Level' and 0<=x.value<=1:
+                chk_var += 1
+        if (chk_var == 2):
+            return True
+        return False
