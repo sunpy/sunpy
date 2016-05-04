86b2236d69c44c610b7ab2ca8611823ae5107464
sudarshan <sudk1896@gmail.com>
2016-05-04 22:43:09 +0530

    is needs to be replaced with '=='

--- sunpy/net/dataretriever/tests/test_swap.py
+++ sunpy/net/dataretriever/tests/test_swap.py
@@ -1,3 +1,5 @@
+#This module was developed with funding provided by
+#the Google Summer of Code 2016.
 import datetime
 import pytest
 
@@ -20,26 +22,26 @@ LCClient = swap.SWAPClient()
 def test_get_url_for_time_range(timerange, url_start,url_end, level):
     urls = LCClient._get_url_for_timerange(timerange, level = 1)
     assert isinstance(urls, list)
-    assert urls[0] is url_start
-    assert urls[-1] is url_end
+    assert urls[0] == url_start
+    assert urls[-1] == url_end
 
 def test_can_handle_query():
     ans0 = swap.SWAPClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'),Instrument('swap'), a.Level(1))
-    assert ans0 is True
+    assert ans0 == True
     ans1 = swap.SWAPClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'),Instrument('swap'))
-    assert ans1 is False
+    assert ans1 == False
     ans2 = swap.SWAPClient._can_handle_query(Time('2015/12/30','2015/12/31'))
-    assert ans2 is False
+    assert ans2 == False
     ans3 = swap.SWAPClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'),Instrument('eve'))
-    assert ans3 is False
+    assert ans3 == False
 
 @pytest.mark.online
 def test_query():
     qr1 = LCClient.query(Time('2015-12-30 00:00:00','2015-12-30 00:05:00'), Level = 1)
     assert isinstance(qr1, QueryResponse)
-    assert len(qr1) is 4
-    assert qr1.time_range()[0] is '2015/12/30'
-    assert qr1.time_range()[1] is '2015/12/30'
+    assert len(qr1) == 4
+    assert qr1.time_range()[0] == '2015/12/30'
+    assert qr1.time_range()[1] == '2015/12/30'
 
 @pytest.mark.online
 @pytest.mark.parametrize("time, instrument, level",
@@ -48,11 +50,11 @@ def test_get(time, instrument, level):
     qr1 = LCClient.query(time,instrument,level)
     res = LCClient.get(qr1)
     download_list = res.wait()
-    assert len(download_list) is len(qr1)
+    assert len(download_list) == len(qr1)
 
 @pytest.mark.online
 def test_fido_query():
     qr = Fido.search(a.Time('2015/12/28 00:00:00', '2015/12/28 00:03:00'), a.Instrument('swap'), a.Level(1))
     assert isinstance(qr, UnifiedResponse)
     response = Fido.fetch(qr)
-    assert len(response) is qr._numfile
+    assert len(response) == qr._numfile
