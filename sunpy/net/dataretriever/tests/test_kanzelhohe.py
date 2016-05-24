4110dd67357de544a059dee8f91aa4512510cd20
sudarshan <sudk1896@gmail.com>
2016-05-24 20:14:26 +0530

    code brushed up.

--- sunpy/net/dataretriever/tests/test_kanzelhohe.py
+++ sunpy/net/dataretriever/tests/test_kanzelhohe.py
@@ -3,14 +3,13 @@
 import datetime
 import pytest
 
+from astropy import units as u
 from sunpy.time.timerange import TimeRange
 from sunpy.net.vso.attrs import Time,Instrument,Level,Wavelength
 from sunpy.net.dataretriever.client import QueryResponse
 from sunpy.net.dataretriever.downloader_factory import UnifiedResponse
 from sunpy.net import Fido
 from sunpy.net import attrs as a
-from astropy import units as u
-
 import sunpy.net.dataretriever.sources.kanzelhohe as kanzelhohe
 
 KClient = kanzelhohe.KanzelhoheClient()
@@ -27,20 +26,21 @@ def test_get_url_for_timerange(timerange, wavelength, url_start, url_end):
     assert urls[1] == url_end
 
 def test_can_handle_query():
-    ans1 = kanzelhohe.KanzelhoheClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'), Instrument('kanzelhohe'), Wavelength(6563*u.AA))
+    time = Time('2015/12/30 00:00:00','2015/12/31 00:05:00')
+    ans1 = kanzelhohe.KanzelhoheClient._can_handle_query(time, Instrument('kanzelhohe'), Wavelength(6563*u.AA))
     assert ans1 is True
-    ans2 = kanzelhohe.KanzelhoheClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'), Instrument('swap'))
+    ans2 = kanzelhohe.KanzelhoheClient._can_handle_query(time, Instrument('swap'))
     assert ans2 is False
-    ans3 = kanzelhohe.KanzelhoheClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'))
+    ans3 = kanzelhohe.KanzelhoheClient._can_handle_query(time)
     assert ans3 is False
-    ans4 = kanzelhohe.KanzelhoheClient._can_handle_query(Time('2015/12/30 00:00:00','2015/12/31 00:05:00'), Instrument('kanzelhohe'), Wavelength(32768*u.AA))
+    ans4 = kanzelhohe.KanzelhoheClient._can_handle_query(time, Instrument('kanzelhohe'), Wavelength(32768*u.AA))
     assert ans4 is True
 
 @pytest.mark.online
 def test_query():
     qr = KClient.query(Time('2015/01/10 00:00:00', '2015/01/10 12:00:00'), Wavelength(6563*u.AA))
     assert isinstance(qr, QueryResponse)
-    assert len(qr) is 2
+    assert len(qr) == 2
     assert qr.time_range()[0] == '2015/01/10'
     assert qr.time_range()[1] == '2015/01/10'
 
