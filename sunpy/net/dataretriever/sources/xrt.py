46a0a102b6bda834f7142a326d46edf43ffb1e18
sudarshan <sudk1896@gmail.com>
2016-05-28 00:02:53 +0530

    [unidown] XRT Client preliminary.

---
+++ sunpy/net/dataretriever/sources/xrt.py
@@ -0,0 +1,67 @@
+#This module was developed with funding provided by
+#the Google Summer of Code 2016.
+
+__author__ = "Sudarshan Konge"
+__email__ = "sudk1896@gmail.com"
+
+import datetime
+import urllib2
+import re
+
+
+from sunpy.net.dataretriever.client import GenericClient
+from sunpy.util.scraper import Scraper
+from sunpy.time import TimeRange
+
+__all__ = ['XRTClient']
+
+class XRTClient(GenericClient):
+
+    def _get_url_for_timerange(self, timerange, **kwargs):
+
+        START_DATE = datetime.datetime(2014, 1, 10, 18, 14, 42)
+        filter_type = kwargs['filter']
+        regex = re.compile('[^a-zA-Z]')
+        filter_type = regex.sub('', filter_type).lower()
+        result = list()
+        if timerange.start < START_DATE:
+            raise ValueError('Earliest date for which XRT data is available is {:%Y-%m-%d}'.format(START_DATE))
+        filter_dict = {'almesh':'Al_mesh', 'alpoly':'Al_poly', 'cpoly':'C_poly', 'tipoly':'Ti_poly', 'thinbe':'thin_Be'}
+        url_pattern = ['http://solar.physics.montana.edu/HINODE/XRT/QL/syn_comp_fits/XRT_{filter}_%Y%m%d_%H%M%S.{s}.fits'.format(s=i) for i in range (0, 10)]
+        arr = [Scraper(pattern, filter = filter_dict[filter_type]).filelist(timerange) for pattern in url_pattern]
+        [result.extend(url) for url in arr if len(url)>0]
+        if not timerange:
+            return []
+        return result
+
+    def _makeimap(self):
+        self.map_['source'] = 'Hinode'
+        self.map_['instrument'] = 'XRT'
+
+     @classmethod
+    def _can_handle_query(cls, *query):
+        """
+        Answers whether client can service the query.
+        
+        Parameters
+        ----------
+        query : list of query objects
+        
+        Returns
+        -------
+        boolean: answer as to whether client can service the query
+        
+        """
+        chkattr = ['Time', 'Instrument', 'Filter']
+        chklist = [x.__class__.__name__ in chkattr for x in query]
+        chk_var = 0
+        types = ['almesh', 'alpoly', 'cpoly', 'tipoly', 'thinbe']
+        regex = re.compile('[^a-zA-Z]')
+        for x in query:
+            if x.__class__.__name__ == 'Instrument' and x.value.lower() == 'xrt':
+                chk_var += 1
+            if x.__class__.__name__ == 'Filter' and type(x.value) is str and regex.sub('',x.value).lower() in types:
+                chk_var += 1
+        if (chk_var == 2):
+            return True
+        return False
