ad8724a5d3344da46443aa9d753ad15c4c86afa6
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-05-30 00:14:56 -0700

    added some formulas

--- sunpy/instr/aia/response.py
+++ sunpy/instr/aia/response.py
@@ -1,17 +1,22 @@
 __author__ = 'TDWilkinson'
 
 """
-Contains functions useful for analysing AIA data.
+AIA response functions by integrating ChiantiPy
 
-Goal: Use SunPy to infer plasma properties like temperature and density in multiwavelength images taken by the AIA. Two routines are necessary to calculate the response functions (while utilyzing ChiantiPy):
+    - analyze AIA instrument response then output a wavelength response
+    - use spectral model (synthetic spectra)
+    - obtain temperature response based on chianti spectral contribution functions
 
-Wavelength response functions: calculate the amount of flux per wavelength
 
-Temperature response functions: calculate the sensitivity of light from the plasma per temperature
 
 other important variables:
 area
-emissivity
+ion emissivity
+
+electron density
+temperature array
+
+# utilize sunpy/instr/tests/test_aia.py
 """
 
 # tools to import listed here as reference (clean out ones not used at end):
@@ -31,7 +36,9 @@ import pandas as pd
 import chiantipy as chpy
 import chianti.core as ch
 
-from sunpy.instr import aia
+import sunpy
+import sunpy.data.test as test
+from sunpy.instr.aia import aiaprep
 from sunpy.net import hek
 from sunpy.time import parse_time
 from sunpy import config
@@ -40,10 +47,11 @@ from sunpy.util.net import check_download_file
 from sunpy import sun
 
 
-# general format
+
 def get_function(ion, emissivity, temperature, density, optional = None):
     """
-    Statement of usefulness of function.
+    General format of functions where this is the statement of usefulness of function. If these were fits images, it would search the header for relevant information.
+
     Parameters
     ----------
     variable1 : what module of import is used.
@@ -51,16 +59,19 @@ def get_function(ion, emissivity, temperature, density, optional = None):
     optional: (optional) string
         A string specifying optional vairable
         e.g. strings
+
+    output: tuple
     """
     pass
 
 
 
-def area():
+def effective_area():
     """
     finds the area of the instrument
     needs to work with channels
     AIA instrument response / effective area
+        A_{eff}=A_{geo}R_p(\lambda)R_S(\lambda)T_E(\lambda)T_F(\lambda)D(\lambda,t)Q(\lambda)
 
     input: string, file (or path to file?) with instrument information
 
@@ -72,35 +83,41 @@ def area():
 
 
 
-def temperature_response():
+def instrument_response():
     """
-    calculates temperature for various features using ChiantiPy
+        Describes the instrument (AIA) response per wavelength by calculating effective area vs wavelength of the strongest emission lines present in the solar feature
+        R(\lambda)=A_{eff}(\lambda,t)G(\lambda)
+
+    :keyword
+    input: string, data file with wavelengths
+
+    :return: float, array describing the response per wavelength
+    """
+    pass
 
 
-    input: string, data file
 
-    channel: string, indicates channels used
+def wavelength_response():
+    """
+    Wavelength response functions: calculate the amount of flux per wavelength
 
     :return:
     """
+    pass
 
 
 
 def emissivity():
     """
 
+    - Use  chianti.core.mspectrum.intensityratio to get spectral line intensities and show relative emissivity per line.
+
+    input:
+
+
     :return:
     """
-
-
-
-def wavelength_response():
-    """
-    :keyword
-    input: string, data file with wavelengths
-
-    :return: float, array describing the response per wavelength
-    """
+    pass
 
 
 
@@ -108,12 +125,43 @@ def spectrum():
     """
     generate a spectral model for various solar features to be used in modules
 
+    - develop an emissivity spectral structure based on plasma  properties
+    - want to read values of physical parameters to determine feature
+    - elemental abundances:  chianti.core.Spectrum.chianti.data.Abundance
+        chianti.core.continuum has places to access freebound and freefree emissions
+
     input:
 
     :return:
     """
+    pass
 
 
+
+def temperature_response():
+    """
+    calculates temperature for various features using ChiantiPy
+     - Will need to use the spectral contribution function in Chiantipy
+            G(\lambda,T)
+    input: string, data file
+
+    channel: string, indicates channels used
+
+    :return:
+    """
+    pass
+
+
+
+class response()
+    """
+    Will I need a class at all in this?
+
+    """
+    __init__:
+
+
+    pass
 
 
 
