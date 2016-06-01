1115400f95c3492b0744bc08a9fb126fb19c438f
sudarshan <sudk1896@gmail.com>
2016-06-01 19:39:49 +0530

    Docs for secchi.

--- sunpy/net/dataretriever/sources/stereo.py
+++ sunpy/net/dataretriever/sources/stereo.py
@@ -14,7 +14,36 @@ from sunpy.time import TimeRange
 __all__ = ['SECCHIClient', 'PLASTICClient', 'IMPACTClient', 'SWAVESClient']
 
 class SECCHIClient(GenericClient):
+    """
+    Returns a list of URLS to XRT files corresponding to value of input timerange.
+    URL source: `http://stereo-ssc.nascom.nasa.gov/data/beacon/`.
+    Parameters
+    ----------
+    timerange: sunpy.time.TimeRange
+        time range for which data is to be downloaded.
+        Example value - TimeRange('2015-12-30 00:00:00','2015-12-31 00:01:00')
+    
+    Instrument: Fixed argument = 'secchi'
+    
+    Source: Two arguments, case-insensitive - 'ahead' or 'behind'
 
+    Detector: Can take any four arguments, case-insensitive - 'euvi', 'cor2', 'hi_1', 'hi_2'
+    Examples
+    --------
+    >>> from sunpy.net import Fido
+    >>> from sunpy.net import attrs as a    
+    >>> results = Fido.search(a.Time('2007/3/2', '2007/3/3'), a.Instrument('secchi'), a.Source('ahead'), a.Detector('euvi'))
+    >>> print(results)
+        [<Table length=4>
+        Start Time           End Time      Source Instrument
+           str19               str19         str5     str6   
+    ------------------- ------------------- ------ ----------
+    2007-03-02 00:00:00 2007-03-03 00:00:00  ahead     secchi
+    2007-03-03 00:00:00 2007-03-04 00:00:00  ahead     secchi
+    2007-03-04 00:00:00 2007-03-05 00:00:00  ahead     secchi
+    2007-03-05 00:00:00 2007-03-06 00:00:00  ahead     secchi]
+    >>> response = Fido.fetch(results)
+    """
     def _get_url_for_timerange(self, timerange, **kwargs):
 
         regex = re.compile('[^a-zA-Z0-9]')
@@ -70,6 +99,12 @@ class PLASTICClient(GenericClient):
     def _get_url_for_timerange(self, timerange, **kwargs):
 
         source = kwargs.get('source').lower()
+        if (source == 'ahead'):
+            START_DATE = datetime.datetime(2006, 10, 1)
+        else:
+            START_DATE = datetime.datetime(2006, 10, 12)
+        if (timerange.start < START_DATE):
+            raise ValueError('Earliest date for which PLASTIC data is available is {:%Y-%m-%d}'.format(START_DATE))
         prefix = 'http://stereo-ssc.nascom.nasa.gov/data/beacon/{source}/plastic/'
         if (source == 'ahead'):
             suffix = '%Y/%m/ST{char}_LB_PLASTIC_%Y%m%d_V08.cdf'
@@ -117,6 +152,9 @@ class IMPACTClient(GenericClient):
     def _get_url_for_timerange(self, timerange, **kwargs):
 
         source = kwargs.get('source').lower()
+        START_DATE = datetime.datetime(2006, 10, 1)
+        if (timerange.start < START_DATE):
+            raise ValueError('Earliest date for which IMPACT data is available is {:%Y-%m-%d}'.format(START_DATE))
         prefix = 'http://stereo-ssc.nascom.nasa.gov/data/beacon/{source}/'
         suffix = 'impact/%Y/%m/ST{char}_LB_IMPACT_%Y%m%d_V02.cdf'
         url_pattern = prefix + suffix
@@ -161,7 +199,10 @@ class SWAVESClient(GenericClient):
     def _get_url_for_timerange(self, timerange, **kwargs):
 
         source = kwargs.get('source')
-        prefix = 'http://stereo-ssc.nascom.nasa.gov/data/beacon/{source}/
+        START_DATE = datetime.datetime(2006, 10, 27)
+        if (timerange.start < START_DATE):
+            raise ValueError('Earliest date for which SWAVES data is available is {:%Y-%m-%d}'.format(START_DATE))        
+        prefix = 'http://stereo-ssc.nascom.nasa.gov/data/beacon/{source}/'
         suffix = 'swaves/%Y/%m/ST{char}_LB_SWAVES_%Y%m%d.idlsave'
         url_pattern = prefix + suffix
         crawler = Scraper(url_pattern, source = source, char = source[0].upper())
@@ -173,6 +214,7 @@ class SWAVESClient(GenericClient):
     def _makeimap(self):
         self.map_['instrument'] = 'swaves'
 
+    @classmethod
     def _can_handle_query(cls, *query):
         """
         Answers whether client can service the query.
