d2bcda45e92e49919ba82a21f0fbd28d9add3590
Stuart Mumford <stuart@cadair.com>
2016-06-09 13:33:38 +0100

    Fix SQLAlchemy intersphinx link

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -94,7 +94,7 @@ release = sunpy.__version__
 
 intersphinx_mapping.pop('h5py', None)
 intersphinx_mapping['astropy'] = ('http://docs.astropy.org/en/stable/', None)
-intersphinx_mapping['sqlalchemy'] = ('http://docs.sqlalchemy.org/en/rel_0_8/', None)
+intersphinx_mapping['sqlalchemy'] = ('http://docs.sqlalchemy.org/en/latest/', None)
 intersphinx_mapping['pandas'] = ('http://pandas.pydata.org/pandas-docs/stable/', None)
 intersphinx_mapping['skimage'] = ('http://scikit-image.org/docs/stable/', None)
 intersphinx_mapping['wcsaxes'] = ('http://wcsaxes.readthedocs.org/en/stable/', None)
