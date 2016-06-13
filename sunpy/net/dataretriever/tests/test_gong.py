0a35a372e045840c7c7fc29ca127f3b0cca747fc
sudarshan <sudk1896@gmail.com>
2016-06-13 20:03:01 +0530

    Tests done. Docs to be added.

--- sunpy/net/dataretriever/tests/test_gong.py
+++ sunpy/net/dataretriever/tests/test_gong.py
@@ -15,8 +15,8 @@ import sunpy.net.dataretriever.sources.gong as gong
 
 GONGClient = gong.GONGClient()
 
-#downloads 4 fits files each of size 3MB.
-#Total size = 12MB.
+downloads 4 fits files each of size 3MB.
+Total size = 12MB.
 trange = Time('2014/6/4 00:00:00', '2014/6/4 00:07:00')
 @pytest.mark.online
 @pytest.mark.parametrize("time, physobs, instrument, wavelength",
@@ -34,8 +34,32 @@ def test_can_handle_query():
     assert GONGClient._can_handle_query(trange, Instrument('z')) is True
     assert GONGClient._can_handle_query(trange, Instrument('ct'), Physobs('intensity'), Wavelength(6563*u.AA)) is True
     assert GONGClient._can_handle_query(trange) is not True
+    
+trange = Time('2016/6/4 00:00:00', '2016/6/4 00:30:00')
+@pytest.mark.online
+def test_query():
+    qr = GONGClient.query(trange, physobs = 'los_magnetic_field', instrument='bb')
+    assert len(qr) == 3
+    assert qr.time_range()[0] == '2016/06/04'
+    assert qr.time_range()[1] == '2016/06/04'
 
+#Downoads 4 fits files each of
+#size 1.2MB. Total size = 4.8MB.
+@pytest.mark.online
+@pytest.mark.parametrize("time, physobs, instrument, wavelength",
+                         [(Time('2016/6/13 03:00', '2016/6/13 04:00'), Physobs('intensity'),
+                           Instrument('ud'), Wavelength(6768*u.AA))])
+def test_get(time, physobs, instrument, wavelength):
+    qr = GONGClient.query(time, physobs, instrument, wavelength)
+    res = GONGClient.get(qr)
+    download_list = res.wait()
+    assert len(qr) == len(download_list)
 
-
-
-                           
+#Downloads 2 fits.gz files of size 1.2MB each.
+#Total size is 2.5MB.
+@pytest.mark.online
+def test_fido_query():
+    qr = Fido.search(a.Time('2016/6/4', '2016/6/4 00:10:00'), a.Physobs('intensity'), a.Wavelength(6768*u.AA))
+    assert isinstance(qr, UnifiedResponse)
+    response = Fido.fetch(qr)
+    assert len(response) == qr._numfile
