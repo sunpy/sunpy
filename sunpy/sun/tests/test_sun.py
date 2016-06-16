b930966815a98a6f0407083fbfcacea6a20be321
Albert Y. Shih <ayshih@gmail.com>
2016-06-16 12:13:49 -0400

    Fix misplaced parenthesis for calculation of true declination

--- sunpy/sun/tests/test_sun.py
+++ sunpy/sun/tests/test_sun.py
@@ -89,9 +89,9 @@ def test_true_rightasenscion():
     assert_quantity_allclose(sun.true_rightascension("2013/12/11"), 258.150 * u.deg, atol=1e-3 * u.deg)
 
 def test_true_declination():
-    assert_quantity_allclose(sun.true_declination("2012/11/11"), -17.687 * u.deg, atol=1e-3 * u.deg)
-    assert_quantity_allclose(sun.true_declination("2245/12/01"), -21.802 * u.deg, atol=1e-3 * u.deg)
-    assert_quantity_allclose(sun.true_declination("2014/05/27"), 21.352 * u.deg, atol=1e-3 * u.deg)
+    assert_quantity_allclose(sun.true_declination("2012/11/11"), -17.467 * u.deg, atol=1e-3 * u.deg)
+    assert_quantity_allclose(sun.true_declination("2245/12/01"), -21.717 * u.deg, atol=1e-3 * u.deg)
+    assert_quantity_allclose(sun.true_declination("2014/05/27"), 21.245 * u.deg, atol=1e-3 * u.deg)
 
 def test_apparent_obliquity_of_ecliptic():
     assert_quantity_allclose(sun.apparent_obliquity_of_ecliptic("2012/11/11"), 23.435 * u.deg, atol=1e-3 * u.deg)
