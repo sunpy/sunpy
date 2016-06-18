66be1c3e1bca5bfd9faa41768902137361c85159
sudarshan <sudk1896@gmail.com>
2016-06-18 17:52:15 +0530

    Polishing code.

--- sunpy/net/attrs.py
+++ sunpy/net/attrs.py
@@ -3,6 +3,7 @@
 from .vso import attrs as vso
 from .jsoc import attrs as jsoc
 
-from .vso.attrs import Time, Instrument, Wavelength, Level
+from .vso.attrs import Time, Instrument, Wavelength, Level, Source, Physobs, Detector, Filter
 
-__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'vso', 'jsoc']
+__all__ = ['Time', 'Instrument', 'Wavelength', 'Level', 'vso', 'jsoc',
+           'Source', 'Physobs', 'Detector', 'Filter']
