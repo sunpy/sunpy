a4d40e8c0edae0fea7b741f8cb766e7e0b87309a
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-06-19 22:58:25 -0700

    connects to aiaprep

---
+++ sunpy/instr/aia/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+__author__ = 'tdwilkinson'
+
+"""
+SunPy
+=====
+
+An open-source Python library for Solar Physics data analysis.
+
+AIA response functions are ...
+
+Web Links
+---------
+Homepage: http://www.sunpy.org
+Documentation: http://docs.sunpy.org
+"""
+
+from .aiaprep import aiaprep
+
+
+
+
