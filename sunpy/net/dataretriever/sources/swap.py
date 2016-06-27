63551b9ddcf467b80a539bcf42fb38e0b4a93899
sudarshan <sudk1896@gmail.com>
2016-06-27 20:14:24 +0530

    Pylint fixes.

--- sunpy/net/dataretriever/sources/swap.py
+++ sunpy/net/dataretriever/sources/swap.py
@@ -5,13 +5,9 @@ __author__ = "Sudarshan Konge"
 __email__ = "sudk1896@gmail.com"
 
 import datetime
-import urllib2
-
 
 from sunpy.net.dataretriever.client import GenericClient
 from sunpy.util.scraper import Scraper
-
-from sunpy.time import TimeRange
 
 __all__ = ['SWAPClient']
 
@@ -26,7 +22,7 @@ class SWAPClient(GenericClient):
     timerange: sunpy.time.TimeRange
         time range for which data is to be downloaded.
         Example value - TimeRange('2015-12-30 00:00:00','2015-12-31 00:01:00')
-    
+
     Instrument: Fixed argument = 'swap'
 
     Level: Level can take only 0,1 or 'q'/'Q' as arguments.
@@ -45,20 +41,20 @@ class SWAPClient(GenericClient):
     >>> print(results)
     >>> [<Table length=2>
          Start Time           End Time      Source Instrument
-           str19               str19         str6     str4   
+           str19               str19         str6     str4
     ------------------- ------------------- ------ ----------
     2015-12-28 00:00:00 2015-12-29 00:00:00 Proba2       swap
     2015-12-29 00:00:00 2015-12-30 00:00:00 Proba2       swap]
-    
+
     >>> response = Fido.fetch(results)
     """
-    
+
     def _get_url_for_timerange(self, timerange, **kwargs):
         """
         returns list of urls corresponding to given TimeRange.
         """
         level = kwargs.get('level', 1)
-        if (level == 1 or level == 0):
+        if level == 1 or level == 0:
             SWAP_STARTDATE = datetime.datetime(2009, 11, 24)
         else:
             SWAP_STARTDATE = datetime.datetime(2010, 1, 4)
@@ -66,7 +62,7 @@ class SWAPClient(GenericClient):
             raise ValueError('Earliest date for which SWAP data is available is {:%Y-%m-%d}'.format(SWAP_STARTDATE))
         datatype = {0: 'eng', 1:'bsd', 'q':'qlk', 'Q':'qlk'}
         prefix = 'http://proba2.oma.be/swap/data/{datatype}/%Y/%m/%d/'
-        if (level == 0 or level == 1):
+        if level == 0 or level == 1:
             suffix = '{instrument}_lv{level}_%Y%m%d_%H%M%S.fits'
         else:
             suffix = '%Y_%m_%d__%H_%M_%S__PROBA2_SWAP_SWAP_174.jp2'
@@ -91,15 +87,15 @@ class SWAPClient(GenericClient):
     def _can_handle_query(cls, *query):
         """
         Answers whether client can service the query.
-        
+
         Parameters
         ----------
         query : list of query objects
-        
+
         Returns
         -------
         boolean: answer as to whether client can service the query
-        
+
         """
         chkattr = ['Time', 'Instrument', 'Level']
         chklist = [x.__class__.__name__ in chkattr for x in query]
