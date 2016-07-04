99ca1c6e3051045e8943636820a3e76b805d8873
Alex Hamilton <Alex_Ian_Hamilton@hotmail.com>
2016-07-04 20:27:51 +0100

    Added the TimeSeriesMetaData class definition.

---
+++ sunpy/timeseries/metadata.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 18:10:26 2016
+
+@author: alex_
+"""
+
+from collections import OrderedDict
+
+from sunpy.time import TimeRange, parse_time
+
+import warnings
+import inspect
+
+
+
+
+class TimeSeriesMetaData:
+    """
+    A metadata object for use with the TimeSeries object.
+    """
+
+    def __init__(self, **kwargs):
+        self.metadata = []
+
+    def append(self, timerange, columns, metadata, **kwargs):
+        """
+        Add the given metadata into the metadata table.
+        """
+        # Check the types are correct.
+        pos = len(self.metadata)
+        if isinstance(timerange, TimeRange) and isinstance(metadata, OrderedDict):
+            for i in range(0, len(self.metadata)):
+                if timerange.start < self.metadata[i][0].start:
+                    pos = i - 1
+        else:
+            warnings.warn_explicit("Incorrect datatime or data for append to MetaData.",
+                                   Warning, __file__, inspect.currentframe().f_back.f_lineno)
+        
+        # Prepare tuple to append.
+        new_metadata = (timerange, columns, metadata)
+        
+        # Check this isn't a duplicate entry (same TR and comnames)
+        duplicate = False
+        if pos < len(self.metadata):
+            old_metadata = self.metadata[pos]
+            if (new_metadata[0].start == old_metadata[0].start) and (new_metadata[0].end == old_metadata[0].end) and (new_metadata[1] == old_metadata[1]):
+                duplicate = True
+                
+        # Insert into the given position
+        if not duplicate:
+            self.metadata.insert(pos, new_metadata)
+            
+    def find(self, datetime, colname=None):
+        """
+        Find all metadata matching the given date/time and optional column name.
+        """
+        # Extract the datetime object.
+        dt = datetime
+        if isinstance(dt, str):
+            dt = parse_time(dt)
+            
+        # Find all results with suitable timerange.
+        results_indexes = []
+        for i in range(0, len(self.metadata)):
+            if dt in self.metadata[i][0]:
+                results_indexes.append(i)
+        
+        # Filter out only those with the correct column.
+        results = []
+        for i in results_indexes:
+            #print('\n\nself.metadata[i][1]: ' + str(self.metadata[i][1]) + '\n\n')
+            if (colname in self.metadata[i][1]) or (not colname):
+                results.append(self.metadata[i][2])
+        
+        return results
+    
+    def get(self, index):
+        # Return the dictionary entry at the given index.
+        return self.metadata[index][1]
+        
+    def concatenate(self, tsmetadata2, **kwargs):
+        """
+        Combine the metadata from a TimeSeriesMetaData object with the current
+        metadata.
+        """
+        for tuple in tsmetadata2.metadata:
+            self.append(tuple[0], tuple[1], tuple[2])
+        
+    def _validate_meta(self, meta):
+        """
+        Validate a meta argument.
+        """
+        return True
+        
+if __name__ == "__main__":
+    tr_1 = TimeRange('2012-06-01 00:00','2012-06-02 00:00')
+    tr_1a = TimeRange('2012-06-01 00:00','2012-06-02 00:00')
+    tr_2 = TimeRange('2012-06-02 00:00','2012-06-03 00:00')
+    tr_3 = TimeRange('2012-06-03 00:00','2012-06-04 00:00')
+    tr_4 = TimeRange('2012-06-04 00:00','2012-06-05 00:00')
+    tr_5 = TimeRange('2012-06-02 00:00','2012-06-06 00:00')
+    
+    # Build a MetaData object
+    md = TimeSeriesMetaData()
+    md.append(tr_1, ['GOES'], OrderedDict([('tr_1','tr_1')]))
+    md.append(tr_2, ['GOES'], OrderedDict([('tr_2','tr_2')]))
+    md.append(tr_3, ['GOES'], OrderedDict([('tr_3','tr_3')]))
+    md.append(tr_4, ['GOES'], OrderedDict([('tr_4','tr_4')]))
+    md.append(tr_5, ['Other'], OrderedDict([('tr_5','tr_5')]))
+    
+    #metadict = OrderedDict([(tr_1, 'tr_1'), (tr_2, 'tr_2'), (tr_3, 'tr_3'), (tr_4, 'tr_4'), (tr_5, 'tr_5')])
+    #md = MetaData(metadict)
+    
+    # Check it
+    time_1 = parse_time('2012-06-01T21:08:12')
+    time_2 = parse_time('2012-06-02T21:08:12')
+    time_3 = parse_time('2012-06-03T21:08:12')
+    time_4 = parse_time('2012-06-04T21:08:12')
+    time_5 = parse_time('2012-06-05T21:08:12')
+    time_6 = parse_time('2012-05-05T21:08:12') # Too early
+    time_7 = parse_time('2012-06-08T21:08:12') # Too late
+    # Without columns
+    md.find(time_1) # One result (tr_1)
+    md.find(time_2) # Two results (tr_2 and tr_5)
+    md.find(time_3) # Two results (tr_5 and tr_3)
+    md.find(time_4) # Two results (tr_5 and tr_4)
+    md.find(time_5) # One result (tr_5)
+    md.find(time_6) # No results (too early)
+    md.find(time_7) # No results (too late)
+    # With columns
+    md.find(time_1, 'Other') # No results (wrong column)
+    md.find(time_2, 'Other') # One result (tr_5)
+    md.find(time_3, 'Other') # One result (tr_5)
+    md.find(time_4, 'Other') # One result (tr_5)
+    md.find(time_5, 'Other') # One result (tr_5)
+    md.find(time_6, 'Other') # No results (too early)
+    md.find(time_7, 'Other')
+    md.find(time_1, 'GOES')
+    md.find(time_2, 'GOES')
+    md.find(time_3, 'GOES')
+    md.find(time_4, 'GOES')
+    md.find(time_5, 'GOES')
+    md.find(time_6, 'GOES') # No results (too early)
+    md.find(time_7, 'GOES') # No results (too late)
--- sunpy/timeseries/tests/test_timeseries_factory.py
+++ sunpy/timeseries/tests/test_timeseries_factory.py
@@ -25,42 +25,42 @@ class TestTimeSeries(object):
     def test_eve(self):
         #Test an EVE TimeSeries
         ts_eve = sunpy.timeseries.TimeSeries(sunpy.data.sample.EVE_LIGHTCURVE, source='EVE')
-        assert isinstance(ts_eve,sunpy.timeseries.sources.eve.EVELightCurve)
+        assert isinstance(ts_eve, sunpy.timeseries.sources.eve.EVELightCurve)
 
     def test_fermi_gbm(self):
         #Test a GBMSummary TimeSeries
         ts_gbm = sunpy.timeseries.TimeSeries(sunpy.data.sample.GBM_LIGHTCURVE, source='GBMSummary')
-        assert isinstance(ts_gbm,sunpy.timeseries.sources.fermi_gbm.GBMSummaryLightCurve)
+        assert isinstance(ts_gbm, sunpy.timeseries.sources.fermi_gbm.GBMSummaryLightCurve)
 
     def test_norrh(self):
         #Test a NoRH TimeSeries
         ts_norrh = sunpy.timeseries.TimeSeries(sunpy.data.sample.NORH_LIGHTCURVE, source='NoRH')
-        assert isinstance(ts_norrh,sunpy.timeseries.sources.norh.NoRHLightCurve)
+        assert isinstance(ts_norrh, sunpy.timeseries.sources.norh.NoRHLightCurve)
 
     def test_goes(self):
         #Test a GOES TimeSeries
         ts_goes = sunpy.timeseries.TimeSeries(sunpy.data.sample.GOES_LIGHTCURVE, source='GOES')
-        assert isinstance(ts_goes,sunpy.timeseries.sources.goes.GOESLightCurve)
+        assert isinstance(ts_goes, sunpy.timeseries.sources.goes.GOESLightCurve)
 
     def test_lyra(self):
         #Test a LYRA TimeSeries
         ts_lyra = sunpy.timeseries.TimeSeries(sunpy.data.sample.LYRA_LEVEL3_LIGHTCURVE, source='LYRA')
-        assert isinstance(ts_lyra,sunpy.timeseries.sources.lyra.LYRALightCurve)
+        assert isinstance(ts_lyra, sunpy.timeseries.sources.lyra.LYRALightCurve)
 
     def test_rhessi(self):
         #Test a RHESSI TimeSeries
         ts_rhessi = sunpy.timeseries.TimeSeries(sunpy.data.sample.RHESSI_LIGHTCURVE, source='RHESSI')
-        assert isinstance(ts_rhessi,sunpy.timeseries.sources.rhessi.RHESSISummaryLightCurve)
+        assert isinstance(ts_rhessi, sunpy.timeseries.sources.rhessi.RHESSISummaryLightCurve)
 
     def test_noaa_ind(self):
         #Test a NOAAPredictIndices TimeSeries
         ts_noaa_ind = sunpy.timeseries.TimeSeries(sunpy.data.sample.NOAAINDICES_LIGHTCURVE, source='NOAAPredictIndices')
-        assert isinstance(ts_noaa_ind,)
+        assert isinstance(ts_noaa_ind, sunpy.timeseries.sources.noaa.NOAAIndicesTimeSeries)
             
     def test_noaa_pre(self):
         #Test a NOAAIndices TimeSeries
         ts_noaa_pre = sunpy.timeseries.TimeSeries(sunpy.data.sample.NOAAPREDICT_LIGHTCURVE, source='NOAAIndices')
-        assert isinstance(ts_noaa_pre,)
+        assert isinstance(ts_noaa_pre, sunpy.timeseries.sources.noaa.NOAAPredictIndicesTimeSeries)
     
     
     
