3e15820b034790c027e4935330e34694b7a89a55
Alex Hamilton <Alex_Ian_Hamilton@hotmail.com>
2016-07-17 02:53:19 +0100

    Added TS.index method. Added timerange, .timeranges, .columns, .metas parameters and .values() method to TSMD class. Added find_indices to the TSMD class and removed the equivilent kwarg from the find method. Get and find now return TSMD objects that can be simplified using the parameter methods above. Fixed the TSMD.append method. Renamed TSMD.truncate to TSMD._truncate and similarly _remove_columns and _rename_columns so users don't access these directly. Updated the TSMD gallery example to match the current interface and demonstrate the current functionality.

--- sunpy/timeseries/metadata.py
+++ sunpy/timeseries/metadata.py
@@ -5,6 +5,7 @@ __email__ = "stuart@mumford.me.uk"
 
 from sunpy.util.metadata import MetaDict
 import itertools
+import copy
 
 import warnings
 import inspect
@@ -53,14 +54,14 @@ class TimeSeriesMetaData:
         it's TimeRange and colnames (list).
         Will add the new entry so the list is in chronological order for the
         TimeRange.start datetime values.
-        
+
         Parameters
         ----------
         timerange : `~sunpy.time.TimeRange`
             The timerange for which a given metadict is relevant. This will
             generally initilly be the full range of the original file, but if
             the TimeSeries gets truncated this may change appropriately.
-        
+
         columns : `str`
             A list of the colomn name strings that the metadata is relevant for.
 
@@ -69,105 +70,138 @@ class TimeSeriesMetaData:
         """
         # Parameters
         metadata = MetaDict(metadata)
-        
+
         # Check the types are correct.
-        pos = len(self.metadata)
+        pos = 0
         if isinstance(timerange, TimeRange):
             for i, meta in enumerate(self.metadata):
-                if timerange.start < meta[0].start:
-                    pos = i - 1
+                if timerange.start > meta[0].start:
+                    pos = i + 1
         else:
             raise ValueError(
                 'Incorrect datatime or data for append to TimeSeriesMetaData.')
-        
+
         # Prepare tuple to append.
         new_metadata = (timerange, columns, metadata)
-        
+
         # Check this isn't a duplicate entry (same TR and comnames)
         duplicate = False
         if pos < len(self.metadata):
             old_metadata = self.metadata[pos]
             if (new_metadata[0] == old_metadata[0]) and (new_metadata[1] == old_metadata[1]):
                 duplicate = True
-                
+
         # Insert into the given position
         if not duplicate:
             self.metadata.insert(pos, new_metadata)
-            
-    def find(self, time=None, colname=None, row=None, indices=False, **kwargs):
+
+    def find_indices(self, time=None, colname=None, **kwargs):
         """
-        Find all metadata matching the given filters for datetime and column name.
-        Will return all metadata entries if no filters are given.
-        
+        Find the indices for all the metadata entries matching the given filters
+        for datetime and/or column name.
+        Will return all metadata entry indices if no filters are given.
+
         Parameters
         ----------
         time : `str` or `~datetime.datetime` optional
             The string (parsed using the `~sunpy.time.parse_time`) or datetime
             that you need metadata for.
-        
+
+        ####ToDo: implmentation decision.
         row : `int` optional
             Integer index of the row within the data (dataframe) to get the
             datetime index from.
-        
+
         colname : `str` optional
             A string that can be used to narrow results to specific columns.
 
         indices : `bool` optional
             If True then return a list of indices, not of MetaDict items.
             Used when other methods use the filters for selecting metadata entries.
-    
+
         Returns
         -------
         list : `list`
-            A list of MetaDict objects that contain all matching metadata.
+            A list of integers that contain all matching metadata.
         """
         # Parameters
         dt = time
-        if row:
-            # Given a row index, get the relevant dt.
-            dt = self.data.index[row]
         if not dt:
             dt = False
         elif isinstance(dt, str):
             dt = parse_time(dt)
-            
+
         # Find all results with suitable timerange.
         results_indices = []
         for i, meta in enumerate(self.metadata):
             if dt in meta[0] or not(dt):
                 results_indices.append(i)
-        
+
         # Filter out only those with the correct column.
         results = []
         for i in results_indices:
             if (colname in self.metadata[i][1]) or (not colname):
-                if indices:
-                    results.append(i)
-                else:
-                    results.append(self.metadata[i][2])
-        
+                results.append(i)
+
         return results
-    
+
+    def find(self, time=None, colname=None, row=None, **kwargs):
+        """
+        Find all metadata matching the given filters for datetime and/or column name.
+        Will return all metadata entries if no filters are given.
+
+        Parameters
+        ----------
+        time : `str` or `~datetime.datetime` optional
+            The string (parsed using the `~sunpy.time.parse_time`) or datetime
+            that you need metadata for.
+
+        ####ToDo: implmentation decision.
+        row : `int` optional
+            Integer index of the row within the data (dataframe) to get the
+            datetime index from.
+
+        colname : `str` optional
+            A string that can be used to narrow results to specific columns.
+
+        Returns
+        -------
+        metadata : `~sunpy.timeseries.metadata.TimeSeriesMetaData`
+            A TimeSeriesMetaData that contain all matching metadata entries.
+        """
+        # Get the indices
+        indices = self.find_indices(time=time, colname=colname, **kwargs)
+
+        # Extract the relevant metadata entries
+        metadata = []
+        for i in indices:
+            metadata.append(copy.copy(self.metadata[i]))
+
+        # Return a TimeSeriesMetaData object
+        return TimeSeriesMetaData(meta=metadata)
+
     def get_index(self, index):
         """
         Return the dictionary entry at the given index.
-        
+
         Parameters
         ----------
         index : `int`
             The integer index of the metadata entry in the list.
-    
+
         Returns
         -------
         metadata : `~sunpy.util.metadata.MetaDict`
             An ordered Dictionary containing the metadata at the given index.
         """
-        return self.metadata[index][1]
+        return self.metadata[index][2]
 
-    def get(self, key, default=None, time=None, colname=None, row=None, itemised=False, **kwargs):
+    def get(self, key, default=None, time=None, colname=None, row=None, **kwargs):
         """
-        Return a list of all the matching entries in the metadata.
-        
+        Return a TimeSeriesMetaData object of all entries matcing the time and
+        colname filters with the dictionaries containing only the key value pairs
+        with the key matching the given input key.
+
         Parameters
         ----------
         key : `str`
@@ -180,50 +214,37 @@ class TimeSeriesMetaData:
             The string (parsed using the `~sunpy.time.parse_time`) or datetime
             that you need metadata for.
 
+        ####ToDo: implmentation decision.
         row : `int` optional
             Integer index of the row within the data (dataframe) to get the
             datetime index from.
-            
+
         colname : `str` optional
             A string that can be used to narrow results to specific columns.
 
         itemised : `bool` optional
             Option to allow the return of the time ranges and column names
             (as list) that match each given value.
-            
+
         Returns
         -------
         list : `list`
             Returns a list of the matching entries or the default value.
-        """        
+        """
         # Find all matching metadata entries
-        indices = self.find(time=time, colname=colname, row=row, indices=True)
-        
-        # Now find each matching entry
-        results = []
-        for i in indices:
-            # Get the value for the entry in this metadata entry
-            value = self.metadata[i][2].get(key)            
-            
-            # Need more details if itemised.
-            if itemised:
-                tr = self.metadata[i][0]
-                colnames = self.metadata[i][1]
-                
-                # Append a tuple of these values
-                results.append(( tr, colnames, value ))
-            else:
-                # Add to the list if a result was returned
-                if value:
-                    results.append(value)
-        
-        # Remove duplicates.
-        if not itemised:
-            results = list(set(results))
-        
-        # Return all the results
-        return results
-        
+        full_metadata = self.find(time=time, colname=colname)
+        metadata = []
+
+        for i, entry in enumerate(full_metadata.metadata):
+            metadict = MetaDict()
+            for curkey, value in entry[2].items():
+                if curkey == key:
+                    metadict.update({key:value})
+            metadata.append((entry[0], entry[1], metadict))
+
+        # Return a TimeSeriesMetaData object
+        return TimeSeriesMetaData(meta=metadata)
+
     def concatenate(self, tsmetadata2, **kwargs):
         """
         Combine the metadata from a TimeSeriesMetaData object with the current
@@ -236,12 +257,12 @@ class TimeSeriesMetaData:
         """
         # Create a copy of the metadata
         meta = TimeSeriesMetaData(self.metadata.copy())
-        
+
         # Append each metadata entry from the second TimeSeriesMetaData object
         # to the original TimeSeriesMetaData object.
         for entry in tsmetadata2.metadata:
             meta.append(entry[0], entry[1], entry[2])
-            
+
         return meta
 
     def update(self, dictionary, time=None, colname=None, row=None, overwrite=False, **kwargs):
@@ -252,22 +273,22 @@ class TimeSeriesMetaData:
         ----------
         dictionary : `dict` or `OrderedDict` or `~sunpy.util.metadata.MetaDict`
             The second TimeSeriesMetaData object.
-            
+
         time : `str` or `~datetime.datetime` optional
             The string (parsed using the `~sunpy.time.parse_time`) or datetime
             to filter the metadata entries updated.
-            
+
         colname : `str` optional
             A string that can be used to narrow results to specific columns.
-            
+
         overwrite : `bool` optional
             Option to define if the user is able to overwrite already present keys.
             Defaults to False, designed to stop users from being able to
             corrupt/damage the metadict values so easily.
         """
         # Find all matching metadata entries
-        indices = self.find(time=time, colname=colname, row=row, indices=True)
-        
+        indices = self.find_indices(time=time, colname=colname, row=row, indices=True)
+
         # Now update each matching entries
         for i in indices:
             # Should we allow the user to overwrite values?
@@ -282,7 +303,7 @@ class TimeSeriesMetaData:
                 # Now recreate the tuple
                 self.metadata[i] = ( self.metadata[i][0], self.metadata[i][1], new_meta )
 
-    def truncate(self, timerange):
+    def _truncate(self, timerange):
         """Removes metadata entries outside of the new (truncated) TimeRange.
         Also adjusts start and end times of time ranges going outside of the
         truncated time range.
@@ -298,25 +319,25 @@ class TimeSeriesMetaData:
             start = metatuple[0].start
             end   = metatuple[0].end
             out_of_range = False
-            
+
             # Find truncations
-            if start < timerange.start and end > timerange.start:
+            if start < timerange.start and end > timerange.start:#d or f
                 # Truncate the start
                 start = timerange.start
-            elif start > timerange.end:
+            elif start > timerange.end:#c
                 # Metadata time range starts after truncated data ends.
                 out_of_range = True
-            if end > timerange.end and start < timerange.end:
+            if end > timerange.end and start < timerange.end:#e or f
                 # Truncate the end
                 end = timerange.end
-            elif end < timerange.start:
+            elif end < timerange.start: # a
                 # Metadata time range finishes before truncated data starts.
                 out_of_range = True
-            
+
             # Add the values if applicable
             if not out_of_range:
                 truncated.append((TimeRange(start, end), metatuple[1], metatuple[2]))
-                
+
         # Update the original list
         self.metadata = truncated
 
@@ -327,10 +348,47 @@ class TimeSeriesMetaData:
         for metatuple in self.metadata:
             all_cols.update(metatuple[1])
         return list(all_cols)
-        
-    def remove_columns(self, colnames):
+
+    @property
+    def metas(self):
+        """Returns a list of all the metadict objects in the TimeSeriesMetaData object."""
+        all_metas = []
+        for metatuple in self.metadata:
+            all_metas.append(metatuple[2])
+        return all_metas
+
+    @property
+    def timeranges(self):
+        """Returns a list of all the TimeRange objects the TimeSeriesMetaData object."""
+        all_tr = []
+        for metatuple in self.metadata:
+            all_tr.append(metatuple[0])
+        return all_tr
+
+    def values(self):
+        """Returns a list of all the values from the metadict objects in each
+        entry in the TimeSeriesMetaData object."""
+        all_vals = set()
+        for metatuple in self.metadata:
+            for key, value in metatuple[2].items():
+                all_vals.add(str(value))
+        all_vals = list(all_vals)
+        all_vals.sort()
+        return all_vals
+
+    @property
+    def timerange(self):
+        """Returns the TimeRange of the entire time series meta data."""
+        start = self.metadata[0][0].start
+        end = self.metadata[0][0].end
+        for metatuple in self.metadata:
+            if end < metatuple[0].end:
+               end = metatuple[0].end
+        return TimeRange(start, end)
+
+    def _remove_columns(self, colnames):
         """Removes the given column/s from the TimeSeriesMetaData object.
-       
+
         Parameters
         ----------
         colnames : `str` or `list`
@@ -357,10 +415,10 @@ class TimeSeriesMetaData:
 
 
 
-    def rename_column(self, old, new):
+    def _rename_column(self, old, new):
         """
         Change the name of a column in all the metadata entries.
-        
+
         Parameters
         ----------
         old : `str`
@@ -373,7 +431,7 @@ class TimeSeriesMetaData:
             # Update the colnames
             colnames = self.metadata[i][1]
             colnames = [w.replace(old, new) for w in colnames]
-            
+
             # Replace values
             self.metadata[i] = ( self.metadata[i][0], colnames, self.metadata[i][2] )
 
@@ -393,15 +451,15 @@ class TimeSeriesMetaData:
                 if col_overlap:
                     warnings.warn_explicit('Metadata entries ' + str(i) + ' and ' + str(j) + ' contain interleaved data.',
                                            Warning, __file__, inspect.currentframe().f_back.f_lineno)
-            
-            
-        
+
+
+
         return True
 
     def to_string(self, depth=10, width=99):
         """
         Print a table-like representation of the TimeSeriesMetaData object.
-        
+
         Parameters
         ----------
         depth : `int`
@@ -417,15 +475,18 @@ class TimeSeriesMetaData:
         colheadings = '|' + 'TimeRange'.ljust(100)[:liswidths[0]] + colspace + 'Columns'.ljust(100)[:liswidths[1]] + colspace + 'Meta'.ljust(100)[:liswidths[2]]  + '|'
         rowspace = "-" * (liswidths[0] + len(colspace) + liswidths[1] + len(colspace) + liswidths[2])
         rowspace = '|' + rowspace + '|'
-        
+
         # Headings
         full = rowspace + '\n' + colheadings + '\n' + rowspace + '\n'
-        
+
         # Add metadata entries
         for entry in self.metadata:
             # Make lists for each of the columns for each metadata entry
             # Padded to the widths given in liswidths
             listr = [ str(entry[0].start), '            to            ', str(entry[0].end) ]
+            # Shorten TimeRange representation if depth of only 2
+            if depth == 2:
+                listr = [ str(entry[0].start), str(entry[0].end) ]
             liscols = []
             for col in entry[1]:
                 liscols.append(col.ljust(100)[:liswidths[1]])
@@ -433,7 +494,7 @@ class TimeSeriesMetaData:
             for key in list(entry[2].keys()):
                 string = str(key) + ': ' + str(entry[2][key])
                 lismeta.append(string.ljust(100)[:liswidths[2]])
-            
+
             # Add lines of the entry upto the given depth
             for i in range(0, depth):
                 if len(listr) > i or len(entry[1]) > i or len(lismeta) > i :
@@ -456,17 +517,17 @@ class TimeSeriesMetaData:
             # Add a line to show if the columns are truncated.
             if len(listr) >= depth or len(entry[1]) >= depth or len(lismeta) >= depth:
                 line = '|'
-                if len(listr) >= depth:
+                if len(listr) > depth:
                     line += '...'.ljust(100)[:liswidths[0]]
                 else:
                     line += ''.ljust(100)[:liswidths[0]]
                 line += colspace
-                if len(entry[1]) >= depth:
+                if len(entry[1]) > depth:
                     line += '...'.ljust(100)[:liswidths[1]]
                 else:
                     line += ''.ljust(100)[:liswidths[1]]
                 line += colspace
-                if len(lismeta) >= depth:
+                if len(lismeta) > depth:
                     line += '...'.ljust(100)[:liswidths[2]]
                 else:
                     line += ''.ljust(100)[:liswidths[2]]
