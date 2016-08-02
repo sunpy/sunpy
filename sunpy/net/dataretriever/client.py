d4f0e50aed1861f49daee7e59b2dd4fe2543312b
Punyaslok Pattnaik <punyaslokpattnaik@yahoo.co.in>
2016-08-02 20:57:22 +0530

    Comment location changed.

--- sunpy/net/dataretriever/client.py
+++ sunpy/net/dataretriever/client.py
@@ -220,10 +220,10 @@ class GenericClient(object):
 
         dobj = Downloader(max_conn=len(urls), max_total=len(urls))
 
+        # We cast to list here in list(zip... to force execution of 
+        # res.require([x]) at the start of the loop.
         for aurl, ncall, filename in list(zip(urls, map(lambda x: res.require([x]),
                                               urls), filenames)):
-            # We cast to list here in list(zip... to force execution of 
-            # res.require([x]) at the start of the loop.
             temp_dict = details.copy()
             temp_dict['file'] = filename
             dobj.download(aurl, kwargs.get('path', None), ncall,
