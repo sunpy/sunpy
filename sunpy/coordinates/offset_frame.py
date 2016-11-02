6edccb36d32280dee5ec7cdbbd7e20d113f8e3b5
Stuart Mumford <stuart@cadair.com>
2016-11-01 23:02:28 -0600

    Add a North Offset factory

---
+++ sunpy/coordinates/offset_frame.py
@@ -0,0 +1,45 @@
+import astropy.units as u
+from astropy.coordinates import SkyOffsetFrame, SphericalRepresentation
+
+__all__ = ['NorthOffsetFrame']
+
+
+class NorthOffsetFrame():
+    """
+    A frame which is relative to some position and another frame. Based on
+    `astropy.coordinates.SkyOffsetFrame`
+
+    Coordinates in a NorthOffsetFrame are both centered on the position
+    specified bu the ``north`` keyword *and* they are oriented in the same
+    manner as the ``north`` frame.
+
+    Unlike `~astropy.coordinates.SkyOffsetFrame` a `NorthOffsetFrame` allows
+    you to specify the position of the new north pole rather than the new
+    origin to centre the new frame.
+
+    Notes
+    -----
+    ``NorthOffsetFrame`` is a wrapper around the
+    `~astropy.coordinates.SkyOffsetFrame` factory class. This class will
+    calculate the desired coordianates of the ``origin`` from the ``north``
+    keyword argument and then create a `~astropy.coordinates.SkyOffsetFrame`.
+
+    Using this frame is equivalent to using
+    `~astropy.coordinates.SkyOffsetFrame` with ``lat = lat - 90*u.deg``
+    """
+
+    def __new__(cls, *args, **kwargs):
+        origin_frame = kwargs.pop('north', None)
+        if origin_frame is None:
+            raise TypeError("Can't initialize an NorthOffsetFrame without origin= keyword.")
+        if hasattr(origin_frame, 'frame'):
+            origin_frame = origin_frame.frame
+
+        rep = origin_frame.represent_as(SphericalRepresentation)
+        new_rep = SphericalRepresentation(lon=rep.lon,
+                                          lat=rep.lat - 90*u.deg,
+                                          distance=rep.distance)
+
+        new_origin = origin_frame.realize_frame(new_rep)
+        kwargs['origin'] = new_origin
+        return SkyOffsetFrame(*args, **kwargs)
