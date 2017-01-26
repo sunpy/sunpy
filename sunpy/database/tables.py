6097d4fa21de2c40e4b14fa55e90e0c370f253fd
Punyaslok Pattnaik <punyaslokpattnaik@yahoo.co.in>
2017-01-27 05:19:06 +0530

    Yet Another attempt to fix pep8 issues :P

--- sunpy/database/tables.py
+++ sunpy/database/tables.py
@@ -36,7 +36,8 @@ __all__ = [
 Base = declarative_base()
 
 # required for the many-to-many relation on tags:entries
-association_table = Table('association', Base.metadata,
+association_table = Table(
+                          'association', Base.metadata,
                           Column('tag_name', String, ForeignKey('tags.name')),
                           Column('entry_id', Integer, ForeignKey('data.id'))
                          )
