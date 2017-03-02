57bbef8b8410e817abf29345a9cb1098fa3e1b81
Michael Charlton <m.charlton@mac.com>
2017-03-02 12:12:35 +0000

    Minor change to formatting of TimeRange.__repr__ see #2012

--- sunpy/time/timerange.py
+++ sunpy/time/timerange.py
@@ -234,14 +234,14 @@ class TimeRange(object):
         center = self.center.strftime(TIME_FORMAT)
         fully_qualified_name = '{0}.{1}'.format(self.__class__.__module__, self.__class__.__name__)
 
-        return ('   <{0} object at {1}'.format(fully_qualified_name, hex(id(self))) +
+        return ('   <{0} object at {1}>'.format(fully_qualified_name, hex(id(self))) +
                 '\n    Start:'.ljust(12) + t1 +
                 '\n    End:'.ljust(12) + t2 +
                 '\n    Center:'.ljust(12) + center +
                 '\n    Duration:'.ljust(12) + str(self.days.value) + ' days or' +
                 '\n    '.ljust(12) + str(self.hours.value) + ' hours or' +
                 '\n    '.ljust(12) + str(self.minutes.value) + ' minutes or' +
-                '\n    '.ljust(12) + str(self.seconds.value) + ' seconds' + '>'
+                '\n    '.ljust(12) + str(self.seconds.value) + ' seconds' +
                 '\n')
 
     def split(self, n=2):
