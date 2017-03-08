287558f62b769ffe14a7ecf6ee548c0c5eb09c06
Nitin Choudhary <nitin.iitkgp23@gmail.com>
2017-03-08 16:15:04 +0530

    Modified all files with 1 function call

--- sunpy/lightcurve/lightcurve.py
+++ sunpy/lightcurve/lightcurve.py
@@ -17,7 +17,7 @@ import pandas
 from sunpy import config
 from sunpy.time import is_time, TimeRange, parse_time
 from sunpy.util.cond_dispatch import ConditionalDispatch, run_cls
-from sunpy.util.config import create_download_dir
+from sunpy.util.config import get_and_create_download_dir
 from sunpy.extern.six.moves import urllib
 from sunpy.extern import six
 
@@ -275,8 +275,7 @@ class LightCurve(object):
         if "directory" in kwargs:
             download_dir = os.path.expanduser(kwargs["directory"])
         else:
-            download_dir = config.get("downloads", "download_dir")
-            create_download_dir()
+            download_dir = get_and_create_download_dir()
 
         # overwrite the existing file if the keyword is present
         if "overwrite" in kwargs:
