5d4e495d2fc43a6422515e9f2bd3899a8f7f03c6
Brigitta Sipocz <bsipocz@gmail.com>
2017-03-13 17:14:09 +0000

    Adding Sunpy specific warnings (inspired by astropy)

--- sunpy/wcs/__init__.py
+++ sunpy/wcs/__init__.py
@@ -53,8 +53,11 @@ References
 from __future__ import absolute_import
 
 import warnings
-from astropy.utils.exceptions import AstropyDeprecationWarning
+from sunpy.utils.exceptions import SunpyDeprecationWarning
 
-warnings.warn("As of v0.8.0, the `sunpy.wcs` module is deprecated and will be removed in a future version. Use `sunpy.coordinates` or `sunpy.map` for coordinate transformations.", AstropyDeprecationWarning)
+warnings.warn("As of v0.8.0, the `sunpy.wcs` module is deprecated and will be "
+              "removed in a future version. Use `sunpy.coordinates` or "
+              "`sunpy.map` for coordinate transformations.",
+              SunpyDeprecationWarning)
 
 from sunpy.wcs.wcs import *
