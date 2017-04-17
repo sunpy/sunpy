c60bea332d72d9e2171c1f65d829c8103f463eef
dpshelio <dps.helio@gmail.com>
2017-04-17 19:19:46 +0100

    EVE converted to use Scraper

--- sunpy/net/dataretriever/sources/eve.py
+++ sunpy/net/dataretriever/sources/eve.py
@@ -3,7 +3,10 @@
 # Google Summer of Code 2014
 
 from __future__ import absolute_import, division, print_function
+import datetime
 
+from sunpy.time import TimeRange
+from sunpy.util.scraper import Scraper
 from sunpy.extern.six.moves.urllib.parse import urljoin
 
 from ..client import GenericClient
@@ -39,7 +42,7 @@ class EVEClient(GenericClient):
 
         Parameters
         ----------
-        timerange: sunpy.time.TimeRange
+        timerange: `sunpy.time.TimeRange`
             time range for which data is to be downloaded.
 
         Returns
@@ -47,27 +50,26 @@ class EVEClient(GenericClient):
         urls : list
             list of URLs corresponding to the requested time range
         """
-        days = timerange.get_dates()
-        urls = []
-        for day in days:
-            urls.append(self._get_url_for_date(day, **kwargs))
-        return urls
+        baseurl = ('http://lasp.colorado.edu/eve/data_access/evewebdata/quicklook/'
+                   'L0CS/SpWx/%Y/%Y%m%d_EVE_L0CS_DIODES_1m.txt')
+        # If start of time range is before 00:00, converted to such, so
+        # files of the requested time ranger are included.
+        # This is done because the archive contains daily files.
+        if timerange.start.time() != datetime.time(0, 0):
+            timerange = TimeRange('{:%Y-%m-%d}'.format(timerange.start), timerange.end)
+        eve = Scraper(baseurl)
+        return eve.filelist(timerange)
 
-    def _get_url_for_date(self, date, **kwargs):
-        """
-        Return URL for corresponding date.
-
-        Parameters
-        ----------
-        date : Python datetime object
-
-        Returns
-        -------
-        URL : string
-        """
-        base_url = 'http://lasp.colorado.edu/eve/data_access/evewebdata/quicklook/L0CS/SpWx/'
-        return urljoin(base_url,
-                                date.strftime('%Y/%Y%m%d') + '_EVE_L0CS_DIODES_1m.txt')
+    def _get_time_for_url(self, urls):
+        baseurl = ('http://lasp.colorado.edu/eve/data_access/evewebdata/quicklook/'
+                   'L0CS/SpWx/%Y/%Y%m%d_EVE_L0CS_DIODES_1m.txt')
+        eve = Scraper(baseurl)
+        times = list()
+        for url in urls:
+            t0 = eve._extractDateURL(url)
+            # hard coded full day as that's the normal.
+            times.append(TimeRange(t0, t0 + datetime.timedelta(days=1)))
+        return times
 
     def _makeimap(self):
         """
