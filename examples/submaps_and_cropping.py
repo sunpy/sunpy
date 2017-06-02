ce6f122fdbdf4b9d6b947ebf00ac0741a0a9994f
Fin <fionnlaghdover1@hotmail.com>
2017-06-02 15:37:37 +0100

    Added plt.show() at end of code.

--- examples/submaps_and_cropping.py
+++ examples/submaps_and_cropping.py
@@ -16,6 +16,7 @@ import astropy.units as u
 
 import sunpy.map
 import sunpy.data.sample
+import matplotlib.pyplot as plt
 
 ##############################################################################
 # Sunpy sample data contains a number of suitable maps, where the sunpy.data.sample.NAME
@@ -39,3 +40,4 @@ rangex = u.Quantity([-900 * u.arcsec, 0 * u.arcsec])
 rangey = u.Quantity([-900 * u.arcsec, -200 * u.arcsec])
 swap_submap = swap_map.submap(rangex, rangey)
 swap_submap.peek(draw_limb=True, draw_grid=True)
+plt.show()
