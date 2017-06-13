02ccf48cf054a439ee627a4cb7e0e483d647bc74
Jack Ireland <jackireland@gmail.com>
2017-06-13 12:05:50 -0400

    added in more functions

--- sunpy/coordinates/great_arc.py
+++ sunpy/coordinates/great_arc.py
@@ -5,21 +5,22 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+import astropy.units as u
 from astropy.coordinates import SkyCoord
 import sunpy.coordinates
 
 
-def great_arc(start_point, end_point, center=None, number_points=100):
+def great_arc(start, end, center=None, number_points=100):
     """
     Calculate a user-specified number of points on a great arc between a start
     and end point on a sphere.
 
     Parameters
     ----------
-    start_point : `~astropy.coordinates.SkyCoord`
+    start : `~astropy.coordinates.SkyCoord`
         Start point.
 
-    end_point : `~astropy.coordinates.SkyCoord`
+    end : `~astropy.coordinates.SkyCoord`
         End point.
 
     center : `~astropy.coordinates.SkyCoord`
@@ -47,37 +48,82 @@ def great_arc(start_point, end_point, center=None, number_points=100):
     >>> b = SkyCoord(-100*u.arcsec, 800*u.arcsec, frame=m.coordinate_frame)
     >>> v = great_arc(a, b)
     """
-    start_point_unit = start_point.transform_to('heliocentric').cartesian.xyz.unit
-    if center is None:
-        c = SkyCoord(0*start_point_unit, 0*start_point_unit, 0*start_point_unit, frame='heliocentric')
-    a_cartesian = start_point.transform_to('heliocentric').cartesian.xyz.to(start_point_unit).value
-    b_cartesian = end_point.transform_to('heliocentric').cartesian.xyz.to(start_point_unit).value
-    c_cartesian = c.transform_to('heliocentric').cartesian.xyz.to(start_point_unit).value
+
+    gc = GreatArcConvertToCartesian(start, end, center)
 
     # Calculate the points along the great arc.
-    v_cartesian = calculate_great_arc(a_cartesian, b_cartesian, c_cartesian, number_points)*start_point_unit
+    v_cartesian = calculate_great_arc(gc.start_cartesian, gc.end_cartesian, gc.center_cartesian, number_points)*gc.start_unit
 
     # Transform the great arc back into the input frame.
     return SkyCoord(v_cartesian[:, 0], v_cartesian[:, 1], v_cartesian[:, 2],
-                    frame='heliocentric', observer=start_point.observer).transform_to(start_point.frame)
+                    frame='heliocentric', observer=gc.observer).transform_to(gc.start_frame)
 
 
-def calculate_great_arc(a_cartesian, b_cartesian, c_cartesian, number_points):
+def great_arc_distance(start, end, center=None):
     """
-    Calculate a user-specified number of points on a great arc between a start
-    and end point on a sphere where the start and end points are assumed to be
-    x,y,z Cartesian triples on a sphere relative to a center.  See the
-    references below for a description of the algorithm
+    Calculate the distance between the start point and end point on a sphere.
 
     Parameters
     ----------
-    a_cartesian : `~numpy.ndarray`
+    start : `~astropy.coordinates.SkyCoord`
+        Start point.
+
+    end : `~astropy.coordinates.SkyCoord`
+        End point.
+
+    center : `~astropy.coordinates.SkyCoord`
+        Center of the sphere.
+
+    Returns
+    -------
+    distance : `astropy.units.Quantity`
+        The distance between the two points on the sphere.
+    """
+    gc = GreatArcConvertToCartesian(start, end, center)
+    this_great_arc = CalculateGreatArcCartesian(gc.start_cartesian, gc.end_cartesian, gc.center_cartesian)
+    return this_great_arc.distance() * gc.start_unit
+
+
+def great_arc_angular_distance(start, end, center=None):
+    """
+    Calculate the angular distance between the start point and end point on a
+    sphere.
+
+    Parameters
+    ----------
+    start : `~astropy.coordinates.SkyCoord`
+        Start point.
+
+    end : `~astropy.coordinates.SkyCoord`
+        End point.
+
+    center : `~astropy.coordinates.SkyCoord`
+        Center of the sphere.
+
+    Returns
+    -------
+
+    """
+    gc = GreatArcConvertToCartesian(start, end, center)
+    this_great_arc = CalculateGreatArcCartesian(gc.start_cartesian, gc.end_cartesian, gc.center_cartesian)
+    return np.rad2deg(this_great_arc.inner_angle) * u.degree
+
+
+def calculate_great_arc(start_cartesian, end_cartesian, center_cartesian, number_points):
+    """
+    Calculate a user-specified number of points on a great arc between a start
+    and end point on a sphere where the start and end points are assumed to be
+    x,y,z Cartesian triples on a sphere relative to a center.
+
+    Parameters
+    ----------
+    start_cartesian : `~numpy.ndarray`
         Start point expressed as a Cartesian xyz triple.
 
-    b_cartesian : `~numpy.ndarray`
+    end_cartesian : `~numpy.ndarray`
         End point expressed as a Cartesian xyz triple.
 
-    c_cartesian : `~numpy.ndarray`
+    center_cartesian : `~numpy.ndarray`
         Center of the sphere expressed as a Cartesian xyz triple
 
     number_points : int
@@ -88,34 +134,114 @@ def calculate_great_arc(a_cartesian, b_cartesian, c_cartesian, number_points):
     arc : `~numpy.ndarray`
         Co-ordinates along the great arc expressed as Cartesian xyz triples.
         The shape of the array is (num, 3).
-
-    References
-    ----------
-    [1] https://www.mathworks.com/matlabcentral/newsreader/view_thread/277881
-    [2] https://en.wikipedia.org/wiki/Great-circle_distance#Vector_version
-
     """
-    center = np.asarray(c_cartesian)
-
-    # Vector from center to first point
-    v1 = np.asarray(a_cartesian) - center
-
-    # Distance of the first point from the center
-    r = np.linalg.norm(v1)
-
-    # Vector from center to second point
-    v2 = np.asarray(b_cartesian) - center
-
-    # The v3 lies in plane of v1 & v2 and is orthogonal to v1
-    v3 = np.cross(np.cross(v1, v2), v1)
-
-    # Ensure that the vector has length r
-    v3 = r * v3 / np.linalg.norm(v3)
+    this_great_arc = CalculateGreatArcCartesian(start_cartesian, end_cartesian, center_cartesian)
 
     # Range through the inner angle between v1 and v2
-    inner_angles = np.linspace(0, np.arctan2(np.linalg.norm(np.cross(v1, v2)),
-                                             np.dot(v1, v2)), num=number_points).reshape(number_points, 1)
+    inner_angles = np.linspace(0, this_great_arc.inner_angle, num=number_points).reshape(number_points, 1)
 
     # Calculate the Cartesian locations from the first to second points
-    return v1[np.newaxis, :] * np.cos(inner_angles) + \
-           v3[np.newaxis, :] * np.sin(inner_angles) + center
+    return this_great_arc.v1[np.newaxis, :] * np.cos(inner_angles) + \
+           this_great_arc.v3[np.newaxis, :] * np.sin(inner_angles) + \
+           this_great_arc.center
+
+
+class CalculateGreatArcCartesian:
+    def __init__(self, start_cartesian, end_cartesian, center_cartesian):
+        """
+        Calculate the properties of a great arc between a start point and an
+        end point on a sphere.  See the references below for a description of
+        the algorithm.
+
+        Parameters
+        ----------
+        start_cartesian : `~numpy.ndarray`
+            Start point expressed as a Cartesian xyz triple.
+
+        end_cartesian : `~numpy.ndarray`
+            End point expressed as a Cartesian xyz triple.
+
+        center_cartesian : `~numpy.ndarray`
+            Center of the sphere expressed as a Cartesian xyz triple
+
+        References
+        ----------
+        [1] https://www.mathworks.com/matlabcentral/newsreader/view_thread/277881
+        [2] https://en.wikipedia.org/wiki/Great-circle_distance#Vector_version
+
+        """
+        self.start_cartesian = start_cartesian
+        self.end_cartesian = end_cartesian
+        self.center_cartesian = center_cartesian
+        self.center = np.asarray(self.center_cartesian)
+
+        # Vector from center to first point
+        self.v1 = np.asarray(self.start_cartesian) - self.center
+
+        # Distance of the first point from the center
+        self.r = np.linalg.norm(self.v1)
+
+        # Vector from center to second point
+        self.v2 = np.asarray(self.end_cartesian) - self.center
+
+        # The v3 vector lies in plane of v1 & v2 and is orthogonal to v1
+        self.v3 = np.cross(np.cross(self.v1, self.v2), self.v1)
+        self.v3 = self.r * self.v3 / np.linalg.norm(self.v3)
+
+        # Inner angle between v1 and v2 in radians
+        self.inner_angle = np.arctan2(np.linalg.norm(np.cross(self.v1, self.v2)),
+                                      np.dot(self.v1, self.v2))
+
+    def distance(self):
+        """
+        Calculate the distance on the sphere between the start point and the end
+        point.
+
+        Returns
+        -------
+        distance : float
+            The distance on the sphere between the start point and the end
+            point.
+        """
+        return self.r * self.inner_angle
+
+
+class GreatArcConvertToCartesian:
+    def __init__(self, start, end, center=None):
+        """
+        A helper class that takes the inputs required to compute a great arc
+        and returns Cartesian triples, co-ordinate frame and unit information
+        required by other functions.
+
+        Parameters
+        ----------
+        start : `~astropy.coordinates.SkyCoord`
+            Start point.
+
+        end : `~astropy.coordinates.SkyCoord`
+            End point.
+
+        center : `~astropy.coordinates.SkyCoord`
+            Center of the sphere.
+        """
+        self.start = start
+        self.end = end
+        self.center = center
+
+        # Units of the start point
+        self.start_unit = self.start.transform_to('heliocentric').cartesian.xyz.unit
+
+        # Co-ordinate frame
+        self.start_frame = self.start.frame
+
+        # Observer details
+        self.observer = self.start.observer
+
+        if self.center is None:
+            self.c = SkyCoord(0*self.start_unit,
+                              0*self.start_unit,
+                              0*self.start_unit, frame='heliocentric')
+
+        self.start_cartesian = self.start.transform_to('heliocentric').cartesian.xyz.to(self.start_unit).value
+        self.end_cartesian = self.end.transform_to('heliocentric').cartesian.xyz.to(self.start_unit).value
+        self.center_cartesian = self.c.transform_to('heliocentric').cartesian.xyz.to(self.start_unit).value
