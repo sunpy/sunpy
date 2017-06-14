a829afd97b1dba2f7271fdc85ff2196bd45c7afa
Punyaslok Pattnaik <punyaslokpattnaik@yahoo.co.in>
2017-06-14 15:49:05 +0530

    Changes made corresponding to comments

--- examples/gallery/magnetogram_active_regions.py
+++ examples/gallery/magnetogram_active_regions.py
@@ -9,61 +9,60 @@ This example shows how to overplot Active Region location on magnetogram plots.
 ##############################################################################
 # Start by importing the necessary modules.
 
-from sunpy.net import Fido, attrs as a
-from astropy import units as u
-import sunpy.map
+import datetime
+
 import matplotlib.pyplot as plt
 import numpy as np
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 from sunpy.io.special import srs
 import sunpy.coordinates
-from astropy.coordinates import SkyCoord
+from sunpy.net import Fido, attrs as a
+import sunpy.map
+from sunpy.time import parse_time
 
 ##############################################################################
 # Let's select a date (yyyy-mm-dd) for which we will be downloading files.
 
-day = "2017-01-01"
+day = parse_time("2017-01-01")
 
 ##############################################################################
 # We will select a small time range to avoid downloading too many files.
 
-start_time = day + " 00:01:00"
-end_time = day + " 00:02:00"
+start_time = day + datetime.timedelta(minutes=1)
+end_time = day + datetime.timedelta(minutes=2)
 
 ##############################################################################
 # Send the search query.
 
 results = Fido.search(a.Time(start_time, end_time),
-						a.Instrument('HMI') & a.vso.Physobs("LOS_magnetic_field"),
- 						a.vso.Sample(60* u.second))
+					  a.Instrument('HMI') & a.vso.Physobs("LOS_magnetic_field"),
+ 					  a.vso.Sample(60* u.second))
 
 ##############################################################################
 # Download the files.
 
-downresp = Fido.fetch(results)
-#downresp = ['/home/punya/sunpy/data/hmi_m_45s_2017_01_01_00_02_15_tai_magnetogram.fits']
+downloaded_files = Fido.fetch(results)
 
 ##############################################################################
 # We will plot only one file in this example.
 
-file_name = downresp[0]
+file_name = downloaded_files[0]
 
 ##############################################################################
 # Now to download and read the SRS file.
-
-##############################################################################
 # Download the SRS file.
 
 srs_results = Fido.search(a.Time(start_time, end_time), a.Instrument('SOON'))
-#srs_downresp = ['/home/punya/sunpy/data/20170101SRS.txt']
-srs_downresp = Fido.fetch(srs_results)
+srs_downloaded_files = Fido.fetch(srs_results)
 
 ##############################################################################
 # We get one SRS file per day. So we pass the filename into the SRS reader. So
 # now `srs_table` contains an astropy table.
 
-srs_table = srs.read_srs(srs_downresp[0])
-print (srs_table)
+srs_table = srs.read_srs(srs_downloaded_files[0])
+print(srs_table)
 
 ##############################################################################
 # We only need the rows which have 'ID' = 'I' or 'IA'.
@@ -86,6 +85,7 @@ smap = sunpy.map.Map(file_name)
 
 im = smap.plot()
 ax = plt.gca()
+ax.set_autoscale_on(False)
 
 ##############################################################################
 # We make a SkyCoord object and plot the active points on the map.
@@ -97,7 +97,8 @@ ax.plot_coord(c, 'o')
 # Add the numbers as labels for each point.
 
 for i, num in enumerate(numbers):
-	ax.annotate(num, (lngs[i].value, lats[i].value), xycoords=ax.get_transform('heliographic_stonyhurst'))
+	ax.annotate(num, (lngs[i].value, lats[i].value),
+				xycoords=ax.get_transform('heliographic_stonyhurst'))
 
 ##############################################################################
 # Now we display the combined plot.
