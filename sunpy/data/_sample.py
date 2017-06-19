9b9dfdccd28f030a7d08bdfc42e8d45efd642bbf
Stuart Mumford <stuart@cadair.com>
2017-06-19 13:50:22 +0100

    Update mirrors for new sample data

--- sunpy/data/_sample.py
+++ sunpy/data/_sample.py
@@ -26,8 +26,7 @@ sampledata_dir = config.get("downloads", "sample_dir")
 # urls to search for the sample data
 _base_urls = (
     'http://data.sunpy.org/sample-data/',
-    'http://hesperia.gsfc.nasa.gov/~schriste/sunpy-sample-data/',
-    'https://github.com/ehsteve/sunpy-sample-data/raw/master/')
+    'https://github.com/sunpy/sample-data/raw/master/sample-data/')
 
 # keys are file shortcuts
 # values consist of filename as well as optional file extension if files are
