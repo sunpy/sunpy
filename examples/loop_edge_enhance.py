db2fcca6e950ce41ac55ea0d1ecf16a40562339e
Steven Christe <steven.d.christe@nasa.gov>
2017-06-23 14:49:43 -0400

    updates based on comments

--- examples/loop_edge_enhance.py
+++ examples/loop_edge_enhance.py
@@ -12,13 +12,13 @@ from scipy import ndimage
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 
-from sunpy.map import Map
+import sunpy.map
 from sunpy.data.sample import AIA_171_IMAGE
 
 ###############################################################################
 # We first create the Map using the sample data and make a submap of a region
 # with some interesting loop features.
-aia = Map(AIA_171_IMAGE)
+aia = sunpy.map.Map(AIA_171_IMAGE)
 bl = SkyCoord(750*u.arcsec, -200*u.arcsec, frame=aia.coordinate_frame)
 tr = SkyCoord(1500*u.arcsec, 550*u.arcsec, frame=aia.coordinate_frame)
 aia_smap = aia.submap(bl, tr)
@@ -31,12 +31,11 @@ edge_enhanced_im = np.hypot(sx, sy)
 
 ###############################################################################
 # Finally we create a new map with the edge enhanced data and plot the result.
-edge_map = Map(edge_enhanced_im, aia_smap.meta)
+edge_map = sunpy.map.Map(edge_enhanced_im, aia_smap.meta)
 
-plt.figure(figsize=(12, 4))
-plt.subplot(121)
+fig = plt.figure(figsize=(12, 4))
+ax = fig.add_subplot(121, projection=aia_smap)
 aia_smap.plot()
-plt.subplot(122)
+ax = fig.add_subplot(122, projection=aia_smap)
 edge_map.plot()
-plt.savefig("gallery_sharp.png")
-#plt.show()
+plt.show()
--- examples/off_limb_enhance.py
+++ examples/off_limb_enhance.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 from sunpy.data.sample import AIA_171_IMAGE
-from sunpy.map import Map
+import sunpy.map
 
 ###############################################################################
 # We first create the Map using the sample data.
-aia = Map(AIA_171_IMAGE)
+aia = sunpy.map.Map(AIA_171_IMAGE)
 
 ###############################################################################
 # Next we build two arrays which include all of the x and y pixel indices.
@@ -26,44 +26,48 @@ x, y = np.meshgrid(*[np.arange(v.value) for v in aia.dimensions]) * u.pix
 ###############################################################################
 # Now we can convert this to helioprojective coordinates and create a new
 # array which contains the normalized radial position for each pixel
-hpc_coords = aia.pixel_to_data(x,y)
+hpc_coords = aia.pixel_to_data(x, y)
 r = np.sqrt(hpc_coords.Tx ** 2 + hpc_coords.Ty ** 2) / aia.rsun_obs
 
 ###############################################################################
 # Let's check how emission above the limb depends on distance
 rsun_step_size = 0.01
 rsun_array = np.arange(1, r.max(), rsun_step_size)
-y = np.array([aia.data[(r > this_r) * (r < this_r + rsun_step_size)].mean() for this_r in rsun_array])
+y = np.array([aia.data[(r > this_r) * (r < this_r + rsun_step_size)].mean()
+              for this_r in rsun_array])
 
 ###############################################################################
 # Next let's plot it along with a fit to the data. We perform the fit in
 # log-linear space.
-params = np.polyfit(rsun_array[rsun_array<1.5], np.log10(y[rsun_array<1.5]), 1)
+params = np.polyfit(rsun_array[rsun_array < 1.5],
+                    np.log10(y[rsun_array < 1.5]), 1)
 
 plt.plot(rsun_array, y, label='data')
-plt.plot(rsun_array, 10**np.poly1d(params)(rsun_array), label=r'fit=Ax^{:.2f}'.format(params[0]))
+label = r'fit=Ax^{:.2f}'.format(params[0])
+plt.plot(rsun_array, 10**np.poly1d(params)(rsun_array), label=label)
 plt.yscale('log')
 plt.ylabel('mean DN')
 plt.xlabel('Rsun')
 plt.legend()
-plt.savefig("gallery_offlimb_dependence.png")
-#plt.show()
+plt.show()
 ###############################################################################
-# We now create our scaling array which we will multiply our original data by 5.
+# We now create our scaling array which we will multiply our original data by 5
 # In order to not affect the emission on the disk, we set the scale factor to
 # unity for values of r below 1.
 scale_exponent = 5
-scale_factor =  r ** scale_exponent
+scale_factor = r ** scale_exponent
 scale_factor[r < 1] = 1
 
 ###############################################################################
 # Let's now plot and compare the results.
-scaled_map = Map(aia.data * scale_factor, aia.meta)
+scaled_map = sunpy.map.Map(aia.data * scale_factor, aia.meta)
 norm = colors.Normalize(vmin=10, vmax=10000)
-plt.figure(figsize=(12,5))
-plt.subplot(1,2,1)
+
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot(121, projection=aia)
 aia.plot()
-plt.subplot(1,2,2)
+aia.draw_limb()
+ax = fig.add_subplot(122, projection=aia)
 scaled_map.plot()
-plt.savefig('gallery_offlimb_enhance.png')
-#plt.show()
+scaled_map.draw_limb()
+plt.show()
