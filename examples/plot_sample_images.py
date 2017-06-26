6906a4b0666216df46eebb51343f63d5b7f9d63a
Steven Christe <steven.d.christe@nasa.gov>
2017-06-25 22:37:08 -0400

    update to plot_sample_images

--- examples/plot_sample_images.py
+++ examples/plot_sample_images.py
@@ -8,18 +8,17 @@ This example shows off all of the sample images provided by SunPy.
 import matplotlib.pyplot as plt
 
 from sunpy.data.sample import file_dict
-from sunpy.map import Map
+import sunpy.map
 
 ###############################################################################
-# We create a loop through all of the sample shortcuts and picks out the images
-# that can be opened by Map. It then plots each one.
+# We create a loop through all of the sample shortcuts and pick out the images
+# that can be opened by Map. We then plot each one.
 for key in file_dict:
     if key.count('IMAGE'):
         try:
-            m = Map(file_dict[key])
+            m = sunpy.map.Map(file_dict[key])
             plt.figure()
             m.plot()
-            plt.savefig('{0}.png'.format(key))
-            #plt.show()
+            plt.show()
         except:
             pass
