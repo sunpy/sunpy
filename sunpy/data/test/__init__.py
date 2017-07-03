8f2c09ad8908b141e3b44d8ebded47307ead9cad
Swapnil Sharma <swap.sha96@gmail.com>
2017-07-03 17:13:28 +0530

    Function which lists all the available filenames.

--- sunpy/data/test/__init__.py
+++ sunpy/data/test/__init__.py
@@ -8,9 +8,13 @@ from astropy.utils.data import get_pkg_data_filename
 
 import sunpy
 
-__all__ = ['rootdir', 'file_list', 'get_test_filepath']
+import fnmatch
+import re
+
+__all__ = ['rootdir', 'file_list', 'get_test_filepath', 'test_data_filenames']
 
 rootdir = os.path.join(os.path.dirname(sunpy.__file__), "data", "test")
+
 
 def get_test_filepath(filename, **kwargs):
     """
@@ -42,3 +46,32 @@ def get_test_filepath(filename, **kwargs):
 
 
 file_list = glob.glob(os.path.join(rootdir, '*.[!p]*'))
+
+
+def test_data_filenames():
+    """
+    Return a list of all test files in ``data/test`` directory.
+
+    Return
+    ------
+    get_all_test_filepath : `list`
+        The name of all test files in ``data/test`` directory.
+
+    """
+    test_data_filenames_list = []
+    excludes = ['*.pyc', '*/__*__', '*.py']
+    excludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
+
+    for root, dirs, files in os.walk(rootdir):
+
+        dirs[:] = [os.path.join(root, d) for d in dirs]
+        dirs[:] = [d for d in dirs if not re.match(excludes, d)]
+
+        files = [os.path.join(root, f) for f in files]
+        files = [f for f in files if not re.match(excludes, f)]
+
+        files = [file.replace(rootdir + '/', '') for file in files]
+
+        test_data_filenames_list.extend(files)
+
+    return test_data_filenames_list
