12f24dadb648a63839ff30f6684937834fc23611
Albert Y. Shih <ayshih@gmail.com>
2017-07-12 18:47:24 -0400

    Added a utility get_earth() to obtain a SkyCoord for the Earth's location

--- sunpy/coordinates/__init__.py
+++ sunpy/coordinates/__init__.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import, division
 from .frames import *
 from .offset_frame import *
 from .transformations import *
+from .utilities import *
 
 from .wcs_utils import *
