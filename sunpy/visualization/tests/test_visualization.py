1596d86f0549d85e1a1e480fb22ede490873ce57
Stuart Mumford <stuart@cadair.com>
2017-07-14 15:15:18 +0100

    pep 8 fixes

--- sunpy/visualization/tests/test_visualization.py
+++ sunpy/visualization/tests/test_visualization.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author: Rajul Srivastava<rajul@gmail.com>
 
-#pylint: disable=W0613
-
 from __future__ import absolute_import
 
 from matplotlib import pyplot
@@ -10,26 +8,16 @@ from matplotlib import pyplot
 from sunpy.visualization import visualization
 
 
-__authors__ = ["Rajul Srivastava",]
-__email__ = ["rajul09@gmail.com",]
-
-
 def test_toggle_pylab():
-	def dummy_function(dummy_arg='foo'):
-		return dummy_arg
+    def dummy_function(dummy_arg='foo'):
+        return dummy_arg
 
-	assert pyplot.isinteractive() == False
+    assert pyplot.isinteractive() is False
 
-	fn = visualization.toggle_pylab(dummy_function)
-	assert fn == dummy_function
-	assert fn() == 'foo'
-	assert fn('bar') == 'bar'
+    fn = visualization.toggle_pylab(dummy_function)
+    assert fn == dummy_function
+    assert fn() == 'foo'
+    assert fn('bar') == 'bar'
 
-	pyplot.ion()
-	assert pyplot.isinteractive() == True
-	
-	fn_toggle = visualization.toggle_pylab(dummy_function)
-	assert fn == dummy_function
-	assert fn() == 'foo'
-	assert fn('bar') == 'bar'
-	assert pyplot.isinteractive() == True
+    pyplot.ion()
+    assert pyplot.isinteractive() is True
