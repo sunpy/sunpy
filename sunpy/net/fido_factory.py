13797e51279c48d52a8d49183ac6966389d01866
Stuart Mumford <stuart@cadair.com>
2017-07-16 18:31:56 +0100

    even more tweaks

--- sunpy/net/fido_factory.py
+++ sunpy/net/fido_factory.py
@@ -159,7 +159,11 @@ class UnifiedResponse(Sequence):
         return self._numfile
 
     def _repr_html_(self):
-        ret = 'Number of Providers: <{}>\n'.format(len(self.responses))
+        nprov = len(self)
+        if nprov == 1:
+            ret = 'Results from {} Provider:</br></br>'.format(len(self))
+        else:
+            ret = 'Results from {} Providers:</br></br>'.format(len(self))
         for block in self.responses:
             ret += "{} Results from the {}:</br>".format(len(block), block.client.__class__.__name__)
             ret += block._repr_html_()
