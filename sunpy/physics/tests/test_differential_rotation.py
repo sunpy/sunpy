2c0d896363a22c82a4ae9abd70eb54ebd515dc32
Jack Ireland <jackireland@gmail.com>
2017-07-28 18:40:30 -0400

    updating tests taking in to account the new calculations

--- sunpy/physics/tests/test_differential_rotation.py
+++ sunpy/physics/tests/test_differential_rotation.py
@@ -2,18 +2,21 @@ from __future__ import absolute_import
 import pytest
 import numpy as np
 from astropy import units as u
+from astropy.time import Time
 from astropy.coordinates import SkyCoord
 from astropy.coordinates import Longitude, Latitude, Angle
 from astropy.tests.helper import assert_quantity_allclose
 from sunpy.coordinates import frames
 from sunpy.coordinates.ephemeris import get_earth
 from sunpy.physics.differential_rotation import diff_rot, solar_rotate_coordinate
+from sunpy.time import parse_time
 
 #pylint: disable=C0103,R0904,W0201,W0212,W0232,E1103
 
 # Please note the numbers in these tests are not checked for physical
 # accuracy, only that they are the values the function was outputting upon
 # implementation.
+
 
 @pytest.fixture
 def seconds_per_day():
@@ -60,22 +63,27 @@ def test_fail(seconds_per_day):
         rot = diff_rot(10 * seconds_per_day, 30 * u.deg, rot_type='garbage')
 
 
-def test_solar_rotate_coordinate:
+def test_solar_rotate_coordinate():
     # Testing along the Sun-Earth line, observer is on the Earth
     obstime = '2010-09-10 12:34:56'
+    newtime = '2010-09-10 13:34:56'
     c = SkyCoord(-570*u.arcsec, 120*u.arcsec, obstime=obstime, observer=get_earth(obstime), frame=frames.Helioprojective)
-    d = solar_rotate_coordinate(c, '2010-09-10 13:34:56')
+    d = solar_rotate_coordinate(c, newtime)
 
     # Test that a SkyCoordinate is created
     assert isinstance(d, SkyCoord)
 
+    # Test the coordinate
+    np.testing.assert_almost_equal(d.Tx.to(u.arcsec).value, -562.3768, decimal=1)
+    np.testing.assert_almost_equal(d.Ty.to(u.arcsec).value, 119.2684, decimal=1)
+    np.testing.assert_almost_equal(d.distance.to(u.km).value, 150083151.9, decimal=1)
 
-    <SkyCoord (Helioprojective: obstime=2010-09-10 13:34:56, rsun=695508.0 km, observer=<HeliographicStonyhurst Coordinate (obstime=2010-09-10 13:34:56): (lon, lat, radius) in (deg, deg, AU)
-    ( 0.,  7.24822784,  1.00695436)>): (Tx, Ty, distance) in (arcsec, arcsec, km)
-    (-562.37689548,  119.26840368,   1.50083152e+08)>
-                   '2012-06-15', '2012-06-15 16:05:23')
-    np.testing.assert_almost_equal(x.to(u.arcsec).value, 574.2, decimal=1)
-    np.testing.assert_almost_equal(y.to(u.arcsec).value, -108.4, decimal=1)
-    # Test that astropy Angles are returned and that they have the expected
-    # units
+    # Test that the SkyCoordinate is Helioprojective
+    assert isinstance(d.frame, frames.Helioprojective)
 
+    # Test the observer
+    assert d.observer.obstime == Time(parse_time(newtime), scale='utc')
+    np.testing.assert_almost_equal(d.observer.lon.to(u.deg).value, 0.0, decimal=5)
+    np.testing.assert_almost_equal(d.observer.lat.to(u.deg).value, 7.248, decimal=3)
+    np.testing.assert_almost_equal(d.observer.radius.to(u.AU).value, 1.006954, decimal=6)
+    assert isinstance(d.observer, frames.HeliographicStonyhurst)
