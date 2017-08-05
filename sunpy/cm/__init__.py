3ec2a960cfc2791c36231e94a84acf0dcc5de657
Stuart Mumford <stuart@cadair.com>
2017-08-05 20:37:09 +0100

    Improve the colormap reference

--- sunpy/cm/__init__.py
+++ sunpy/cm/__init__.py
@@ -1,3 +1,10 @@
+"""
+The following colormaps are provided by this module:
+
+"""
 from __future__ import absolute_import
 
 from sunpy.cm.cm import *
+
+for cmname in cmlist.keys():
+    __doc__ += "* '`~sunpy.cm.cm.{}`'\n".format(cmname)
