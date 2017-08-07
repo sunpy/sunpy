160f73f477a318f1e88f11f590a738d49bf31517
Nitin Choudhary <nitin.iitkgp23@gmail.com>
2017-08-07 16:43:29 +0530

    Added more tests

--- sunpy/net/jsoc/tests/test_jsoc.py
+++ sunpy/net/jsoc/tests/test_jsoc.py
@@ -7,6 +7,7 @@ Created on Wed Mar 26 20:17:06 2014
 import os
 import tempfile
 import datetime
+import pandas as pd
 import astropy.table
 import astropy.time
 import astropy.units as u
@@ -62,7 +63,7 @@ def test_post_pass():
     responses = client.search(
         attrs.Time('2012/1/1T00:00:00', '2012/1/1T00:00:45'),
         attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'))
-    aa = client.request_data(responses, return_resp=True)
+    aa = client.request_data(responses)
     tmpresp = aa._d
     assert tmpresp['protocol'] == 'fits'
     assert tmpresp['method'] == 'url'
@@ -74,7 +75,7 @@ def test_post_wavelength():
         attrs.Time('2010/07/30T13:30:00', '2010/07/30T14:00:00'),
         attrs.Series('aia.lev1_euv_12s'), attrs.Wavelength(193 * u.AA) |
         attrs.Wavelength(335 * u.AA), attrs.Notify('jsoc@cadair.com'))
-    aa = client.request_data(responses, return_resp=True)
+    aa = client.request_data(responses)
     tmpresp = aa[0]._d
     assert tmpresp['protocol'] == 'fits'
     assert tmpresp['method'] == 'url'
@@ -126,53 +127,13 @@ def test_get_request():
     assert isinstance(aa, Results)
 
 
-@pytest.mark.online
-def test_results_filenames():
-    responses = client.search(
-        attrs.Time('2014/1/1T1:00:36', '2014/1/1T01:01:38'),
-        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'))
-    path = tempfile.mkdtemp()
-    aa = client.fetch(responses, path=path)
-    assert isinstance(aa, Results)
-    files = aa.wait(progress=False)
-    assert len(files) == len(responses)
-    for hmiurl in aa.map_:
-        assert os.path.isfile(hmiurl)
-
-
-@pytest.mark.online
+@pytest.mark.online         # passed
 def test_invalid_query():
     with pytest.raises(ValueError):
         client.search(attrs.Time('2012/1/1T01:00:00', '2012/1/1T01:00:45'))
 
 
-@pytest.mark.online
-def test_make_recordset():
-    d1 = {'end_time': datetime.datetime(2014, 1, 1, 1, 0, 35),
-          'series': 'aia.lev1_euv_12s',
-          'start_time': datetime.datetime(2014, 1, 1, 0, 0, 35)
-         }
-    r1 = 'aia.lev1_euv_12s[2014.01.01_00:00:35_TAI-2014.01.01_01:00:35_TAI]'
-    assert client._make_recordset(**d1) == r1
-    d1.update({'segment': 'image'})
-    r2 = 'aia.lev1_euv_12s[2014.01.01_00:00:35_TAI-2014.01.01_01:00:35_TAI]{image}'
-    assert client._make_recordset(**d1) == r2
-    d1.update({'segment': ['image'], 'wavelength': 304*u.AA})
-    r3 = 'aia.lev1_euv_12s[2014.01.01_00:00:35_TAI-2014.01.01_01:00:35_TAI][304]{image}'
-    assert client._make_recordset(**d1) == r3
-
-    d2 = {'end_time': datetime.datetime(2014, 1, 1, 1, 0, 35),
-          'series': 'hmi.sharp_720s',
-          'start_time': datetime.datetime(2014, 1, 1, 0, 0, 35)
-         }
-    d2.update({'primekey': {'HARPNUM': '4864'}})
-    r4 = 'hmi.sharp_720s[4864][2014.01.01_00:00:35_TAI-2014.01.01_01:00:35_TAI]'
-    assert client._make_recordset(**d2) == r4
-    d2['primekey'] = {'HARPNUM': '4864','Foo': '123'}
-    assert client._make_recordset(**d2) == r4
-
-
-@pytest.mark.online
+@pytest.mark.online          # PASSED
 def test_lookup_records_errors():
     d1 = {'end_time': datetime.datetime(2014, 1, 1, 1, 0, 35),
           'start_time': datetime.datetime(2014, 1, 1, 0, 0, 35)
@@ -198,3 +159,57 @@ def test_lookup_records_errors():
     d1.update({'segment': 'foo'})
     with pytest.raises(TypeError):
         client._lookup_records(d1)
+
+
+@pytest.mark.online
+def test_search_metadata():
+    metadata = client.search_metadata(attrs.Time('2014-01-01T00:00:00', '2014-01-01T00:02:00'),
+                                      attrs.Series('aia.lev1_euv_12s'), attrs.Wavelength(304*u.AA))
+    assert isinstance(metadata, pd.DataFrame)
+    assert metadata.shape == (11, 176)
+    for i in metadata.index.values:
+        assert (i.startswith('aia.lev1_euv_12s') and i.endswith('[304]'))
+
+@pytest.mark.online
+def test_request_data_error():
+    responses = client.query(
+        attrs.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'),
+        attrs.Protocol('foo'))
+    with pytest.raises(TypeError):
+        req = client.request_data(responses)
+
+
+@pytest.mark.online
+def test_request_data_protocol():
+    responses = client.query(
+        attrs.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'))
+    req = client.request_data(responses)
+    assert req._d['method'] == 'url'
+    assert req._d['protcol'] == 'fits'
+
+    responses = client.query(
+        attrs.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'),
+        attrs.Protocol('fits'))
+    req = client.request_data(responses)
+    assert req._d['method'] == 'url'
+    assert req._d['protcol'] == 'fits'
+
+    responses = client.query(
+        attrs.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'),
+        attrs.Protocol('as-is'))
+    req = client.request_data(responses)
+    assert req._d['method'] == 'url_quick'
+    assert req._d['protcol'] == 'as-is'
+
+
+@pytest.mark.online
+def test_check_request():
+    responses = client.query(
+        attrs.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        attrs.Series('hmi.M_45s'), attrs.Notify('jsoc@cadair.com'))
+    req = client.request_data(responses)
+    assert client.check_request(req) == 0
