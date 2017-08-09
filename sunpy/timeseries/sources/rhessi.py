b240407b222157e01eea4aaaca0b8c7d18995224
Jack Ireland <jackireland@gmail.com>
2017-08-09 12:57:57 -0400

    removing sources from examples (not needed because they are FITS files)

--- sunpy/timeseries/sources/rhessi.py
+++ sunpy/timeseries/sources/rhessi.py
@@ -47,7 +47,7 @@ class RHESSISummaryTimeSeries(GenericTimeSeries):
     --------
     >>> import sunpy.data.sample
     >>> import sunpy.timeseries
-    >>> rhessi = sunpy.timeseries.TimeSeries(sunpy.data.sample.RHESSI_TIMESERIES, source='RHESSI')
+    >>> rhessi = sunpy.timeseries.TimeSeries(sunpy.data.sample.RHESSI_TIMESERIES)
     >>> rhessi.peek()   # doctest: +SKIP
 
     References
