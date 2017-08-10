2efb9c3a87a3679f6b5b0aca9cb456f7cd1621b1
Nabil Freij <nabil.freij@gmail.com>
2017-08-10 09:55:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -226,7 +226,8 @@ else:
                 'matplotlib': 'http://matplotlib.org/',
                 'numpy': 'http://docs.scipy.org/doc/numpy/',
             },
-            'abort_on_example_error': True
+            'abort_on_example_error': True,
+            'plot_gallery': False
         }
 
     except ImportError:
