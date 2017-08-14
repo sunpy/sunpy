8964860be0b0b5f79cf2a13f19445edb0608bdbf
Nabil <nabil.freij@gmail.com>
2017-08-14 17:54:01 +0200

    removal of folder for CI that openjpeg uses

--- continuous-integration/travis/openjpeg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create the glymur config file to set the path for the openjpeg library
-mkdir -p $HOME/.config/glymur
-cat << EOF > $HOME/.config/glymur/glymurrc
-[library]
-openjp2:  $HOME/miniconda/envs/test/lib/libopenjp2.so
-EOF
