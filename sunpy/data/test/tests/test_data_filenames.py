9d9b757b68958b47b4d71cab70e50773af39b6d4
Nabobalis <nabil.freij@gmail.com>
2017-10-05 09:57:19 +0200

    Minor changes to the test and __init__ file

--- sunpy/data/test/tests/test_data_filenames.py
+++ sunpy/data/test/tests/test_data_filenames.py
@@ -1,27 +1,17 @@
-import pytest
 import os
+import pytest
 import sunpy.data.test
 
 
 @pytest.fixture
 def mockreturn(path):
-    return_value = [
-        ('/universe', ('solar_system',), ()),
-        ('/universe/solar_system', (), ('sun.py', 'earth', 'moon.pyc')),
-    ]
-    return return_value
+    return [(os.path.join('test', 'data', ''), (), ('code.py', 'test_file', 'code.pyc', '__init__.py'))]
 
 
 def test_test_data_filenames(monkeypatch):
     monkeypatch.setattr(os, 'walk', mockreturn)
     monkeypatch.setattr(os.path, 'isfile', mockreturn)
     output = sunpy.data.test.test_data_filenames()
-    all_exist = True
-    for file in output:
-        if not os.path.isfile(file):
-            all_exist = False
-            break
-    new_op = []
-    for path in output:
-        new_op.append(path.replace('\\', '/'))  # Handle in case of system = win32
-    assert isinstance(new_op, list) and new_op == ['/universe/solar_system/earth'] and all_exist
+    assert isinstance(output, list)
+    # Only the test file and not the py/pyc files should be in the return.
+    assert output == [os.path.join('test', 'data', '', 'test_file')]
