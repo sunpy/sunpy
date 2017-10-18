d92caa574b4b350edc5ba5a4af7445176c9db270
Prateek Chanda <prateekkol21@gmail.com>
2017-10-18 19:43:10 +0530

    Update imageanimator.py
    User can now define the units for ImageAnimatorWCS. Default will be u.km.

--- sunpy/visualization/imageanimator.py
+++ sunpy/visualization/imageanimator.py
@@ -9,6 +9,7 @@ import matplotlib.animation as mplanim
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import mpl_toolkits.axes_grid1.axes_size as Size
 import astropy.wcs
+import astropy.units as u
 
 from sunpy.extern import six
 from sunpy.extern.six.moves import range
@@ -936,7 +937,9 @@ class ImageAnimatorWCS(ImageAnimator):
     Extra keywords are passed to imshow.
 
     """
-    def __init__(self, data, wcs=None, image_axes=[-1, -2], unit_x_axis=None, unit_y_axis=None,
+    
+    @u.quantity_input(unit_x_axis= u.km , unit_y_axis =u.km)
+    def __init__(self, data, wcs=None, image_axes=[-1, -2], unit_x_axis, unit_y_axis,
                  axis_ranges=None, **kwargs):
         if not isinstance(wcs, astropy.wcs.WCS):
             raise ValueError("wcs data should be provided.")
@@ -949,7 +952,7 @@ class ImageAnimatorWCS(ImageAnimator):
         self.slices_wcsaxes = list_slices_wcsaxes[::-1]
         self.unit_x_axis = unit_x_axis
         self.unit_y_axis = unit_y_axis
-        super(ImageAnimatorWCS, self).__init__(data, image_axes=image_axes, axis_ranges=axis_ranges, **kwargs)
+        super(ImageAnimatorWCS, self).__init__(data, image_axes=image_axes, axis_ranges=axis_ranges,unit_x_axis, unit_y_axis , **kwargs)
 
     def _get_main_axes(self):
         axes = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], projection=self.wcs, slices=self.slices_wcsaxes)
@@ -957,12 +960,12 @@ class ImageAnimatorWCS(ImageAnimator):
         return axes
 
     def _set_unit_in_axis(self, axes):
-        if self.unit_x_axis is not None:
-            axes.coords[2].set_format_unit(self.unit_x_axis)
-            axes.coords[2].set_ticks(exclude_overlapping=True)
-        if self.unit_y_axis is not None:
-            axes.coords[1].set_format_unit(self.unit_y_axis)
-            axes.coords[1].set_ticks(exclude_overlapping=True)
+        
+        axes.coords[2].set_format_unit(self.unit_x_axis)
+        axes.coords[2].set_ticks(exclude_overlapping=True)
+
+        axes.coords[1].set_format_unit(self.unit_y_axis)
+        axes.coords[1].set_ticks(exclude_overlapping=True)
 
 
     def plot_start_image(self, ax):
