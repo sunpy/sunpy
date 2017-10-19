87d6632c2dd96cd2422788e19627a3eabb80d27d
Prateek Chanda <prateekkol21@gmail.com>
2017-10-19 09:31:07 +0530

    An extra function to get user input for label
    User may not give an input for the unit. So parameter set back to None.
    A new function declared to take set the input unit to corresponding astropy.units

--- sunpy/visualization/imageanimator.py
+++ sunpy/visualization/imageanimator.py
@@ -939,7 +939,7 @@ class ImageAnimatorWCS(ImageAnimator):
     """
     
     
-    def __init__(self, data, unit_x_axis, unit_y_axis, wcs=None, image_axes=[-1, -2], 
+    def __init__(self, data, unit_x_axis=None, unit_y_axis=None, wcs=None, image_axes=[-1, -2], 
                  axis_ranges=None, **kwargs):
         if not isinstance(wcs, astropy.wcs.WCS):
             raise ValueError("wcs data should be provided.")
@@ -952,20 +952,29 @@ class ImageAnimatorWCS(ImageAnimator):
         self.slices_wcsaxes = list_slices_wcsaxes[::-1]
         self.unit_x_axis = unit_x_axis
         self.unit_y_axis = unit_y_axis
-        super(ImageAnimatorWCS, self).__init__(data,  unit_x_axis, unit_y_axis , image_axes=image_axes, axis_ranges=axis_ranges, **kwargs)
+        super(ImageAnimatorWCS, self).__init__(data, image_axes=image_axes, axis_ranges=axis_ranges, **kwargs)
 
     def _get_main_axes(self):
         axes = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], projection=self.wcs, slices=self.slices_wcsaxes)
         self._set_unit_in_axis(axes)
         return axes
 
-    def _set_unit_in_axis(self, axes):
+    def _get_unit_from_user(self):
+        if self.unit_x_axis is not None:
+            self.unit_x_axis = u.Unit(self.unit_x_axis)
+        if self.unit_y_axis is not None:
+            self.unit_y_axis = u.Unit(self.unit_y_axis)
         
-        axes.coords[2].set_format_unit(self.unit_x_axis)
-        axes.coords[2].set_ticks(exclude_overlapping=True)
-
-        axes.coords[1].set_format_unit(self.unit_y_axis)
-        axes.coords[1].set_ticks(exclude_overlapping=True)
+        
+    
+    def _set_unit_in_axis(self, axes):
+        if self.unit_x_axis is not None:
+            axes.coords[2].set_format_unit(self.unit_x_axis)
+            axes.coords[2].set_ticks(exclude_overlapping=True)
+ 
+        if self.unit_y_axis is not None:
+            axes.coords[1].set_format_unit(self.unit_y_axis)
+            axes.coords[1].set_ticks(exclude_overlapping=True)
 
 
     def plot_start_image(self, ax):
