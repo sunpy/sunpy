ae2d6fd96cd544ed783d8cb6bae2e2df69e43adc
dpshelio <dps.helio@gmail.com>
2017-10-21 19:12:53 +0100

    styling fixes

--- sunpy/net/dataretriever/sources/kanzelhohe.py
+++ sunpy/net/dataretriever/sources/kanzelhohe.py
@@ -1,5 +1,5 @@
-#This module was developed with funding provided by
-#the Google Summer of Code 2016.
+# This module was developed with funding provided by
+# the Google Summer of Code 2016.
 """
 This module implements Kanzelhohe Client.
 """
@@ -14,6 +14,7 @@ from sunpy.util.scraper import Scraper
 from sunpy.net import attrs as a
 
 __all__ = ['KanzelhoheClient']
+
 
 class KanzelhoheClient(GenericClient):
     """
@@ -59,16 +60,19 @@ class KanzelhoheClient(GenericClient):
         """
         returns list of urls corresponding to given TimeRange.
         """
-        wave_float = (kwargs['wavelength'].min if isinstance(kwargs['wavelength'], a.Wavelength) else kwargs['wavelength'].wavemin).value
-        table = {6563:['halpha2k/recent', 'halph_fr'], 32768:['caiia', 'caiik_fi'], 5460:['phokada', 'bband_fi']}
-        #Checking if value is close enough to a wavelength value.
-        #Converting from one unit to other introduces precision errors.
+        wave = kwargs['wavelength']
+        wave_float = (wave.min if isinstance(wave, a.Wavelength) else wave.wavemin).value
+        table = {6563: ['halpha2k/recent', 'halph_fr'],
+                 32768: ['caiia', 'caiik_fi'],
+                 5460: ['phokada', 'bband_fi']}
+        # Checking if value is close enough to a wavelength value.
+        # Converting from one unit to other introduces precision errors.
         try:
-            #The isclose function in numpy can only check two arrays if they
-            #are close to each other within some precision. Standalone values such
-            #as int, doubles etc can't be checked that way. In order to do that, we put the
-            #two indiviual values in two seperate arrays da and db and then apply
-            #isclose on both those arrays.
+            # The isclose function in numpy can only check two arrays if they
+            # are close to each other within some precision. Standalone values such
+            # as int, doubles etc can't be checked that way. In order to do that, we put the
+            # two indiviual values in two seperate arrays da and db and then apply
+            # isclose on both those arrays.
             da = list()
             da.append(wave_float)
             for wave_nums in table.keys():
@@ -77,11 +81,13 @@ class KanzelhoheClient(GenericClient):
                 if np.isclose(da, db, 1e-10, 1e-10):
                     wave = wave_nums
 
-            date_table = {6563: datetime.datetime(2000, 7, 20, 7, 45, 46), 5460: datetime.datetime(2011, 1, 7, 10, 7, 33),
+            date_table = {6563: datetime.datetime(2000, 7, 20, 7, 45, 46),
+                          5460: datetime.datetime(2011, 1, 7, 10, 7, 33),
                           32768: datetime.datetime(2010, 7, 31, 8, 10, 59)}
             START_DATE = date_table[wave]
             if timerange.start < START_DATE:
-                raise ValueError('Earliest date for which Kanzelhohe data is available is {:%Y-%m-%d}'.format(START_DATE))
+                msg = 'Earliest date for which Kanzelhohe data is available is {:%Y-%m-%d}'
+                raise ValueError(msg.format(START_DATE))
             prefix = "http://cesar.kso.ac.at/{datatype}/%Y/"
             suffix = ""
             if wave != 6563:
@@ -99,7 +105,7 @@ class KanzelhoheClient(GenericClient):
         """
         Helper Function:used to hold information about source.
         """
-        self.map_['source'] = 'Global Halpha Network' #TODO: check with kanzelhohe
+        self.map_['source'] = 'Global Halpha Network'  # TODO: check with kanzelhohe
         self.map_['instrument'] = 'Kanzelhohe HA2'
         self.map_['phyobs'] = 'irradiance'
         self.map_['provider'] = 'Kanzelhohe'
@@ -118,14 +124,10 @@ class KanzelhoheClient(GenericClient):
         boolean: answer as to whether client can service the query
 
         """
-        chkattr = ['Time', 'Instrument', 'Wavelength']
-        chklist = [x.__class__.__name__ in chkattr for x in query]
         chk_var = 0
-        values = [6563, 5460, 32768]
         for x in query:
             if x.__class__.__name__ == 'Instrument' and type(x.value) is str and x.value.lower() == 'kanzelhohe':
                 chk_var += 1
             if x.__class__.__name__ == 'Wavelength':
                 chk_var += 1
         return chk_var == 2
-
--- sunpy/net/dataretriever/tests/test_kanzelhohe.py
+++ sunpy/net/dataretriever/tests/test_kanzelhohe.py
@@ -1,67 +1,78 @@
 """
 This module tests the Kanzelhohe Client
 """
-#This module was developed with funding provided by
-#the Google Summer of Code 2016.
+# This module was developed with funding provided by
+# the Google Summer of Code 2016.
 import pytest
 import datetime
 
 from astropy import units as u
 from sunpy.time.timerange import TimeRange
-from sunpy.net.vso.attrs import Time, Instrument, Wavelength
+from sunpy.net import attrs as a
 from sunpy.net.dataretriever.client import QueryResponse
 from sunpy.net.fido_factory import UnifiedResponse
 from sunpy.net import Fido
-from sunpy.net import attrs as a
 import sunpy.net.dataretriever.sources.kanzelhohe as kanzelhohe
 
 KClient = kanzelhohe.KanzelhoheClient()
 
+
 @pytest.mark.online
 @pytest.mark.parametrize("timerange, wavelength, url_start, url_end",
-                         [(TimeRange('2015/01/10 00:00:00', '2015/01/10 12:00:00'), Wavelength(6563*u.AA),
-                           'http://cesar.kso.ac.at/halpha2k/recent/2015/kanz_halph_fr_20150110_102629.fts.gz',
-                           'http://cesar.kso.ac.at/halpha2k/recent/2015/kanz_halph_fr_20150110_113524.fts.gz')])
+                         [(TimeRange('2015/01/10 00:00:00', '2015/01/10 12:00:00'),
+                           a.Wavelength(6563 * u.AA),
+                           '2015/kanz_halph_fr_20150110_102629.fts.gz',
+                           '2015/kanz_halph_fr_20150110_113524.fts.gz')])
 def test_get_url_for_timerange(timerange, wavelength, url_start, url_end):
-    urls = KClient._get_url_for_timerange(timerange, wavelength = wavelength)
+    urls = KClient._get_url_for_timerange(timerange, wavelength=wavelength)
     assert isinstance(urls, list)
-    assert urls[0] == url_start
-    assert urls[1] == url_end
-TRANGE = Time('2015/12/30 00:00:00', '2015/12/31 00:05:00')
+    assert urls[0] == "http://cesar.kso.ac.at/halpha2k/recent/" + url_start
+    assert urls[1] == "http://cesar.kso.ac.at/halpha2k/recent/" + url_end
+
+
+TRANGE = a.Time('2015/12/30 00:00:00', '2015/12/31 00:05:00')
+
+
 @pytest.mark.parametrize("time, instrument, wavelength, expected",
-                         [(TRANGE, Instrument('kanzelhohe'), Wavelength(6563*u.AA), True),
-                          (TRANGE, Instrument('swap'), None, False),
+                         [(TRANGE, a.Instrument('kanzelhohe'), a.Wavelength(6563*u.AA), True),
+                          (TRANGE, a.Instrument('swap'), None, False),
                           (TRANGE, None, None, False),
-                          (TRANGE, Instrument('kanzelhohe'), Wavelength(32768*u.AA), True)])
+                          (TRANGE, a.Instrument('kanzelhohe'), a.Wavelength(32768*u.AA), True)])
 def test_can_handle_query(time, instrument, wavelength, expected):
     assert KClient._can_handle_query(time, instrument, wavelength) is expected
 
+
 @pytest.mark.online
 def test_query():
-    qr = KClient.search(Time('2015/01/10 00:00:00', '2015/01/10 12:00:00'), a.Wavelength(6563*u.AA))
+    qr = KClient.search(a.Time('2015/01/10 00:00:00', '2015/01/10 12:00:00'),
+                        a.Wavelength(6563 * u.AA))
     assert isinstance(qr, QueryResponse)
     assert len(qr) == 2
     assert qr.time_range().start.date() == datetime.date(2015, 1, 10)
     assert qr.time_range().end.date() == datetime.date(2015, 1, 10)
 
+
+# This test downloads 3 files
+# Each file is 4.5MB, total size
+# is 13.4MB
 @pytest.mark.online
 @pytest.mark.parametrize("time, wavelength",
-[(Time('2015/01/02 07:30:00', '2015/01/02 07:38:00'), Wavelength(3276.8*u.nm))])
-#This test downloads 3 files
-#Each file is 4.5MB, total size
-#is 13.4MB
+                         [(a.Time('2015/01/02 07:30:00', '2015/01/02 07:38:00'),
+                           a.Wavelength(3276.8 * u.nm))])
 def test_get(time, wavelength):
     qr = KClient.search(time, wavelength)
     res = KClient.get(qr)
     download_list = res.wait()
     assert len(download_list) == len(qr)
 
-##This test downloads 3 files
-##Each file is 4.5MB, total size
-##is 13.4MB
+
+# This test downloads 3 files
+# Each file is 4.5MB, total size
+# is 13.4MB
 @pytest.mark.online
 def test_fido_query():
-    qr = Fido.search(a.Time('2016/01/05 07:30:00', '2016/01/05 07:38:00'), a.Instrument('kanzelhohe'), a.Wavelength(546.0*u.nm))
+    qr = Fido.search(a.Time('2016/01/05 07:30:00', '2016/01/05 07:38:00'),
+                     a.Instrument('kanzelhohe'), a.Wavelength(546.0 * u.nm))
     assert isinstance(qr, UnifiedResponse)
     response = Fido.fetch(qr)
     assert len(response) == qr._numfile
