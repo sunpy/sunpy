490ffc550a93b54006cbe9772af7d540738e3e43
dpshelio <dps.helio@gmail.com>
2017-10-21 18:29:18 +0100

    Fixed kanzelhohe to the latest updates of fido

--- sunpy/net/dataretriever/tests/test_kanzelhohe.py
+++ sunpy/net/dataretriever/tests/test_kanzelhohe.py
@@ -4,12 +4,13 @@ This module tests the Kanzelhohe Client
 #This module was developed with funding provided by
 #the Google Summer of Code 2016.
 import pytest
+import datetime
 
 from astropy import units as u
 from sunpy.time.timerange import TimeRange
 from sunpy.net.vso.attrs import Time, Instrument, Wavelength
 from sunpy.net.dataretriever.client import QueryResponse
-from sunpy.net.dataretriever.downloader_factory import UnifiedResponse
+from sunpy.net.fido_factory import UnifiedResponse
 from sunpy.net import Fido
 from sunpy.net import attrs as a
 import sunpy.net.dataretriever.sources.kanzelhohe as kanzelhohe
@@ -37,11 +38,11 @@ def test_can_handle_query(time, instrument, wavelength, expected):
 
 @pytest.mark.online
 def test_query():
-    qr = KClient.query(Time('2015/01/10 00:00:00', '2015/01/10 12:00:00'), Wavelength(6563*u.AA))
+    qr = KClient.search(Time('2015/01/10 00:00:00', '2015/01/10 12:00:00'), a.Wavelength(6563*u.AA))
     assert isinstance(qr, QueryResponse)
     assert len(qr) == 2
-    assert qr.time_range()[0] == '2015/01/10'
-    assert qr.time_range()[1] == '2015/01/10'
+    assert qr.time_range().start.date() == datetime.date(2015, 1, 10)
+    assert qr.time_range().end.date() == datetime.date(2015, 1, 10)
 
 @pytest.mark.online
 @pytest.mark.parametrize("time, wavelength",
@@ -50,7 +51,7 @@ def test_query():
 #Each file is 4.5MB, total size
 #is 13.4MB
 def test_get(time, wavelength):
-    qr = KClient.query(time, wavelength)
+    qr = KClient.search(time, wavelength)
     res = KClient.get(qr)
     download_list = res.wait()
     assert len(download_list) == len(qr)
@@ -58,7 +59,7 @@ def test_get(time, wavelength):
 ##This test downloads 3 files
 ##Each file is 4.5MB, total size
 ##is 13.4MB
-##@pytest.mark.online
+@pytest.mark.online
 def test_fido_query():
     qr = Fido.search(a.Time('2016/01/05 07:30:00', '2016/01/05 07:38:00'), a.Instrument('kanzelhohe'), a.Wavelength(546.0*u.nm))
     assert isinstance(qr, UnifiedResponse)
