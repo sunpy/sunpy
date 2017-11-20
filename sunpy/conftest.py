396f3835fefd19e5bc1b33d90ca86e7070cd0908
Stuart Mumford <stuart@cadair.com>
2017-11-20 17:53:01 +0000

    Change to using pytest-remotedata for out online tests

--- sunpy/conftest.py
+++ sunpy/conftest.py
@@ -2,14 +2,8 @@ from __future__ import absolute_import, print_function
 from functools import partial
 
 import os
-import socket
 import tempfile
 import json
-
-from sunpy.extern.six.moves.urllib.request import urlopen
-from sunpy.extern.six.moves.urllib.error import URLError
-
-import pytest
 
 # Force MPL to use non-gui backends for testing.
 try:
@@ -19,42 +13,8 @@ except ImportError:
 else:
     matplotlib.use('Agg')
 
-from astropy.tests import disable_internet
 from sunpy.tests.hash import HASH_LIBRARY_NAME
 from sunpy.tests.helpers import new_hash_library, figure_test_pngfiles
-
-GOOGLE_URL = 'http://www.google.com'
-
-
-def site_reachable(url):
-    try:
-        urlopen(url, timeout=1)
-    except (URLError, socket.timeout):
-        return False
-    else:
-        return True
-
-
-is_online = partial(site_reachable, GOOGLE_URL)
-
-
-def pytest_runtest_setup(item):
-    """
-    pytest hook to skip all tests that have the mark 'online' if the
-    client is online (simply detected by checking whether http://www.google.com
-    can be requested).
-    """
-    if isinstance(item, item.Function):
-        if 'online' in item.keywords and not is_online():
-            msg = 'skipping test {0} (reason: client seems to be offline)'
-            pytest.skip(msg.format(item.name))
-
-        if 'online' not in item.keywords:
-            disable_internet.turn_off_internet()
-
-
-def pytest_runtest_teardown(item, nextitem):
-    disable_internet.turn_on_internet()
 
 
 def pytest_unconfigure(config):
--- sunpy/lightcurve/tests/test_rhessi.py
+++ sunpy/lightcurve/tests/test_rhessi.py
@@ -20,25 +20,25 @@ class TestRHESSISummaryLightCurve(object):
     def timerange_b(self):
         return TimeRange('2004/06/03', '2004/06/04')
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_hsi_range(self, timerange_a):
         """Test creation with two times"""
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create(timerange_a.start, timerange_a.end)
         assert isinstance(lc1, sunpy.lightcurve.RHESSISummaryLightCurve)
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_hsi_timerange(self, timerange_a):
         """Test creation with a TimeRange"""
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create(timerange_a)
         assert isinstance(lc1, sunpy.lightcurve.RHESSISummaryLightCurve)
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_hsi_default(self):
         """Test creation with no input"""
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create()
         assert isinstance(lc1, sunpy.lightcurve.RHESSISummaryLightCurve)
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_data(self, timerange_a, timerange_b):
         """Test presence of data"""
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create(timerange_b)
@@ -46,20 +46,20 @@ class TestRHESSISummaryLightCurve(object):
         assert not lc1.data.empty
         assert not lc2.data.empty
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_header(self, timerange_b):
         """Test presence of TELESCOP in header"""
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create(timerange_b)
         assert lc1.header['TELESCOP'] == 'HESSI'
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_hsi_url(self):
         """Test creation with url"""
         url = 'http://soleil.i4ds.ch/hessidata/metadata/catalog/hsi_obssumm_20030302_146.fits'
         lc1 = sunpy.lightcurve.RHESSISummaryLightCurve.create(url)
         assert isinstance(lc1, sunpy.lightcurve.RHESSISummaryLightCurve)
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_filename(self, timerange_a, timerange_b):
         """Compare data from two different time ranges to make
         sure they are not the same"""
@@ -68,7 +68,7 @@ class TestRHESSISummaryLightCurve(object):
         with pytest.raises(AssertionError):
             np.testing.assert_allclose(lc1, lc2)
 
-    @pytest.mark.online
+    @pytest.mark.remote_data
     def test_get_url(self, timerange_a, timerange_b):
         """Test the getting of urls"""
         g = sunpy.lightcurve.RHESSISummaryLightCurve
