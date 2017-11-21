4bec71ace023e5456e60c1f57be93d1e34a6929e
Stuart Mumford <stuart@cadair.com>
2017-11-21 16:58:26 +0000

    skip remote data tests if pytest_remotedata is not installed

--- sunpy/conftest.py
+++ sunpy/conftest.py
@@ -15,6 +15,35 @@ else:
 
 from sunpy.tests.hash import HASH_LIBRARY_NAME
 from sunpy.tests.helpers import new_hash_library, figure_test_pngfiles
+from sunpy.extern import six
+
+import pytest
+
+
+# Don't actually import pytest_remotedata because that can do things to the
+# entrypoints code in pytest.
+if six.PY2:
+    import imp
+    try:
+        imp.find_module('pytest_remotedata')
+        HAVE_REMOTEDATA = True
+    except ImportError:
+        HAVE_REMOTEDATA = False
+else:
+    import importlib
+    remotedata_spec = importlib.util.find_spec("pytest_remotedata")
+    HAVE_REMOTEDATA = remotedata_spec is not None
+
+
+def pytest_runtest_setup(item):
+    """
+    pytest hook to skip all tests that have the mark 'online' if the
+    client is online (simply detected by checking whether http://www.google.com
+    can be requested).
+    """
+    if isinstance(item, item.Function):
+        if 'remote_data' in item.keywords and not HAVE_REMOTEDATA:
+            pytest.skip("skipping remotedata tests as pytest-remotedata is not installed")
 
 
 def pytest_unconfigure(config):
