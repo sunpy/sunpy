9e90f37b6d9598a1039c7896aef6925f5769e013
osisasha <osisasha@gmail.com>
2017-11-22 14:19:51 +0300

    Wavelength nm changed to AA; unit check .to(u.unit).value added; long lines wrapped

--- sunpy/physics/limb_darkening.py
+++ sunpy/physics/limb_darkening.py
@@ -45,14 +45,18 @@ def limbdark(observation, limb_cut=True):
     >>> newmap.peek()
     """
 
-    if observation.wavelength < 400 * u.nm or observation.wavelength > 1500 * u.nm:
-        raise ValueError("The wavelength of the observation must be between 400 and 1500 nm.")
+    if (observation.wavelength < 4000 * u.AA or
+            observation.wavelength > 15000 * u.AA):
+        raise ValueError("The wavelength of the observation must be \
+                          between 4000 and 15000 Angstrom.")
 
-    wavelength = observation.wavelength.value
+    wavelength = observation.wavelength.to(u.AA).value
     x_center, y_center = observation.world_to_pixel(
         SkyCoord(0, 0, unit=u.arcsec, frame=observation.coordinate_frame))
-    x_dim, y_dim = observation.dimensions.x.value, observation.dimensions.y.value
-    radius = observation.rsun_obs.value / observation.scale.axis1.value
+    x_dim = observation.dimensions.x.to(u.pixel).value
+    y_dim = observation.dimensions.y.to(u.pixel).value
+    radius = (observation.rsun_obs.to(u.arcsec).value /
+              observation.scale.axis1.to(u.arcsec / u.pixel).value)
 
     a = np.array([-8.9829751, 0.0069093916, -1.8144591e-6, 2.2540875e-10,
                   -1.3389747e-14, 3.0453572e-19])
@@ -66,8 +70,8 @@ def limbdark(observation, limb_cut=True):
     vl = sum(b * wavelength)
 
     x_grid, y_grid = np.meshgrid(np.arange(int(x_dim)), np.arange(int(y_dim)))
-    x_2 = (x_grid - x_center.value) ** 2
-    y_2 = (y_grid - y_center.value) ** 2
+    x_2 = (x_grid - x_center.to(u.pixel).value) ** 2
+    y_2 = (y_grid - y_center.to(u.pixel).value) ** 2
 
     dist_grid = np.sqrt(x_2 + y_2)
     dist_grid = dist_grid / radius
@@ -79,7 +83,8 @@ def limbdark(observation, limb_cut=True):
     new_data = observation.data / limbfilt
     new_meta = observation.meta.copy()
     new_meta['history'] += ' DARKLIMB CORRECTED'
-    new_observation = observation._new_instance(new_data, new_meta, observation.plot_settings)
+    new_observation = (observation._new_instance(
+        new_data, new_meta, observation.plot_settings))
 
     if limb_cut is True:
         new_observation.data[dist_grid > radius] = np.nan
