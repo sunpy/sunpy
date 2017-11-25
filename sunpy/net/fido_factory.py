f6ed217e46bf12b79e58d4ae3dfa97deb496437b
Stuart Mumford <stuart@cadair.com>
2017-11-25 11:10:30 +0000

    fix 504 error by not doing a fetch in a doctest

--- sunpy/net/fido_factory.py
+++ sunpy/net/fido_factory.py
@@ -350,9 +350,9 @@ class UnifiedDownloaderFactory(BasicRegistrationFactory):
         Example
         --------
         >>> from sunpy.net.vso.attrs import Time, Instrument
-        >>> unifresp = Fido.search(Time('2012/3/4','2012/3/6'), Instrument('AIA'))  # doctest: +REMOTE_DATA
-        >>> downresp = Fido.fetch(unifresp)  # doctest: +REMOTE_DATA
-        >>> file_paths = downresp.wait()  # doctest: +REMOTE_DATA
+        >>> unifresp = Fido.search(Time('2012/3/4','2012/3/5'), Instrument('EIT'))  # doctest: +REMOTE_DATA
+        >>> downresp = Fido.fetch(unifresp)  # doctest: +SKIP
+        >>> file_paths = downresp.wait()  # doctest: +SKIP
         """
         wait = kwargs.pop("wait", True)
         progress = kwargs.pop("progress", True)
