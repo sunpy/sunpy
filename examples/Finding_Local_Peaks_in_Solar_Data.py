e797c8fca8cb5e09f4ed96b5fa27b478d47085c1
nabobalis <nabil.freij@gmail.com>
2017-12-01 16:00:19 +0100

    minor changes to text and code

--- examples/Finding_Local_Peaks_in_Solar_Data.py
+++ examples/Finding_Local_Peaks_in_Solar_Data.py
@@ -6,7 +6,6 @@ Finding Local Peaks in Solar Data
 How to find regions of local maxima
 """
 import astropy.units as u
-
 from scipy.signal import argrelmax
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
@@ -16,70 +15,68 @@ import numpy as np
 import sunpy.map
 from sunpy.data.sample import AIA_193_IMAGE
 
-#########################################################
-# We first create the Map using the sample data.
+###############################################################################
+# We will first create a Map using some sample data and display it.
 
 aiamap = sunpy.map.Map(AIA_193_IMAGE)
-
-#########################################################
-# Now we do a quick plot
-
 plt.figure()
 aiamap.plot()
 plt.colorbar()
 plt.show()
 
-################################################################################
-# Since this is a 2D Map, we store the aiamap data in a variable called Intensity
-# 'x' and 'y' denote the X and Y coordinates of the map data respectively.
+
+###############################################################################
+# Before we find regions of local maxima, we need to create some variables that
+# store pixel coordinates for the 2D SDO/AIA data we are using.
+# These variables are used for plotting in 3D later on.
 
 x = y = np.arange(0, len(aiamap.data))
 X, Y = np.meshgrid(x, y)
-zs = np.array([aiamap.data[x][y] for x, y in zip(np.ravel(X), np.ravel(Y))])
-Intensity = zs.reshape(X.shape)
+
+###############################################################################
+# We will only consider peaks within the AIA data that are above a threshold
+# value. Once these values are thresholded, the next step is to
+# calcualte the pixel locations of local maxima posistions.
+
+thres = 0.3 * (np.max(aiamap.data) - np.min(aiamap.data)) + np.min(aiamap.data)
+idxs = argrelmax(aiamap.data)
+thres_idxs = np.where(aiamap.data[idxs] > thres)
 
 
-#####################################################################################
-# Next we check for the position in the aiamap data where we get such local maxima.
-# We will consider only those peaks which are greater than a threshold value.
+###############################################################################
+# We now store the (x, y) coordinates of the regions where we get
+# the local maxima.
 
-thres = 0.3
-thres = thres * (np.max(Intensity) - np.min(Intensity)) + np.min(Intensity)
-indexes = argrelmax(Intensity)
-filter_ind = ind = np.where(Intensity[indexes] > thres)
+xmax = idxs[0][thres_idxs[0]]
+ymax = idxs[1][thres_idxs[0]]
 
 
-################################################################################
-# We now store the x , y coordinates of the regions where we get such local peaks
-
-xmax = indexes[0][ind[0]]
-ymax = indexes[1][ind[0]]
-
-############################################################################
+###############################################################################
 # We now check for the indices at which we get such a local maxima and plot
 # those positions marked red in the aiamap data.
 
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
-zs2 = np.array([Intensity[x][y] for x, y in zip(xmax, ymax)])
-ax.plot_surface(X, Y, Intensity)
-ax.view_init(elev=10., azim=360)
+ax.plot_surface(X, Y, aiamap.data)
+ax.view_init(elev=39, azim=64)
+zs2 = np.array([aiamap.data[x][y] for x, y in zip(xmax, ymax)])
 ax.scatter(ymax, xmax, zs2, color='r')
-ax.set_xlabel('X Coordinates')
-ax.set_ylabel('Y Coordinates')
+ax.set_xlabel('x coordinate')
+ax.set_ylabel('y coordinate')
 ax.set_zlabel('Intensity')
 plt.show()
 
 
-###################################################################################
-# We therefore import the coordinate functionality for the local maximal regions.
+###############################################################################
+# Now we need to turn the pixel coordinates into the world location so
+# they can be easily overlaid on the Map.
 
-hpc_max = aiamap.pixel_to_data(xmax*u.pixel, ymax*u.pixel)
+hpc_max = aiamap.pixel_to_world(ymax*u.pixel, xmax*u.pixel)
 
 
-################################################################################
-# Finally we do an aia plot to check for the specified locations (marked x)
-# corresponding to local peaks.
+###############################################################################
+# Finally we do an AIA plot to check for the local maxima locations
+# which will be marked with a `x` label.
 
 fig = plt.figure()
 ax = plt.subplot(projection=aiamap)
