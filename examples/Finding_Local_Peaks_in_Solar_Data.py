3dd7a8cdb2579c40292e302b22c93b4ece55f288
Prateek Chanda <prateekkol21@gmail.com>
2017-12-07 22:02:17 +0530

    Removing arglemax -> Scikit.peak_local_max

--- examples/Finding_Local_Peaks_in_Solar_Data.py
+++ examples/Finding_Local_Peaks_in_Solar_Data.py
@@ -6,7 +6,7 @@ Finding Local Peaks in Solar Data
 How to find regions of local maxima
 """
 import astropy.units as u
-from scipy.signal import argrelmax
+from skimage.feature import peak_local_max
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
@@ -37,17 +37,7 @@ X, Y = np.meshgrid(x, y)
 # value. Once these values are thresholded, the next step is to
 # calcualte the pixel locations of local maxima posistions.
 
-thres = 0.3 * (np.max(aiamap.data) - np.min(aiamap.data)) + np.min(aiamap.data)
-idxs = argrelmax(aiamap.data)
-thres_idxs = np.where(aiamap.data[idxs] > thres)
-
-
-###############################################################################
-# We now store the (x, y) coordinates of the regions where we get
-# the local maxima.
-
-xmax = idxs[0][thres_idxs[0]]
-ymax = idxs[1][thres_idxs[0]]
+coordinates = peak_local_max(aiamap.data, min_distance=60, threshold_rel=0.2)
 
 
 ###############################################################################
@@ -58,10 +48,10 @@ fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, aiamap.data)
 ax.view_init(elev=39, azim=64)
-zs2 = np.array([aiamap.data[x][y] for x, y in zip(xmax, ymax)])
-ax.scatter(ymax, xmax, zs2, color='r')
-ax.set_xlabel('x coordinate')
-ax.set_ylabel('y coordinate')
+zs2 = np.array([aiamap.data[x][y] for x, y in zip(coordinates[:, 0], coordinates[:, 1])])
+ax.scatter(coordinates[:, 1], coordinates[:, 0],zs2, color = 'r')
+ax.set_xlabel('X Coordinates')
+ax.set_ylabel('Y Coordinates')
 ax.set_zlabel('Intensity')
 
 
@@ -69,7 +59,7 @@ ax.set_zlabel('Intensity')
 # Now we need to turn the pixel coordinates into the world location so
 # they can be easily overlaid on the Map.
 
-hpc_max = aiamap.pixel_to_world(ymax*u.pixel, xmax*u.pixel)
+hpc_max = aiamap.pixel_to_data(coordinates[:, 1]*u.pixel, coordinates[:, 0]*u.pixel)
 
 
 ###############################################################################
