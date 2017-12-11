9273271e26af6bed2e95c2cb8b8f2695964e2eab
Stuart Mumford <stuart@cadair.com>
2017-12-11 16:58:29 +0000

    Add a test which is currently not working

---
+++ sunpy/io/special/asdf/tags/map/tests/test_genericmap.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+import sunpy.map
+from sunpy.data.test import get_test_filepath
+from sunpy.io.special.asdf.extension import SunpyExtension
+
+asdf = pytest.importorskip('asdf')
+from asdf.tests.helpers import assert_roundtrip_tree
+
+
+aia_path = get_test_filepath("aia_171_level1.fits")
+
+
+def test_genericmap_basic(tmpdir):
+
+    tree = {'smap': sunpy.map.Map(aia_path)}
+
+    assert_roundtrip_tree(tree, tmpdir, extensions=SunpyExtension())
