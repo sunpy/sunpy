9b541e9d6fa077273ff374c91a133aa0d7e084ab
rajasekharmekala <rajasekharreddymekala732@gmail.com>
2017-12-13 17:37:11 +0530

    spaces removed

--- examples/finegrained_plot.py
+++ examples/finegrained_plot.py
@@ -18,19 +18,19 @@ from sunpy.data.sample import AIA_171_IMAGE
 
 
 ###############################################################################
-# skyCoord module provides flexible  celestial coordinate representation and a 
-# draw_limb method overpplots the boundary of the spacial objects.Date of the 
-# image taken can also be displayed in the plot. 
+# skyCoord module provides flexible  celestial coordinate representation and a
+# draw_limb method overpplots the boundary of the spacial objects.Date of the
+# image taken can also be displayed in the plot.
 
 aiamap = sunpy.map.Map(AIA_171_IMAGE)
 
 bl = SkyCoord(-400*u.arcsec, -900*u.arcsec, frame=aiamap.coordinate_frame)
 tr = SkyCoord(800*u.arcsec, 700*u.arcsec, frame=aiamap.coordinate_frame)
-ams = aiamap.submap(bl,tr)
-title_obsdate='{:.20}'.format('{:%Y-%b-%d %H:%M:%s}'.format(ams.date))
+ams = aiamap.submap(bl, tr)
+title_obsdate ='{:.20}'.format('{:%Y-%b-%d %H:%M:%s}'.format(ams.date))
 
 ###############################################################################
-# The figure displays Coordinates on all four edges by default.The edges can be 
+# The figure displays Coordinates on all four edges by default.The edges can be
 # picked for labelling and in this example we remove the labels and tick on the
 # top and right edges.
 # The plots show the grid as white faint dashed lines by default and that can be removed.
@@ -39,7 +39,7 @@ title_obsdate='{:.20}'.format('{:%Y-%b-%d %H:%M:%s}'.format(ams.date))
 fig = plt.figure(figsize=(5, 5))
 ax = plt.subplot(projection=ams)
 ams.plot()
-ams.draw_limb(color='black',linewidth=2,linestyle='dashed')
+ams.draw_limb(color='black', linewidth=2, linestyle='dashed')
 overlay = ax.get_coords_overlay('heliographic_stonyhurst')
 lon = overlay[0]
 lat = overlay[1]
@@ -57,12 +57,10 @@ ax.grid(False)
 tx, ty = ax.coords
 tx.set_major_formatter('s')
 ty.set_major_formatter('s')
-ax.set_title('AIA 171$\AA$ '+ title_obsdate)
+ax.set_title('AIA 171$\AA$ ' + title_obsdate)
 ax.set_ylabel('y [arcsec]')
 ax.set_xlabel('x [arcsec]')
-plt.colorbar(fraction=0.035, pad=0.03,label='DN', ax=ax)
+plt.colorbar(fraction=0.035, pad=0.03, label='DN', ax=ax)
 # plt.colorbar(ax=ax)
 plt.show()
-
-
 
