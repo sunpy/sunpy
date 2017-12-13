76262743d0cc950b1f7165eafe62e60c3381272f
Prateek Chanda <prateekkol21@gmail.com>
2017-12-13 17:47:08 +0530

    pep8 fix

--- examples/Finding_Local_Peaks_in_Solar_Data.py
+++ examples/Finding_Local_Peaks_in_Solar_Data.py
@@ -3,9 +3,11 @@
 Finding Local Peaks in Solar Data
 =================================
 
-Detecting radiation peaks in the solar surface is often crucial in the study of solar flares. This example illustrates detection of those
-areas where there is a spike in solar radiation intensity. We use the peak_local_max function under the scikit library to find those regions in the map 
-data where the intensity values form a local maxima. Finally we plot those peaks in the original AIA plot.  
+Detecting radiation peaks in the solar surface is often crucial in the study of solar flares.
+This example illustrates detection of those areas where there is a spike in solar radiation intensity.
+We use the peak_local_max function under the scikit library to find those regions in the map
+data where the intensity values form a local maxima.
+Finally we plot those peaks in the original AIA plot.  
 """
 import numpy as np
 import astropy.units as u
@@ -37,14 +39,14 @@ X, Y = np.meshgrid(x, y)
 
 
 #######################################################################################
-# We will only consider peaks within the AIA data that have minimum intensity 
-# value equal to threshold_rel * max(Intensity) which is 20% of the maximum intensity 
-# in our case. The next step is to calcualte the pixel locations of local maxima 
+# We will only consider peaks within the AIA data that have minimum intensity
+# value equal to threshold_rel * max(Intensity) which is 20% of the maximum intensity
+# in our case. The next step is to calculate the pixel locations of local maxima
 # positions where peaks are separated by atleast min_distance = 60 pixels.
 # This function comes from scikit image and the documenation is found
 # here `~skimage.feature.peak_local_max`.
 
-coordinates = peak_local_max(aiamap.data, min_distance=60, threshold_rel=0.2)
+coordinates = peak_local_max(aiamap.data, min_distance = 60, threshold_rel = 0.2)
 
 
 ###############################################################################
@@ -55,7 +57,7 @@ fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, aiamap.data)
 ax.view_init(elev=39, azim=64)
-peaks_pos = aiamap.data[coordinates[:,0],coordinates[:,1]]
+peaks_pos = aiamap.data[coordinates[:, 0], coordinates[:, 1]]
 ax.scatter(coordinates[:, 1], coordinates[:, 0], peaks_pos, color = 'r')
 ax.set_xlabel('X Coordinates')
 ax.set_ylabel('Y Coordinates')
