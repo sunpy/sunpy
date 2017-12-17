8b4716836c1214b06da0ab2deb2eb79cb49216db
nabobalis <nabil.freij@gmail.com>
2017-12-17 11:32:51 +0000

    Test for ana

--- sunpy/io/tests/test_ana.py
+++ sunpy/io/tests/test_ana.py
@@ -8,7 +8,7 @@ import pytest
 
 from sunpy.io import ana
 
-from sunpy.tests.helpers import skip_ana 
+from sunpy.tests.helpers import skip_ana
 
 # Create a test image, store it, reread it and compare
 img_size = (456, 345)
@@ -21,7 +21,6 @@ img_i16 = img_i16.astype(np.int16)
 img_f32 = img_src*1.0/img_src.max()
 img_f32 = img_f32.astype(np.float32)
 
-@skip_ana
 def test_i8c():
     # Test int 8 compressed functions
     afilename = tempfile.NamedTemporaryFile().name
@@ -29,7 +28,6 @@ def test_i8c():
     img_i8c_rec = ana.read(afilename)
     assert np.sum(img_i8c_rec[0][0] - img_i8) == 0
 
-@skip_ana
 def test_i8u():
     # Test int 8 uncompressed functions
     afilename = tempfile.NamedTemporaryFile().name
@@ -37,7 +35,6 @@ def test_i8u():
     img_i8u_rec = ana.read(afilename)
     assert np.sum(img_i8u_rec[0][0] - img_i8) == 0
 
-@skip_ana
 def test_i16c():
     # Test int 16 compressed functions
     afilename = tempfile.NamedTemporaryFile().name
@@ -45,7 +42,6 @@ def test_i16c():
     img_i16c_rec = ana.read(afilename)
     assert np.sum(img_i16c_rec[0][0] - img_i16) == 0
 
-@skip_ana
 def test_i16u():
     # Test int 16 uncompressed functions
     afilename = tempfile.NamedTemporaryFile().name
@@ -53,7 +49,6 @@ def test_i16u():
     img_i16u_rec = ana.read(afilename)
     assert np.sum(img_i16u_rec[0][0] - img_i16) == 0
 
-@skip_ana
 def test_f32u():
     # Test float 32 uncompressed functions
     afilename = tempfile.NamedTemporaryFile().name
@@ -61,7 +56,6 @@ def test_f32u():
     img_f32u_rec = ana.read(afilename)
     assert np.sum(img_f32u_rec[0][0]- img_f32) == 0
 
-@skip_ana
 def test_f32c():
     # Test if float 32 compressed functions
     #TODO: Bug with same code. Needs to be tracked down.
