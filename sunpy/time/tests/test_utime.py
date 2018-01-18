c5b57010b3af781938a3ab4c3a6ec40df2a02668
James Paul Mason <jmason86@gmail.com>
2018-01-18 14:59:32 -0500

    Abbreviated the tests to a single line

--- sunpy/time/tests/test_utime.py
+++ sunpy/time/tests/test_utime.py
@@ -4,10 +4,8 @@ from astropy.time import Time
 
 
 def test_utime_t0():
-    t = Time('1979-01-01T00:00:00')
-    assert t.utime == 0.0
+    assert Time('1979-01-01T00:00:00').utime == 0.0
 
 
 def test_utime_random_date():
-    t = Time('2018-01-13T13:32:56')
-    assert t.utime == 1231853576.0
+    assert Time('2018-01-13T13:32:56').utime == 1231853576.0
