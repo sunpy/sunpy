e27fc5416e02ff5c4d84b1a9ef91cecec99f6298
James Paul Mason <jmason86@gmail.com>
2018-01-24 15:51:17 -0500

    Included inverse test (expected to fail) and creation of UTime by import

--- sunpy/time/tests/test_utime.py
+++ sunpy/time/tests/test_utime.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, division, print_function
-
+import pytest
 from astropy.time import Time
+
+# Ignore PEP8 F401 warning here, this registers TimeUTime in astropy.time.Time
+from utime import TimeUTime
 
 
 def test_utime_t0():
@@ -9,3 +12,12 @@ def test_utime_t0():
 
 def test_utime_random_date():
     assert Time('2018-01-13T13:32:56').utime == 1231853576.0
+
+
+@pytest.mark.xfail
+def test_conversion_from_utime():
+    """
+    This is expected to fail until astropy issue 7092 is resolved.
+    """
+    t2 = Time(1231853576.0, format='utime')
+    assert t2.iso == '2018-01-13T13:32:56.000'
