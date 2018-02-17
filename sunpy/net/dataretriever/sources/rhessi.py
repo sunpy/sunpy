2873714254843e46d8ccdfa2491abc2551726426
Steven Christe <steven.d.christe@nasa.gov>
2018-02-17 16:03:27 -0500

    fixes to references to renamed functions

--- sunpy/net/dataretriever/sources/rhessi.py
+++ sunpy/net/dataretriever/sources/rhessi.py
@@ -21,7 +21,7 @@ class RHESSIClient(GenericClient):
             Date range should be specified using a TimeRange, or start
             and end dates at datetime instances or date strings.
         """
-        return rhessi.get_obssum_filename(timerange)
+        return rhessi.get_observing_summary_filename(timerange)
 
     def _makeimap(self):
         """
