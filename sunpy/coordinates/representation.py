67d42c56dc1403852b3fa244d57973bd96ca89cb
yash_jain <yashjainjain1704@gmail.com>
2018-02-22 22:17:25 +0530

    Placing the script in right order

--- sunpy/coordinates/representation.py
+++ sunpy/coordinates/representation.py
@@ -6,6 +6,8 @@ representation classes, primarily for use with the Helioprojective coordinate
 system, due to the convention of Longitude going from -180 to 180 degrees.
 """
 
+
+
 from __future__ import absolute_import, division
 
 from collections import OrderedDict
@@ -15,12 +17,14 @@ import warnings
 from astropy.utils.exceptions import AstropyDeprecationWarning
 from astropy import units as u
 from astropy.coordinates.representation import (SphericalRepresentation,
-                                                UnitSphericalRepresentation,
-                                                CartesianRepresentation)
+                                              UnitSphericalRepresentation,
+                                              CartesianRepresentation)
 from astropy.coordinates import Longitude, Latitude
 
+
+
 __all__ = ['Longitude180', 'SphericalWrap180Representation',
-           'UnitSphericalWrap180Representation']
+         'UnitSphericalWrap180Representation']
 
 
 class Longitude180(Longitude):
@@ -62,65 +66,67 @@ class Longitude180(Longitude):
         return self
 
 
-class UnitSphericalWrap180Representation(UnitSphericalRepresentation):
-    """
-    Representation of points in 3D Spherical coordinates.
-    This representation allows for a negative Longitude.
-    It does so by setting wrap_angle=180 degrees.
-
-    Parameters
-    ----------
-    lon, lat: `~astropy.units.Quantity`
-        The longitude and latitude of the point(s) in angular units. The
-        latitude should be between -90 and +90 degrees, and the longitude
-        is allowed to have any value between -180 to 180 degrees. These
-        can also be instances of `~astropy.units.Angle`,
-        `~astropy.coordinates.Longitude`, or `~astropy.coordinates.Latitude`.
-
-    distance: `~astropy.units.Quantity`
-        The distance to the point(s). If the distance is a length, it is
-        passed to the :class:`~astropy.coordinates.Distance` class, else
-        it is passed to the :class:`~astropy.units.Quantity` class.
-
-    copy: bool, optional
-        If True, arrays will be copied rather than referenced.
-    """
-
-    attr_classes = OrderedDict([('lon', Longitude180), ('lat', Latitude)])
-    recommended_units = {'lon': u.deg, 'lat': u.deg}
-
-
-class SphericalWrap180Representation(SphericalRepresentation):
-    """
-    Representation of points in 3D Spherical coordinates.
-    This representation allows for a negative Longitude.
-    It does so by setting wrap_angle=180 degrees.
-
-    Parameters
-    ----------
-    lon, lat: `~astropy.units.Quantity`
-        The longitude and latitude of the point(s) in angular units. The
-        latitude should be between -90 and +90 degrees, and the longitude
-        is allowed to have any value between -180 to 180 degrees. These
-        can also be instances of `~astropy.units.Angle`,
-        `~astropy.coordinates.Longitude`, or `~astropy.coordinates.Latitude`.
-
-    distance: `~astropy.units.Quantity`
-        The distance to the point(s). If the distance is a length, it is
-        passed to the :class:`~astropy.coordinates.Distance` class, else
-        it is passed to the :class:`~astropy.units.Quantity` class.
-
-    copy: bool, optional
-        If True, arrays will be copied rather than referenced.
-    """
-
-    attr_classes = OrderedDict([('lon', Longitude180), ('lat', Latitude),
-                                ('distance', u.Quantity)])
-    recommended_units = {'lon': u.deg, 'lat': u.deg}
-
-    _unitrep = UnitSphericalWrap180Representation
-
-
 with warnings.catch_warnings():
   warnings.simplefilter('ignore', AstropyDeprecationWarning)
+
+
+  class UnitSphericalWrap180Representation(UnitSphericalRepresentation):
+      """
+      Representation of points in 3D Spherical coordinates.
+      This representation allows for a negative Longitude.
+      It does so by setting wrap_angle=180 degrees.
+
+      Parameters
+      ----------
+      lon, lat: `~astropy.units.Quantity`
+          The longitude and latitude of the point(s) in angular units. The
+          latitude should be between -90 and +90 degrees, and the longitude
+          is allowed to have any value between -180 to 180 degrees. These
+          can also be instances of `~astropy.units.Angle`,
+          `~astropy.coordinates.Longitude`, or `~astropy.coordinates.Latitude`.
+
+      distance: `~astropy.units.Quantity`
+          The distance to the point(s). If the distance is a length, it is
+          passed to the :class:`~astropy.coordinates.Distance` class, else
+          it is passed to the :class:`~astropy.units.Quantity` class.
+
+      copy: bool, optional
+          If True, arrays will be copied rather than referenced.
+      """
+
+      attr_classes = OrderedDict([('lon', Longitude180), ('lat', Latitude)])
+      recommended_units = {'lon': u.deg, 'lat': u.deg}
+
+
+  class SphericalWrap180Representation(SphericalRepresentation):
+      """
+      Representation of points in 3D Spherical coordinates.
+      This representation allows for a negative Longitude.
+      It does so by setting wrap_angle=180 degrees.
+
+      Parameters
+      ----------
+      lon, lat: `~astropy.units.Quantity`
+          The longitude and latitude of the point(s) in angular units. The
+          latitude should be between -90 and +90 degrees, and the longitude
+          is allowed to have any value between -180 to 180 degrees. These
+          can also be instances of `~astropy.units.Angle`,
+          `~astropy.coordinates.Longitude`, or `~astropy.coordinates.Latitude`.
+
+      distance: `~astropy.units.Quantity`
+          The distance to the point(s). If the distance is a length, it is
+          passed to the :class:`~astropy.coordinates.Distance` class, else
+          it is passed to the :class:`~astropy.units.Quantity` class.
+
+      copy: bool, optional
+          If True, arrays will be copied rather than referenced.
+      """
+
+      attr_classes = OrderedDict([('lon', Longitude180), ('lat', Latitude),
+                                  ('distance', u.Quantity)])
+      recommended_units = {'lon': u.deg, 'lat': u.deg}
+
+      _unitrep = UnitSphericalWrap180Representation
+
+
 
