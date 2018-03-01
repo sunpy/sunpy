195a3428fc7cf575e7726a94e8a96b783ced5977
Stuart Mumford <stuart@cadair.com>
2018-03-01 18:32:13 +0000

    Merge pull request #2487 from nabobalis/peek_return
    remove plt.show and no more returning figure for peek

--- sunpy/timeseries/sources/rhessi.py
+++ sunpy/timeseries/sources/rhessi.py
@@ -77,19 +77,12 @@ class RHESSISummaryTimeSeries(GenericTimeSeries):
         **kwargs : `dict`
             Any additional plot arguments that should be used
             when plotting.
-
-        Returns
-        -------
-        fig : `~matplotlib.Figure`
-            A plot figure.
         """
         # Check we have a timeseries valid for plotting
         self._validate_data_for_ploting()
 
         figure = plt.figure()
         axes = plt.gca()
-
-        #dates = matplotlib.dates.date2num(self.data.index)
 
         lc_linecolors = rhessi.hsi_linecolors()
 
