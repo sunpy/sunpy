a16fbed690b8e43c1726f8735aacecf3a95594a7
yash_jain <yashjainjain1704@gmail.com>
2018-03-06 01:29:10 +0530

    Adds pytest fixures

--- sunpy/map/sources/tests/test_trace_source.py
+++ sunpy/map/sources/tests/test_trace_source.py
@@ -1,29 +1,35 @@
 import os
 import glob
 
+import pytest
+
 from sunpy.map.sources.trace import TRACEMap
 from sunpy.map import Map
 import sunpy.data.test
 
 path = sunpy.data.test.rootdir
 fitspath = glob.glob(os.path.join(path, "tsi20010130_025823_a2.fits"))
-trace = Map(fitspath)
+
+@pytest.fixture(scope="module")
+def createTRACE():
+    """Creates a TRACEMap from a FITS file."""
+    return Map(fitspath)
 
 # TRACE Tests
-def test_fitstoTRACE():
+def test_fitstoTRACE(createTRACE):
     """Tests the creation of TRACEMap using FITS."""
-    assert isinstance(trace, TRACEMap)
+    assert isinstance(createTRACE, TRACEMap)
 
-def test_is_datasource_for():
+def test_is_datasource_for(createTRACE):
     """Test the is_datasource_for method of TRACEMap.
     Note that header data to be provided as an argument
     can be a MetaDict object."""
-    assert trace.is_datasource_for(trace.data, trace.meta)
+    assert createTRACE.is_datasource_for(createTRACE.data, createTRACE.meta)
 
-def test_measurement():
+def test_measurement(createTRACE):
     """Tests the measurement property of the TRACEMap object."""
-    assert int(trace.measurement) == 171
+    assert int(createTRACE.measurement) == 171
 
-def test_observatory():
+def test_observatory(createTRACE):
     """Tests the observatory property of the TRACEMap object."""
-    assert trace.observatory == "TRACE"
+    assert createTRACE.observatory == "TRACE"
