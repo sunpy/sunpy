53c2581fc2d08ab05cb58929b5b7c763aa639bad
David Stansby <dstansby@gmail.com>
2018-03-06 17:26:35 +0000

    Compare saved image in local directory, instead of temp dir

--- sunpy/tests/helpers.py
+++ sunpy/tests/helpers.py
@@ -43,7 +43,6 @@ skip_windows = pytest.mark.skipif(platform.system() == 'Windows', reason="Window
 skip_glymur = pytest.mark.skipif(SKIP_GLYMUR, reason="Glymur can not be imported")
 skip_ana = pytest.mark.skipif(SKIP_ANA, reason="ANA is not available")
 
-
 @pytest.fixture
 def warnings_as_errors(request):
     warnings.simplefilter('error')
@@ -51,18 +50,19 @@ def warnings_as_errors(request):
     request.addfinalizer(lambda *args: warnings.resetwarnings())
 
 
+hash_library = hash.hash_library
 new_hash_library = {}
-figure_test_pngfiles = {}
 
 
 def figure_test(test_function):
     """
     A decorator for a test that verifies the hash of the current figure or the returned figure,
     with the name of the test function as the hash identifier in the library.
-    A PNG is also created with a temporary filename, with the lookup stored in the
-    `figure_test_pngfiles` dictionary.
+    A PNG is also created in the 'result_image' director, which is created
+    on the current path.
 
-    All such decorated tests are marked with `pytest.mark.figure` for convenient filtering.
+    All such decorated tests are marked with `pytest.mark.figure` for
+    convenient filtering.
 
     Examples
     --------
@@ -78,24 +78,29 @@ def figure_test(test_function):
 
         name = "{0}.{1}".format(test_function.__module__,
                                 test_function.__name__)
+        # Run the test function and get the figure
         plt.figure()
-        pngfile = tempfile.NamedTemporaryFile(delete=False)
-        figure_hash = hash.hash_figure(test_function(*args, **kwargs), out_stream=pngfile)
-        figure_test_pngfiles[name] = pngfile.name
-        pngfile.close()
+        fig = test_function(*args, **kwargs)
+        if fig is None:
+            fig = plt.gcf()
 
         # Save the image that was generated
         os.makedirs('result_images', exist_ok=True)
-        result_image_loc = 'result_images/{}.png'.format(name)
+        result_image_loc = os.path.join('result_images', '{}.png'.format(name))
         plt.savefig(result_image_loc)
+        plt.close()
+
+        # Create hash
+        imgdata = open(result_image_loc, "rb")
+        figure_hash = hash._hash_file(imgdata)
+        imgdata.close()
 
         new_hash_library[name] = figure_hash
-        if name not in hash.hash_library:
+        if name not in hash_library:
             pytest.fail("Hash not present: {0}".format(name))
 
-        if hash.hash_library[name] != figure_hash:
+        if hash_library[name] != figure_hash:
             raise RuntimeError('Figure hash does not match expected hash.\n'
                                'New image generated and placed at {}'.format(result_image_loc))
 
-        plt.close()
     return wrapper
