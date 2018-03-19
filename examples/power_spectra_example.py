fc58fc0bd450d865b27e3e68280b9a02ff370b78
yash_jain <yashjainjain1704@gmail.com>
2018-03-19 15:53:17 +0530

    Changed the input data

--- examples/power_spectra_example.py
+++ examples/power_spectra_example.py
@@ -16,29 +16,28 @@ from scipy import signal
 import astropy.units as u
 
 import sunpy.timeseries
-from sunpy.data.sample import GOES_XRS_TIMESERIES
+from sunpy.data.sample import RHESSI_TIMESERIES
 
 ###############################################################################
 # Let's first load a TimeSeries from sample data.
 
-ts = sunpy.timeseries.TimeSeries(GOES_XRS_TIMESERIES)
-# Data is taken from GOES X-Ray Sensor(XRS), which observes full-disk-integrated
-# solar flux in two broadband channels: 1--8 angstrom (long); and
-# 0.5--4 angstrom (short)
+# Input data contains 9 columns , which are evenly sampled with a time
+# step of 4 seconds.
+ts = sunpy.timeseries.TimeSeries(RHESSI_TIMESERIES)
 
 ###############################################################################
 # We now use scipy's periodogram to estimate the power spectra of the first 
 # column of the Timeseries. Astropy's Lomb-Scargle Periodograms can be used
 # instead of scipy's periodogram.
 
-xray_short = ts.columns[0]
-freq, spectra = signal.periodogram(ts.data[xray_short], fs=0.48)
-
+x_ray = ts.columns[0]
+# The suitable value for fs would be 0.25 Hz as cadence of the data is 4 s.
+freq, spectra = signal.periodogram(ts.data[xray_short], fs=0.25)
 ###############################################################################
 # Plot the power spectrum
 
 plt.semilogy(freq, spectra)
 plt.title('Power Spectrum of {}'.format(xray_short))
-plt.ylabel('Power Spectral Density [{:LaTeX}]'.format(ts.units[xray_short] ** 2 / u.Hz))
+plt.ylabel('Power Spectral Density [{:LaTeX}]'.format(ts.units[x_ray] ** 2 / u.Hz))
 plt.xlabel('Frequency [Hz]')
 plt.show()
