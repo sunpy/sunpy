49b5d8070a0f7d12349c0210c15933f4e1905883
Prateek Chanda <prateekkol21@gmail.com>
2018-04-03 19:44:22 +0530

    Update base_and_running_difference_mapcubes.py

--- examples/base_and_running_difference_mapcubes.py
+++ examples/base_and_running_difference_mapcubes.py
@@ -36,9 +36,9 @@ for i, map_i in enumerate(aiamapcube[1:]):
     diffdata = aiamapcube_rot.data - map_i.data
     smap_run = sunpy.map.Map(diffdata, map_i.meta)
     smap_base.plot_settings['cmap'] = cm.get_cmap('Greys_r')
-    smap_base.plot_settings['norm'] = colors.LogNorm(100, smap.max())
+    smap_base.plot_settings['norm'] = colors.LogNorm(100, smap_base.max())
     smap_run.plot_settings['cmap'] = cm.get_cmap('Greys_r')
-    smap_run.plot_settings['norm'] = colors.LogNorm(100, smap.max())
+    smap_run.plot_settings['norm'] = colors.LogNorm(100, smap_run.max())
     base_diffmap.append(smap_base)
     running_diffmap.append(smap_run)
 
