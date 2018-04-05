1213d767183dfbad4ae0440f4bc20d7c16d93093
nabobalis <nabil.freij@gmail.com>
2018-04-05 18:19:30 +0200

    stu addressed

--- sunpy/net/fido_factory.py
+++ sunpy/net/fido_factory.py
@@ -351,7 +351,7 @@ class UnifiedDownloaderFactory(BasicRegistrationFactory):
         --------
         >>> from sunpy.net.vso.attrs import Time, Instrument
         >>> unifresp = Fido.search(Time('2012/3/4','2012/3/5'), Instrument('EIT'))  # doctest: +REMOTE_DATA
-        >>> downresp = Fido.fetch(unifresp)  # doctest: +REMOTE_DATA
+        >>> downresp = Fido.fetch(unifresp)  # doctest: +SKIP
         >>> file_paths = downresp.wait()  # doctest: +SKIP
         """
         wait = kwargs.pop("wait", True)
