1fdc4b719b9615fa857dd6258f8c3020bdd6a8c4
Vishnunarayan K I <appukuttancr@gmail.com>
2018-05-14 20:31:39 +0530

    Fix the whole of TimeRange(?)

--- sunpy/time/__init__.py
+++ sunpy/time/__init__.py
@@ -1,8 +1,8 @@
 """Time related functionality"""
 from __future__ import absolute_import
 
+from sunpy.time.astropy_time import *
 from sunpy.time.time import *
 from sunpy.time.timerange import *
 from sunpy.time.julian import *
 from sunpy.time.utime import *
-from sunpy.time.astropy_time import Time
