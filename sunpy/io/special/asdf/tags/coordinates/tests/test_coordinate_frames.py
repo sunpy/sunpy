53e106c2f5ab4085613e71fdc7ff316ea6eba982
Stuart Mumford <stuart@cadair.com>
2018-05-18 15:47:49 +0100

    it still fails

--- sunpy/io/special/asdf/tags/coordinates/tests/test_coordinate_frames.py
+++ sunpy/io/special/asdf/tags/coordinates/tests/test_coordinate_frames.py
@@ -15,17 +15,39 @@ from asdf.tests.helpers import assert_roundtrip_tree
 
 @pytest.fixture(params=(Heliocentric, HeliographicCarrington,
                         HeliographicStonyhurst, Helioprojective))
-def coordframe(request):
+def coordframe_scalar(request):
     frame = request.param
 
     narg = 2
     if frame is Heliocentric:
         narg = 3
 
-    data = np.random.random((narg, 10)) * u.arcsec
+    data = np.random.random((narg, 1)) * u.arcsec
     return frame(*data, obstime='2018-01-01T00:00:00')
 
 
-def test_saveframe(coordframe, tmpdir):
-    tree = {'frame': coordframe}
+@pytest.fixture(params=(Heliocentric, HeliographicCarrington,
+                        HeliographicStonyhurst, Helioprojective))
+def coordframe_array(request):
+    frame = request.param
+
+    narg = 2
+    if frame is Heliocentric:
+        data = np.random.random((narg, 1)) * u.km
+        data2 = np.random.random((narg, 1)) * u.km
+        data3 = np.random.random((narg, 1)) * u.km
+        return frame(data, data2, data3, obstime='2018-01-01T00:00:00')
+
+    data = np.random.random((narg, 1)) * u.arcsec
+    data2 = np.random.random((narg, 1)) * u.arcsec
+    return frame(data, data2, obstime='2018-01-01T00:00:00')
+
+
+def test_saveframe(coordframe_scalar, tmpdir):
+    tree = {'frame': coordframe_scalar}
+    assert_roundtrip_tree(tree, tmpdir)
+
+
+def test_saveframe_arr(coordframe_array, tmpdir):
+    tree = {'frame': coordframe_array}
     assert_roundtrip_tree(tree, tmpdir)
