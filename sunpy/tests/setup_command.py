79d8f0f71caa0bbc3cc606a0a245cd52d75db95b
Stuart Mumford <stuart@cadair.com>
2018-06-20 22:51:47 +0100

    Change to using pytest-cov inside the test runner
    This enables us to have either term or html reports (html is still the
    default) and allows coverage to work with parallel

--- sunpy/tests/setup_command.py
+++ sunpy/tests/setup_command.py
@@ -22,6 +22,8 @@ class SunPyTest(AstropyTest):
                       'Also run tests that do require a internet connection.'),
                      ('online-only', None,
                       'Only run test that do require a internet connection.'),
+                     ('cov-report=', None,
+                      'How to display the coverage report, should be either "html" or "term"'),
                      ('figure', None,
                       'Run the figure tests.'),
                      # Run only tests that check figure generation
@@ -36,6 +38,7 @@ class SunPyTest(AstropyTest):
         self.online_only = False
         self.figure = False
         self.figure_only = False
+        self.cov_report = True
 
     def generate_testing_command(self):
         """
@@ -44,17 +47,30 @@ class SunPyTest(AstropyTest):
 
         cmd_pre = ''  # Commands to run before the test function
         cmd_post = ''  # Commands to run after the test function
+        cov_report = False
 
         if self.coverage:
-            pre, post = self._generate_coverage_commands()
-            cmd_pre += pre
-            cmd_post += post
+            # Copy the raw .coverage file back so it can be used for CI reports
+            cwd_covfile = os.path.join(os.path.abspath("."), ".coverage")
+            cmd_post = ('import shutil, os; '
+                        'covfile = os.path.join(os.path.abspath("."), ".coverage"); '
+                        f'shutil.copyfile(covfile, "{cwd_covfile}")'
+                        ' if os.path.isfile(covfile) else None; ')
+
+            # Special case html as the default report
+            if self.cov_report and (isinstance(self.cov_report, bool) or "html" in self.cov_report):
+                html_cov = os.path.join(os.path.abspath("."), "htmlcov")
+                cov_report = f'html:{html_cov}'
+            else:
+                cov_report = self.cov_report
 
         cmd = ('{cmd_pre}{0}; import {1.package_name}, sys; result = ('
                '{1.package_name}.self_test('
                'package={1.package!r}, '
                'test_path={1.test_path!r}, '
                'args={1.args!r}, '
+               'coverage={1.coverage!r}, '
+               'cov_report="{cov_report}", '
                'plugins={1.plugins!r}, '
                'verbose={1.verbose_results!r}, '
                'pastebin={1.pastebin!r}, '
@@ -74,6 +90,7 @@ class SunPyTest(AstropyTest):
                'sys.exit(result)')
         return cmd.format('pass',
                           self,
+                          cov_report=cov_report,
                           figure_dir=os.path.join(os.path.abspath('.'), "figure_test_images"),
                           cmd_pre=cmd_pre,
                           cmd_post=cmd_post)
