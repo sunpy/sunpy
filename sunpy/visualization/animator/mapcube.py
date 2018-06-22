750fedf109d96a30c15de3a94495b8c8c217d7f2
nabobalis <nabil.freij@gmail.com>
2018-06-22 11:36:10 +0100

    doc string fix

--- sunpy/visualization/animator/mapcube.py
+++ sunpy/visualization/animator/mapcube.py
@@ -2,12 +2,15 @@
 
 import warnings
 
-from sunpy.util.exceptions import SunpyDeprecationWarning
+from sunpy.util.decorators import deprecated
+from sunpy.util.decorators import add_common_docstring
 from sunpy.visualization.animator.mapsequenceanimator import MapSequenceAnimator
 
 __all__ = ['MapCubeAnimator']
 
-warnings.warn("Deprecated in favor of MapSequenceAnimator. MapSequence has the same functionality as MapCube.",
-              SunpyDeprecationWarning, stacklevel=2)
 
-MapCubeAnimator = MapSequenceAnimator
+@deprecated('0.9.1', message='MapCubeAnimator deprecated in favor of MapSequenceAnimator. MapSequence has the same functionality as MapCube.')
+@add_common_docstring(append='MapCubeAnimator deprecated in favor of MapSequenceAnimator. MapSequence has the same functionality as MapCube.',
+                      prepend=MapSequenceAnimator.__doc__)
+def MapCubeAnimator(*args, **kwargs):
+    return MapSequenceAnimator(*args, **kwargs)
