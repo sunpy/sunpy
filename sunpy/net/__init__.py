b200e69a74144b26a4b826b84cdbc57aa004a994
nabobalis <nabil.freij@gmail.com>
2018-06-22 14:16:58 +0100

    hard import in __init__

--- sunpy/net/__init__.py
+++ sunpy/net/__init__.py
@@ -2,6 +2,7 @@
 
 # Import and register the clients but we do not want them in the namespace, we import them as _
 from sunpy.net import base_client as _, dataretriever as _
+from sunpy.net import vso as _, jsoc as _
 from sunpy.net.fido_factory import Fido
 
 __all__ = ["Fido"]
