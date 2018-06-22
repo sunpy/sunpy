cde1559e91ea8718fb383d101669498765a2b120
nabobalis <nabil.freij@gmail.com>
2018-06-22 17:03:50 +0100

    fixed my code

--- sunpy/visualization/animator/mapcube.py
+++ sunpy/visualization/animator/mapcube.py
@@ -6,6 +6,5 @@ from sunpy.visualization.animator.mapsequenceanimator import MapSequenceAnimator
 __all__ = ['MapCubeAnimator']
 
 
-MapCubeAnimator = deprecated(MapSequenceAnimator, "0.9.1",
-                             message='MapCubeAnimator deprecated in favor of MapSequenceAnimator. \
-                                      MapSequence has the same functionality as MapCube.')
+MapCubeAnimator = deprecated("0.9.1", message='MapCubeAnimator deprecated in favor of MapSequenceAnimator. \
+                                               MapSequence has the same functionality as MapCube.')(MapSequenceAnimator)
