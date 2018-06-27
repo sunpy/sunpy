08b43b6712b7bcd179f3cf9f949e1bc5db1b2c89
Vishnunarayan K I <appukuttancr@gmail.com>
2018-06-27 19:15:35 +0530

    yapf test_goes

--- sunpy/instr/tests/test_goes.py
+++ sunpy/instr/tests/test_goes.py
@@ -143,87 +143,94 @@ def test_goes_chianti_tem_errors():
 def test_goes_chianti_tem_case1():
     # test case 1: satellite > 7, abundances = coronal
     temp1, em1 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=15,
-                                       date=DATE)
+                                        date=DATE)
     np.testing.assert_allclose(temp1, Quantity([11.28], unit="MK"), rtol=0.01)
     assert all(em1 < Quantity([4.79e+48], unit="1/cm**3")) and \
-      em1 > Quantity([4.78e+48], unit="1/cm**3")
+        em1 > Quantity([4.78e+48], unit="1/cm**3")
 
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case2():
     # test case 2: satellite > 7, abundances = photospheric
     temp2, em2 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=15,
-                                       date=DATE, abundances="photospheric")
+                                        date=DATE, abundances="photospheric")
     assert all(temp2 < Quantity([10.25], unit="MK")) and \
-      all(temp2 > Quantity([10.24], unit="MK"))
+        all(temp2 > Quantity([10.24], unit="MK"))
     assert all(em2 < Quantity([1.12e+49], unit="1/cm**3")) and \
-      all(em2 > Quantity([1.11e+49], unit="1/cm**3"))
+        all(em2 > Quantity([1.11e+49], unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case3():
     # test case 3: satellite < 8 and != 6, abundances = coronal
     temp3, em3 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=5,
-                                       date=DATE,
-                                       abundances="coronal")
+                                        date=DATE,
+                                        abundances="coronal")
     assert all(temp3 < Quantity([11.43], unit="MK")) and \
-      all(temp3 > Quantity([11.42], unit="MK"))
+        all(temp3 > Quantity([11.42], unit="MK"))
     assert all(em3 < Quantity([3.85e+48], unit="1/cm**3")) and \
-      all(em3 > Quantity([3.84e+48], unit="1/cm**3"))
+        all(em3 > Quantity([3.84e+48], unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case4():
     # test case 4: satellite < 8 and != 6, abundances = photospheric
     temp4, em4 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=5,
-                                       date=DATE,
-                                       abundances="photospheric")
+                                        date=DATE,
+                                        abundances="photospheric")
     assert all(temp4 < Quantity([10.42], unit="MK")) and \
-      all(temp4 > Quantity([10.41], unit="MK"))
+        all(temp4 > Quantity([10.41], unit="MK"))
     assert all(em4 < Quantity(8.81e+48, unit="1/cm**3")) and \
-      all(em4 > Quantity(8.80e+48, unit="1/cm**3"))
+        all(em4 > Quantity(8.80e+48, unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case5():
     # test case 5: satellite = 6, date < 1983-06-28, abundances = coronal
     temp5, em5 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=6,
-                                       date="1983-06-27",
-                                       abundances="coronal")
+                                        date="1983-06-27",
+                                        abundances="coronal")
     assert all(temp5 < Quantity(12.30, unit="MK")) and \
-      all(temp5 > Quantity(12.29, unit="MK"))
+        all(temp5 > Quantity(12.29, unit="MK"))
     assert all(em5 < Quantity(3.13e+48, unit="1/cm**3")) and \
-      all(em5 > Quantity(3.12e+48, unit="1/cm**3"))
+        all(em5 > Quantity(3.12e+48, unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case6():
     # test case 6: satellite = 6, date < 1983-06-28, abundances = photospheric
     temp6, em6 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=6,
-                                       date="1983-06-27",
-                                       abundances="photospheric")
+                                        date="1983-06-27",
+                                        abundances="photospheric")
     assert all(temp6 < Quantity(11.44, unit="MK")) and \
-      all(temp6 > Quantity(11.43, unit="MK"))
+        all(temp6 > Quantity(11.43, unit="MK"))
     assert all(em6 < Quantity(6.74e+48, unit="1/cm**3")) and \
-      all(em6 > Quantity(6.73e+48, unit="1/cm**3"))
+        all(em6 > Quantity(6.73e+48, unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case7():
     # test case 7: satellite = 6, date > 1983-06-28, abundances = coronal
     temp7, em7 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=6,
-                                       date=DATE,
-                                       abundances="coronal")
+                                        date=DATE,
+                                        abundances="coronal")
     assert all(temp7 < Quantity(11.34, unit="MK")) and \
-      all(temp7 > Quantity(11.33, unit="MK"))
+        all(temp7 > Quantity(11.33, unit="MK"))
     assert all(em7 < Quantity(4.08e+48, unit="1/cm**3")) and \
-      all(em7 > Quantity(4.07e+48, unit="1/cm**3"))
+        all(em7 > Quantity(4.07e+48, unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_goes_chianti_tem_case8():
     # test case 8: satellite = 6, date > 1983-06-28, abundances = photospheric
     temp8, em8 = goes._goes_chianti_tem(LONGFLUX, SHORTFLUX, satellite=6,
-                                       date=DATE,
-                                       abundances="photospheric")
+                                        date=DATE,
+                                        abundances="photospheric")
     assert all(temp8 < Quantity(10.36, unit="MK")) and \
-      all(temp8 > Quantity(10.35, unit="MK"))
+        all(temp8 > Quantity(10.35, unit="MK"))
     assert all(em8 < Quantity(9.39e+48, unit="1/cm**3")) and \
-      all(em8 > Quantity(9.38e+48, unit="1/cm**3"))
+        all(em8 > Quantity(9.38e+48, unit="1/cm**3"))
+
 
 @pytest.mark.remote_data
 def test_calculate_radiative_loss_rate():
@@ -331,7 +338,7 @@ def test_calc_rad_loss_obstime():
         "rad_loss_cumul": Quantity([6.03702783e+19, 1.20740557e+20,
                                     1.81110835e+20, 2.41481113e+20,
                                     3.01851392e+20], unit="J")
-        }
+    }
     assert sorted(rad_loss_test.keys()) == sorted(rad_loss_expected.keys())
     assert_quantity_allclose(rad_loss_test["rad_loss_rate"],
                              rad_loss_expected["rad_loss_rate"], rtol=0.0001)
@@ -395,7 +402,8 @@ def test_goes_lx_nokwargs():
     assert sorted(lx_test.keys()) == sorted(lx_expected.keys())
     assert_quantity_allclose(lx_test["longlum"], lx_expected["longlum"], rtol=0.1)
     assert_quantity_allclose(lx_test["shortlum"], lx_expected["shortlum"],
-                       rtol=0.1)
+                             rtol=0.1)
+
 
 def test_goes_lx_date():
     # Define input values of flux and time.
@@ -410,7 +418,8 @@ def test_goes_lx_date():
     assert sorted(lx_test.keys()) == sorted(lx_expected.keys())
     assert_quantity_allclose(lx_test["longlum"], lx_expected["longlum"], rtol=0.001)
     assert_quantity_allclose(lx_test["shortlum"], lx_expected["shortlum"],
-                       rtol=0.001)
+                             rtol=0.001)
+
 
 def test_goes_lx_obstime():
     # Define input values of flux and time.
@@ -438,15 +447,16 @@ def test_goes_lx_obstime():
     assert sorted(lx_test.keys()) == sorted(lx_expected.keys())
     assert_quantity_allclose(lx_test["longlum"], lx_expected["longlum"], rtol=0.1)
     assert_quantity_allclose(lx_test["shortlum"], lx_expected["shortlum"],
-                       rtol=0.1)
+                             rtol=0.1)
     assert_quantity_allclose(lx_test["longlum_int"], lx_expected["longlum_int"],
-                       rtol=0.1)
+                             rtol=0.1)
     assert_quantity_allclose(lx_test["shortlum_int"], lx_expected["shortlum_int"],
-                       rtol=0.1)
+                             rtol=0.1)
     assert_quantity_allclose(lx_test["longlum_cumul"], lx_expected["longlum_cumul"],
-                       rtol=0.1)
+                             rtol=0.1)
     assert_quantity_allclose(lx_test["shortlum_cumul"],
-                       lx_expected["shortlum_cumul"], rtol=0.1)
+                             lx_expected["shortlum_cumul"], rtol=0.1)
+
 
 def test_flux_to_classletter():
     """Test converting fluxes into a class letter"""
@@ -465,11 +475,13 @@ def test_flux_to_classletter():
     assert goes.flux_to_flareclass(6.9e-07 * u.watt/u.m**2) == 'B6.9'
     assert goes.flux_to_flareclass(2.1e-05 * u.watt/u.m**2) == 'M2.1'
 
+
 def test_class_to_flux():
     classes = ['A3.49', 'A0.23', 'M1', 'X2.3', 'M5.8', 'C2.3', 'B3.45', 'X20']
     results = Quantity([3.49e-8, 2.3e-9, 1e-5, 2.3e-4, 5.8e-5, 2.3e-6, 3.45e-7, 2e-3], 'W/m2')
     for c, r in zip(classes, results):
         assert_almost_equal(r.value, goes.flareclass_to_flux(c).value)
+
 
 def test_joint_class_to_flux():
     classes = ['A3.49', 'A0.23', 'M1', 'X2.3', 'M5.8', 'C2.3', 'B3.45', 'X20']
