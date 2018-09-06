16d57425ac3fb714b61f698c6bb2669d39041c6d
David Stansby <dstansby@gmail.com>
2018-09-06 10:50:27 +0100

    Remove py2 check for pathlib

--- sunpy/net/dataretriever/client.py
+++ sunpy/net/dataretriever/client.py
@@ -10,14 +10,7 @@ import datetime
 from abc import ABCMeta
 from collections import OrderedDict, namedtuple
 from functools import partial
-
-# Python 3:
-# Remove in 1.0: Py2 does not have pathlib
-try:
-    import pathlib
-    HAS_PATHLIB = True
-except ImportError:
-    HAS_PATHLIB = False
+import pathlib
 
 import numpy as np
 import astropy.table
@@ -354,7 +347,7 @@ class GenericClient(object):
         """
         # Check for type of path
         if path is not None:
-            if HAS_PATHLIB and isinstance(path, pathlib.Path):
+            if isinstance(path, pathlib.Path):
                 path = str(path.absolute())
             elif not isinstance(path, six.string_types):
                 err = "path should be either 'pathlib.Path' or 'str'. "\
