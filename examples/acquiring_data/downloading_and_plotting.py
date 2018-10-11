d8b70273ef8dcb9d6a233e576dd901039422ecec
David Stansby <dstansby@gmail.com>
2018-10-11 11:03:00 +0100

    Bare bones downloading and plotting example

---
+++ examples/acquiring_data/downloading_and_plotting.py
@@ -0,0 +1,37 @@
+"""
+=============================
+Downloading and plotting data
+=============================
+"""
+###############################################################################
+# Import the required modules.
+from sunpy.net import Fido, attrs
+import sunpy.map
+import matplotlib.pyplot as plt
+
+###############################################################################
+# Define the timerange and instrument.
+timerange = attrs.Time('1996/05/24 11:00', '1996/05/24 12:00')
+instrument = attrs.Instrument('C3')
+
+###############################################################################
+# Search for data that is from the requested instrument and in the requested
+# timerange.
+result = Fido.search(timerange, instrument)
+result
+
+###############################################################################
+# Download the files. "downloaded_files" contains a list of the location of
+# each of the locally downloaded files.
+downloaded_files = Fido.fetch(result)
+downloaded_files
+
+###############################################################################
+# Create a SunPy Map object from the first downloaded file.
+lascomap = sunpy.map.Map(downloaded_files[0])
+
+###############################################################################
+# Plot the Map.
+fig, ax = plt.subplots()
+lascomap.plot(axes=ax)
+plt.show()
