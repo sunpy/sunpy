ce239fa927131b578bd0a8b4fddcefc6759f8d92
Nabil Freij <nabil.freij@gmail.com>
2018-10-17 14:49:39 +0100

    Better error messages and docs

--- sunpy/net/tests/test_baseclient.py
+++ sunpy/net/tests/test_baseclient.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from sunpy.net import base_client
+from sunpy.net import base_client, vso, jsoc, dataretriever
 
 
 def test_registry():
@@ -8,9 +7,19 @@ def test_registry():
     Just to see that some of the clients are in the registry.
     """
 
+    # TODO: Better this
+    client_list = [vso.VSOClient, jsoc.JSOCClient, dataretriever.EVEClient,
+                   dataretriever.LYRAClient, dataretriever.NOAAIndicesClient,
+                   dataretriever.NOAAPredictClient, dataretriever.NoRHClient,
+                   dataretriever.RHESSIClient, dataretriever.SRSClient,
+                   dataretriever.XRSClient]
+
     keys = base_client.BaseClient._registry.keys()
     items = base_client.BaseClient._registry.items()
 
-    # Todo: Make this better
     assert len(keys) == 10
     assert len(items) == 10
+
+    for client in client_list:
+        assert client in keys
+        assert (client, client._can_handle_query) in items
--- sunpy/net/vso/attrs.py
+++ sunpy/net/vso/attrs.py
@@ -20,8 +20,8 @@ from datetime import datetime
 from astropy import units as u
 
 from sunpy.time import TimeRange as _TimeRange
-from sunpy.net.attr import Attr, AttrWalker, AttrAnd,\
-                           AttrOr, DummyAttr, ValueAttr, SimpleAttr
+from sunpy.net.attr import (Attr, AttrWalker, AttrAnd,
+                            AttrOr, DummyAttr, ValueAttr, SimpleAttr)
 from sunpy.util.multimethod import MultiMethod
 from sunpy.time import parse_time
 
