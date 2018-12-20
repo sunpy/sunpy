60be3b0245310c1e8255d370355d7ce78a3726aa
Stuart Mumford <stuart@cadair.com>
2018-12-20 14:01:56 +0000

    Skip the tests on windows until asdf 2.3.1

--- sunpy/io/special/asdf/tags/tests/test_coordinate_frames.py
+++ sunpy/io/special/asdf/tags/tests/test_coordinate_frames.py
@@ -1,17 +1,21 @@
+import sys
+from distutils.version import LooseVersion
+
 import numpy as np
 import pytest
 
 import astropy.units as u
-pytest.importorskip('asdf', '2.0.2')
+asdf = pytest.importorskip('asdf', '2.0.2')
 from asdf.tests.helpers import assert_roundtrip_tree
 
-from sunpy.tests.helpers import skip_windows
 from sunpy.coordinates.frames import (Heliocentric, Helioprojective,
-                                      HeliographicCarrington, HeliographicStonyhurst)
+                                      HeliographicCarrington,
+                                      HeliographicStonyhurst)
 
 
-@pytest.fixture(params=(Heliocentric, HeliographicCarrington,
-                        HeliographicStonyhurst, Helioprojective))
+@pytest.fixture(
+    params=(Heliocentric, HeliographicCarrington, HeliographicStonyhurst,
+            Helioprojective))
 def coordframe_scalar(request):
     frame = request.param
 
@@ -23,8 +27,9 @@ def coordframe_scalar(request):
     return frame(*data, obstime='2018-01-01T00:00:00')
 
 
-@pytest.fixture(params=(Heliocentric, HeliographicCarrington, HeliographicStonyhurst,
-                        Helioprojective))
+@pytest.fixture(
+    params=(Heliocentric, HeliographicCarrington, HeliographicStonyhurst,
+            Helioprojective))
 def coordframe_array(request):
     frame = request.param
 
@@ -38,15 +43,19 @@ def coordframe_array(request):
 
 # Skip these two tests on windows due to a weird interaction with atomicfile
 # and tmpdir
+skip_windows_asdf = pytest.mark.skipif(
+    (LooseVersion(asdf.__version__) < LooseVersion("2.3.1")
+     and sys.platform.startswith("win")),
+    reason="See https://github.com/spacetelescope/asdf/pull/632")
 
 
-@skip_windows
+@skip_windows_asdf
 def test_saveframe(coordframe_scalar, tmpdir):
     tree = {'frame': coordframe_scalar}
     assert_roundtrip_tree(tree, tmpdir)
 
 
-@skip_windows
+@skip_windows_asdf
 def test_saveframe_arr(coordframe_array, tmpdir):
     tree = {'frame': coordframe_array}
     assert_roundtrip_tree(tree, tmpdir)
