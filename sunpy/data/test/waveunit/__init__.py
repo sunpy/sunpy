a7fcc87a718c8f16de7f21f68091e41188a93d50
Pulkit gera <pulkithifi@yahoo.com>
2019-01-01 13:43:34 +0530

    pathlib added to test_tables.py

--- sunpy/data/test/waveunit/__init__.py
+++ sunpy/data/test/waveunit/__init__.py
@@ -1,9 +1,9 @@
-import os.path
+import pathlib
 
 from sunpy.data.test import rootdir as testrootdir
 
-waveunitdir = os.path.join(testrootdir, 'waveunit')
-MEDN_IMAGE = os.path.join(waveunitdir, 'medn_halph_fl_20050501_074655.fts')
-MQ_IMAGE = os.path.join(waveunitdir, 'mq130812.084253.fits')
-NA_IMAGE = os.path.join(waveunitdir, 'na120701.091058.fits')
-SVSM_IMAGE = os.path.join(waveunitdir, 'svsm_e3100_S2_20110625_1856.fts')
+waveunitdir = str(pathlib.Path.home().joinpath(testrootdir, 'waveunit'))
+MEDN_IMAGE = str(pathlib.Path.home().joinpath(waveunitdir, 'medn_halph_fl_20050501_074655.fts'))
+MQ_IMAGE = str(pathlib.Path.home().joinpath(waveunitdir, 'mq130812.084253.fits'))
+NA_IMAGE = str(pathlib.Path.home().joinpath(waveunitdir, 'na120701.091058.fits'))
+SVSM_IMAGE = str(pathlib.Path.home().joinpath(waveunitdir, 'svsm_e3100_S2_20110625_1856.fts'))
