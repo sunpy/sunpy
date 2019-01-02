68abf57b6ad401ca42d2bec8e55a21b867b9eea8
Jack Ireland <jackireland@gmail.com>
2019-01-02 12:28:08 -0500

    modified diff rot

--- sunpy/physics/differential_rotation.py
+++ sunpy/physics/differential_rotation.py
@@ -133,7 +133,16 @@ def solar_rotate_coordinate(coordinate, observer=None, time=None, **diff_rot_kwa
 
     Example
     -------
-
+    >>> import astropy.units as u
+    >>> from astropy.coordinates import SkyCoord
+    >>> from sunpy.coordinates import frames
+    >>> from sunpy.physics.differential_rotation import solar_rotate_coordinate
+    >>> start_time = '2010-09-10 12:34:56'
+    >>> end_time = '2010-09-10 13:34:56'
+    >>> c = SkyCoord(-570*u.arcsec, 120*u.arcsec, obstime=start_time, frame=frames.Helioprojective)
+    >>> solar_rotate_coordinate(c, time=end_time)
+    <SkyCoord (Helioprojective: obstime=2010-09-10T13:34:56.000, rsun=695508.0 km, observer=<HeliographicStonyhurst Coordinate for 'earth'>): (Tx, Ty, distance) in (arcsec, arcsec, km)
+        (-562.37689548, 119.26840368, 1.50083152e+08)>
     """
     # Check the input and create the new observer
     if (observer is not None) and (time is not None):
