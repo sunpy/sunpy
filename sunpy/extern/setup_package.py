5008084372a37e3ac50de74142a2fd8d63e50d23
Pulkit gera <pulkithifi@yahoo.com>
2019-01-07 04:21:39 +0530

    setup files reverted back

--- sunpy/extern/setup_package.py
+++ sunpy/extern/setup_package.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from pathlib import Path
+import os
 
 
 def get_package_data():
-    paths = [str(Path.home().joinpath('js', '*.js'))]
+    paths = [os.path.join('js', '*.js')]
     return {'sunpy.extern': paths}
