8dd7a3106fd2ae22502ac8e339c80c093c81570e
Nabil Freij <nabil.freij@gmail.com>
2019-01-09 10:09:33 +0000

    comments and moved the file location

--- examples/units_and_coordinates/constants_reference.py
+++ examples/units_and_coordinates/constants_reference.py
@@ -4,17 +4,17 @@
 SunPy Constants
 ===============
 
-The example shows all of the constants provided by sunpy.
+The example shows all of the solar physics specific constants provided by SunPy.
 """
 
 from sunpy.sun import constants as con
 
 ##############################################################################
 # All constants are stored in a dictionary. A list of all available keys
-# which describe the constant can be had with the following command.
-con.constants.keys()
+# which describe each constant can be had with the following command.
+print(con.constants.keys())
 
 ##############################################################################
 # The following command will display all constants as well as their values
 # in an astropy `Table <http://docs.astropy.org/en/stable/table/index.html>`_
-print(con.print_all())
+con.print_all()
