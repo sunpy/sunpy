9fd0bc2204e56ef43a5a80fbf63d2ddede8fb26b
Stuart Mumford <stuart@cadair.com>
2019-02-07 11:23:54 +0000

    Override the default astropy test command

--- setup.py
+++ setup.py
@@ -5,12 +5,25 @@ import itertools
 
 from setuptools.config import read_configuration
 
+# Append cwd for pip 19
 sys.path.append(os.path.abspath("."))
 import ah_bootstrap  # noqa
 
-from astropy_helpers.setup_helpers import setup # noqa
+from astropy_helpers.setup_helpers import setup, register_commands # noqa
 
-# Create an 'all' extra which is all the others combined
+################################################################################
+# Override the default Astropy Test Command
+################################################################################
+cmdclass = register_commands()
+try:
+    from sunpy.tests.setup_command import SunPyTest
+    # Overwrite the Astropy Testing framework
+    cmdclass['test'] = type('SunPyTest', (SunPyTest,),
+                            {'package_name': 'sunpy'})
+except Exception:
+    # Catch everything, if it doesn't work, we still want SunPy to install.
+    pass
+
 extras = read_configuration("setup.cfg")['options']['extras_require']
 extras['all'] = list(itertools.chain.from_iterable(extras.values()))
 
