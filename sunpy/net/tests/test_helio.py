d1967165fbe0aebfe2ed9b1d2b1d8107e231a95d
Deepankar Sharma <dash.srmk@gmail.com>
2019-03-12 16:58:40 +0530

    Added more unit tests

--- sunpy/net/tests/test_helio.py
+++ sunpy/net/tests/test_helio.py
@@ -5,7 +5,7 @@ import pytest
 
 from sunpy.net.helio.parser import (link_test, taverna_parser, wsdl_retriever,
                                     endpoint_parser, webservice_parser)
-from sunpy.net.helio import hec
+from sunpy.net.helio.hec import HECClient
 
 
 def wsdl_endpoints():
@@ -255,19 +255,33 @@ def test_link_test_on_urlerror(mock_link_test):
 
 
 @pytest.mark.remote_data
-def test_time_query():
-    hc = hec.HECClient()
-    start = '2005/01/03'
-    end = '2005/12/03'
-    table_name = 'rhessi_hxr_flare'
-    res = hc.time_query(start, end, table=table_name, max_records=10)
-    assert len(res.array) == 10
+@pytest.fixture(scope="module")
+def client():
+    link = 'http://helio.mssl.ucl.ac.uk:80/helio_hec/HelioTavernaService?wsdl'
+    return HECClient(link)
 
 
-def test_get_table_names():
-    hc = hec.HECClient()
-    tables = hc.get_table_names()
+@pytest.mark.remote_data
+def test_get_table_names(client):
+    tables = client.get_table_names()
     assert len(tables) == 126
     table = tables[0][0]
     assert isinstance(table, bytes)
     assert table == b'timed_see_flare'
+
+
+@pytest.mark.remote_data
+def test_select_table(client, monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda x: "11")
+    assert isinstance(client.select_table(), bytes)
+    monkeypatch.setattr('builtins.input', lambda x: "e")
+    assert client.select_table() is None
+
+
+@pytest.mark.remote_data
+def test_time_query(client):
+    start = '2005/01/03'
+    end = '2005/12/03'
+    table_name = b'rhessi_hxr_flare'
+    res = client.time_query(start, end, table=table_name, max_records=10)
+    assert len(res.array) == 10
