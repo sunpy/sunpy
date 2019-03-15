6ae84a124238f1e932b17eec3cd268269b2b1442
Laura Hayes <hayesla@tcd.ie>
2019-03-15 16:03:16 -0400

    added docstr

--- sunpy/net/dataretriever/sources/fermi_gbm.py
+++ sunpy/net/dataretriever/sources/fermi_gbm.py
@@ -6,6 +6,52 @@ __all__ = ['GBMClient']
 
 
 class GBMClient(GenericClient):
+    """
+    This GBMClient is for the Gamma-Ray Burst Monitor (GBM) instrument
+    aboard the Fermi satellite. Although GBMs primary objective  is to
+    detect gamma-ray bursts, it provides high quailty high energy solar
+    flare observations.
+
+    The instrument consists of 12 Sodium Iodide (NaI) scintillation
+    detectors, which are sensitive of an energy range from 4keV to 1MeV.
+    At any one time, 6 of the NaI detectors are Sunward facing. The
+    detectors are numbered ['n0, n1', ...'n11']. This client supports the
+    user to choose which detector to use through the 'a.Detector' attribute.
+    The default detector is 'n5'.
+
+    The GBM data comes in daily version files in two formats:
+        CSPEC - counts accumulated every  4.096 seconds in 128 energy channels
+    for each detector.
+        CTIME - counts accumulated every 0.256 seconds in 8 energy channels
+    Both of which can be accessed through the attrs 'a.Resolution'.
+    The default data type is CSPEC unless the user defines.
+
+    Examples
+    --------
+    >>> from sunpy.net import Fido, attrs as a
+    >>> res = Fido.search(a.Time('2015-06-21 00:00', '2015-06-23 23:59'),
+                          a.Instrument('gbm'), a.Detector('n3'), a.Resolution('ctime'))
+    >>> res
+    <sunpy.net.fido_factory.UnifiedResponse object at 0x125961c50>
+    Results from 1 Provider:
+
+    3 Results from the GBMClient:
+         Start Time           End Time      Source Instrument Wavelength
+           str19               str19         str5     str3       str3
+    ------------------- ------------------- ------ ---------- ----------
+    2015-06-21 00:00:00 2015-06-23 23:59:00  FERMI        GBM        nan
+    2015-06-21 00:00:00 2015-06-23 23:59:00  FERMI        GBM        nan
+    2015-06-21 00:00:00 2015-06-23 23:59:00  FERMI        GBM        nan
+
+
+    References
+    ----------
+    * `Fermi Mission Homepage <https://fermi.gsfc.nasa.gov>`_
+    * `Fermi GBM Homepage <https://fermi.gsfc.nasa.gov/science/instruments/gbm.html>`_
+    * `Fermi Science Support Center <https://fermi.gsfc.nasa.gov/ssc/>`_
+    * `Fermi Data Product <https://fermi.gsfc.nasa.gov/ssc/data/access/>`_
+    """
+
     def _get_url_for_timerange(self, timerange, **kwargs):
         """
         Returns the url for Fermi/GBM data for the given date.
