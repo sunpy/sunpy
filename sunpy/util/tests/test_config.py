355f9c1bed05a79d562fc6fd056c9b6333089d0a
Stuart Mumford <stuart@cadair.com>
2019-03-16 15:14:26 +0000

    Change the default sample data directory to appdirs user_data_dir

--- sunpy/util/tests/test_config.py
+++ sunpy/util/tests/test_config.py
@@ -1,11 +1,12 @@
 import io
 import os
 import sys
+from pathlib import Path
 
 from sunpy import config
 from sunpy.util.config import (get_and_create_sample_dir, get_and_create_download_dir,
                                CONFIG_DIR, print_config,
-                               _find_config_files, _get_user_configdir, _is_writable_dir)
+                               _find_config_files, _get_user_configdir, _is_writable_dir, dirs)
 
 USER = os.path.expanduser('~')
 
@@ -68,7 +69,7 @@ def test_get_and_create_download_dir():
 def test_get_and_create_sample_dir():
     # test default config
     path = get_and_create_sample_dir()
-    assert path == os.path.join(USER, 'sunpy', 'data', 'sample_data')
+    assert Path(path) == Path(dirs.user_data_dir)
     # test updated config
     new_path = os.path.join(USER, 'sample_data_here_please')
     config.set('downloads', 'sample_dir', new_path)
