14a15784217e8f63ccd740aedcf4ec757443a594
Jack Ireland <jackireland@gmail.com>
2019-04-14 18:14:28 -0400

    updating the example to use the new functions

--- examples/map/base_and_running_difference_mapcubes.py
+++ examples/map/base_and_running_difference_mapcubes.py
@@ -9,10 +9,9 @@ Base differencing uses a fixed map when compared to running difference.
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-import matplotlib.cm as cm
 
 import sunpy.map
-import sunpy.physics.differential_rotation as diffrot
+from import sunpy.physics.differential_rotation import differential_rotate
 from sunpy.data.sample import AIA_193_CUTOUT01_IMAGE, AIA_193_CUTOUT02_IMAGE, AIA_193_CUTOUT03_IMAGE
 
 ###############################################################################
@@ -30,19 +29,34 @@ aiamapseq = sunpy.map.Map(AIA_193_CUTOUT01_IMAGE, AIA_193_CUTOUT02_IMAGE,
 # We then store all the difference maps in a list.
 base_diffmap = []
 running_diffmap = []
-for i, map_i in enumerate(aiamapseq[1:]):
-    aiamap_rot = diffrot.diffrot_map(map_i, time=aiamapseq[0].date)
-    aiamapseq_rot = diffrot.diffrot_map(map_i, time=aiamapseq[i].date)
-    diffdata = map_i.data - aiamap_rot.data
-    smap_base = sunpy.map.Map(diffdata, map_i.meta)
-    diffdata = aiamapseq_rot.data - map_i.data
-    smap_run = sunpy.map.Map(diffdata, map_i.meta)
-    smap_base.plot_settings['cmap'] = plt.get_cmap('Greys_r')
-    smap_base.plot_settings['norm'] = colors.LogNorm(100, smap_base.max())
-    smap_run.plot_settings['cmap'] = plt.get_cmap('Greys_r')
-    smap_run.plot_settings['norm'] = colors.LogNorm(100, smap_run.max())
-    base_diffmap.append(smap_base)
-    running_diffmap.append(smap_run)
+
+# Storage for the differentially rotated maps. The first map is
+# not differentially rotated.  We will also need the top left
+
+# Let's do a base difference
+drm = [aiamapseq[0]]
+for m in aiamapseq[1:]:
+    # differentially rotate each map and store it
+    drm.append(differential_rotate(m, time=aiamapseq[0].date))
+
+# Find the area common to all the differentially rotated maps
+common_submap = drm[0]
+for m in drm[1:]:
+    bl, tr = overlap_coordinates(common_submap, m)
+    common_submap = m.submap(bl, tr)
+common_submap_dimensions = common_submap.dimensions
+
+
+# Now do a base difference using the size of the common submap data.
+base_difference_maps = [drm[0].submap(bl, tr)]
+for m in drm[1:]:
+    m_submap = m.submap(bl, tr)
+    base_difference_data = base_difference_maps[0].data - m_submap.data
+    base_difference_map = sunpy.map.Map(base_difference_data, m.meta)
+    base_difference_map.plot_settings['cmap'] = plt.get_cmap('Greys_r')
+    base_difference_map.plot_settings['norm'] = colors.LogNorm(100, base_difference_data.max())
+    base_difference_maps.append(base_difference_map)
+
 
 ############################################################################
 # This plots the original MapSequence
@@ -50,8 +64,8 @@ aiamapseq.peek()
 
 ##############################################################################
 # This plots the final MapSequence obtained after implementing the base difference
-result_mapseq = sunpy.map.MapSequence(base_diffmap)
-result_mapseq.peek()
+base_difference_mapsequence = sunpy.map.MapSequence(base_difference_maps)
+base_difference_mapsequence.peek()
 
 ############################################################################
 # This plots the final MapSequence after implementing the running difference
