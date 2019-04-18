536b5759cf191a1bd503b4ca05f991518e9f0f99
Shane Maloney <shane.maloney@tcd.ie>
2019-04-18 09:23:34 +0100

    Fix PEP8 issues

--- sunpy/map/maputils.py
+++ sunpy/map/maputils.py
@@ -1,9 +1,10 @@
 """
-Hold utility map function which do not belong in map objects
+Holds utility map function which do not belong in map objects
 """
 
 
 __all__ = ['test_function']
 
+
 def test_function(map):
-	return map.name
+    return map.name
