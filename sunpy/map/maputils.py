882d1d07eb7396c633025d190f11ba5759cb7d1f
Nabil Freij <nabil.freij@gmail.com>
2019-05-02 15:22:42 -0400

    Update sunpy/map/maputils.py
    Co-Authored-By: wafels <jackireland@gmail.com>

--- sunpy/map/maputils.py
+++ sunpy/map/maputils.py
@@ -124,7 +124,7 @@ def is_all_off_disk(smap):
     The check is performed by calculating the distance of every
     pixel from the center of the Sun. If they are all off-disk,
     then the function returns `True`. Otherwise, the function
-    returns False.
+    returns `False`.
 
     Parameters
     ----------
