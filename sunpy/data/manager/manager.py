65d887e6d995349200adea244003f35d519edf4a
Vishnunarayan K I <appukuttancr@gmail.com>
2019-05-22 22:02:55 +0530

    Fix: Fix cache logic

--- sunpy/data/manager/manager.py
+++ sunpy/data/manager/manager.py
@@ -10,6 +10,8 @@ class DataManager:
         self._downloader = downloader
         self._storage = storage
 
+        self._file_cache = {}
+
         self._skip_hash_check = False
         self._skip_file_names = []
 
@@ -18,14 +20,26 @@ class DataManager:
             # skip hash check = down
             # skip file name = down or replace
             # check store
-            if name in self._skip_file_names:
+            if self._skip_hash_check:
+                file_path, _ = self._download_and_hash(urls)
+            elif name in self._skip_file_names:
+                # TODO: Replace file
                 file_path, _ = self._download_and_hash(urls)
             else:
                 details = self._storage.find_by_hash(sha_hash)
                 if not details:
-                   file_path, file_hash =
+                    file_path, file_hash = self._download_and_hash(urls)
+                    # TODO: add modified date
+                    # TODO: add function names
+                    self._storage.store({
+                        'file_hash': file_hash,
+                        'file_path': file_path,
+                    })
+                else:
+                    file_path, file_hash = details['file_path'], details['file_hash']
 
-
+            # TODO: handle name clashing
+            self._file_cache[name] = file_path
             return func(*args, **kwargs)
 
         return function
