60f8a719376d46779d512b40a88ac2f0bdd512d7
Vishnunarayan K I <appukuttancr@gmail.com>
2019-05-29 18:46:17 +0530

    Implement replace_file and add a basic test for data manager

--- sunpy/data/manager/manager.py
+++ sunpy/data/manager/manager.py
@@ -1,5 +1,7 @@
 from contextlib import contextmanager
 
+import functools
+
 
 class DataManager:
     """
@@ -13,36 +15,70 @@ class DataManager:
         self._file_cache = {}
 
         self._skip_hash_check = False
-        self._skip_file_names = []
+        self._skip_file = {}  # Dict[str, str]
 
-    def require(self, func, name, urls, sha_hash):
-        def function(*args, **kwargs):
-            # skip hash check = down
-            # skip file name = down or replace
-            # check store
-            if self._skip_hash_check:
-                file_path, _ = self._download_and_hash(urls)
-            elif name in self._skip_file_names:
-                # TODO: Replace file
-                file_path, _ = self._download_and_hash(urls)
-            else:
-                details = self._storage.find_by_hash(sha_hash)
-                if not details:
-                    file_path, file_hash = self._download_and_hash(urls)
-                    # TODO: add modified date
-                    # TODO: add function names
-                    self._storage.store({
-                        'file_hash': file_hash,
-                        'file_path': file_path,
-                    })
+    def require(self, name, urls, sha_hash):
+        """decorator for doing stuff
+
+        Parameters
+        ----------
+        func: function
+        function for the decorator
+        name: str
+        The name to reference the file with
+        urls: list
+        List of urls to download the file from
+        sha_hash: str
+        Hash of file
+        """
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                # skip hash check = down
+                # skip file name = down or replace
+                # check store
+                replace = self._skip_file.get(name, None)
+                if replace:
+                    pass
+                elif self._skip_hash_check:
+                    file_path, _ = self._download_and_hash(urls)
                 else:
-                    file_path, file_hash = details['file_path'], details['file_hash']
+                    details = self._storage.find_by_hash(sha_hash)
+                    if not details:
+                        file_path, file_hash = self._download_and_hash(urls)
+                        # TODO: add modified date
+                        # TODO: add function names
+                        self._storage.store({
+                            'file_hash': file_hash,
+                            'file_path': file_path,
+                        })
+                    else:
+                        file_path, file_hash = details['file_path'], details['file_hash']
 
-            # TODO: handle name clashing
-            self._file_cache[name] = file_path
-            return func(*args, **kwargs)
+                # TODO: handle name clashing
+                self._file_cache[name] = file_path
+                return func(*args, **kwargs)
+            return wrapper
 
-        return function
+        return decorator
+
+    @contextmanager
+    def replace_file(self, name, uri):
+        """Replaces the file by the name with the file provided by the url/path
+
+        TODO: Hash
+
+        Parameters
+        ----------
+        name: str
+        Name of the file provided in the `require` decorator
+        uri: str
+        URI of the file which replaces original file. One of `http`, `https`, `ftp`
+        or `file`
+        """
+        try:
+            self._skip_file[name] = uri
+        finally:
+            _ = self._skip_file.pop(name, None)
 
     @contextmanager
     def skip_hash_check(self):
