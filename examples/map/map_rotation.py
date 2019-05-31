40c2e64bf0455d75e8b7a3288c8c8007bfafdc49
Steven Christe <steven.christe@nasa.gov>
2019-05-31 14:40:00 -0500

    Update map_rotation.py

--- examples/map/map_rotation.py
+++ examples/map/map_rotation.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-=========================================
-Map Rotation
-=========================================
+==============
+Rotating a Map
+==============
 
-In this example we rotate a map.
+How to rotate a map.
 """
-
-##############################################################################
-# Start by importing the necessary modules.
 import astropy.units as u
 
 import matplotlib.pyplot as plt
@@ -16,34 +13,24 @@ import matplotlib.pyplot as plt
 import sunpy.map
 import sunpy.data.sample
 
-##############################################################################
-# Sunpy sample data contains a number of suitable maps, where the sunpy.data.sample.NAME
-# returns the location of the given FITS file.
+###############################################################################
+# We start with the sample data
 aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ##############################################################################
-# This has a resolution of:
-print(aia_map.dimensions)
-
-##############################################################################
-# To find out more specifics about this map and the instrument used, check it's
-# metatdata:
-print(aia_map.meta)
-
-##############################################################################
-# Maps can also be rotated by using the `~sunpy.map.GenericMap.rotate` method
-# with a specified angle, supplied as an Astropy Quantity with angular units:
+# `~sunpy.map.GenericMap` provides the `~sunpy.map.GenericMap.rotate` method
+# which accepts an angle. This returns a rotated map and does not rotate in
+# place. The data array size is expanded so that none of the original data is
+# lost due to clipping. Note that subsequent rotations are not compounded.
+# The map is only rotated by the specified amount from the original maps
+# orientation.
 aia_rotated = aia_map.rotate(angle=30 * u.deg)
 
-plt.figure()
+###############################################################################
+# Let's now plot the results.
+fig = plt.figure()
+ax = plt.subplot(projection=aia_rotated)
 aia_rotated.plot()
 aia_rotated.draw_limb()
 aia_rotated.draw_grid()
-
-# Or using Radians
-aia_rotated = aia_map.rotate(angle=0.5 * u.rad)
-# Note: the data array is expanded so that none of the original data is lost
-# through clipping.
-# Also note that subsequent rotations are not compunded, the map is only rotated
-# by the specified amount from the original maps orientation.
 plt.show()
