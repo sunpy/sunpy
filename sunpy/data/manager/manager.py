cb0a21762ed890816104d3bc3df5d8ef3d4bccb0
Vishnunarayan K I <appukuttancr@gmail.com>
2019-06-04 15:19:18 +0530

    chore: Make data manager use `Cache` and fix the tests

--- sunpy/data/manager/manager.py
+++ sunpy/data/manager/manager.py
@@ -9,10 +9,9 @@ class DataManager:
     DataManager
     """
 
-    def __init__(self, downloader, storage):
+    def __init__(self, cache):
         # TODO: Folder prefix should be init argument
-        self._downloader = downloader
-        self._storage = storage
+        self._cache = cache
 
         self._file_cache = {}
 
@@ -40,23 +39,16 @@ class DataManager:
                     if replace.startswith('file://'):
                         file_path = replace[len('file://'):]
                     else:
-                        file_path, _ = self._download_and_hash([replace])
+                        file_path = self._cache.download([replace], redownload=True)
                 elif self._skip_hash_check:
-                    # TODO: This redownloads every time
-                    # Use cache instead
-                    file_path, _ = self._download_and_hash(urls)
+                    # XXX: Should this redownload every time
+                    file_path = self._cache.download(urls, redownload=True)
                 else:
-                    details = self._storage.find_by_hash(sha_hash)
+                    details = self._cache.get_by_hash(sha_hash)
                     if not details:
-                        file_path, file_hash = self._download_and_hash(urls)
-                        # TODO: add modified date
-                        # TODO: add function names
-                        self._storage.store({
-                            'file_hash': file_hash,
-                            'file_path': file_path,
-                        })
+                        file_path = self._cache.download(urls)
                     else:
-                        file_path, file_hash = details['file_path'], details['file_hash']
+                        file_path = details['file_path']
 
                 # TODO: handle name clashing
                 self._file_cache[name] = file_path
