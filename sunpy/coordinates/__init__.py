504cbfafaeca9d2bd275e5cdc36406a2926b14eb
Albert Y. Shih <ayshih@gmail.com>
2019-06-06 11:04:21 -0400

    Make our own diagram description and move it up a level

--- sunpy/coordinates/__init__.py
+++ sunpy/coordinates/__init__.py
@@ -1,7 +1,29 @@
+"""
+This subpackage contains:
+
+* A robust framework for working with coordinate systems
+* Functions to obtain the locations of solar-system bodies
+* Functions to calculate Sun-specific coordinate information
+  (`sunpy.coordinates.sun`)
+
+The diagram below shows all of Sun-based and Earth-based coordinate systems
+available through `sunpy.coordinates`, as well as the transformations between
+them.  Each frame is labeled with the name of its class and its alias (useful
+for converting other coordinates to them using attribute-style access).
+
+The frames colored in cyan are implemented in `astropy.coordinates`, and there
+are other astronomical frames that can be transformed to that are not shown
+below.  See the documentation for `astropy.coordinates` for more information.
+
+"""
+
 from .frames import *
 from .offset_frame import *
 from . import transformations
+from .transformations import _make_sunpy_graph
 from .ephemeris import *
 from . import sun
 
 from .wcs_utils import *
+
+__doc__ += _make_sunpy_graph()
