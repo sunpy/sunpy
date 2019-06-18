da41cff41cbbf23b2aec1c003d7300e8c0c07861
Vishnunarayan K I <appukuttancr@gmail.com>
2019-06-18 22:42:37 +0530

    feat: Add config options for download dir and cache expiry

--- sunpy/data/__init__.py
+++ sunpy/data/__init__.py
@@ -1,12 +1,14 @@
-from sunpy.data._sample import download_sample_data
-from sunpy.data.data_manager.cache import Cache
-from sunpy.data.data_manager.storage import SqliteStorage
-from sunpy.data.data_manager.downloader import ParfiveDownloader
-from sunpy.data.data_manager.manager import DataManager
-from sunpy.util.config import get_and_create_download_dir
 import astropy.units as u
 
-_download_dir = get_and_create_download_dir()
+from sunpy import config
+from sunpy.data._sample import download_sample_data
+from sunpy.data.data_manager.cache import Cache
+from sunpy.data.data_manager.downloader import ParfiveDownloader
+from sunpy.data.data_manager.manager import DataManager
+from sunpy.data.data_manager.storage import SqliteStorage
+from sunpy.util.config import CACHE_DIR
+
+_download_dir = config.get('downloads', 'remote_data_manager_dir')
 manager = DataManager(
     Cache(
         ParfiveDownloader(),
@@ -17,9 +19,9 @@ manager = DataManager(
 
 cache = Cache(
     ParfiveDownloader(),
-    SqliteStorage(_download_dir + '/cache.db'),
-    _download_dir,
-    expiry=10*u.day
+    SqliteStorage(CACHE_DIR + '/cache.db'),
+    CACHE_DIR,
+    expiry=int(config.get('downloads', 'cache_expiry')) * u.day
 )
 
 __all__ = ["download_sample_data", "manager", "cache"]
