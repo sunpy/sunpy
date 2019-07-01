5661416757f2bf941f2b9dcfaefdba2d3d71d06c
Stuart Mumford <stuart@cadair.com>
2019-07-01 21:54:41 +0100

    Fix that

--- sunpy/conftest.py
+++ sunpy/conftest.py
@@ -47,8 +47,8 @@ def tmp_config_dir(request):
     tmpdir = tempfile.TemporaryDirectory()
 
     os.environ["SUNPY_CONFIGDIR"] = str(pathlib.Path(__file__).parent / "data")
-    astropy.config.paths.set_temp_config._temp_path = str(tmpdir)
-    astropy.config.paths.set_temp_cache._temp_path = str(tmpdir)
+    astropy.config.paths.set_temp_config._temp_path = str(tmpdir.name)
+    astropy.config.paths.set_temp_cache._temp_path = str(tmpdir.name)
 
     yield
 
