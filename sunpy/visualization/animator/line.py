32b8197edc0f21223ce39f1697d1d85ce2ca6421
Stuart Mumford <stuart@cadair.com>
2019-07-31 11:08:21 -0400

    Merge pull request #3283 from yashrsharma44/plot-bug-fix
    Shifted the axis for calculation of edges_to_centers for LineAnimator

--- sunpy/visualization/animator/line.py
+++ sunpy/visualization/animator/line.py
@@ -95,11 +95,18 @@ class LineAnimator(ArrayAnimator):
             axis_ranges = None
         if axis_ranges is None or axis_ranges[self.plot_axis_index] is None:
             self.xdata = np.arange(data.shape[self.plot_axis_index])
+        # Else derive the xdata as pixel centers from the pixel edges supplied by
+        # the user in axis_ranges[plot_axis_index]
         else:
-            # Else derive the xdata as the centers of the pixel/bin edges
-            # supplied by the user for the plotted axis.
-            self.xdata = edges_to_centers_nd(np.asarray(axis_ranges[self.plot_axis_index]),
-                                             plot_axis_index)
+            # If the shape of the array is a 1D array, get the centers about axis=0
+            if np.asarray(axis_ranges[self.plot_axis_index]).ndim == 1:
+                self.xdata = edges_to_centers_nd(np.asarray(axis_ranges[self.plot_axis_index]), 0)
+
+            # Else derive the xdata as pixel centers from the pixel edges supplied by
+            # the user in axis_ranges[plot_axis_index] along axis=plot_axis_index
+            else:
+                self.xdata = edges_to_centers_nd(np.asarray(axis_ranges[self.plot_axis_index]),
+                                                 plot_axis_index)
         if ylim is None:
             ylim = (data.min(), data.max())
         if xlim is None:
