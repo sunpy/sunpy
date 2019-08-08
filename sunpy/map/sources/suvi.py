d7f4fa8854faf9c50512cde8ed0e4b5af3394aec
wafels <jackreland@gmail.com>
2019-08-08 09:49:06 -0400

    added in more documentation on SUVI

--- sunpy/map/sources/suvi.py
+++ sunpy/map/sources/suvi.py
@@ -19,12 +19,29 @@ __all__ = ['SUVIMap']
 class SUVIMap(GenericMap):
     """SUVI Image Map.
 
-    The Solar Ultraviolet Imager is a telescope that monitors the sun in the
-    extreme ultraviolet wavelength range. By observing the sun, SUVI is able
-    to compile full disk solar images around the clock. It replaces the GOES
-    Solar X-ray Imager (SXI) instrument and represents a change in both
-    spectral coverage and spatial resolution over SXI.
+    The Solar Ultraviolet Imager (SUVI) is a normal-incidence Cassegrain telescope
+    that shares considerable design heritage with the Atmospheric Imaging Assembly
+    (AIA). SUVI images the Sun in six EUV wavelengths: 9.4, 13.1, 17.1, 19.5,
+    28.4, and 30.4 nm. The instrument consists of the main imaging telescope, a
+    Guide Telescope (GT) and a Camera Electronics Box (CEB) mechanically integrated
+    to the telescope, and a SUVI Electronics Box (SEB). The SEB provides power and
+    data interfaces to the spacecraft. The optical chain in the telescope consists
+    of thin film entrance filters (metals vapor deposited on a supporting metallic
+    mesh), multi-layer coated primary and secondary mirrors, a set of thin film
+    analysis filters in two filter wheels near the focal plane, and a charge-coupled
+    device (CCD) detector. The CCD consists of 1280 x 1280 pixels with a plate
+    scale of 2.5 arcsec and together with the optical system provides a nominal
+    53 arcminute square field of view (FOV) from the geostationary orbit. An
+    aperture selector with a 60 degree  opening and two multi-segmented mirrors
+    enable the imaging in any of the six wavelengths in a single telescope body.
+    A nominal 4-minute cadence provides for the observation of the Sun in all
+    wavelengths while meeting large dynamic range requirements on single- and
+    multi-spectral images. The GT provides Sun-pointing knowledge. In the nominal
+    Sun-pointing case, the spacecraft control system uses the GT data to control the
+    line-of-sight (LOS) to the Sun.
 
+    Notes
+    -----
     SUVI color tables are the same as the AIA color tables for the same
     wavelength with exceptions of SUVI 195 and 284 (which has no direct
     equivalent obviously).  SUVI 195 and 284 images use the AIA 193 & 335 color
@@ -33,6 +50,7 @@ class SUVIMap(GenericMap):
     References
     ----------
     * `SUVI Instrument Page <https://www.goes-r.gov/spacesegment/suvi.html>`_
+    * `Coronal Imaging with the Solar UltraViolet Image <http://doi.org/10.1007/s11207-019-1411-0>`_
     """
 
     def __init__(self, data, header, **kwargs):
