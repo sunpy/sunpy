86f64c21cafa056ab71da34bf15df99be675983c
hayesla <hayesla@tcd.ie>
2019-10-23 15:58:14 -0400

    fixing remote data for tests

--- sunpy/net/dataretriever/tests/test_lyra_ud.py
+++ sunpy/net/dataretriever/tests/test_lyra_ud.py
@@ -15,6 +15,7 @@ from sunpy.net.tests.strategies import time_attr
 LCClient = lyra.LYRAClient()
 
 
+@pytest.mark.remote_data
 @pytest.mark.parametrize("timerange,url_start,url_end", [
     (TimeRange('2012/1/7', '2012/1/7'),
      'http://proba2.oma.be/lyra/data/bsd/2012/01/07/lyra_20120107-000000_lev2_std.fits',
