0b16249b333f88efbdab9e5ac0f29a4093d4e46b
David Stansby <dstansby@gmail.com>
2019-11-13 08:34:45 +0000

    Apply suggestions from code review
    Co-Authored-By: Stuart Mumford <stuart@cadair.com>

--- sunpy/util/types.py
+++ sunpy/util/types.py
@@ -3,7 +3,8 @@ A collection of ABCs to use to shortcut having to import things.
 """
 import abc
 
+
 class DatabaseEntryType(metaclass=abc.ABCMeta):
     """
-    A Base class for sunpy.database.DatabaseEntry.
+    A base class for `sunpy.database.tables.DatabaseEntry`.
     """
