fe089fa612d9871b042c769aaff830e99c4efc0f
Stuart Mumford <stuart@cadair.com>
2019-11-13 10:10:53 +0000

    Don't make the Type and ABC to fix metaclass conflict

--- sunpy/util/types.py
+++ sunpy/util/types.py
@@ -1,10 +1,10 @@
 """
 A collection of ABCs to use to shortcut having to import things.
 """
-import abc
 
 
-class DatabaseEntryType(metaclass=abc.ABCMeta):
+class DatabaseEntryType:
     """
     A base class for `sunpy.database.tables.DatabaseEntry`.
+    This class should not be used directly.
     """
