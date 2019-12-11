da06212757a3c4452283a0599a0086073b944c51
Stuart Mumford <stuart@cadair.com>
2019-12-11 11:30:25 -0700

    Add help comments for test and build_docs

--- setup.py
+++ setup.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python
+import sys
+
+################################################################################
+# Raise helpful messages for test and build_docs commands
+################################################################################
+test_help = """\
+Running tests is no longer done using 'python setup.py test'.
+
+Instead you will need to run:
+    tox -e offline
+if you don't already have tox installed, you can install it with:
+    pip install tox
+if you only want to run part of the test suite, you can also use pytest directly with:
+    pip install -e .[dev]
+    pytest
+for more information, see:
+  https://docs.sunpy.org/en/latest/dev_guide/tests.html
+"""
+
+if 'test' in sys.argv:
+    print(test_help)
+    sys.exit(1)
+
+docs_help = """\
+Building the documentation is no longer done using 'python setup.py build_docs'.
+
+Instead you will need to run:
+    tox -e build_docs
+if you don't already have tox installed, you can install it with:
+    pip install tox
+for more information, see:
+   https://docs.sunpy.org/en/latest/dev_guide/documentation.html#usage
+"""
+
+if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
+    print(docs_help)
+    sys.exit(1)
+
+################################################################################
+# Actual setup.py content
+################################################################################
 from itertools import chain
 
 from setuptools import setup
