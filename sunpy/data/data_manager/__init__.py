30184e0c6d99ec6c1e6772e454d2a66c86a34c8a
Stuart Mumford <stuart@cadair.com>
2020-01-09 11:42:47 +0000

    polish up docs for data_manager a little

---
+++ sunpy/data/data_manager/__init__.py
@@ -0,0 +1,4 @@
+from sunpy.data.data_manager.cache import Cache
+from sunpy.data.data_manager.downloader import ParfiveDownloader
+from sunpy.data.data_manager.manager import DataManager
+from sunpy.data.data_manager.storage import SqliteStorage
