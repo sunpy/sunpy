c2bdf19c40b29d31b5a1a8bd073379cec6fb1ea6
Raahul Singh <raahul.s18@iiits.in>
2020-01-10 21:10:23 +0530

    Adds changelog and fixes pep8 issues.

--- sunpy/visualization/wcsaxes_compat.py
+++ sunpy/visualization/wcsaxes_compat.py
@@ -128,7 +128,7 @@ def wcsaxes_heliographic_overlay(axes, grid_spacing: u.deg = 10*u.deg, annotate=
         Spacing for longitude and latitude grid in degrees.
     annotate : `bool`
         Passing `False` disables the axes labels and the ticks on the top and right axes.
-    
+
     Returns
     -------
     `~astropy.visualization.wcsaxes.WCSAxes`
