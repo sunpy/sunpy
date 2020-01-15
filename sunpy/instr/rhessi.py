2aed98be0577d7eccda79849f53ee9af6fc37a35
Laubeee <silvanlaube@hotmail.com>
2020-01-15 19:40:26 +0100

    add methods to read, format and print rhessi flare list
    addresses #1018

--- sunpy/instr/rhessi.py
+++ sunpy/instr/rhessi.py
@@ -6,7 +6,11 @@ data.
 import re
 import csv
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 import numpy as np
+import pandas as pd
 
 import astropy.units as u
 from astropy.time import Time, TimeDelta
@@ -15,7 +19,10 @@ import sunpy.io
 from sunpy.coordinates import sun
 from sunpy.time import TimeRange, parse_time
 
-__all__ = ['parse_observing_summary_hdulist', 'backprojection', 'parse_observing_summary_dbase_file']
+__all__ = [
+    'parse_observing_summary_hdulist', 'backprojection', 'parse_observing_summary_dbase_file',
+    'read_flare_list', 'read_flare_file', 'print_flare_list', 'convert_flag_dict'
+]
 
 
 # Measured fixed grid parameters
@@ -26,6 +33,50 @@ grid_orientation = (3.53547, 2.75007, 3.53569, 2.74962, 3.92596, 2.35647,
 
 lc_linecolors = ('black', 'pink', 'green', 'blue', 'brown', 'red',
                  'navy', 'orange', 'green')
+
+known_flare_list_sources = {
+    "NASA": "https://hesperia.gsfc.nasa.gov/hessidata/dbase/",
+    "i4DS": "http://soleil.i4ds.ch/hessidata/dbase/",
+}
+
+flagid2flag = {
+    'SAA_AT_START': 'SS',
+    'SAA_AT_END': 'SE',
+    'SAA_DURING_FLARE': 'SD',
+    'ECLIPSE_AT_START': 'ES',
+    'ECLIPSE_AT_END': 'EE',
+    'ECLIPSE_DURING_FLARE': 'ED',
+    'FLARE_AT_SOF': 'FS',
+    'FLARE_AT_EOF': 'FE',
+    'NON_SOLAR': 'NS',
+    'FAST_RATE_MODE': 'FR',
+    'FRONT_DECIMATION': 'DF',
+    'ATT_STATE_AT_PEAK': '',  # 0-3 -> changes flag an to An
+    'DATA_GAP_AT_START': 'GS',
+    'DATA_GAP_AT_END': 'GE',
+    'DATA_GAP_DURING_FLARE': 'GD',
+    'PARTICLE_EVENT': 'PE',
+    'DATA_QUALITY': 'Qn',  # 0-11
+    'POSITION_QUALITY': 'P1',
+    'ATTEN_0': 'a0',
+    'ATTEN_1': 'a1',
+    'ATTEN_2': 'a2',
+    'ATTEN_3': 'a3',
+    'REAR_DECIMATION': 'DR',
+    'MAGNETIC_REGION': 'MR',
+    'IMAGE_STATUS': '',
+    'SPECTRUM_STATUS': '',
+    'SOLAR_UNCONFIRMED': 'PS',
+    'SOLAR': ''
+}
+
+fits_rec_keys = [
+    'ID_NUMBER', 'START_TIME', 'END_TIME', 'PEAK_TIME', 'BCK_TIME', 'IMAGE_TIME', 'ENERGY_RANGE_FOUND', 'ENERGY_HI',
+    'PEAK_COUNTRATE', 'BCK_COUNTRATE', 'TOTAL_COUNTS', 'PEAK_CORRECTION', 'TOTAL_CORRECTION', 'POSITION',
+    'FILENAME', 'FLAGS', 'SEG_INDEX_MASK', 'EXTRA_BCK', 'SFLAG1', 'ACTIVE_REGION', 'GOES_CLASS', 'RADIAL_OFFSET',
+    'PEAK_PHFLUX', 'TOT_PHFLUENCE', 'E_PHFLUENCE', 'PEAK_PHFLUX_SIGMA', 'TOT_PHFLUENCE_SIGMA', 'E_PHFLUENCE_SIGMA',
+    'GOES_LEVEL_PK', 'ALT_ID'
+]
 
 
 def parse_observing_summary_dbase_file(filename):
@@ -348,3 +399,181 @@ def _build_energy_bands(label, bands):
     unit = matched.group('UNIT').strip()
 
     return [f'{band} {unit}' for band in bands]
+
+
+def read_flare_list(start, end, source='NASA', file_format="hessi_flare_list_%Y%m.fits", inc=relativedelta(months=+1)):
+    """
+    Read and combine RHESSI flare lists from .fits files as specified with further parameters
+    Dates are allowed in the following formats:
+        'YY-mm', 'YYYYmm', 'YYYY-mm', 'YYYYmmdd', 'YYYY-mm-dd', 'YYYY-mm-ddThh:MM:ss'
+
+    Parameters
+    ----------
+    start : `str`
+        Start date of period within which flares should be loaded.
+    end : `str`
+        End date of period within which flares should be loaded.
+    source : `str`, optional
+        Source from where .fits files should be loaded. Can be an URL or a local folder.
+        Known sources are "NASA" and "i4DS".
+        Defaults to ``"NASA"``.
+    file_format : `str`, optional
+        Specifies the naming convention of the files available in the source folder.
+        The changing parts should be given as format string as in ``strftime``.
+        Defaults to ``"hessi_flare_list_%Y%m.fits"``.
+    inc : `timedelta`, `relativedelta`, optional
+        Specifies by how much time the single files are separated.
+        Defaults to ``relativedelta(months=+1)``.
+
+    Returns
+    -------
+    ``pandas.DataFrame``
+        out : ``pandas.DataFrame`` containing the flares within the given time constraints
+    """
+
+    formats = {
+        5: "%y-%m",  # YY-mm
+        6: "%Y%m",  # YYYYmm
+        7: "%Y-%m",  # YYYY-mm
+        8: "%Y%m%d",  # YYYYmmdd
+        10: "%Y-%m-%d",  # YYYY-mm-dd
+        19: "%Y-%m-%dT%H:%M:%S",  # YYYY-mm-ddThh:MM:ss
+    }
+    try:
+        start_dt = datetime.strptime(start, formats[len(start)])
+        end_dt = datetime.strptime(end, formats[len(end)])
+    except (KeyError, ValueError):
+        raise ValueError("invalid datetime")
+
+    format_str = file_format[file_format.index("%"):file_format.rindex("%") + 2]
+    cur_format = start_dt.strftime(format_str)
+    end_format = end_dt.strftime(format_str)
+
+    if source in known_flare_list_sources:
+        source = known_flare_list_sources[source]
+
+    cur_dt = start_dt
+    result = pd.DataFrame()
+    while cur_format <= end_format:
+        file = file_format.replace(format_str, cur_format)
+        result = result.append(read_flare_file(source + file), ignore_index=True)
+        cur_dt = cur_dt + inc
+        cur_format = cur_dt.strftime(format_str)
+
+    # filter results for more detailed time constraints (if applicable)
+    if len(end) < 8:
+        end_dt += relativedelta(months=+1)  # add month if no further constraints were defined
+    elif len(end) <= 10:
+        end_dt += relativedelta(days=+1)  # add day if end date was specified on a day-basis
+    else:
+        end_dt += relativedelta(microsecond=+1)  # add 1ms so "smaller" operator works as intended
+
+    result = result[result['END_TIME'] >= start_dt]
+    result = result[result['START_TIME'] < end_dt]
+    return result
+
+
+def read_flare_file(file):
+    """
+    Read RHESSI flare list .fits file into ``pandas.DataFrame``.
+    TIME values are parsed with format 'utime', which is the same as Unix timestamp but starts 9 years later.
+    FLAGS are assigned their respective label (FLAG ID) and returned as `dict`.
+
+    Parameters
+    ----------
+    file : `str`
+        The URL or local filename of the hessi flare list.
+
+    Returns
+    -------
+    ``pandas.DataFrame``
+        out : ``pandas.DataFrame`` containing the parsed flares.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> rhessi.read_flare_file("https://hesperia.gsfc.nasa.gov/hessidata/dbase/hessi_flare_list_201802.fits")
+
+    References
+    ----------
+    https://hesperia.gsfc.nasa.gov/rhessi3/data-access/rhessi-data/flare-list/index.html
+    """
+    try:
+        fits = sunpy.io.fits.read(file)
+    except Exception:
+        raise RuntimeError("couldn't load file " + file)
+
+    results = []
+    for row in fits[3].data[0:20]:
+        result_row = {}
+        for k in fits_rec_keys:
+            if k.endswith('_TIME'):
+                result_row[k] = parse_time(row[k], format="utime")
+                result_row[k].format = "datetime"  # for human readable display inside the DF
+            elif k == 'FLAGS':
+                flags = {}
+                for i, fid in zip(row[k], fits[2].data['FLAG_IDS'][0]):
+                    flags[fid] = i
+                result_row[k] = flags
+            else:
+                result_row[k] = row[k]
+        results.append(result_row)
+
+    return pd.DataFrame(results)
+
+
+def print_flare_list(data_frame):
+    """
+    Convert and print flares similar to the available .txt flare lists.
+
+    Parameters
+    ----------
+    data_frame : ``pandas.DataFrame``
+        DataFrame containing the flares to print.
+    """
+
+    for idx, row in data_frame.iterrows():
+        print(
+            "{id:9} {st} {pt} {et} {dur:5} {peak:6} {n:9} {e:>11} {x:5} {y:5} {r:6} {ar:4}  {flags}".format(
+                id=row['ID_NUMBER'],
+                st=row['START_TIME'].strftime('%e-%b-%Y %H:%M:%S'),
+                pt=row['PEAK_TIME'].strftime('%H:%M:%S'),
+                et=row['END_TIME'].strftime('%H:%M:%S'),
+                dur=int(round((row['END_TIME'] - row['START_TIME']).to_value("sec"))),
+                peak=int(row['PEAK_COUNTRATE']),
+                n=int(row['TOTAL_COUNTS']),
+                e=str(int(row['ENERGY_HI'][0])) + "-" + str(int(row['ENERGY_HI'][1])),
+                x=int(round(row['POSITION'][0])),
+                y=int(round(row['POSITION'][1])),
+                r=int(round((row['POSITION'][0] ** 2 + row['POSITION'][1] ** 2) ** 0.5)),
+                ar=row['ACTIVE_REGION'],
+                flags=" ".join(convert_flag_dict(row['FLAGS'])),
+            )
+        )
+
+
+def convert_flag_dict(flags_dict):
+    """
+    Convert flag dictionary into array of short abbreviations (as in available .txt lists).
+
+    Parameters
+    ----------
+    flags_dict : `dict`
+        Dictionary containing the flags where key is the flag id and value the value from the byte mask.
+
+    Returns
+    -------
+    `list`
+        out : `list` of 2-character strings, each representing a present flag (as in available .txt lists).
+
+    """
+    flags = []
+    for k in flags_dict:
+        # note: as of now the Q-flag is missing if the flag DATA_QUALITY is 0
+        if flags_dict[k] > 0 and flagid2flag[k] != '':
+            if k.startswith('ATTEN_') and int(k[-1:]) == flags_dict['ATT_STATE_AT_PEAK']:
+                flags.append(flagid2flag[k].upper())
+            else:
+                flags.append(flagid2flag[k].replace("n", str(flags_dict[k])))
+    flags.sort(key=str.lower)
+    return flags
