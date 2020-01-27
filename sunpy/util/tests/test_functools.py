89ccffc56cc65d013a5d0ae3380ba2b99f8d9d8d
Stuart Mumford <stuart@cadair.com>
2020-01-27 17:06:28 +0000

    Fix python 3.6 tests

--- sunpy/util/tests/test_functools.py
+++ sunpy/util/tests/test_functools.py
@@ -1,19 +1,22 @@
 """
 These tests are borrowed from CPython
 """
-import unittest
 import sys
-import decimal
+import unittest
 import collections
+
+import pytest
 
 from sunpy.util.functools import seconddispatch
 
 
 class TestSingleDispatch(unittest.TestCase):
     def test_simple_overloads(self):
+
         @seconddispatch
         def g(dummy, obj):
             return "base"
+
         def g_int(dummy, i):
             return "integer"
         g.register(int, g_int)
@@ -22,19 +25,26 @@ class TestSingleDispatch(unittest.TestCase):
         self.assertEqual(g(None, [1,2,3]), "base")
 
     def test_mro(self):
+
         @seconddispatch
         def g(obj):
             return "base"
+
         class A:
             pass
+
         class C(A):
             pass
+
         class B(A):
             pass
+
         class D(C, B):
             pass
+
         def g_A(dummy, a):
             return "A"
+
         def g_B(dummy, b):
             return "B"
         g.register(A, g_A)
@@ -45,9 +55,11 @@ class TestSingleDispatch(unittest.TestCase):
         self.assertEqual(g(None, D()), "B")
 
     def test_register_decorator(self):
+
         @seconddispatch
         def g(dummy, obj):
             return "base"
+
         @g.register(int)
         def g_int(dummy, i):
             return "int %s" % (i,)
@@ -67,13 +79,16 @@ class TestSingleDispatch(unittest.TestCase):
         if sys.flags.optimize < 2:
             self.assertEqual(g.__doc__, "Simple test")
 
+    @pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
     def test_annotations(self):
         @seconddispatch
         def i(dummy, arg):
             return "base"
+
         @i.register
         def _(dummy, arg: collections.abc.Mapping):
             return "mapping"
+
         @i.register
         def _(dummy, arg: "collections.abc.Sequence"):
             return "sequence"
