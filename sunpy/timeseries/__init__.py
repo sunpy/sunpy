92f808100ba4454d3cb9fea1e82ecf627a035def
Pankaj Mishra <pankajmishra1511@gmail.com>
2020-01-31 22:16:58 +0530

    fixed doc for timeseries v1.0+

--- sunpy/timeseries/__init__.py
+++ sunpy/timeseries/__init__.py
@@ -1,7 +1,13 @@
 from sunpy.timeseries.metadata import TimeSeriesMetaData
-from sunpy.timeseries.sources import *
 from sunpy.timeseries.timeseries_factory import TimeSeries
 from sunpy.timeseries.timeseriesbase import GenericTimeSeries
+from sunpy.timeseries.sources.eve import EVESpWxTimeSeries
+from sunpy.timeseries.sources.goes import XRSTimeSeries
+from sunpy.timeseries.sources.noaa import NOAAIndicesTimeSeries, NOAAPredictIndicesTimeSeries
+from sunpy.timeseries.sources.lyra import LYRATimeSeries
+from sunpy.timeseries.sources.norh import NoRHTimeSeries
+from sunpy.timeseries.sources.rhessi import RHESSISummaryTimeSeries
+from sunpy.timeseries.sources.fermi_gbm import GBMSummaryTimeSeries
 
 try:
     # Register pandas datetime converter with matplotlib
