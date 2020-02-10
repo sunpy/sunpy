0dd5f64185b2f564e9f3ba2d876e3312b7b56b06
Nabil Freij <nabil.freij@gmail.com>
2020-02-10 20:59:38 +0000

    woops

--- sunpy/database/tests/test_tables.py
+++ sunpy/database/tests/test_tables.py
@@ -247,9 +247,9 @@ def test_entry_from_qr_block_kev(qr_block_with_kev_unit):
     entry = DatabaseEntry._from_query_result_block(qr_block_with_kev_unit)
     assert entry.source == 'RHESSI'
     assert entry.provider == 'LSSP'
-    assert entry.fileid == '/hessidata/2011/09/19/hsi_20110919_233340_002.fits'
-    assert entry.observation_time_start == datetime(2011, 9, 19, 23, 33, 40)
-    assert entry.observation_time_end == datetime(2011, 9, 20, 1, 9, 20)
+    assert entry.fileid == '/hessidata/2011/09/20/hsi_20110920_010920_001.fits'
+    assert entry.observation_time_start == datetime(2011, 9, 20, 1, 9, 20)
+    assert entry.observation_time_end == datetime(2011, 9, 20, 2, 27, 40)
     assert entry.instrument == 'RHESSI'
     assert round(entry.wavemin, 3) == 0.413
     assert round(entry.wavemax, 7) == 0.0000729
@@ -553,7 +553,7 @@ def test_entry_from_query_results_with_none_wave_and_default_unit(
             observation_time_end=datetime(2012, 9, 14, 0, 0),
             instrument='VIRGO', size=512000.0, wavemin=None,
             wavemax=None)
-        ]
+    ]
 
     for e in entries:
         assert e in expected
