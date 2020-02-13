b789e3e4ea0c8c40d2b0a7ffb526cc3cc32e3f96
Stuart Mumford <stuart@cadair.com>
2020-02-13 16:47:19 +0000

    Split out VSO exceptions into their own file

---
+++ sunpy/net/vso/exceptions.py
@@ -0,0 +1,29 @@
+class NoData(Exception):
+
+    """ Risen for callbacks of VSOClient that are unable to supply
+    information for the request. """
+    pass
+
+
+class DownloadFailed(Exception):
+    pass
+
+
+class MissingInformation(Exception):
+    pass
+
+
+class UnknownMethod(Exception):
+    pass
+
+
+class MultipleChoices(Exception):
+    pass
+
+
+class UnknownVersion(Exception):
+    pass
+
+
+class UnknownStatus(Exception):
+    pass
