bf7e46cbb70b10ededae97befc128fad3995517a
Albert Y. Shih <ayshih@gmail.com>
2020-02-14 09:32:55 -0500

    Added a helper function for testing equality/closeness of longitudes

---
+++ sunpy/coordinates/tests/helpers.py
@@ -0,0 +1,12 @@
+from astropy.coordinates import Longitude
+from astropy.tests.helper import assert_quantity_allclose
+import astropy.units as u
+
+
+def assert_longitude_allclose(actual, desired, atol=None):
+    """
+    This works like :func:`~astropy.tests.helper.assert_quantity_allclose`, except it is intended
+    for longitude angles (i.e., angles that wrap every 360 degrees).
+    """
+    difference = Longitude(actual - desired, wrap_angle=180*u.deg)
+    assert_quantity_allclose(difference, 0*u.deg, atol=atol)
