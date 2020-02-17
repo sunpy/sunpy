6d2543daa4f322f6464741580cba811c1b5b6437
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-02-18 03:25:40 +0530

    added gong and farside clients

---
+++ sunpy/net/dataretriever/sources/gong.py
@@ -0,0 +1,244 @@
+import datetime
+import numpy as np
+
+from sunpy.net.dataretriever.client import GenericClient
+from sunpy.util.scraper import Scraper
+from sunpy.net import attrs as a
+
+__all__ = ['GONGClient', 'FARSIDEClient']
+
+
+class GONGClient(GenericClient):
+    """
+    Returns a list of URLS to GONG files corresponding to value of input timerange.
+    URL source: `ftp://gong2.nso.edu/QR/` and `http://gong2.nso.edu/HA/haf/`.
+    Parameters
+    ----------
+    timerange: sunpy.time.TimeRange
+        time range for which data is to be downloaded.
+        Example value - TimeRange('2015-12-30 00:00:00','2015-12-31 00:01:00')
+    Instrument: Arguments are any one from ['bigbear', 'cerrotelolo',
+                'learmonth', 'maunaloa', 'teide', 'udaipur','tucson']
+    Physobs: Two arguments or none 'intensity' and 'los_magnetic_field'
+    GONGClient expects at least one argument from Physobs or Instrument
+    Examples
+    --------
+    >>> from sunpy.net import Fido
+    >>> from sunpy.net import attrs as a
+    >>> res = Fido.search(a.Time('2016/6/4', '2016/6/4 02:00:00'), a.Instrument('udaipur'),
+    ...                   a.Physobs('LOS_MAGNETIC_FIELD'))  #doctest: +REMOTE_DATA
+    >>> print(res)  #doctest: +REMOTE_DATA
+    Results from 1 Provider:
+    <BLANKLINE>
+    2 Results from the GONGClient:
+         Start Time           End Time      Source Instrument Wavelength
+           str19               str19         str4     str7       str3
+    ------------------- ------------------- ------ ---------- ----------
+    2016-06-04 00:00:00 2016-06-04 02:00:00   GONG    udaipur        nan
+    2016-06-04 00:00:00 2016-06-04 02:00:00   GONG    udaipur        nan
+    <BLANKLINE>
+    <BLANKLINE>
+    """
+
+    def _get_url_for_timerange(self, timerange, **kwargs):
+        """
+        Return list of URLS corresponding to value of input timerange.
+        Parameters
+        ----------
+        timerange: `sunpy.time.TimeRange`
+            time range for which data is to be downloaded.
+        Returns
+        -------
+        urls : list
+            list of URLs corresponding to the requested time range
+        """
+        table_physobs = {'INTENSITY': 'i', 'LOS_MAGNETIC_FIELD': 'b'}
+        table_instruments = {'bigbear': 'bb', 'cerrotololo': 'ct', 'learmonth': 'le',
+                             'maunaloa': 'ml', 'teide': 'td', 'udaipur': 'ud', 'tucson': 'z'}
+
+        # Is PhysObs entered?
+        physobs_in = ('physobs' in kwargs.keys() and kwargs['physobs'] in table_physobs.keys())
+        # Is instrument entered?
+        instrument_in = ('instrument' in kwargs.keys()
+                         and kwargs['instrument'] in table_instruments.keys())
+        wavelength_in = ('wavelength' in kwargs.keys())
+
+        url_pattern_1 = ('ftp://gong2.nso.edu/QR/{id}qa/%Y%m/{ObsID}{id}qa%y%m%d/'
+                         '{ObsID}{id}qa%y%m%dt%H%M.fits.gz')
+        url_pattern_2 = 'http://gong2.nso.edu/HA/haf/%Y%m/%Y%m%d/%Y%m%d%H%M%S{ObsID}h.fits.fz'
+
+        pattern_table = {6562: url_pattern_1, 6563: url_pattern_1, 6768: url_pattern_2}
+
+        result = list()  # final list of urls
+        patterns = list()
+        if not physobs_in:
+            patterns = [url_pattern_1, url_pattern_2]
+        else:
+            if kwargs['physobs'].lower() in ['intensity', 'los_magnetic_field']:
+                patterns.append(url_pattern_1)
+            else:
+                # Differentiate on basis of wavelength
+
+                # The isclose function in numpy can only check two arrays if they
+                # are close to each other within some precision. Standalone values such
+                # as int, doubles etc can't be checked that way. In order to do that, we put the
+                # two indiviual values in two seperate arrays da and db and then apply
+                # isclose on both those arrays.
+                if not wavelength_in:
+                    patterns.extend([url_pattern_1, url_pattern_2])
+                else:
+                    try:
+                        wave = kwargs['wavelength']
+                        wave_float = (
+                            wave.min if isinstance(
+                                wave, a.Wavelength) else wave.wavemin).value
+                        da = list()
+                        da.append(wave_float)
+                        for wave_nums in pattern_table.keys():
+                            db = list()
+                            db.append(wave_nums)
+                            if np.isclose(da, db, 1e-10, 1e-10):
+                                wave = wave_nums
+                                break
+                        patterns.append(pattern_table[wave])
+                    except BaseException:
+                        raise NameError("Enter correct wavelength range and units")
+        # All valid patterns to be downloaded are in the patterns list.
+        instruments_to = list()  # The instruments from which user wants to download
+        if not instrument_in:
+            instruments_to.extend(table_instruments.values())
+        else:
+            instruments_to.append(table_instruments[kwargs['instrument']])
+
+        for pattern_ in patterns:
+            urls = list()
+            if (pattern_ == url_pattern_1):
+                if not physobs_in:
+                    for instr in instruments_to:
+                        arr = Scraper(pattern_, id='i', ObsID=instr).filelist(timerange)
+                        urls.extend(arr)
+                        arr = Scraper(pattern_, id='b', ObsID=instr).filelist(timerange)
+                        urls.extend(arr)
+                else:
+                    for instr in instruments_to:
+                        arr = Scraper(pattern_,
+                                      id=table_physobs[kwargs['physobs']],
+                                      ObsID=instr).filelist(timerange)
+                        urls.extend(arr)
+                result.extend(urls)
+            elif pattern_ == url_pattern_2:
+                urls = [url_pattern_2.format(ObsID=ids[0].upper()) for ids in instruments_to]
+                arr = [Scraper(pattern__).filelist(timerange) for pattern__ in urls]
+                [result.extend(url) for url in arr if len(url) > 0]
+
+        if not timerange:
+            return []
+        return result
+
+    def _makeimap(self):
+        """
+        Helper Function: used to hold information about source.
+        """
+        self.map_['source'] = 'GONG'
+
+    @classmethod
+    def _can_handle_query(cls, *query):
+        """
+        Answers whether client can service the query.
+        Parameters
+        ----------
+        query : list of query objects
+        Returns
+        -------
+        boolean: answer as to whether client can service the query
+        """
+        physobs = ['INTENSITY', 'LOS_MAGNETIC_FIELD']  # from VSO
+        instruments = ['bigbear', 'cerrotololo', 'learmonth', 'maunaloa',
+                       'teide', 'udaipur', 'tucson']  # for Magnetogram and intensity
+        chk_instr, chk_physobs = 0, 0
+        for x in query:
+            if x.__class__.__name__ == 'Instrument' and x.value.lower() in instruments:
+                chk_instr += 1
+            if x.__class__.__name__ == 'Physobs' and x.value in physobs:
+                chk_physobs += 1
+        if chk_instr == 1 or chk_physobs == 1:
+            return True
+        return False
+
+
+class FARSIDEClient(GenericClient):
+    """
+    Returns a list of URLS to FARSIDE files corresponding to value of input timerange.
+    URL source: `http://farside.nso.edu/oQR/fqo/`.
+    Parameters
+    ----------
+    timerange: sunpy.time.TimeRange
+        time range for which data is to be downloaded.
+        Example value - TimeRange('2015-12-30 00:00:00','2015-12-31 00:01:00')
+    Instrument: Fixed argument = 'farside'
+    Returns
+    -------
+    urls: list
+    list of urls corresponding to requested time range.
+    Examples
+    --------
+    >>> from sunpy.net import Fido
+    >>> from sunpy.net import attrs as a
+    >>> results = Fido.search(a.Time('2015/4/2','2015/4/4'),
+    ...                       a.Instrument('farside')) #doctest: +REMOTE_DATA
+    >>> print(results)  #doctest: +REMOTE_DATA
+    Results from 1 Provider:
+    <BLANKLINE>
+    5 Results from the FARSIDEClient:
+         Start Time           End Time      Source Instrument Wavelength
+           str19               str19         str4     str7       str3
+    ------------------- ------------------- ------ ---------- ----------
+    2015-04-02 00:00:00 2015-04-04 00:00:00   GONG    farside        nan
+    2015-04-02 00:00:00 2015-04-04 00:00:00   GONG    farside        nan
+    2015-04-02 00:00:00 2015-04-04 00:00:00   GONG    farside        nan
+    2015-04-02 00:00:00 2015-04-04 00:00:00   GONG    farside        nan
+    2015-04-02 00:00:00 2015-04-04 00:00:00   GONG    farside        nan
+    <BLANKLINE>
+    <BLANKLINE>
+    >>> response = Fido.fetch(results)  #doctest: +SKIP
+    """
+
+    def _get_url_for_timerange(self, timerange, **kwargs):
+        """
+        returns list of urls corresponding to given TimeRange.
+        """
+        START_DATE = datetime.datetime(2006, 5, 13, 12, 0, 0)
+        if timerange.start < START_DATE:
+            msg = 'Earliest date for which FARSIDE data is available is {:%Y-%m-%d}'
+            raise ValueError(msg.format(START_DATE))
+        if not timerange:
+            return []
+        url_pattern = 'http://farside.nso.edu/oQR/fqo/%Y%m/mrfqo%y%m%d/mrfqo%y%m%dt%H%M.fits'
+        result = Scraper(url_pattern).filelist(timerange)
+
+        return result
+
+    def _makeimap(self):
+        """
+        Helper Function:used to hold information about source.
+        """
+        self.map_['source'] = 'GONG'
+        self.map_['instrument'] = 'farside'
+
+    @classmethod
+    def _can_handle_query(cls, *query):
+        """
+        Answers whether client can service the query.
+        Parameters
+        ----------
+        query : list of query objects
+        Returns
+        -------
+        boolean: answer as to whether client can service the query
+        """
+        chkattr = ['Time', 'Instrument']
+        chklist = [x.__class__.__name__ in chkattr for x in query]
+        for x in query:
+            if x.__class__.__name__ == 'Instrument' and x.value.lower() == 'farside':
+                return all(chklist)
+        return False
