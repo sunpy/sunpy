6d763a29c44ffbfbf8788d42905ccf5324ecf466
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-03-11 13:20:02 +0530

    merged table & date_table

--- sunpy/net/dataretriever/sources/kanzelhohe.py
+++ sunpy/net/dataretriever/sources/kanzelhohe.py
@@ -17,18 +17,20 @@ class KanzelhoheClient(GenericClient):
     The earliest data for H-alpha 2k - 20-Jul-2000
                           Ca-II k - 31-Jul-2010
                           Continuum - 7-Jan-2011
+
     Parameters
     ----------
-    timerange: sunpy.time.TimeRange
+    timerange: `~sunpy.time.TimeRange`
         time range for which data is to be downloaded.
         Example value - TimeRange('2015-12-30 00:00:00','2015-12-31 00:01:00')
     Instrument: Fixed argument = 'kanzelhohe'
     Wavelength: Fixed argument = astropy.units.quantity.Quantity
                 The physical value of wavelength will belong to any of [5460, 6563, 32768]
                 and units will be Angstroms.
+
     Returns
     -------
-    urls: list
+    urls: `list`
     list of urls corresponding to requested time range.
     Examples
     --------
@@ -52,55 +54,47 @@ class KanzelhoheClient(GenericClient):
     def _get_url_for_timerange(self, timerange, **kwargs):
         """
         Return list of URLS corresponding to value of input timerange.
+
         Parameters
         ----------
-        timerange: `sunpy.time.TimeRange`
+        timerange: `~sunpy.time.TimeRange`
             time range for which data is to be downloaded.
+
         Returns
         -------
-        urls : list
+        urls : `list`
             list of URLs corresponding to the requested time range
         """
         wave = kwargs['wavelength']
         wave_float = (wave.min if isinstance(wave, a.Wavelength) else wave.min()).value
-        table = {6563: ['halpha2k/recent', 'halph_fr'],
-                 32768: ['caiia', 'caiik_fi'],
-                 5460: ['phokada', 'bband_fi']}
+        table = {6563: {'datatype': ['halpha2k/recent', 'halph_fr'],
+                        'start_time': datetime.datetime(2000, 7, 20, 7, 45, 46)},
+                 32768: {'datatype': ['caiia', 'caiik_fi'],
+                         'start_time': datetime.datetime(2000, 7, 20, 7, 45, 46)},
+                 5460: {'datatype': ['phokada', 'bband_fi'],
+                        'start_time': datetime.datetime(2000, 7, 20, 7, 45, 46)}}
         # Checking if value is close enough to a wavelength value.
         # Converting from one unit to other introduces precision errors.
-        try:
-            # The isclose function in numpy can only check two arrays if they
-            # are close to each other within some precision. Standalone values such
-            # as int, doubles etc can't be checked that way. In order to do that, we put the
-            # two indiviual values in two seperate arrays da and db and then apply
-            # isclose on both those arrays.
-            da = list()
-            da.append(wave_float)
-            for wave_nums in table.keys():
-                db = list()
-                db.append(wave_nums)
-                if np.isclose(da, db, 1e-10, 1e-10):
-                    wave = wave_nums
-
-            date_table = {6563: datetime.datetime(2000, 7, 20, 7, 45, 46),
-                          5460: datetime.datetime(2011, 1, 7, 10, 7, 33),
-                          32768: datetime.datetime(2010, 7, 31, 8, 10, 59)}
-            START_DATE = date_table[wave]
-            if timerange.start < START_DATE:
-                msg = 'Earliest date for which Kanzelhohe data is available is {:%Y-%m-%d}'
-                raise ValueError(msg.format(START_DATE))
-            prefix = "http://cesar.kso.ac.at/{datatype}/%Y/"
-            suffix = ""
-            if wave != 6563:
-                suffix = "%Y%m%d/processed/"
-            url_pattern = prefix + suffix + "kanz_{datatype1}_%Y%m%d_%H%M%S.fts.gz"
-            crawler = Scraper(url_pattern, datatype=table[wave][0], datatype1=table[wave][1])
-            if not timerange:
-                return []
-            result = crawler.filelist(timerange)
-            return result
-        except BaseException:
-            raise ValueError("Enter wavelength with proper values and units")
+        wavelengths = table.keys()
+        wave_list = list(filter(lambda x: np.isclose(x, wave_float), wavelengths))
+        if(len(wave_list) == 0):
+            raise ValueError("no value is enough closer to allowed wavelengths")
+        wave = wave_list[0]
+        start_date = table[wave]['start_time']
+        if timerange.start < start_date:
+            msg = 'Earliest date for which Kanzelhohe data is available is {:%Y-%m-%d}'
+            raise ValueError(msg.format(start_date))
+        prefix = "http://cesar.kso.ac.at/{datatype}/%Y/"
+        suffix = ""
+        if wave != 6563:
+            suffix = "%Y%m%d/processed/"
+        url_pattern = prefix + suffix + "kanz_{datatype1}_%Y%m%d_%H%M%S.fts.gz"
+        dtypes = table[wave]['datatype']
+        scraper = Scraper(url_pattern, datatype=dtypes[0], datatype1=dtypes[1])
+        if not timerange:
+            return []
+        result = scraper.filelist(timerange)
+        return result
 
     def _makeimap(self):
         """
@@ -115,9 +109,11 @@ class KanzelhoheClient(GenericClient):
     def _can_handle_query(cls, *query):
         """
         Answers whether client can service the query.
+
         Parameters
         ----------
         query : list of query objects
+
         Returns
         -------
         boolean: answer as to whether client can service the query
