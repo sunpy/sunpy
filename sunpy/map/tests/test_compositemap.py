90417680d8869f0e81219aa6a882ef58c8b3b4e1
Nabil Freij <nabil.freij@gmail.com>
2020-03-28 16:13:43 +0000

    changes to fix tests

--- sunpy/map/tests/test_compositemap.py
+++ sunpy/map/tests/test_compositemap.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
 """
 Test Composite Map
 """
 import os
 import pytest
 
-import numpy as np
 
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-import matplotlib.pyplot as plt
-
-import sunpy
 import sunpy.map
-import sunpy.coordinates
 import sunpy.data.test
 from sunpy.tests.helpers import figure_test
-from sunpy.map import compositemap
 
 testpath = sunpy.data.test.rootdir
 
@@ -27,13 +18,13 @@ def aia171_test_map():
 
 
 @pytest.fixture
-def heliographic_test_map():
-    return sunpy.map.Map(os.path.join(testpath, 'heliographic_phase_map.fits.gz'))
+def hmi_test_map():
+    return sunpy.map.Map(os.path.join(testpath, 'resampled_hmi.fits'))
 
 
 @pytest.fixture
-def composite_test_map():
-    return sunpy.map.Map(aia171_test_map, heliographic_test_map, composite=True)
+def composite_test_map(aia171_test_map, hmi_test_map):
+    return sunpy.map.Map(aia171_test_map, hmi_test_map, composite=True)
 
 
 @figure_test
