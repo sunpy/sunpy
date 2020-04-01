1d04c072676475fbeebabb4066c1e9900e281887
David Stansby <dstansby@gmail.com>
2020-04-01 15:05:42 +0100

    Fix map source test warnings

--- sunpy/map/sources/tests/test_euvi_source.py
+++ sunpy/map/sources/tests/test_euvi_source.py
@@ -6,19 +6,25 @@ This particular test file pertains to EUVIMap.
 import os
 import glob
 
+import pytest
+
 from sunpy.map.sources.stereo import EUVIMap
 from sunpy.map import Map
 from sunpy.coordinates import sun
 import sunpy.data.test
+from sunpy.util.exceptions import SunpyUserWarning
+
 
 path = sunpy.data.test.rootdir
 fitspath = glob.glob(os.path.join(path, "euvi_20090615_000900_n4euA_s.fts"))
 euvi = Map(fitspath)
 
+
 # EUVI Tests
 def test_fitstoEIT():
     """Tests the creation of EUVIMap using FITS."""
     assert isinstance(euvi, EUVIMap)
+
 
 def test_is_datasource_for():
     """Test the is_datasource_for method of EUVIMap.
@@ -26,23 +32,28 @@ def test_is_datasource_for():
     can be a MetaDict object."""
     assert euvi.is_datasource_for(euvi.data, euvi.meta)
 
+
 def test_measurement():
     """Tests the measurement property of the EUVIMap object."""
     assert euvi.measurement.value == 171
+
 
 def test_observatory():
     """Tests the observatory property of the EUVIMap object."""
     assert euvi.observatory == "STEREO A"
 
+
 def test_rsun_obs():
     """Tests the rsun_obs property"""
     assert euvi.rsun_obs.value == euvi.meta['rsun']
+
 
 def test_rsun_missing():
     """Tests output if 'rsun' is missing"""
     euvi_no_rsun = Map(fitspath)
     euvi_no_rsun.meta['rsun'] = None
-    assert euvi_no_rsun.rsun_obs.value == sun.angular_radius(euvi.date).to('arcsec').value
+    with pytest.warns(SunpyUserWarning, match='Missing metadata for solar radius'):
+        assert euvi_no_rsun.rsun_obs.value == sun.angular_radius(euvi.date).to('arcsec').value
 
 
 def test_norm_clip():
--- sunpy/map/sources/tests/test_xrt_source.py
+++ sunpy/map/sources/tests/test_xrt_source.py
@@ -7,44 +7,50 @@ import os
 import glob
 import numpy as np
 from matplotlib import colors
+import pytest
 
 import astropy.units as u
+from astropy.utils.exceptions import AstropyUserWarning
 
 from sunpy.map.sources.hinode import XRTMap
 from sunpy.map import Map
 import sunpy.data.test
 
-path = sunpy.data.test.rootdir
-fitspath = glob.glob(os.path.join(path, "HinodeXRT.fits"))
-xrt = Map(fitspath)
+
+@pytest.fixture
+def xrt():
+    path = sunpy.data.test.rootdir
+    fitspath = glob.glob(os.path.join(path, "HinodeXRT.fits"))
+    with pytest.warns(AstropyUserWarning, match='File may have been truncated'):
+        return Map(fitspath)
 
 
 # XRT Tests
-def test_fitstoXRT():
+def test_fitstoXRT(xrt):
     """Tests the creation of XRTMap using FITS."""
     assert isinstance(xrt, XRTMap)
 
 
-def test_is_datasource_for():
+def test_is_datasource_for(xrt):
     """Test the is_datasource_for method of XRTMap.
     Note that header data to be provided as an argument
     can be a MetaDict object."""
     assert xrt.is_datasource_for(xrt.data, xrt.meta)
 
 
-def test_observatory():
+def test_observatory(xrt):
     """Tests the observatory property of the XRTMap object."""
     assert xrt.observatory == "Hinode"
 
 
-def test_measurement():
+def test_measurement(xrt):
     """Tests the measurement property of the XRTMap object."""
     measurement = xrt.filter_wheel1_measurements[5].replace("_", " ")
     measurement += '-' + xrt.filter_wheel2_measurements[1].replace("_", " ")
     assert xrt.measurement == measurement
 
 
-def test_wheel_measurements():
+def test_wheel_measurements(xrt):
     """Tests the filter_wheel_measurements objects present
     in the XRTMap object."""
     assert (xrt.filter_wheel1_measurements ==
