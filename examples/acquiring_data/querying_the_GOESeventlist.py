6cf7fb6757706b42cded37a94a8330638e3d46f6
hayesla <hayesla@tcd.ie>
2020-04-01 23:20:37 -0400

    adding suggested edits from review

--- examples/acquiring_data/querying_the_GOESeventlist.py
+++ examples/acquiring_data/querying_the_GOESeventlist.py
@@ -22,13 +22,13 @@ tend = '2013/10/29'
 ###################################################################
 # We then use the client to query the HEK for a list of events
 # that were detected by the GOES X-ray Sensor (XRS) instrument between
-# tstart and tend.
+# `tstart` and `tend`.
 result = client.search(hek.attrs.Time(tstart, tend),
                        hek.attrs.EventType(event_type),
                        hek.attrs.OBS.Observatory == 'GOES')
 
 ###################################################################
-# The result is returned as a `Table <sunpy.net.hek.hek.HEKTable>`.
+# The result is returned as a `~sunpy.net.hek.hek.HEKTable`.
 # We can print the number of flares and inspect the result information.
 # We can also print the key-values that correspond to the HEK parameters
 # returned in result.
@@ -41,24 +41,23 @@ print(result.keys())
 # For example, let's search only for large solar flares with
 # a GOES class > M1.0. This can be achieved by using the FL.GOESCls
 # attribute of the HEK client:
-goes_class_filter = 'M1.0'
 
 result_m1 = client.search(hek.attrs.Time(tstart, tend),
                           hek.attrs.EventType(event_type),
-                          hek.attrs.FL.GOESCls > goes_class_filter,
+                          hek.attrs.FL.GOESCls > 'M1.0',
                           hek.attrs.OBS.Observatory == 'GOES')
 
 print(result_m1)
 
 ###################################################################
-# The results returned to the `Table <sunpy.net.hek.hek.HEKTable>`
-# contain alot of information and we may only want to keep some main
-# results such as start time, end time, peak time, GOES-class, active
-# region number. This can be done as so:
+# The results returned to the `~sunpy.net.hek.hek.HEKTable`
+# contain a lot of information and we may only want to keep some main
+# results such as start time, end time, peak time, GOES-class, and
+# active region number. This can be done as so:
 new_table = result_m1[['event_starttime', 'event_peaktime',
                        'event_endtime', 'fl_goescls', 'ar_noaanum', ]]
 
 ###################################################################
-# These results can then be saved to a csv file, or whatever file
-# format that `astropy.table.Table` supports
+# These results can then be saved to a CSV file, or any other file
+# format that `~astropy.table.Table` supports
 new_table.write('october_M1_flares.csv', format='csv')
