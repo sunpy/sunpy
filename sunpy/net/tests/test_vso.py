a37f670c746c0307266a56c576ca1e9d2a543e2f
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-04-06 02:37:36 +0530

    added mock test for iter_sort_response

--- sunpy/net/tests/test_vso.py
+++ sunpy/net/tests/test_vso.py
@@ -45,7 +45,8 @@ class MockQRResponse:
         self.provideritem = list()
 
         if records is not None:
-            self.provideritem = [MockObject(record=MockObject(recorditem=[ri])) for ri in records]
+            self.provideritem = [MockObject(record=MockObject(recorditem=[MockObject(
+                                 time=MockObject(start=ti)) for ti in records]))]
 
         if errors is not None:
             self.provideritem.extend([MockObject(error=err) for err in errors])
@@ -53,7 +54,10 @@ class MockQRResponse:
 
 @pytest.fixture
 def mock_response():
-    return MockQRResponse(records=[1, 2], errors=['FAILED'])
+    # defining unsorted queryresult with only time.start to mock test `iter_sort_response()`
+    # note: some of the attributes were remove to avoid clutter.
+    times = [20050101001332, 20050101001313, 20050101001210]
+    return MockQRResponse(records=times, errors=['FAILED'])
 
 
 @pytest.fixture
@@ -325,8 +329,9 @@ def test__parse_date(input, expected):
     assert vso.vso._parse_date(input) == expected
 
 
-def test_iter_records(mock_response):
-    assert list(vso.vso.iter_records(mock_response)) == [1, 2]
+def test_iter_sort_response(mock_response):
+    times = [i.time.start for i in vso.vso.iter_sort_response(mock_response)]
+    assert times == sorted(times)
 
 
 def test_iter_errors(mock_response):
@@ -493,7 +498,7 @@ def test_incorrect_content_disposition(client):
     files = client.fetch(results[0:1])
 
     assert len(files) == 1
-    assert  files[0].endswith("mdi_vw_v_9466622_9466622.tar")
+    assert files[0].endswith("mdi_vw_v_9466622_9466622.tar")
     assert "Content" not in files[0]
 
 
