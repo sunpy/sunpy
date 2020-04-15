49a37e488210ad72717625ae46155bc3e08ea592
Raahul Singh <raahul.s18@iiits.in>
2020-04-16 02:22:03 +0530

    Adds deprecation decorator.

--- sunpy/util/decorators.py
+++ sunpy/util/decorators.py
@@ -6,6 +6,8 @@ import inspect
 import textwrap
 import warnings
 import functools
+from inspect import signature, isclass, Parameter
+from functools import wraps
 
 from sunpy.util.exceptions import SunpyDeprecationWarning, SunpyPendingDeprecationWarning
 
@@ -264,3 +266,42 @@ class add_common_docstring:
         if self.kwargs:
             func.__doc__ = func.__doc__.format(**self.kwargs)
         return func
+
+def deprecate_positional_args(f):
+    """Decorator for methods that issues warnings for positional arguments
+    Using the keyword-only argument syntax in pep 3102, arguments after the
+    * will issue a warning when passed as a positional argument.
+    Parameters
+    ----------
+    f : function
+        function to check arguments on
+
+    References
+    ----------
+    This has been adopted from Scikit-Learn
+    """
+    sig = signature(f)
+    kwonly_args = []
+    all_args = []
+
+    for name, param in sig.parameters.items():
+        if param.kind == Parameter.POSITIONAL_OR_KEYWORD:
+            all_args.append(name)
+        elif param.kind == Parameter.KEYWORD_ONLY:
+            kwonly_args.append(name)
+
+    @wraps(f)
+    def inner_f(*args, **kwargs):
+        extra_args = len(args) - len(all_args)
+        if extra_args > 0:
+            # ignore first 'self' argument for instance methods
+            args_msg = ['{}={}'.format(name, arg)
+                        for name, arg in zip(kwonly_args[:extra_args],
+                                             args[-extra_args:])]
+            warnings.warn("Pass {} as keyword args. From version 2.0 "
+                          "passing these as positional arguments will "
+                          "result in an error".format(", ".join(args_msg)),
+                          FutureWarning)
+        kwargs.update({k: arg for k, arg in zip(sig.parameters, args)})
+        return f(**kwargs)
+    return inner_f
