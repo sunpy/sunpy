015641ffc96d82ec5db808bf8873a247a02b0572
Stuart Mumford <stuart@cadair.com>
2020-05-07 18:23:17 +0100

    Implement a custom user agent for all parfive based downloads

---
+++ sunpy/util/parfive_helpers.py
@@ -0,0 +1,27 @@
+import sys
+from functools import wraps
+
+import aiohttp
+import parfive
+from parfive import Results
+
+import sunpy
+
+__all__ = ['Downloader', 'Results']
+
+
+# Overload the parfive downloader class to set the User-Agent string
+class Downloader(parfive.Downloader):
+
+    @wraps(parfive.Downloader.__init__)
+    def __init__(self, *args, **kwargs):
+        headers = kwargs.pop("headers", None)
+
+        if headers is None or 'User-Agent' not in headers:
+            headers = {'User-Agent':
+                       f"sunpy/{sunpy.__version__} parfive/{parfive.__version__} "
+                       f"aiohttp/{aiohttp.__version__} python/{sys.version[:5]}"}
+
+        kwargs["headers"] = headers
+
+        super().__init__(*args, **kwargs)
