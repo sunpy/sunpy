5c636764f13e73e6cf017ed3197a3723091c4851
Nabil Freij <nabil.freij@gmail.com>
2020-05-08 15:30:02 +0100

    Merge pull request #4118 from dstansby/header-doc
    Improve fits header helper doc

--- sunpy/map/header_helper.py
+++ sunpy/map/header_helper.py
@@ -49,10 +49,11 @@ def make_fitswcs_header(data, coordinate,
 
     Parameters
     ----------
-    data : `~numpy.ndarray`
-        Array data of Map for which a header is required.
-    coordinates : `~astropy.coordinates.SkyCoord` or `~astropy.coordinates.BaseFrame`
-        Coordinate object to get meta information for map header.
+    data : `~numpy.ndarray` or `tuple`
+        Array data of Map for which a header is required, or the shape of the
+        data array (in numpy order, i.e. ``(y_size, x_size)``).
+    coordinate : `~astropy.coordinates.SkyCoord` or `~astropy.coordinates.BaseFrame`
+        The coordinate of the reference pixel.
     reference_pixel :`~astropy.units.Quantity` of size 2, optional
         Reference pixel along each axis. These are expected to be Cartestian ordered, i.e
         the first index is the x axis, second index is the y axis. Defaults to
