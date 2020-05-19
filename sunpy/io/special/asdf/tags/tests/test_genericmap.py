7364ee11da28a400f6eb7f77f3e8729f9c5fdcb1
Stuart Mumford <stuart@cadair.com>
2020-05-19 11:06:05 +0100

    More cleanup

--- sunpy/io/special/asdf/tags/tests/test_genericmap.py
+++ sunpy/io/special/asdf/tags/tests/test_genericmap.py
@@ -1,6 +1,7 @@
 """
 isort:skip_file.
 """
+# flake8: noqa: E402
 import platform
 from distutils.version import LooseVersion
 
@@ -9,12 +10,12 @@ import pytest
 
 import astropy.units as u
 asdf = pytest.importorskip('asdf', '2.0')
-from asdf.tests.helpers import assert_roundtrip_tree  # NOQA
+from asdf.tests.helpers import assert_roundtrip_tree
 
-import sunpy.map  # NOQA
-from sunpy.data.test import get_test_filepath  # NOQA
-from sunpy.tests.helpers import asdf_entry_points  # NOQA
-from sunpy.io.special.asdf.extension import SunpyExtension  # NOQA
+import sunpy.map
+from sunpy.data.test import get_test_filepath
+from sunpy.tests.helpers import asdf_entry_points
+from sunpy.io.special.asdf.extension import SunpyExtension
 
 
 @pytest.fixture
