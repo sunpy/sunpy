6e4e084c870c6675ec3a48adc6231a1d393122e3
Will Barnes <will.t.barnes@gmail.com>
2020-05-29 23:50:20 -0400

    Explicitly import ESPTimeSeries (#4217)
    * explicitly import ESPTimeSeries
    
    * fix isort

--- sunpy/timeseries/sources/__init__.py
+++ sunpy/timeseries/sources/__init__.py
@@ -6,7 +6,7 @@ Each mission should have its own file with one or more classes defined.
 Typically, these classes will be subclasses of the
 `sunpy.timeseries.TimeSeries`.
 """
-from sunpy.timeseries.sources.eve import EVESpWxTimeSeries
+from sunpy.timeseries.sources.eve import ESPTimeSeries, EVESpWxTimeSeries
 from sunpy.timeseries.sources.fermi_gbm import GBMSummaryTimeSeries
 from sunpy.timeseries.sources.goes import XRSTimeSeries
 from sunpy.timeseries.sources.lyra import LYRATimeSeries
