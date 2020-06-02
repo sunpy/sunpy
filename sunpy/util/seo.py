d61fb9cc29f117f5550432c3254d58c38206a4c4
Raahul Singh <raahul.s18@iiits.in>
2020-06-02 20:15:07 +0530

    Adds Sunspotter dataset object

---
+++ sunpy/util/seo.py
@@ -0,0 +1,86 @@
+"""
+This module implements functions that can be used to match rows across various Data Catalogues.
+All these functions will be coalesced into the Search Events Object.
+"""
+
+import pandas as pd
+import numpy as np
+from astropy.io.votable import parse
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+import warnings
+from sunpy.util import SunpyUserWarning
+
+class Sunspotter:
+    """
+    For the Sunspotter Dataset.
+    """
+    def __init__(self, *, rankings=None, classifications=None,
+                 properties=None, timefits=None, delimiter=';'):
+        """
+        Parameters
+        ----------
+        rankings: `str`
+            file path to the `rankings` CSV file.
+        classifications: `str`
+            file path to the `classifications` CSV file.
+        properties: `str`
+            file path to the `lookup_properties` CSV file.
+        timefits: `str`
+            file path to the `lookup_timefits` CSV file.
+        delimiter : `str`
+            Delimiter while reading the CSV files.
+            Default delimiter is `;`
+        """
+        self.rankings = rankings
+        self.classifications = classifications
+        self.properties = properties
+        self.timefits = timefits
+
+        self.delimiter = delimiter
+
+        self.get_dataframes()
+
+    def get_dataframes(self):
+        """
+        Get dataframes from the sunspotter CSV files.
+        """
+        self.rankings = pd.read_csv(self.rankings, delimiter=self.delimiter)
+        self.classifications = pd.read_csv(self.classifications, delimiter=self.delimiter)
+        self.properties = pd.read_csv(self.properties, delimiter=self.delimiter)
+        self.timefits = pd.read_csv(self.timefits, delimiter=self.delimiter)
+
+    def get_timefits_id(self, obsdate):
+        """
+        Identifies the id for the observations for a given observation date.
+        This id can be used to identify the particular observation across 
+        different sunspotter datasets.
+
+        Parameters
+        ----------
+        obsdate: `str` of format `yyyy-mm-dd hh-mm-ss`
+            The date and time of a particular observation.
+
+        Returns
+        -------
+        idx: `numpy.int64`
+            The id for the observations for a given observation date.
+        """
+        return self.timesfits[self.timesfits.obs_date == obsdate].get(key='#id').iloc[0]
+
+    def get_properties(self, idx):
+        """
+        Gets the list of properties of all ARs corresponding to a particular id.
+
+        Parameters
+        ----------
+        idx: `numpy.int64`
+            The id corresponding to a particular observation.
+
+        Returns
+        -------
+        properties_list: `pd.DataFrame`
+            DataFrame corresponding to all the observations for a given id.
+        """
+        return self.properties[self.properties.id_filename == idx]
+
+
