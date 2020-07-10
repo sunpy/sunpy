0a2cb2c405dcc35561b43f7b6a2aa332f9fe232f
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-07-10 15:47:57 +0530

    HECClient subclass of BaseClient

---
+++ sunpy/net/helio/attrs.py
@@ -0,0 +1,13 @@
+from sunpy.net.attr import SimpleAttr
+
+__all__ = ['max_records']
+
+
+class max_records(SimpleAttr):
+    """
+    The maximum number of desired records.
+    """
+class Catalogue(SimpleAttr):
+    """
+    The catalogue to query from.
+    """
