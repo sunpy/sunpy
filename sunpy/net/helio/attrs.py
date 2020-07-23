a2f1019743f70c0929dc38782cd70f62da0cf549
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-07-24 03:11:02 +0530

    warning for bytestring tablename

--- sunpy/net/helio/attrs.py
+++ sunpy/net/helio/attrs.py
@@ -1,19 +1,12 @@
 from sunpy.net.attr import SimpleAttr
 
-__all__ = ['MaxRecords', 'Catalogue', 'TableName']
+__all__ = ['MaxRecords', 'TableName']
 
 
 class MaxRecords(SimpleAttr):
     """
     The maximum number of desired records.
     """
-
-
-class Catalogue(SimpleAttr):
-    """
-    The catalogue to query from.
-    """
-
 
 class TableName(SimpleAttr):
     """
