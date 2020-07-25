0da321abf559ef601ce6805db65854b9afefaf99
Nabil Freij <nabil.freij@gmail.com>
2020-07-25 08:52:43 +0100

    Update sunpy/physics/tests/test_differential_rotation.py

--- sunpy/physics/tests/test_differential_rotation.py
+++ sunpy/physics/tests/test_differential_rotation.py
@@ -265,7 +265,7 @@ def test_differential_rotate_time_off_disk(all_off_disk_map):
     # Test a map that is entirely off the disk of the Sun
     # Should report an error
     with pytest.raises(ValueError):
-        differential_rotate(all_off_disk_map)
+        differential_rotate(all_off_disk_map, time=new_time)
 
 
 # Tests of the helper functions
