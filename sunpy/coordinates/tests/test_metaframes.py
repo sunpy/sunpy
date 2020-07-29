8091d4adc3e4cd470f1ba25b1b498b5943a46e89
Nabil Freij <nabil.freij@gmail.com>
2020-07-29 09:05:22 +0100

    pin pytest

--- sunpy/coordinates/tests/test_metaframes.py
+++ sunpy/coordinates/tests/test_metaframes.py
@@ -184,7 +184,7 @@ def test_alternate_rotation_model():
                                    f.HeliocentricInertial])
 @given(lon=longitudes(), lat=latitudes(),
        obstime=times(), rotated_time1=times(), rotated_time2=times())
-@settings(deadline=2000)
+@settings(deadline=None)
 def test_rotatedsun_transforms(frame, lon, lat, obstime, rotated_time1, rotated_time2):
     # Tests the transformations (to, from, and loopback) for consistency with `diff_rot` output
 
