7f21129c24564bf5f0b85dc64ef9c0c13e9e0329
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-08-02 03:18:36 +0530

    updated examples for goes HEK

--- examples/time_series/goes_hek_m25.py
+++ examples/time_series/goes_hek_m25.py
@@ -9,35 +9,34 @@ import matplotlib.pyplot as plt
 
 from sunpy.net import Fido
 from sunpy.net import attrs as a
-from sunpy.net import hek
-from sunpy.time import TimeRange, parse_time
+from sunpy.time import parse_time
 from sunpy.timeseries import TimeSeries
 
 ###############################################################################
-# Let's first grab GOES XRS data for a particular time of interest
-tr = TimeRange(['2011-06-07 04:00', '2011-06-07 12:00'])
-results = Fido.search(a.Time(tr), a.Instrument.xrs)
+# Let's grab GOES XRS data for a particular time of interest and the HEK flare
+# data for this time from the NOAA Space Weather Prediction Center (SWPC)
+
+tr = a.Time('2011-06-07 04:00', '2011-06-07 12:00')
+results = Fido.search(tr, a.Instrument.xrs | a.hek.FL & (a.hek.FRM.Name == 'SWPC'))
 
 ###############################################################################
-# Then download the data and load it into a TimeSeries
+# Then download the XRS data and load it into a TimeSeries
 files = Fido.fetch(results)
 goes = TimeSeries(files)
 
 ###############################################################################
-# Next lets grab the HEK flare data for this time from the NOAA Space Weather
-# Prediction Center (SWPC)
-client = hek.HEKClient()
-flares_hek = client.search(a.Time(tr.start, tr.end),
-                           hek.attrs.FL, hek.attrs.FRM.Name == 'SWPC')
+# Next lets load the HEK metadata from Fido result to a HEKRow
+hek_results = results.get_response(1)
+flares_hek = hek_results[0]
 
 ###############################################################################
 # Lets plot everything together
 fig, ax = plt.subplots()
 goes.plot()
-ax.axvline(parse_time(flares_hek[0].get('event_peaktime')).datetime)
-ax.axvspan(parse_time(flares_hek[0].get('event_starttime')).datetime,
-           parse_time(flares_hek[0].get('event_endtime')).datetime,
-           alpha=0.2, label=flares_hek[0].get('fl_goescls'))
+ax.axvline(parse_time(flares_hek['event_peaktime']).datetime)
+ax.axvspan(parse_time(flares_hek['event_starttime']).datetime,
+           parse_time(flares_hek['event_endtime']).datetime,
+           alpha=0.2, label=flares_hek['fl_goescls'])
 ax.legend(loc=2)
 ax.set_yscale('log')
 plt.show()
