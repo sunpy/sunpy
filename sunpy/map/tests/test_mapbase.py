a10ff5978ca725358fc4317fb36d98bb20915583
Lazar Zivadinovic <lzivadinovic@itekako.com>
2020-09-01 12:40:02 +0200

    Add tests for naxis metadata in genericmap.resample

--- sunpy/map/tests/test_mapbase.py
+++ sunpy/map/tests/test_mapbase.py
@@ -590,8 +590,11 @@ def test_resample_metadata(generic_map, sample_method, new_dimensions):
     assert resampled_map.meta['crpix2'] == (resampled_map.data.shape[0] + 1) / 2.
     assert resampled_map.meta['crval1'] == generic_map.center.Tx.value
     assert resampled_map.meta['crval2'] == generic_map.center.Ty.value
+    assert resampled_map.meta['naxis1'] == new_dimensions[0].value
+    assert resampled_map.meta['naxis2'] == new_dimensions[1].value
     for key in generic_map.meta:
-        if key not in ('cdelt1', 'cdelt2', 'crpix1', 'crpix2', 'crval1', 'crval2'):
+        if key not in ('cdelt1', 'cdelt2', 'crpix1', 'crpix2', 'crval1', \
+                       'crval2', 'naxis1', 'naxis2'):
             assert resampled_map.meta[key] == generic_map.meta[key]
 
 
