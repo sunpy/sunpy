324b389f83bdf886b785a701a4d3136c8601ba16
Aryan Chouhan <chouhanaryan@yahoo.com>
2020-09-25 15:46:34 +0530

    Extend GenericTimeSeries concatenate to allow iterables + tests

--- sunpy/timeseries/timeseriesbase.py
+++ sunpy/timeseries/timeseriesbase.py
@@ -5,6 +5,7 @@ This module provies `sunpy.timeseries.GenericTimeSeries` which all other
 import copy
 import warnings
 from collections import OrderedDict
+from collections.abc import Iterable
 
 import pandas as pd
 
@@ -358,15 +359,16 @@ class GenericTimeSeries:
 
     def concatenate(self, otherts, same_source=False, **kwargs):
         """
-        Concatenate with another `~sunpy.timeseries.TimeSeries`. This function
-        will check and remove any duplicate times. It will keep the column
-        values from the original timeseries to which the new time series is
-        being added.
+        Concatenate with another `~sunpy.timeseries.TimeSeries` or an iterable containing multiple
+        `~sunpy.timeseries.TimeSeries`. This function will check and remove any duplicate times.
+        It will keep the column values from the original timeseries to which the new time
+        series is being added.
 
         Parameters
         ----------
-        otherts : `~sunpy.timeseries.TimeSeries`
-            Another `~sunpy.timeseries.TimeSeries`.
+        otherts : `~sunpy.timeseries.TimeSeries` or `collections.abc.Iterable`
+            Another `~sunpy.timeseries.TimeSeries` or an iterable containing multiple
+            `~sunpy.timeseries.TimeSeries`.
         same_source : `bool`, optional
             Set to `True` to check if the sources of the time series match. Defaults to `False`.
 
@@ -379,27 +381,35 @@ class GenericTimeSeries:
         -----
         Extra keywords are passed to `pandas.concat`.
         """
-        # TODO: decide if we want to be able to concatenate multiple time series at once.
-        # check to see if nothing needs to be done
+        # Check to see if nothing needs to be done in case a TimeSeries is provided.
         if self == otherts:
             return self
 
         # Check the sources match if specified.
-        if same_source and not (isinstance(otherts, self.__class__)):
-            raise TypeError("TimeSeries classes must match if specified.")
+        if same_source and not (
+            isinstance(otherts, self.__class__)
+            or all(isinstance(series, self.__class__) for series in otherts)
+        ):
+            raise TypeError("TimeSeries classes must match if 'same_source' is specified.")
 
-        # Concatenate the metadata and data
-        kwargs['sort'] = kwargs.pop('sort', False)
-        meta = self.meta.concatenate(otherts.meta)
-        data = pd.concat([self._data.copy(), otherts.to_dataframe()], **kwargs)
+        # If an iterable is not provided, it must be a TimeSeries object, so wrap it in a list.
+        if not isinstance(otherts, Iterable):
+            otherts = [otherts]
+
+        # Concatenate the metadata and data.
+        kwargs["sort"] = kwargs.pop("sort", False)
+        meta = self.meta.concatenate([series.meta for series in otherts])
+        data = pd.concat(
+            [self._data.copy(), *list(series.to_dataframe() for series in otherts)], **kwargs
+        )
 
         # Add all the new units to the dictionary.
         units = OrderedDict()
         units.update(self.units)
-        units.update(otherts.units)
+        units.update({k: v for unit in list(series.units for series in otherts) for k, v in unit.items()})
 
         # If sources match then build similar TimeSeries.
-        if self.__class__ == otherts.__class__:
+        if all(self.__class__ == series.__class__ for series in otherts):
             object = self.__class__(data.sort_index(), meta, units)
         else:
             # Build generic time series if the sources don't match.
