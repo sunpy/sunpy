c02542c27bbddf0e97e230e18d557eaa46f8a1f8
resakra <resakra@protonmail.com>
2020-10-02 17:12:35 +0200

    changed all c3 to c2

--- examples/acquiring_data/downloading_lascoC3.py
+++ examples/acquiring_data/downloading_lascoC3.py
@@ -1,9 +1,9 @@
 """
 ======================================
-Downloading and plotting LASCO C3 data
+Downloading and plotting LASCO C2 data
 ======================================
 
-How to download SOHO/LASCO C3 data with Fido and make a plot.
+How to download SOHO/LASCO C2 data with Fido and make a plot.
 """
 import matplotlib.pyplot as plt
 
@@ -19,7 +19,7 @@ from sunpy.net import attrs as a
 # a timerange and the instrument.
 timerange = a.Time('2002/05/24 11:06', '2002/05/24 11:07')
 instrument = a.Instrument.lasco
-detector = a.Detector.c3
+detector = a.Detector.c2
 result = Fido.search(timerange, instrument)
 
 ###############################################################################
