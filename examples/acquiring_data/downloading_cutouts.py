863f24c1b0ca4e615d5c6491f87db43626164b6c
Will Barnes <will.t.barnes@gmail.com>
2020-11-30 02:57:48 -0500

    changelog, test, example

--- examples/acquiring_data/downloading_cutouts.py
+++ examples/acquiring_data/downloading_cutouts.py
@@ -1,40 +1,93 @@
-from aiapy.util import sdo_location
+"""
+===============================================
+Requesting cutouts of AIA images from the JSOC
+===============================================
+
+This example shows how to request a cutout of a series of
+AIA images from the JSOC and animate the resulting sequence.
+"""
+import matplotlib.pyplot as plt
 
 import astropy.time
 import astropy.units as u
 from astropy.coordinates import SkyCoord
+from astropy.visualization import ImageNormalize, SqrtStretch
 
-from sunpy.coordinates import Helioprojective
-from sunpy.net import attrs, jsoc
+import sunpy.map
+from sunpy.net import Fido, attrs, jsoc
 
-tstart = astropy.time.Time('2015-10-17T04:33:30.000')
-duration = 12 * u.h
+#####################################################
+# First, query a full frame AIA image.
+t0 = astropy.time.Time('2012-09-24T14:56:03', scale='utc', format='isot')
+q = Fido.search(
+    attrs.Instrument.aia,
+    attrs.Physobs.intensity,
+    attrs.Wavelength(171*u.angstrom),
+    attrs.Time(t0, t0 + 13*u.s),
+)
+m = sunpy.map.Map(Fido.fetch(q))
 
-obs = sdo_location(tstart)
-frame = Helioprojective(observer=obs, obstime=obs.obstime)
+#####################################################
+# Next, we will create a submap from this image. We will
+# crop the field of view to active region NOAA 11575.
+m_cutout = m.submap(
+    SkyCoord(-500*u.arcsec, -275*u.arcsec, frame=m.coordinate_frame),
+    top_right=SkyCoord(150*u.arcsec, 375*u.arcsec, frame=m.coordinate_frame),
+)
+m_cutout.peek()
 
-width = 345.6 * u.arcsec
-height = 345.6 * u.arcsec
-Txc = -517.2 * u.arcsec
-Tyc = -246 * u.arcsec
-
-bottom_left = SkyCoord(Tx=Txc-width/2, Ty=Tyc-height/2, frame=frame)
-
+#####################################################
+# We want to watch the evolution of this active region
+# in time, but we do not want to download the full frame
+# image at each timestep. Instead, we will use our submap
+# to create a cutout request from the JSOC.
+#
+# First, create a `~sunpy.net.jsoc.JSOCClient` instance.
 c = jsoc.JSOCClient()
 
+#####################################################
+# Next, construct the cutout from the submap
+# above using the `~sunpy.net.jsoc.attrs.Cutout` attribute.
+cutout = jsoc.attrs.Cutout(
+    m_cutout.bottom_left_coord,
+    top_right=m_cutout.top_right_coord,
+    tracking=True
+)
+
+#####################################################
+# Now we are ready to construct the query. Note that all of this is
+# the same for a full-frame image except for the
+# cutout component.
 q = c.search(
-    attrs.Time(tstart, tstart + duration),
-    attrs.Wavelength(171*u.angstrom),
+    attrs.Time(m_cutout.date - 6*u.h, m_cutout.date + 6*u.h),
+    attrs.Wavelength(m_cutout.wavelength),
     attrs.Sample(12*u.min),
-    jsoc.attrs.Series('aia.lev1_euv_12s'),
-    jsoc.attrs.Notify('will.t.barnes@gmail.com'),
-    jsoc.attrs.Segment('image'),
-    jsoc.attrs.Cutout(bottom_left, width=width, height=height, tracking=True),
+    jsoc.attrs.Series.aia_lev1_euv_12s,
+    jsoc.attrs.Notify('jsoc@cadair.com'),  # Put your email here
+    jsoc.attrs.Segment.image,
+    cutout,
 )
-resp = c.request_data(q, method='url', protocol='fits')
-while not resp.has_succeeded():
+
+#####################################################
+# Submit the export request and download the data. This
+# may take some time as the data has to be processed and
+# downloaded.
+req = c.request_data(q, method='url', protocol='fits')
+while not req.has_succeeded():
     continue
-req = c.get_request(
-    resp,
-    path='/Users/willbarnes/Desktop/cutout-request-tests', max_conn=2,
-)
+files = c.get_request(req, max_conn=2)
+files.sort()
+
+#####################################################
+# Now that we've downloaded the files, we can create
+# a `~sunpy.map.MapSequence` from them.
+m_seq = sunpy.map.Map(files, sequence=True)
+
+#####################################################
+# Finally, we can construct an animation in time from
+# our stack of cutouts and interactively flip through
+# each image in our sequence.
+for m in m_seq:
+    m.plot_settings['norm'] = ImageNormalize(vmin=0, vmax=5e3, stretch=SqrtStretch())
+m_seq.peek()
+plt.show()
