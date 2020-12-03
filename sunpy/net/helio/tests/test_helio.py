6429ef11b2386dbfbcc81b180a0b7c2afd9b19ee
Nabil Freij <nabil.freij@gmail.com>
2020-12-03 20:18:43 +0000

    missing line

--- sunpy/net/helio/tests/test_helio.py
+++ sunpy/net/helio/tests/test_helio.py
@@ -311,6 +311,7 @@ def test_client_search(client):
     res = client.search(a.Time(start, end), a.helio.TableName(table_name), a.helio.MaxRecords(10))
     assert len(res) == 10
 
+
 @pytest.mark.remote_data
 def test_HECResponse_iter(client):
     start = '2005/01/03'
