8752f04ea1e4e633568255fee1e06cec042db187
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2020-12-07 19:20:53 +0530

    Update offset_frame.py

--- sunpy/coordinates/offset_frame.py
+++ sunpy/coordinates/offset_frame.py
@@ -15,6 +15,13 @@ class NorthOffsetFrame:
     This class should be used when specifying a new north pole is natural.  In constrast, for
     shifting the origin in the projected sky (e.g., where helioprojective X and Y coordinates are
     zero), use `~astropy.coordinates.SkyOffsetFrame` instead.
+    
+    
+    Examples
+    --------
+
+    .. minigallery:: examples.units_and_coordinates 
+
 
     Parameters
     ----------
