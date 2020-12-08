6dfa6b32a046347ab3c564be58434f784a1e4d33
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2020-12-09 02:04:17 +0530

    New NorthOffsetFrame Example from function example (#4709)
    Co-authored-by: Albert Y. Shih <ayshih@gmail.com>
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/coordinates/offset_frame.py
+++ sunpy/coordinates/offset_frame.py
@@ -23,42 +23,7 @@ class NorthOffsetFrame:
 
     Examples
     --------
-    A common use for this is to create a frame derived from Heliographic, which
-    has the north pole at some point of interest. In this new frame, lines of
-    longitude form great circles radially away from the point, and lines of
-    latitude measure angular distance from the point.
-
-    In this example the new frame is shifted so the new north pole is at (20,
-    20) in the Heliographic Stonyhurst frame. The new grid is overplotted in
-    blue.
-
-    .. plot::
-        :include-source:
-
-        import matplotlib.pyplot as plt
-        import astropy.units as u
-
-        from astropy.coordinates import SkyCoord
-        from sunpy.coordinates import NorthOffsetFrame
-
-        import sunpy.map
-        from sunpy.data.sample import AIA_171_IMAGE
-
-        m = sunpy.map.Map(AIA_171_IMAGE)
-
-        north = SkyCoord(20*u.deg, 20*u.deg, frame="heliographic_stonyhurst")
-        new_frame = NorthOffsetFrame(north=north)
-
-        ax = plt.subplot(projection=m)
-        m.plot()
-
-        overlay = ax.get_coords_overlay('heliographic_stonyhurst')
-        overlay[0].set_ticks(spacing=30. * u.deg, color='white')
-        overlay.grid(ls='-', color='white')
-
-        overlay = ax.get_coords_overlay(new_frame)
-        overlay[0].set_ticks(spacing=30. * u.deg)
-        overlay.grid(ls='-', color='blue')
+    .. minigallery:: sunpy.coordinates.NorthOffsetFrame
 
     Notes
     -----
