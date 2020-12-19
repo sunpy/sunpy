9e0e8763f212b0cd7035ca52f5532cccd2448065
David Stansby <dstansby@gmail.com>
2020-12-19 10:42:47 +0000

    Define custom __dir__ for attrs

--- sunpy/net/dataretriever/attrs/goes.py
+++ sunpy/net/dataretriever/attrs/goes.py
@@ -3,6 +3,11 @@ from sunpy.net.attr import SimpleAttr
 __all__ = ["SatelliteNumber"]
 
 
+# Define a custom __dir__ to restrict tab-completion to __all__
+def __dir__():
+    return __all__
+
+
 class SatelliteNumber(SimpleAttr):
     """
     The GOES Satellite Number
