d1cd7473e042800f36e36c44b1514409d2327bbf
kathivashkiv <kate777iv@gmail.com>
2020-12-23 00:07:23 +0200

    Add initial benchmarks

---
+++ benchmarks/benchmarks_for_map.py
@@ -0,0 +1,30 @@
+# Initial benchmarks
+# See "Writing benchmarks" in the asv docs for more information.
+
+import sunpy.data.sample
+import sunpy.map
+
+class TimeSuite:
+    """
+    An example benchmark that times the performance of sunpy map
+    creation
+    """
+    def setup(self):
+        self._map = sunpy.data.sample.AIA_171_IMAGE
+
+    def time_create_sunpy_map(self):
+        aia_map = sunpy.map.Map(self._map)
+
+class MemSuite:
+    def setup(self):
+        self._map = sunpy.data.sample.AIA_171_IMAGE
+        
+    def mem_sunpy_map(self):
+        return sunpy.map.Map(self._map)
+
+class PeakMemorySuite:
+    def setup(self):
+        self._map = sunpy.data.sample.AIA_171_IMAGE
+
+    def peakmem_sunpy_map(self):
+        aia_map = sunpy.map.Map(self._map)
