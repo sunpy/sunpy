853c7337e98913fd07d4d6959c6e3425b0312c3d
kathivashkiv <kate777iv@gmail.com>
2020-12-23 20:28:27 +0200

    Tox codestyle run

--- benchmarks/benchmarks_for_map.py
+++ benchmarks/benchmarks_for_map.py
@@ -12,7 +12,7 @@ class TimeSuite:
         self._map = sunpy.data.sample.AIA_171_IMAGE
 
     def time_create_sunpy_map(self):
-        aia_map = sunpy.map.Map(self._map)
+        sunpy.map.Map(self._map)
 
 
 class MemSuite:
@@ -28,4 +28,4 @@ class PeakMemorySuite:
         self._map = sunpy.data.sample.AIA_171_IMAGE
 
     def peakmem_sunpy_map(self):
-        aia_map = sunpy.map.Map(self._map)
+        sunpy.map.Map(self._map)
