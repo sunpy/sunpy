4d6a58e0e6045948d93455d71b6f9a9bc20a480a
Laura Hayes <hayesla@tcd.ie>
2020-12-23 09:37:01 +0000

    fixing text

--- examples/plotting/xy_lims.py
+++ examples/plotting/xy_lims.py
@@ -3,8 +3,8 @@
 Plotting with xlim and ylim on Map
 ==================================
 
-In this example we are going to look how to use xlim
-and ylim when plotting Map with WCSAxes.
+In this example we are going to look at how to set the axes
+range using xlim and ylim when plotting a Map with WCSAxes.
 """
 
 import matplotlib.pyplot as plt
@@ -16,17 +16,17 @@ import sunpy.data.sample
 import sunpy.map
 
 ###############################################################################
-# Lets start by creating a Map from the sample data and plotting it.
+# Lets start by creating a Map from the sample data.
 aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ###############################################################################
 # Now lets say for example we are only interested in plotting a certain region
 # of this Map. One way this could be done is to create a submap over the region
-# of interest and then plotting that. Another useful way is to just set the axes
-# range over which to plot using Matplotlib's xlim and ylim functions.
+# of interest and then plotting that. Another useful way is to set the axes
+# range over which to plot using Matplotlib's set_xlim and set_ylim functions.
 # The axes that Matplotlib uses is in pixel coordinates (e.g. of image data array)
 # rather than world coordinates (e.g. in arcsecs) so we need to define our limits that
-# are passed to .set_xlim, .set_lim to pixel coordinates.
+# are passed to set_xlim(), set_lim() to pixel coordinates.
 # We can define our limits we want to use in world coordinates and then work out what pixel
 # coordinates these correspond to.
 # Lets choose xlimits and ylimits in arcsecs that we are interested in.
@@ -34,7 +34,7 @@ xlims_world = [500, 1100]*u.arcsec
 ylims_world = [-800, 0]*u.arcsec
 
 ###############################################################################
-# We can then covert these into a SkyCoord which can be passed to Map.world_to_pixel to
+# We can then covert these into a SkyCoord which can be passed to Map.world_to_pixel() to
 # determine which pixel coordinates these represent on the Map.
 world_coords = SkyCoord(Tx=xlims_world, Ty=ylims_world, frame=aia_map.coordinate_frame)
 pixel_coords = aia_map.world_to_pixel(world_coords)
