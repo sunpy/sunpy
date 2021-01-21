dc4c60d2d2602d58a8f55c86f946507679ce644f
Stuart Mumford <stuart@cadair.com>
2021-01-21 15:45:13 +0000

    doctest fix

--- sunpy/util/_table_attribute.py
+++ sunpy/util/_table_attribute.py
@@ -86,16 +86,6 @@ except ImportError:
         ----------
         default : object
             Default value for attribute
-        Examples
-        --------
-        >>> from astropy.table import Table, TableAttribute
-        >>> class MyTable(Table):
-        ...     identifier = TableAttribute(default=1)
-        >>> t = MyTable(identifier=10)
-        >>> t.identifier
-        10
-        >>> t.meta
-        OrderedDict([('__attributes__', {'identifier': 10})])
         """
 
     class QTable(QTable):
