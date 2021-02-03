c4816734ace35e4a59ccd5e6db114c97e93b05bb
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2021-02-03 15:44:29 +0000

    Update jsoc.py
    changed error message

--- sunpy/net/jsoc/jsoc.py
+++ sunpy/net/jsoc/jsoc.py
@@ -463,7 +463,7 @@ class JSOCClient(BaseClient):
        """
         for resp in jsoc_response.query_args:
             if 'notify' not in resp:
-                raise ValueError('Email address is not specified by a.jsoc.Notify')
+                raise ValueError('Pass attrs.jsoc.Notify with a JSOC registered email as an argument to fido.search')
 
         if len(jsoc_response) != jsoc_response._original_num_rows:
             warnings.warn("Downloading of sliced JSOC results is not supported. "
