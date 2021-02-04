250a359e5b60d31fe96897bfeca5c4ee72db5453
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2021-02-04 16:16:04 +0530

    Added appropriate error message for a JSOC search when Notify isn't passed (#4950)
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/net/jsoc/jsoc.py
+++ sunpy/net/jsoc/jsoc.py
@@ -461,6 +461,12 @@ class JSOCClient(BaseClient):
             A Results object
 
        """
+        for resp in jsoc_response.query_args:
+            if 'notify' not in resp:
+                raise ValueError('A registered email is required to get data from JSOC. '
+                                 'Please supply an email with attrs.jsoc.Notify to Fido.search. '
+                                 'Then pass those new results back into Fido.fetch')
+
         if len(jsoc_response) != jsoc_response._original_num_rows:
             warnings.warn("Downloading of sliced JSOC results is not supported. "
                           "All the files present in the original response will "
