7f405943ac892edb3817136b1128cdb548fdd7e7
Nabil Freij <nabil.freij@gmail.com>
2021-02-27 20:43:52 +0000

    Merge pull request #5038 from nabobalis/master
    added support to more than one registry url for hec

--- sunpy/net/helio/registry_links.py
+++
@@ -1,6 +0,0 @@
-# contains links to HELIO registries
-"""
-The following link is the link to the registry, should it be necessary, this
-link can easily be replaced or swapped.
-"""
-LINK = 'http://helio.ukssdc.ac.uk/helio_registry/viewResourceEntry_body.jsp?XML=true&IVORN=ivo://helio-vo.eu'
