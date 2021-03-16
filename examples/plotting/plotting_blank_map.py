a11b6e7391eb96131b169c202f93d2ede13674b3
Megh <m11dedhia@gmail.com>
2021-03-17 00:13:43 +0530

    modified:   plotting_blank_map.py

--- examples/plotting/plotting_blank_map.py
+++ examples/plotting/plotting_blank_map.py
@@ -20,13 +20,13 @@ from sunpy.coordinates import frames
 ################################################################################
 # First we will create a blank map using with an array of zeros.
 # Since there is no WCS information, we will need to construct a header to pass to Map.
-data = np.full((1000, 1000), np.nan)
+data = np.full((100, 100), np.nan)
 
 # Define coordinates and frame of reference and make the header using sunpy.map.make_fitswcs_header
 skycoord = SkyCoord(0*u.arcsec, 0*u.arcsec, obstime='2013-10-28',
                     observer='earth', frame=frames.Helioprojective)
 # Scale set to the following for solar limb to be in the field of view
-header = sunpy.map.make_fitswcs_header(data, skycoord, scale=[2, 2]*u.arcsec/u.pixel)
+header = sunpy.map.make_fitswcs_header(data, skycoord, scale=[3, 3]*u.arcsec/u.pixel)
 
 # Use sunpy.map.Map to create the blank map
 blank_map = sunpy.map.Map(data, header)
@@ -37,9 +37,7 @@ blank_map = sunpy.map.Map(data, header)
 # Initialize the plot and add the map to it
 fig = plt.figure()
 ax = plt.subplot(projection=blank_map)
-
-# Plot the map with alpha=0 to provide white plot
-blank_map.plot(alpha=0)
+blank_map.plot()
 blank_map.draw_limb(color="k")
 blank_map.draw_grid(color="k")
 
