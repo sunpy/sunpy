51e0f98de5c7a231452fa943d1d5c03f610b4b04
jeffreypaul15 <jeffrey.paul200@gmail.com>
2021-03-18 23:20:55 +0530

    fixed check for np.nan

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -1484,7 +1484,7 @@ class GenericMap(NDData):
         pad_x = int(np.max((diff[1], 0)))
         pad_y = int(np.max((diff[0], 0)))
 
-        if issubclass(self.data.dtype.type, numbers.Integral) and (missing == np.nan):
+        if issubclass(self.data.dtype.type, numbers.Integral) and (np.isnan(missing)):
             if version.parse(np.__version__) >= version.parse("1.20.0"):
                 raise ImportError('Missing NaN value is not supported for Integer map')
             warnings.warn("Integer map data is incompatible with specified missing value",
