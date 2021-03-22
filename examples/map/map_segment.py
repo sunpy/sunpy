b385866b7a7fff8cfcab702f2ce7304493c2adcd
jeffreypaul15 <jeffrey.paul200@gmail.com>
2021-03-22 13:31:50 +0530

    codestyle and added mask

--- examples/map/map_segment.py
+++ examples/map/map_segment.py
@@ -6,13 +6,13 @@ Segmenting a Map based on transformation of coordinates
 A demostration creating a segment of a particular map from
 transformed coordinates.
 """
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from reproject import reproject_interp
 
 import astropy.units as u
-from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
 
 import sunpy.map
 from sunpy.coordinates import Helioprojective, NorthOffsetFrame, RotatedSunFrame, transform_with_sun_center
@@ -29,16 +29,17 @@ all_hpc = sunpy.map.all_coordinates_from_map(smap)
 all_hgs = all_hpc.transform_to("heliographic_stonyhurst")
 
 ###############################################################################
-# Let's then segment the data based on coordinates and convert them to pixel
-# index. We convert the segmented pixel values to 0.
+# Let's then segment the data based on coordinates and create a boolean mask
+# where True indicates the segmented mask.
 segment = np.logical_and(all_hgs.lon < 10 * u.deg, all_hgs.lon > 0 * u.deg)
 
 ###############################################################################
-# Let's plot the segment separately
-new_data = np.asarray(segment, dtype='float')
-new_data = np.where(new_data == False, np.nan, new_data)
-
-new_frame_map = sunpy.map.Map(new_data * smap.data, smap.meta)
+# Let's plot the segment separately, we create a new map with the segment as the
+# mask.
+new_frame_map = sunpy.map.Map(
+    smap.data,
+    smap.meta,
+    mask=np.logical_not(segment))
 fig = plt.figure()
 fig.add_subplot(projection=smap)
 new_frame_map.plot()
@@ -61,10 +62,10 @@ segment = np.logical_and(
 
 ###############################################################################
 # Let's plot the ofsetted segment separately
-new_data = np.asarray(segment, dtype='float')
-new_data[new_data == False] = np.nan
-
-ofsetted_map = sunpy.map.Map(new_data * smap.data, smap.meta)
+ofsetted_map = sunpy.map.Map(
+    smap.data,
+    smap.meta,
+    mask=np.logical_not(segment))
 fig = plt.figure()
 fig.add_subplot(projection=smap)
 ofsetted_map.plot()
@@ -104,10 +105,10 @@ segment = np.logical_and(
     rot_coords.Tx > 800 * u.arcsec,
     rot_coords.Ty < 300 * u.arcsec)
 
-new_data = np.asarray(segment, dtype='float')
-new_data[new_data == False] = np.nan
-
-rotated_map = sunpy.map.Map(new_data * out_warp.data, out_warp.meta)
+rotated_map = sunpy.map.Map(
+    out_warp.data,
+    out_warp.meta,
+    mask=np.logical_not(segment))
 rotated_map.plot_settings = out_warp.plot_settings
 
 ###############################################################################
