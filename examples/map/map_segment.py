679db36ea4dadd8c67bd452ae3ee0a27a319ea6d
jeffreypaul15 <jeffrey.paul200@gmail.com>
2021-03-22 17:19:02 +0530

    removed RotatedSunFrame and added np operations

--- examples/map/map_segment.py
+++ examples/map/map_segment.py
@@ -46,6 +46,12 @@ new_frame_map.plot()
 plt.show()
 
 ###############################################################################
+# We can perform various mathematical operations on the extracted segment such
+# as averaging the pixel values or finding the sum of the segment.
+mean_pix = np.average(segment)
+sum_pix = np.sum(segment)
+
+###############################################################################
 # This example can also be extended while using NorthOffsetFrame
 # Let us offset the north pole and create the frame
 north = SkyCoord(20 * u.deg, 20 * u.deg, frame="heliographic_stonyhurst")
@@ -72,48 +78,7 @@ ofsetted_map.plot()
 plt.show()
 
 ###############################################################################
-# Another example would be to transform the coordinates to a RotatedSunFrame
-# Referring to the example of differentially rotating a map, we create a
-# RotatedSunFrame to transform the coordinates
-in_time = smap.date
-
-out_time = in_time + 5 * u.day
-out_frame = Helioprojective(observer='earth', obstime=out_time)
-rot_frame = RotatedSunFrame(base=out_frame, rotated_time=in_time)
-out_shape = smap.data.shape
-out_center = SkyCoord(0 * u.arcsec, 0 * u.arcsec, frame=out_frame)
-header = sunpy.map.make_fitswcs_header(out_shape,
-                                       out_center,
-                                       scale=u.Quantity(smap.scale))
-out_wcs = WCS(header)
-out_wcs.coordinate_frame = rot_frame
-
-with transform_with_sun_center():
-    arr, _ = reproject_interp(smap, out_wcs, out_shape)
-
-out_warp = sunpy.map.Map(arr, out_wcs)
-out_warp.plot_settings = smap.plot_settings
-
-###############################################################################
-# Transforming the coordinates to the RotatedSunFrame
-all_hpc = sunpy.map.all_coordinates_from_map(out_warp)
-rot_coords = all_hpc.transform_to(rot_frame)
-
-###############################################################################
-# Creating the segment and converting it into a separate map
-segment = np.logical_and(
-    rot_coords.Tx > 800 * u.arcsec,
-    rot_coords.Ty < 300 * u.arcsec)
-
-rotated_map = sunpy.map.Map(
-    out_warp.data,
-    out_warp.meta,
-    mask=np.logical_not(segment))
-rotated_map.plot_settings = out_warp.plot_settings
-
-###############################################################################
-# Let's plot the new segment
-fig = plt.figure()
-fig.add_subplot(projection=smap)
-rotated_map.plot()
-plt.show()
+# We can also find the maximum, minimum or average pixel values of the segment
+mean_pix = np.average(segment)
+max_pix = np.max(segment*smap.data)
+min_pix = np.min(segment*smap.data)
