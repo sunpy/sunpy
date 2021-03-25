ec107a176d3fad045a9636f2030bae6523f7c122
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-03-25 17:51:14 +0000

    codestyle

--- examples/map/map_segment.py
+++ examples/map/map_segment.py
@@ -2,8 +2,7 @@
 =======================================================
 Segmenting a Map based on transformation of coordinates
 =======================================================
-
-A demonstration extracting a region of a particular map based on
+This example demonstrates extracting a region of a particular map based on
 world coordinates in different systems.
 """
 import matplotlib.pyplot as plt
@@ -30,18 +29,16 @@ all_hgs = all_hpc.transform_to("heliographic_stonyhurst")
 
 ######################################################################
 # Let's then segment the data based on coordinates and create a boolean mask
-# where True indicates invalid or deselected data.
+# where `True` indicates invalid or deselected data.
 # This is the convention used by the `numpy.ma` module.
 segment = np.logical_or(all_hgs.lon >= 50 * u.deg, all_hgs.lon <= 0 * u.deg)
 
 ######################################################################
 # Let's plot the segment separately, we create a new map with the segment as the
-# mask.
-# Numpy's masked arrays allow for a combination of standart numpy array and
-# a mask array. When an element of the mask is True, the corresponding element
+# mask. Numpy's masked arrays allow for a combination of standard numpy array and
+# a mask array. When an element of the mask is `True`, the corresponding element
 # of the associated array is said to be masked (invalid).
-# More information about Numpy's masked arrays :
-# https://numpy.org/doc/stable/reference/maskedarray.generic.html#using-numpy-ma
+# `More information about numpy's masked arrays <https://numpy.org/doc/stable/reference/maskedarray.generic.html#using-numpy-ma>`
 # From the segment, we now mask out all values where `all_hgs.lon` is
 # NaN.
 segment = np.logical_or(segment, np.isnan(all_hgs.lon))
@@ -62,25 +59,25 @@ print(f"Original Map : mean = {smap.data.mean()}, sum = {smap.data.sum()}")
 print(f"Segmented Map : mean = {new_frame_mask.mean()}, sum = {new_frame_mask.sum()}")
 
 ######################################################################
-# This example can also be extended while using NorthOffsetFrame
-# Let us offset the north pole and create the frame
+# This example can also be extended while using `sunpy.coordinates.NorthOffsetFrame`
+# Let us offset the north pole and create the frame.
 north = SkyCoord(20 * u.deg, 20 * u.deg, frame="heliographic_stonyhurst")
 f = NorthOffsetFrame(north=north)
 
 ######################################################################
-# We then transform coordinates to the Offsetted frame and segment the data
-# based on conditions
+# We then transform coordinates to the offsetted frame and segment the data
+# based on conditions.
 all_hpc = sunpy.map.all_coordinates_from_map(smap)
 ofsetted_coords = all_hpc.transform_to(f)
 segment = np.logical_or(ofsetted_coords.lon >= 30 * u.deg,
                         ofsetted_coords.lon <= -20 * u.deg)
 
 ######################################################################
-# Masking out the NaN values of `ofsetted_coords.lon`, we get
+# Masking out the NaN values of ``ofsetted_coords.lon``, we get:
 segment = np.logical_or(segment, np.isnan(ofsetted_coords.lon))
 
 ######################################################################
-# Let's plot the ofsetted segment separately
+# Let's plot the ofsetted segment separately.
 offsetted_map = sunpy.map.Map(smap.data, smap.meta, mask=segment)
 fig = plt.figure()
 ax = plt.subplot(projection=smap)
