af1d3adbcec73a2f3258a328645c3765218b90a9
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2021-03-26 17:11:40 +0530

    Apply suggestions from code review
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- examples/map/map_segment.py
+++ examples/map/map_segment.py
@@ -21,7 +21,7 @@ smap = sunpy.map.Map(AIA_171_IMAGE)
 
 ######################################################################
 # A utility function gives us access to the helioprojective coordinate of each
-# pixel. From this we then transform the coordinates to the required frame.
+# pixel in this map. From this we then transform the coordinates to the required frame.
 # For this example we are going to extract a region based on the
 # heliographic Stonyhurst coordinates, so we transform to that frame.
 all_hpc = sunpy.map.all_coordinates_from_map(smap)
@@ -30,17 +30,16 @@ all_hgs = all_hpc.transform_to("heliographic_stonyhurst")
 ######################################################################
 # Let's then segment the data based on coordinates and create a boolean mask
 # where `True` indicates invalid or deselected data.
-# mask. Numpy's masked arrays allow for a combination of standard numpy array and
+# Numpy's masked arrays allow for a combination of standard numpy array and
 # a mask array. When an element of the mask is `True`, the corresponding element
 # of the associated array is said to be masked (invalid).
-# `More information about numpy's masked arrays <https://numpy.org/doc/stable/reference/maskedarray.generic.html#using-numpy-ma>`
-# From the segment, we now mask out all values where ``all_hgs.lon`` is NaN.
-# This is the convention used by the `numpy.ma` module.
+# For more information about numpy's masked arrays see :mod:`numpy.ma`.
+# We now mask out all values not in our coordinate range and where the coordinates are NaN.
 segment_mask = np.logical_or(all_hgs.lon >= 35 * u.deg, all_hgs.lon <= -35 * u.deg)
 segment_mask |= np.isnan(all_hgs.lon)
 
 ######################################################################
-# Let's plot the segment separately, we create a new map with the segment as the mask
+# To plot the segment separately, we create a new map with the segment as the mask
 new_frame_map = sunpy.map.Map(smap.data, smap.meta, mask=segment_mask)
 fig = plt.figure()
 ax = plt.subplot(projection=smap)
@@ -51,13 +50,14 @@ plt.show()
 ######################################################################
 # We can perform various mathematical operations on the extracted segment such
 # as averaging the pixel values or finding the sum of the segment.
-masked_data = np.ma.array(smap.data, mask=segment_mask)
+masked_data = np.ma.array(new_frame_map.data, mask=new_frame_map.mask)
 
 print(f"Original Map : mean = {smap.data.mean()}, sum = {smap.data.sum()}")
 print(f"Segment : mean = {masked_data.mean()}, sum = {masked_data.sum()}")
 
 ######################################################################
-# This example can also be extended while using `sunpy.coordinates.NorthOffsetFrame`
+# Using `sunpy.coordinates.NorthOffsetFrame`
+# ------------------------------------------
 # Let us offset the north pole and create the frame.
 north = SkyCoord(20 * u.deg, 20 * u.deg, frame="heliographic_stonyhurst")
 offset_frame = NorthOffsetFrame(north=north)
@@ -89,10 +89,10 @@ plt.show()
 ######################################################################
 # We can also find the maximum, minimum or average pixel values of the segment
 # and compare it with the original map.
-offset_masked_data = np.ma.array(smap.data, mask=segment_mask)
+offset_masked_data = np.ma.array(offsetted_map.data, mask=offsetted_map.mask)
 print(f"Original Map : mean = {smap.data.mean()}, "
       f"maximum value = {smap.data.max()}, "
       f"minimum value = {smap.data.min()}")
-print(f"Offsetted segment : mean = {offset_masked_data.mean()}, "
+print(f"Offset segment : mean = {offset_masked_data.mean()}, "
       f"maximum value = {offset_masked_data.max()}, "
       f"minimum value = {offset_masked_data.min()}")
