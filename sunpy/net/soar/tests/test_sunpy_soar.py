182b2e8fec659b416c9d7af6b3041f73d622ea29
David Stansby <dstansby@gmail.com>
2021-03-26 10:36:29 +0000

    Update test to use fetch

--- sunpy/net/soar/tests/test_sunpy_soar.py
+++ sunpy/net/soar/tests/test_sunpy_soar.py
@@ -1,3 +1,4 @@
+import sunpy.map
 from sunpy.net import Fido, attrs as a
 
 from sunpy.net.soar import Identifier
@@ -5,9 +6,15 @@ from sunpy.net.soar import Identifier
 
 def test_search():
     id = a.Instrument('EUI')
-    time = a.Time('2020-02-01', '2021-02-20')
-    level = a.Level(2)
+    time = a.Time('2021-02-01', '2021-02-02')
+    level = a.Level(1)
     identifier = Identifier('EUI-FSI174-IMAGE')
 
     res = Fido.search(id, time, level, identifier)
     assert len(res) == 1
+    assert len(res[0]) == 43
+
+    files = Fido.fetch(res[0, 0])
+    assert len(files) == 1
+
+    eui_map = sunpy.map.Map(files[0])
