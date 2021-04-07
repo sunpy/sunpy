a7b2874e82c552d3579c52abd6243c6a02d352fa
Will Barnes <will.t.barnes@gmail.com>
2021-04-07 13:42:07 -0400

    more tests

--- sunpy/map/sources/tests/test_eui_source.py
+++ sunpy/map/sources/tests/test_eui_source.py
@@ -1,13 +1,17 @@
 """Tests for EUI Solar Orbiter Map"""
 import pytest
 
+import astropy.units as u
+
+from sunpy.data.test import get_test_filepath
 from sunpy.map import Map
 from sunpy.map.sources import EUIMap
 
 
 @pytest.fixture
-def eui_map():
-    return Map(eui_test_fits_file)
+def eui_map(scope="module"):
+    fname = get_test_filepath('solo_L2_eui-hrieuv174-image_20201019T200200202_V01.fits')
+    return Map(fname)
 
 
 def test_EUIMap(eui_map):
@@ -24,6 +28,18 @@ def test_observatory(eui_map):
 
 def test_measurement(eui_map):
     assert eui_map.measurement == u.Quantity(174, 'angstrom')
+
+
+def test_exposure_time(eui_map):
+    assert eui_map.exposure_time == u.Quantity(3, 's')
+
+
+def test_level_number(eui_map):
+    assert eui_map.processing_level == 2
+
+
+def test_unit(eui_map):
+    assert eui_map.unit == u.Unit('ct / s')
 
 
 def test_norm_clip(eui_map):
