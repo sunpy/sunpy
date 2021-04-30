34c401be58b8525e9accf92622be9fd90fad7d21
David Stansby <dstansby@gmail.com>
2021-04-30 20:50:44 +0100

    Add in-situ test

--- sunpy/net/soar/tests/test_sunpy_soar.py
+++ sunpy/net/soar/tests/test_sunpy_soar.py
@@ -21,6 +21,19 @@ def test_search():
     eui_map = sunpy.map.Map(files[0])
 
 
+def test_insitu_search():
+    id = a.Instrument('MAG')
+    time = a.Time('2020-04-16', '2020-04-17')
+    identifier = Identifier('MAG-RTN-NORMAL-1-MINUTE')
+
+    res = Fido.search(id, time, identifier)
+    assert len(res) == 1
+    assert len(res[0]) == 2
+
+    files = Fido.fetch(res[0, 0])
+    assert len(files) == 1
+
+
 def test_no_results():
     id = a.Instrument('EUI')
     time = a.Time('2019-02-01', '2019-02-02')
