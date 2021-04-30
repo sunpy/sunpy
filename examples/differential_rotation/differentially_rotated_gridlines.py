ef32149b9a86d2cdfb289fb60d5427b9012a29e4
Stuart Mumford <stuart@cadair.com>
2021-04-30 15:13:23 +0100

    Defer figures in diffrot grid lines example

--- examples/differential_rotation/differentially_rotated_gridlines.py
+++ examples/differential_rotation/differentially_rotated_gridlines.py
@@ -22,22 +22,29 @@ from sunpy.data.sample import AIA_171_IMAGE
 # Let's use an AIA observation, and plot lines of constant longitude in
 # heliographic Stonyhurst. We'll plot the normal lines (prior to applying
 # differential rotation) in white and the differentially rotated lines in
-# blue. Be aware that the differentially rotated lines are plotted in the
-# original coordinate frame, so it doesn't account for any motion of the
-# observer over 27 days.
+# blue.
 
 aiamap = sunpy.map.Map(AIA_171_IMAGE)
 fig = plt.figure()
 ax = plt.subplot(projection=aiamap)
 aiamap.plot(clip_interval=(1., 99.95)*u.percent)
 
+##############################################################################
 # Lines of constant longitude prior to differential rotation
+
+# sphinx_gallery_defer_figures
+
 overlay1 = ax.get_coords_overlay('heliographic_stonyhurst')
 overlay1[0].set_ticks(spacing=15. * u.deg)
 overlay1[1].set_ticks(spacing=90. * u.deg)
 overlay1.grid(ls='-', color='white')
 
+##############################################################################
 # Differentially rotating the lines of constant longitude by 27 days
+# Be aware that the differentially rotated lines are plotted in the
+# original coordinate frame, so it doesn't account for any motion of the
+# observer over 27 days.
+
 rs_hgs = RotatedSunFrame(base=HeliographicStonyhurst(obstime=aiamap.date),
                          duration=27*u.day)
 overlay2 = ax.get_coords_overlay(rs_hgs)
