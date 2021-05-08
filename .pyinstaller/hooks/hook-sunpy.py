8b92179454cd43b6eafcd9d143467bd4e021edb1
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-08 16:32:43 +0530

    modified hooks for sunpy and separated test imports

--- .pyinstaller/hooks/hook-sunpy.py
+++ .pyinstaller/hooks/hook-sunpy.py
@@ -1,13 +1,14 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_entry_point, collect_submodules, copy_metadata
 
+# Imports needed to run tests
 datas, hiddenimports = collect_entry_point("pytest11")
-
-datas += collect_data_files("sunpy")
-datas += collect_data_files("dask")
-datas += collect_data_files("drms")
-datas += copy_metadata("sunpy")
-
-hiddenimports += collect_submodules('sunpy')
 hiddenimports += collect_submodules('numpy.distutils')
 hiddenimports += collect_submodules('distutils')
 hiddenimports += ['sunpy.data.data_manager.tests.mocks']
+
+
+hiddenimports += collect_submodules('sunpy')
+
+datas += collect_data_files("sunpy")
+datas += collect_data_files("drms")
+datas += copy_metadata("sunpy")
