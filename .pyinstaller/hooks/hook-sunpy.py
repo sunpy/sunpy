c1a02886776c12d873f6b124ee01f21c9ef7b3bb
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-08 16:04:44 +0530

    removed skimage hiddenimport and installed hooks from github

--- .pyinstaller/hooks/hook-sunpy.py
+++ .pyinstaller/hooks/hook-sunpy.py
@@ -10,5 +10,4 @@ datas += copy_metadata("sunpy")
 hiddenimports += collect_submodules('sunpy')
 hiddenimports += collect_submodules('numpy.distutils')
 hiddenimports += collect_submodules('distutils')
-hiddenimports += ['skimage.filters.rank.core_cy_3d']
 hiddenimports += ['sunpy.data.data_manager.tests.mocks']
