1b9da7084bf4607f1176694d3e824e0e9af155ac
Albert Y. Shih <ayshih@gmail.com>
2021-06-09 13:52:39 +0000

    Fixed a bug where the HCC->HGS transformation lost the HGS rsun attribute

--- sunpy/coordinates/transformations.py
+++ sunpy/coordinates/transformations.py
@@ -444,8 +444,8 @@ def hcc_to_hgs(helioccoord, heliogframe):
     newrepr = helioccoord.cartesian.transform(total_matrix)
     int_coord = HeliographicStonyhurst(newrepr, obstime=hcc_observer_at_hcc_obstime.obstime)
 
-    # Loopback transform HGS if there is a change in obstime
-    return _transform_obstime(int_coord, heliogframe.obstime)
+    # Loopback transform HGS as needed
+    return int_coord.transform_to(heliogframe)
 
 
 @frame_transform_graph.transform(FunctionTransformWithFiniteDifference,
