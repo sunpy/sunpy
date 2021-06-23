9c7107361961c6c9bbf731fc9e76be1d0261fbab
Albert Y. Shih <ayshih@gmail.com>
2021-06-22 23:15:28 -0400

    Use numpy.indices()

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -2311,7 +2311,7 @@ class GenericMap(NDData):
         levels = self._process_levels_arg(levels, warn_units=True)
 
         # Pixel indices
-        x, y = np.meshgrid(*map(np.arange, np.flipud(self.data.shape)))
+        y, x = np.indices(self.data.shape)
 
         # Prepare a local variable in case we need to mask values
         data = self.data
