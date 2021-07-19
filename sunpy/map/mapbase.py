b8cbbdbee5322549b5a3ae9c784d4cc107bfaf39
David Stansby <dstansby@gmail.com>
2021-07-19 16:08:39 +0100

    Remove incorrect timesys warning in Map

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -745,7 +745,7 @@ class GenericMap(NDData):
             # user that it will be ignored.
             timesys = 'TAI'
             timesys_meta = self.meta.get('timesys', '').upper()
-            if timesys_meta != 'TAI':
+            if timesys_meta not in ('', 'TAI'):
                 warn_metadata('Found "TAI" in time string, ignoring TIMESYS keyword '
                               f'which is set to "{timesys_meta}".')
         else:
