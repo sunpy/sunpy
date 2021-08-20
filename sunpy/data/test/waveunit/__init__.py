476fe0d6b67793db4f24e94385b5bf4c3162f968
WIll Barnes <will.t.barnes@gmail.com>
2021-08-20 16:24:09 -0400

    fix up io tests

--- sunpy/data/test/waveunit/__init__.py
+++ sunpy/data/test/waveunit/__init__.py
@@ -1,9 +1,6 @@
-import os.path
+from sunpy.data.test import get_test_filepath
 
-from sunpy.data.test import rootdir as testrootdir
-
-waveunitdir = os.path.join(testrootdir, 'waveunit')
-MEDN_IMAGE = os.path.join(waveunitdir, 'medn_halph_fl_20050501_074655.fts')
-MQ_IMAGE = os.path.join(waveunitdir, 'mq130812.084253.fits')
-NA_IMAGE = os.path.join(waveunitdir, 'na120701.091058.fits')
-SVSM_IMAGE = os.path.join(waveunitdir, 'svsm_e3100_S2_20110625_1856.fts')
+MEDN_IMAGE = get_test_filepath('waveunit/medn_halph_fl_20050501_074655.header')
+MQ_IMAGE = get_test_filepath('waveunit/mq130812.084253.header')
+NA_IMAGE = get_test_filepath('waveunit/na120701.091058.header')
+SVSM_IMAGE = get_test_filepath('waveunit/svsm_e3100_S2_20110625_1856.header')
