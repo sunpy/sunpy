37c48a92943ecd57c1fa406e113bb54ef49709de
David Stansby <dstansby@gmail.com>
2021-09-30 11:27:22 +0100

    Update docstring

--- sunpy/visualization/limb.py
+++ sunpy/visualization/limb.py
@@ -14,15 +14,17 @@ from sunpy.visualization import wcsaxes_compat
 __all__ = ['draw_limb']
 
 
-def draw_limb(axes, observer, *, rsun=R_sun, resolution=1000, **kwargs):
+@u.quantity_input
+def draw_limb(axes, observer, *, rsun: u.m = R_sun, resolution=1000, **kwargs):
     """
-    Draws the solar limb as seen by the map's observer.
+    Draws the solar limb as seen by the specified observer.
 
-    The limb is a circle for only the simplest plots.  If the coordinate frame
-    of the limb is different from the coordinate frame of the plot axes, not
-    only may the limb not be a true circle, a portion of the limb may be hidden
-    from the observer.  In that case, the circle is divided into visible and
-    hidden segments, represented by solid and dotted lines, respectively.
+    The limb is a circle for only the simplest plots.  If the specified
+    observer of the limb is different from the observer of the coordinate frame
+    of the plot axes, not only may the limb not be a true circle, a portion of
+    the limb may be hidden from the observer.  In that case, the circle is
+    divided into visible and hidden segments, represented by solid and dotted
+    lines, respectively.
 
     Parameters
     ----------
@@ -31,8 +33,8 @@ def draw_limb(axes, observer, *, rsun=R_sun, resolution=1000, **kwargs):
     observer : `astropy.coordinates.SkyCoord`
         Observer coordinate for which the limb is drawn.
     rsun : `~astropy.units.Quantity`
-        Solar radius at which to draw the limb. Defaults to the standard
-        photospheric radius.
+        Solar radius (in physical length units) at which to draw the limb.
+        Defaults to the standard photospheric radius.
     resolution : `int`
         The number of points to use to represent the limb.
 
