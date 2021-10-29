c1e72c5e36fc0009c62e59732fedea04a717d788
David Stansby <dstansby@gmail.com>
2021-10-29 10:27:34 +0000

    Clip maximum connections in CDAWeb client

--- sunpy/net/cdaweb/test/test_cdaweb.py
+++ sunpy/net/cdaweb/test/test_cdaweb.py
@@ -15,3 +15,15 @@ def test_query():
 
     files = Fido.fetch(res)
     assert len(files) == 3
+
+
+@pytest.mark.remote_data
+def test_download():
+    # Check that clipping the max splits at 3 reliably works for CDAWeb
+    # This query has 4 small files
+    trange = a.Time('2021/07/01', '2021/07/05')
+    dataset = a.cdaweb.Dataset('SOLO_L2_MAG-RTN-NORMAL-1-MINUTE')
+    result = Fido.search(trange, dataset)
+    assert len(result[0]) == 4
+    files = Fido.fetch(result)
+    assert len(files.errors) == 0
