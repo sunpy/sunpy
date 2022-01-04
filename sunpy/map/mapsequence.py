e6479d4c03f04479cd39f847e13af9b000d6fb4b
Conor MacBride <conor@macbride.me>
2022-01-04 19:32:35 +0000

    Fix see also references
    Signed-off-by: Conor MacBride <conor@macbride.me>

--- sunpy/map/mapsequence.py
+++ sunpy/map/mapsequence.py
@@ -279,7 +279,7 @@ class MapSequence:
 
         See Also
         --------
-        MapSequenceAnimator
+        `sunpy.visualization.animator.MapSequenceAnimator`
 
         Examples
         --------
