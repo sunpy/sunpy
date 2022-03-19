185573c032874dc91875d579a1935f407004de86
akash5100 <shivamvermashs@gmail.com>
2022-03-19 15:13:17 +0530

    Changes as requested

--- examples/map_transformations/azimuthal_equidistant_projection.py
+++ examples/map_transformations/azimuthal_equidistant_projection.py
@@ -1,11 +1,14 @@
 """
-===============================================
-Azimuthal Equidistant Projection with Reproject
-===============================================
+=====================================================
+Reprojecting to a Map Projection with a Custom Origin
+=====================================================
 
-In this example, we will reproject an AIA image into an
-`azimuthal equidistant projection <https://en.wikipedia.org/wiki/Azimuthal_equidistant_projection>`__.
-You will need v0.6 or higher  of the `reproject` package installed.
+In this example, we show how to reproject a map to a map projection with a
+custom origin.  Here, we choose the target map projection to be the
+`azimuthal equidistant projection <https://en.wikipedia.org/wiki/Azimuthal_equidistant_projection>`__,
+also known as the Postel projection, which has useful properties relative to a
+specified origin of the projection.  If a different map projection is desired,
+modifying this example is straightforward.
 
 """
 
@@ -13,36 +16,33 @@ import matplotlib.pyplot as plt
 
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-from astropy.wcs import WCS
 
 import sunpy.map
 from sunpy.data.sample import AIA_171_IMAGE
 
 ###############################################################################
-# `~sunpy.map.Map` accepts a wide variety of inputs.
-# In this example we load the sample AIA image which we imported earlier from
-# `sunpy.data.sample`.
+# We will use one of the AIA images from the sample data.  We fix the range of
+# values for the Map's normalizer for a prettier image.
 
 aia_map = sunpy.map.Map(AIA_171_IMAGE)
-
-###############################################################################
-# Negative value pixels can appear that lead to ugly looking images.
-# This can be fixed by setting the lower limit of the normalization.
-
 aia_map.plot_settings['norm'].vmin = 0
 aia_map.plot_settings['norm'].vmax = 10000
 
 ###############################################################################
-# Next, we define the origin of the postel projection using an `astropy.coordinates.SkyCoord` object.
-# `~astropy.coordinates.Skycoord` provides a flexible interface for celestial coordinate representation, manipulation, and transformation between systems.
+# Next, we create a `~astropy.coordinates.SkyCoord` to define the custom origin
+# of the map projection.  Here, we are going to center the projection at the
+# helioprojective coordinates of a particular active region.  We want our map
+# projection to be in heliographic Stonyhurst coordinates, so we transform the
+# origin coordinate accordingly.
 
 origin_hpc = SkyCoord(735*u.arcsec, -340*u.arcsec, frame=aia_map.coordinate_frame)
 origin = origin_hpc.heliographic_stonyhurst
 
 ###############################################################################
-# We can then use our origin coordinate to create a FITS-WCS header.
-# From this new header, we can derive the world coordinate system (WCS)
-# of our postel projection.
+# We then create a FITS-WCS header that includes our custom origin coordinate.
+# The azimuthal equidistant projection is specified by the code `"ARC"`.
+# See :ref:`astropy:supported_projections` for the projection codes for other
+# projections.
 
 out_shape = (750, 750)
 out_header = sunpy.map.make_fitswcs_header(
@@ -51,27 +51,21 @@ out_header = sunpy.map.make_fitswcs_header(
     scale=[0.4, 0.4]*u.deg/u.pix,
     projection_code="ARC"
 )
-out_wcs = WCS(out_header)
 
 ###############################################################################
-# Now, we can use the WCS of postel projection to reproject the AIA map.
+# We reproject the map to our FITS-WCS header and copy over the plot settings.
 
-out_map = aia_map.reproject_to(out_wcs)
+out_map = aia_map.reproject_to(out_header)
 out_map.plot_settings = aia_map.plot_settings
 
 ###############################################################################
 
-# sphinx_gallery_defer_figures
-
-# Finally, we'll plot both our original and reprojected images.
+# Finally, we plot both the original and reprojected maps side by side.
 
 fig = plt.figure(figsize=(8, 4))
 
-###############################################################################
-
-# sphinx_gallery_defer_figures
-
-# Plot the original AIA map
+# Plot the original AIA map, with the active region circled in red and the
+# heliographic grid and solar limb in blue.
 
 ax = fig.add_subplot(1, 2, 1, projection=aia_map)
 aia_map.plot(axes=ax)
@@ -79,14 +73,15 @@ aia_map.draw_grid(axes=ax, color='blue')
 aia_map.draw_limb(axes=ax, color='blue')
 ax.plot_coord(origin, 'o', color='red', fillstyle='none', markersize=20)
 
+# sphinx_gallery_defer_figures
+
 ###############################################################################
-# Plot the reprojected AIA map
+# Plot the reprojected AIA map, again with the active region circled in red and
+# the heliographic grid and solar limb in blue.
 
 ax = fig.add_subplot(1, 2, 2, projection=out_map)
-out_map.plot(axes=ax, autoalign=True, annotate=False)
+out_map.plot(axes=ax)
 out_map.draw_grid(axes=ax, color='blue')
 out_map.draw_limb(axes=ax, color='blue')
-ax.set_xlim(-0.5, out_shape[1] - 0.5)
-ax.set_ylim(-0.5, out_shape[0] - 0.5)
 ax.set_title('Postel projection centered at ROI', y=-0.1)
 plt.show()
