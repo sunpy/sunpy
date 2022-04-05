df37cdfb2aea360f99837ed579ca4b88c0563635
Akash Verma <akashzsh08@gmail.com>
2022-04-05 12:13:37 +0530

    Update Timeseries Docs  (#6024)
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/timeseries/timeseriesbase.py
+++ sunpy/timeseries/timeseriesbase.py
@@ -35,8 +35,8 @@ class GenericTimeSeries:
 
     Parameters
     ----------
-    data : `~pandas.DataFrame`
-        A `pandas.DataFrame` representing one or more fields as a function of time.
+    data : `~pandas.DataFrame` or `numpy.array`
+        A `pandas.DataFrame` or `numpy.array` representing one or more fields as a function of time.
     meta : `~sunpy.timeseries.metadata.TimeSeriesMetaData`, optional
         The metadata giving details about the time series data/instrument.
         Defaults to `None`.
