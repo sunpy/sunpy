bce1d047392cb13e9adeb6ec796cbf1e48f60447
Nabil Freij <nabil.freij@gmail.com>
2022-04-05 22:59:43 -0700

    CR comments

--- examples/plotting/grid_plotting.py
+++ examples/plotting/grid_plotting.py
@@ -15,19 +15,20 @@ import sunpy.map
 
 ################################################################################
 # Let's start with a sample AIA image.
+
 aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ################################################################################
 # Now we can plot the image, and add grids in heliographic Stonyhurst and
 # heliographic Carrington coordinates.
+
 fig = plt.figure()
 ax1 = fig.add_subplot(121, projection=aia_map)
-aia_map.plot(axes=ax1)
+aia_map.plot(axes=ax1, clip_interval=(1, 99.9)*u.percent)
 stonyhurst_grid = aia_map.draw_grid(axes=ax1, system='stonyhurst')
 
-
 ax2 = fig.add_subplot(122, projection=aia_map)
-aia_map.plot(axes=ax2)
+aia_map.plot(axes=ax2, clip_interval=(1, 99.9)*u.percent)
 carrington_grid = aia_map.draw_grid(axes=ax2, system='carrington')
 
 for ax in [ax1, ax2]:
@@ -42,7 +43,6 @@ for ax in [ax1, ax2]:
 
 stonyhurst_grid['lon'].set_ticks([-30, -45, -60] * u.deg)
 carrington_grid['lon'].set_ticks([-45, -60, -75] * u.deg)
-
 
 fig.subplots_adjust(wspace=0.5)
 
