4cf2588b6a41d3d30fe6ee7e75b6508a8d2bdac0
Nabil Freij <nabil.freij@gmail.com>
2022-04-12 21:18:22 -0700

    Merge pull request #6067 from nabobalis/main

--- sunpy/net/jsoc/tests/test_jsoc.py
+++ sunpy/net/jsoc/tests/test_jsoc.py
@@ -1,4 +1,3 @@
-import os
 import tempfile
 
 import pytest
@@ -22,14 +21,12 @@ def client():
 
 
 @pytest.fixture
-def jsoc_response_double():
+def jsoc_response_double(jsoc_test_email):
     resp = JSOCResponse([{'T_REC': '2011/01/01T00:00', 'INSTRUME': 'AIA'},
                          {'T_REC': '2011/01/02T00:00', 'INSTRUME': 'AIA'}])
-
     resp.query_args = [{'start_time': astropy.time.Time("2020-01-01T01:00:36.000"),
                         'end_time': astropy.time.Time("2020-01-01T01:00:38.000"),
-                        'series': 'hmi.M_45s', 'notify': 'jsoc@cadair.com'}]
-
+                        'series': 'hmi.M_45s', 'notify': pytest.jsoc_test_email}]
     return resp
 
 
@@ -65,17 +62,6 @@ def test_query(client):
     assert len(Jresp) == 2
 
 
-@pytest.mark.remote_data
-def test_post_pass(client):
-    responses = client.search(
-        a.Time('2020/1/1T00:00:00', '2020/1/1T00:00:45'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
-    aa = client.request_data(responses)
-    tmpresp = aa._d
-    assert tmpresp['protocol'] == 'fits'
-    assert tmpresp['method'] == 'url'
-
-
 def test_show(client):
     jdict = {'TELESCOP': ['SDO/HMI', 'SDO/AIA'], 'CAR_ROT': [2145, 2145]}
     responses = JSOCResponse(astropy.table.Table(jdict))
@@ -83,24 +69,6 @@ def test_show(client):
     assert isinstance(showtable, astropy.table.Table)
     assert showtable.colnames == ['TELESCOP']
     assert showtable['TELESCOP'][0] == 'SDO/HMI'
-
-
-@pytest.mark.remote_data
-def test_post_wavelength(client):
-    responses = client.search(
-        a.Time('2020/07/30T13:30:00', '2020/07/30T14:00:00'),
-        a.jsoc.Series('aia.lev1_euv_12s'), a.Wavelength(193 * u.AA) |
-        a.Wavelength(335 * u.AA), a.jsoc.Notify('jsoc@cadair.com'))
-    aa = client.request_data(responses)
-    [r.wait() for r in aa]
-    tmpresp = aa[0]._d
-    assert tmpresp['protocol'] == 'fits'
-    assert tmpresp['method'] == 'url'
-    assert tmpresp['count'] == '302'
-    tmpresp = aa[1]._d
-    assert tmpresp['protocol'] == 'fits'
-    assert tmpresp['method'] == 'url'
-    assert tmpresp['count'] == '302'
 
 
 @pytest.mark.remote_data
@@ -122,10 +90,10 @@ def test_post_wave_series(client):
 
 
 @pytest.mark.remote_data
-def test_wait_get(client):
+def test_wait_get(client, jsoc_test_email):
     responses = client.search(
         a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email))
     path = tempfile.mkdtemp()
     res = client.fetch(responses, path=path)
     assert isinstance(res, Results)
@@ -133,21 +101,10 @@ def test_wait_get(client):
 
 
 @pytest.mark.remote_data
-def test_get_request(client):
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
-    bb = client.request_data(responses)
-    path = tempfile.mkdtemp()
-    aa = client.get_request(bb, path=path)
-    assert isinstance(aa, Results)
-
-
-@pytest.mark.remote_data
-def test_get_request_tar(client):
+def test_get_request_tar(client, jsoc_test_email):
     responses = client.search(
         a.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email))
     bb = client.request_data(responses, method='url-tar')
     bb.wait()
     path = tempfile.mkdtemp()
@@ -156,7 +113,7 @@ def test_get_request_tar(client):
 
     responses = client.search(
         a.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email),
         a.jsoc.Protocol('as-is'))
     bb = client.request_data(responses, method='url-tar')
     bb.wait()
@@ -286,101 +243,35 @@ def test_make_recordset(client):
 
 
 @pytest.mark.remote_data
-def test_request_data_error(client):
+def test_request_data_error(client, jsoc_test_email):
     responses = client.search(
         a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email),
         a.jsoc.Protocol('foo'))
     with pytest.raises(TypeError):
         client.request_data(responses)
 
 
 @pytest.mark.remote_data
-def test_request_data_protocol(client):
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
-    req = client.request_data(responses)
-    req.wait()
-    assert req._d['method'] == 'url'
-    assert req._d['protocol'] == 'fits'
-
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
-        a.jsoc.Protocol('fits'))
-    req = client.request_data(responses)
-    req.wait()
-    assert req._d['method'] == 'url'
-    assert req._d['protocol'] == 'fits'
-
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
-        a.jsoc.Protocol('as-is'))
-    req = client.request_data(responses)
-    req.wait()
-    assert req._d['method'] == 'url_quick'
-    assert req._d['protocol'] == 'as-is'
-
-
-@pytest.mark.remote_data
-def test_request_data_method(client):
+def test_request_data_method(client, jsoc_test_email):
     responses = client.search(
         a.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email))
     req = client.request_data(responses, method='url-tar')
-    req.wait()
-    assert req._d['method'] == 'url-tar'
-    assert req._d['protocol'] == 'fits'
-
-    responses = client.search(
-        a.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
-        a.jsoc.Protocol('as-is'))
-    req = client.request_data(responses, method='url-tar')
-    req.wait()
-    assert req._d['method'] == 'url-tar'
-    assert req._d['protocol'] == 'as-is'
-
-
-@pytest.mark.remote_data
-def test_check_request(client):
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:00:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
-    req = client.request_data(responses)
     req.wait()
     assert req.status == 0
+    assert req._d['method'] == 'url-tar'
+    assert req._d['protocol'] == 'fits'
 
-
-@pytest.mark.flaky(reruns_delay=30)
-@pytest.mark.remote_data
-def test_results_filenames(client):
     responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:01:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'))
-    path = tempfile.mkdtemp()
-    files = client.fetch(responses, path=path)
-    assert isinstance(files, Results)
-    assert len(files) == len(responses)
-    for hmiurl in files:
-        assert os.path.isfile(hmiurl)
-
-
-@pytest.mark.flaky(reruns_delay=30)
-@pytest.mark.remote_data
-def test_results_filenames_as_is(tmp_path, client):
-    responses = client.search(
-        a.Time('2020/1/1T1:00:36', '2020/1/1T01:01:38'),
-        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify('jsoc@cadair.com'),
+        a.Time('2012/1/1T1:00:36', '2012/1/1T01:00:38'),
+        a.jsoc.Series('hmi.M_45s'), a.jsoc.Notify(pytest.jsoc_test_email),
         a.jsoc.Protocol('as-is'))
-    assert len(responses) == 2
-    files = client.fetch(responses, path=tmp_path)
-    assert isinstance(files, Results)
-    assert len(files) == len(responses)
-    for hmiurl in files:
-        assert os.path.isfile(hmiurl)
+    req = client.request_data(responses, method='url-tar')
+    req.wait()
+    assert req.status == 0
+    assert req._d['method'] == 'url-tar'
+    assert req._d['protocol'] == 'as-is'
 
 
 def test_can_handle_query_no_series(client):
@@ -399,7 +290,7 @@ def test_jsoc_attrs(client):
 
 @pytest.mark.flaky(reruns_delay=30)
 @pytest.mark.remote_data
-def test_jsoc_cutout_attrs(client):
+def test_jsoc_cutout_attrs(client, jsoc_test_email):
     m_ref = sunpy.map.Map(sunpy.data.test.get_test_filepath('aia_171_level1.fits'))
     cutout = a.jsoc.Cutout(
         SkyCoord(-500*u.arcsec, -275*u.arcsec, frame=m_ref.coordinate_frame),
@@ -410,7 +301,7 @@ def test_jsoc_cutout_attrs(client):
         a.Time(m_ref.date, m_ref.date + 1 * u.min),
         a.Wavelength(171*u.angstrom),
         a.jsoc.Series.aia_lev1_euv_12s,
-        a.jsoc.Notify('jsoc@cadair.com'),  # Put your email here
+        a.jsoc.Notify(pytest.jsoc_test_email),
         a.jsoc.Segment.image,
         cutout,
     )
