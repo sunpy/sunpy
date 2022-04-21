fe8ce53a3c388de1d0753283e0c46f8d9db709c5
Stuart Mumford <stuart@cadair.com>
2022-04-21 10:20:21 +0000

    Move to not deprecated setuptools API

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ import os
 from itertools import chain
 
 from extension_helpers import get_extensions
-from setuptools.config import read_configuration
+
+try:
+    # Recommended for setuptools 61.0.0+
+    # (though may disappear in the future)
+    from setuptools.config.setupcfg import read_configuration
+except ImportError:
+    from setuptools.config import read_configuration
 
 ################################################################################
 # Programmatically generate some extras combos.
