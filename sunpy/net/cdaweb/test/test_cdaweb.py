6600c2992c61a452a8edfab95c5cd0577f763335
David Stansby <dstansby@gmail.com>
2022-04-23 19:08:16 +0100

    Use vcrpy for CDAWEB test

--- sunpy/net/cdaweb/test/test_cdaweb.py
+++ sunpy/net/cdaweb/test/test_cdaweb.py
@@ -5,16 +5,13 @@ from sunpy.net import Fido
 from sunpy.net.cdaweb.attrs import Dataset
 
 
-@pytest.mark.remote_data
+@pytest.mark.vcr()
 def test_query():
     res = Fido.search(a.Time('2018-11-01', '2018-11-01 01:00:00'),
                       Dataset('WI_H1_SWE') | Dataset('WI_H5_SWE'))
     assert len(res) == 2
     assert len(res[0]) == 1
     assert len(res[1]) == 2
-
-    files = Fido.fetch(res)
-    assert len(files) == 3
 
 
 @pytest.mark.remote_data
@@ -27,3 +24,4 @@ def test_download():
     assert len(result[0]) == 4
     files = Fido.fetch(result)
     assert len(files.errors) == 0
+    assert len(files) == 4
