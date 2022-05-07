fdb76a08afc957701f9c15760f63e123439a3d35
akash5100 <shivamvermashs@gmail.com>
2022-05-07 20:11:54 +0530

    initial commit to download packages

--- tools/update_extern.py
+++ tools/update_extern.py
@@ -4,49 +4,89 @@
 This script is used to update the files in `sunpy/extern` while making new releases.
 """
 
-# TODO:
-# [x] Get list of all the packages imported in sunpy/extern
-# [] Search for the source distribution of each package in `USER/home/sunpy/extern`
-# [] Extract the source distribution to a temporary directory
-# [] Update the python files in `sunpy/extern`
-
-
-import ast
+import os
+import shutil
+from zipfile import ZipFile
+import requests
 from pathlib import Path
 
 # get parent directory of sunpy
 SUNPY_DIR = Path(__file__).parent.parent
 
 
-def visit_import(node):
-    for name in node.names:
-        modules.append(name.name.split(".")[0])
+# List of packages to update
+PACKAGES = ["appdirs", "distro", "inflect", "parse"]
+
+# Dictionary to store the authors of the packages
+AUTHORS = {
+    "appdirs": "ActiveState",
+    "distro": "python-distro",
+    "inflect": "jaraco",
+    "parse": "r1chardj0n3s",
+}
 
 
-def visit_from(node):
-    # if node.module is missing it's a "from . import ..." statement
-    # if level > 0 it's a "from .submodule import ..." statement
-    if node.module is not None and node.level == 0:
-        modules.append(node.module.split(".")[0])
+def get_latest_version(package):
+    """
+    Get the latest version of the package.
+    """
+    url = f"https://api.github.com/repos/{AUTHORS[package]}/{package}/releases/latest"
+    response = requests.get(url)
+    return response.json()["tag_name"]
 
 
-# get all the modules imported in sunpy/extern
-file_names = ["appdirs", "distro", "inflect", "parse"]
+def get_download_url(package, version):
+    """
+    Get the download url for the package.
+    """
+    url = f"https://github.com/{AUTHORS[package]}/{package}/archive/refs/tags/{version}.zip"
+    return url
 
-modules = list()
 
-for i in file_names:
-    with open(SUNPY_DIR/f"sunpy/extern/{i}.py", "r") as f:
-        # print(f"Reading {i}.py")
-        tree = ast.parse(f.read())
-        for node in ast.walk(tree):
-            if isinstance(node, ast.Import):
-                visit_import(node)
-            elif isinstance(node, ast.ImportFrom):
-                visit_from(node)
+def download_package(url, package):
+    """
+    Download the package.
+    """
+    # Make a temporaty directory
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
 
-modules = set(modules)
-print(modules)
+    response = requests.get(url, stream=True)
+    with open(f"tmp/{package}.zip", "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
 
-# Output:
-# {'functools', 'win32com', 'logging', '__future__', 'subprocess', 'typing', 'datetime', 'winreg', 'platform', 'array', 'os', 'json', 'shlex', 'sys', 'win32api', 'com', 'warnings', 'decimal', 'ast', 'ctypes', 're', '_winreg', 'argparse'}
+
+def unzip(filename):
+    """
+    Extract the package.
+    """
+    with ZipFile(filename, "r") as zip_file:
+        zip_file.extractall("tmp")
+    # Remove the zip file
+    os.remove(filename)
+
+
+def extract_appdirs(destination):
+    """
+    Extract the appdirs package.
+    """
+    # Get appdirs.py from the folder
+    print(destination)
+
+
+if __name__ == "__main__":
+    # for package in PACKAGES:
+    # # get the url of the package
+    # url = get_download_url(package, get_latest_version(package))
+
+    # # download the package
+    # download_package(url, package)
+
+    # Open the zip file
+    # unzip(f"tmp/{package}.zip")
+
+    # Extract the required files one by one
+    extract_appdirs(SUNPY_DIR / "appdirs")
+    # shutil.unpack_archive(f"{package}.zip", SUNPY_DIR / "temp")
