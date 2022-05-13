a2d1fa789a38577eed382d546ec1f346e2524e7b
Nabil Freij <freij@baeri.org>
2022-05-13 14:10:57 -0700

    tidy up of some io tests

--- sunpy/io/tests/test_fits.py
+++ sunpy/io/tests/test_fits.py
@@ -7,21 +7,17 @@ import pytest
 
 import astropy.io.fits as fits
 
-import sunpy.data.test
-import sunpy.io._fits
 from sunpy.data.test import get_test_filepath, test_data_filenames
 from sunpy.data.test.waveunit import MEDN_IMAGE, MQ_IMAGE, NA_IMAGE, SVSM_IMAGE
-from sunpy.io._fits import extract_waveunit, get_header, header_to_fits
-from sunpy.io.fits import extract_waveunit, format_comments_and_history, get_header, header_to_fits
+from sunpy.io import _fits
 from sunpy.util import MetaDict, SunpyMetadataWarning
 
 TEST_RHESSI_IMAGE = get_test_filepath('hsi_image_20101016_191218.fits')
 TEST_AIA_IMAGE = get_test_filepath('aia_171_level1.fits')
 TEST_EIT_HEADER = get_test_filepath('EIT_header/efz20040301.000010_s.header')
 TEST_SWAP_HEADER = get_test_filepath('SWAP/resampled1_swap.header')
-
-# Some of the tests images contain an invalid BLANK keyword; ignore the warning
-# raised by this
+# Some of the tests images contain an invalid BLANK keyword
+# ignore the warning raised by this
 pytestmark = pytest.mark.filterwarnings("ignore:Invalid 'BLANK' keyword in header")
 
 
@@ -33,7 +29,7 @@ pytestmark = pytest.mark.filterwarnings("ignore:Invalid 'BLANK' keyword in heade
      (TEST_RHESSI_IMAGE, range(0, 2), 2)]
 )
 def test_read_hdus(fname, hdus, length):
-    pairs = sunpy.io._fits.read(fname, hdus=hdus)
+    pairs = _fits.read(fname, hdus=hdus)
     assert len(pairs) == length
 
 
@@ -50,32 +46,32 @@ def test_read_hdus(fname, hdus, length):
 )
 def test_extract_waveunit(fname, waveunit):
     if Path(fname).suffix == '.header':
-        header = format_comments_and_history(fits.Header.fromtextfile(fname))
+        header = _fits.format_comments_and_history(fits.Header.fromtextfile(fname))
     else:
-        header = get_header(fname)[0]
-    waveunit = extract_waveunit(header)
+        header = _fits.get_header(fname)[0]
+    waveunit = _fits.extract_waveunit(header)
     assert waveunit is waveunit
 
 
 def test_simple_write(tmpdir):
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
     outfile = tmpdir / "test.fits"
-    sunpy.io._fits.write(str(outfile), data, header)
+    _fits.write(str(outfile), data, header)
     assert outfile.exists()
 
 
 def test_extra_comment_write(tmpdir):
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
     header["KEYCOMMENTS"]["TEST"] = "Hello world"
     outfile = tmpdir / "test.fits"
-    sunpy.io._fits.write(str(outfile), data, header)
+    _fits.write(str(outfile), data, header)
     assert outfile.exists()
 
 
 def test_simple_write_compressed(tmpdir):
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
     outfile = tmpdir / "test.fits"
-    sunpy.io._fits.write(str(outfile), data, header, hdu_type=fits.CompImageHDU)
+    _fits.write(str(outfile), data, header, hdu_type=fits.CompImageHDU)
     assert outfile.exists()
     with fits.open(str(outfile)) as hdul:
         assert len(hdul) == 2
@@ -85,11 +81,11 @@ def test_simple_write_compressed(tmpdir):
 def test_simple_write_compressed_difftypeinst(tmpdir):
     # `hdu_type=fits.CompImageHDU` and `hdu_type=fits.CompImageHDU()`
     # should produce identical FITS files
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
     outfile_type = str(tmpdir / "test_type.fits")
     outfile_inst = str(tmpdir / "test_inst.fits")
-    sunpy.io._fits.write(outfile_type, data, header, hdu_type=fits.CompImageHDU)
-    sunpy.io._fits.write(outfile_inst, data, header, hdu_type=fits.CompImageHDU())
+    _fits.write(outfile_type, data, header, hdu_type=fits.CompImageHDU)
+    _fits.write(outfile_inst, data, header, hdu_type=fits.CompImageHDU())
     assert fits.FITSDiff(outfile_type, outfile_inst, ignore_comments=['PCOUNT']).identical
 
 
@@ -99,7 +95,7 @@ def test_simple_write_compressed_difftypeinst(tmpdir):
      ({'quantize_level': -32}, True)]
 )
 def test_simple_write_compressed_instance(tmpdir, kwargs, should_fail):
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
     outfile = tmpdir / "test.fits"
 
     # Ensure HDU instance is used correctly
@@ -107,7 +103,7 @@ def test_simple_write_compressed_instance(tmpdir, kwargs, should_fail):
     hdu.header['HELLO'] = 'world'  # should be in the written file
     hdu.header['TELESCOP'] = 'other'  # should be replaced with 'SDO/AIA'
     hdu.header['NAXIS'] = 5  # should be replaced with 2
-    sunpy.io._fits.write(str(outfile), data, header, hdu_type=hdu)
+    _fits.write(str(outfile), data, header, hdu_type=hdu)
     assert outfile.exists()
     with fits.open(str(outfile)) as hdul:
         assert len(hdul) == 2
@@ -130,7 +126,7 @@ def test_write_with_metadict_header_astropy(tmpdir):
     meta_header = MetaDict(OrderedDict(header))
     temp_file = tmpdir / "temp.fits"
     with pytest.warns(SunpyMetadataWarning, match='The meta key comment is not valid ascii'):
-        sunpy.io._fits.write(str(temp_file), data, meta_header)
+        _fits.write(str(temp_file), data, meta_header)
     assert temp_file.exists()
     fits_file.close()
 
@@ -148,15 +144,15 @@ def test_fitsheader():
             fits_file = fits.open(ffile)
             fits_file.verify("fix")
             meta_header = MetaDict(OrderedDict(fits_file[0].header))
-            sunpy.io._fits.header_to_fits(meta_header)
+            _fits.header_to_fits(meta_header)
             fits_file.close()
 
 
 def test_warn_nonascii():
     # Check that a non-ascii character raises a warning and not an error
     with pytest.warns(SunpyMetadataWarning, match='not valid ascii'):
-        fits = header_to_fits({'bad': 'test\t',
-                               'good': 'test'})
+        fits = _fits.header_to_fits({'bad': 'test\t',
+                                     'good': 'test'})
     assert 'GOOD' in fits.keys()
     assert 'BAD' not in fits.keys()
 
@@ -164,8 +160,8 @@ def test_warn_nonascii():
 def test_warn_nan():
     # Check that a NaN value raises a warning and not an error
     with pytest.warns(SunpyMetadataWarning, match='has a NaN value'):
-        fits = header_to_fits({'bad': float('nan'),
-                               'good': 1.0})
+        fits = _fits.header_to_fits({'bad': float('nan'),
+                                     'good': 1.0})
     assert 'GOOD' in fits.keys()
     assert 'BAD' not in fits.keys()
 
@@ -173,21 +169,16 @@ def test_warn_nan():
 def test_warn_longkey():
     # Check that a key that is too long raises a warning and not an error
     with pytest.warns(SunpyMetadataWarning, match='The meta key badlongkey is too long'):
-        fits = header_to_fits({'badlongkey': 'test',
-                               'goodkey': 'test'})
+        fits = _fits.header_to_fits({'badlongkey': 'test',
+                                     'goodkey': 'test'})
     assert 'GOODKEY' in fits.keys()
     assert 'BADLONGKEY' not in fits.keys()
 
 
 def test_read_memmap():
-    # Check that the FITS reader can read a memmap without memmap argument by default
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE)[0]
+    data, _ = _fits.read(TEST_AIA_IMAGE, memmap=True)[0]
+    assert data.base is not None
     assert isinstance(data.base, mmap.mmap)
 
-    # Check that memmap=True does the same thing
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE, memmap=True)[0]
-    assert isinstance(data.base, mmap.mmap)
-
-    # Check that memmap=False doesn't do memory mapping
-    data, header = sunpy.io._fits.read(TEST_AIA_IMAGE, memmap=False)[0]
+    data, _ = _fits.read(TEST_AIA_IMAGE, memmap=False)[0]
     assert data.base is None
--- sunpy/io/tests/test_jp2.py
+++ sunpy/io/tests/test_jp2.py
@@ -2,46 +2,31 @@ import numpy as np
 
 from sunpy.data.test import get_test_filepath
 from sunpy.io.header import FileHeader
-from sunpy.map import GenericMap, Map
+from sunpy.io.jp2 import get_header, read
 from sunpy.tests.helpers import skip_glymur
 
 AIA_193_JP2 = get_test_filepath("2013_06_24__17_31_30_84__SDO_AIA_AIA_193.jp2")
 
 
 @skip_glymur
-def test_read_data():
-    """
-    Tests the reading of the JP2 data.
-    """
-    import glymur
-    data = glymur.Jp2k(AIA_193_JP2)[...]
+def test_read():
+    data, header = read(AIA_193_JP2)[0]
     assert isinstance(data, np.ndarray)
+    assert data.shape == (4096, 4096)
+    assert isinstance(header, FileHeader)
 
 
 @skip_glymur
 def test_read_header():
-    """
-    Tests the reading of the JP2 header.
-    """
-    from sunpy.io.jp2 import get_header
     header = get_header(AIA_193_JP2)[0]
     assert isinstance(header, FileHeader)
 
 
 @skip_glymur
-def test_read_file():
-    """
-    Tests the reading of the complete JP2 file and its conversion into a SunPy
-    map.
-    """
-    map_ = Map(AIA_193_JP2)
-    assert isinstance(map_, GenericMap)
-
-
-@skip_glymur
 def test_read_memmap():
-    """
-    Passing memmap as keyword argument should not raise an error.
-    """
-    map_ = Map(AIA_193_JP2, memmap=True)
-    assert isinstance(map_, GenericMap)
+    data, _ = read(AIA_193_JP2, memmap=True)[0]
+    # The data is shared, with what, I am unclear
+    assert data.base is not None
+    # Keyword is not passed in the function call
+    data, _ = read(AIA_193_JP2, memmap=False)[0]
+    assert data.base is not None
