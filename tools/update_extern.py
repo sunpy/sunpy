227e7df38e03520cda76981897e2da2fed34fb62
Akash Verma <akashzsh08@gmail.com>
2022-05-14 11:10:41 +0530

    Update tools/update_extern.py
    Co-authored-by: Conor MacBride <conor@macbride.me>

--- tools/update_extern.py
+++ tools/update_extern.py
@@ -12,10 +12,11 @@ SUNPY_DIR = Path(__file__).parent.parent
 
 
 PACKAGES = {
-    "appdirs": ["ActiveState", "appdirs.py"],
-    "distro": ["python-distro", "src/distro/distro.py"],
-    "inflect": ["jaraco", "inflect/__init__.py"],
-    "parse": ["r1chardj0n3s", "parse.py"],
+    "appdirs": ["ActiveState", "appdirs", "appdirs.py"],
+    "distro": ["python-distro", "distro", "src/distro/distro.py"],
+    "inflect": ["jaraco", "inflect", "inflect/__init__.py"],
+    "modest_image": ["glue-viz", "glue", "glue/external/modest_image.py"],
+    "parse": ["r1chardj0n3s", "parse", "parse.py"],
 }
 
 
