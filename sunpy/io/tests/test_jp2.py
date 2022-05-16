ba107ffac7f297e3b2f98d276ea664232c0a5d54
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2022-05-16 10:54:56 -0400

    Add simple jp2 write test

--- sunpy/io/tests/test_jp2.py
+++ sunpy/io/tests/test_jp2.py
@@ -1,12 +1,14 @@
 import numpy as np
+import sunpy.io.jp2 as jp2
 
 from sunpy.data.test import get_test_filepath
 from sunpy.io.header import FileHeader
 from sunpy.io.jp2 import get_header, read
+from sunpy.io import _fits
 from sunpy.tests.helpers import skip_glymur
 
 AIA_193_JP2 = get_test_filepath("2013_06_24__17_31_30_84__SDO_AIA_AIA_193.jp2")
-
+TEST_AIA_IMAGE = get_test_filepath('aia_171_level1.fits')
 
 @skip_glymur
 def test_read():
@@ -30,3 +32,10 @@ def test_read_memmap():
     # Keyword is not passed in the function call
     data, _ = read(AIA_193_JP2, memmap=False)[0]
     assert data.base is not None
+
+def test_simple_write(tmpdir):
+    data, header = _fits.read(TEST_AIA_IMAGE)[0]
+    outfile = tmpdir / "test.jp2"
+    jp2.write(str(outfile), data, header)
+    assert outfile.exists()
+
