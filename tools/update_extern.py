acd2b0676763cc9b3f946832781dcb066ac76893
akash5100 <shivamvermashs@gmail.com>
2022-05-25 00:33:52 +0530

    Use new endpoint to update extern packages

--- tools/update_extern.py
+++ tools/update_extern.py
@@ -2,14 +2,9 @@
 Updates all the libraries in ``sunpy/extern``
 """
 
-import os
 import json
-import shutil
-import tempfile
 import urllib.request
-from parfive import Downloader
 from pathlib import Path
-from zipfile import ZipFile
 
 SUNPY_EXTERN_DIR = Path(__file__).parent.parent / "sunpy" / "extern"
 
@@ -53,17 +48,8 @@ def download_github_file(user: str, repo: str, src: Path, dest: Path):
     with open(dest, "wb") as f:
         print(f"Updating {user}/{repo}:refs/tags/{version}")
         f.write(response.read())
-    # zip_file = download_package(user, repo)
-    # temp_dir = tempfile.mkdtemp()
-    # with ZipFile(zip_file, "r") as f:
-    #     folder = Path(f.namelist()[0]).parts[0]
-    #     ext = f.extract(f"{folder}/{src}", temp_dir)
-    # dest = Path(dest)
-    # if dest.exists() and dest.is_file():
-    #     os.remove(dest)
-    # shutil.move(ext, dest)
 
 
 if __name__ == "__main__":
-    for package, (user, repo, src) in LICENSES.items():
+    for package, (user, repo, src) in PACKAGES.items():
         download_github_file(user, repo, src, SUNPY_EXTERN_DIR / package)
