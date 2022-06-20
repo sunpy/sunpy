fe01e1d8110939f23d857d61583b64f172f37846
Matt Wentzel-Long <88404225+mwhv2@users.noreply.github.com>
2022-06-20 14:20:11 -0400

    Update draw
    Fix doctest directive and some formatting issues

--- sunpy/visualization/draw.py
+++ sunpy/visualization/draw.py
@@ -15,8 +15,8 @@ __all__ = ["draw_equator", "draw_prime_meridian"]
 
 def draw_equator(axes, resolution=300, **kwargs):
     """
-    Draws the equator as seen by the axes observer. 
-    
+    Draws the equator as seen by the axes observer.
+
     Parameters
     ----------
     axes : `matplotlib.axes.Axes`
@@ -30,14 +30,14 @@ def draw_equator(axes, resolution=300, **kwargs):
     >>> from sunpy.visualization import draw
     >>> import sunpy.data.sample   # doctest: +REMOTE_DATA
     >>> aia = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)   # doctest: +REMOTE_DATA
-    >>> aia.plot()   # doctest: +REMOTE_DATA
-    >>> draw.draw_equator(aia._check_axes(None))   # doctest: +REMOTE_DATA
+    >>> aia.plot()   # doctest: +SKIP
+    >>> draw.draw_equator(aia._check_axes(None))   # doctest: +SKIP
     """
     if not wcsaxes_compat.is_wcsaxes(axes):
         raise ValueError('axes must be a WCSAxes')
     axes_frame = axes._transform_pixel2world.frame_out
     observer = axes_frame.observer
-    
+
     c_kw = {'linewidth': 1,
             'color': 'white',
             'zorder': 100}
@@ -45,10 +45,10 @@ def draw_equator(axes, resolution=300, **kwargs):
 
     transform = axes.get_transform(axes_frame)
     c_kw.setdefault('transform', transform)
-    
+
     lat = 0*u.deg
     lat0 = SkyCoord(np.linspace(-90, 90, resolution) * u.deg,
-                    np.ones(resolution) * lat, 
+                    np.ones(resolution) * lat,
                     frame=HeliographicStonyhurst)
     lat0 = lat0.transform_to(axes_frame)
 
@@ -57,17 +57,17 @@ def draw_equator(axes, resolution=300, **kwargs):
     is_visible = lat0.spherical.distance <= reference_dist
     vis = np.where(is_visible == True)
     visible = lat0[vis]
-    
-    equator = Line2D(visible.Tx.deg, visible.Ty.deg, 
+
+    equator = Line2D(visible.Tx.deg, visible.Ty.deg,
                      **c_kw)
     axes.add_line(equator)
     return equator
 
 def draw_prime_meridian(axes, resolution=300, **kwargs):
     """
-    Draws the prime meridian as seen by the axes observer. 
+    Draws the prime meridian as seen by the axes observer.
     Any part that is hidden is not plotted.
-    
+
     Parameters
     ----------
     axes : `matplotlib.axes.Axes`
@@ -75,12 +75,11 @@ def draw_prime_meridian(axes, resolution=300, **kwargs):
     resolution : `int`
         The number of points used to represent the prime meridian.
     """
-    from matplotlib.transforms import IdentityTransform
     if not wcsaxes_compat.is_wcsaxes(axes):
         raise ValueError('axes must be a WCSAxes')
     axes_frame = axes._transform_pixel2world.frame_out
     observer = axes_frame.observer
-    
+
     c_kw = {'linewidth': 1,
             'color': 'white',
             'zorder': 100}
@@ -88,20 +87,20 @@ def draw_prime_meridian(axes, resolution=300, **kwargs):
 
     transform = axes.get_transform(axes_frame)
     c_kw.setdefault('transform', transform)
-    
+
     lon = 0*u.deg
     lon0 = SkyCoord(np.ones(resolution) * lon,
-                    np.linspace(-90, 90, resolution) * u.deg, 
+                    np.linspace(-90, 90, resolution) * u.deg,
                     frame=HeliographicStonyhurst)
     lon0 = lon0.transform_to(axes_frame)
-    
+
     rsun = axes_frame.rsun
     reference_dist = np.sqrt(observer.radius**2 - rsun**2)
     is_visible = lon0.spherical.distance <= reference_dist
-    vis = np.where(is_visible == True)[0]
+    vis = np.where(is_visible == True)
     visible = lon0[vis]
-    
-    prime_meridian = Line2D(visible.Tx.deg, visible.Ty.deg, 
-                     **c_kw)
+
+    prime_meridian = Line2D(visible.Tx.deg, visible.Ty.deg,
+                            **c_kw)
     axes.add_line(prime_meridian)
     return prime_meridian
