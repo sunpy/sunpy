289539588a36ebd8571dbf9d72d4d3ac77391ad9
David Stansby <dstansby@gmail.com>
2022-09-29 14:52:52 +0100

    Remove deprecated animator code

---
+++ changelog/xxxx.removal.rst
@@ -0,0 +1,3 @@
+Most of the `sunpy.visualization.animator` subpackge has been removed, with the exception of `~sunpy.visualization.animator.MapSequenceAnimator`
+It has been moved into the standalone `mpl-animators <https://pypi.org/project/mpl-animators>`_ package
+Please update your imports to replace ``sunpy.visualization.animator`` with ``mpl_animators``.
--- sunpy/visualization/animator/__init__.py
+++ sunpy/visualization/animator/__init__.py
@@ -1,34 +1,3 @@
-import mpl_animators as _animators
-
-from sunpy.util.decorators import deprecated
 from sunpy.visualization.animator.mapsequenceanimator import MapSequenceAnimator
 
-__all__ = ['LineAnimator', 'ImageAnimator', 'ArrayAnimatorWCS',
-           'ArrayAnimator', 'BaseFuncAnimator', 'MapSequenceAnimator']
-
-deprecated_animator = deprecated("3.1", alternative="the new mpl-animators package")
-
-
-@deprecated_animator
-class BaseFuncAnimator(_animators.BaseFuncAnimator):
-    pass
-
-
-@deprecated_animator
-class ArrayAnimator(_animators.ArrayAnimator):
-    pass
-
-
-@deprecated_animator
-class ArrayAnimatorWCS(_animators.ArrayAnimatorWCS):
-    pass
-
-
-@deprecated_animator
-class ImageAnimator(_animators.ImageAnimator):
-    pass
-
-
-@deprecated_animator
-class LineAnimator(_animators.LineAnimator):
-    pass
+__all__ = ['MapSequenceAnimator']
