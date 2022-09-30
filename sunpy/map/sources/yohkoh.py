9ad9d5dd74e7f3e6763ed1b7f7dc35456bdfeacf
Nabil Freij <nabil.freij@gmail.com>
2022-09-30 10:14:13 -0700

    Backport PR #6436: Clean up SXTMap and add source tests and test data

--- sunpy/map/sources/yohkoh.py
+++ sunpy/map/sources/yohkoh.py
@@ -3,14 +3,14 @@
 __author__ = "Jack Ireland"
 __email__ = "jack.ireland@nasa.gov"
 
-import numpy as np
 
+import astropy.units as u
+from astropy.coordinates import ITRS, SphericalRepresentation
 from astropy.visualization import PowerStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 
 from sunpy.map import GenericMap
 from sunpy.map.sources.source_type import source_stretch
-from sunpy.sun import constants
 
 __all__ = ['SXTMap']
 
@@ -18,17 +18,18 @@ __all__ = ['SXTMap']
 class SXTMap(GenericMap):
     """Yohkoh SXT Image Map
 
-    The Yohkoh Soft X-ray Telescope (SXT) the full solar disk
-    (42 x 42 arcminutes)in the 0.25 - 4.0 keV range.
-    It consists of a glancing incidence mirror and a CCD sensor and
+    The Yohkoh Soft X-ray Telescope (SXT) observed the full solar disk
+    (42 x 42 arcminutes) in the 0.25 - 4.0 keV range.
+    It consisted of a glancing incidence mirror and a CCD sensor and
     used thin metallic filters to acquire images in restricted
-    portions of its energy range. SXT could resolve features down to 2.5
-    arcseconds. Information about the temperature and density of the plasma
+    portions of its energy range.
+    SXT could resolve features down to 2.5 arcseconds.
+    Information about the temperature and density of the plasma
     emitting the observed x-rays was obtained by comparing images acquired with
-    the different filters. Images could be obtained every 2 to 8 seconds.
+    the different filters.
+    Images could be obtained every 2 to 8 seconds.
     Smaller images with a single filter could be obtained as frequently as
     once every 0.5 seconds.
-
     Yohkoh was launched on 30 August 1991 and ceased operations on
     14 December 2001.
 
@@ -47,33 +48,22 @@ class SXTMap(GenericMap):
             stretch=source_stretch(self.meta, PowerStretch(0.5)), clip=False)
 
     @property
+    def _supported_observer_coordinates(self):
+        return [(('long', 'lat', 'radius'), {'lon': self.meta.get('long'),
+                                             'lat': self.meta.get('lat'),
+                                             'distance': self.meta.get('radius'),
+                                             'unit': (u.deg, u.deg, u.km),
+                                             'representation_type': SphericalRepresentation,
+                                             'frame': ITRS, })
+                ] + super()._supported_observer_coordinates
+
+    @property
     def observatory(self):
         return "Yohkoh"
 
     @property
     def detector(self):
         return "SXT"
-
-    @property
-    def dsun(self):
-        """
-        For Yohkoh Maps, DSUN_OBS is not always defined. In this case the
-        SOLAR_R keyword is used to calculate dsun.
-        """
-
-        # 2012/12/19 - the SXT headers do not have a value of the distance from
-        # the spacecraft to the center of the Sun.  The FITS keyword 'DSUN_OBS'
-        # appears to refer to the observed diameter of the Sun.  Until such
-        # time as that is calculated and properly included in the file, we will
-        # use simple trigonometry to calculate the distance of the center of
-        # the Sun from the spacecraft.  Note that the small angle approximation
-        # is used, and the solar radius stored in SXT FITS files is in arcseconds.
-
-        if 'solar_r' in self.meta:
-            dsun = constants.radius / (np.deg2rad(self.meta['solar_r'] / 3600.0))
-        else:
-            dsun = constants.au
-        return self.meta.get('dsun_obs', dsun)
 
     @property
     def measurement(self):
