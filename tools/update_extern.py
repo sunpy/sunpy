bb1a09b118afde653aaf7655c4ed844294a89de0
Nabil Freij <nabil.freij@gmail.com>
2022-11-02 18:39:23 -0700

    update extern readme and code

--- tools/update_extern.py
+++ tools/update_extern.py
@@ -14,8 +14,8 @@ PACKAGES = {
     "appdirs_license.txt": ["ActiveState", "appdirs", "LICENSE.txt"],
     "distro.py": ["python-distro", "distro", "src/distro/distro.py"],
     "distro_license.rst": ["python-distro", "distro", "LICENSE"],
-    "inflect.py": ["jaraco", "inflect", "inflect/__init__.py"],
-    "inflect_license.txt": ["jaraco", "inflect", "LICENSE"],
+    # Inflect is not here to avoid a dependency on pydantic.
+    # Since it only does one specific thing, we should be able to pin the version permanently.
     "parse.py": ["r1chardj0n3s", "parse", "parse.py"],
     "parse_license.txt": ["r1chardj0n3s", "parse", "LICENSE"],
 }
