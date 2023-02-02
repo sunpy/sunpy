de21b7317064a9fdc19b30fab9c41fa2c47c72fc
Akshit Tyagi <tyagiakshit833@gmail.com>
2023-02-02 17:34:06 +0530

    use jsoc example email system in SHARP example

--- examples/acquiring_data/querying_and_loading_SHARP_data.py
+++ examples/acquiring_data/querying_and_loading_SHARP_data.py
@@ -5,6 +5,8 @@ Querying and loading SHARP data
 
 In this example we will demonstrate how to acquire [Spaceweather HMI Active Region Patch (SHARP)](http://jsoc.stanford.edu/doc/data/hmi/sharp/sharp.htm) data and load it into a `sunpy.map.Map`.
 """
+import os
+
 import astropy.units as u
 
 import sunpy.map
@@ -13,16 +15,17 @@ from sunpy.net import attrs as a
 
 ###################################################################
 # To get access to SHARP data, we will need to query the [JSOC/Stanford](http://jsoc.stanford.edu/).
-# We will use `Fido <sunpy.net.fido_factory.UnifiedDownloaderFactory>` 
+# We will use `Fido <sunpy.net.fido_factory.UnifiedDownloaderFactory>`
 # and make use of the JSOC attributes that allow us to query the JSOC.
 
+jsoc_email = os.environ["JSOC_EMAIL"]
 
 result = Fido.search(a.Time("2023-02-01 21:00:00", "2023-02-01 22:30:00"),
-                      a.Sample(1*u.hour),
-                      a.jsoc.Series("hmi.sharp_cea_720s"),
-                      a.jsoc.PrimeKey("HARPNUM", 7871),
-                      a.jsoc.Notify(notify_email),
-                      a.jsoc.Segment("Bp"))
+                     a.Sample(1*u.hour),
+                     a.jsoc.Series("hmi.sharp_cea_720s"),
+                     a.jsoc.PrimeKey("HARPNUM", 7871),
+                     a.jsoc.Notify(jsoc_email),
+                     a.jsoc.Segment("Bp"))
 # Then fetch the file.
 file = Fido.fetch(result)
 
