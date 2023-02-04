447b16be43b915e39fc3b362134284c0d99268af
Nabil Freij <nabil.freij@gmail.com>
2023-02-04 12:33:39 -0800

    Apply suggestions from code review
    Co-authored-by: Will Barnes <will.t.barnes@gmail.com>

--- examples/acquiring_data/querying_and_loading_SHARP_data.py
+++ examples/acquiring_data/querying_and_loading_SHARP_data.py
@@ -3,7 +3,7 @@
 Querying and loading SHARP data
 ===============================
 
-In this example we will demonstrate how to acquire [Spaceweather HMI Active Region Patch (SHARP)](http://jsoc.stanford.edu/doc/data/hmi/sharp/sharp.htm) data and load it into a `sunpy.map.Map`.
+In this example we will demonstrate how to acquire `Spaceweather HMI Active Region Patch (SHARP) <http://jsoc.stanford.edu/doc/data/hmi/sharp/sharp.htm>`__ data and load it into a `sunpy.map.Map`.
 """
 import os
 
@@ -14,18 +14,19 @@ from sunpy.net import Fido
 from sunpy.net import attrs as a
 
 ###################################################################
-# To search for SHARP data, we will need to query the [JSOC/Stanford](http://jsoc.stanford.edu/).
+# To search for SHARP data, we will need to query the `JSOC <http://jsoc.stanford.edu/>`__.
 # We will use `Fido <sunpy.net.fido_factory.UnifiedDownloaderFactory>`
 # and make use of the JSOC attributes that allow us to query the JSOC.
 
-# You will need to replace this with your own registered email address:
-# http://jsoc.stanford.edu/ajax/register_email.html
+# Exporting data from the JSOC requires registering your email first. 
+# Please replace this with your email address once you have registered.
+# See `this page <http://jsoc.stanford.edu/ajax/register_email.html>`__ for more details.
 jsoc_email = os.environ["JSOC_EMAIL"]
 
-result = Fido.search(a.Time("2023-02-01 21:00:00", "2023-02-01 22:30:00"),
+result = Fido.search(a.Time("2011-03-09 23:20:00", "2011-03-09 23:30:00"),
                      a.Sample(1*u.hour),
                      a.jsoc.Series("hmi.sharp_cea_720s"),
-                     a.jsoc.PrimeKey("HARPNUM", 7871),
+                     a.jsoc.PrimeKey("HARPNUM", 401),
                      a.jsoc.Notify(jsoc_email),
                      a.jsoc.Segment("Bp"))
 print(result)
@@ -39,6 +40,6 @@ file = Fido.fetch(result)
 # Now that we have the file, we can construct a `sunpy.map.Map` and plot it.
 
 sharp_map = sunpy.map.Map(file)
-sharp_map.plot()
+sharp_map.plot(vmin=-1500, vmax=1500)
 
 plt.show()
