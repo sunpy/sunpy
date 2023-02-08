6187f9b7ecc3c87e39261db5f53b191afd87aff0
Ansh Dixit <awesomeansh03@gmail.com>
2023-02-09 01:11:05 +0530

    modified stereo_loc_sunpy

--- examples/units_and_coordinates/stereo_loc_sunpy.py
+++ examples/units_and_coordinates/stereo_loc_sunpy.py
@@ -3,9 +3,12 @@
 Getting the location of STEREO-A using sunpy
 ============================================
 
-How to get the position of planetary bodies im the solar system using
+This example shows how to get and plot the position of planetary bodies within the solar system using
 `astropy's solar system ephemeris <http://docs.astropy.org/en/stable/coordinates/solarsystem.html#solar-system-ephemerides>`__ information and sunpy.
 """
+##############################################################################
+# Import the required modules.
+
 import datetime
 
 import matplotlib.pyplot as plt
@@ -13,21 +16,23 @@ import matplotlib.pyplot as plt
 from astropy.time import Time
 
 from sunpy.coordinates import get_body_heliographic_stonyhurst
+
 from sunpy.coordinates.ephemeris import get_horizons_coord
+
+##############################################################################
+# You can make use of `~datetime.datetime.now` to pass current date and time.
+# Now can use `get_horizons_coord()` to get the coordinates of STEREO-A.
 
 today = datetime.datetime.now()
 obstime = Time(today)
-
-##############################################################################
-# Lets grab the positions of STEREO-A in Heliographic Stonyhurst
-# coordinates.
-
-
 aia = get_horizons_coord('STEREO-A', obstime)
 
 ##############################################################################
-# Lets grab the positions of each of the planets in Heliographic Stonyhurst
-# coordinates.
+# Now We are using `get_body_heliographic_stonyhurst()` to get the coordinates
+# of other bodies.
+# We have not used `get_body_heliographic_stonyhurst()` to find the coordinates
+# of STEREO-A as it's position and velocity cannot be calculated with the 
+# 'builtin' ephemeris.
 
 planet_list = ['mars', 'sun', 'earth']
 planet_coord = [get_body_heliographic_stonyhurst(
@@ -36,7 +41,7 @@ planet_list.append('STEREO-A')
 planet_coord.append(aia)
 
 ##############################################################################
-# Let's plot the results. Remember the Sun is at the center of this coordinate
+# Now you can plot the results. Remember the Sun is at the center of this coordinate
 # system.
 
 fig = plt.figure()
@@ -44,4 +49,5 @@ ax = fig.add_subplot(projection='polar')
 for this_planet, this_coord in zip(planet_list, planet_coord):
     ax.plot(this_coord.lon.to('rad'), this_coord.radius, 'o', label=this_planet)
 ax.legend()
+
 plt.show()
