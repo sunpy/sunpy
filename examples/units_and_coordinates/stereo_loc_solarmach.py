405d50b53f3e91850b607764246339f07096b82d
Ansh Dixit <awesomeansh03@gmail.com>
2023-02-08 13:45:41 +0530

    stereo file added

---
+++ examples/units_and_coordinates/stereo_loc_solarmach.py
@@ -0,0 +1,34 @@
+from solarmach import SolarMACH
+import datetime
+from astropy.time import Time
+
+# necessary options
+body_list = ['STEREO-A', 'Earth', 'Mars']
+vsw_list = [400, 400, 400]  
+time = datetime.datetime.now()
+date = str(time)
+
+# optional parameters
+coord_sys = 'Stonyhurst'                                                                                        
+reference_long = 273                                                 
+reference_lat = 0                                
+plot_spirals = True                            
+plot_sun_body_line = True                        
+long_offset = 270                                
+reference_vsw = 400                             
+return_plot_object = False                      
+transparent = False                            
+numbered_markers = True                       
+
+sm = SolarMACH(date, body_list, vsw_list, reference_long, reference_lat, coord_sys)
+
+sm.plot(
+   plot_spirals=plot_spirals,
+   plot_sun_body_line=plot_sun_body_line,
+   reference_vsw=reference_vsw,
+   transparent=transparent,
+   numbered_markers=numbered_markers,
+   long_offset=long_offset,
+   return_plot_object=return_plot_object,
+   
+)
---
+++ examples/units_and_coordinates/stereo_loc_sunpy.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from astropy.time import Time
+import datetime
+from sunpy.coordinates import get_body_heliographic_stonyhurst
+
+today = datetime.datetime.now()
+obstime = Time(today)
+
+from sunpy.coordinates.ephemeris import get_horizons_coord
+aia = get_horizons_coord('STEREO-A', obstime)
+
+planet_list = ['mars', 'sun', 'earth']
+planet_coord = [get_body_heliographic_stonyhurst(
+    this_planet, time=obstime) for this_planet in planet_list]
+planet_list.append('STEREO-A')
+planet_coord.append(aia)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='polar')
+for this_planet, this_coord in zip(planet_list, planet_coord):
+    ax.plot(this_coord.lon.to('rad'), this_coord.radius, 'o', label=this_planet)
+ax.legend()
+plt.show()
