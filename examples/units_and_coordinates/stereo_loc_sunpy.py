60cbefbd940cd1253f6e781ebffadf37909af5b5
Ansh Dixit <awesomeansh03@gmail.com>
2023-02-10 01:40:57 +0530

    modified stereo_loc_sunpy.py

--- examples/units_and_coordinates/stereo_loc_sunpy.py
+++ examples/units_and_coordinates/stereo_loc_sunpy.py
@@ -1,7 +1,7 @@
 """
-==================================
-Getting the trajectory of STEREO-A
-==================================
+============================================
+Getting the location and trajectory of STEREO-A using sunpy
+============================================
 
 This example shows how to get and plot the position of planetary bodies within the solar system using
 `astropy's solar system ephemeris <http://docs.astropy.org/en/stable/coordinates/solarsystem.html#solar-system-ephemerides>`__ information and sunpy.
@@ -23,7 +23,7 @@ from sunpy.coordinates.ephemeris import get_horizons_coord
 today = datetime.datetime.now()
 obstime = Time(today)
 aia = get_horizons_coord('STEREO-A', obstime)
-trajectory_coords = get_horizons_coord('STEREO-A', {'start': '2012-11-11', 'stop': '2013-05-11', 'step': '5d'})
+trajectory_coords = get_horizons_coord('STEREO-A', {'start': '2012-11-11', 'stop': '2013-05-11', 'step': '180m'})
 
 ##############################################################################
 # Now We are using `get_body_heliographic_stonyhurst()` to get the coordinates
@@ -32,7 +32,7 @@ trajectory_coords = get_horizons_coord('STEREO-A', {'start': '2012-11-11', 'stop
 # of STEREO-A as it's position and velocity cannot be calculated with the
 # 'builtin' ephemeris.
 
-planets = ['sun', 'earth']
+planets = ['earth', 'sun']
 planet_coords = [get_body_heliographic_stonyhurst(
     planet, time=obstime) for planet in planets]
 planets.append('STEREO-A')
@@ -48,13 +48,10 @@ for coord in trajectory_coords:
 for planet, coord in zip(planets, planet_coords):
     ax.plot(coord.lon.to('rad'), coord.radius, 'o', label=planet)
 
+##############################################################################
+# You have to seperately insert the label.
 
 ax.plot(coord.lon.to('deg'), coord.radius, label='Trajectory of STEREO-A', color='yellow')
-ax.legend(bbox_to_anchor=(1.25, 1.30), ncol=2)
+ax.legend(loc = 'upper right')
 
 plt.show()
-
-##############################################################################
-# Other packages that will achieve this task:
-#
-# * `solarmach <https://github.com/jgieseler/solarmach>`__
