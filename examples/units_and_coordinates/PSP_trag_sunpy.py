4b63b13f93f164b4f263d49c8a4978ac11ff2c91
Ansh Dixit <awesomeansh03@gmail.com>
2023-02-11 00:41:43 +0530

    updated PSP_trag_sunpy.py

--- examples/units_and_coordinates/PSP_trag_sunpy.py
+++ examples/units_and_coordinates/PSP_trag_sunpy.py
@@ -1,9 +1,9 @@
 """
-==================================
-Getting the trajectory of STEREO-A
-==================================
+============================================
+Getting the trajectory of Parker Solar Probe
+============================================
 
-This example shows how to get and plot the position of planetary bodies within the solar system using
+This example shows how to plot the trajectory of Parker Solar Probe within the solar system using
 `astropy's solar system ephemeris <http://docs.astropy.org/en/stable/coordinates/solarsystem.html#solar-system-ephemerides>`__ information and sunpy.
 """
 import datetime
@@ -18,25 +18,22 @@ from sunpy.coordinates.ephemeris import get_horizons_coord
 ##############################################################################
 # You can make use of `~datetime.datetime.now` to pass current date and time.
 # Now can use `get_horizons_coord() <https://docs.sunpy.org/en/stable/generated/api/sunpy.coordinates.get_horizons_coord.html>`__
-# to get the coordinates of STEREO-A.
+# to get the coordinates of Parker Solar Probe.
 
 today = datetime.datetime.now()
 obstime = Time(today)
-aia = get_horizons_coord('STEREO-A', obstime)
-trajectory_coords = get_horizons_coord('STEREO-A', {'start': '2012-11-11', 'stop': '2013-05-11', 'step': '3d'})
+trajectory_coords = get_horizons_coord('Parker Solar Probe', {'start': '2021-10-11', 'stop': '2022-01-12', 'step': '180m'})
 
 ##############################################################################
 # Now We are using `get_body_heliographic_stonyhurst()` to get the coordinates
-# of other bodies.
+# of other bodies such as sun ans earth.
 # We have not used `get_body_heliographic_stonyhurst()` to find the coordinates
-# of STEREO-A as it's position and velocity cannot be calculated with the
+# of Parker Solar Probe as it's position and velocity cannot be calculated with the
 # 'builtin' ephemeris.
 
 planets = ['earth', 'sun']
 planet_coords = [get_body_heliographic_stonyhurst(
     planet, time=obstime) for planet in planets]
-planets.append('STEREO-A')
-planet_coords.append(aia)
 
 ##############################################################################
 # Now we will create a polar plot of these coordinates.
@@ -44,10 +41,10 @@ planet_coords.append(aia)
 fig = plt.figure()
 ax = fig.add_subplot(projection='polar')
 for coord in trajectory_coords:
-    ax.plot(coord.lon.to('deg'), coord.radius, 'y.', markersize=2)
+    ax.plot(coord.lon.to('rad'), coord.radius, 'y.', markersize=2)
 for planet, coord in zip(planets, planet_coords):
     ax.plot(coord.lon.to('rad'), coord.radius, 'o', label=planet)
-ax.plot(coord.lon.to('deg'), coord.radius, label='Trajectory of STEREO-A', color='yellow')
+ax.plot(coord.lon.to('rad'), coord.radius, label='Trajectory of Parker Solar Probe', color='#c8c825')
 ax.legend(loc='upper right')
 
 plt.show()
