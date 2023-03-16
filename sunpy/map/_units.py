91c7fcd969087d980de24331277d0ac9955ca914
Telomelonia <shuklas2002@gmail.com>
2023-03-16 09:44:41 +0530

    Update _units.py

--- sunpy/map/_units.py
+++ sunpy/map/_units.py
@@ -6,9 +6,9 @@ import astropy.units as u
 maxwell = u.def_unit(['Mx', 'Maxwell', 'maxwell'], 1e-8 * u.si.Wb, prefixes=True, doc="Maxwell: CGS unit for magnetic flux")
 u.add_enabled_units([maxwell])
 
-muSH = u.def_unit(
-    "muSH",
-    represents=((2 * np.pi * u.solRad**2)/1000000),
+MH = u.def_unit(
+    "MH",
+    represents=(3.0437*10^6 * u.km * u.km),
     prefixes=True,
     doc="Provide millionths of solar hemisphere units")
-u.add_enabled_units([muSH])
+u.add_enabled_units([MH])
