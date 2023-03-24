1e1199fc04aa9b0c4f1cc7548f2599f1580e96d6
Nabil Freij <nabil.freij@gmail.com>
2023-03-24 13:49:31 -0700

    Tweaks to the text to follow RCs

--- examples/map/lasco_mask.py
+++ examples/map/lasco_mask.py
@@ -8,9 +8,9 @@ unprocessed LASCO C2 coronagraph image.
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import numpy.ma as ma
 
 import astropy.units as u
+from astropy.coordinates import SkyCoord
 
 from sunpy.map import Map
 from sunpy.map.maputils import all_coordinates_from_map
@@ -31,43 +31,40 @@ lasco_map = Map(lasco_file)
 # radii. So, our mask will have two parts: an inner component which masks the
 # occulter and an outer component which masks data outside the field of view.
 #
-# First, let's define the Sun center using ``CRPIXi`` and apply a shift (if
-# necessary) to the reference coordinates ``CRVALi``
-# (see `here <https://lasco-www.nrl.navy.mil/level_1/level_1_keywords.html>`_).
-
-x_occult = lasco_map.meta['crval1']/lasco_map.meta['cdelt1'] + lasco_map.meta['crpix1']
-y_occult = lasco_map.meta['crval2']/lasco_map.meta['cdelt2'] + lasco_map.meta['crpix2']
-occult_coord = lasco_map.pixel_to_world(x_occult*u.pixel, y_occult*u.pixel)
-
-###############################################################################
-# Next, we will follow a process similar to
+# We will follow a process similar to
 # :ref:`sphx_glr_generated_gallery_computer_vision_techniques_finding_masking_bright_pixels.py`
-# to express the coordinates relative to the occulter center. Then, we will
-# use `numpy.ma` functions to mask the correct regions.
-# Note that the inner mask extends just beyond 2 solar radii to mask the
-# Fresnel diffraction caused by the occulter edge.
+# to express the coordinates relative to the occulter center.
 
 hpc_coords = all_coordinates_from_map(lasco_map)
-norm_mask = np.sqrt((hpc_coords.Tx-occult_coord.Tx)**2 +
-                    (hpc_coords.Ty-occult_coord.Ty)**2)
-mask_inner = ma.masked_less_equal(norm_mask, lasco_map.rsun_obs*2.4)
-mask_outer = ma.masked_greater_equal(norm_mask, lasco_map.rsun_obs*6)
+occult_coord = SkyCoord(0*u.deg, 0*u.deg, frame="helioprojective")
+pixel_radii = np.sqrt((hpc_coords.Tx-occult_coord.Tx)**2 +
+                      (hpc_coords.Ty-occult_coord.Ty)**2)
+# Note that the inner mask extends just beyond 2 solar radii to mask the
+# Fresnel diffraction caused by the occulter edge.
+mask_inner = pixel_radii < lasco_map.rsun_obs*2.4
+mask_outer = pixel_radii > lasco_map.rsun_obs*6
 final_mask = mask_inner + mask_outer
 
 ###############################################################################
-# To apply the final mask, create a new map using ``.mask`` to get the actual
-# masked array.
+# To apply the final mask, we must create a new map.
 
-masked_lasco = Map(lasco_map.data, lasco_map.meta, mask=final_mask.mask)
+masked_lasco = Map(lasco_map.data, lasco_map.meta, mask=final_mask)
 
 # Before plotting the map, we need to create a new colormap to ensure we mask
 # the bad values correctly.
 occult_colormap = lasco_map.cmap.copy()
 occult_colormap.set_bad('black')
 
-plt.figure()
-masked_lasco.plot(clip_interval=(2, 98)*u.percent, cmap=occult_colormap)
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1, projection=lasco_map)
+ax2 = fig.add_subplot(1, 2, 2, projection=masked_lasco)
+
+lasco_map.plot(clip_interval=(2, 98)*u.percent, cmap=occult_colormap, axes=ax1)
+lasco_map.draw_limb()
+ax1.set_title("Level 1 LASCO C2")
+
+masked_lasco.plot(clip_interval=(2, 98)*u.percent, cmap=occult_colormap, axes=ax2)
 masked_lasco.draw_limb()
-plt.title("Custom mask for LASCO C2")
+ax2.set_title("Masked LASCO C2")
 
 plt.show()
--- examples/plotting/lasco_overlay.py
+++ examples/plotting/lasco_overlay.py
@@ -5,8 +5,6 @@ Overlay an AIA image on a LASCO C2 coronagraph
 
 This example shows the steps needed to overlay the disc and off-limb
 components of an AIA image within the masked occulter of a LASCO C2 image.
-
-It also demonstrates the usage of ``hypy`` to download data from the Helioviewer Project.
 """
 from datetime import datetime
 
@@ -20,6 +18,7 @@ from astropy.coordinates import SkyCoord
 import sunpy.data.sample
 from sunpy.coordinates import Helioprojective
 from sunpy.map import Map
+from sunpy.util.config import get_and_create_download_dir
 
 ###############################################################################
 # First, we will acquire a calibrated LASCO C2 image from Helioviewer and
@@ -27,13 +26,20 @@ from sunpy.map import Map
 
 lasco_jp2_file = hvpy.save_file(hvpy.getJP2Image(datetime(2011, 6, 7, 6, 34),
                                                  DataSource.LASCO_C2.value),
-                                filename="lasco.jp2", overwrite=True)
+                                filename=get_and_create_download_dir() + "/LASCO_C2.jp2", overwrite=True)
 lasco_map = Map(lasco_jp2_file)
 aia_map = Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ###############################################################################
-# In order to plot off-limb features of the AIA image, we need to reproject AIA
-# using `~sunpy.coordinates.Helioprojective.assume_spherical_screen`.
+# In order to plot off-limb features of the AIA image.
+#
+# Note that off-disk AIA data are not retained by default because an
+# additional assumption is required to define the location of the AIA
+# emission in 3D space. We can use
+# :meth:`~sunpy.coordinates.Helioprojective.assume_spherical_screen` to
+# retain the off-disk AIA data. See
+# :ref:`sphx_glr_generated_gallery_map_transformations_reprojection_spherical_screen.py`
+# for more reference.
 
 projected_coord = SkyCoord(0*u.arcsec, 0*u.arcsec,
                            obstime=lasco_map.observer_coordinate.obstime,
@@ -45,6 +51,8 @@ projected_header = sunpy.map.make_fitswcs_header(aia_map.data.shape,
                                                  scale=u.Quantity(aia_map.scale),
                                                  instrument=aia_map.instrument,
                                                  wavelength=aia_map.wavelength)
+# We use `assume_spherical_screen` to ensure that the off limb AIA pixels are reprojected
+# otherwise it will only be the on disk pixels that are reprojected.
 with Helioprojective.assume_spherical_screen(aia_map.observer_coordinate):
     aia_reprojected = aia_map.reproject_to(projected_header)
 
