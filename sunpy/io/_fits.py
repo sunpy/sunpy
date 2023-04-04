f4a78da6ebb68dfaa18e6ded5e0fcb9d95d51698
Nabil Freij <nabil.freij@gmail.com>
2023-04-03 21:07:49 -0700

    Update other parts of docs to avoid warnings

--- sunpy/io/_fits.py
+++ sunpy/io/_fits.py
@@ -285,9 +285,9 @@ def extract_waveunit(header):
 
     Parameters
     ----------
-    header : `sunpy._io.header.FileHeader`
-        One `~sunpy._io.header.FileHeader` instance which was created by
-        reading a FITS file. For example, `sunpy._io.fits.get_header` returns a list of
+    header : `sunpy.io.header.FileHeader`
+        One `~sunpy.io.header.FileHeader` instance which was created by
+        reading a FITS file. For example, `sunpy.io._fits.get_header` returns a list of
         such instances.
 
     Returns
