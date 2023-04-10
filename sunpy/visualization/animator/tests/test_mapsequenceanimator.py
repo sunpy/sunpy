4fa76a85a2fb15f6a2d7e3d303e041cf6747b796
jayantbhakar <jayant.bhakar@gmail.com>
2023-04-10 14:53:34 +0530

    captured deprecation warning in visualization

--- sunpy/visualization/animator/tests/test_mapsequenceanimator.py
+++ sunpy/visualization/animator/tests/test_mapsequenceanimator.py
@@ -19,23 +19,26 @@ def test_map_sequence(aia171_test_map):
         sequence=True,
     )
 
-
+@pytest.mark.filterwarnings("ignore::sunpy.util.exceptions.SunpyDeprecationWarning")
 def test_construct_map_sequence_animator(test_map_sequence):
     map_animator = MapSequenceAnimator(test_map_sequence)
     assert isinstance(map_animator, MapSequenceAnimator)
 
 
 @figure_test
+@pytest.mark.filterwarnings("ignore::sunpy.util.exceptions.SunpyDeprecationWarning")
 def test_map_sequence_animator_wcs_simple_plot(test_map_sequence):
     map_animator = MapSequenceAnimator(test_map_sequence)
     return map_animator.fig
 
 
+@pytest.mark.filterwarnings("ignore::sunpy.util.exceptions.SunpyDeprecationWarning")
 def test_axes(test_map_sequence):
     map_animator = MapSequenceAnimator(test_map_sequence)
     assert isinstance(map_animator.axes, WCSAxes)
 
 
+@pytest.mark.filterwarnings("ignore::sunpy.util.exceptions.SunpyDeprecationWarning")
 def test_map_sequence_animator_wcs_update_plot(test_map_sequence):
     map_animator = MapSequenceAnimator(test_map_sequence)
     map1 = map_animator.im.get_array()
