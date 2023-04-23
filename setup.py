1edf44dbea0dcf0215234163b65c03c749772801
David Stansby <dstansby@gmail.com>
2023-04-23 16:57:57 +0100

    Use pyproject for setuptools_scm

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup  # isort:skip
-import os
 
 from extension_helpers import get_extensions
 
-setup(
-    use_scm_version={'write_to': os.path.join('sunpy', '_version.py')},
-    ext_modules=get_extensions(),
-)
+setup(ext_modules=get_extensions())
