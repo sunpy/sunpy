e8617130a014a9337d852e077f71b2d65bb13ec7
David Stansby <dstansby@gmail.com>
2023-04-23 16:50:17 +0100

    Specify all extras in setup.cfg

--- setup.py
+++ setup.py
@@ -1,33 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup  # isort:skip
 import os
-from itertools import chain
 
 from extension_helpers import get_extensions
 
-try:
-    # Recommended for setuptools 61.0.0+
-    # (though may disappear in the future)
-    from setuptools.config.setupcfg import read_configuration
-except ImportError:
-    from setuptools.config import read_configuration
-
-################################################################################
-# Programmatically generate some extras combos.
-################################################################################
-extras = read_configuration("setup.cfg")['options']['extras_require']
-
-# Dev is everything
-extras['dev'] = list(chain(*extras.values()))
-
-# All is everything but tests and docs
-exclude_keys = ("tests", "docs", "dev")
-ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
-# Concatenate all the values together for 'all'
-extras['all'] = list(chain.from_iterable(ex_extras.values()))
-
 setup(
-    extras_require=extras,
     use_scm_version={'write_to': os.path.join('sunpy', '_version.py')},
     ext_modules=get_extensions(),
 )
