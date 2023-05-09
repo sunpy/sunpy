f40b9eeab80535976ba455e31ba6d70b49018c49
Nabil Freij <nabil.freij@gmail.com>
2023-05-09 16:45:20 +0100

    Patch pre-commit

--- sunpy/database/tests/test_tables.py
+++ sunpy/database/tests/test_tables.py
@@ -525,7 +525,6 @@ def test_entry_from_query_results_with_none_wave_and_default_unit(
             instrument='VIRGO', size=332814.0),
     ]
 
-
     for e in entries:
         assert e in expected
     for e in expected:
