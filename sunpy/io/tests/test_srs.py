68dfdef02fb491ca5b08343334231ce593524159
Paul Wright <paul@pauljwright.co.uk>
2023-05-29 12:13:36 +0100

    adding additional test files

--- sunpy/io/tests/test_srs.py
+++ sunpy/io/tests/test_srs.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 import astropy.units as u
@@ -8,49 +7,79 @@ from astropy.tests.helper import assert_quantity_allclose
 from sunpy.data.test import get_test_filepath
 from sunpy.io.special import srs
 
-filenames = [{'file': '20150906SRS.txt', 'rows': 5},
-             {'file': '20150306SRS.txt', 'rows': 4},
-             {'file': '20150101SRS.txt', 'rows': 9},
-             # This is a corrected copy
-             {'file': "20100621SRS.txt", "rows": 3}]
+filenames = [
+    # Mixedcase files
+    {"file": "20150906SRS.txt", "rows": 5},
+    {"file": "20150306SRS.txt", "rows": 4},
+    {"file": "20150101SRS.txt", "rows": 9},
+    {"file": "20100621SRS.txt", "rows": 3},  # This is a corrected copy
+    # Uppercase files
+    {"file": "19960106SRS.txt", "rows": 4},  # inc. spurious `NNN` on final line
+    {"file": "19960430SRS.txt", "rows": 1},
+    {"file": "19960513SRS.txt", "rows": 4},  # inc. empty `COMMENT` column
+    {"file": "20000922SRS.txt", "rows": 10},  # inc. line with `III.`
+    {"file": "20000927SRS.txt", "rows": 9},  # inc. line with `EFFECTIVE 2 OCT`
+    {"file": "20001001SRS.txt", "rows": 12},  # inc. line with `COMMENT`
+    {"file": "20020624SRS.txt", "rows": 14},  # inc. line with `PLAIN`
+    {"file": "20020628SRS.txt", "rows": 13},  # inc. line with `This message`
+]
 
-COORDINATES = [{'text': 'N10W05', 'latitude': 10, 'longitude': 5},
-               {'text': 'N89E00', 'latitude': 89, 'longitude': 0},
-               {'text': 'S33E02', 'latitude': -33, 'longitude': -2},
-               {'text': 'S01', 'latitude': -1, 'longitude': None}]
+COORDINATES = [
+    {"text": "N10W05", "latitude": 10, "longitude": 5},
+    {"text": "N89E00", "latitude": 89, "longitude": 0},
+    {"text": "S33E02", "latitude": -33, "longitude": -2},
+    {"text": "S01", "latitude": -1, "longitude": None},
+]
 
-LOCATION = Column(data=[x['text'] for x in COORDINATES], name='Location')
+LOCATION = Column(data=[x["text"] for x in COORDINATES], name="Location")
 LONGLAT = Table()
-LONGLAT.add_column(MaskedColumn(data=[x['longitude'] for x in COORDINATES], name='Longitude',
-                                unit=u.deg, mask=True, fill_value=-99999))
-LONGLAT.add_column(MaskedColumn(data=[x['latitude'] for x in COORDINATES], name='Latitude',
-                                unit=u.deg, fill_value=-99999))
+LONGLAT.add_column(
+    MaskedColumn(
+        data=[x["longitude"] for x in COORDINATES],
+        name="Longitude",
+        unit=u.deg,
+        mask=True,
+        fill_value=-99999,
+    )
+)
+LONGLAT.add_column(
+    MaskedColumn(
+        data=[x["latitude"] for x in COORDINATES],
+        name="Latitude",
+        unit=u.deg,
+        fill_value=-99999,
+    )
+)
 
 
-@pytest.mark.filterwarnings('ignore:dropping mask in Quantity column')
+@pytest.mark.filterwarnings("ignore:dropping mask in Quantity column")
 @pytest.mark.parametrize(
-    ('path', 'number_of_rows'),
-    [(get_test_filepath(elem['file']), elem['rows']) for elem in filenames]
+    ("path", "number_of_rows"),
+    [(get_test_filepath(elem["file"]), elem["rows"]) for elem in filenames],
 )
 def test_number_of_rows(path, number_of_rows):
     table = srs.read_srs(path)
     assert len(table) == number_of_rows
 
 
-@pytest.mark.parametrize(('text', 'longitude'),
-                         [(elem['text'], elem['longitude']) for elem in COORDINATES])
+@pytest.mark.parametrize(
+    ("text", "longitude"), [(elem["text"], elem["longitude"]) for elem in COORDINATES]
+)
 def test_parse_longitude(text, longitude):
     assert srs.parse_longitude(text) == longitude
 
 
-@pytest.mark.parametrize(('text', 'latitude'),
-                         [(elem['text'], elem['latitude']) for elem in COORDINATES])
+@pytest.mark.parametrize(
+    ("text", "latitude"), [(elem["text"], elem["latitude"]) for elem in COORDINATES]
+)
 def test_parse_latitude(text, latitude):
     assert srs.parse_latitude(text) == latitude
 
 
-@pytest.mark.parametrize(('loc_column', 'exp_longitude', 'exp_latitude'),
-                         [(LOCATION, LONGLAT['Longitude'], LONGLAT['Latitude'])])
+@pytest.mark.parametrize(
+    ("loc_column", "exp_longitude", "exp_latitude"),
+    [(LOCATION, LONGLAT["Longitude"], LONGLAT["Latitude"])],
+)
 def test_parse_location(loc_column, exp_longitude, exp_latitude):
     latitude, longitude = srs.parse_location(loc_column)
     assert_quantity_allclose(latitude, exp_latitude)
