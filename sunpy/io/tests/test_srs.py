43723d25971085019923dd3444b1ef3d1c6b37d4
Paul Wright <paul@pauljwright.co.uk>
2023-05-29 16:07:00 +0100

    ValueError tests

--- sunpy/io/tests/test_srs.py
+++ sunpy/io/tests/test_srs.py
@@ -63,13 +63,42 @@ def test_parse_location(loc_column, exp_longitude, exp_latitude):
     assert_quantity_allclose(latitude, exp_latitude)
     assert_quantity_allclose(longitude, exp_longitude)
 
-def test_try_drop_empty_column():
-    data_lines = ['NMBR  LOCATION  LO  COMMENT', '8000  S14W96   232']
-    expected_pattern_dict = {
+@pytest.fixture
+def data_lines():
+    return ['NMBR  LOCATION  LO  COMMENT', '8000  S14W96   232']
+
+@pytest.fixture
+def expected_pattern_dict():
+    return {
         'Nmbr': r'^\d+$',
         'Location': r'[NESW]\d{2}[NESW]\d{2}',
         'Lo': r'^\d+$',
+        # 'Comment': r'^[a-zA-Z]+$', # having this causes an issue
     }
-    column_name_to_drop = 'COMMENT'
+
+@pytest.fixture
+def column_name_to_drop():
+    return 'COMMENT'
+
+def test_try_drop_empty_column(data_lines, expected_pattern_dict, column_name_to_drop):
     result = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
-    assert result == ['Nmbr Location Lo', '8000  S14W96   232']
+    out = data_lines.copy()
+    out[0] = ' '.join([col.title() for col in out[0].split()][:-1])
+    print(out[0])
+    print(result)
+    assert result == out
+
+def test_tdec_colname_format(expected_pattern_dict, column_name_to_drop):
+    data_lines = ['NMBR  LOCATION  LO  COMMENT']
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+
+def test_tdec_incorrect_data_format(data_lines, column_name_to_drop):
+    expected_pattern_dict = [r'^\d+$', r'[NESW]\d{2}[NESW]\d{2}', r'^\d+$']
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+
+def test_tdec_incorrect_colname_format(data_lines, expected_pattern_dict):
+    column_name_to_drop = 2
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
