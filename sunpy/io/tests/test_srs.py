680548c8ac6c4ef120e583e538358e13bede06f7
Paul Wright <paul@pauljwright.co.uk>
2023-05-31 17:04:27 +0100

    Parse uppercase SRS files (#7035)
    Co-authored-by: Shane Maloney <maloneys@tcd.ie>
    Co-authored-by: Alasdair Wilson <60351846+alasdairwilson@users.noreply.github.com>
    Co-authored-by: Alasdair Wilson <alasdair.wlsn@gmail.com>
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/io/tests/test_srs.py
+++ sunpy/io/tests/test_srs.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 import astropy.units as u
@@ -8,11 +7,19 @@ from astropy.tests.helper import assert_quantity_allclose
 from sunpy.data.test import get_test_filepath
 from sunpy.io.special import srs
 
-filenames = [{'file': '20150906SRS.txt', 'rows': 5},
-             {'file': '20150306SRS.txt', 'rows': 4},
-             {'file': '20150101SRS.txt', 'rows': 9},
-             # This is a corrected copy
-             {'file': "20100621SRS.txt", "rows": 3}]
+filenames = [{'file': 'SRS/20150906SRS.txt', 'rows': 5},
+             {'file': 'SRS/20150306SRS.txt', 'rows': 4},
+             {'file': 'SRS/20150101SRS.txt', 'rows': 9},
+             {'file': 'SRS/20100621SRS.txt', 'rows': 3},  # This is a corrected copy
+             # Uppercase files
+             {'file': 'SRS/19960106SRS.txt', 'rows': 4},  # inc. spurious `NNN` on final line
+             {'file': 'SRS/19960430SRS.txt', 'rows': 1},
+             {'file': 'SRS/19960513SRS.txt', 'rows': 4},  # inc. empty `COMMENT` column
+             {'file': 'SRS/20000922SRS.txt', 'rows': 10},  # inc. line with `III.`
+             {'file': 'SRS/20000927SRS.txt', 'rows': 9},  # inc. line with `EFFECTIVE 2 OCT`
+             {'file': 'SRS/20001001SRS.txt', 'rows': 12},  # inc. line with `COMMENT`
+             {'file': 'SRS/20020624SRS.txt', 'rows': 14},  # inc. line with `PLAIN`
+             {'file': 'SRS/20020628SRS.txt', 'rows': 13}]  # inc. line with `This message`
 
 COORDINATES = [{'text': 'N10W05', 'latitude': 10, 'longitude': 5},
                {'text': 'N89E00', 'latitude': 89, 'longitude': 0},
@@ -55,3 +62,88 @@ def test_parse_location(loc_column, exp_longitude, exp_latitude):
     latitude, longitude = srs.parse_location(loc_column)
     assert_quantity_allclose(latitude, exp_latitude)
     assert_quantity_allclose(longitude, exp_longitude)
+
+# test _try_drop_empty_column (tdec)
+
+@pytest.fixture
+def data_lines():
+    return ['NMBR  LOCATION  LO  COMMENT', '8000  S14W96   232']
+
+@pytest.fixture
+def expected_pattern_dict():
+    return {
+        'Nmbr': r'^\d+$',
+        'Location': r'^(?:[NESW](?:\d{2})){1,2}$',
+        'Lo': r'^\d+$',
+        'Comment': r'^[a-zA-Z]+$',
+        'Additional Number': r'^\d+$',
+    }
+
+@pytest.fixture
+def column_name_to_drop():
+    return 'COMMENT'
+
+def test_tdec(data_lines, expected_pattern_dict, column_name_to_drop):
+    result = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+    out = data_lines.copy()
+    out[0] = ' '.join([col.title() for col in out[0].split()][:-1])
+    assert result == out
+
+def test_tdec_smallest_dict_example(data_lines, expected_pattern_dict, column_name_to_drop):
+    """
+    Smallest possible dictionary given the ``data_lines``
+    """
+    keys_to_keep = ['Nmbr', 'Location', 'Lo']
+    filtered_dict = {key: value for key, value in expected_pattern_dict.items() if key in keys_to_keep}
+    result = srs._try_drop_empty_column(column_name_to_drop, data_lines, filtered_dict)
+    out = data_lines.copy()
+    out[0] = ' '.join([col.title() for col in out[0].split()][:-1])
+    assert result == out
+
+def test_tdec_too_small_dict_example(data_lines, expected_pattern_dict, column_name_to_drop):
+    """
+    Columns aren't a subset of the dictionary: no pattern to match for column `LO`
+    """
+    keys_to_keep = ['Nmbr', 'Location']
+    filtered_dict = {key: value for key, value in expected_pattern_dict.items() if key in keys_to_keep}
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, filtered_dict)
+
+def test_tdec_no_data(expected_pattern_dict, column_name_to_drop):
+    """
+    No data associated with header
+    """
+    data_lines = ['NMBR  LOCATION  LO  COMMENT', 'NONE']
+    out = data_lines.copy()
+    out[0] = ' '.join([col.title() for col in out[0].split()][:-1])
+    assert out == srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+
+def test_tdec_col_not_empty(expected_pattern_dict, column_name_to_drop):
+    """
+    Column not empty
+    """
+    data_lines = [
+        'NMBR  LOCATION  LO  COMMENT',
+        '8000  S14W96   232',
+        '3020 N20E20  210 additional_info',
+        ]
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+
+def test_tdec_col_not_match_pattern(expected_pattern_dict, column_name_to_drop):
+    """
+    `LOCATION` column doesn't match the correct pattern
+    """
+    data_lines = [
+        'NMBR  LOCATION  LO  COMMENT',
+        '8000  S14W926   232',
+        ]
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column(column_name_to_drop, data_lines, expected_pattern_dict)
+
+def test_tdec_colname_not_exist(data_lines, expected_pattern_dict):
+    """
+    Try remove column name that does not exist in data
+    """
+    with pytest.raises(ValueError):
+        _ = srs._try_drop_empty_column('nonexistent_column_name', data_lines, expected_pattern_dict)
