87a680b4a084e271d8dfcda690f44ccec87ce10e
Akshit Tyagi <tyagiakshit833@gmail.com>
2023-07-01 01:36:36 +0530

    update scraper tests

--- sunpy/net/tests/test_scraper.py
+++ sunpy/net/tests/test_scraper.py
@@ -9,36 +9,36 @@ from sunpy.net.scraper import Scraper, get_timerange_from_exdict
 from sunpy.time import TimeRange, parse_time
 
 PATTERN_EXAMPLES = [
-    ('%b%y', relativedelta(months=1)),
+    ('%b%Y', relativedelta(months=1)),
     ('%m%y', relativedelta(months=1)),
     ('%H%d', relativedelta(hours=1)),
-    ('%y%b', relativedelta(months=1)),
+    ('%Y%b', relativedelta(months=1)),
 ]
 
 
 def testDirectoryDatePattern():
-    s = Scraper('%Y/%m/%d/%Y%m%d_%H%M%S_59.fit.gz')
+    s = Scraper('{{year:4d}}/{{month:2d}}/{{day:2d}}/{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}_59.fit.gz')
     testpath = '2014/03/05/20140305_013000_59.fit.gz'
     d = parse_time((2014, 3, 5, 1, 30))
     assert s.matches(testpath, d)
 
 
 def testDirectoryDatePatternFalse():
-    s = Scraper('%Y/%m/%d/%Y%m%d_%H%M%S_59.fit.gz')
+    s = Scraper('{{year:4d}}/{{month:2d}}/{{day:2d}}/{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}_59.fit.gz')
     testpath = '2013/03/05/20140305_013000_59.fit.gz'
     d = parse_time((2014, 3, 5, 1, 30))
     assert not s.matches(testpath, d)
 
 
 def testDirectoryObsPattern():
-    s = Scraper('%y%m%d/{observatory}_%Y%m%d.fits', observatory='SDO')
+    s = Scraper('{{year:4d}}{{month:2d}}{{day:2d}}/{observatory}_{{year:4d}}{{month:2d}}{{day:2d}}.fits', observatory='SDO')
     testpath = '140305/SDO_20140305.fits'
     d = parse_time((2014, 3, 5))
     assert s.matches(testpath, d)
 
 
 def testDirectoryRange():
-    s = Scraper('%Y/%m/%d/%Y%m%d_%H.fit.gz')
+    s = Scraper('{{year:4d}}/{{month:2d}}/{{day:2d}}/{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}.fit.gz')
     directory_list = ['2009/12/30/', '2009/12/31/', '2010/01/01/',
                       '2010/01/02/', '2010/01/03/']
     timerange = TimeRange('2009-12-30', '2010-01-03')
@@ -54,7 +54,7 @@ def testDirectoryRegex():
 
 
 def testDirectoryRangeFalse():
-    s = Scraper('%Y%m%d/%Y%m%d_%H.fit.gz')
+    s = Scraper('{{year:4d}}{{month:2d}}{{day:2d}}/{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}.fit.gz')
     directory_list = ['20091230/', '20091231/', '20100101/',
                       '20090102/', '20090103/']
     timerange = TimeRange('2009/12/30', '2010/01/03')
@@ -62,7 +62,7 @@ def testDirectoryRangeFalse():
 
 
 def testNoDateDirectory():
-    s = Scraper('mySpacecraft/myInstrument/xMinutes/aaa%y%b.ext')
+    s = Scraper('mySpacecraft/myInstrument/xMinutes/aaa{{year:4d}}%b.ext')
     directory_list = ['mySpacecraft/myInstrument/xMinutes/']
     timerange = TimeRange('2009/11/20', '2010/01/03')
     assert s.range(timerange) == directory_list
@@ -74,13 +74,13 @@ def test_smallerPattern(pattern, mintime):
 
 
 def testDirectoryRangeHours():
-    s = Scraper('%Y%m%d_%H/%H%M.csv')
+    s = Scraper('{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}/{{hour:2d}}{{month:2d}}.csv')
     timerange = TimeRange('2009-12-31T23:40:00', '2010-01-01T01:15:00')
     assert len(s.range(timerange)) == 3  # 3 directories (1 per hour)
 
 
 def testDirectoryRange_single():
-    s = Scraper('%Y%m%d/%H_%M.csv')
+    s = Scraper('{{year:4d}}{{month:2d}}{{day:2d}}/{{hour:2d}}_{{month:2d}}.csv')
     startdate = parse_time((2010, 10, 10, 5, 0))
     enddate = parse_time((2010, 10, 10, 7, 0))
     timerange = TimeRange(startdate, enddate)
@@ -88,7 +88,7 @@ def testDirectoryRange_single():
 
 
 def testDirectoryRange_Month():
-    s = Scraper('%Y%m/%d/%j_%H.txt')
+    s = Scraper('{{year:4d}}{{month:2d}}/{{day:2d}}/%j_{{hour:2d}}.txt')
     startdate = parse_time((2008, 2, 20, 10))
     enddate = parse_time((2008, 3, 2, 5))
     timerange = TimeRange(startdate, enddate)
@@ -100,7 +100,7 @@ def testDirectoryRange_Month():
 
 
 def testNoDirectory():
-    s = Scraper('files/%Y%m%d_%H%M.dat')
+    s = Scraper('files/{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}.dat')
     startdate = parse_time((2010, 1, 10, 20, 30))
     enddate = parse_time((2010, 1, 20, 20, 30))
     timerange = TimeRange(startdate, enddate)
@@ -109,26 +109,26 @@ def testNoDirectory():
 
 def testExtractDates_usingPattern():
     # Standard pattern
-    s = Scraper('data/%Y/%m/%d/fits/swap/swap_00174_fd_%Y%m%d_%H%M%S.fts.gz')
+    s = Scraper('data/{{year:4d}}/{{month:2d}}/{{day:2d}}/fits/swap/swap_00174_fd_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}.fts.gz')
     testURL = 'data/2014/05/14/fits/swap/swap_00174_fd_20140514_200135.fts.gz'
     timeURL = parse_time((2014, 5, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
     # Not-full repeated pattern
-    s = Scraper('data/%Y/fits/swap/swap_00174_fd_%Y%m%d_%H%M%S.fts.gz')
+    s = Scraper('data/{{year:4d}}/fits/swap/swap_00174_fd_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}.fts.gz')
     testURL = 'data/2014/fits/swap/swap_00174_fd_20140514_200135.fts.gz'
     timeURL = parse_time((2014, 5, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
 
 
 def testExtractDates_notSeparators():
-    s = Scraper('data/%Y/%m/swap%m%d_%H%M%S')
+    s = Scraper('data/{{year:4d}}/{{month:2d}}/swap{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}')
     testURL = 'data/2014/05/swap0514_200135'
     timeURL = parse_time((2014, 5, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
 
 
 def testExtractDates_notSeparators_andSimilar():
-    s = Scraper('data/%Y/Jun%b%d_%H%M%S')
+    s = Scraper('data/{{year:4d}}/Jun%b{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}')
     testURL = 'data/2014/JunJun14_200135'
     timeURL = parse_time((2014, 6, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
@@ -136,21 +136,21 @@ def testExtractDates_notSeparators_andSimilar():
     timeURL = parse_time((2014, 5, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
     # and testing with the month afterwards
-    s = Scraper('data/%Y/%dJun%b_%H%M%S')
+    s = Scraper('data/{{year:4d}}/{{day:2d}}Jun%b_{{hour:2d}}{{month:2d}}{{second:2d}}')
     testURL = 'data/2014/14JunJun_200135'
     timeURL = parse_time((2014, 6, 14, 20, 1, 35))
     assert s._extractDateURL(testURL) == timeURL
 
 
 def testURL_pattern():
-    s = Scraper('fd_%Y%m%d_%H%M%S.fts')
+    s = Scraper('fd_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}.fts')
     assert s._URL_followsPattern('fd_20130410_231211.fts')
     assert not s._URL_followsPattern('fd_20130410_231211.fts.gz')
     assert not s._URL_followsPattern('fd_20130410_ar_231211.fts.gz')
 
 
 def testURL_patternMillisecondsGeneric():
-    s = Scraper('fd_%Y%m%d_%H%M%S_%e.fts')
+    s = Scraper('fd_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}_{{millisecond:6d}}.fts')
     assert s._URL_followsPattern('fd_20130410_231211_119.fts')
     assert not s._URL_followsPattern('fd_20130410_231211.fts.gz')
     assert not s._URL_followsPattern('fd_20130410_ar_231211.fts.gz')
@@ -161,14 +161,14 @@ def testURL_patternMillisecondsZeroPadded():
     # Milliseconds must be zero-padded in order to match URL lengths.
     now_mock = Mock(return_value=datetime.datetime(2019, 4, 19, 0, 0, 0, 4009))
     with patch('sunpy.net.scraper.datetime', now=now_mock):
-        s = Scraper('fd_%Y%m%d_%H%M%S_%e.fts')
+        s = Scraper('fd_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}_{{millisecond:6d}}.fts')
     now_mock.assert_called_once()
     assert s.now == 'fd_20190419_000000_004.fts'
 
 
 def testFilesRange_sameDirectory_local():
     s = Scraper('/'.join(['file:/', str(rootdir),
-                          'EIT_header', 'efz%Y%m%d.%H%M%S_s.header']))
+                          'EIT_header', 'efz{{year:4d}}{{month:2d}}{{day:2d}}.{{hour:2d}}{{month:2d}}{{second:2d}}_s.header']))
     startdate = parse_time((2004, 3, 1, 4, 0))
     enddate = parse_time((2004, 3, 1, 6, 30))
     assert len(s.filelist(TimeRange(startdate, enddate))) == 3
@@ -179,8 +179,8 @@ def testFilesRange_sameDirectory_local():
 
 @pytest.mark.remote_data
 def testFilesRange_sameDirectory_remote():
-    pattern = ('http://proba2.oma.be/{instrument}/data/bsd/%Y/%m/%d/'
-               '{instrument}_lv1_%Y%m%d_%H%M%S.fits')
+    pattern = ('http://proba2.oma.be/{instrument}/data/bsd/{{year:4d}}/{{month:2d}}/{{day:2d}}/'
+               '{instrument}_lv1_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}.fits')
     s = Scraper(pattern, instrument='swap')
     startdate = parse_time((2014, 5, 14, 0, 0))
     enddate = parse_time((2014, 5, 14, 0, 5))
@@ -195,7 +195,7 @@ def testFilesRange_sameDirectory_remote():
 @pytest.mark.remote_data
 def testFilesRange_sameDirectory_months_remote():
     pattern = ('http://www.srl.caltech.edu/{spacecraft}/DATA/{instrument}/'
-               'Ahead/1minute/AeH%y%b.1m')
+               'Ahead/1minute/AeH{{year:4d}}%b.1m')
     s = Scraper(pattern, spacecraft='STEREO', instrument='HET')
     startdate = parse_time((2007, 8, 1))
     enddate = parse_time((2007, 9, 10))
@@ -208,7 +208,7 @@ def testFilesRange_sameDirectory_months_remote():
 
 @pytest.mark.remote_data
 def test_ftp():
-    pattern = 'ftp://solar-pub.nao.ac.jp/pub/nsro/norh/data/tcx/%Y/%m/tca%y%m%d'
+    pattern = 'ftp://solar-pub.nao.ac.jp/pub/nsro/norh/data/tcx/{{year:4d}}/{{month:2d}}/tca{{year:4d}}{{month:2d}}{{day:2d}}'
     s = Scraper(pattern)
     timerange = TimeRange('2016/5/18 15:28:00', '2016/5/20 16:30:50')
     urls = s.filelist(timerange)
@@ -221,7 +221,7 @@ def test_ftp():
 def test_filelist_url_missing_directory():
     # Asserts solution to ticket #2684.
     # Attempting to access data for the year 1960 results in a 404, so no files are returned.
-    pattern = 'http://lasp.colorado.edu/eve/data_access/evewebdataproducts/level2/%Y/%j/'
+    pattern = 'http://lasp.colorado.edu/eve/data_access/evewebdataproducts/level2/{{year:4d}}/%j/'
     s = Scraper(pattern)
     timerange = TimeRange('1960/01/01 00:00:00', '1960/01/02 00:00:00')
     assert len(s.filelist(timerange)) == 0
@@ -230,7 +230,7 @@ def test_filelist_url_missing_directory():
 @pytest.mark.remote_data
 def test_filelist_relative_hrefs():
     # the url opened by the scraper from below pattern contains some links which don't have hrefs
-    pattern = 'http://www.bbso.njit.edu/pub/archive/%Y/%m/%d/bbso_halph_fr_%Y%m%d_%H%M%S.fts'
+    pattern = 'http://www.bbso.njit.edu/pub/archive/{{year:4d}}/{{month:2d}}/{{day:2d}}/bbso_halph_fr_{{year:4d}}{{month:2d}}{{day:2d}}_{{hour:2d}}{{month:2d}}{{second:2d}}.fts'
     s = Scraper(pattern)
     timerange = TimeRange('2016/5/18 15:28:00', '2016/5/18 16:30:00')
     assert s.domain == 'http://www.bbso.njit.edu/'
@@ -241,9 +241,9 @@ def test_filelist_relative_hrefs():
 
 
 @pytest.mark.parametrize(('pattern', 'check_file'), [
-    (r'MyFile_%Y_%M_%e\.(\D){2}\.fits', 'MyFile_2020_55_234.aa.fits'),
+    (r'MyFile_{{year:4d}}_{{month:2d}}_{{millisecond:6d}}\.(\D){2}\.fits', 'MyFile_2020_55_234.aa.fits'),
     (r'(\d){5}_(\d){2}\.fts', '01122_25.fts'),
-    (r'_%Y%m%d__%ec(\d){5}_(\d){2}\s.fts', '_20201535__012c12345_33 .fts')])
+    (r'_{{year:4d}}{{month:2d}}{{day:2d}}__{{millisecond:6d}}c(\d){5}_(\d){2}\s.fts', '_20201535__012c12345_33 .fts')])
 def test_regex(pattern, check_file):
     s = Scraper(pattern, regex=True)
     assert s._URL_followsPattern(check_file)
@@ -252,7 +252,7 @@ def test_regex(pattern, check_file):
 @pytest.mark.remote_data
 def test_regex_data():
     prefix = r'https://gong2.nso.edu/oQR/zqs/'
-    pattern = prefix + r'%Y%m/mrzqs%y%m%d/mrzqs%y%m%dt%H%Mc(\d){4}_(\d){3}\.fits.gz'
+    pattern = prefix + r'{{year:4d}}{{month:2d}}/mrzqs{{year:4d}}{{month:2d}}{{day:2d}}/mrzqs{{year:4d}}{{month:2d}}{{day:2d}}t{{hour:2d}}{{month:2d}}c(\d){4}_(\d){3}\.fits.gz'
     s = Scraper(pattern, regex=True)
     timerange = TimeRange('2020-01-05', '2020-01-06T16:00:00')
     assert s._URL_followsPattern(prefix + '202001/mrzqs200106/mrzqs200106t1514c2226_297.fits.gz')
@@ -261,7 +261,7 @@ def test_regex_data():
 
 @pytest.mark.remote_data
 def test_extract_files_meta():
-    baseurl0 = r'ftp://solar-pub.nao.ac.jp/pub/nsro/norh/data/tcx/%Y/%m/(\w){3}%y%m%d'
+    baseurl0 = r'ftp://solar-pub.nao.ac.jp/pub/nsro/norh/data/tcx/{{year:4d}}/{{month:2d}}/(\w){3}{{year:4d}}{{month:2d}}{{day:2d}}'
     extractpattern0 = '{}/tcx/{year:4d}/{month:2d}/{wave}{:4d}{day:2d}'
     s0 = Scraper(baseurl0, regex=True)
     timerange0 = TimeRange('2020/1/1 4:00', '2020/1/2')
@@ -272,7 +272,7 @@ def test_extract_files_meta():
     assert metalist0[1]['day'] == 2
 
     prefix = r'https://gong2.nso.edu/oQR/zqs/'
-    baseurl1 = prefix + r'%Y%m/mrzqs%y%m%d/mrzqs%y%m%dt%H%Mc(\d){4}_(\d){3}\.fits.gz'
+    baseurl1 = prefix + r'{{year:4d}}{{month:2d}}/mrzqs{{year:4d}}{{month:2d}}{{day:2d}}/mrzqs{{year:4d}}{{month:2d}}{{day:2d}}t{{hour:2d}}{{month:2d}}c(\d){4}_(\d){3}\.fits.gz'
     extractpattern1 = ('{}/zqs/{year:4d}{month:2d}/mrzqs{:4d}{day:2d}/mrzqs{:6d}t'
                        '{hour:2d}{minute:2d}c{CAR_ROT:4d}_{:3d}.fits.gz')
     s1 = Scraper(baseurl1, regex=True)
@@ -299,7 +299,7 @@ def test_get_timerange_with_extractor(exdict, start, end):
 def test_yearly_overlap():
     # Check that a time range that falls within the interval that a file represents
     # returns a single result.
-    pattern = "https://www.ngdc.noaa.gov/stp/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/goes-xrs-report_%Y.txt"
+    pattern = "https://www.ngdc.noaa.gov/stp/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/goes-xrs-report_{{year:4d}}.txt"
     scraper = Scraper(pattern)
 
     # Should return a single file for 2013
@@ -308,7 +308,7 @@ def test_yearly_overlap():
 
 
 def test_check_timerange():
-    s = Scraper('%Y.fits')
+    s = Scraper('{{year:4d}}.fits')
     # Valid time range for 2014.fits is the whole of 2014
     # Test different cases to make sure check_timerange is working as expected
 
