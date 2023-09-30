93552594b87e3b48ec7e6b29d78da80f6cc71024
Trestan Simon <trestansimon@gmail.com>
2023-09-30 02:27:47 -0600

    Add remaining definitions from pfsspy

--- sunpy/map/sources/gong.py
+++ sunpy/map/sources/gong.py
@@ -3,10 +3,12 @@ GONG Map subclass definitions
 """
 import numpy as np
 from astropy.time import Time
+import astropy.units as u
 
 from sunpy.map import GenericMap
+from sunpy.coordinates import get_earth, HeliographicStonyhurst
 
-__all__ = ['GongSynopticMap']
+__all__ = ['GongSynopticMap', 'ADAPTMap']
 
 
 class GongSynopticMap(GenericMap):
@@ -39,3 +41,43 @@ class GongSynopticMap(GenericMap):
         """Determines if header corresponds to an GONG map."""
         return (str(header.get('TELESCOP', '')).endswith('GONG') and
                 str(header.get('CTYPE1', '').startswith('CRLN')))
+
+
+class ADAPTMap(GenericMap):
+    def __init__(self, data, header, **kwargs):
+        if 'date-obs' not in header:
+            header['date-obs'] = header['maptime']
+        # Fix CTYPE
+        if header['ctype1'] == 'Long':
+            header['ctype1'] = 'CRLN-CAR'
+        if header['ctype2'] == 'Lat':
+            header['ctype2'] = 'CRLT-CAR'
+
+        super().__init__(data, header, **kwargs)
+
+    @classmethod
+    def is_datasource_for(cls, data, header, **kwargs):
+        """Determines if header corresponds to an ADAPT map."""
+        return header.get('model') == 'ADAPT'
+
+
+def _observer_coord_meta(observer_coord):
+    """
+    Convert an observer coordinate into FITS metadata.
+    """
+    new_obs_frame = HeliographicStonyhurst(
+        obstime=observer_coord.obstime)
+    observer_coord = observer_coord.transform_to(new_obs_frame)
+
+    new_meta = {}
+    new_meta['hglt_obs'] = observer_coord.lat.to_value(u.deg)
+    new_meta['hgln_obs'] = observer_coord.lon.to_value(u.deg)
+    new_meta['dsun_obs'] = observer_coord.radius.to_value(u.m)
+    return new_meta
+
+
+def _earth_obs_coord_meta(obstime):
+    """
+    Return metadata for an Earth obeserver coordinate.
+    """
+    return _observer_coord_meta(get_earth(obstime))
