87db75083ecb97d37c0e95cf536d24f9c5c6402b
David Stansby <dstansby@gmail.com>
2023-10-17 22:19:39 +0100

    Remove deprecated sunpy.io.cdf (#7240)

--- sunpy/io/cdf.py
+++
@@ -1,9 +0,0 @@
-from sunpy.util.exceptions import warn_deprecated
-from . import _cdf
-from ._cdf import *  # NOQA
-
-__doc__ = _cdf.__doc__
-__all__ = _cdf.__all__
-
-warn_deprecated("The `sunpy.io.cdf` module is deprecated, as it was designed "
-                "for internal use.")
