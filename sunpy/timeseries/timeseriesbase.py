9b8e53c386c7d4c7cd999f89ab6921d4613add27
David Stansby <dstansby@gmail.com>
2024-01-22 20:49:23 +0000

    Backport PR #7391: ignore filter for pandas

--- sunpy/timeseries/timeseriesbase.py
+++ sunpy/timeseries/timeseriesbase.py
@@ -866,7 +866,7 @@ class GenericTimeSeries:
         Parameters
         ----------
         **kwargs : `dict`
-            All keyword arguments are passed to `pandas.DataFrame.to_numpy`.
+            All keyword arguments are passed to ``pandas.DataFrame.to_numpy``.
 
         Returns
         -------
