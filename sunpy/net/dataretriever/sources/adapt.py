1af871d7bcbcc0e15b2b7a958dfeb63ab76827d6
gillyspace27 <gilly@swri.org>
2024-02-29 01:38:56 -0700

    updated adapt code

--- sunpy/net/dataretriever/sources/adapt.py
+++ sunpy/net/dataretriever/sources/adapt.py
@@ -1,11 +1,74 @@
-from sunpy.net import attrs
-from sunpy.net.dataretriever import GenericClient
-from sunpy.net.dataretriever.attrs.adapt import *
-import astropy.units as u
-import sunpy.coordinates
 
-#the names of all the classes in this file as a list
-__all__ = ['ADAPTClient', 'carrington_time']
+import sunpy.net
+from sunpy.net import attr, attrs
+from sunpy.net.dataretriever import GenericClient
+
+class ADAPTFileType(attr.SimpleAttr):
+    """
+    ADAPT file type: Public.
+    """
+    pass
+
+class ADAPTLngType(attr.SimpleAttr):
+    """
+    ADAPT longitude type: Carrington Fixed, Central Meridian, East Limb.
+    """
+    pass
+
+class ADAPTInputSource(attr.SimpleAttr):
+    """
+    ADAPT input source: All, KPVT, VSM, GONG, HMI, MDI, MWO.
+    """
+    pass
+
+class ADAPTDataAssimilation(attr.SimpleAttr):
+    """
+    ADAPT data assimilation: WH, enLS, enkf, enLAKF.
+    """
+    pass
+
+class ADAPTResolution(attr.SimpleAttr):
+    """
+    ADAPT model spatial resolution: 1.0 deg, 0.2 deg.
+    """
+    pass
+
+class ADAPTVersionYear(attr.SimpleAttr):
+    """
+    ADAPT code version year.
+    """
+    pass
+
+class ADAPTVersionMonth(attr.SimpleAttr):
+    """
+    ADAPT code version month.
+    """
+    pass
+
+class ADAPTEvolutionMode(attr.SimpleAttr):
+    """
+    ADAPT evolution mode: Data assimilation step, Intermediate step, Forecast step.
+    """
+    pass
+
+class ADAPTHelioData(attr.SimpleAttr):
+    """
+    ADAPT helioseismic data: Not added or no data, Far-side, Emergence, Both emergence & far-side.
+    """
+    pass
+
+class ADAPTRealizations(attr.SimpleAttr):
+    """
+    ADAPT realizations: number of model/file realizations, e.g., 16 -> "016"
+    """
+    pass
+
+
+class ADAPTMagData(attr.SimpleAttr):
+    """
+    ADAPT magnetic data: Not added or no data, Mag-los, Mag-vector, Mag- both los & vector, Mag- polar avg obs, Mag- los & polar, Mag- vector & polar, Mag- both los and vector & polar.
+    """
+    pass
 
 
 class ADAPTClient(GenericClient):
@@ -13,9 +76,13 @@ class ADAPTClient(GenericClient):
     baseurl = r'https://gong.nso.edu/adapt/maps/gong/%Y/adapt(\d){5}_(\d){2}(\w){1}(\d){3}_(\d){12}_(\w){1}(\d){8}(\w){1}(\d){1}\.fts\.gz'
     pattern = '{}adapt{ADAPTFileType:1d}{ADAPTLngType:1d}{ADAPTInputSource:1d}{ADAPTDataAssimilation:1d}{ADAPTResolution:1d}_{ADAPTVersionYear:2d}{ADAPTVersionMonth:1l}{ADAPTRealizations:3d}_{year:4d}{month:2d}{day:2d}{hour:2d}{minute:2d}_{ADAPTEvolutionMode:1l}{days_since_last_obs:2d}{hours_since_last_obs:2d}{minutes_since_last_obs:2d}{seconds_since_last_obs:2d}{ADAPTHelioData:1l}{ADAPTMagData:1d}.fts.gz'
 
+
+
     @classmethod
     def register_values(cls):
-        adict ={attrs.Instrument: [('ADAPT', 'ADvanced Adaptive Prediction Technique.')],
+        from sunpy.net import attrs as a
+
+        adict = {attrs.Instrument: [('ADAPT', 'ADvanced Adaptive Prediction Technique.')],
                 attrs.Source: [('NSO', 'National Solar Observatory.')],
                 attrs.Provider: [('GONG', 'Global Oscillation Network Group.')],
                 ADAPTFileType: [('4', 'Public')],
@@ -31,42 +98,15 @@ class ADAPTClient(GenericClient):
                 ADAPTMagData: [('0', 'Not added or no data'), ('1', 'Mag-los'), ('2', 'Mag-vector'), ('3', 'Mag- both los & vector'), ('4', 'Mag- polar avg obs'), ('5', 'Mag- los & polar'), ('6', 'Mag- vector & polar'), ('7', 'Mag- both los and vector & polar')]
                 }
         return adict
-    
-    @classmethod
-    def _attrs_module(cls):
-        return 'adapt', 'sunpy.net.dataretriever.attrs.adapt'
 
     @classmethod
     def _can_handle_query(cls, *query):
         required = {attrs.Instrument, attrs.Time}
 
-        optional = {ADAPTFileType, ADAPTLngType, ADAPTInputSource, ADAPTDataAssimilation, 
-                    ADAPTResolution, ADAPTVersionYear, ADAPTVersionMonth, ADAPTEvolutionMode, 
+        optional = {ADAPTFileType, ADAPTLngType, ADAPTInputSource, ADAPTDataAssimilation,
+                    ADAPTResolution, ADAPTVersionYear, ADAPTVersionMonth, ADAPTEvolutionMode,
                     ADAPTHelioData, ADAPTMagData}
 
         all_attrs = {type(x) for x in query}
         # print(all_attrs)
         return required.issubset(all_attrs) and all_attrs.issubset(required.union(optional))
-    
-    @property
-    def info_url(cls):
-        return ("https://nso.edu/data/nisp-data/adapt-maps/")
-
-def carrington_time(CR=2193, frames=1):
-    """Get the start date for the start of a carrington rotation, 
-    and a duration that will retrieve a given number of frames
-
-    Returns:
-        _type_: _description_
-    """
-
-    date = sunpy.coordinates.sun.carrington_rotation_time(CR)
-    date_end = date + frames*(3*1.9999999 * u.hour)
-
-    # Format the Search Dates
-    tstring = r"%Y-%m-%dT%H:%M:%S"
-    get_date    =date.strftime(tstring)
-    get_date_end=date_end.strftime(tstring)
-    return get_date, get_date_end
-
-
