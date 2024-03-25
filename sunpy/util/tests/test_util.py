c4b866b571c6ddcc6384023e61128f4bc244905b
Ansh Dixit <awesomeansh03@gmail.com>
2024-03-26 02:10:28 +0530

    removed byte str unit tests

--- sunpy/util/tests/test_util.py
+++ sunpy/util/tests/test_util.py
@@ -50,8 +50,8 @@ def test_expand_list():
 
 
 def test_expand_list_generator():
-    lst = ['a', 'b', [], (['c', 'd']), tuple(), ['e'], b'f']
-    assert list(util.expand_list_generator(lst)) == ['a', 'b', 'c', 'd', 'e', 'f']
+    lst = ['a', 'b', [], (['c', 'd']), tuple(), ['e']]
+    assert list(util.expand_list_generator(lst)) == ['a', 'b', 'c', 'd', 'e']
 
 
 def test_partial_key_match():
