5625a6c3d9e923d60b609ef4b238c17a9a923b08
Nabil Freij <nabil.freij@gmail.com>
2024-03-28 16:27:47 -0700

    Fix tests and broken call

--- sunpy/io/__init__.py
+++ sunpy/io/__init__.py
@@ -6,7 +6,7 @@ and is not added to the API reference pages.
 """
 from sunpy.util.exceptions import warn_deprecated
 
-warn_deprecated('6.0', 'This subpackage is not intended for public use and will not be importable in the future.')
-from sunpy.io._file_tools import  read_file, write_file
+warn_deprecated('The toplevel space of the io subpackage was never intended for public use and will be removed in the future.')
+from sunpy.io._file_tools import read_file, write_file
 
 __all__ = ['read_file', 'write_file']
