b939d1154f36292055959bd9c65379e5beecf046
Ahmed Hossam <hoss4614@gmail.com>
2024-04-01 22:55:53 +0200

    add test_client_search_limit

--- sunpy/net/helio/tests/test_helio.py
+++ sunpy/net/helio/tests/test_helio.py
@@ -294,13 +294,19 @@ def test_client_search(client):
     res = client.search(a.Time(start, end), a.helio.TableName(table_name), a.helio.MaxRecords(10))
     assert len(res) == 10
 
-    with pytest.raises(ValueError, match="Helio will only return a max of 20000 results."):
-        res = client.search(a.Time(start, end), a.helio.TableName(table_name), a.helio.MaxRecords(99999))
-
     with pytest.warns(SunpyUserWarning, match="Number of results is the same as current limit. "
                       "It is possible your query has been truncated. "
                       "If you want to change this, set `a.helio.MaxRecords` to a higher value."):
         res = client.search(a.Time(start, end), a.helio.TableName(table_name), a.helio.MaxRecords(10))
+
+
+def test_client_search_limit(client):
+    start = '2005/01/03'
+    end = '2005/12/03'
+    table_name = 'rhessi_hxr_flare'
+
+    with pytest.raises(ValueError, match="Helio will only return a max of 20000 results."):
+        res = client.search(a.Time(start, end), a.helio.TableName(table_name), a.helio.MaxRecords(99999))
 
 
 @pytest.mark.remote_data
