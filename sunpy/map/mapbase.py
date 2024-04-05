d6ad2f7acb3273581226e554f261c079ee52f0fc
Nabil Freij <nabil.freij@gmail.com>
2024-04-05 08:53:46 -0700

    Merge pull request #7551 from ayshih/arm
    Fixed an unnecessary division computation for an unsupported division by a Map
    
    (cherry picked from commit 5dde5790b692c77385a5af85835ea705557c74c8)

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -566,6 +566,7 @@ class GenericMap(NDData):
     def __rmul__(self, value):
         return self.__mul__(value)
 
+    @check_arithmetic_compatibility
     def __truediv__(self, value):
         return self.__mul__(1/value)
 
