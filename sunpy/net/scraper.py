20d30d2b129db85db3421d4a61a8be87617162db
ryuusama <shahh8138@gmail.com>
2024-04-06 12:27:37 +0530

    parametrized

--- sunpy/net/scraper.py
+++ sunpy/net/scraper.py
@@ -275,7 +275,10 @@ class Scraper:
                     opn.close()
             except HTTPError as http_err:
                 # Ignore missing directories (issue #2684).
-                if http_err.code in [400, 404, 403]:
+                if http_err.code == 404:
+                    log.debug(f"Directory {directory} not found.")
+                    continue
+                if http_err.code in [400, 403]:
                     log.debug(f"Got error {http_err.code} while scraping {directory} : {http_err.reason}")
                     raise
                 if http_err.code in [429, 504]:
