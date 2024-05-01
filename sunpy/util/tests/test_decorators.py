a2fa6766207eea5a1d99817350a00c8f4a511bf5
Nabil Freij <nabil.freij@gmail.com>
2024-05-01 18:35:18 +0100

    Updated deprecated to use astropy underneath as we can pass in the warning type

--- sunpy/util/tests/test_decorators.py
+++ sunpy/util/tests/test_decorators.py
@@ -5,42 +5,27 @@ import pytest
 import astropy.units as u
 
 from sunpy.util.decorators import (
-    active_contexts,
+    ACTIVE_CONTEXTS,
     deprecate_positional_args_since,
     deprecated,
-    get_removal_version,
     sunpycontextmanager,
 )
-from sunpy.util.exceptions import SunpyDeprecationWarning, SunpyPendingDeprecationWarning
-
-
-def test_removal_version_since_lts():
-    major, minor = get_removal_version('2.0')
-    assert major == 2
-    assert minor == 1
-
-
-def test_removal_version_not_since_lts():
-    major, minor = get_removal_version('2.1')
-    assert major == 3
-    assert minor == 1
+from sunpy.util.exceptions import SunpyDeprecationWarning
 
 
 @pytest.mark.parametrize(
-    ('since', 'pending', 'warning', 'message', 'warning_message'),
+    ('since', 'warning', 'message', 'warning_message'),
     [
-        ('2.0', False, SunpyDeprecationWarning, '',
-         'The foo function is deprecated and may be removed in version 2.1.'),
-        ('2.1', False, SunpyDeprecationWarning, '',
-         'The foo function is deprecated and may be removed in version 3.1.'),
-        ('2.0', True, SunpyPendingDeprecationWarning, '',
-         'The foo function will be deprecated in version 2.0.'),
-        ('2.0', False, SunpyDeprecationWarning,
+        ('2.0', SunpyDeprecationWarning, '',
+         'The foo function is deprecated and may be removed in a future version'),
+        ('2.1', SunpyDeprecationWarning, '',
+         'The foo function is deprecated and may be removed in a future version'),
+        ('2.0', SunpyDeprecationWarning,
          'Custom deprecation message', 'Custom deprecation message'),
     ]
 )
-def test_deprecated_warning_message(since, pending, warning, message, warning_message):
-    @deprecated(since, pending=pending, message=message)
+def test_deprecated_warning_message(since, warning, message, warning_message):
+    @deprecated(since, message=message)
     def foo():
         pass
     with pytest.warns(warning, match=warning_message):
@@ -99,11 +84,11 @@ def somefunc():
 
 def test_somefunc_context():
     # Check that the context is not active before entering
-    assert not active_contexts.get('somefunc' , False)
+    assert not ACTIVE_CONTEXTS.get('somefunc' , False)
 
     with somefunc():
         # Check that the context is active while inside
-        assert active_contexts.get('somefunc', False)
+        assert ACTIVE_CONTEXTS.get('somefunc', False)
 
     # Check that the context is not active after exiting
-    assert not active_contexts.get('somefunc' , False)
+    assert not ACTIVE_CONTEXTS.get('somefunc' , False)
