d0f5d98c632cf6971d7014761377fadbc0022ed8
Ahmed Hossam <hoss4614@gmail.com>
2024-05-22 17:36:45 +0300

    remove is_coord_prop from get_unit calls

--- sunpy/net/hek/utils.py
+++ sunpy/net/hek/utils.py
@@ -64,7 +64,7 @@ def parse_columns_to_table(table, attributes, is_coord_prop = False):
                 elif attribute.get("is_chaincode", False):
                     new_column.append(parse_chaincode(value, idx, attribute, table[attribute["unit_prop"]][idx]))
                 else:
-                    new_column.append(value * get_unit(attribute["unit_prop"], table[unit_attr][idx], is_coord_prop = is_coord_prop))
+                    new_column.append(value * get_unit(attribute["unit_prop"], table[unit_attr][idx]))
             table[attribute["name"]] = new_column
 
     for attribute in attributes:
@@ -83,7 +83,7 @@ def parse_unit(table, attribute, is_coord_prop = False):
         unit_attr = attribute["unit_prop"]
     for row in table:
         if unit_attr in table.colnames and row[unit_attr] not in ["", None] and table[attribute["name"]].unit is not None:
-            table[attribute["name"]].unit = get_unit(attribute["unit_prop"], row[unit_attr], is_coord_prop = is_coord_prop)
+            table[attribute["name"]].unit = get_unit(attribute["unit_prop"], row[unit_attr])
             break
     return table
 
@@ -98,8 +98,8 @@ def parse_chaincode(value, idx, attribute, unit_prop):
         coord1_unit = u.R_sun
         coord2_unit = u.deg
     elif attribute["frame"] == "icrs":
-        coord1_unit = get_unit("coord1_unit", unit_prop, is_coord_prop = True)
-        coord2_unit = get_unit("coord2_unit", unit_prop, is_coord_prop = True)
+        coord1_unit = get_unit("coord1_unit", unit_prop)
+        coord2_unit = get_unit("coord2_unit", unit_prop)
 
     coordinates_str = value.split('((')[1].split('))')[0]
     coord1_list = [float(coord.split()[0]) for coord in coordinates_str.split(',')] * coord1_unit
@@ -114,7 +114,7 @@ def parse_chaincode(value, idx, attribute, unit_prop):
 
 # NOTE: Needs unit test
 def get_unit(unit_prop, str):
--   """Converts str into astropy unit."""
+    """Converts str into astropy unit."""
     cm2 = u.def_unit("cm2", u.cm**3)
     m2 = u.def_unit("m2", u.m**2)
     m3 = u.def_unit("m3", u.m**3)
