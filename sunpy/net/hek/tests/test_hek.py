198c4842fd9f8a429da94cd3281e6a611f6892e4
Ahmed Hossam <hoss4614@gmail.com>
2024-06-24 22:25:27 +0300

    move tests to test_utils

--- sunpy/net/hek/tests/test_hek.py
+++ sunpy/net/hek/tests/test_hek.py
@@ -1,34 +1,15 @@
 import copy
-import json
 
-import numpy as np
 import pytest
-from regions import PolygonSkyRegion
 
-from astropy import units as u
 from astropy.time import Time
 
 from sunpy.net import attr, attrs, hek
-from sunpy.net.hek.utils import COORD_FILE_PATH, UNIT_FILE_PATH
 
 
 @pytest.fixture
 def foostrwrap(request):
     return hek.attrs._StringParamAttrWrapper("foo")
-
-@pytest.fixture
-def read_unit_attributes():
-    with open(UNIT_FILE_PATH) as unit_file:
-        unit_properties = json.load(unit_file)
-
-    return unit_properties
-
-@pytest.fixture
-def read_coord_attributes():
-    with open(COORD_FILE_PATH) as coord_file:
-        coord_properties = json.load(coord_file)
-
-    return coord_properties
 
 class HEKResult:
     """
@@ -236,42 +217,3 @@ def test_query_multiple_operators():
                             attrs.hek.FL.GOESCls > "M1.0",
                             attrs.hek.OBS.Observatory == "GOES")
     assert len(results) == 7
-
-@pytest.mark.remote_data
-def test_astropy_unit_parsing(read_unit_attributes, read_coord_attributes):
-    client = hek.HEKClient()
-    tstart = '2014/10/24 20:50'
-    tend = '2014/10/25 00:14'
-    event_type = 'FL'
-    result = client.search(attrs.Time(tstart, tend), attrs.hek.EventType(event_type))
-    unit_properties = read_unit_attributes
-    coord_properties = read_coord_attributes
-    unit_attributes_with_unit = [ prop for prop in unit_properties["attributes"] if prop.get("unit_prop",None) is not None]
-    coord_attributes_with_unit = [prop for prop in coord_properties["attributes"] if not prop.get("is_chaincode", False) and not prop.get("is_unit_prop",False)]
-
-    for attribute in unit_attributes_with_unit + coord_attributes_with_unit:
-        if attribute["name"] in result.colnames:
-            assert all([value in ['', None] or isinstance(value, u.Quantity) for value in result[attribute['name']]])
-
-
-@pytest.mark.remote_data
-def test_chaincode_parsing(read_coord_attributes):
-    client = hek.HEKClient()
-    tstart = '2014/10/24 20:50'
-    tend = '2014/10/25 00:14'
-    event_type = 'FL'
-    result = client.search(attrs.Time(tstart, tend), attrs.hek.EventType(event_type))
-    coord_properties = read_coord_attributes
-    chaincode_properties = [prop for prop in coord_properties["attributes"] if prop.get("is_chaincode", False)]
-
-    for attribute in chaincode_properties:
-        if attribute["name"] in result.colnames:
-            assert all([value in ['', None] or isinstance(value, PolygonSkyRegion) for value in result[attribute['name']]])
-
-@pytest.mark.remote_data
-def test_missing_times():
-    # Check for https://github.com/sunpy/sunpy/pull/7627#issuecomment-2113451964
-    client = hek.HEKClient()
-    results = client.search(attrs.Time('2024-05-10', '2024-05-12'), attrs.hek.AR.NOAANum == 13664)
-    assert isinstance(results["event_peaktime"][0], np.ma.core.MaskedConstant)
-    assert results["event_peaktime"][6].isot == "2024-05-10T16:08:00.000"
