23ba0e53057ff52340ad52c031fe316faa2a943c
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2024-07-08 16:25:33 +0530

    Update _asdf.py

--- sunpy/io/_asdf.py
+++ sunpy/io/_asdf.py
@@ -4,7 +4,21 @@ import asdf_astropy
 __all__ = ["write"]
 def write(fname, data, header, **kwargs):
     # Create the ASDF file
-    "This function will write a asdf file "
+        """
+    Take data and header pairs and save it to asdf file 
+    inspired from https://docs.sunpy.org/en/stable/generated/gallery/saving_and_loading_data/genericmap_in_asdf.html
+
+
+    Parameters
+    ----------
+    fname : `str`
+        File name, with extension.
+    data : `numpy.ndarray`
+        n-dimensional data array.
+    header : `dict`
+        A header dictionary.
+
+    """
 
     meta = dict(header)
     with asdf.AsdfFile() as af:
