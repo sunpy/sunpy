4868e88902a3e3604f42791f2a6c736eb3109696
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2024-07-10 15:59:25 +0530

    added support for loading an asdf sunpy map
    added support for loading an asdf sunpy map which has been saved using map.save

--- sunpy/io/_asdf.py
+++ sunpy/io/_asdf.py
@@ -1,14 +1,21 @@
 import asdf
 import asdf_astropy
+from pathlib import Path
+import numpy as np 
+from sunpy.io.header import FileHeader
+from collections import OrderedDict
+__all__ = ["write","read","get_header"]
 
-__all__ = ["write"]
 def write(fname, data, header, **kwargs):
-    # Create the ASDF file
-        """
+    """
     Take data and header pairs and save it to asdf file 
     inspired from https://docs.sunpy.org/en/stable/generated/gallery/saving_and_loading_data/genericmap_in_asdf.html
 
+    format:
+    map_name.asdf (main tree)
+        meta: (header info)
 
+        data : data blocks
     Parameters
     ----------
     fname : `str`
@@ -19,9 +26,53 @@ def write(fname, data, header, **kwargs):
         A header dictionary.
 
     """
-
+    map_name = Path(fname)
+    map_name = map_name.name
     meta = dict(header)
     with asdf.AsdfFile() as af:
-        af.tree={"meta":meta,"data":data}
+        af.tree={map_name:{"meta":meta,"data":data}}
 
         af.write_to(fname)
+
+
+def read(fname,**kwargs):
+    """
+    A function to read asdf_file
+    parameters
+    ----------
+    fname : Str
+
+    returns : list of (data,meta) 
+    
+    """
+    with asdf.open(fname) as af:
+
+        map_name = Path(fname)
+        map_name = map_name .name
+
+        data = af[map_name]["data"].data
+        data_array = np.asarray(data)
+
+        meta_data= af[map_name]["meta"]
+        meta_data = OrderedDict(meta_data)
+        meta_data = FileHeader(meta_data)
+
+        return [(data_array,meta_data)]
+def get_header(fname):
+    """
+    read an asdf file and return the meta_data (meta) from the file
+    
+    """
+    with asdf.open(fname) as af:
+
+        map_name = Path(fname)
+        map_name = map_name .name
+
+
+
+        meta_data= af[map_name]["meta"]
+        meta_data = OrderedDict(meta_data)
+        meta_data = FileHeader(meta_data)
+
+        return [meta_data]
+    
