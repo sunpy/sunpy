597608c0f359dd64a9d29f1bd99dfb96cf45c34b
Sauravroy34 <sauravnotfound29@gmail.com>
2024-07-11 11:59:59 +0530

    fixed docstring

--- sunpy/io/_asdf.py
+++ sunpy/io/_asdf.py
@@ -33,7 +33,6 @@ def write(fname, data, header, **kwargs):
     meta = dict(header)
     with asdf.AsdfFile() as af:
         af.tree={map_name:{"meta":meta,"data":data}}
-
         af.write_to(fname)
 
 
@@ -53,18 +52,16 @@ def read(fname,**kwargs):
     """
 
     with asdf.open(fname) as af:
-
         map_name = Path(fname)
         map_name = map_name .name
-
         data = af[map_name]["data"].data
         data_array = np.asarray(data)
-
         meta_data= af[map_name]["meta"]
         meta_data = OrderedDict(meta_data)
         meta_data = FileHeader(meta_data)
-
         return [(data_array,meta_data)]
+
+
 def get_header(fname):
     """
     Read a asdf file and return just the headers for all HDU's.
@@ -83,11 +80,9 @@ def get_header(fname):
 
         map_name = Path(fname)
         map_name = map_name .name
-
-
-
         meta_data= af[map_name]["meta"]
         meta_data = OrderedDict(meta_data)
         meta_data = FileHeader(meta_data)
 
         return [meta_data]
+    
