93e35e63175e698337a001baa4f1615241ea3721
Sauravroy34 <sauravnotfound29@gmail.com>
2024-07-14 12:01:27 +0530

    minor fixes

--- sunpy/io/tests/test_asdf.py
+++ sunpy/io/tests/test_asdf.py
@@ -4,7 +4,7 @@ import asdf
 
 from sunpy.data.test import get_test_filepath
 from sunpy.io._asdf import get_header, get_keys_name, read, write
-from sunpy.io.header import FileHeader
+from sunpy.io._header import FileHeader
 from sunpy.map import Map
 
 AIA_MAP = get_test_filepath("aiamap_genericmap_1.0.0.asdf")
