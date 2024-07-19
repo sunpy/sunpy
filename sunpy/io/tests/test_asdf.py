3c9da3c09c54b34a998016e47dac3d998980b4f8
Sauravroy34 <sauravnotfound29@gmail.com>
2024-07-19 14:06:04 +0530

    added pytest fixture in test_asdf

--- sunpy/io/tests/test_asdf.py
+++ sunpy/io/tests/test_asdf.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import asdf
 
@@ -7,18 +8,21 @@ from sunpy.data.test import get_test_filepath
 from sunpy.io._asdf import get_header, get_keys_name, read, write
 from sunpy.io._header import FileHeader
 
-map_for_asdf = get_test_filepath("aiamap_genericmap_1.0.0.asdf")
-fits_map = get_test_filepath("aia_171_level1.fits")
 
+@pytest.fixture
+def test_data():
+    map_for_asdf = get_test_filepath("aiamap_genericmap_1.0.0.asdf")
+    fits_map = get_test_filepath("aia_171_level1.fits")
+    return [map_for_asdf,fits_map]
 def test_read():
-    cont = read(map_for_asdf)
+    cont = read(test_data[0])
     assert isinstance(cont, list)
     assert isinstance(cont[0][0], np.ndarray)
     assert isinstance(cont[0][1], FileHeader)
     assert cont[0][0].shape == (2, 2)
 
 def test_write_and_verify(tmpdir):
-    data, header = read(map_for_asdf)[0]
+    data, header = read(test_data[0])[0]
     outfile = tmpdir / "test.asdf"
     write(str(outfile), data, header)
     assert outfile.exists()
@@ -28,16 +32,16 @@ def test_write_and_verify(tmpdir):
     assert header == get_header(str(outfile))[0]
 
 def test_load_save_and_verify(tmpdir):
-    data ,header = read(map_for_asdf)[0]
-    map_obj = sunpy.map.Map(map_for_asdf)
+    data ,header = read(test_data[0])[0]
+    map_obj = sunpy.map.Map(test_data[0])
     outpath = tmpdir / "save.asdf"
     map_obj.save(str(outpath))
     assert outpath.exists()
     assert map_obj.meta == header
     assert np.array_equal(data,map_obj.data)
 
-def verify_data_and_header(tmpdir):
-    map_obj_fits = sunpy.map.Map(fits_map)
+def test_data_and_header(tmpdir):
+    map_obj_fits = sunpy.map.Map(test_data[1])
     outpah = tmpdir/"fits_map_in_asdf.asdf"
     map_obj_fits.save(str(outpah))
     assert outpah.exists()
@@ -46,11 +50,11 @@ def verify_data_and_header(tmpdir):
     assert np.array_equal(map_obj_asdf.data,map_obj_fits.data)
 
 def test_get_header():
-    header = get_header(map_for_asdf)[0]
+    header = get_header(test_data[0])[0]
     assert isinstance(header, FileHeader)
 
 def test_keys_name():
-    with asdf.open(map_for_asdf) as af:
+    with asdf.open(test_data[0]) as af:
         rootkeys = af.tree.keys()
         main_data_keys = [key for key in rootkeys if key not in ['asdf_library', 'history']]
-        assert get_keys_name(map_for_asdf) == main_data_keys[0]
+        assert get_keys_name(test_data[0]) == main_data_keys[0]
