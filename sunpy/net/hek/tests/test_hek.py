5d367213c289503be12932bd21f2914a5eb6e087
Ahmed Hossam <hoss4614@gmail.com>
2024-07-29 13:18:58 +0300

    add pytest.mark.remote_data to test_event_types

--- sunpy/net/hek/tests/test_hek.py
+++ sunpy/net/hek/tests/test_hek.py
@@ -365,6 +365,7 @@ def test_flares_python_logical_ops():
     for row in result[0]['fl_peakflux']:
         assert row.value > 1000.0
 
+@pytest.mark.remote_data
 @pytest.mark.parametrize("event_type", ['AR', 'CE', 'CD', 'CW', 'FI', 'FE', 'FA', 'LP', 'OS', 'SS', 'EF', 'CJ', 'PG', 'OT', 'NR', 'SG', 'SP', 'CR', 'CC', 'ER', 'TO', 'HY', 'BU', 'EE', 'PB', 'PT'])
 def test_event_types(event_type):
     tstart = '2017/09/06 11:59:04'
