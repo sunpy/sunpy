5d0f7909bbf24b4587c95b6596ec4905d861596b
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2024-09-03 22:46:12 +0530

    Update sunpy/map/tests/test_mapbase.py
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- sunpy/map/tests/test_mapbase.py
+++ sunpy/map/tests/test_mapbase.py
@@ -644,7 +644,7 @@ def test_save_asdf(tmpdir, aia171_test_map):
     aia171_test_map.save(outpath, filetype= "asdf")
     loaded_save_asdf = sunpy.map.Map(str(outpath))
     assert isinstance(loaded_save_asdf, sunpy.map.sources.AIAMap)
-   #Compare metadata without considering ordering of keys
+    # Compare metadata without considering ordering of keys
     assert dict(loaded_save_asdf.meta) == dict(aia171_test_map.meta)
     np.testing.assert_array_equal(loaded_save_asdf.data, aia171_test_map.data)
 
