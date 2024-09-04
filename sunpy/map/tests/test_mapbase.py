ce79317caa798fdbfee39bf260236a7220b271cb
Pratham2110 <prathamhole@gmail.com>
2024-09-05 00:45:06 +0530

    test fixed

--- sunpy/map/tests/test_mapbase.py
+++ sunpy/map/tests/test_mapbase.py
@@ -1454,7 +1454,7 @@ def test_submap_inputs(generic_map2, coords):
 def test_contour_deprecation_warning(simple_map):
 
     with pytest.warns(SunpyDeprecationWarning, match="The 'contour' method is deprecated and will be removed in a future release. Please use 'get_contours'."):
-        simple_map.contour([1.5, 2.5])
+        simple_map.contour(1.5)
 
 
 def test_get_contours_contourpy(simple_map):
