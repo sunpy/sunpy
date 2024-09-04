d80709ba8ea44de6c13b180dcd4a50e781404f09
Sauravroy34 <sauravnotfound29@gmail.com>
2024-09-04 20:28:55 +0530

    fixes

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -1615,6 +1615,7 @@ class GenericMap(NDData):
         Notes
         -----
         - Saving with the ``.jp2`` extension will cast the data to uint8 values to support the JPEG2000 format.
+
         - Saving with the ``.asdf`` extension will save the map as an ASDF file, storing the map's attributes
         under the key ``'sunpymap'`` in the ASDF tree.
 
