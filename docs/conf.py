92165aa566d8f7a69e519e26b8719df91bd95964
Stuart Mumford <stuart@cadair.com>
2024-09-30 11:41:10 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ if _version.is_postrelease:
 elif _version.is_devrelease:
     version = release = f'{_version.base_version}.dev{_version.dev}'
 is_development = _version.is_devrelease
+is_release = not(_version.is_prerelease or _version.is_devrelease)
 
 project = 'sunpy'
 author = 'The SunPy Community'
