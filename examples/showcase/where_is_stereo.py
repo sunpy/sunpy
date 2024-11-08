f2a03ba7fae4c9471783747ee459bf4a6edc18c7
Albert Y. Shih <ayshih@gmail.com>
2024-11-07 17:24:06 -0800

    Use a fixed time in the past for plot of STEREO locations

--- examples/showcase/where_is_stereo.py
+++ examples/showcase/where_is_stereo.py
@@ -27,7 +27,7 @@ from sunpy.time import parse_time
 ##############################################################################
 # Define the time for the plot as the time when this script is run.
 
-obstime = parse_time('now')
+obstime = parse_time('2024-Oct-17 20:23')
 
 ##############################################################################
 # Define a convenience function to extract the first full orbit from a
