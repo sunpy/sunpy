ff6f0cf309ac391b4dbe19675d7becd4afae818e
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2024-11-15 01:06:50 +0530

    Update solarnet.py

--- sunpy/net/solarnet/solarnet.py
+++ sunpy/net/solarnet/solarnet.py
@@ -1,9 +1,21 @@
 
 from sunpy.net.base_client import BaseClient
+from sunpy.net.solarnet import attrs as a
+import requests
 
 base_url = "https://solarnet2.oma.be/service/api/svo/{}"
 
 
 class SolarnetClient(BaseClient):
 
-    pass
+    def search(self,*query,**kwargs):
+        query = and_(*query)
+        block = a.walker.create(query)[0]
+        if "datasets" in block:
+            url = base_url.format(block["datasets"])
+            block.pop("datasets")
+        req = requests.get(url,params=block)
+        data = req.json()["objects"]
+        for i in data:
+            link = i["data_location]["file_url"]
+            
