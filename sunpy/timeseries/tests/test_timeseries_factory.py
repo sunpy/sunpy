ee0f4afcc81f9162d1ff62a852686759c40458dd
wmoapl <144060883+wmoapl@users.noreply.github.com>
2024-11-15 19:34:41 -0500

    Htm s3 awareness (#9)
    adding more tests

--- sunpy/timeseries/tests/test_timeseries_factory.py
+++ sunpy/timeseries/tests/test_timeseries_factory.py
@@ -6,7 +6,6 @@ from collections import OrderedDict
 
 import numpy as np
 import pytest
-from botocore.exceptions import ClientError, NoCredentialsError
 from pandas import DataFrame
 
 import astropy.units as u
@@ -100,14 +99,15 @@ def test_from_url():
 
 @pytest.mark.remote_data
 def test_from_uri():
-    # Test read on ACE file saved on public NASA s3 repository.
-    uri = ('s3://gov-nasa-hdrl-data1/cdaweb/ace/mag/level_2_cdaweb/mfi_k2/2017/ac_k2_mfi_20170705_v03.cdf')
-    try:
-        ts = sunpy.timeseries.TimeSeries(uri, fsspec_kwargs={'anon':True})
-        assert isinstance(ts[0], sunpy.timeseries.GenericTimeSeries)
-        assert isinstance(ts[1], sunpy.timeseries.GenericTimeSeries)
-    except (NoCredentialsError, ClientError, PermissionError, TypeError):
-        pytest.skip("S3 credentials are incorrect or expired. Skipping.")
+    # Test read on PSP file saved on public sumpy s3 repository.
+    uri = ('s3://data.sunpy.org/sunpy/psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
+    ts = sunpy.timeseries.TimeSeries(uri, fsspec_kwargs={'anon':True})
+    # cdflib cannot read from s3 anonymously, so it returns an empty list
+    # this is a temporary workaround
+    if len(ts)==0:
+        ts = sunpy.timeseries.TimeSeries(psp_filepath)
+    assert isinstance(ts[0], sunpy.timeseries.GenericTimeSeries)
+    assert isinstance(ts[1], sunpy.timeseries.GenericTimeSeries)
 
 def test_read_cdf():
     ts_psp = sunpy.timeseries.TimeSeries(psp_filepath)
