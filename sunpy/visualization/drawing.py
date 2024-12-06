535c46af01a57c653ff067e291dc0382a6df17d4
Will Barnes <will.t.barnes@gmail.com>
2024-12-05 17:27:43 -0700

    Update drawing.py
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- sunpy/visualization/drawing.py
+++ sunpy/visualization/drawing.py
@@ -203,7 +203,7 @@ def extent(axes, wcs, *, rsun: u.m = R_sun, **kwargs):
     ----------
     axes : `astropy.visualization.wcsaxes.WCSAxes`
         The axes to plot the prime meridian on, or "None" to use current axes.
-    wcs : `~astropy.wcs.WCS`
+    wcs : `~astropy.wcs.wcsapi.BaseLowLevelWCS`
         The WCS that defines the extent to be drawn.
     rsun : `~astropy.units.Quantity`
         Solar radius (in physical length units) at which to draw the solar
