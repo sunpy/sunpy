63043f9778763ffe9995d4ec2eb6918e6808165a
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2024-12-31 02:49:51 +0530

    Update sunpy/map/sources/soho.py
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/map/sources/soho.py
+++ sunpy/map/sources/soho.py
@@ -136,13 +136,10 @@ class LASCOMap(GenericMap):
         """
         ctype1 = self.meta.get('ctype1', None)
         ctype2 = self.meta.get('ctype2', None)
-
         if ctype1.lower() in ("solar-x", "solar_x"):
             ctype1 = 'HPLN-TAN'
-
         if ctype2.lower() in ("solar-y", "solar_y"):
             ctype2 = 'HPLT-TAN'
-
         return SpatialPair(ctype1, ctype2)
 
     @property
