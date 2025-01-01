8d076bb636d5327aeba178e0e5e6a9331b7762bc
Nabil Freij <nabil.freij@gmail.com>
2025-01-01 14:01:51 -0800

    Online tests

--- sunpy/net/tests/test_fido.py
+++ sunpy/net/tests/test_fido.py
@@ -206,8 +206,8 @@ def test_repr():
         a.Time("2012/1/1", "2012/1/2"), a.Instrument.lyra)
     rep = repr(results)
     rep = rep.split('\n')
-    # 9 header lines, the results table and two blank lines at the end
-    assert len(rep) == 9 + len(list(results)[0]) + 2
+    # 6 preamble lines, 2 table header rule, the results table data and two blank lines at the end
+    assert len(rep) == 6 + 2 + len(results[0]) + 2
 
 
 def filter_queries(queries):
