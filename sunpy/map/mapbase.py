d27f026faae604e2257b3f188201832a4f9effa0
Prtm2110 <prathamhole@gmail.com>
2025-01-01 23:46:04 +0530

    Indent fix

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -2646,13 +2646,13 @@ class GenericMap(NDData):
             axes (e.g., a difference in rotation angle).
 
             - If ``pcolormesh``, this method will use :meth:`~matplotlib.axes.Axes.pcolormesh`
-            instead of the default :meth:`~matplotlib.axes.Axes.imshow`. Specifying `True`
-            is equivalent to specifying ``pcolormesh``.
+              instead of the default :meth:`~matplotlib.axes.Axes.imshow`. Specifying `True`
+              is equivalent to specifying ``pcolormesh``.
             - If ``reproject``, the map's data will be automatically reprojected to match the
-            WCS of the target axes using the :meth:`~sunpy.map.GenericMap.reproject_to` method,
-            ensuring proper alignment.
+              WCS of the target axes using the :meth:`~sunpy.map.GenericMap.reproject_to` method,
+              ensuring proper alignment.
             - If ``False``, no auto-alignment is performed, and the map is plotted using the
-            default method without any adjustments.
+              default method without any adjustments.
 
         **imshow_kwargs : `dict`
             Any additional imshow arguments are passed to :meth:`~matplotlib.axes.Axes.imshow`.
