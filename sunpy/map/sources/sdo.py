adb75f90d4214cd62b90434fd75a143cbf51ba8f
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-01-07 00:53:24 +0530

    Update sunpy/map/sources/sdo.py
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/map/sources/sdo.py
+++ sunpy/map/sources/sdo.py
@@ -189,7 +189,7 @@ class HMISynopticMap(HMIMap):
     -----
     The sign of ``CDELT1`` in the header of (some) HMI synoptic maps is negative,
     but needs to be positive for the underlying data array in order to agree with
-    HMI magnetograms as well as JSOC-hosted ONGs of the synoptic maps.  Accordingly,
+    HMI magnetograms as well as JSOC-hosted PNGs of the synoptic maps. Accordingly,
     we use the absolute value of ``CDELT1`` to force positivity.
 
     References
