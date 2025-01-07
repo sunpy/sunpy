0cdd6181de3caa7911906a5fba52821ad400ec46
Steven Christe <ehsteve@users.noreply.github.com>
2025-01-07 13:43:16 -0500

    Update goes_xrs_nrt_data.py

--- examples/time_series/goes_xrs_nrt_data.py
+++ examples/time_series/goes_xrs_nrt_data.py
@@ -22,7 +22,9 @@ from sunpy.time import parse_time
 # This allows us to download the file and load it straight into a `pandas.DataFrame`.
 # This file updates every minute and contains only the last 7 days worth of data.
 
-goes_json_data = pd.read_json("https://services.swpc.noaa.gov/json/goes/primary/xrays-7-day.json")
+goes_json_data = pd.read_json(
+    "https://services.swpc.noaa.gov/json/goes/primary/xrays-7-day.json"
+)
 
 ###############################################################################
 # XRS collects data in two energy channels, "0.05-0.4nm" and "0.1-0.8nm".
@@ -45,20 +47,25 @@ time_array = parse_time(goes_short["time_tag"])
 # "xrsa" and "xrsb" (the channel names for GOES XRS), to their corresponding
 # physical flux units, ``u.W/u.m**2``.
 
-units = dict([("xrsa", u.W/u.m**2), ("xrsb", u.W/u.m**2)])
+units = dict([("xrsa", u.W / u.m ** 2), ("xrsb", u.W / u.m ** 2)])
 
 ###############################################################################
 # We need to create a metadata dictionary for the data.
 # Typically, `sunpy.timeseries.TimeSeries` reads the metadata directly from the file.
 # However, here we need to define our own metadata and we will keep it fairly simple.
 
-meta = dict({"instrument": "GOES X-ray sensor", "measurements": "primary", "type": "quicklook"})
+meta = dict(
+    {"instrument": "GOES X-ray sensor", "measurements": "primary", "type": "quicklook"}
+)
 
 ###############################################################################
 #  The final pre-step is create a new `pandas.DataFrame` which we can pass to
 # `sunpy.timeseries.TimeSeries` as the data input.
 
-goes_data = pd.DataFrame({"xrsa": goes_short["flux"].values, "xrsb": goes_long["flux"].values}, index=time_array.datetime)
+goes_data = pd.DataFrame(
+    {"xrsa": goes_short["flux"].values, "xrsb": goes_long["flux"].values},
+    index=time_array.datetime,
+)
 
 ###############################################################################
 # Now we will create a `sunpy.timeseries.TimeSeries` by passing in the data,
@@ -69,7 +76,9 @@ goes_ts = ts.TimeSeries(goes_data, meta, units, source="xrs")
 ###############################################################################
 # NOAA also provides the past 7 days of flare information which we can also parse.
 
-flare_events = pd.read_json('https://services.swpc.noaa.gov/json/goes/primary/xray-flares-7-day.json')
+flare_events = pd.read_json(
+    "https://services.swpc.noaa.gov/json/goes/primary/xray-flares-7-day.json"
+)
 
 ###############################################################################
 # Finally, we can plot the timeseries and overlay all the flares that occurred.
@@ -78,12 +87,19 @@ flare_events = pd.read_json('https://services.swpc.noaa.gov/json/goes/primary/xr
 fig, ax = plt.subplots()
 goes_ts.plot(axes=ax)
 for index, this_flare in flare_events.iterrows():
-    start_time, peak_time, end_time = parse_time([this_flare['begin_time'], this_flare['max_time'], this_flare['end_time']])
+    start_time, peak_time, end_time = parse_time(
+        [this_flare["begin_time"], this_flare["max_time"], this_flare["end_time"]]
+    )
     if start_time > (goes_ts.time[-1] - TimeDelta(1 * u.day)):
         ax.axvline(peak_time.datetime)
-        ax.axvspan(start_time.datetime,
-                   end_time.datetime,
-                   alpha=0.2, label=f'{peak_time} {this_flare["max_class"]}')
-ax.set_xlim(goes_ts.time[-1].datetime, (goes_ts.time[-1] - TimeDelta(1 * u.day)).datetime)
+        ax.axvspan(
+            start_time.datetime,
+            end_time.datetime,
+            alpha=0.2,
+            label=f'{peak_time} {this_flare["max_class"]}',
+        )
+ax.set_xlim(
+    goes_ts.time[-1].datetime, (goes_ts.time[-1] - TimeDelta(1 * u.day)).datetime
+)
 ax.legend(loc=2)
 plt.show()
