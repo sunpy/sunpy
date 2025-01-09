fffcc012f03f6bdc899afa49ee92a2356af95349
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-01-09 16:49:53 +0530

    Update sunpy/map/sources/soho.py
    Co-authored-by: Albert Y. Shih <ayshih@gmail.com>

--- sunpy/map/sources/soho.py
+++ sunpy/map/sources/soho.py
@@ -47,14 +47,12 @@ class EITMap(GenericMap):
         """
         Override the default implementation of coordinate_system to handle EITMAP-specific logic for CTYPE values.
         """
-        if self.meta:
-            ctype1 = self.meta.get('ctype1', '')
-            ctype2 = self.meta.get('ctype2', '')
-            if ctype1.lower() in ("solar-x", "solar_x"):
-                ctype1 = 'HPLN-TAN'
-            if ctype2.lower() in ("solar-y", "solar_y"):
-                ctype2 = 'HPLT-TAN'
-            return SpatialPair(ctype1, ctype2)
+        ctype1, ctype2 = self.meta['ctype1'], self.meta['ctype2']
+        if ctype1.lower() in ("solar-x", "solar_x"):
+            ctype1 = 'HPLN-TAN'
+        if ctype2.lower() in ("solar-y", "solar_y"):
+            ctype2 = 'HPLT-TAN'
+        return SpatialPair(ctype1, ctype2)
 
     @property
     def date(self):
