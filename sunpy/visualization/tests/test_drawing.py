224aba273fc264ecca28445cd46de8bd6fd54a98
Will Barnes <will.t.barnes@gmail.com>
2025-01-10 17:34:01 -0500

    make figure test non-square map

--- sunpy/visualization/tests/test_drawing.py
+++ sunpy/visualization/tests/test_drawing.py
@@ -22,7 +22,7 @@ def cropped_aia193_sample_map():
     import sunpy.data.sample
     m = sunpy.map.Map(sunpy.data.sample.AIA_193_JUN2012)
     return m.submap(SkyCoord(Tx=300*u.arcsec, Ty=-450*u.arcsec,frame=m.coordinate_frame),
-                    width=500*u.arcsec,
+                    width=200*u.arcsec,
                     height=500*u.arcsec)
 
 
