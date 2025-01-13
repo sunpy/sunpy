b816102d1c7bf141979f51deaa3ed64df8c481f2
ankitkhushwaha <ankitkhushwaha.dev@gmail.com>
2025-01-13 23:11:24 +0530

    update approach

--- sunpy/timeseries/sources/fermi_gbm.py
+++ sunpy/timeseries/sources/fermi_gbm.py
@@ -14,6 +14,7 @@ import sunpy.io
 import sunpy.io._file_tools
 from sunpy.time import parse_time
 from sunpy.timeseries.timeseriesbase import GenericTimeSeries
+from sunpy.util.exceptions import warn_user
 from sunpy.util.metadata import MetaDict
 from sunpy.visualization import peek_show
 
@@ -33,7 +34,7 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
     This summary lightcurve makes use of the CSPEC (daily version) data set which consists of the counts
     accumulated every 4.096 seconds in 128 energy channels for each of the 14 detectors.
 
-    Note that the data is re-binned from the original 128 into the following 8 pre-determined energy channels.
+    Note that by default the data is re-binned from the original 128 into the following 8 pre-determined energy channels.
     The rebinning method treats the counts in each of the original 128 channels as
     all having the energy of the average energy of that channel. For example, the
     counts in an 14.5--15.6 keV original channel would all be accumulated into the
@@ -46,6 +47,18 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
     * 100-300 keV
     * 300-800 keV
     * 800-2000 keV
+
+    Additionally, GBM timeseries with different energy bins ranges can be calculated by using the ``energy_bands`` keyword.
+
+    Parameters
+    ----------
+    energy_bands : `list`, `tuple`
+        The energy ranges to be calculated.
+
+    Returns
+    -------
+    `sunpy.timeseries.sources.fermi_gbm.GBMSummaryTimeSeries`
+        with custom energy bins.
 
     Examples
     --------
@@ -72,7 +85,6 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
         self.__header = None
         self.__energy_bins = None
         self.__count_data = None
-
 
     def plot(self, axes=None, columns=None, **kwargs):
         """
@@ -167,25 +179,27 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
             cls.__header = kwargs.pop('header')
             cls.__energy_bins = kwargs.pop('energy_bins')
             cls.__count_data = kwargs.pop('count_data')
+
+        ebands = kwargs.get('energy_bands', [4, 15, 25, 50, 100, 300, 800, 2000]*u.keV)
+        for eband in ebands:
+            if not eband.unit.is_equivalent(u.keV):
+                unit_str, other_str = get_err_str(eband.unit), get_err_str(u.keV)
+                raise u.UnitConversionError(f"{unit_str} and {other_str} are not convertible.")
+        ebands = [eband.to(u.keV) if eband.unit != u.keV else eband for eband in ebands]
+        ebands = sorted(ebands)
+
         # rebin the 128 energy channels into some summary ranges
         # some of default bin ranges are
         # 4-15 keV, 15 - 25 keV, 25-50 keV, 50-100 keV, 100-300 keV, 300-800 keV, 800 - 2000 keV
         # put the data in the units of counts/s/keV
-        summary_counts = _bin_data_for_summary(cls.__energy_bins, cls.__count_data, **kwargs)
-
+        summary_counts, column_labels, ebands = _bin_data_for_summary(
+            cls.__energy_bins, cls.__count_data, ebands
+        )
         # get the time information in datetime format with the correct MET adjustment
         met_ref_time = parse_time('2001-01-01 00:00')  # Mission elapsed time
         gbm_times = met_ref_time + TimeDelta(cls.__count_data['time'], format='sec')
         gbm_times.precision = 9
         gbm_times = gbm_times.isot.astype('datetime64')
-
-        ebands = kwargs.get('energy_bands', [4, 15, 25, 50, 100, 300, 800, 2000]*u.keV)
-        ebands = [eband.to(u.keV) if eband.unit != u.keV else eband for eband in ebands]
-        ebands = sorted(ebands)
-        column_labels = [
-            f"{eband.value}-{next_eband.value} {eband.unit}"
-            for eband, next_eband in zip(ebands, ebands[1:])
-        ]
 
         # Add the units data
         units = OrderedDict((col, u.ct / u.s / eband.unit) for col, eband in zip(column_labels, ebands))
@@ -207,13 +221,27 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
             return kwargs['meta'].get('INSTRUME', '').startswith('GBM')
 
     @classmethod
-    def energy_bins(cls, energy_bands = None):
+    def energy_bins(cls, energy_bands):
         """
-        Determine GBM timeseries with different energy bins ranges
+        Determine GBM timeseries with different energy bins ranges.
+
         Parameters
         ----------
-        ebands : `list`, `tuple`
-            The energy bins you want.
+        energy_bands : `list`, `tuple`
+            The energy ranges to be calculated.
+
+        Returns
+        -------
+        `sunpy.timeseries.sources.fermi_gbm.GBMSummaryTimeSeries`
+            with custom energy bins.
+
+        Examples
+        --------
+        >>> import astropy.units as u
+        >>> import sunpy.timeseries
+        >>> import sunpy.data.sample  # doctest: +REMOTE_DATA
+        >>> gbm = sunpy.timeseries.TimeSeries(sunpy.data.sample.GBM_TIMESERIES, source='GBMSummary')  # doctest: +REMOTE_DATA
+        >>> gbm_ts = gbm.energy_bins([50, 100, 200, 400, 1000] * u.keV)  # doctest: +SKIP
         """
         from sunpy.timeseries.timeseries_factory import TimeSeries
         data, meta, units = cls._parse_hdus(
@@ -226,7 +254,7 @@ class GBMSummaryTimeSeries(GenericTimeSeries):
         return TimeSeries(data, meta, units)
 
 
-def _bin_data_for_summary(energy_bins, count_data, **kwargs):
+def _bin_data_for_summary(energy_bins, count_data, ebands):
     """
     Rebin the 128 energy channels into some summary ranges and put the data in
     the units of counts/s/keV.
@@ -248,21 +276,57 @@ def _bin_data_for_summary(energy_bins, count_data, **kwargs):
         The array of count data to rebin.
     """
     # list of energy bands to sum between
-    ebands = kwargs.get('energy_bands',  [4, 15, 25, 50, 100, 300, 800, 2000]*u.keV)
-    ebands = [eband.to(u.keV) if eband.unit != u.keV else eband for eband in ebands]
+    ebands = ebands
     if len(ebands) <= 1:
-        raise UserWarning("'energy_bands' must contain more than one element.")
-    ebands = sorted(ebands)
+        raise ValueError("'energy_bands' must contain more than one element.")
     e_center = (energy_bins['e_min'] + energy_bins['e_max']) / 2
-    indices = [np.searchsorted(e_center, e.value) for e in ebands]
+    e_min, e_max = round(energy_bins['e_min'].min()), np.ceil(energy_bins['e_max'].max())
 
+    if min(ebands).value >= e_min and max(ebands).value <= e_max:
+        pass
+    # ebands doesn't overlap with energy bins
+    elif max(ebands).value <= e_min or min(ebands).value >= e_max:
+        warn_user(
+            "Data is not available for"
+            f" {min(ebands)}-{max(ebands)}, "
+            "updating energy bins to default value")
+        ebands = [4, 15, 25, 50, 100, 300, 800, 2000] * u.keV
+    else:
+        warn_user("Only Partially Data is available for"
+                f" {e_min} keV-{e_max} keV")
+        # ebands fully overlap with energy bins
+        if min(ebands).value < e_min and max(ebands).value > e_max:
+            ebands = list(filter(lambda x: e_min <= x.value <= e_max, ebands))
+            ebands.append(e_max * u.keV)
+            ebands.insert(0, e_min * u.keV)
+
+        # ebands partially overlap with energy bins
+        elif min(ebands).value >= e_min and max(ebands).value > e_max:
+            ebands = list(filter(lambda x: x.value <= e_max, ebands))
+            ebands.append(e_max * u.keV)
+        elif min(ebands).value < e_min and max(ebands).value <= e_max:
+            ebands = list(filter(lambda x: x.value >= e_min, ebands))
+            ebands.insert(0, e_min * u.keV)
+
+    column_labels = [
+        f"{eband.value}-{next_eband.value} {eband.unit}"
+        for eband, next_eband in zip(ebands, ebands[1:])
+    ]
+    indices = [np.searchsorted(e_center, e.value) for e in ebands]
     summary_counts = []
     for ind_start, ind_end in zip(indices[:-1], indices[1:]):
-        if ind_start != len(e_center) or ind_end != len(e_center):
-            # sum the counts in the energy bands, and find counts/s/keV
-            summed_counts = np.sum(count_data["counts"][:, ind_start:ind_end], axis=1)
-            energy_width = (energy_bins["e_max"][ind_end - 1] - energy_bins["e_min"][ind_start])
-            summary_counts.append(summed_counts/energy_width/count_data["exposure"])
-        else:
-            summary_counts.append([np.nan]*len(count_data["exposure"]))
-    return np.array(summary_counts).T
+        # sum the counts in the energy bands, and find counts/s/keV
+        summed_counts = np.sum(count_data["counts"][:, ind_start:ind_end], axis=1)
+        energy_width = (energy_bins["e_max"][ind_end - 1] - energy_bins["e_min"][ind_start])
+        summary_counts.append(summed_counts/energy_width/count_data["exposure"])
+    return np.array(summary_counts).T, column_labels, ebands
+
+
+def get_err_str(unit: u.UnitBase) -> str:
+    unit_str = unit.to_string("generic")
+    physical_type = unit.physical_type
+    if physical_type != "unknown":
+        unit_str = f"'{unit_str}' ({physical_type})"
+    else:
+        unit_str = f"'{unit_str}'"
+    return unit_str
--- sunpy/timeseries/sources/tests/test_fermi_gbm.py
+++ sunpy/timeseries/sources/tests/test_fermi_gbm.py
@@ -6,6 +6,7 @@ import astropy.units as u
 import sunpy.timeseries
 from sunpy.data.test import get_test_filepath
 from sunpy.tests.helpers import figure_test
+from sunpy.util import SunpyUserWarning
 
 fermi_gbm_filepath = get_test_filepath('gbm.fits')
 
@@ -35,36 +36,69 @@ def test_fermi_gbm_peek(fermi_gbm_test_ts):
 
 
 def test_fermi_gbm_default_energy_bins():
+    # Checks for default_ebin, custom_ebin, out-for-bound ebin
+    custom_bin = [45, 95, 150, 270, 500] * u.keV
     ts_gbm = sunpy.timeseries.TimeSeries(fermi_gbm_filepath)
+    ts_gbm_1 = ts_gbm.energy_bins(custom_bin)
+    ts_gbm_2 = sunpy.timeseries.TimeSeries(fermi_gbm_filepath,
+                                           energy_bands = custom_bin)
+    with pytest.warns(SunpyUserWarning, match = "Data is not available for"):
+        ts_gbm_3 = sunpy.timeseries.TimeSeries(fermi_gbm_filepath, energy_bands = [20, 40, 60, 80, 100] * u.eV)
+    with pytest.warns(SunpyUserWarning, match = "Data is not available for"):
+        ts_gbm_4 = ts_gbm.energy_bins([20, 40, 60, 80, 100] * u.eV)
+
     cols_list = [
-        '4.0-15.0 keV', '15.0-25.0 keV', '25.0-50.0 keV',
-        '50.0-100.0 keV', '100.0-300.0 keV', '300.0-800.0 keV',
-        '800.0-2000.0 keV'
+        '4.0-15.0 keV', '15.0-25.0 keV', '25.0-50.0 keV', '50.0-100.0 keV',
+        '100.0-300.0 keV', '300.0-800.0 keV', '800.0-2000.0 keV'
+    ]
+    cols_list_ = [
+        f"{ebin.value}-{next_ebin.value} {ebin.unit}"
+        for ebin, next_ebin in zip(custom_bin, custom_bin[1:])
     ]
     assert np.all(ts_gbm.to_dataframe().columns == cols_list)
+    assert np.all(ts_gbm_1.to_dataframe().columns == cols_list_)
+    assert np.all(ts_gbm_2.to_dataframe().columns == cols_list_)
+    assert np.all(ts_gbm_3.to_dataframe().columns == cols_list)
+    assert np.all(ts_gbm_4.to_dataframe().columns == cols_list)
 
 
 @pytest.mark.parametrize("energy_bins", [
-    [45, 95, 150, 270, 500] * u.keV,
-    [45, 95, 75, 300, 200] * u.keV,
-    [50, 100, 300, 400] * u.J,
-    [200 * u.keV, 1000 * u.keV, 1 * u.J, 30 * u.J],
-    [2 * u.eV, 5 * u.J, 32 * u.keV, 7 * u.J]
+    [2 * u.eV, 100 * u.eV, 50 * u.keV, 200 * u.keV],
+    [50 * u.keV, 200 * u.keV, 2500 * u.keV],
+    [2 * u.eV, 100 * u.eV, 50 * u.keV, 200 * u.keV, 2500 * u.keV]
 ])
 def test_fermi_gbm_custom_energy_bins(fermi_gbm_test_ts, energy_bins):
+    # Checks for fully overlapped ebin, partial ebin ranges
+    e_min, e_max = 4*u.keV, 2000*u.keV
     ts_gbm_base = fermi_gbm_test_ts
-    ts_gbm = sunpy.timeseries.TimeSeries(fermi_gbm_filepath, energy_bands=energy_bins)
+    with pytest.warns(SunpyUserWarning, match = "Only Partially Data is available for"):
+        ts_gbm = sunpy.timeseries.TimeSeries(fermi_gbm_filepath, energy_bands=energy_bins)
+    with pytest.warns(SunpyUserWarning, match = "Only Partially Data is available for"):
+        ts_gbm_energy_bins = ts_gbm_base.energy_bins(energy_bins)
+    with pytest.warns(SunpyUserWarning, match = "Only Partially Data is available for"):
+        ts_gbm_energy_bins__ = ts_gbm.energy_bins(energy_bins)
 
-    ts_gbm_energy_bins = ts_gbm_base.energy_bins(energy_bins)
-    ts_gbm_energy_bins__ = ts_gbm.energy_bins(energy_bins)
+    energy_bins = [ebin.to(u.keV) if ebin.unit != u.keV else ebin for ebin in energy_bins]
     energy_bins = sorted(energy_bins)
-    col_list = [
-        f'{(first.to(u.keV)).value}-{(second.to(u.keV)).value} keV'
-        for first, second in zip(energy_bins, energy_bins[1:])
+    # This only checks for fully overlapped ebin and partial ebin ranges
+    if min(energy_bins) < e_min and max(energy_bins) > e_max:
+        energy_bins = list(filter(lambda x: e_min <= x <= e_max, energy_bins))
+        energy_bins.append(e_max * u.keV)
+        energy_bins.insert(0, e_min * u.keV)
+    elif min(energy_bins) >= e_min and max(energy_bins) > e_max:
+        energy_bins = list(filter(lambda x: x <= e_max, energy_bins))
+        energy_bins.append(e_max)
+    elif min(energy_bins) < e_min and max(energy_bins) <= e_max:
+        energy_bins = list(filter(lambda x: x >= e_min, energy_bins))
+        energy_bins.insert(0, e_min * u.keV)
+
+    cols_list_ = [
+        f"{ebin.value}-{next_ebin.value} keV"
+        for ebin, next_ebin in zip(energy_bins, energy_bins[1:])
     ]
-    assert np.all(ts_gbm.to_dataframe().columns == col_list)
-    assert np.all(ts_gbm_energy_bins.to_dataframe().columns == col_list)
-    assert np.all(ts_gbm_energy_bins__.to_dataframe().columns == col_list)
+    assert np.all(ts_gbm.to_dataframe().columns == cols_list_)
+    assert np.all(ts_gbm_energy_bins.to_dataframe().columns == cols_list_)
+    assert np.all(ts_gbm_energy_bins__.to_dataframe().columns == cols_list_)
 
 
 @pytest.mark.parametrize("energy_bins", [
@@ -72,7 +106,18 @@ def test_fermi_gbm_custom_energy_bins(fermi_gbm_test_ts, energy_bins):
     []
 ])
 def test_fermi_gbm_invalid_energy_bins(fermi_gbm_test_ts, energy_bins):
-    with pytest.raises(UserWarning, match = "'energy_bands' must contain more than one element."):
+    with pytest.raises(ValueError, match = "'energy_bands' must contain more than one element."):
         sunpy.timeseries.TimeSeries(fermi_gbm_filepath, energy_bands=energy_bins)
-    with pytest.raises(UserWarning, match = "'energy_bands' must contain more than one element."):
+    with pytest.raises(ValueError, match = "'energy_bands' must contain more than one element."):
+        fermi_gbm_test_ts.energy_bins(energy_bins)
+
+
+@pytest.mark.parametrize("energy_bins", [
+    [4*u.keV, 10*u.m, 20*u.keV],
+    [4*u.keV, 10*u.mag, 20*u.keV]
+])
+def test_fermi_gbm_invalid_unit_energy_bins(fermi_gbm_test_ts, energy_bins):
+    with pytest.raises(u.UnitConversionError, match = r"are not convertible.$"):
+        sunpy.timeseries.TimeSeries(fermi_gbm_filepath, energy_bands=energy_bins)
+    with pytest.raises(u.UnitConversionError, match = r"are not convertible.$"):
         fermi_gbm_test_ts.energy_bins(energy_bins)
