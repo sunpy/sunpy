5c5d5fe1f74f6c9c48df85463d972767b4f9c924
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-01-14 20:10:33 +0530

    Delete fido_test.py

--- fido_test.py
+++
@@ -1,7 +0,0 @@
-from sunpy.net import Fido
-from sunpy.net import attrs as a
-
-result = Fido.search(a.Time('2012/3/4', '2012/3/6'), a.Instrument.lasco)
-print(result)
-
-print("Errors dict", result.errors)
