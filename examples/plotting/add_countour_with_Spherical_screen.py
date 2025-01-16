6d54b7aa80f43648b21206588777fdbfc85200f1
tanishy7777 <23110328@iitgn.ac.in>
2025-01-16 23:31:33 +0530

    Adds without sphericalscreeen

--- examples/plotting/add_countour_with_Spherical_screen.py
+++ examples/plotting/add_countour_with_Spherical_screen.py
@@ -3,11 +3,11 @@
 Add contours to a map using a SphericalScreen
 =============================================
 
-In this example, we will plot the 171 AIA image and add contours to it using `draw_contours`.
-We will also plot the 193 AIA image and add contours to it. The contours of  193 AIA will drawn
-by transforming the coordinates from the 171 image to the 193 image using the SphericalScreen class.
-By specifying the axes of the 173 image in `draw_contours`, the contours will be drawn on the 193 image from
-the 173 image.
+In this example, we try to show that SphericalScreen can be used as a general method
+to transform the coordinates(change the frame of reference i.e. the observer)
+from one image to another as seen by a new observer, even without reprojection.
+Here, we will draw contours on one image and then use the `SphericalScreen`
+class to draw the contours on another image using the former's perspective.
 """
 
 import matplotlib.pyplot as plt
@@ -20,36 +20,38 @@ from sunpy.coordinates.screens import SphericalScreen
 
 ##############################################################################
 # For this example, we will start with the sample data. To create the maps,
-# we need the 171 and 193 AIA images. Both of these data can be downloaded
-# with ``Fido``.
+# we need the 171 and 193 AIA images.
 
-aia193 = sunpy.map.Map(sunpy.data.sample.AIA_193_IMAGE)
-aia171 = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
+aia193_map = sunpy.map.Map(sunpy.data.sample.AIA_193_IMAGE)
+aia171_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ##############################################################################
 # Create a figure with two subplots. The first subplot will have the 171 image
-# with contours drawn on it.
-fig= plt.figure(figsize=(10, 5))
-ax1 = fig.add_subplot(1, 2, 1, projection=aia171)
-aia171.plot(axes=ax1, clip_interval=(1,99.9)*u.percent)
-aia171.draw_contours(levels=[1,5]*u.percent, axes=ax1, colors='C0')
-ax1.set_title("171 Contours on 171")
-
-##############################################################################
-# The second subplot will have the 193 image drawn on it.
-ax2 = fig.add_subplot(1,2,2,projection=aia193)
-aia193.plot(axes=ax2)
+# with contours drawn on it. The second subplot will have the 193 image drawn
+# on it.
+fig= plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 3, 1, projection=aia171_map)
+aia171_map.plot(axes=ax1, clip_interval=(1,99.9)*u.percent)
+aia171_map.draw_contours(levels=[1,5]*u.percent, axes=ax1, colors='C0')
+ax1.set_title("Contours on 173")
+ax2 = fig.add_subplot(1,3,2,projection=aia193_map)
+aia193_map.plot(axes=ax2)
 
 ##############################################################################
 # We will now draw the contours on the 193 image. To do this, we will use the
-# `SphericalScreen` class. We will transform the coordinates from the 171 image
-# to the 193 image using the `SphericalScreen` class. By specifying the
-# axes of the 173 image in `draw_contours`, the contours will be drawn on the
-# 193 image from the 171 image.
-with SphericalScreen(aia171.observer_coordinate):
-    aia171.draw_contours(levels=[1,5]*u.percent, axes=ax2, colors='C2')
-ax2.set_title("171 Contours on 193")
+# `SphericalScreen` class so that it uses the observer/coordinates of the 171
+# image. So essentially we are transforming the coordinates of the contours
+# for the 193 image.
+with SphericalScreen(aia171_map.observer_coordinate):
+    aia171_map.draw_contours(levels=[1,5]*u.percent, axes=ax2, colors='C2')
+ax2.set_title("Contours on 193 with SphericalScreen")
+
 
 ##############################################################################
-# Display the plot.
+# without `SphereicalScreen` class the contours are drawn on the 193 image
+# using the 193 image's observer/coordinates.
+ax3 = fig.add_subplot(1,3,3,projection=aia193_map)
+aia193_map.plot(axes=ax3)
+aia171_map.draw_contours(levels=[1,5]*u.percent, axes=ax3, colors='C2')
+ax3.set_title("Contours on 193 without SphericalScreen")
 plt.show()
