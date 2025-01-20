88ea9c38406451756e1ae425b251797360619f4a
tanishy7777 <23110328@iitgn.ac.in>
2025-01-20 23:54:18 +0530

    Formatting changes

--- examples/plotting/add_countour_with_spherical_screen.py
+++ examples/plotting/add_countour_with_spherical_screen.py
@@ -26,37 +26,44 @@ aia171_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 ##############################################################################
 # We want to show how we can use `~sunpy.coordinates.SphericalScreen` as a
 # way to transform the observer coordinates from one map to another. For this
-# Lets try drawing contours on the AIA 171 map and use the observer
-# coordinates from this for contours on the AIA 193 map. Lets first start by
-# creating a figure with two subplots. The first subplot will have the AIA 171
-# map with contours drawn on it. The second subplot will have the AIA 193
-#  map drawn on it.
+# lets try drawing contours on the AIA 171 map(Plot 1) and use the observer
+# coordinates from this for contours on the AIA 193 map(Plot 2). To do this,
+# we will use the `~sunpy.coordinates.SphericalScreen` class so that it uses
+# the observer coordinates of the AIA 171 image. So essentially we are
+# transforming the coordinates of the contours for the AIA 193 image.
+# If we dont use the `~sunpy.coordinates.SphericalScreen` class the contours
+# are drawn on the 193 Map using the 193 Map's observer coordinates(Plot 3).
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 3, 1, projection=aia171_map)
-aia171_map.plot(axes=ax1, clip_interval=(1,99.9)*u.percent)
-aia171_map.draw_contours(levels=[1, 5]*u.percent, axes=ax1, colors='C0')
-ax1.set_title("Contours on 173")
-ax2 = fig.add_subplot(1, 3, 2, projection=aia193_map)
+fig = plt.figure(figsize=(10, 4))
+
+ax1 = fig.add_subplot(131, projection=aia171_map)
+ax2 = fig.add_subplot(132, projection=aia193_map)
+ax3 = fig.add_subplot(133, projection=aia193_map)
+
+aia171_map.plot(axes=ax1, clip_interval=(1, 99.9) * u.percent)
+aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax1, colors='C0')
+ax1.set_title("Contours on AIA 171 map")
+ax1.set_xlabel("")
+
 aia193_map.plot(axes=ax2)
-
-##############################################################################
-# We will now draw the contours on the AIA 193 map. To do this, we will use
-# the `~sunpy.coordinates.SphericalScreen` class so that it uses the observer
-# coordinates of the AIA 171 image. So essentially we are transforming
-# the coordinates of the contours for the AIA 193 image.
-
 with SphericalScreen(aia171_map.observer_coordinate):
-    aia171_map.draw_contours(levels=[1, 5]*u.percent, axes=ax2, colors='C2')
-ax2.set_title("Contours on 193 with SphericalScreen")
+    aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax2, colors='C2')
+ax2.set_title("Contours with SphericalScreen")
+ax2.set_xlabel("")
 
-##############################################################################
-# without `~sunpy.coordinates.SphericalScreen` class the contours are drawn
-# on the 193 image using the 193 image's observer/coordinates.
-
-ax3 = fig.add_subplot(1,3,3,projection=aia193_map)
 aia193_map.plot(axes=ax3)
-aia171_map.draw_contours(levels=[1,5]*u.percent, axes=ax3, colors='C2')
-ax3.set_title("Contours on 193 without SphericalScreen")
+aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax3, colors='C2')
+ax3.set_title("Contours without SphericalScreen")
+ax3.set_xlabel("")
+
+for ax in [ax1, ax2, ax3]:
+    ax.coords[0].set_ticks_visible(False)
+    ax.coords[0].set_ticklabel_visible(False)
+    ax.coords[1].set_ticks_visible(False)
+    ax.coords[1].set_ticklabel_visible(False)
+
+
+plt.subplots_adjust(wspace=0)
+fig.tight_layout()
 
 plt.show()
