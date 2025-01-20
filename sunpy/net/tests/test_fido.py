4b5667b065f2a46323acc3a3c505f4747d806f4a
tanishy7777 <23110328@iitgn.ac.in>
2025-01-20 22:50:15 +0530

    Adds test for output error string

--- sunpy/net/tests/test_fido.py
+++ sunpy/net/tests/test_fido.py
@@ -67,8 +67,8 @@ def test_offline_fido(query):
 
 
 @pytest.mark.remote_data
-# Until we get more mocked, we can't really do this to online clients.
-# TODO: Hypothesis this again
+# # Until we get more mocked, we can't really do this to online clients.
+# # TODO: Hypothesis this again
 @pytest.mark.parametrize("query", [
     (a.Instrument.eve & a.Time('2014/7/7', '2014/7/14') & a.Level.zero),
     (a.Instrument.rhessi & a.Time('2014/7/7', '2014/7/14')),
