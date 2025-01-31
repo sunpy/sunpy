df544b62b372fa152d35e65b77735230bdc27062
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2025-01-31 11:46:24 +0530

    Update sunpy/coordinates/_scweb_query.py
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- sunpy/coordinates/_scweb_query.py
+++ sunpy/coordinates/_scweb_query.py
@@ -31,38 +31,22 @@ def _create_xml_request(name, time_range,system):
     """
     if not isinstance(time_range, TimeRange):
         raise ValueError("time_range must be a SunPy TimeRange object.")
-
-    # Extract start and end times in ISO format
     start_time = time_range.start.isot
     end_time = time_range.end.isot
-
-    # Root element
     data_request = ET.Element("DataRequest", xmlns="http://sscweb.gsfc.nasa.gov/schema")
-
-    # Description
     ET.SubElement(data_request, "Description")
-
-    # TimeInterval
     time_interval = ET.SubElement(data_request, "TimeInterval")
     ET.SubElement(time_interval, "Start").text = start_time
     ET.SubElement(time_interval, "End").text = end_time
-
-    # Satellites
     satellites = ET.SubElement(data_request, "Satellites")
     ET.SubElement(satellites, "Id").text = name
     ET.SubElement(satellites, "ResolutionFactor").text = "1"
-
-    # OutputOptions
     output_options = ET.SubElement(data_request, "OutputOptions")
-
-    # CoordinateOptions
-    # NOTE available coordinate options X, Y, Z, Latitude, Longitude , local-time (if applicable)
     coordinate_components = ["Lat", "Lon"]
     for component in coordinate_components:
         coordinate_option = ET.SubElement(output_options, "CoordinateOptions")
         ET.SubElement(coordinate_option, "CoordinateSystem").text = system
         ET.SubElement(coordinate_option, "Component").text = component
-
     return ET.tostring(ET.ElementTree(data_request).getroot(), encoding="unicode")
 
 def _send_requests(xml):
