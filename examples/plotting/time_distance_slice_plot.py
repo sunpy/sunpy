c5e5db70a4ac601f32370d96109ed37f60970ed8
Prtm2110 <prathamhole@gmail.com>
2025-02-05 02:59:25 +0530

    Added angular_separation plot

--- examples/plotting/time_distance_slice_plot.py
+++ examples/plotting/time_distance_slice_plot.py
@@ -3,8 +3,8 @@
 Time-Distance Slice Plot of Sequence of Maps
 ============================================
 
-This example showcases how you can use :func:`sunpy.map.pixelate_coord_path` and 
-:func:`sunpy.map.sample_at_coords()` on a sequence of images to extract a time-distance 
+This example showcases how you can use :func:`sunpy.map.pixelate_coord_path` and
+:func:`sunpy.map.sample_at_coords()` on a sequence of images to extract a time-distance
 slice and identify features in the solar atmosphere.
 """
 import matplotlib.pyplot as plt
@@ -31,15 +31,28 @@ files = Fido.fetch(query)
 aia_sequence = sunpy.map.Map(files, sequence=True)
 
 ########################################################################################################################
-# Next, we will define a path in a `~astropy.coordinates.SkyCoord` object named `line_coords` and pass it to 
-# :func:`sunpy.map.pixelate_coord_path`. This will return the pixel coordinates for every pixel that intersects 
-# with the coordinate path. To obtain the values of these pixels/intensities, we pass the output to 
+# Next, we will define a path in a `~astropy.coordinates.SkyCoord` object named ``line_coords`` and pass it to
+# :func:`sunpy.map.pixelate_coord_path`. This will return the pixel coordinates for every pixel that intersects
+# with the coordinate path. To obtain the values of these pixels/intensities, we pass the output to
 # :func:`sunpy.map.sample_at_coords()`.
 
 line_coords = SkyCoord([-350, 100], [-250, 650], unit=u.arcsec, frame=aia_sequence[0].coordinate_frame)
 intensity_coords = sunpy.map.pixelate_coord_path(aia_sequence[0], line_coords)
 intensity = sunpy.map.sample_at_coords(aia_sequence[0], intensity_coords)
 angular_separation = intensity_coords.separation(intensity_coords[0]).to(u.arcsec)
+
+#############################################################################################################################
+# We can now plot ``line_coords`` on the first map in the sequence and plot intensity vs. the angular distance along the slit.
+
+fig = plt.figure(figsize=(10, 4))
+ax1 = fig.add_subplot(121, projection=aia_sequence[0])
+aia_sequence[0].plot(axes=ax1)
+ax1.plot_coord(intensity_coords)
+
+ax2 = fig.add_subplot(122)
+ax2.plot(angular_separation, intensity)
+ax2.set_xlabel("Angular distance along slit [arcsec]")
+ax2.set_ylabel(f"Intensity [{aia_sequence[0].unit}]")
 
 ###############################################################################
 # Let's now create a rectangular slice (i.e., a map cutout).
@@ -57,7 +70,7 @@ with propagate_with_solar_surface():
     aia_sequence_aligned = sunpy.map.Map([m.reproject_to(cutout_map.wcs) for m in aia_sequence], sequence=True)
 
 ##########################################################################################################
-# Now we can plot the time-distance slice of the sequence of maps using :func:`~matplotlib.pyplot.imshow().
+# Now we can plot the time-distance slice of the sequence of maps using :func:`~matplotlib.pyplot.imshow()`.
 
 fig, ax = plt.subplots(figsize=(15, 6))
 norm = norm=ImageNormalize(vmin=0, vmax=3e3, stretch=SqrtStretch())
