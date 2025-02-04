49b41e83e9ba1a320b6eb3369de59ef3cca76daa
Prtm2110 <prathamhole@gmail.com>
2025-02-05 02:49:24 +0530

    correcting grammar

--- examples/plotting/time_distance_slice_plot.py
+++ examples/plotting/time_distance_slice_plot.py
@@ -3,7 +3,9 @@
 Time-Distance Slice Plot of Sequence of Maps
 ============================================
 
-This example showcases how you can use :func:`sunpy.map.pixelate_coord_path` and sunpy.map.sample_at_coords() on a sequence of images to extract a time-distance slice to identify features in the solar atmosphere.
+This example showcases how you can use :func:`sunpy.map.pixelate_coord_path` and 
+:func:`sunpy.map.sample_at_coords()` on a sequence of images to extract a time-distance 
+slice and identify features in the solar atmosphere.
 """
 import matplotlib.pyplot as plt
 
@@ -17,8 +19,9 @@ from sunpy.net import Fido
 from sunpy.net import attrs as a
 
 ###############################################################################
-# First we will import sequence of images from AIA 171 angstrom separated by 1 hour,
+# First, we will import a sequence of images from AIA 171 angstrom separated by 1 hour
 # and create a sequence of maps.
+
 query = Fido.search(a.Time('2018-05-30 02:00:00', '2018-05-30 10:00:00'),
                     a.Instrument.aia,
                     a.Wavelength(171*u.angstrom),
@@ -28,17 +31,18 @@ files = Fido.fetch(query)
 aia_sequence = sunpy.map.Map(files, sequence=True)
 
 ########################################################################################################################
-# Next we will define a path in a `~astropy.coordinates.SkyCoord` object named `line_coords` and pass it to :func:`sunpy.map.pixelate_coord_path` and it will return the pixel coordinates for every pixel that intersects with a coordinate path.
-# To obtain the values of these pixels/intensity, pass the output to :func:`sunpy.map.sample_at_coords()`.
+# Next, we will define a path in a `~astropy.coordinates.SkyCoord` object named `line_coords` and pass it to 
+# :func:`sunpy.map.pixelate_coord_path`. This will return the pixel coordinates for every pixel that intersects 
+# with the coordinate path. To obtain the values of these pixels/intensities, we pass the output to 
+# :func:`sunpy.map.sample_at_coords()`.
 
 line_coords = SkyCoord([-350, 100], [-250, 650], unit=u.arcsec, frame=aia_sequence[0].coordinate_frame)
 intensity_coords = sunpy.map.pixelate_coord_path(aia_sequence[0], line_coords)
 intensity = sunpy.map.sample_at_coords(aia_sequence[0], intensity_coords)
 angular_separation = intensity_coords.separation(intensity_coords[0]).to(u.arcsec)
 
-
 ###############################################################################
-# Let's now create rectangular slice i.e., map cutout.
+# Let's now create a rectangular slice (i.e., a map cutout).
 
 corner = SkyCoord(Tx=-250*u.arcsec, Ty=0*u.arcsec, frame=aia_sequence[0].coordinate_frame)
 width = 250*u.arcsec
@@ -46,14 +50,14 @@ cutout_map = aia_sequence[0].submap(corner, width=width, height=500*u.arcsec)
 
 ##################################################################################################
 # Now that we have our cutout, we can reproject each map in our sequence to
-# the WCS of that cutout, using the `~sunpy.coordinates.propagate_with_solar_surface`
+# the WCS of that cutout using the `~sunpy.coordinates.propagate_with_solar_surface`
 # context manager to adjust the field of view of the cutout with the rotation of the solar surface.
 
 with propagate_with_solar_surface():
     aia_sequence_aligned = sunpy.map.Map([m.reproject_to(cutout_map.wcs) for m in aia_sequence], sequence=True)
 
-################################################################################################
-#Now we can plot the time-distance slice of the sequence of maps using matplotlib.pyplot.imshow().
+##########################################################################################################
+# Now we can plot the time-distance slice of the sequence of maps using :func:`~matplotlib.pyplot.imshow().
 
 fig, ax = plt.subplots(figsize=(15, 6))
 norm = norm=ImageNormalize(vmin=0, vmax=3e3, stretch=SqrtStretch())
