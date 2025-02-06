7b4d80c6352657fa237c49d747bec771884e2153
Prtm2110 <prathamhole@gmail.com>
2025-02-07 00:07:19 +0530

    doc string updated

--- sunpy/map/mapbase.py
+++ sunpy/map/mapbase.py
@@ -2228,10 +2228,10 @@ class GenericMap(NDData):
             The default value of 'func' is `~numpy.sum`; using this causes
             superpixel to sum over (dimension[0], dimension[1]) pixels of the
             original map.
+        conservative_mask : bool, optional
             If `True`, a superpixel is masked if any of its constituent pixels are masked.
             If `False`, a superpixel is masked only if all of its constituent pixels are masked.
             Default is `False`.
-            If `True`, mask propagation is enabled. A superpixel will be masked if any of its constituent pixels are masked. The default is `False`.
 
         Returns
         -------
