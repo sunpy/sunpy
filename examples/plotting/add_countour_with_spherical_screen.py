c9c2675d1e3cf6836e1e1a6bae7a83b14fb5385e
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-02-19 03:43:41 +0530

    Update examples/plotting/add_countour_with_spherical_screen.py
    Co-authored-by: Albert Y. Shih <ayshih@gmail.com>

--- examples/plotting/add_countour_with_spherical_screen.py
+++ examples/plotting/add_countour_with_spherical_screen.py
@@ -34,46 +34,37 @@ aia193_map = sunpy.map.Map(sunpy.data.sample.AIA_193_IMAGE)
 aia171_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ##############################################################################
-# We want to show how we can use `~sunpy.coordinates.SphericalScreen` as a
-# way to transform the observer coordinates from one map to another. For this
-# lets try drawing contours on the AIA 171 map(Plot 1) and use the observer
-# coordinates from this for contours on the AIA 193 map(Plot 2). To do this,
-# we will use the `~sunpy.coordinates.SphericalScreen` class so that it uses
-# the observer coordinates of the AIA 171 image. So essentially we are
-# transforming the coordinates of the contours for the AIA 193 image.
-# If we dont use the `~sunpy.coordinates.SphericalScreen` class the contours
-# are drawn on the 193 Map using the 193 Map's observer coordinates(Plot 3).
+# First, let's plot the AIA 171 contours on their native map.  The contours
+# extend off disk due to the coronal loops.
 
-fig = plt.figure(figsize=(10, 4))
+fig = plt.figure()
+ax = fig.add_subplot(projection=aia171_map)
+aia171_map.plot(axes=ax)
+aia171_map.draw_contours(axes=ax, levels=[1000, 2500] * u.DN, colors="green")
 
-ax1 = fig.add_subplot(131, projection=aia171_map)
-ax2 = fig.add_subplot(132, projection=aia193_map)
-ax3 = fig.add_subplot(133, projection=aia193_map)
+##############################################################################
+# Next, let's try overlaying those same AIA 171 contours on the AIA 193 map.
+# Note that none of the off-disk contours appear.  Again, this is because it
+# is not well defined where to plot such contours from the vantage point of
+# the AIA 193 map.
 
-aia171_map.plot(axes=ax1, clip_interval=(1, 99.9) * u.percent)
-aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax1, colors='C0')
-ax1.set_title("Contours on AIA 171 map")
-ax1.set_xlabel("")
+fig = plt.figure()
+ax = fig.add_subplot(projection=aia193_map)
+aia193_map.plot(axes=ax, title="Default overlay behavior")
+aia171_map.draw_contours(axes=ax, levels=[1000, 2500] * u.DN, colors="green")
 
-aia193_map.plot(axes=ax2)
-with SphericalScreen(aia171_map.observer_coordinate):
-    aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax2, colors='C2')
-ax2.set_title("Contours with SphericalScreen")
-ax2.set_xlabel("")
+##############################################################################
+# Finally, let's overlay those contours again, but this time using the
+# `~sunpy.coordinates.SphericalScreen` context manager.  This screen assumes
+# that off-disk points lie on the inside of a spherical screen.  Note that the
+# off-disk contours are now visible.
 
-aia193_map.plot(axes=ax3)
-aia171_map.draw_contours(levels=[1, 5] * u.percent, axes=ax3, colors='C2')
-ax3.set_title("Contours without SphericalScreen")
-ax3.set_xlabel("")
-
-for ax in [ax1, ax2, ax3]:
-    ax.coords[0].set_ticks_visible(False)
-    ax.coords[0].set_ticklabel_visible(False)
-    ax.coords[1].set_ticks_visible(False)
-    ax.coords[1].set_ticklabel_visible(False)
-
-
-plt.subplots_adjust(wspace=0)
-fig.tight_layout()
+fig = plt.figure()
+ax = fig.add_subplot(projection=aia193_map)
+aia193_map.plot(axes=ax, title="Using SphericalScreen")
+with SphericalScreen(aia171_map.observer_coordinate, only_off_disk=True):
+    aia171_map.draw_contours(axes=ax, levels=[1000, 2500] * u.DN, colors="green")
 
 plt.show()
+
+# sphinx_gallery_thumbnail_number = 3
