6f7882e137a6d777dc409c38645a717c9141c7d1
Prisha Sharma <psharma1_be22@thapar.edu>
2025-02-20 00:22:24 +0530

    changed test_models.py

--- sunpy/tests/test_models.py
+++ sunpy/tests/test_models.py
@@ -14,11 +14,14 @@ def test_valid_model_loading(model_name):
 
 def test_cache_functionality():
     """
-    Ensure model caching works properly for a single model.
+    Ensure model caching works properly for a specific model.
     """
-    model_name = next(iter(sun_models._MODELS.keys()))  
+    model_name = "avrett_loeser_2008"
+    assert model_name in sun_models._MODELS
+
     model_data_1 = getattr(sun_models, model_name)
     model_data_2 = getattr(sun_models, model_name)
+
     assert model_data_1 is model_data_2
     assert model_name in sun_models._MODEL_CACHE
 
