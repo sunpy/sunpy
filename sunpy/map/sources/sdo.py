ddb462003c901445ad930f04582a73f4ccc0b4c4
Will Barnes <will.t.barnes@gmail.com>
2025-03-13 11:31:58 -0400

    allow non-FITS-standard units on HMI map

--- sunpy/map/sources/sdo.py
+++ sunpy/map/sources/sdo.py
@@ -166,6 +166,15 @@ class HMIMap(GenericMap):
         self.meta['T_OBS'] = parse_time(date).utc.isot
 
     @property
+    def unit(self):
+        unit_str = self.meta.get('bunit', None)
+        if unit_str is None:
+            return
+        # Maxwells aren't in the IAU unit style manual and therefore not a valid FITS unit
+        # The mapbase unit property forces this validation, so we must override it to prevent it.
+        return u.Unit(unit_str)
+
+    @property
     def detector(self):
         return self.meta.get("detector", "HMI")
 
@@ -245,15 +254,6 @@ class HMISynopticMap(HMIMap):
 
     def _set_reference_date(self, date):
         self.meta['T_OBS'] = parse_time(date).utc.isot
-
-    @property
-    def unit(self):
-        unit_str = self.meta.get('bunit', None)
-        if unit_str is None:
-            return
-        # Maxwells aren't in the IAU unit style manual and therefore not a valid FITS unit
-        # The mapbase unit property forces this validation, so we must override it to prevent it.
-        return u.Unit(unit_str)
 
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
