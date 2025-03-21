611b786236fa560a78fdc1a3efda87ec2a557cd6
Chris Lowder <chris.lowder@swri.org>
2025-03-21 10:41:31 -0600

    describes a bit more about PUNCH and map functions

--- sunpy/map/sources/punch.py
+++ sunpy/map/sources/punch.py
@@ -11,9 +11,7 @@ class PUNCHMap(GenericMap):
     """
     PUNCH Map.
 
-    The Polarimeter to Unify the Corona and Heliosphere (PUNCH) is a constellation observatory consisting of four satellites, together imaging the solar corona in polarized white light.
-
-    # TODO - More mission details here - WFI, NFI, mosaic, etc.
+    The Polarimeter to Unify the Corona and Heliosphere (PUNCH) is a constellation observatory consisting of four satellites, together imaging the solar corona in polarized white light. Coordinated observations from three Wide Field Imagers (WFIs) and one Near Field Imager (NFI) are processed and meshed into one virtual observatory with a field of view spanning 45-degrees in radius away from the Sun.
 
     PUNCH launched and began operations on 11 March 2025.
 
@@ -36,13 +34,12 @@ class PUNCHMap(GenericMap):
 
     @property
     def reference_date(self):
-        """
-        The reference date for the coordinate system.
-        """
+        """Reference date for the coordinate system."""
         return self._get_date('DATE-OBS') or super().reference_date
 
     @property
     def unit(self):
+        """Units of the observation."""
         unit_str = self.meta.get('bunit', self.meta.get("BUNIT"))
         if unit_str is None:
             return
@@ -50,18 +47,18 @@ class PUNCHMap(GenericMap):
 
     @property
     def observatory(self):
+        """Observatory identifier."""
         return self.meta.get("OBSRVTRY")
 
     @property
     def instrument(self):
+        """Instrument identifier."""
         return self.meta.get("INSTRUME")
 
 
     # Used by the Map factory to determine if this subclass should be used
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
-        """
-        Determines if data, header corresponds to a PUNCH image
-        """
+        """Determines if data, header corresponds to a PUNCH image."""
         # Returns True only if this is data and header from PUNCH
         return header.get('instrume', '').startswith('NFI') or header.get('instrume', '').startswith('WFI') or header.get('obsrvtry', '').startswith('PUNCH')
