993c131cc46ae9fc301918b8a9d71dce4e970c5a
SunPyBot <sunpybot@users.noreply.github.com>
2026-09-09 11:01:42 +0000

    Run cruft update from package template

--- sunpy/version.py
+++ sunpy/version.py
@@ -10,7 +10,7 @@ except Exception:
     import warnings
 
     warnings.warn(
-        f'could not determine {__name__.split(".")[0]} package version; this indicates a broken installation',
+        f"could not determine {__name__.split('.')[0]} package version; this indicates a broken installation",
         stacklevel=2,
     )
     del warnings
